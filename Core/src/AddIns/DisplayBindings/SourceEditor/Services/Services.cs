22167352495f25369125f7259dd93c324d9dc323
Lluis Sanchez <lluis@novell.com>
2005-09-27 23:25:01 +0000

    Forgot one file.
    svn path=/trunk/monodevelop/; revision=50897

---
+++ Core/src/AddIns/DisplayBindings/SourceEditor/Services/Services.cs
@@ -0,0 +1,59 @@
+using System;
+
+using MonoDevelop.Core;
+using MonoDevelop.Core.Execution;
+using MonoDevelop.Core.Gui;
+using MonoDevelop.Projects;
+
+namespace MonoDevelop.SourceEditor
+{
+	class Services
+	{
+		static IDebuggingService debuggingService;
+		static MessageService messageService;
+		static ResourceService resourceService;
+		static LanguageBindingService languageBindingService;
+		static DispatchService dispatchService;
+
+		public static MessageService MessageService {
+			get {
+				if (messageService == null)
+					messageService = (MessageService) ServiceManager.GetService (typeof(MessageService));
+				return messageService;
+			}
+		}
+	
+		public static ResourceService Resources {
+			get {
+				if (resourceService == null)
+					resourceService = (ResourceService) ServiceManager.GetService (typeof(ResourceService));
+				return resourceService;
+			}
+		}
+	
+		public static IDebuggingService DebuggingService {
+			get {
+				if (debuggingService == null)
+					debuggingService = (IDebuggingService) ServiceManager.GetService (typeof(IDebuggingService));
+				return debuggingService;
+			}
+		}
+	
+		public static LanguageBindingService Languages {
+			get {
+				if (languageBindingService == null)
+					languageBindingService = (LanguageBindingService) ServiceManager.GetService (typeof(LanguageBindingService));
+				return languageBindingService;
+			}
+		}
+	
+		public static DispatchService DispatchService {
+			get {
+				if (dispatchService == null)
+					dispatchService = (DispatchService) ServiceManager.GetService (typeof(DispatchService));
+				return dispatchService;
+			}
+		}
+	}
+}
+
