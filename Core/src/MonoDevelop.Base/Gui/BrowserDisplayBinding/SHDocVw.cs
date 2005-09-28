a40e5d276ae94b31c64b9427aaebb2af99e8e8a5
Lluis Sanchez <lluis@novell.com>
2005-09-28 14:49:55 +0000

    Assembly reorganization: remove unused dirs.
    svn path=/trunk/monodevelop/; revision=50944

--- Core/src/MonoDevelop.Base/Gui/BrowserDisplayBinding/SHDocVw.cs
+++
@@ -1,6 +0,0 @@
-// <file>
-//     <copyright see="prj:///doc/copyright.txt"/>
-//     <license see="prj:///doc/license.txt"/>
-//     <owner name="Mike Krüger" email="mike@icsharpcode.net"/>
-//     <version value="$version"/>
-// </file>
