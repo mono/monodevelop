80256d4828be3cf4f50da69eb1d7d2899b93b861
Lluis Sanchez <lluis@novell.com>
2005-10-04 10:00:37 +0000

    2005-10-04  Lluis Sanchez Gual  <lluis@novell.com>
    	* Services.cs: Added missing file.
    	* Makefile.am: Fixed assembly references.
    
    
    svn path=/trunk/monodevelop/; revision=51160

---
+++ Extras/DebuggerAddIn/Services.cs
@@ -0,0 +1,58 @@
+using System;
+using MonoDevelop.Core;
+using MonoDevelop.Core.Gui;
+using MonoDevelop.Core.Execution;
+using MonoDevelop.Projects.Gui;
+using MonoDevelop.Projects.Ambience;
+
+namespace MonoDevelop.Debugger
+{
+	public class Services
+	{
+		static IDebuggingService debuggingService;
+		static ResourceService resourceService;
+		static IconService icons;
+		static AmbienceService ambienceService;
+		static DispatchService dispatchService;
+		
+		public static DispatchService DispatchService {
+			get {
+				if (dispatchService == null)
+					dispatchService = (DispatchService) ServiceManager.GetService (typeof(DispatchService));
+				return dispatchService;
+			}
+		}
+
+		public static IconService Icons {
+			get {
+				if (icons == null)
+					icons = (IconService) ServiceManager.GetService (typeof(IconService));
+				return icons;
+			}
+		}
+	
+		public static IDebuggingService DebuggingService {
+			get {
+				if (debuggingService == null)
+					debuggingService = (IDebuggingService) ServiceManager.GetService (typeof(IDebuggingService));
+				return debuggingService;
+			}
+		}
+	
+		internal static ResourceService Resources {
+			get {
+				if (resourceService == null)
+					resourceService = (ResourceService) ServiceManager.GetService (typeof(ResourceService));
+				return resourceService;
+			}
+		}
+
+		public static AmbienceService Ambience {
+			get {
+				if (ambienceService == null)
+					ambienceService = (AmbienceService) ServiceManager.GetService (typeof(AmbienceService));
+				return ambienceService;
+			}
+		}
+	}
+}
