ec3c202732198da6dc8221357bbc6b6cd9a86ab3
Jacob Ilsø Christensen <jacobilsoe@mono-cvs.ximian.com>
2005-12-15 18:28:29 +0000

    2005-12-15  Jacob Ilsoe Christensen  <jacobilsoe@gmail.com>
    	* configure.in:
    	* Extras/Makefile.am:
    	* Extra/ChangeLogAddIn/*:
    	Added ChangeLogAddIn.
    
    
    svn path=/trunk/monodevelop/; revision=54478

---
+++ Extras/ChangeLogAddIn/ChangeLogAddIn.cs
@@ -0,0 +1,118 @@
+using System;
+using System.IO;
+
+using MonoDevelop.Core;
+using MonoDevelop.Projects;
+using MonoDevelop.Components.Commands;
+using MonoDevelop.Ide.Gui;
+using MonoDevelop.Ide.Gui.Pads;
+using MonoDevelop.Ide.Gui.Content;
+
+namespace MonoDevelop.ChangeLogAddIn
+{
+        public enum ChangeLogCommands
+        {
+                InsertHeader,
+				InsertEntry
+        }
+
+	public class InsertHeaderHandler : CommandHandler
+	{
+		protected override void Run()
+		{
+			Document document = ChangeLogAddInHelperMethods.GetActiveChangeLogDocument();
+			
+			if (document == null)
+				return;
+			
+			IEditableTextBuffer textBuffer = document.Content as IEditableTextBuffer;			
+			
+			if (textBuffer == null)
+				return;
+		
+			string name = Runtime.Properties.GetProperty("ChangeLogAddIn.Name", "Full Name");
+			string email = Runtime.Properties.GetProperty("ChangeLogAddIn.Email", "Email Address");
+			string date = DateTime.Now.ToString("yyyy-MM-dd");
+			string text = date + "  " + name + " <" + email + "> " + Environment.NewLine + Environment.NewLine;
+
+			textBuffer.InsertText(textBuffer.CursorPosition, text);			
+		}
+
+		protected override void Update(CommandInfo info)
+		{
+			info.Enabled = ChangeLogAddInHelperMethods.GetActiveChangeLogDocument() != null;
+		}
+	}
+
+	public class InsertEntryHandler : CommandHandler
+	{
+		protected override void Run()
+		{
+			Document document = ChangeLogAddInHelperMethods.GetActiveChangeLogDocument();
+			
+			if (document == null)
+				return;
+			
+			IEditableTextBuffer textBuffer = document.Content as IEditableTextBuffer;			
+			
+			if (textBuffer == null)
+				return;
+
+			ProjectFile projectFile = GetSelectedProjectFile();
+
+			if (projectFile == null)
+				return;
+
+			string changeLogFileName = document.FileName;
+			string changeLogFileNameDirectory = Path.GetDirectoryName(changeLogFileName);
+			string selectedFileName = projectFile.Name;
+			string selectedFileNameDirectory = Path.GetDirectoryName(selectedFileName);
+	
+			if (selectedFileNameDirectory.StartsWith(changeLogFileNameDirectory))
+				textBuffer.InsertText(textBuffer.CursorPosition,
+					"\t* " + selectedFileName.Substring(changeLogFileNameDirectory.Length + 1) + ":" + Environment.NewLine); 				
+		}
+
+		protected override void Update(CommandInfo info)
+		{
+			info.Enabled = (ChangeLogAddInHelperMethods.GetActiveChangeLogDocument() != null) && (GetSelectedProjectFile() != null);
+		}
+
+		private ProjectFile GetSelectedProjectFile()
+		{
+			Pad pad = IdeApp.Workbench.Pads[typeof(SolutionPad)];
+
+			if (pad == null)
+				return null;
+
+			SolutionPad solutionPad = pad.Content as SolutionPad;
+
+			if (solutionPad == null)
+				return null;
+
+			ITreeNavigator navigator = solutionPad.GetSelectedNode();
+
+			if (navigator == null)
+				return null;
+
+			return navigator.DataItem as ProjectFile;			
+		}
+	}
+
+	internal class ChangeLogAddInHelperMethods
+	{
+		public static Document GetActiveChangeLogDocument()
+		{
+			Document document = IdeApp.Workbench.ActiveDocument;
+
+			if (document == null)
+				return null;
+			
+			if (document.FileName.EndsWith("ChangeLog"))
+				return document;
+			
+			return null;		
+		}		
+	}
+}
+	
---
+++ Extras/ChangeLogAddIn/ChangeLogAddInOptionsPanel.cs
@@ -0,0 +1,48 @@
+using Gtk;
+
+using MonoDevelop.Core;
+using MonoDevelop.Core.Gui.Dialogs;
+
+namespace MonoDevelop.ChangeLogAddIn
+{
+	public class ChangeLogAddInOptionPanel : AbstractOptionPanel
+	{
+		Entry nameEntry = new Entry();
+		Entry emailEntry = new Entry();
+		
+		public override void LoadPanelContents()
+		{
+			VBox hBox = new VBox();
+			this.Add(hBox);
+						
+			Label descriptionLabel = new Label(GettextCatalog.GetString("Specify personal information used in ChangeLog entries"));
+			descriptionLabel.SetAlignment(0.0f, 0.5f);
+			hBox.PackStart(descriptionLabel, false, false, 10);
+			
+			Label nameLabel = new Label(GettextCatalog.GetString("Full Name:"));
+			nameLabel.SetAlignment(0.0f, 0.5f);
+			nameEntry.Text = Runtime.Properties.GetProperty("ChangeLogAddIn.Name", "Full Name");
+
+			Label emailLabel = new Label(GettextCatalog.GetString("Email Address:"));
+			emailLabel.SetAlignment(0.0f, 0.5f);
+			emailEntry.Text = Runtime.Properties.GetProperty("ChangeLogAddIn.Email", "Email Address");
+
+			Table table = new Table(2, 2, false);
+			table.RowSpacing = 6;
+			table.ColumnSpacing = 6;
+			table.Attach(nameLabel, 0, 1, 0, 1, AttachOptions.Fill, AttachOptions.Fill, 0, 0);
+			table.Attach(nameEntry, 1, 2, 0, 1);
+			table.Attach(emailLabel, 0, 1, 1, 2, AttachOptions.Fill, AttachOptions.Fill, 0, 0);
+			table.Attach(emailEntry, 1, 2, 1, 2);
+
+			hBox.PackStart(table, false, false, 0);
+		}
+		
+		public override bool StorePanelContents()
+		{
+			Runtime.Properties.SetProperty("ChangeLogAddIn.Name", nameEntry.Text);
+			Runtime.Properties.SetProperty("ChangeLogAddIn.Email", emailEntry.Text);
+			return true;
+		}
+	}
+}
