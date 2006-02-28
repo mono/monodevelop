b2392566f8146bc74977d9a045ce8457d65e9b5b
Jacob Ilsø Christensen <jacobilsoe@mono-cvs.ximian.com>
2006-02-28 20:58:45 +0000

    2006-02-28  Jacob Ilsø Christensen  <jacobilsoe@gmail.com> 	* ChangeLogAddIn.addin.xml: 	* ChangeLogAddInOptionsPanel.cs: 	* ChangeLogAddIn.cs: 	Fixed spelling.
    svn path=/trunk/monodevelop/; revision=57429

--- Extras/ChangeLogAddIn/ChangeLogAddIn.cs
+++ Extras/ChangeLogAddIn/ChangeLogAddIn.cs
@@ -1,5 +1,5 @@
 /*
-Copyright (C) 2006  Jacob Ilsoe Christensen
+Copyright (C) 2006  Jacob Ilsø Christensen
 
 This program is free software; you can redistribute it and/or
 modify it under the terms of the GNU General Public License
