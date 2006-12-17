266e971ef4cb8437bc3a24ed236e1263c5fe4538
Lluis Sanchez <lluis@novell.com>
2006-12-17 17:37:59 +0000

    * MonoDevelop.Components/FileSelector.cs: Track changes in FileService   API.
    svn path=/trunk/monodevelop/; revision=69614

--- Core/src/MonoDevelop.Components/MonoDevelop.Components/FileSelector.cs
+++ Core/src/MonoDevelop.Components/MonoDevelop.Components/FileSelector.cs
@@ -9,7 +9,6 @@ namespace MonoDevelop.Components
 	{
 		const string LastPathProperty = "MonoDevelop.FileSelector.LastPath";
 		PropertyService propertyService = (PropertyService) ServiceManager.GetService (typeof (PropertyService));
-		FileUtilityService fileUtilityService = (FileUtilityService) ServiceManager.GetService (typeof (FileUtilityService));
 
 		public FileSelector () : this (GettextCatalog.GetString ("Open file..."), FileChooserAction.Open)
 		{
@@ -51,9 +50,9 @@ namespace MonoDevelop.Components
 				this.SetCurrentFolder (Environment.GetFolderPath (Environment.SpecialFolder.Personal));
 
 			// add default project path as a MD bookmark
-			string pathName = propertyService.GetProperty ("MonoDevelop.Core.Gui.Dialogs.NewProjectDialog.DefaultPath", fileUtilityService.GetDirectoryNameWithSeparator (Environment.GetFolderPath (Environment.SpecialFolder.Personal))).ToString ();
+			string pathName = propertyService.GetProperty ("MonoDevelop.Core.Gui.Dialogs.NewProjectDialog.DefaultPath", Runtime.FileService.GetDirectoryNameWithSeparator (Environment.GetFolderPath (Environment.SpecialFolder.Personal))).ToString ();
 
-			if (fileUtilityService.IsDirectory (pathName)) {
+			if (Runtime.FileService.IsDirectory (pathName)) {
 				try {
 					this.AddShortcutFolder (pathName);
 				} catch {
