b419fbe554a2a48b68cc923b42c36472767259f5
Lluis Sanchez <lluis@novell.com>
2007-04-19 11:41:44 +0000

    * AssemblyInfo.cs: Avoid version auto-increment. * MonoDevelop.WebReferences.mdp: Don't copy referenced assemblies. * Makefile.am: Flush.
    svn path=/trunk/monodevelop/; revision=75933

--- Extras/MonoDevelop.WebReferences/AssemblyInfo.cs
+++ Extras/MonoDevelop.WebReferences/AssemblyInfo.cs
@@ -23,7 +23,7 @@ using System.Runtime.CompilerServices;
 // You can specify all values by your own or you can build default build and revision
 // numbers with the '*' character (the default):
 
-[assembly: AssemblyVersion("0.1.3.*")]
+[assembly: AssemblyVersion("0.1.3.0")]
 
 // The following attributes specify the key for the sign of your assembly. See the
 // .NET Framework documentation for more information about signing.
