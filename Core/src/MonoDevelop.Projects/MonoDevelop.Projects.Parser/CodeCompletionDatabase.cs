967e68085527a6fc9a7f65f108a1405181aa87a9
Jeffrey Stedfast <fejj@novell.com>
2007-05-16 20:03:05 +0000

    2007-05-16  Jeffrey Stedfast  <fejj@novell.com>
    	* MonoDevelop.Projects.Parser/CodeCompletionDatabase.cs: Bump the
    	version number.
    
    
    svn path=/trunk/monodevelop/; revision=77509

--- Core/src/MonoDevelop.Projects/MonoDevelop.Projects.Parser/CodeCompletionDatabase.cs
+++ Core/src/MonoDevelop.Projects/MonoDevelop.Projects.Parser/CodeCompletionDatabase.cs
@@ -51,7 +51,7 @@ namespace MonoDevelop.Projects.Parser
 	{
 		static readonly int MAX_ACTIVE_COUNT = 100;
 		static readonly int MIN_ACTIVE_COUNT = 50;
-		static protected readonly int FORMAT_VERSION = 22;
+		static protected readonly int FORMAT_VERSION = 23;
 		
 		NamespaceEntry rootNamespace;
 		protected ArrayList references;
