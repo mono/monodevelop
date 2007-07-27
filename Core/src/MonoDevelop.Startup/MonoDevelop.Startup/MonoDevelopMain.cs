1f607f41237ad93788c49c6913b3a610b9bb66ca
Lluis Sanchez <lluis@novell.com>
2007-07-27 14:37:26 +0000

    * MonoDevelop.Startup/MonoDevelopMain.cs: If MD fails to start, rebuild   the add-in database and try again.
    svn path=/trunk/monodevelop/; revision=82854

--- Core/src/MonoDevelop.Startup/MonoDevelop.Startup/MonoDevelopMain.cs
+++ Core/src/MonoDevelop.Startup/MonoDevelop.Startup/MonoDevelopMain.cs
@@ -11,13 +11,26 @@ namespace MonoDevelop.Startup
 		public static int Main (string[] args)
 		{
 			Runtime.Initialize (true);
+			bool retry = false;
 			
-		//	AddinManager.CheckAssemblyLoadConflicts = true;
-			try {
-				return Runtime.ApplicationService.StartApplication ("IDE", args);
-			} finally {
-				Runtime.Shutdown ();
+			do {
+				try {
+					return Runtime.ApplicationService.StartApplication ("IDE", args);
+				} catch (Exception ex) {
+					if (!retry) {
+						AddinManager.Registry.Rebuild (new Mono.Addins.ConsoleProgressStatus (true));
+						retry = true;
+					} else {
+						Console.WriteLine (ex);
+						Console.WriteLine ("MonoDevelop failed to start. Some of the assemblies required to run MonoDevelop (for example gtk-sharp, gnome-sharp or gtkhtml-sharp) may not be properly installed in the GAC.");
+						retry = false;
+					}
+				} finally {
+					Runtime.Shutdown ();
+				}
 			}
+			while (retry);
+			return -1;
 		}
 	}
 }
