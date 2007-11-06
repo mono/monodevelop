bcfabb295b58baca1df0c35bcfa38aa2d001abb0
Mike Krüger <mkrueger@novell.com>
2007-11-06 08:28:30 +0000

    Changed some copyright headers (made the old #develop files explicit GPL).
    svn path=/trunk/monodevelop/; revision=88952

--- Core/src/MonoDevelop.Projects/MonoDevelop.Projects/TypelibImporter.cs
+++ Core/src/MonoDevelop.Projects/MonoDevelop.Projects/TypelibImporter.cs
@@ -1,9 +1,22 @@
-// <file>
-//     <copyright see="prj:///doc/copyright.txt"/>
-//     <license see="prj:///doc/license.txt"/>
-//     <owner name="Poul Staugaard" email="poul@staugaard.dk"/>
-//     <version value="$version"/>
-// </file>
+//  TypelibImporter.cs
+//
+//  This file was derived from a file from #Develop. 
+//
+//  Copyright (C) 2001-2007 Poul Staugaard <poul@staugaard.dk>
+// 
+//  This program is free software; you can redistribute it and/or modify
+//  it under the terms of the GNU General Public License as published by
+//  the Free Software Foundation; either version 2 of the License, or
+//  (at your option) any later version.
+// 
+//  This program is distributed in the hope that it will be useful,
+//  but WITHOUT ANY WARRANTY; without even the implied warranty of
+//  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+//  GNU General Public License for more details.
+//  
+//  You should have received a copy of the GNU General Public License
+//  along with this program; if not, write to the Free Software
+//  Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
 #if false
 using System;
 using System.IO;
