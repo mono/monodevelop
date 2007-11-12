e36e6fae8e558ddbc6555ba4e9bc2065992777cd
Michael Hutchinson <mhutchinson@novell.com>
2007-11-12 23:21:12 +0000

    * MonoDevelop.Core/Runtime.cs: Add new SetProcessName method.
    svn path=/trunk/monodevelop/; revision=89505

--- Core/src/MonoDevelop.Core/MonoDevelop.Core/Runtime.cs
+++ Core/src/MonoDevelop.Core/MonoDevelop.Core/Runtime.cs
@@ -28,6 +28,8 @@
 
 using System;
 using System.Collections;
+using System.Runtime.InteropServices;
+using System.Text;
 
 using MonoDevelop.Core;
 using MonoDevelop.Core.AddIns;
@@ -124,6 +126,33 @@ namespace MonoDevelop.Core
 				if (applicationService == null)
 					applicationService = new ApplicationService ();
 				return applicationService;
+			}
+		}
+		
+		public static void SetProcessName (string name)
+		{
+			try {
+				unixSetProcessName (name);
+			} catch (Exception e) {
+				LoggingService.LogError ("Error setting process name", e);
+			}
+		}
+		
+		[DllImport ("libc")] // Linux
+		private static extern int prctl (int option, byte [] arg2, IntPtr arg3, IntPtr arg4, IntPtr arg5);
+		
+		[DllImport ("libc")] // BSD
+		private static extern void setproctitle (byte [] fmt, byte [] str_arg);
+		
+		//this is from http://abock.org/2006/02/09/changing-process-name-in-mono/
+		static void unixSetProcessName (string name)
+		{
+			try {
+				if (prctl (15 /* PR_SET_NAME */, Encoding.ASCII.GetBytes (name + "\0"), IntPtr.Zero, IntPtr.Zero, IntPtr.Zero) != 0) {
+					throw new ApplicationException ("Error setting process name: " + Mono.Unix.Native.Stdlib.GetLastError ());
+				}
+			} catch (EntryPointNotFoundException) {
+				setproctitle (Encoding.ASCII.GetBytes ("%s\0"), Encoding.ASCII.GetBytes (name + "\0"));
 			}
 		}
 	}
