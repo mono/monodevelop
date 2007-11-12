36605249edd69903ae9a75a40fdd2721816fa38e
Michael Hutchinson <mhutchinson@novell.com>
2007-11-12 23:22:36 +0000

    * MonoDevelop.Startup/MonoDevelopMain.cs: Use SetProcessName; "killall   monodevelop" will work now. Convert some WriteLines to logging   calls.
    svn path=/trunk/monodevelop/; revision=89506

--- Core/src/MonoDevelop.Startup/MonoDevelop.Startup/MonoDevelopMain.cs
+++ Core/src/MonoDevelop.Startup/MonoDevelop.Startup/MonoDevelopMain.cs
@@ -16,15 +16,17 @@ namespace MonoDevelop.Startup
 			
 			do {
 				try {
+					Runtime.SetProcessName ("monodevelop");
 					IdeStartup app = new IdeStartup ();
 					return app.Run (args);
 				} catch (Exception ex) {
 					if (!retry) {
+						LoggingService.LogWarning ("MonoDevelop failed to start. Rebuilding addins registry.");
 						AddinManager.Registry.Rebuild (new Mono.Addins.ConsoleProgressStatus (true));
+						LoggingService.LogInfo ("Addin registry rebuilt. Restarting MonoDevelop.");
 						retry = true;
 					} else {
-						Console.WriteLine (ex);
-						Console.WriteLine ("MonoDevelop failed to start. Some of the assemblies required to run MonoDevelop (for example gtk-sharp, gnome-sharp or gtkhtml-sharp) may not be properly installed in the GAC.");
+						LoggingService.LogFatalError ("MonoDevelop failed to start. Some of the assemblies required to run MonoDevelop (for example gtk-sharp, gnome-sharp or gtkhtml-sharp) may not be properly installed in the GAC.", ex);
 						retry = false;
 					}
 				} finally {
