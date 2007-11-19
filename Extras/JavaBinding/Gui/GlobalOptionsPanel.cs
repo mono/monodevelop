8b1f6e69b42db47b2075b00b8735e54dcc454a18
Lluis Sanchez <lluis@novell.com>
2007-11-19 23:43:49 +0000

    * Gui/GlobalOptionsPanel.cs, Gui/ProjectConfigurationPropertyPanel.cs,   JavaBinding.mdp, Makefile.am: Migrated to Stetic.
    svn path=/trunk/monodevelop/; revision=89950

--- Extras/JavaBinding/Gui/GlobalOptionsPanel.cs
+++ Extras/JavaBinding/Gui/GlobalOptionsPanel.cs
@@ -11,45 +11,6 @@ namespace JavaBinding
 {
 	public class GlobalOptionsPanelPanel : AbstractOptionPanel
 	{
-		class GlobalOptionsPanelWidget : GladeWidgetExtract 
-		{
-			//
-			// Gtk Controls	
-			//
-			[Glade.Widget] Gnome.FileEntry ikvmPathEntry;
-			[Glade.Widget] Entry compilerPathEntry;
-			[Glade.Widget] Entry classpathEntry;
-			[Glade.Widget] ComboBox compilerCombo;
-			
-			// compiler chooser
-			
- 			public GlobalOptionsPanelWidget () : base ("Java.glade", "GlobalOptionsPanel")
- 			{
-				ListStore store = new ListStore (typeof (string));
-				store.AppendValues (GettextCatalog.GetString ("Javac"));
-				store.AppendValues (GettextCatalog.GetString ("Gcj"));
-				compilerCombo.Model = store;
-				CellRendererText cr = new CellRendererText ();
-				compilerCombo.PackStart (cr, true);
-				compilerCombo.AddAttribute (cr, "text", 0);
-				compilerCombo.Active = (int) JavaLanguageBinding.Properties.CompilerType;
-				
-				ikvmPathEntry.Filename = JavaLanguageBinding.Properties.IkvmPath;
-				compilerPathEntry.Text = JavaLanguageBinding.Properties.CompilerCommand;
-				classpathEntry.Text = JavaLanguageBinding.Properties.Classpath;
-			}
-			
-			public bool Store ()
-			{
-				JavaLanguageBinding.Properties.IkvmPath = ikvmPathEntry.Filename;
-				JavaLanguageBinding.Properties.CompilerCommand = compilerPathEntry.Text;
-				JavaLanguageBinding.Properties.Classpath = classpathEntry.Text;
-				
-				JavaLanguageBinding.Properties.CompilerType = (JavaCompiler) compilerCombo.Active;
-				return true;
-			}
-		}
-
 		GlobalOptionsPanelWidget widget;
 		
 		public override void LoadPanelContents()
@@ -62,6 +23,37 @@ namespace JavaBinding
 			bool result = true;
 			result = widget.Store ();
  			return result;
+		}
+	}
+
+	partial class GlobalOptionsPanelWidget : Gtk.Bin 
+	{
+		public GlobalOptionsPanelWidget ()
+		{
+			Build ();
+			
+			ListStore store = new ListStore (typeof (string));
+			store.AppendValues (GettextCatalog.GetString ("Javac"));
+			store.AppendValues (GettextCatalog.GetString ("Gcj"));
+			compilerCombo.Model = store;
+			CellRendererText cr = new CellRendererText ();
+			compilerCombo.PackStart (cr, true);
+			compilerCombo.AddAttribute (cr, "text", 0);
+			compilerCombo.Active = (int) JavaLanguageBinding.Properties.CompilerType;
+			
+			ikvmPathEntry.Path = JavaLanguageBinding.Properties.IkvmPath;
+			compilerPathEntry.Text = JavaLanguageBinding.Properties.CompilerCommand;
+			classpathEntry.Text = JavaLanguageBinding.Properties.Classpath;
+		}
+		
+		public bool Store ()
+		{
+			JavaLanguageBinding.Properties.IkvmPath = ikvmPathEntry.Path;
+			JavaLanguageBinding.Properties.CompilerCommand = compilerPathEntry.Text;
+			JavaLanguageBinding.Properties.Classpath = classpathEntry.Text;
+			
+			JavaLanguageBinding.Properties.CompilerType = (JavaCompiler) compilerCombo.Active;
+			return true;
 		}
 	}
 }
--- Extras/JavaBinding/Gui/ProjectConfigurationPropertyPanel.cs
+++ Extras/JavaBinding/Gui/ProjectConfigurationPropertyPanel.cs
@@ -30,96 +30,6 @@ namespace JavaBinding
 {
 	public class ProjectConfigurationPropertyPanel : AbstractOptionPanel
 	{
-		class CodeGenerationPanelWidget : GladeWidgetExtract 
-		{
-			//
-			// Gtk Controls	
-			//
-			[Glade.Widget] Entry symbolsEntry;
-			[Glade.Widget] Entry mainClassEntry;
-			[Glade.Widget] Entry compilerEntry;
-			[Glade.Widget] Entry classPathEntry;
-			[Glade.Widget] ComboBox compileTargetCombo;
-			[Glade.Widget] CheckButton generateWarningsCheckButton;
-			[Glade.Widget] CheckButton deprecationCheckButton;
-			[Glade.Widget] CheckButton enableOptimizationCheckButton;
-			[Glade.Widget] CheckButton warningsAsErrorsCheckButton;
-			[Glade.Widget] CheckButton generateDebugInformationCheckButton;
-			[Glade.Widget] RadioButton compilerGcjButton;
-			[Glade.Widget] RadioButton compilerJavacButton;
-			
-			// compiler chooser
-			
-			JavaCompilerParameters compilerParameters = null;
-			DotNetProjectConfiguration configuration;
-			
- 			public CodeGenerationPanelWidget (Properties CustomizationObject) : base ("Java.glade", "CodeGenerationPanel")
- 			{	
-				configuration = ((Properties)CustomizationObject).Get<DotNetProjectConfiguration> ("Config");
-				compilerParameters = (JavaCompilerParameters) configuration.CompilationParameters;
-				
-				ListStore store = new ListStore (typeof (string));
-				store.AppendValues (GettextCatalog.GetString ("Executable"));
-				store.AppendValues (GettextCatalog.GetString ("Library"));
-				compileTargetCombo.Model = store;
-				CellRendererText cr = new CellRendererText ();
-				compileTargetCombo.PackStart (cr, true);
-				compileTargetCombo.AddAttribute (cr, "text", 0);
-				compileTargetCombo.Active = (int) configuration.CompileTarget;
-
-				if (compilerParameters.Compiler == JavaCompiler.Javac)
-					compilerJavacButton.Active = true;
-				else
-					compilerGcjButton.Active = true;
-					
-				compilerJavacButton.Toggled += new EventHandler (OnCompilerToggled);
-				compilerGcjButton.Toggled += new EventHandler (OnCompilerToggled);
-	
-				enableOptimizationCheckButton.Active = compilerParameters.Optimize;
-				generateDebugInformationCheckButton.Active = configuration.DebugMode;
-				deprecationCheckButton.Active = compilerParameters.Deprecation;
-				generateWarningsCheckButton.Active = compilerParameters.GenWarnings;
-				warningsAsErrorsCheckButton.Active = !configuration.RunWithWarnings;
-				
-				compilerEntry.Text = compilerParameters.CompilerPath;
-				classPathEntry.Text = compilerParameters.ClassPath;				
-				mainClassEntry.Text = compilerParameters.MainClass;				
-				symbolsEntry.Text = compilerParameters.DefineSymbols;				
-			}
-			
-			void OnCompilerToggled (object o, EventArgs args)
-			{
-				if (compilerJavacButton.Active)
-					compilerEntry.Text = "javac";
-				else
-					compilerEntry.Text = "gcj";
-			}
-			
-			public bool Store ()
-			{
-				if (compilerParameters == null)
-					return true;
-
-				if (compilerJavacButton.Active)
-					compilerParameters.Compiler = JavaCompiler.Javac;
-				else
-					compilerParameters.Compiler = JavaCompiler.Gcj;
-
-				configuration.CompileTarget = (CompileTarget) compileTargetCombo.Active;
-				compilerParameters.GenWarnings = generateWarningsCheckButton.Active;			
-				compilerParameters.Deprecation = deprecationCheckButton.Active;			
-				configuration.DebugMode = generateDebugInformationCheckButton.Active;			
-				compilerParameters.Optimize = enableOptimizationCheckButton.Active;
-				configuration.RunWithWarnings = !warningsAsErrorsCheckButton.Active;
-				
-				compilerParameters.CompilerPath = compilerEntry.Text;
-				compilerParameters.ClassPath = classPathEntry.Text;
-				compilerParameters.MainClass = mainClassEntry.Text;
-				compilerParameters.DefineSymbols = symbolsEntry.Text;
-				return true;
-			}
-		}
-
 		CodeGenerationPanelWidget widget;
 		
 		public override void LoadPanelContents()
@@ -134,4 +44,80 @@ namespace JavaBinding
  			return result;
 		}
 	}
+
+	partial class CodeGenerationPanelWidget : Gtk.Bin 
+	{
+		JavaCompilerParameters compilerParameters = null;
+		DotNetProjectConfiguration configuration;
+		
+		public CodeGenerationPanelWidget (Properties CustomizationObject)
+		{	
+			Build ();
+			
+			configuration = ((Properties)CustomizationObject).Get<DotNetProjectConfiguration> ("Config");
+			compilerParameters = (JavaCompilerParameters) configuration.CompilationParameters;
+			
+			ListStore store = new ListStore (typeof (string));
+			store.AppendValues (GettextCatalog.GetString ("Executable"));
+			store.AppendValues (GettextCatalog.GetString ("Library"));
+			compileTargetCombo.Model = store;
+			CellRendererText cr = new CellRendererText ();
+			compileTargetCombo.PackStart (cr, true);
+			compileTargetCombo.AddAttribute (cr, "text", 0);
+			compileTargetCombo.Active = (int) configuration.CompileTarget;
+
+			if (compilerParameters.Compiler == JavaCompiler.Javac)
+				compilerJavacButton.Active = true;
+			else
+				compilerGcjButton.Active = true;
+				
+			compilerJavacButton.Toggled += new EventHandler (OnCompilerToggled);
+			compilerGcjButton.Toggled += new EventHandler (OnCompilerToggled);
+
+			enableOptimizationCheckButton.Active = compilerParameters.Optimize;
+			generateDebugInformationCheckButton.Active = configuration.DebugMode;
+			deprecationCheckButton.Active = compilerParameters.Deprecation;
+			generateWarningsCheckButton.Active = compilerParameters.GenWarnings;
+			warningsAsErrorsCheckButton.Active = !configuration.RunWithWarnings;
+			
+			compilerEntry.Text = compilerParameters.CompilerPath;
+			classPathEntry.Text = compilerParameters.ClassPath;				
+			mainClassEntry.Text = compilerParameters.MainClass;				
+			symbolsEntry.Text = compilerParameters.DefineSymbols;	
+			OnCompilerToggled (null, null);
+		}
+		
+		void OnCompilerToggled (object o, EventArgs args)
+		{
+			if (compilerJavacButton.Active)
+				compilerEntry.Text = "javac";
+			else
+				compilerEntry.Text = "gcj";
+		}
+		
+		public bool Store ()
+		{
+			if (compilerParameters == null)
+				return true;
+
+			if (compilerJavacButton.Active)
+				compilerParameters.Compiler = JavaCompiler.Javac;
+			else
+				compilerParameters.Compiler = JavaCompiler.Gcj;
+
+			configuration.CompileTarget = (CompileTarget) compileTargetCombo.Active;
+			compilerParameters.GenWarnings = generateWarningsCheckButton.Active;			
+			compilerParameters.Deprecation = deprecationCheckButton.Active;			
+			configuration.DebugMode = generateDebugInformationCheckButton.Active;			
+			compilerParameters.Optimize = enableOptimizationCheckButton.Active;
+			configuration.RunWithWarnings = !warningsAsErrorsCheckButton.Active;
+			
+			compilerParameters.CompilerPath = compilerEntry.Text;
+			compilerParameters.ClassPath = classPathEntry.Text;
+			compilerParameters.MainClass = mainClassEntry.Text;
+			compilerParameters.DefineSymbols = symbolsEntry.Text;
+			return true;
+		}
+	}
+
 }
