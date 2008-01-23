539eb80e20319523a522dea0b238ad722fc16aa6
Michael Hutchinson <mhutchinson@novell.com>
2008-01-23 21:36:43 +0000

    2008-01-11  Michael Hutchinson <mhutchinson@novell.com>
    	* MonoDevelop.Projects/DotNetProject.cs: Set an attribute on the project
    	  options so that language bindings know whether to set debug defines on
    	  the new configuration.
    
    
    svn path=/branches/monodevelop/main/1.0/; revision=93717

--- main/src/core/MonoDevelop.Projects/MonoDevelop.Projects/DotNetProject.cs
+++ main/src/core/MonoDevelop.Projects/MonoDevelop.Projects/DotNetProject.cs
@@ -147,12 +147,14 @@ namespace MonoDevelop.Projects
 			languageBinding = FindLanguage (language);
 			
 			if (languageBinding != null) {
+				projectOptions.SetAttribute ("DefineDebug", "True");
 				DotNetProjectConfiguration configuration = (DotNetProjectConfiguration) CreateConfiguration ("Debug");
 				configuration.CompilationParameters = languageBinding.CreateCompilationParameters (projectOptions);
 				Configurations.Add (configuration);
 				
 				configuration = (DotNetProjectConfiguration) CreateConfiguration ("Release");
 				configuration.DebugMode = false;
+				projectOptions.SetAttribute ("DefineDebug", "False");
 				configuration.CompilationParameters = languageBinding.CreateCompilationParameters (projectOptions);
 				Configurations.Add (configuration);
 			}
