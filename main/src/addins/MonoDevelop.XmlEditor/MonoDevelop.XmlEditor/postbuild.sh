c4bc5cab64d42948e789d38fd36baf15ee384518
Michael Hutchinson <mhutchinson@novell.com>
2008-03-06 19:25:41 +0000

    Xml editor reorg part 2
    svn path=/trunk/monodevelop/; revision=97613

--- main/src/addins/MonoDevelop.XmlEditor/MonoDevelop.XmlEditor/postbuild.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-cp MonoDevelop.XmlEditor.addin.xml ../../build/AddIns/XmlEditor
-mkdir -p ../../build/AddIns/XmlEditor/schemas
-cp -r schemas/*.xsd ../../build/AddIns/XmlEditor/schemas
-cp -r schemas/*.txt ../../build/AddIns/XmlEditor/schemas
-cp -r schemas/*.html ../../build/AddIns/XmlEditor/schemas
