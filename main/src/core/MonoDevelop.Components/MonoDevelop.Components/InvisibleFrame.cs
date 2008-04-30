29786620419be829b7354498a88bf4b30abd21d3
Michael Hutchinson <mhutchinson@novell.com>
2008-04-30 18:54:32 +0000

    * Makefile.am, MonoDevelop.Components.mdp,   MonoDevelop.Components/InvisibleFrame.cs: Add a simple bin widget,   for plugging/replacing widgets in more complex layouts.
    svn path=/trunk/monodevelop/; revision=102231

---
+++ main/src/core/MonoDevelop.Components/MonoDevelop.Components/InvisibleFrame.cs
@@ -0,0 +1,64 @@
+// 
+// InvisibleFrame.cs
+// 
+// Author:
+//   Michael Hutchinson <mhutchinson@novell.com>
+// 
+// Copyright (C) 2008 Novell, Inc (http://www.novell.com)
+// 
+// Permission is hereby granted, free of charge, to any person obtaining
+// a copy of this software and associated documentation files (the
+// "Software"), to deal in the Software without restriction, including
+// without limitation the rights to use, copy, modify, merge, publish,
+// distribute, sublicense, and/or sell copies of the Software, and to
+// permit persons to whom the Software is furnished to do so, subject to
+// the following conditions:
+// 
+// The above copyright notice and this permission notice shall be
+// included in all copies or substantial portions of the Software.
+// 
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+// EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+// MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+// NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE
+// LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION
+// OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION
+// WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+//
+
+using System;
+using Gtk;
+
+namespace MonoDevelop.Components
+{
+	
+	public class InvisibleFrame : Bin
+	{
+		public InvisibleFrame()
+		{
+		}
+		
+		protected override void OnSizeRequested (ref Requisition requisition)
+		{
+			Widget c = Child;
+			if (c != null)
+				c.GetSizeRequest (out requisition.Width, out requisition.Height);
+		}
+		
+		protected override void OnSizeAllocated (Gdk.Rectangle allocation)
+		{
+			Widget c = Child;
+			if (c != null)
+				c.SizeAllocate (allocation);
+		}
+		
+		public Widget ReplaceChild (Widget widget)
+		{
+			Widget old = Child;
+			if (old != null)
+				Remove (old);
+			Add (widget);
+			return old;
+		}
+	}
+}
