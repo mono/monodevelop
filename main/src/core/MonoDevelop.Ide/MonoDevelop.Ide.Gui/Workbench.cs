a214ff6dc198da069044fcee2dfcf7a78dc5e5e9
Lluis Sanchez <lluis@novell.com>
2008-05-21 15:20:19 +0000

    * MonoDevelop.Ide.Gui/Workbench.cs,   MonoDevelop.Ide.Gui/SdiWorkspaceLayout.cs: Fix bug 361084 -   Dragging a tab to a different position doesn't change its position   in the tab order.
    svn path=/trunk/monodevelop/; revision=103701

--- main/src/core/MonoDevelop.Ide/MonoDevelop.Ide.Gui/Workbench.cs
+++ main/src/core/MonoDevelop.Ide/MonoDevelop.Ide.Gui/Workbench.cs
@@ -760,6 +760,17 @@ namespace MonoDevelop.Ide.Gui
 					return pad;
 			return null;
 		}
+		
+		internal void ReorderDocuments (int oldPlacement, int newPlacement)
+		{
+			IViewContent content = workbench.ViewContentCollection[oldPlacement];
+			workbench.InternalViewContentCollection.RemoveAt (oldPlacement);
+			workbench.InternalViewContentCollection.Insert (newPlacement, content);
+			
+			Document doc = documents [oldPlacement];
+			documents.RemoveAt (oldPlacement);
+			documents.Insert (newPlacement, doc);
+		}
 	}
 	
 	class FileInformation
