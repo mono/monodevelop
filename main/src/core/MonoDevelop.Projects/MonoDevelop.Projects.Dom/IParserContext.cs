156970b84fa6f8485709628caf78717be0dea8a2
Mike Krüger <mkrueger@novell.com>
2008-06-10 15:44:37 +0000

    * MonoDevelop.Projects.Dom/ProjectCodeCompletionDatabase.cs,   MonoDevelop.Projects.Dom/ExpressionResult.cs,   MonoDevelop.Projects.Dom/IParserContext.cs,   MonoDevelop.Projects.Dom/IExpressionFinder.cs,   MonoDevelop.Projects.Dom/ExpressionContext.cs,   MonoDevelop.Projects.Parser/ExpressionResult.cs: Worked on dom.
    svn path=/trunk/monodevelop/; revision=105447

---
+++ main/src/core/MonoDevelop.Projects/MonoDevelop.Projects.Dom/IParserContext.cs
@@ -0,0 +1,117 @@
+//
+// IParserContext.cs
+//
+// Author:
+//   Mike Krüger <mkrueger@novell.com>
+//
+// Copyright (C) 2008 Novell, Inc (http://www.novell.com)
+//
+// Permission is hereby granted, free of charge, to any person obtaining
+// a copy of this software and associated documentation files (the
+// "Software"), to deal in the Software without restriction, including
+// without limitation the rights to use, copy, modify, merge, publish,
+// distribute, sublicense, and/or sell copies of the Software, and to
+// permit persons to whom the Software is furnished to do so, subject to
+// the following conditions:
+// 
+// The above copyright notice and this permission notice shall be
+// included in all copies or substantial portions of the Software.
+// 
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+// EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+// MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+// NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE
+// LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION
+// OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION
+// WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+//
+
+using System;
+using System.Collections.Generic;
+
+namespace MonoDevelop.Projects.Dom
+{
+	public interface IParserContext
+	{
+		IExpressionFinder GetExpressionFinderFor (string fileName);
+		ICompilationUnit  GetCompilationUnitFor (string fileName);
+		
+		List<IMember> GetNamespaceContents (string nsName);
+		SearchTypeResult SearchType (SearchTypeRequest request);
+		
+		IType LookupType (IReturnType returnType);
+	}
+	
+	public class SearchTypeRequest
+	{
+		string name;
+		int    genericParameterCount;
+		ICompilationUnit currentCompilationUnit;
+		int    caretLine, caretColumn;
+		
+		public string Name {
+			get {
+				return name;
+			}
+		}
+		
+		public int GenericParameterCount {
+			get {
+				return genericParameterCount;
+			}
+		}
+		
+		public ICompilationUnit CurrentCompilationUnit {
+			get {
+				return currentCompilationUnit;
+			}
+		}
+		
+		public int CaretLine {
+			get {
+				return caretLine;
+			}
+		}
+		
+		public int CaretColumn {
+			get {
+				return caretColumn;
+			}
+		}
+		
+		public SearchTypeRequest (ICompilationUnit currentCompilationUnit, int caretLine, int caretColumn, string name)
+		{
+			this.currentCompilationUnit = currentCompilationUnit;
+			this.caretLine   = caretLine;
+			this.caretColumn = caretColumn;
+			this.name        = name;
+			this.genericParameterCount = -1;
+		}
+		
+		public SearchTypeRequest (ICompilationUnit currentCompilationUnit, int caretLine, int caretColumn, string name, int genericParameterCount)
+		{
+			this.currentCompilationUnit = currentCompilationUnit;
+			this.caretLine   = caretLine;
+			this.caretColumn = caretColumn;
+			this.name        = name;
+			this.genericParameterCount = genericParameterCount;
+		}
+		
+	}
+	
+	public class SearchTypeResult 
+	{
+		IReturnType result;
+		
+		public IReturnType Result {
+			get {
+				return result;
+			}
+		}
+		
+		public SearchTypeResult (IReturnType result)
+		{
+			this.result = result;
+		}
+	}
+}
