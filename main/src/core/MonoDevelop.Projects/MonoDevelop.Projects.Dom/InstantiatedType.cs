33832eef317550ca1bd0f4718f43a0d71efc8c78
Mike Krüger <mkrueger@novell.com>
2008-10-21 21:25:28 +0000

    * MonoDevelop.Projects/MonoDevelop.Projects.Dom/InstantiatedType.cs:   changed header.
    svn path=/trunk/monodevelop/; revision=116676

--- main/src/core/MonoDevelop.Projects/MonoDevelop.Projects.Dom/InstantiatedType.cs
+++ main/src/core/MonoDevelop.Projects/MonoDevelop.Projects.Dom/InstantiatedType.cs
@@ -1,7 +1,29 @@
-// InstantiatedType.cs created with MonoDevelop
-// User: mkrueger at 23:17 21.10.2008
 //
-// To change standard headers go to Edit->Preferences->Coding->Standard Headers
+// InstantiatedType.cs
+//
+// Author:
+//   Mike Krüger <mkrueger@novell.com>
+//
+// Copyright (C) 2008 Novell, Inc (http://www.novell.com)
+//
+// Permission is hereby granted, free of charge, to any person obtaining
+// a copy of this software and associated documentation files (the
+// "Software"), to deal in the Software without restriction, including
+// without limitation the rights to use, copy, modify, merge, publish,
+// distribute, sublicense, and/or sell copies of the Software, and to
+// permit persons to whom the Software is furnished to do so, subject to
+// the following conditions:
+// 
+// The above copyright notice and this permission notice shall be
+// included in all copies or substantial portions of the Software.
+// 
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+// EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+// MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+// NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE
+// LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION
+// OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION
+// WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 //
 
 using System;
@@ -11,25 +33,15 @@ namespace MonoDevelop.Projects.Dom
 {
 	public class InstantiatedType : DomType
 	{
-		IType             uninstantiatedType;
-		IList<IReturnType> genericParameters;
 		
 		public IList<IReturnType> GenericParameters {
-			get {
-				return genericParameters;
-			}
-			set {
-				genericParameters = value;
-			}
+			get;
+			set;
 		}
 
 		public IType UninstantiatedType {
-			get {
-				return uninstantiatedType;
-			}
-			set {
-				uninstantiatedType = value;
-			}
+			get;
+			set;
 		}
 
 		
