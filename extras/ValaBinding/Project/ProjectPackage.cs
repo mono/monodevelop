fda7b20b507e8b3fdc9707773727a276c2e67ed4
Levi Bard <levibard@mono-cvs.ximian.com>
2009-01-12 19:09:53 +0000

    Use relative path for project package references.
    svn path=/trunk/monodevelop/; revision=123108

--- extras/ValaBinding/Project/ProjectPackage.cs
+++ extras/ValaBinding/Project/ProjectPackage.cs
@@ -32,6 +32,7 @@
 
 using System;
 using System.IO;
+using System.Text;
 using System.Text.RegularExpressions;
 using System.Collections.Generic;
 
@@ -241,5 +242,53 @@ namespace MonoDevelop.ValaBinding
 			
 			return dirs.ToArray ();
 		}
+		
+		/// <summary>
+		/// Converts an absolute path to a relative one
+		/// </summary>
+		/// <param name="absolutePath">
+		/// A <see cref="System.String"/>: The absolute path
+		/// </param>
+		/// <param name="relativeTo">
+		/// A <see cref="System.String"/>: The path to which the output path shall be relative
+		/// </param>
+		/// <returns>
+		/// A <see cref="System.String"/>: The relative path from relativeTo to absolutePath
+		/// </returns>
+		public static string ToRelativePath (string absolutePath, string relativeTo) {
+			List<string>  fileTokens = new List<string> (absolutePath.Split (Path.DirectorySeparatorChar)),
+						  anchorTokens = new List<string> (relativeTo.Split (Path.DirectorySeparatorChar));
+			StringBuilder builder = new StringBuilder ();
+			int length = 0;
+
+			if (!Path.IsPathRooted(absolutePath)){ return absolutePath; }
+			if (absolutePath == relativeTo){ return Path.GetFileName (absolutePath); }
+	
+			if (absolutePath.StartsWith (relativeTo) && Directory.Exists (relativeTo)) {
+				builder.AppendFormat (".{0}", Path.DirectorySeparatorChar);
+			}// if absolutePath is inside relativeTo
+	
+			for (;0 != fileTokens.Count && 0 != anchorTokens.Count;) {
+				if (fileTokens[0] == anchorTokens[0]) {
+					fileTokens.RemoveAt (0);
+					anchorTokens.RemoveAt (0);
+				} else { break; }
+			}// strip identical leading path
+	
+			for (int i=0; i < anchorTokens.Count-1; ++i) {
+				builder.AppendFormat ("..{0}", Path.DirectorySeparatorChar);
+			}// navigate out of anchor subdir
+	
+			foreach (string token in fileTokens) {
+				builder.AppendFormat ("{0}{1}", token, Path.DirectorySeparatorChar);
+			}// append filepath
+	
+			length = builder.Length;
+			if (0 < builder.Length && Path.DirectorySeparatorChar == builder[builder.Length-1]) {
+				--length;
+			}// check for trailing separator
+
+			return builder.ToString (0, length);
+		}// ToRelativePath
 	}
 }
