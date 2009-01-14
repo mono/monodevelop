651a4455765cdea078d08db29fb4cf49dfe6d0dd
Lluis Sanchez <lluis@novell.com>
2009-01-14 17:09:15 +0000

    * MonoDevelop.Ide/MonoDevelop.Ide.Gui.Dialogs/GoToDialog.cs: Revert last   change, the full name is required for opening the file.
    svn path=/trunk/monodevelop/; revision=123380

--- main/src/core/MonoDevelop.Ide/MonoDevelop.Ide.Gui.Dialogs/GoToDialog.cs
+++ main/src/core/MonoDevelop.Ide/MonoDevelop.Ide.Gui.Dialogs/GoToDialog.cs
@@ -363,7 +363,7 @@ namespace MonoDevelop.Ide.Gui.Dialogs
 
 			foreach (Project p in projects) {
 				foreach (ProjectFile file in p.Files) {
-					list.Add (p.GetRelativeChildPath (file.FilePath));
+					list.Add (file.FilePath);
 				}
 			}
 			return list;
