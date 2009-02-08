17ba9410b1b23693699e5e80c7b07c1db27b935d
Michael Hutchinson <mhutchinson@novell.com>
2009-02-08 01:19:24 +0000

    * BuildVariables.cs: Include in svn so that autotools isn't required   to build MD.
    svn path=/trunk/monodevelop/; revision=126222

---
+++ main/src/core/MonoDevelop.Ide/BuildVariables.cs
@@ -0,0 +1,9 @@
+namespace MonoDevelop.Ide
+{
+	internal class BuildVariables
+	{
+		public static string PackageVersion = "1.9.2";
+		public static string PackageVersionLabel = "2.0 Beta 1";
+	}
+}
+
