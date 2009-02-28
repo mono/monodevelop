00c82d1aceedc96a975288fb809840f3a3453a19
Michael Hutchinson <mhutchinson@novell.com>
2009-02-28 04:50:41 +0000

    * MonoDevelop.AspNet.Mvc/Gui: * MonoDevelop.AspNet.Mvc/AspMvcCommands.cs: * MonoDevelop.AspNet.Mvc/MonoDevelop.AspNet.Mvc.addin.xml: * MonoDevelop.AspNet.Mvc/Gui/FolderNodeBuilderExtension.cs: Add   context menu commands for adding views and controllers to the   appropriate folders.
    * MonoDevelop.AspNet.Mvc/MonoDevelop.AspNet.Mvc.csproj: Updated.
    
    svn path=/trunk/monodevelop/; revision=128257

---
+++ extras/MonoDevelop.AspNet.Mvc/MonoDevelop.AspNet.Mvc/Gui/FolderNodeBuilderExtension.cs
@@ -0,0 +1,100 @@
+// 
+// FolderNodeBuilderExtension.cs
+//  
+// Author:
+//       Michael Hutchinson <mhutchinson@novell.com>
+// 
+// Copyright (c) 2009 Novell, Inc. (http://www.novell.com)
+// 
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+// 
+// The above copyright notice and this permission notice shall be included in
+// all copies or substantial portions of the Software.
+// 
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+// THE SOFTWARE.
+
+using System;
+using MonoDevelop.Components.Commands;
+using MonoDevelop.Projects;
+using MonoDevelop.Ide.Gui.Components;
+using MonoDevelop.Ide.Gui.Pads.ProjectPad;
+
+namespace MonoDevelop.AspNet.Mvc.Gui
+{
+	
+	class FolderNodeBuilderExtension : NodeBuilderExtension
+	{
+		public override bool CanBuildNode (Type dataType)
+		{
+			return typeof (ProjectFolder).IsAssignableFrom (dataType);
+		}
+		
+		public override Type CommandHandlerType {
+			get { return typeof (FolderCommandHandler); }
+		}
+	}
+	
+	class FolderCommandHandler : NodeCommandHandler
+	{
+		[CommandUpdateHandler (AspMvcCommands.AddController)]
+		public void AddControllerUpdate (CommandInfo info)
+		{
+			ProjectFolder pf = (ProjectFolder)CurrentNode.DataItem;
+			string rootName = System.IO.Path.Combine (pf.Project.BaseDirectory, "Controllers");
+			info.Enabled = info.Visible = pf.Path.StartsWith (rootName)
+				&& (pf.Path.Length == rootName.Length || pf.Path[rootName.Length] == System.IO.Path.DirectorySeparatorChar);
+		}
+		
+		[CommandHandler (AspMvcCommands.AddController)]
+		public void AddController ()
+		{
+			AddFile ("AspMvcController");
+		}
+		
+		[CommandUpdateHandler (AspMvcCommands.AddView)]
+		public void AddViewUpdate (CommandInfo info)
+		{
+			ProjectFolder pf = (ProjectFolder)CurrentNode.DataItem;
+			string rootName = System.IO.Path.Combine (pf.Project.BaseDirectory, "Views");
+			info.Enabled = info.Visible = pf.Path.StartsWith (rootName)
+				&& (pf.Path.Length == rootName.Length || pf.Path[rootName.Length] == System.IO.Path.DirectorySeparatorChar);
+		}
+		
+		[CommandHandler (AspMvcCommands.AddView)]
+		public void AddView ()
+		{
+			AddFile ("AspMvcViewPage");
+		}
+		
+		//adapted from GtkCore
+		void AddFile (string id)
+		{
+			AspMvcProject project = CurrentNode.GetParentDataItem (typeof(AspMvcProject), true) as AspMvcProject;
+			if (project == null)
+				return;
+			
+			object currentItem = CurrentNode.DataItem;
+				
+			ProjectFolder folder = CurrentNode.GetParentDataItem (typeof(ProjectFolder), true) as ProjectFolder;
+			string path = folder != null? folder.Path : project.BaseDirectory;
+
+			MonoDevelop.Ide.Gui.IdeApp.ProjectOperations.CreateProjectFile (project, path, id);
+			MonoDevelop.Ide.Gui.IdeApp.ProjectOperations.Save (project);
+			
+			ITreeNavigator nav = Tree.GetNodeAtObject (currentItem);
+			if (nav != null)
+				nav.Expanded = true;
+		}
+	}
+}
