c492159a1e430b43dc0bb23217339af2c860efba
Mike Krüger <mkrueger@novell.com>
2009-03-12 16:50:10 +0000

    * Makefile.am: * CSharpBinding.csproj: * CSharpBinding.addin.xml: * Parser/CSharpFormatter.cs: * FormattingStrategy/CSharpFormattingStyleDefinition.xml: Worked on   code formatter.
    svn path=/trunk/monodevelop/; revision=129162

---
+++ main/src/addins/CSharpBinding/Parser/CSharpFormatter.cs
@@ -0,0 +1,87 @@
+// 
+// CSharpFormatter.cs
+//  
+// Author:
+//       Mike Krüger <mkrueger@novell.com>
+// 
+// Copyright (c) 2009 Mike Krüger
+// 
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+// 
+// The above copyright notice and this permission notice shall be included in
+// all copies or substantial portions of the Software.
+// 
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+// THE SOFTWARE.
+
+using System;
+using System.Text;
+using MonoDevelop.Ide.Gui.Content;
+using MonoDevelop.Projects;
+using MonoDevelop.Projects.Text;
+
+namespace CSharpBinding.Parser
+{
+	public class CSharpFormatter : AbstractFormatter
+	{
+		public override bool CanFormat (string mimeType)
+		{
+			return mimeType == "text/x-csharp";
+		}
+		
+		static int GetNextTabstop (int currentColumn, int tabSize)
+		{
+			int result = currentColumn + tabSize;
+			return (result / tabSize) * tabSize;
+		}
+		
+		protected override string InternalFormat (SolutionItem policyParent, string input, int startOffset, int endOffset)
+		{
+			TextStylePolicy currentPolicy = policyParent != null
+					? policyParent.Policies.Get<TextStylePolicy> ()
+					: MonoDevelop.Projects.Policies.PolicyService.GetDefaultPolicy<TextStylePolicy> ();
+			input = input ?? "";
+			int line = 0, col = 0;
+			string eolMarker = currentPolicy.GetEolMarker ();
+			StringBuilder result = new StringBuilder ();
+			
+			for (int i = startOffset; i <= endOffset; i++) {
+				char ch = input[i];
+				switch (ch) {
+				case '\t':
+					if (currentPolicy.TabsToSpaces) {
+						int tabWidth = GetNextTabstop (col, currentPolicy.TabWidth) - col;
+						result.Append (new string (' ', tabWidth));
+						col += tabWidth;
+					} else 
+						goto default;
+					break;
+				case '\r':
+					if (i + 1 < input.Length && input[i + 1] == '\n')
+						i++;
+					goto case '\n';
+				case '\n':
+					result.Append (eolMarker);
+					line++;
+					col = 0;
+					break;
+				default:
+					result.Append (ch);
+					col++;
+					break;
+				}
+			}
+			return result.ToString ();
+		}
+	}
+}
