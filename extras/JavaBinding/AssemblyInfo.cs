a975df0ad6543d0e3ba6293f486bf5790c3df951
Lluis Sanchez <lluis@novell.com>
2009-03-17 02:08:00 +0000

    Bump MD version
    svn path=/branches/monodevelop/extras/JavaBinding/2.0/; revision=129522

--- extras/JavaBinding/AssemblyInfo.cs
+++ extras/JavaBinding/AssemblyInfo.cs
@@ -5,5 +5,5 @@ using System.Reflection;
 [assembly: AssemblyProduct ("MonoDevelop")]
 [assembly: AssemblyTitle ("Java Language Binding")]
 [assembly: AssemblyDescription ("Java Language Binding")]
-[assembly: AssemblyVersion ("1.9.3")]
+[assembly: AssemblyVersion ("2.0")]
 [assembly: AssemblyCopyright ("GPL")]
