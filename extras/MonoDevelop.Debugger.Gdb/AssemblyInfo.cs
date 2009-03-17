e40b94260d929cd779f0aaf87a931178cb130b01
Lluis Sanchez <lluis@novell.com>
2009-03-17 02:09:28 +0000

    Bump MD version
    svn path=/branches/monodevelop/extras/MonoDevelop.Debugger.Gdb/2.0/; revision=129525

--- extras/MonoDevelop.Debugger.Gdb/AssemblyInfo.cs
+++ extras/MonoDevelop.Debugger.Gdb/AssemblyInfo.cs
@@ -5,5 +5,5 @@ using System.Reflection;
 [assembly: AssemblyProduct ("MonoDevelop")]
 [assembly: AssemblyTitle ("GDB support for Mono.Debugging")]
 [assembly: AssemblyDescription ("GNU Debugger support for Mono.Debugging")]
-[assembly: AssemblyVersion ("1.9.3")]
+[assembly: AssemblyVersion ("2.0")]
 [assembly: AssemblyCopyright ("MIT X11")]
