ce93ba1af83f2bd913539c931cdfee0432a3d1f5
Mike Krüger <mkrueger@novell.com>
2009-04-17 07:12:19 +0000

    * MonoDevelop.Core.Gui/IMementoCapable.cs: Changed IMementoCapable   interface.
    svn path=/trunk/monodevelop/; revision=131973

--- main/src/core/MonoDevelop.Core.Gui/MonoDevelop.Core.Gui/IMementoCapable.cs
+++ main/src/core/MonoDevelop.Core.Gui/MonoDevelop.Core.Gui/IMementoCapable.cs
@@ -1,43 +1,38 @@
-//  IMementoCapable.cs
 //
-//  This file was derived from a file from #Develop. 
+// IMementoCapable.cs
 //
-//  Copyright (C) 2001-2007 Mike Krüger <mkrueger@novell.com>
+// Author:
+//   Mike Krüger <mkrueger@novell.com>
+//
+// Copyright (C) 2009 Novell, Inc (http://www.novell.com)
+//
+// Permission is hereby granted, free of charge, to any person obtaining
+// a copy of this software and associated documentation files (the
+// "Software"), to deal in the Software without restriction, including
+// without limitation the rights to use, copy, modify, merge, publish,
+// distribute, sublicense, and/or sell copies of the Software, and to
+// permit persons to whom the Software is furnished to do so, subject to
+// the following conditions:
 // 
-//  This program is free software; you can redistribute it and/or modify
-//  it under the terms of the GNU General Public License as published by
-//  the Free Software Foundation; either version 2 of the License, or
-//  (at your option) any later version.
+// The above copyright notice and this permission notice shall be
+// included in all copies or substantial portions of the Software.
 // 
-//  This program is distributed in the hope that it will be useful,
-//  but WITHOUT ANY WARRANTY; without even the implied warranty of
-//  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
-//  GNU General Public License for more details.
-//  
-//  You should have received a copy of the GNU General Public License
-//  along with this program; if not, write to the Free Software
-//  Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
-
-using MonoDevelop.Core;
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+// EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+// MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+// NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE
+// LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION
+// OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION
+// WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+//
 
 namespace MonoDevelop.Core.Gui
 {
-	/// <summary>
-	/// This interface flags an object beeing "mementocapable". This means that the
-	/// state of the object could be saved to an <see cref="Properties"/> object
-	/// and set from a object from the same class.
-	/// This is used to save and restore the state of GUI objects.
-	/// </summary>
 	public interface IMementoCapable
 	{
-		/// <summary>
-		/// Creates a new memento from the state.
-		/// </summary>
-		ICustomXmlSerializer CreateMemento ();
-		
-		/// <summary>
-		/// Sets the state to the given memento.
-		/// </summary>
-		void SetMemento (ICustomXmlSerializer memento);
+		ICustomXmlSerializer Memento {
+			get;
+			set;
+		}
 	}
 }
