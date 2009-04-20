72b80be16d34c3cafba4025f4c8e50cdcd935549
Mike Krüger <mkrueger@novell.com>
2009-04-20 11:01:50 +0000

    * Makefile.am: * MonoDevelop.Projects.Gui.csproj: * MonoDevelop.Projects.Gui.Completion/TextUtilities.cs: * MonoDevelop.Projects.Gui.Completion/DeclarationViewWindow.cs: * MonoDevelop.Projects.Gui.Dialogs.OptionPanels/OutputOptionsPanel.cs: * MonoDevelop.Projects.Gui.Dialogs.OptionPanels/CombineBuildOptions.cs: * MonoDevelop.Projects.Gui.Dialogs.OptionPanels/GeneralProjectOptions.cs: * gtk-gui/MonoDevelop.Projects.Gui.Dialogs.OptionPanels.StartupOptionsPanelWidget.cs:   Relicensed some old stuff. Removed text utilities. These functions   can't be done on that level. The backend bindings have to handle   this stuff.
    svn path=/trunk/monodevelop/; revision=132138

--- main/src/core/MonoDevelop.Projects.Gui/MonoDevelop.Projects.Gui.Dialogs.OptionPanels/CombineBuildOptions.cs
+++ main/src/core/MonoDevelop.Projects.Gui/MonoDevelop.Projects.Gui.Dialogs.OptionPanels/CombineBuildOptions.cs
@@ -1,61 +1,58 @@
-//  CombineBuildOptions.cs
+// CombineBuildOptions.cs
 //
-//  This file was derived from a file from #Develop. 
+// Author:
+//   Mike Krüger <mkrueger@novell.com>
 //
-//  Copyright (C) 2001-2007 Mike Krüger <mkrueger@novell.com>
-// 
-//  This program is free software; you can redistribute it and/or modify
-//  it under the terms of the GNU General Public License as published by
-//  the Free Software Foundation; either version 2 of the License, or
-//  (at your option) any later version.
-// 
-//  This program is distributed in the hope that it will be useful,
-//  but WITHOUT ANY WARRANTY; without even the implied warranty of
-//  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
-//  GNU General Public License for more details.
-//  
-//  You should have received a copy of the GNU General Public License
-//  along with this program; if not, write to the Free Software
-//  Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
+// Copyright (c) 2009 Novell, Inc (http://www.novell.com)
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in
+// all copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+// THE SOFTWARE.
+
 using System;
-using System.Collections;
-using System.ComponentModel;
-
-using MonoDevelop.Core;
-using MonoDevelop.Core.Gui.Dialogs;
-using MonoDevelop.Projects;
-
-using Gtk;
-using MonoDevelop.Components;
 
 namespace MonoDevelop.Projects.Gui.Dialogs.OptionPanels
 {
-	internal class CombineBuildOptions : ItemOptionsPanel
+	class CombineBuildOptions : ItemOptionsPanel
 	{
 		CombineBuildOptionsWidget widget;
 		
-		public override Widget CreatePanelWidget()
+		public override Gtk.Widget CreatePanelWidget()
 		{
-			return (widget = new  CombineBuildOptionsWidget (ConfiguredSolution));
+			return widget = new  CombineBuildOptionsWidget (ConfiguredSolution);
 		}
-
+		
 		public override void ApplyChanges ()
 		{
 			widget.Store ();
-		}					
+		}
 	}
 
 	partial class CombineBuildOptionsWidget : Gtk.Bin 
 	{
 		Solution solution;
-
+		
 		public CombineBuildOptionsWidget (Solution solution)
 		{
 			Build ();
 			this.solution = solution;
 			folderEntry.Path = solution.OutputDirectory;
 		}
-
+		
 		public void Store()
 		{
 			solution.OutputDirectory = folderEntry.Path;
