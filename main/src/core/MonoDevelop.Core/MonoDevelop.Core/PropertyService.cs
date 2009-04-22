d8e8d8ff64229894d54b3d77082aa9e3b79ef9df
Michael Hutchinson <mhutchinson@novell.com>
2009-04-22 00:13:43 +0000

    * MonoDevelop.Core/PropertyService.cs: Add an IsMac property.
    svn path=/trunk/monodevelop/; revision=132343

--- main/src/core/MonoDevelop.Core/MonoDevelop.Core/PropertyService.cs
+++ main/src/core/MonoDevelop.Core/MonoDevelop.Core/PropertyService.cs
@@ -73,6 +73,33 @@ namespace MonoDevelop.Core
 			}
 		}
 		
+		public static bool IsMac {
+			get; private set;
+		}
+		
+		//From Managed.Windows.Forms/XplatUI
+		static bool IsRunningOnMac ()
+		{
+			IntPtr buf = IntPtr.Zero;
+			try {
+				buf = System.Runtime.InteropServices.Marshal.AllocHGlobal (8192);
+				// This is a hacktastic way of getting sysname from uname ()
+				if (uname (buf) == 0) {
+					string os = System.Runtime.InteropServices.Marshal.PtrToStringAnsi (buf);
+					if (os == "Darwin")
+						return true;
+				}
+			} catch {
+			} finally {
+				if (buf != IntPtr.Zero)
+					System.Runtime.InteropServices.Marshal.FreeHGlobal (buf);
+			}
+			return false;
+		}
+		
+		[System.Runtime.InteropServices.DllImport ("libc")]
+		static extern int uname (IntPtr buf);
+		
 		static PropertyService ()
 		{
 			if (!LoadProperties (Path.Combine (ConfigPath, FileName))) {
@@ -85,6 +112,7 @@ namespace MonoDevelop.Core
 				if (PropertyChanged != null)
 					PropertyChanged (sender, args);
 			};
+			IsMac = IsRunningOnMac ();
 		}
 		
 		static bool LoadProperties (string fileName)
