f6ef9308317d0846e95cd8eb7a485fde9187f983
Mike Krüger <mkrueger@novell.com>
2009-04-24 11:21:29 +0000

    * AssemblyInfo.cs: Changed assembly info.
    svn path=/trunk/monodevelop/; revision=132570

--- main/src/addins/ChangeLogAddIn/AssemblyInfo.cs
+++ main/src/addins/ChangeLogAddIn/AssemblyInfo.cs
@@ -6,4 +6,4 @@ using System.Reflection;
 [assembly: AssemblyTitle ("ChangeLog Add-in")]
 [assembly: AssemblyDescription ("Add-in for working with ChangeLog files")]
 [assembly: AssemblyVersion ("2.1.0")]
-[assembly: AssemblyCopyright ("GPL")]
+[assembly: AssemblyCopyright ("MIT X11")]
