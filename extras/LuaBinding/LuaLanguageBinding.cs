44a1c4a8c9b6407b6a2d76aad5af2b15f79a152f
Mike Krüger <mkrueger@novell.com>
2009-04-26 07:25:16 +0000

    Added simple lua binding.
    svn path=/trunk/monodevelop/; revision=132680

---
+++ extras/LuaBinding/LuaLanguageBinding.cs
@@ -0,0 +1,118 @@
+// 
+// MyClass.cs
+//  
+// Author:
+//       Mike Krüger <mkrueger@novell.com>
+// 
+// Copyright (c) 2009 Novell, Inc (http://www.novell.com)
+// 
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+// 
+// The above copyright notice and this permission notice shall be included in
+// all copies or substantial portions of the Software.
+// 
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+// THE SOFTWARE.
+
+using System;
+using MonoDevelop.Projects.Dom.Parser;
+using MonoDevelop.Projects.CodeGeneration;
+using MonoDevelop.Projects;
+using MonoDevelop.Core;
+
+
+namespace LuaBinding
+{
+	public class LuaLanguageBinding : IDotNetLanguageBinding
+	{
+		public string Language {
+			get { 
+				return "Lua"; 
+			}
+		}
+		
+		public string SingleLineCommentTag { 
+			get { 
+				return "--"; 
+			} 
+		}
+		public string BlockCommentStartTag { 
+			get { 
+				return "--["; 
+			}
+		}
+		public string BlockCommentEndTag { 
+			get { 
+				return "]--"; 
+			} 
+		}
+		
+		public IParser Parser {
+			get { 
+				return null; 
+			}
+		}
+		
+		public IRefactorer Refactorer {
+			get { 
+				return null; 
+			}
+		}
+		
+		
+		public bool IsSourceCodeFile (string fileName)
+		{
+			string ext = System.IO.Path.GetExtension (fileName);
+			return ext.Equals(".lua", StringComparison.OrdinalIgnoreCase);
+		}
+		
+		public string GetFileName (string baseName)
+		{
+			return baseName + ".lua";
+		}
+
+		#region IDotNetLanguageBinding implementation
+		public BuildResult Compile (ProjectItemCollection items, DotNetProjectConfiguration configuration, MonoDevelop.Core.IProgressMonitor monitor)
+		{
+			return null;
+		}
+		
+		public ProjectParameters CreateProjectParameters (System.Xml.XmlElement projectOptions)
+		{
+			return new LuaCompilerParameters ();
+		}
+		
+		public ConfigurationParameters CreateCompilationParameters (System.Xml.XmlElement projectOptions)
+		{
+			return new LuaCompilerParameters ();
+		}
+		
+		public System.CodeDom.Compiler.CodeDomProvider GetCodeDomProvider ()
+		{
+			return null;
+		}
+		
+		public MonoDevelop.Core.ClrVersion[] GetSupportedClrVersions ()
+		{
+			return new ClrVersion[] { ClrVersion.Net_1_1 };
+		}
+		
+		public string ProjectStockIcon {
+			get {
+				return "md-project";
+			}
+		}
+		#endregion
+
+	}
+}
