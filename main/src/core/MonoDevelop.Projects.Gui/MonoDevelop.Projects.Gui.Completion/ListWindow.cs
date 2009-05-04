3aaa99d1c23c356b0d069ab6783d11a41e03fa5b
Mike Krüger <mkrueger@novell.com>
2009-05-04 11:22:15 +0000

    * MonoDevelop.Projects.Gui.Completion/ListWindow.cs: Changed default   for completewithpunctuation
    svn path=/trunk/monodevelop/; revision=133479

--- main/src/core/MonoDevelop.Projects.Gui/MonoDevelop.Projects.Gui.Completion/ListWindow.cs
+++ main/src/core/MonoDevelop.Projects.Gui/MonoDevelop.Projects.Gui.Completion/ListWindow.cs
@@ -185,7 +185,7 @@ namespace MonoDevelop.Projects.Gui.Completion
 		
 		bool CompleteWithSpaceOrPunctuation {
 			get {
-				return MonoDevelop.Core.PropertyService.Get ("CompleteWithSpaceOrPunctuation", false);
+				return MonoDevelop.Core.PropertyService.Get ("CompleteWithSpaceOrPunctuation", true);
 			}
 		}
 		
