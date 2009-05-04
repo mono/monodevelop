92a5298f310b9cf8512c48c021937720d92704eb
Lluis Sanchez <lluis@novell.com>
2009-05-04 11:50:57 +0000

    * WindowsPlatform.cs: * WindowsPlatform.csproj: Implemented mime type and file icon support.
    svn path=/trunk/monodevelop/; revision=133486

--- main/src/addins/WindowsPlatform/WindowsPlatform.cs
+++ main/src/addins/WindowsPlatform/WindowsPlatform.cs
@@ -1,7 +1,12 @@
 using System;
+using System.IO;
+using System.Drawing;
+using System.Drawing.Imaging;
 using MonoDevelop.Core.Gui;
 using System.Runtime.InteropServices;
 using System.Collections;
+using System.Collections.Generic;
+using Microsoft.Win32;
 
 namespace MonoDevelop.Platform
 {
@@ -38,6 +43,56 @@ namespace MonoDevelop.Platform
 		
 		public override void SetFileAttributes (string fileName, object attributes)
 		{
+		}
+		
+		protected override string OnGetMimeTypeForUri (string uri)
+		{
+			string ext = Path.GetExtension (uri).ToLower ();
+			
+			RegistryKey typeKey = Registry.ClassesRoot.OpenSubKey (ext, false);
+			if (typeKey == null)
+				return base.OnGetMimeTypeForUri (uri);
+			try {
+				string type = (string) typeKey.GetValue ("Content Type");
+				if (type == null) {
+					string ptype = (string) typeKey.GetValue ("PerceivedType");
+					if (ptype == "text")
+						type = "text/plain";
+				}
+				return type ?? base.OnGetMimeTypeForUri (uri);
+			}
+			finally {
+				typeKey.Close ();
+			}
+		}
+		
+		protected override Gdk.Pixbuf OnGetPixbufForType (string type, Gtk.IconSize size)
+		{
+			return base.OnGetPixbufForType (type, size);
+		}
+		
+		Dictionary<string, Gdk.Pixbuf> icons = new Dictionary<string, Gdk.Pixbuf> ();
+		
+		protected override Gdk.Pixbuf OnGetPixbufForFile (string filename, Gtk.IconSize size)
+		{
+			SHFILEINFO shinfo = new SHFILEINFO();
+			Win32.SHGetFileInfo (filename, 0, ref shinfo, (uint) Marshal.SizeOf (shinfo), Win32.SHGFI_ICON | Win32.SHGFI_SMALLICON | Win32.SHGFI_ICONLOCATION | Win32.SHGFI_TYPENAME);
+			string key = shinfo.iIcon.ToString () + " - " + shinfo.szDisplayName;
+			Gdk.Pixbuf pix;
+			if (!icons.TryGetValue (key, out pix)) {
+				System.Drawing.Icon icon = System.Drawing.Icon.FromHandle (shinfo.hIcon);
+				pix = CreateFromResource (icon.ToBitmap ());
+				icons[key] = pix;
+			}
+			return pix;
+		}
+		
+		public Gdk.Pixbuf CreateFromResource (Bitmap bitmap)
+		{
+			MemoryStream ms = new MemoryStream ();
+			bitmap.Save (ms, ImageFormat.Png);
+			ms.Position = 0;
+			return new Gdk.Pixbuf (ms);
 		}
 	}
 }
