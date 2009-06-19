946ddf2c35ae75d88344a7ac598787b8eda1202d
Mike Krüger <mkrueger@novell.com>
2009-06-19 07:31:57 +0000

    * src/tools/mdrun/src/AssemblyInfo.cs: * src/tools/mdhost/src/AssemblyInfo.cs: Commented out empty assembly   key file attribute (Bug 513981 - Project template causes CS1726   errors).
    svn path=/trunk/monodevelop/; revision=136464

--- main/src/tools/mdrun/src/AssemblyInfo.cs
+++ main/src/tools/mdrun/src/AssemblyInfo.cs
@@ -29,5 +29,5 @@ using System.Runtime.CompilerServices;
 // The following attributes specify the key for the sign of your assembly. See the
 // .NET Framework documentation for more information about signing.
 // This is not required, if you don't want signing let these attributes like they're.
-[assembly: AssemblyDelaySign(false)]
-[assembly: AssemblyKeyFile("")]
+//[assembly: AssemblyDelaySign(false)]
+//[assembly: AssemblyKeyFile("")]
