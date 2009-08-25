d071ac9f35a51971bd8d3567d99ce84397e224e2
Mike Krüger <mkrueger@novell.com>
2009-08-25 20:16:28 +0000

    * MonoDevelop.CodeGeneration/NullCheckGenerator.cs: Only full   reference ArgumentNullException when System namespace isn't   imported.
    svn path=/trunk/monodevelop/; revision=140661

--- main/src/addins/MonoDevelop.Refactoring/MonoDevelop.CodeGeneration/NullCheckGenerator.cs
+++ main/src/addins/MonoDevelop.Refactoring/MonoDevelop.CodeGeneration/NullCheckGenerator.cs
@@ -97,7 +97,7 @@ namespace MonoDevelop.CodeGeneration
 					        new PrimitiveExpression (null)
 					    ), new ThrowStatement (
 					    	new ObjectCreateExpression (
-					        	new TypeReference ("System.ArgumentNullException"),
+					        	Options.MatchNamespaceImports (new TypeReference ("System.ArgumentNullException")),
 					            new List<Expression> { new PrimitiveExpression (member.Name) }
 							)
 					    )
