9666732bd2e562b805d5bd06175af3c7a4e7e2e1
Lluis Sanchez <lluis@novell.com>
2009-08-31 16:10:08 +0000

    * BuildVariables.cs: * icons/SplashScreen.png: * MonoDevelop.Ide.Gui.Dialogs/SplashScreen.cs: Updated splash window.
    svn path=/trunk/monodevelop/; revision=140967

--- main/src/core/MonoDevelop.Ide/BuildVariables.cs
+++ main/src/core/MonoDevelop.Ide/BuildVariables.cs
@@ -3,7 +3,7 @@ namespace MonoDevelop.Ide
 	internal class BuildVariables
 	{
 		public static string PackageVersion = "2.1.0";
-		public static string PackageVersionLabel = "2.1.0";
+		public static string PackageVersionLabel = "2.2 Beta 1";
 	}
 }
 
--- main/src/core/MonoDevelop.Ide/MonoDevelop.Ide.Gui.Dialogs/SplashScreen.cs
+++ main/src/core/MonoDevelop.Ide/MonoDevelop.Ide.Gui.Dialogs/SplashScreen.cs
@@ -58,10 +58,10 @@ namespace MonoDevelop.Ide.Gui.Dialogs {
 			Pango.Layout pl = new Pango.Layout (PangoContext);
 			Pango.FontDescription des = this.Style.FontDescription.Copy();
 			pl.FontDescription = des;
-		//	pl.SetMarkup("<b><span foreground='#cccccc'>" + BuildVariables.PackageVersionLabel + "</span></b>");
+			pl.SetMarkup("<b><span foreground='#cccccc'>" + BuildVariables.PackageVersionLabel + "</span></b>");
 			int w,h;
 			pl.GetPixelSize (out w, out h);
-			GdkWindow.DrawLayout (gc, bitmap.Width - w - 40, 110, pl);
+			GdkWindow.DrawLayout (gc, bitmap.Width - w - 75, 90, pl);
 			return base.OnExposeEvent (evt);
 		}
 		
