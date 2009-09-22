bf42fc35e075cbbe612442b72bd9668e152b1304
Mike Krüger <mkrueger@novell.com>
2009-09-22 12:14:45 +0000

    * Makefile.am: * CSharpBinding.csproj: * MonoDevelop.CSharp.Dom: * MonoDevelop.CSharp.Dom/Attribute.cs: * MonoDevelop.CSharp.Dom/Identifier.cs: * MonoDevelop.CSharp.Dom/TypeDeclaration.cs: * MonoDevelop.CSharp.Dom/AttributeSection.cs: * MonoDevelop.CSharp.Dom/UsingDeclaration.cs: * MonoDevelop.CSharp.Dom/DelegateDeclaration.cs: * MonoDevelop.CSharp.Dom/QualifiedIdentifier.cs: * MonoDevelop.CSharp.Dom/NamespaceDeclaration.cs: * MonoDevelop.CSharp.Dom/UsingAliasDeclaration.cs: Worked on c# dom.
    svn path=/trunk/monodevelop/; revision=142385

---
+++ main/src/addins/CSharpBinding/MonoDevelop.CSharp.Dom/AttributeSection.cs
@@ -0,0 +1,62 @@
+// 
+// AttributeSection.cs
+//  
+// Author:
+//       Mike Krüger <mkrueger@novell.com>
+// 
+// Copyright (c) 2009 Novell, Inc (http://www.novell.com)
+// 
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+// 
+// The above copyright notice and this permission notice shall be included in
+// all copies or substantial portions of the Software.
+// 
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+// THE SOFTWARE.
+
+using System;
+using System.Linq;
+using MonoDevelop.Projects.Dom;
+using System.Collections.Generic;
+
+namespace MonoDevelop.CSharp.Dom
+{
+	public class AttributeSection : AbstractNode
+	{
+		const int TargetRole = 101;
+		
+		
+		public string AttributeTarget {
+			get {
+				Identifier target = TargetIdentifier;
+				return target != null ? target.Name : null;
+			}
+		}
+		
+		public Identifier TargetIdentifier {
+			get {
+				return (Identifier)GetChildByRole (TargetRole);
+			}
+		}
+		
+		public IEnumerable<Attribute> Attributes {
+			get {
+				return base.GetChildrenByRole (AttributeRole).Cast <Attribute>();
+			}
+		}
+		
+		public AttributeSection ()
+		{
+		}
+	}
+}
