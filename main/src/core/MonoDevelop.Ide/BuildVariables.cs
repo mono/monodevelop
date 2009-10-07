51984f9f1d923253299c5b1b775ff127585aeaad
Lluis Sanchez <lluis@novell.com>
2009-10-07 15:27:52 +0000

    Created 2.1.1 branch.
    svn path=/branches/monodevelop/extras/MonoDevelop.Debugger.Win32/2.1.1/; revision=143676

--- main/src/core/MonoDevelop.Ide/BuildVariables.cs
+++
@@ -1,9 +0,0 @@
-namespace MonoDevelop.Ide
-{
-	internal class BuildVariables
-	{
-		public static string PackageVersion = "2.1.1";
-		public static string PackageVersionLabel = "2.2 Beta 2";
-	}
-}
-
