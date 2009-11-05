89ef007bf31f1a86331d474c3603535f3e6875c2
Lluis Sanchez <lluis@novell.com>
2009-11-05 17:47:52 +0000

    Created 2.2 branch.
    svn path=/branches/monodevelop/extras/JavaBinding/2.2/; revision=145504

--- main/contrib/NRefactory/Project/Src/Parser/gen.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-echo Generating with coco
-set CRFRAMES = "Frames"
-cp CSharp/cs.ATG Frames/cs.ATG
-mono Frames/SharpCoco.exe -namespace ICSharpCode.NRefactory.Parser.CSharp Frames/cs.ATG
-mv Frames/Parser.cs CSharp/Parser.cs
-
-#mono Frames/SharpCoco.exe -trace GIPXA -namespace ICSharpCode.NRefactory.Parser.VB VBNet/VBNET.ATG
