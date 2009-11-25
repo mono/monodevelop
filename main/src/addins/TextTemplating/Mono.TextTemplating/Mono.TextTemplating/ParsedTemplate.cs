cf6199a32a306c5a7ce7ab30aa476a42d9599269
Michael Hutchinson <mhutchinson@novell.com>
2009-11-25 22:09:55 +0000

    * Mono.TextTemplating/ParsedTemplate.cs: Don't check if included   file exists before trying to resolve it from the host, because host   may use include paths. Patch from Aaron Bockover.
    svn path=/trunk/monodevelop/; revision=146951

--- main/src/addins/TextTemplating/Mono.TextTemplating/Mono.TextTemplating/ParsedTemplate.cs
+++ main/src/addins/TextTemplating/Mono.TextTemplating/Mono.TextTemplating/ParsedTemplate.cs
@@ -168,11 +168,6 @@ namespace Mono.TextTemplating
 				LogError ("Unexpected attributes in include directive", includeDirective.StartLocation);
 				return;
 			}
-			if (!File.Exists (fileName)) {
-				LogError ("Included file '" + fileName + "' does not exist.", includeDirective.StartLocation);
-				return;
-			}
-			
 			string content, resolvedName;
 			if (host.LoadIncludeText (fileName, out content, out resolvedName))
 				Parse (host, new Tokeniser (resolvedName, content), true);
