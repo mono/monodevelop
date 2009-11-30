96fd98c742feb80ebf396e954751cd3a0e01d4b4
Lluis Sanchez <lluis@novell.com>
2009-11-30 19:46:11 +0000

    * MonoDevelop.Debugger.Soft/SoftDebuggerAdaptor.cs: Don't call   ToString when it doesn't have been overriden.
    svn path=/trunk/monodevelop/; revision=147120

--- main/src/addins/MonoDevelop.Debugger.Soft/MonoDevelop.Debugger.Soft/SoftDebuggerAdaptor.cs
+++ main/src/addins/MonoDevelop.Debugger.Soft/MonoDevelop.Debugger.Soft/SoftDebuggerAdaptor.cs
@@ -59,10 +59,12 @@ namespace MonoDevelop.Debugger.Soft
 			else if ((obj is ObjectMirror) && cx.Options.AllowTargetInvoke) {
 				ObjectMirror ob = (ObjectMirror) obj;
 				MethodMirror method = OverloadResolve (cx, "ToString", ob.Type, new TypeMirror[0], true, false, false);
-				StringMirror res = (StringMirror) cx.RuntimeInvoke (method, obj, new Value[0]);
-				return res.Value;
+				if (method.DeclaringType.FullName != "System.Object") {
+					StringMirror res = (StringMirror) cx.RuntimeInvoke (method, obj, new Value[0]);
+					return res.Value;
+				}
 			}
-			return GetValueTypeName (ctx, obj);
+			return GetDisplayTypeName (GetValueTypeName (ctx, obj));
 		}
 
 
