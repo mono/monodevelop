afa6b2cb892557c6e47bf4a3d8cee7063f751b4f
Michael Hutchinson <mhutchinson@novell.com>
2010-02-05 22:36:59 +0000

    * src/core/MonoDevelop.Ide/MonoDevelop.Ide.Gui/TextEditor.cs: * src/core/MonoDevelop.Ide/MonoDevelop.Ide.Gui.Content/IEditableTextBuffer.cs: * src/core/MonoDevelop.Ide/MonoDevelop.Ide.Gui/HiddenTextEditorViewContent.cs: * src/addins/MonoDevelop.SourceEditor2/MonoDevelop.SourceEditor/SourceEditorView.cs:   Added method to check whether text editor has input focus.
    * src/core/MonoDevelop.Ide/MonoDevelop.Ide.Commands/EditCommands.cs:
      Only enable "Insert standard header" command when text editor has
      input focus. 
    
    * src/core/MonoDevelop.Ide/MonoDevelop.Ide.addin.xml: Improve ordering
      of edit menu.
    
    * src/addins/MonoDevelop.Refactoring/MonoDevelop.CodeGeneration/CodeGenerationCommands.cs:
      Only enable command when text editor has input focus. 
    
    * src/addins/MonoDevelop.Refactoring/MonoDevelop.Refactoring.addin.xml:
      Insert the Code Generation command into edit menu and editor context
      menu.
    
    svn path=/trunk/monodevelop/; revision=150962

--- main/src/addins/MonoDevelop.SourceEditor2/MonoDevelop.SourceEditor/SourceEditorView.cs
+++ main/src/addins/MonoDevelop.SourceEditor2/MonoDevelop.SourceEditor/SourceEditorView.cs
@@ -954,6 +954,11 @@ namespace MonoDevelop.SourceEditor
 		}
 		public event EventHandler CaretPositionSet;
 		public event EventHandler<TextChangedEventArgs> TextChanged;
+		
+		public bool HasInputFocus {
+			get { return TextEditor.HasFocus; }
+		}
+		
 		#endregion
 		
 		#region ITextBuffer
