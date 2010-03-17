5b1ab36269e7c224ad9f10b609b5248dc0ff80e5
Lluis Sanchez <lluis@novell.com>
2010-03-17 12:47:51 +0000

    Merged MD.Projects into MD.Core, and MD.Projects.Gui, MD.Core.Gui and MD.Components into MD.Ide.
    svn path=/trunk/monodevelop/; revision=153744

--- main/src/core/MonoDevelop.Startup/MonoDevelop.Startup/MonoDevelopMain.cs
+++ main/src/core/MonoDevelop.Startup/MonoDevelop.Startup/MonoDevelopMain.cs
@@ -2,10 +2,10 @@
 using System;
 using System.IO;
 
-using MonoDevelop.Ide.Gui;
 using MonoDevelop.Core;
 using MonoDevelop.Core.ProgressMonitoring;
 using Mono.Addins;
+using MonoDevelop.Ide;
 
 namespace MonoDevelop.Startup
 {
