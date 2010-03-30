a00c35ede9088833dd1a294c6eade91177aa9d34
Michael Hutchinson <mhutchinson@novell.com>
2010-03-30 02:01:47 +0000

    * MonoDevelop.IPhone.csproj: * IPhoneExecutionModeSet.cs: * MonoDevelop.IPhone.addin.xml: Add execution modes for every   installed SDK.
    svn path=/trunk/monodevelop/; revision=154411

---
+++ extras/MonoDevelop.IPhone/MonoDevelop.IPhone/IPhoneExecutionModeSet.cs
@@ -0,0 +1,73 @@
+// 
+// MeeGoExecutionHandler.cs
+//  
+// Author:
+//       Michael Hutchinson <mhutchinson@novell.com>
+// 
+// Copyright (c) 2010 Novell, Inc. (http://www.novell.com)
+// 
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+// 
+// The above copyright notice and this permission notice shall be included in
+// all copies or substantial portions of the Software.
+// 
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+// THE SOFTWARE.
+
+using System;
+using MonoDevelop.Core.Execution;
+using System.Collections.Generic;
+
+namespace MonoDevelop.IPhone
+{
+	public class IPhoneExecutionModeSet : IExecutionModeSet
+	{
+		public string Name { get { return "IPhone";  } }
+		
+		public IEnumerable<IExecutionMode> ExecutionModes {
+			get {
+				var v32 = new IPhoneSdkVersion (new int[] { 3, 2 });
+				foreach (var v in IPhoneFramework.InstalledSdkVersions) {
+					var vStr = v.ToString ();
+					yield return new IPhoneExecutionMode (TargetDevice.IPhone, vStr);
+					if (v.CompareTo (v32) >= 0)
+						yield return new IPhoneExecutionMode (TargetDevice.IPad, vStr);
+				}
+			}
+		}
+	}
+	
+	class IPhoneExecutionMode : IExecutionMode
+	{
+		public IPhoneExecutionMode (TargetDevice targetDevice, string sdkVersion)
+		{
+			this.TargetDevice = targetDevice;
+			this.SdkVersion = sdkVersion;
+			this.Name = (targetDevice == TargetDevice.IPad? "iPad Simulator " : "iPhone Simulator ")
+				+ SdkVersion;
+		}
+		
+		IPhoneExecutionHandler handler;
+		
+		public string Name { get; private set; }
+		public string Id { get { return "IPhoneExecutionMode"; } }
+		public TargetDevice TargetDevice { get; private set; }
+		public string SdkVersion { get; private set; }
+		
+		public IExecutionHandler ExecutionHandler {
+			get {
+				return handler ?? (handler = new IPhoneExecutionHandler ());
+			}
+		}
+	}
+}
