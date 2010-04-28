aa0bd384e09ccb56d5ec8f257a3bdd403da01ff2
Lluis Sanchez <lluis@novell.com>
2010-04-28 18:20:36 +0000

    Created 2.3 branch.
    svn path=/branches/monodevelop/extras/VersionControl.Subversion.Win32/2.3/; revision=156367

--- extras/MonoDevelop.Debugger.Win32/CorApi2/Metadata/Constants.cs
+++
@@ -1,9 +0,0 @@
-//---------------------------------------------------------------------
-//  This file is part of the CLR Managed Debugger (mdbg) Sample.
-// 
-//  Copyright (C) Microsoft Corporation.  All rights reserved.
-//---------------------------------------------------------------------
-namespace Microsoft.Samples.Debugging.CorMetadata
-{
-    
-}
