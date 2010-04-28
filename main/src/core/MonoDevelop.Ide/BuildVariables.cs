d645b36795fa07d3e6856bd4f2cb031c12fec0a1
Lluis Sanchez <lluis@novell.com>
2010-04-28 17:06:15 +0000

    * configure.in: * src/core/MonoDevelop.Ide/BuildVariables.cs: Require Mono 2.6.1.   Updated version label.
    svn path=/trunk/monodevelop/; revision=156351

--- main/src/core/MonoDevelop.Ide/BuildVariables.cs
+++ main/src/core/MonoDevelop.Ide/BuildVariables.cs
@@ -3,7 +3,7 @@ namespace MonoDevelop.Ide
 	internal class BuildVariables
 	{
 		public static string PackageVersion = "2.3";
-		public static string PackageVersionLabel = "2.4 Alpha 1";
+		public static string PackageVersionLabel = "2.4 Beta 1";
 	}
 }
 
