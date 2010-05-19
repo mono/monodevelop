0a26b3ddf0e231ddcc7e39b15f662d5f8e79d416
Mike Krüger <mkrueger@novell.com>
2010-05-19 10:44:45 +0000

    * MonoDevelop.Projects.Dom/CompilationUnit.cs: fixed collection   already modified exception.
    svn path=/trunk/monodevelop/; revision=157540

--- main/src/core/MonoDevelop.Core/MonoDevelop.Projects.Dom/CompilationUnit.cs
+++ main/src/core/MonoDevelop.Core/MonoDevelop.Projects.Dom/CompilationUnit.cs
@@ -217,11 +217,8 @@ namespace MonoDevelop.Projects.Dom
 				string longest = "";
 				string lastAlias = null;
 				if (callingType != null && returnType.DecoratedFullName.StartsWith (callingType.FullName)) {
-					int p1 = callingType.FullName.Count (ch => ch == '.') + 1;
-					while (p1 > 0) {
-						returnType.Parts.RemoveAt (0);
-						p1--;
-					}
+					int p1 = System.Math.Min (callingType.FullName.Count (ch => ch == '.') + 1, returnType.Parts.Count);
+					returnType.Parts.RemoveRange (0, p1);
 					returnType.Namespace = "";
 					return returnType;
 				}
