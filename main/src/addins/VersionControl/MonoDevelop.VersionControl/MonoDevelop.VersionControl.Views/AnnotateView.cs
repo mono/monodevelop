c1c1771bcd28bb137ef8ee68a5c66660b9c7ade5
Mike Krüger <mkrueger@novell.com>
2010-05-31 07:02:23 +0000

    * MonoDevelop.VersionControl.Views/AnnotateView.cs: Added missing   string to translation.
    svn path=/trunk/monodevelop/; revision=158187

--- main/src/addins/VersionControl/MonoDevelop.VersionControl/MonoDevelop.VersionControl.Views/AnnotateView.cs
+++ main/src/addins/VersionControl/MonoDevelop.VersionControl/MonoDevelop.VersionControl.Views/AnnotateView.cs
@@ -194,7 +194,7 @@ namespace MonoDevelop.VersionControl.Views
 		{
 			StatusBarContext ctx = IdeApp.Workbench.StatusBar.CreateContext ();
 			ctx.AutoPulse = true;
-			ctx.ShowMessage (ImageService.GetImage ("md-version-control", IconSize.Menu), "Retrieving history");
+			ctx.ShowMessage (ImageService.GetImage ("md-version-control", IconSize.Menu), GettextCatalog.GetString ("Retrieving history"));
 			
 			ThreadPool.QueueUserWorkItem (delegate {
 				try {
