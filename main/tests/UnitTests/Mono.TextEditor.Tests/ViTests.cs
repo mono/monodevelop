e3ff2edf86af9394eae70da30a1b37e1b7bd0369
Michael Hutchinson <mhutchinson@novell.com>
2010-06-01 02:26:51 +0000

    * Mono.TextEditor.Tests/ViTests.cs: More tests.
    svn path=/trunk/monodevelop/; revision=158246

--- main/tests/UnitTests/Mono.TextEditor.Tests/ViTests.cs
+++ main/tests/UnitTests/Mono.TextEditor.Tests/ViTests.cs
@@ -36,40 +36,172 @@ namespace Mono.TextEditor.Tests
 		[Test]
 		public void ColumnMotion ()
 		{
-			var data = new TextEditorData  ();
-			data.Document.Text = "Test";
-			var mode = new TestViEditMode (data);
-			data.CurrentMode = mode;
-			
-			Assert.AreEqual (0, data.Caret.Offset);
+			var mode = new TestViEditMode () { Text = "Test\nText" };
+			Assert.AreEqual (0, mode.Col);
 			mode.Input ('l');
-			Assert.AreEqual (1, data.Caret.Offset);
+			Assert.AreEqual (1, mode.Col);
+			mode.Input ("ll");
+			Assert.AreEqual (3, mode.Col);
 			mode.Input ('l');
-			Assert.AreEqual (2, data.Caret.Offset);
+			Assert.AreEqual (3, mode.Col);
+			mode.Input ("hh");
+			Assert.AreEqual (1, mode.Col);
 			mode.Input ('h');
-			Assert.AreEqual (1, data.Caret.Offset);
+			Assert.AreEqual (0, mode.Col);
 			mode.Input ('h');
-			Assert.AreEqual (0, data.Caret.Offset);
+			Assert.AreEqual (0, mode.Col);
+		}
+		
+		[Test]
+		public void LineMotion ()
+		{
+			var mode = new TestViEditMode () { Text =
+@"abc def
+ghy jklmn op
+qrstu
+vwxyz",
+			};
+			mode.Caret.Offset = 0;
+			Assert.AreEqual (0, mode.Line);
+			mode.Input ('j');
+			Assert.AreEqual (1, mode.Line);
+			mode.Input ('k');
+			Assert.AreEqual (0, mode.Line);
+			mode.Input ("jjjj");
+			Assert.AreEqual (3, mode.Line);
+			mode.Caret.Line = 1;
+			mode.Caret.Column = 11;
+			mode.Input ('j');
+			Assert.AreEqual (4, mode.Col);
+			mode.Input ('k');
+			Assert.AreEqual (11, mode.Col);
+		}
+		
+		[Test]
+		public void ChangeWord ()
+		{
+			var mode = new TestViEditMode () { Text =
+@"abc def
+ghy jklmn op
+qrstu",
+			};
+			mode.Caret.Offset = 0;
+			mode.Input ("jwcwhi");
+			Assert.AreEqual ("ghy hi op", mode.GetLine ());
+		}
+		
+		[Test]
+		public void DeleteWord ()
+		{
+			var mode = new TestViEditMode () { Text =
+@"abc def
+ghy jklmn op
+qrstu",
+			};
+			mode.Caret.Offset = 0;
+			mode.Input ("jwlldw");
+			Assert.AreEqual ("ghy jkop", mode.GetLine ());
+		}
+		
+		[TestFixtureSetUp] 
+		public void SetUp()
+		{
+			Gtk.Application.Init ();
+		}
+		
+		[TestFixtureTearDown] 
+		public void Dispose()
+		{
 		}
 	}
 	
-	//allows access to protected members
 	class TestViEditMode : Mono.TextEditor.Vi.ViEditMode
 	{
+		public TestViEditMode () : this (new TextEditorData ())
+		{
+			Data.Options.WordFindStrategy = new Mono.TextEditor.Vi.ViWordFindStrategy ();
+		}
+		
+		//used to prevent edit actions from the HandleKeypress causing Caret/SelectionPositionChanged
+		bool inputting;
+		
 		public TestViEditMode (TextEditorData data)
 		{
+			data.CurrentMode = this;
 			var f = typeof (EditMode).GetField ("textEditorData", BindingFlags.NonPublic | BindingFlags.Instance);
 			f.SetValue (this, data);
+			data.Caret.PositionChanged += delegate(object sender, DocumentLocationEventArgs e) {
+				if (!inputting)
+					this.CaretPositionChanged ();
+			};
+			data.SelectionChanged += delegate(object sender, EventArgs e) {
+				if (!inputting)
+					this.SelectionChanged ();
+			};
+		}
+		
+		public new TextEditorData Data {
+			get { return base.Data; }
+		}
+		
+		public new Document Document {
+			get { return base.Document; }
+		}
+		
+		public new Caret Caret {
+			get { return base.Caret; }
+		}
+		
+		public int Col {
+			get { return Caret.Column; }
+		}
+		
+		public int Line {
+			get { return Caret.Line; }
+		}
+		
+		public string Text {
+			get { return Data.Document.Text; }
+			set { Data.Document.Text = value; }
+		}
+		
+		public string GetLine ()
+		{
+			return GetLine (Line);
+		}
+		
+		public string GetLine (int line)
+		{
+			var seg = Document.GetLine (line);
+			return Document.GetTextAt (seg.Offset, seg.EditableLength);
+		}
+		
+		public char GetChar (int offset)
+		{
+			return Document.GetCharAt (offset);
+		}
+		
+		public char GetChar ()
+		{
+			return Document.GetCharAt (Caret.Offset);
 		}
 		
 		public void Input (Gdk.Key key, uint unicodeKey, Gdk.ModifierType modifier)
 		{
+			inputting = true;
 			HandleKeypress (key, unicodeKey, modifier);
+			inputting = false;
 		}
 		
 		public void Input (char unicodeKey)
 		{
-			HandleKeypress ((Gdk.Key)0, unicodeKey, Gdk.ModifierType.None);
+			Input ((Gdk.Key)0, unicodeKey, Gdk.ModifierType.None);
+		}
+		
+		public void Input (string sequence)
+		{
+			foreach (char c in sequence)
+				Input ((Gdk.Key)0, c, Gdk.ModifierType.None);
 		}
 	}
 }
