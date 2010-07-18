336fa8a0651d055e8bc39937d09aac71d257a4b0
Mike Krüger <mkrueger@novell.com>
2010-07-18 18:35:45 +0000

    * MonoDevelop.DesignerSupport/ClassOutlineTextEditorExtension.cs:   Track API changes.
    svn path=/trunk/monodevelop/; revision=160554

--- main/src/addins/MonoDevelop.DesignerSupport/MonoDevelop.DesignerSupport/ClassOutlineTextEditorExtension.cs
+++ main/src/addins/MonoDevelop.DesignerSupport/MonoDevelop.DesignerSupport/ClassOutlineTextEditorExtension.cs
@@ -113,7 +113,7 @@ namespace MonoDevelop.DesignerSupport
 					col = ((IMember)o).BodyRegion.Start.Column;
 				}
 				if (line > -1) {
-					Editor.Parent.SetCaretTo (line - 1, Math.Max (1, col) - 1);
+					Editor.SetCaretTo (line - 1, Math.Max (1, col) - 1);
 				}
 			};
 
