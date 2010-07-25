cfa376aa811cbc69aeb4a5ea92bbf34f8913ead5
Mike Krüger <mkrueger@novell.com>
2010-07-25 10:03:13 +0200

    Added namespace tests.

---
+++ main/tests/test-projects/completion-db-test/CompletionDbTest/NamespaceTest.cs
@@ -0,0 +1,16 @@
+using System;
+namespace Level1
+{
+	namespace Level2
+	{
+		class Level2Class {}
+		
+		namespace Level3
+		{
+			namespace Level4
+			{
+				class Level4Class {}
+			}
+		}	
+	}
+}
