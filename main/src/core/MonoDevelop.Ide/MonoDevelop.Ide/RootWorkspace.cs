71da0e65a800586ebcd9ecd389a3b7b0b8d359c3
Lluis Sanchez <lluis@novell.com>
2010-08-03 20:01:07 +0200

    Pick a good default configuration when loading a solution for the first time. Fixes bug 627297 - Projects from VS.NET solution are not build.

--- main/src/core/MonoDevelop.Ide/MonoDevelop.Ide/RootWorkspace.cs
+++ main/src/core/MonoDevelop.Ide/MonoDevelop.Ide/RootWorkspace.cs
@@ -717,6 +717,9 @@ namespace MonoDevelop.Ide
 							UseDefaultRuntime = true;
 					}
 				}
+				else {
+					ActiveConfigurationId = GetBestDefaultConfiguration ();
+				}
 			}
 			catch (Exception ex) {
 				LoggingService.LogError ("Exception while loading user solution preferences.", ex);
@@ -732,6 +735,29 @@ namespace MonoDevelop.Ide
 					LoggingService.LogError ("Exception in LoadingUserPreferences.", ex);
 				}
 			}
+		}
+		
+		string GetBestDefaultConfiguration ()
+		{
+			// 'Debug' is always the best candidate. If there is no debug, pick
+			// the configuration with the highest number of built projects.
+			int nbuilds = 0;
+			string bestConfig = null;
+			foreach (Solution sol in GetAllSolutions ()) {
+				foreach (string conf in sol.GetConfigurations ()) {
+					if (conf == "Debug")
+						return conf;
+					SolutionConfiguration sconf = sol.GetConfiguration (new SolutionConfigurationSelector (conf));
+					int c = 0;
+					foreach (var sce in sconf.Configurations)
+						if (sce.Build) c++;
+					if (c > nbuilds) {
+						nbuilds = c;
+						bestConfig = conf;
+					}
+				}
+			}
+			return bestConfig;
 		}
 		
 		public void SavePreferences (WorkspaceItem item)
