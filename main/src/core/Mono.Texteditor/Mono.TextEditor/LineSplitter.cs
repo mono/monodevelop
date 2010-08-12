778426a3f3c0722eb85aef5ed2982c577fa90b7b
Mike Krüger <mkrueger@novell.com>
2010-08-12 15:42:42 +0200

    Splitted up the new classes in their own files.

--- main/src/core/Mono.Texteditor/Mono.TextEditor/LineSplitter.cs
+++ main/src/core/Mono.Texteditor/Mono.TextEditor/LineSplitter.cs
@@ -31,37 +31,6 @@ using System.Linq;
 
 namespace Mono.TextEditor
 {
-	public interface ILineSplitter
-	{
-		int Count { get; }
-		
-		IEnumerable<LineSegment> Lines { get; }
-
-		void Clear ();
-		
-		/// <summary>
-		/// Initializes the splitter with a new text. No events are fired during this process.
-		/// </summary>
-		/// <param name="text"></param>
-		void Initalize (string text);
-		
-		LineSegment Get (int number);
-		LineSegment GetLineByOffset (int offset);
-		int OffsetToLineNumber (int offset);
-
-		void TextReplaced (object sender, ReplaceEventArgs args);
-		void TextRemove (int offset, int length);
-		void TextInsert (int offset, string text);
-
-		IEnumerable<LineSegment> GetLinesBetween (int startLine, int endLine);
-		IEnumerable<LineSegment> GetLinesStartingAt (int startLine);
-		IEnumerable<LineSegment> GetLinesReverseStartingAt (int startLine);
-		
-		event EventHandler<LineEventArgs> LineChanged;
-		event EventHandler<LineEventArgs> LineInserted;
-		event EventHandler<LineEventArgs> LineRemoved;
-	}
-
 	public class LineSplitter : ILineSplitter
 	{
 		public IEnumerable<LineSegment> Lines {
@@ -72,15 +41,17 @@ namespace Mono.TextEditor
 				} while (iter.MoveNext ());
 			}
 		}
-		
+
 		public LineSplitter ()
 		{
 			tree.ChildrenChanged += (sender, args) => UpdateNode (args.Node);
-			tree.NodeRotateLeft += (sender, args) => {
+			tree.NodeRotateLeft += (sender, args) =>
+			{
 				UpdateNode (args.Node);
 				UpdateNode (args.Node.Parent);
 			};
-			tree.NodeRotateRight += (sender, args) => {
+			tree.NodeRotateRight += (sender, args) =>
+			{
 				UpdateNode (args.Node);
 				UpdateNode (args.Node.Parent);
 			};
@@ -96,7 +67,7 @@ namespace Mono.TextEditor
 			int curLine = startLine;
 			do {
 				yield return iter.Current;
-			} while (iter.MoveNext() && curLine++ <= endLine);
+			} while (iter.MoveNext () && curLine++ <= endLine);
 		}
 
 		public IEnumerable<LineSegment> GetLinesStartingAt (int startLine)
@@ -107,9 +78,9 @@ namespace Mono.TextEditor
 			var iter = startNode.Iter;
 			do {
 				yield return iter.Current;
-			} while (iter.MoveNext());
+			} while (iter.MoveNext ());
 		}
-		
+
 		public IEnumerable<LineSegment> GetLinesReverseStartingAt (int startLine)
 		{
 			var startNode = GetNode (startLine);
@@ -118,32 +89,32 @@ namespace Mono.TextEditor
 			var iter = startNode.Iter;
 			do {
 				yield return iter.Current;
-			} while (iter.MoveBack());
+			} while (iter.MoveBack ());
 		}
 
 		public LineSegment Get (int number)
 		{
 			return GetNode (number);
 		}
-		
+
 		public LineSegment GetLineByOffset (int offset)
 		{
 			return Get (OffsetToLineNumber (offset));
 		}
-		
+
 		public void TextReplaced (object sender, ReplaceEventArgs args)
 		{
 			if (args.Count > 0)
 				TextRemove (args.Offset, args.Count);
-			if (!string.IsNullOrEmpty(args.Value))
+			if (!string.IsNullOrEmpty (args.Value))
 				TextInsert (args.Offset, args.Value);
 		}
 
 		public void TextRemove (int offset, int length)
 		{
-			if (length == 0 || (Count == 1 && Length == 0)) 
-				return; 
-
+			if (length == 0 || (Count == 1 && Length == 0))
+				return;
+			
 			var startNode = GetNodeAtOffset (offset);
 			int charsRemoved = startNode.EndOffset - offset;
 			if (offset + length < startNode.EndOffset) {
@@ -175,7 +146,7 @@ namespace Mono.TextEditor
 			Clear ();
 			inInit = true;
 			try {
-				TextInsert(0, text);
+				TextInsert (0, text);
 			} finally {
 				inInit = false;
 			}
@@ -183,7 +154,7 @@ namespace Mono.TextEditor
 
 		public void TextInsert (int offset, string text)
 		{
-			if (string.IsNullOrEmpty(text))
+			if (string.IsNullOrEmpty (text))
 				return;
 			
 			var line = GetNodeAtOffset (offset);
@@ -200,16 +171,16 @@ namespace Mono.TextEditor
 				textOffset = delimiterEndOffset;
 				lineOffset += curLineLength;
 			}
-
+			
 			if (textOffset != text.Length)
-				ChangeLength(line, line.Length + text.Length - textOffset);
+				ChangeLength (line, line.Length + text.Length - textOffset);
 		}
 
-		internal struct Delimiter 
+		internal struct Delimiter
 		{
 			public readonly int Offset;
 			public readonly int Length;
-			
+
 			public int EndOffset {
 				get { return Offset + Length; }
 			}
@@ -220,8 +191,8 @@ namespace Mono.TextEditor
 				Length = length;
 			}
 		}
-		
-		internal static IEnumerable<Delimiter> FindDelimiter (string text) 
+
+		static internal IEnumerable<Delimiter> FindDelimiter (string text)
 		{
 			for (int i = 0; i < text.Length; i++) {
 				switch (text[i]) {
@@ -239,8 +210,8 @@ namespace Mono.TextEditor
 				}
 			}
 		}
-		
-		internal static int CountLines (string text)
+
+		static internal int CountLines (string text)
 		{
 			return FindDelimiter (text).Count ();
 		}
@@ -251,82 +222,71 @@ namespace Mono.TextEditor
 			internal RedBlackTree<TreeNode>.RedBlackTreeNode treeNode;
 
 			public RedBlackTree<TreeNode>.RedBlackTreeIterator Iter {
-				get {
-					return new RedBlackTree<TreeNode>.RedBlackTreeIterator (treeNode);
-				}
+				get { return new RedBlackTree<TreeNode>.RedBlackTreeIterator (treeNode); }
 			}
 
 			public override int Offset {
-				get {
-					return treeNode != null ? GetOffsetFromNode (treeNode) : -1;
-				}
+				get { return treeNode != null ? GetOffsetFromNode (treeNode) : -1; }
 				set {
 					throw new NotSupportedException ();
 				}
 			}
 
 
-			public int Count       = 1;
+			public int Count = 1;
 			public int TotalLength;
-			
-			public TreeNode (int length, int delimiterLength) : base (length, delimiterLength)
+
+			public TreeNode (int length, int delimiterLength) : base(length, delimiterLength)
 			{
 			}
-			
+
 			public override string ToString ()
 			{
-				return String.Format ("[TreeNode: Line={0}, Count={1}, TotalLength={2}]",
-				                      base.ToString (),
-				                      Count,
-				                      TotalLength);
+				return String.Format ("[TreeNode: Line={0}, Count={1}, TotalLength={2}]", base.ToString (), Count, TotalLength);
 			}
 		}
 
 		readonly RedBlackTree<TreeNode> tree = new RedBlackTree<TreeNode> ();
 
 		public int Count {
-			get {
-				return tree.Count;
-			}
+			get { return tree.Count; }
 		}
-		
+
 		public int Length {
-			get {
-				return tree.Root.Value.TotalLength;
-			}
+			get { return tree.Root.Value.TotalLength; }
 		}
-	
+
 		static void UpdateNode (RedBlackTree<TreeNode>.RedBlackTreeNode node)
 		{
 			if (node == null)
 				return;
 			
-			int count       = 1;
+			int count = 1;
 			int totalLength = node.Value.Length;
 			
 			if (node.Left != null) {
-				count       += node.Left.Value.Count;
+				count += node.Left.Value.Count;
 				totalLength += node.Left.Value.TotalLength;
 			}
 			
 			if (node.Right != null) {
-				count       += node.Right.Value.Count;
+				count += node.Right.Value.Count;
 				totalLength += node.Right.Value.TotalLength;
 			}
 			if (count != node.Value.Count || totalLength != node.Value.TotalLength) {
-				node.Value.Count       = count;
+				node.Value.Count = count;
 				node.Value.TotalLength = totalLength;
 				UpdateNode (node.Parent);
 			}
 		}
-		
+
 		public void Clear ()
 		{
 			tree.Root = new RedBlackTree<TreeNode>.RedBlackTreeNode (new TreeNode (0, 0));
 			tree.Root.Value.treeNode = tree.Root;
 			tree.Count = 1;
 		}
-		
+
 		public TreeNode InsertAfter (TreeNode segment, int length, int delimiterLength)
 		{
 			var result = new TreeNode (length, delimiterLength) { StartSpan = segment.StartSpan };
@@ -349,18 +309,18 @@ namespace Mono.TextEditor
 			OnLineChanged (new LineEventArgs (result));
 			return result;
 		}
-		
+
 		public override string ToString ()
 		{
 			return tree.ToString ();
 		}
-		
-		
+
+
 		public void ChangeLength (TreeNode line, int newLength)
 		{
 			ChangeLength (line, newLength, line.DelimiterLength);
 		}
-		
+
 		public void ChangeLength (TreeNode line, int newLength, int delimiterLength)
 		{
 			line.Length = newLength;
@@ -368,34 +328,37 @@ namespace Mono.TextEditor
 			OnLineChanged (new LineEventArgs (line));
 			UpdateNode (line.Iter.CurrentNode);
 		}
-		
+
 		protected virtual void OnLineChanged (LineEventArgs e)
 		{
-			if (inInit) return;
+			if (inInit)
+				return;
 			EventHandler<LineEventArgs> handler = LineChanged;
 			if (handler != null)
 				handler (this, e);
 		}
 		public event EventHandler<LineEventArgs> LineChanged;
-		
+
 		protected virtual void OnLineInserted (LineEventArgs e)
 		{
-			if (inInit) return;
+			if (inInit)
+				return;
 			EventHandler<LineEventArgs> handler = LineInserted;
 			if (handler != null)
 				handler (this, e);
 		}
 		public event EventHandler<LineEventArgs> LineInserted;
-		
+
 		protected virtual void OnLineRemoved (LineEventArgs e)
 		{
-			if (inInit) return;
+			if (inInit)
+				return;
 			EventHandler<LineEventArgs> handler = LineRemoved;
 			if (handler != null)
 				handler (this, e);
 		}
 		public event EventHandler<LineEventArgs> LineRemoved;
-		
+
 		public static int GetOffsetFromNode (RedBlackTree<TreeNode>.RedBlackTreeNode node)
 		{
 			int offset = node.Left != null ? node.Left.Value.TotalLength : 0;
@@ -413,7 +376,7 @@ namespace Mono.TextEditor
 
 		RedBlackTree<TreeNode>.RedBlackTreeNode GetTreeNodeAtOffset (int offset)
 		{
-			if (offset == tree.Root.Value.TotalLength) 
+			if (offset == tree.Root.Value.TotalLength)
 				return tree.Root.OuterRight;
 			RedBlackTree<TreeNode>.RedBlackTreeNode node = tree.Root;
 			int i = offset;
@@ -423,22 +386,22 @@ namespace Mono.TextEditor
 				if (node.Left != null && i < node.Left.Value.TotalLength) {
 					node = node.Left;
 				} else {
-					if (node.Left != null) 
+					if (node.Left != null)
 						i -= node.Left.Value.TotalLength;
 					i -= node.Value.Length;
-					if (i < 0) 
+					if (i < 0)
 						return node;
 					node = node.Right;
-				} 
+				}
 			}
 		}
-		
+
 		public TreeNode GetNodeAtOffset (int offset)
 		{
 			var node = GetTreeNodeAtOffset (offset);
 			return node != null ? node.Value : null;
 		}
-		
+
 		public int OffsetToLineNumber (int offset)
 		{
 			var node = GetTreeNodeAtOffset (offset);
@@ -455,21 +418,21 @@ namespace Mono.TextEditor
 			}
 			return result;
 		}
-		
+
 		public void RemoveLine (TreeNode line)
 		{
-			var parent = line.Iter.CurrentNode.Parent; 
+			var parent = line.Iter.CurrentNode.Parent;
 			tree.RemoveAt (line.Iter);
-			UpdateNode (parent); 
+			UpdateNode (parent);
 			OnLineRemoved (new LineEventArgs (line));
 		}
-		
+
 		public TreeNode GetNode (int index)
 		{
-#if DEBUG
+			#if DEBUG
 			if (index < 0)
 				Debug.Assert (false, "index must be >=0 but was " + index + "." + Environment.NewLine + "Stack trace:" + Environment.StackTrace);
-#endif
+			#endif
 			RedBlackTree<TreeNode>.RedBlackTreeNode node = tree.Root;
 			int i = index;
 			while (true) {
@@ -485,117 +448,9 @@ namespace Mono.TextEditor
 						return node.Value;
 					i--;
 					node = node.Right;
-				} 
+				}
 			}
 		}
 		#endregion
-	}
-
-	/// <summary>
-	/// A very fast line splitter for read-only documents that generates lines only on demand.
-	/// </summary>
-	public class PrimitiveLineSplitter : ILineSplitter
-	{
-		int textLength;
-		List<LineSplitter.Delimiter> delimiters = new List<LineSplitter.Delimiter> ();
-		
-		sealed class PrimitiveLineSegment : LineSegment
-		{
-			public override int Offset { get; set; }
-			
-			public PrimitiveLineSegment (int offset, int length, int delimiterLength) : base(length, delimiterLength)
-			{
-				Offset = offset;
-			}
-		}
-
-		public int Count {
-			get { return delimiters.Count + 1; }
-		}
-
-		public IEnumerable<LineSegment> Lines {
-			get { return GetLinesStartingAt(0); }
-		}
-
-		public void Initalize (string text)
-		{
-			delimiters = new List<LineSplitter.Delimiter> (LineSplitter.FindDelimiter (text));
-			textLength = text.Length;
-		}
-
-		public void Clear ()
-		{
-			delimiters.Clear ();
-			textLength = 0;
-		}
-
-		public LineSegment Get (int number)
-		{
-			if (number < 0)
-				return null;
-			int startOffset = number > 0 ? delimiters[number - 1].EndOffset : 0;
-			int endOffset;
-			int delimiterLength;
-			if (number < delimiters.Count) {
-				endOffset = delimiters[number].Offset;
-				delimiterLength = delimiters[number].Length;
-			} else {
-				endOffset = textLength;
-				delimiterLength = 0;
-			}
-			return new PrimitiveLineSegment (startOffset, endOffset - startOffset, delimiterLength);
-		}
-
-		public LineSegment GetLineByOffset (int offset)
-		{
-			return Get (OffsetToLineNumber (offset));
-		}
-		
-		public int OffsetToLineNumber (int offset)
-		{
-			for (int i = 0; i < delimiters.Count; i++) {
-				var delimiter = delimiters[i];
-				if (offset < delimiter.Offset)
-					return i;
-			}
-			return - 1;
-		}
-
-		public void TextReplaced (object sender, ReplaceEventArgs args)
-		{
-			throw new NotSupportedException ("Operation not supported on this line splitter.");
-		}
-
-		public void TextRemove (int offset, int length)
-		{
-			throw new NotSupportedException ("Operation not supported on this line splitter.");
-		}
-
-		public void TextInsert (int offset, string text)
-		{
-			throw new NotSupportedException ("Operation not supported on this line splitter.");
-		}
-
-		public IEnumerable<LineSegment> GetLinesBetween (int startLine, int endLine)
-		{
-			for (int i = startLine; i <= endLine; i++)
-				yield return Get (i);
-		}
-
-		public IEnumerable<LineSegment> GetLinesStartingAt (int startLine)
-		{
-			for (int i = startLine; i < Count; i++)
-				yield return Get (i);
-		}
-
-		public IEnumerable<LineSegment> GetLinesReverseStartingAt (int startLine)
-		{
-			for (int i = Count - 1; i --> 0;)
-				yield return Get (i);
-		}
-
-		public event EventHandler<LineEventArgs> LineChanged;
-		public event EventHandler<LineEventArgs> LineInserted;
-		public event EventHandler<LineEventArgs> LineRemoved;
 	}
 }
