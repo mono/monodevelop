3f9274518d8e24997893e84583f3bbae66970be8
Mike Krüger <mkrueger@novell.com>
2010-08-26 08:19:44 +0200

    Fix error underlining.

--- main/src/addins/MonoDevelop.SourceEditor2/MonoDevelop.SourceEditor/SourceEditorWidget.cs
+++ main/src/addins/MonoDevelop.SourceEditor2/MonoDevelop.SourceEditor/SourceEditorWidget.cs
@@ -535,12 +535,12 @@ namespace MonoDevelop.SourceEditor
 //			info.Line -= 1;
 			
 			// If the line is already underlined
-			if (errors.ContainsKey (info.Region.Start.Line - 1))
+			if (errors.ContainsKey (info.Region.Start.Line))
 				return;
 			
 			LineSegment line = this.TextEditor.Document.GetLine (info.Region.Start.Line);
 			ErrorMarker error = new ErrorMarker (info, line);
-			errors [info.Region.Start.Line - 1] = error;
+			errors [info.Region.Start.Line] = error;
 			error.AddToLine (this.TextEditor.Document);
 		}
 		#endregion
