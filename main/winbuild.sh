4356c899a4856a96dfaf386a10260c312f9c3d06
Michael Hutchinson <mhutchinson@novell.com>
2010-09-15 19:57:50 -0400

    Pass additional arguments to MSBuild

--- main/winbuild.sh
+++ main/winbuild.sh
@@ -1,2 +1,2 @@
 export PATH="/c/Windows/Microsoft.NET/Framework/v3.5:$PATH"
-MSBuild.exe Main.sln -p:Configuration=DebugWin32 -p:Platform=x86
+MSBuild.exe Main.sln -p:Configuration=DebugWin32 -p:Platform=x86 $*
