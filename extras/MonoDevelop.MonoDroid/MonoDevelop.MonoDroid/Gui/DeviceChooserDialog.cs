a5dfe812716080ec1507058753ecf3db7c426083
Michael Hutchinson <mhutchinson@novell.com>
2010-10-06 19:03:49 -0400

    Make device chooser dialog use DeviceManager.

--- extras/MonoDevelop.MonoDroid/MonoDevelop.MonoDroid/Gui/DeviceChooserDialog.cs
+++ extras/MonoDevelop.MonoDroid/MonoDevelop.MonoDroid/Gui/DeviceChooserDialog.cs
@@ -35,28 +35,26 @@ using MonoDevelop.Core.ProgressMonitoring;
 namespace MonoDevelop.MonoDroid.Gui
 {
 	public partial class DeviceChooserDialog : Gtk.Dialog
-	{
-		Gdk.Pixbuf banner;
-		Pango.Layout bannerText;
-		
+	{	
 		ListStore store = new ListStore (typeof (object));
+		StatusLabel status = new StatusLabel ();
+		
+		//we only allow use of emulators that were started from this MD instance
+		static Dictionary<string,string> runningEmulators = new Dictionary<string,string> ();
 		
 		public DeviceChooserDialog ()
 		{
 			this.Build ();
 			
-			banner = Gdk.Pixbuf.LoadFromResource ("banner.png");
-			bannerPlaceholder.HeightRequest = banner.Height;
-			
-			bannerPlaceholder.ExposeEvent += BannerExpose;
+			var banner = new HeaderBanner () {
+				Text = GettextCatalog.GetString ("Select Device"),
+				Image = Gdk.Pixbuf.LoadFromResource ("banner.png"),
+			};
+			bannerPlaceholder.Add (banner);
 			
 			deviceListTreeView.Model = store;
 			var txtRenderer = new CellRendererText ();
 			deviceListTreeView.AppendColumn ("Devices", txtRenderer, DeviceDataFunc);
-			
-			refreshButton.Clicked += delegate {
-				RefreshList ();
-			};
 			
 			createEmulatorButton.Clicked += delegate {
 				MonoDroidFramework.Toolbox.StartAvdManager ();
@@ -66,17 +64,9 @@ namespace MonoDevelop.MonoDroid.Gui
 				TreeIter iter;
 				if (deviceListTreeView.Selection.GetSelected (out iter)) {
 					var avd = store.GetValue (iter, 0) as AndroidVirtualDevice;
+					//status.StartOperation (GettextCatalog.GetString ("Starting virtual device '{0}'...", avd.Name));
 					if (avd != null) {
-						//FIXME: actually log the output and status
-						var op = MonoDroidFramework.Toolbox.StartAvd (avd);
-						op.Completed += delegate {
-							if (!op.Success) {
-								MonoDevelop.Ide.MessageService.ShowError (
-									"Failed to start AVD",
-									op.ErrorText);
-							}
-							RefreshList ();
-						};
+						MonoDroidFramework.DeviceManager.StartEmulator (avd);
 					}
 				}
 			};
@@ -86,7 +76,26 @@ namespace MonoDevelop.MonoDroid.Gui
 			buttonOk.Sensitive = false;
 			startEmulatorButton.Sensitive = false;
 			
-			RefreshList ();
+			MonoDroidFramework.DeviceManager.DevicesUpdated += OnDevicesUpdated;
+			OnDevicesUpdated (null, EventArgs.Empty);
+		}
+		
+		protected override void OnDestroyed ()
+		{
+			MonoDroidFramework.DeviceManager.DevicesUpdated -= OnDevicesUpdated;
+			base.OnDestroyed ();
+		}
+
+		void OnDevicesUpdated (object sender, EventArgs e)
+		{
+			var values = new Dictionary<string,object> ();
+			if (MonoDroidFramework.DeviceManager.VirtualDevices != null)
+				foreach (var dev in MonoDroidFramework.DeviceManager.VirtualDevices)
+					values.Add (dev.Name, dev);
+			if (MonoDroidFramework.DeviceManager.Devices != null)
+				foreach (var dev in MonoDroidFramework.DeviceManager.Devices)
+					values.Add (dev.ID, dev);
+			LoadData (values);
 		}
 
 		void UpdatedSelection (object sender, EventArgs e)
@@ -125,115 +134,171 @@ namespace MonoDevelop.MonoDroid.Gui
 			}
 		}
 		
-		void EnsureBannerLayout ()
+		void LoadData (Dictionary<string,object> devices)
 		{
-			if (bannerText != null)
-				bannerText.Dispose ();
-			bannerText = CreatePangoLayout ("");
-			var msg = GettextCatalog.GetString ("Select Device");
-			var font = PropertyService.IsMac? "Lucida Grande 24" : "Sans 24";
+			store.Clear ();
 			
-			bannerText.SetMarkup ("<span font=\"" + font + "\">" + msg + "</span>");
+			foreach (var o in devices.OrderBy (kvp => kvp.Key))
+				store.AppendValues (o.Value);
 		}
 		
-		void KillBannerLayout ()
-		{
-			if (bannerText != null) {
-				bannerText.Dispose ();
-				bannerText = null;
+		public AndroidDevice Device { get; private set; }
+	}
+	
+	class HeaderBanner : DrawingArea 
+	{
+		Gdk.Pixbuf image;
+		Pango.Layout layout;
+		string text;
+		
+		public Gdk.Pixbuf Image {
+			get { return image; }
+			set {
+				image = value;
+				if (image != null) {
+					HeightRequest = image.Height;
+				} else {
+					HeightRequest = 0;
+				}
+				QueueDraw ();
+			}
+		}
+		
+		public string Text {
+			get { return text; }
+			set {
+				text = value;
+				KillLayout ();
+				QueueDraw ();
 			}
 		}
 		
 		protected override void OnDestroyed ()
 		{
 			base.OnDestroyed ();
-			KillBannerLayout ();
+			KillLayout ();
+			if (image != null) {
+				image.Dispose ();
+				image = null;
+			}
 		}
 		
 		protected override void OnStyleSet (Gtk.Style previous_style)
 		{
 			base.OnStyleSet (previous_style);
-			KillBannerLayout ();
+			KillLayout ();
 		}
 		
-		[GLib.ConnectBefore]
-		void BannerExpose (object o, Gtk.ExposeEventArgs args)
+		Pango.Layout EnsureLayout ()
 		{
-			var evt = args.Event;
-			var srcHeight = banner.Height;
-			var srcWidth = banner.Width;
+			if (layout != null)
+				layout.Dispose ();
+			layout = null;
 			
-			var alloc = bannerPlaceholder.Allocation;
+			if (text != null) {
+				layout = CreatePangoLayout ("");
+				
+				//TODO: make this a property?
+				var font = PropertyService.IsMac? "Lucida Grande 24" : "Sans 24";
+				
+				layout.SetMarkup ("<span font=\"" + font + "\">" + text + "</span>");
+			}
+			
+			return layout;
+		}
+		
+		void KillLayout ()
+		{
+			if (layout != null) {
+				layout.Dispose ();
+				layout = null;
+			}
+		}
+		
+		protected override bool OnExposeEvent (Gdk.EventExpose evt)
+		{
+			if (image == null)
+				return base.OnExposeEvent (evt);
+			
+			var srcHeight = image.Height;
+			var srcWidth = image.Width;
+			
+			var alloc = this.Allocation;
 			var w = Math.Min (alloc.Width, srcWidth);
 			var h = Math.Min (alloc.Height, srcHeight);
 			
-			var gc = bannerPlaceholder.Style.BlackGC; //dummy
-			evt.Window.DrawPixbuf (gc, banner, 0, 0, alloc.Left, alloc.Top, w, h, Gdk.RgbDither.Normal, 0, 0);
+			var gc = this.Style.BlackGC; //dummy
+			evt.Window.DrawPixbuf (gc, image, 0, 0, alloc.Left, alloc.Top, w, h, Gdk.RgbDither.Normal, 0, 0);
 			
-			EnsureBannerLayout ();
+			var layout = EnsureLayout ();
 			
-			int txtH, txtW;
-			bannerText.GetPixelSize (out txtW, out txtH);
-			int pxFromBottom = 8;
-			var textY = alloc.Top + srcHeight - txtH - pxFromBottom;
+			if (layout != null) {
+				int txtH, txtW;
+				layout.GetPixelSize (out txtW, out txtH);
+				int pxFromBottom = 8;
+				var textY = alloc.Top + srcHeight - txtH - pxFromBottom;
+				
+				evt.Window.DrawLayout (this.Style.WhiteGC, 140, textY, layout);
+			}
 			
-			evt.Window.DrawLayout (bannerPlaceholder.Style.WhiteGC, 140, textY, bannerText);
-			
-			args.RetVal = false;
+			return false;
+		}
+	}
+	
+	class StatusLabel : Gtk.HBox
+	{
+		Image image = new Image ();
+		Button cancelButton = new Button ();
+		Button detailsButton = new Button ();
+		Label label = new Label ();
+		Gdk.PixbufAnimation spinner;
+		
+		public StatusLabel ()
+		{
+			this.PackStart (image, false, false, 0);
+			this.PackStart (label, true, true, 0);
+			this.PackStart (cancelButton, false, false, 0);
+			this.PackStart (detailsButton, false, false, 0);
+			spinner = Gdk.PixbufAnimation.LoadFromResource ("spinner-big.gif");
+			this.HeightRequest = spinner.Height;
+			label.Ellipsize = Pango.EllipsizeMode.End;
+			label.Xalign = 0;
+			label.Show ();
 		}
 		
-		//FIXME: hook up to a progress indicator, add a cancel button and display errors
-		void RefreshList ()
-		{
-			refreshButton.Sensitive = false;
-			store.Clear ();
-			
-			var op = new AggregatedAsyncOperation ();
-			var devicesOp = MonoDroidFramework.Toolbox.GetDevices (Console.Out);
-			var virtualDevicesOp = MonoDroidFramework.Toolbox.GetAllVirtualDevices (Console.Out);
-			op.Add (devicesOp);
-			op.Add (virtualDevicesOp);
-			op.StartMonitoring ();
-			op.Completed += delegate {
-				try {
-					if (devicesOp.Success || virtualDevicesOp.Success) {
-						LoadDeviceData (devicesOp.Result, virtualDevicesOp.Result);
-					} else {
-						Gtk.Application.Invoke (delegate {
-							refreshButton.Sensitive = true;
-						});
-					}
-					devicesOp.Dispose ();
-					virtualDevicesOp.Dispose ();
-				} catch (Exception ex) {
-					LoggingService.LogError ("Error loading device data", ex);
-				}
-			};
-		}
-		
-		void LoadDeviceData (List<AndroidDevice> devices, List<AndroidVirtualDevice> virtualDevices)
-		{
-			var uniqueDevices = new Dictionary<string,object> ();
-			if (devices != null) {
-				foreach (var d in devices) {
-					uniqueDevices.Add (d.ID, d);
+		bool spinning;
+		bool IsSpinning {
+			get { return spinning; }
+			set {
+				if (spinning == value)
+					return;
+				spinning = value;
+				if (spinning) {
+					image.PixbufAnimation = spinner;
+					image.Show ();
+				} else {
+					image.PixbufAnimation = null;
+					image.Hide ();
 				}
 			}
-			if (virtualDevices != null) {
-				foreach (var avd in virtualDevices) {
-					//FIXME: figure out how to match these properly
-					if (!uniqueDevices.ContainsKey (avd.Name))
-						uniqueDevices.Add (avd.Name, avd);
-				}
-			}
-			Gtk.Application.Invoke (delegate {
-				foreach (var o in uniqueDevices.OrderBy (kvp => kvp.Key))
-					store.AppendValues (o.Value);
-				refreshButton.Sensitive = true;
-			});
 		}
 		
-		public AndroidDevice Device { get; private set; }
+		public string Text {
+			get { return label.Text; }
+			set { label.Text = value; }
+		}
+		
+		public void StartOperation (string message)
+		{
+			IsSpinning = true;
+			Text = message;
+		}
+		
+		public void EndOperation (string message)
+		{
+			IsSpinning = false;
+			Text = message;
+		}
 	}
 }
 
