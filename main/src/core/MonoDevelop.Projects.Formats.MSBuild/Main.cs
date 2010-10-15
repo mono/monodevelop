c6ceb00dea55f55a870d18c3194f4eb61ccb76e9
Lluis Sanchez Gual <lluis@novell.com>
2010-10-15 12:06:48 +0200

    Fix crash when shutting down MD

--- main/src/core/MonoDevelop.Projects.Formats.MSBuild/Main.cs
+++ main/src/core/MonoDevelop.Projects.Formats.MSBuild/Main.cs
@@ -82,8 +82,11 @@ namespace MonoDevelop.Projects.Formats.MSBuild
 			Thread t = new Thread (delegate () {
 				while (true) {
 					Thread.Sleep (1000);
-					if (Process.GetProcessById (id) == null) {
-						exitEvent.Set ();
+					try {
+						// Throws exception if process is not running
+						Process.GetProcessById (id);
+					}
+					catch {
 						break;
 					}
 				}
