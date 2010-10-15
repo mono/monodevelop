1b501acc0f0d6484527ee791bc9e5d2ee4d1fa94
Michael Hutchinson <mhutchinson@novell.com>
2010-10-15 17:13:42 -0400

    [Debugger] Only show valid/live values in the locals pad

--- main/src/addins/MonoDevelop.Debugger/MonoDevelop.Debugger/LocalsPad.cs
+++ main/src/addins/MonoDevelop.Debugger/MonoDevelop.Debugger/LocalsPad.cs
@@ -28,12 +28,14 @@
 using System;
 using System.Linq;
 using Mono.Debugging.Client;
+using System.Collections.Generic;
 
 namespace MonoDevelop.Debugger
 {
 	public class LocalsPad: ObjectValuePad
 	{
 		StackFrame lastFrame;
+		HashSet<string> lastExpressions = new HashSet<string> ();
 		
 		public LocalsPad()
 		{
@@ -45,11 +47,31 @@ namespace MonoDevelop.Debugger
 		{
 			base.OnUpdateList ();
 			StackFrame frame = DebuggingService.CurrentFrame;
-			if (frame != null && !FrameEquals (frame, lastFrame)) {
+			
+			if (frame == null || !FrameEquals (frame, lastFrame)) {
 				tree.ClearExpressions ();
-				tree.AddExpressions (frame.GetAllLocals ().Select (i => i.Name));
-				lastFrame = frame;
+				lastExpressions = null;
 			}
+			lastFrame = frame;
+			
+			if (frame == null)
+				return;
+			
+			//FIXME: tree should use the local refs rather than expressions. ATM we exclude items without names
+			var expr = new HashSet<string> (frame.GetAllLocals ().Select (i => i.Name)
+				.Where (n => !string.IsNullOrEmpty (n) && n != "?"));
+			
+			//add expressions not in tree already, remove expressions that are longer valid
+			if (lastExpressions != null) {
+				foreach (string rem in lastExpressions.Except (expr))
+					tree.RemoveExpression (rem);
+				foreach (string rem in expr.Except (lastExpressions))
+					tree.AddExpression (rem);
+			} else {
+				tree.AddExpressions (expr);
+			}
+			
+			lastExpressions = expr;
 		}
 		
 		static bool FrameEquals (StackFrame a, StackFrame z)
