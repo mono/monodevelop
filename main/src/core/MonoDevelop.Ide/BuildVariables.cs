72031f33bc7008aa8659d09a7e246c33fa4b6555
Lluis Sanchez <lluis@novell.com>
2010-10-27 20:49:59 +0200

    Updated build vars

--- main/src/core/MonoDevelop.Ide/BuildVariables.cs
+++ main/src/core/MonoDevelop.Ide/BuildVariables.cs
@@ -2,8 +2,8 @@ namespace MonoDevelop.Ide
 {
 	internal class BuildVariables
 	{
-		public static string PackageVersion = "2.4";
-		public static string PackageVersionLabel = "2.4";
+		public static string PackageVersion = "2.4.1";
+		public static string PackageVersionLabel = "2.4.1";
 	}
 }
 
