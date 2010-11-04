9928aab5caac11b636e121b0d809f9a7bda55a44
Mike Krüger <mkrueger@novell.com>
2010-11-04 08:11:55 +0100

    Fixed 'Bug 651069 - Syntax highlighting exception after making change to {}s and running build all'.

--- main/src/core/Mono.Texteditor/Mono.TextEditor.Highlighting/SyntaxModeService.cs
+++ main/src/core/Mono.Texteditor/Mono.TextEditor.Highlighting/SyntaxModeService.cs
@@ -231,11 +231,22 @@ namespace Mono.TextEditor.Highlighting
 				if (startLine < 0)
 					return;
 				try {
-					var spanStack = doc.GetLine (startLine).StartSpan.Clone ();
+					var lineSegment = doc.GetLine (startLine);
+					if (lineSegment == null)
+						return;
+					var span = lineSegment.StartSpan;
+					if (span == null)
+						return;
+					var spanStack = span.Clone ();
 					SyntaxMode.SpanParser parser = mode.CreateSpanParser(doc, mode, null, spanStack);
 					foreach (var line in doc.GetLinesStartingAt (startLine)) {
+						if (line == null)
+							return;
 						if (line.Offset > endOffset) {
-							bool equal = line.StartSpan.Equals(spanStack);
+							span = line.StartSpan;
+							if (span == null)
+								return;
+							bool equal = span.Equals(spanStack);
 							doUpdate |= !equal;
 							if (equal)
 								break;
