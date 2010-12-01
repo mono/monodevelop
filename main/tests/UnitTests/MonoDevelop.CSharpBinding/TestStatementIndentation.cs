ec1fdcdcbff10ccc630be4ccee04d862095be74c
Mike Krüger <mkrueger@novell.com>
2010-12-01 11:15:17 +0100

    Added unit test for if brace forcement.

--- main/tests/UnitTests/MonoDevelop.CSharpBinding/TestStatementIndentation.cs
+++ main/tests/UnitTests/MonoDevelop.CSharpBinding/TestStatementIndentation.cs
@@ -1167,7 +1167,41 @@ if (b) {
 			}
 	}
 }", data.Document.Text);
-		}		
+		}
+		
+		
+		[Test()]
+		public void TestIfForcementAdd ()
+		{
+			TextEditorData data = new TextEditorData ();
+			data.Document.FileName = "a.cs";
+			data.Document.Text = @"class Test
+{
+	void TestMethod ()
+	{
+		if (true)
+			Call ();
+	}
+}";
+			
+			CSharpFormattingPolicy policy = new CSharpFormattingPolicy ();
+			
+			policy.StatementBraceStyle = BraceStyle.EndOfLine;
+			policy.IfElseBraceForcement = BraceForcement.AddBraces;
+			CSharp.Dom.CompilationUnit compilationUnit = new CSharpParser ().Parse (data);
+			compilationUnit.AcceptVisitor (new DomIndentationVisitor (policy, data), null);
+			System.Console.WriteLine (data.Document.Text);
+			Assert.AreEqual (@"class Test
+{
+	void TestMethod ()
+	{
+		if (true) {
+			Call ();
+		}
+	}
+}", data.Document.Text);
+		}
+		
 		[Test()]
 		public void TestElseOnNewLine ()
 		{
