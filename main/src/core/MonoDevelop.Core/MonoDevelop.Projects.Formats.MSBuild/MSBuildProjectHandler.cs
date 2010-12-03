f9de5685002dff275b52948b86a0978c3f71bccb
Lluis Sanchez Gual <lluis@novell.com>
2010-12-03 17:34:58 +0100

    Fix bug 625370 - MD adds duplicate MSBuild imports to VS2008+ projects Default imports are now only added when the project is created.

--- main/src/core/MonoDevelop.Core/MonoDevelop.Projects.Formats.MSBuild/MSBuildProjectHandler.cs
+++ main/src/core/MonoDevelop.Core/MonoDevelop.Projects.Formats.MSBuild/MSBuildProjectHandler.cs
@@ -802,7 +802,10 @@ namespace MonoDevelop.Projects.Formats.MSBuild
 			
 			List<string> currentImports = msproject.Imports;
 			List<string> imports = new List<string> (currentImports);
-			UpdateImports (imports);
+			
+			// If the project is not new, don't add the default project imports,
+			// just assume that the current imports are correct
+			UpdateImports (imports, newProject);
 			foreach (string imp in imports) {
 				if (!currentImports.Contains (imp)) {
 					msproject.AddNewImport (imp, null);
@@ -1039,9 +1042,9 @@ namespace MonoDevelop.Projects.Formats.MSBuild
 			buildItem.Condition = pref.Condition;
 		}
 		
-		void UpdateImports (List<string> imports)
+		void UpdateImports (List<string> imports, bool addItemTypeImports)
 		{
-			if (targetImports != null) {
+			if (targetImports != null && addItemTypeImports) {
 				foreach (string imp in targetImports)
 					if (!imports.Contains (imp))
 						imports.Add (imp);
