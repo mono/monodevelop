3cb4ee33997b5ddd5df42bc11ebccc9d7e9ecd54
Lluis Sanchez Gual <lluis@novell.com>
2010-12-20 17:23:08 +0100

    Added Git addin and updated version number.

--- setup/WixSetup/build.js
+++ setup/WixSetup/build.js
@@ -1,7 +1,7 @@
 var MAJOR_VERSION = 2;
 var MINOR_VERSION = 4;
-var POINT_VERSION = 1;
-var BUILD_VERSION = 0;
+var POINT_VERSION = 9;
+var BUILD_VERSION = 1;
 
 var sh = new ActiveXObject("WScript.Shell");
 var fs = new ActiveXObject("Scripting.FileSystemObject");
