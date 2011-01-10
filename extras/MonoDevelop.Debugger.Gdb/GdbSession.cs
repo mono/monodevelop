e68b07c66ebbaf44c2357ff39f76c285f818470f
Robert Hollencamp <rhollencamp@gmail.com>
2011-01-10 13:52:19 -0500

    [MonoDevelop.Debugger.Gdb] If stepping out of outermost frame (main), use '-exec-continue' to avoid error about "finish not meaningful in outermost frame"

--- extras/MonoDevelop.Debugger.Gdb/GdbSession.cs
+++ extras/MonoDevelop.Debugger.Gdb/GdbSession.cs
@@ -253,8 +253,14 @@ namespace MonoDevelop.Debugger.Gdb
 		protected override void OnFinish ()
 		{
 			SelectThread (activeThread);
-			RunCommand ("-stack-select-frame", "0");
-			RunCommand ("-exec-finish");
+			
+			GdbCommandResult res = RunCommand ("-stack-info-depth", "2");
+			if (res.GetValue ("depth") == "1") {
+				RunCommand ("-exec-continue");
+			} else {
+				RunCommand ("-stack-select-frame", "0");
+				RunCommand ("-exec-finish");
+			}
 		}
 
 		protected override object OnInsertBreakEvent (BreakEvent be, bool activate)
