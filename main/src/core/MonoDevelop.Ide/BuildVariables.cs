dda992c2d774bace6ce184dd0b3328db04f1f01c
Lluis Sanchez Gual <lluis@novell.com>
2011-01-13 18:21:58 +0100

    Bump MD version to 2.6

--- main/src/core/MonoDevelop.Ide/BuildVariables.cs
+++ main/src/core/MonoDevelop.Ide/BuildVariables.cs
@@ -2,8 +2,8 @@ namespace MonoDevelop.Ide
 {
 	internal class BuildVariables
 	{
-		public static string PackageVersion = "2.4";
-		public static string PackageVersionLabel = "2.4";
+		public static string PackageVersion = "2.5.90";
+		public static string PackageVersionLabel = "2.6 Beta 1";
 	}
 }
 
