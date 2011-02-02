953b84c03d25ff4b52cab23a795ea45865a69818
Lluis Sanchez Gual <lluis@novell.com>
2011-02-02 13:51:46 +0100

    More .net 4.0 migration work

--- main/winbuild.sh
+++ main/winbuild.sh
@@ -1,2 +1,2 @@
-export PATH="/c/Windows/Microsoft.NET/Framework/v3.5:$PATH"
+export PATH="/c/Windows/Microsoft.NET/Framework/v4.0.30319:$PATH"
 MSBuild.exe Main.sln -p:Configuration=DebugWin32 -p:Platform=x86 $*
