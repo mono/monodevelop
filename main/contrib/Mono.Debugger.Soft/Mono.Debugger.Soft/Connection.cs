7986c072b02f41689805a2739f1c207644caba2e
Michael Hutchinson <mhutchinson@novell.com>
2011-02-01 23:03:28 -0500

    [Sdb] Update from Mono 6f1cb104

--- main/contrib/Mono.Debugger.Soft/Mono.Debugger.Soft/Connection.cs
+++ main/contrib/Mono.Debugger.Soft/Mono.Debugger.Soft/Connection.cs
@@ -21,6 +21,16 @@ namespace Mono.Debugger.Soft
 		public int MinorVersion {
 			get; set;
 		}
+
+		/*
+		 * Check that this version is at least major:minor
+		 */
+		public bool AtLeast (int major, int minor) {
+			if ((MajorVersion > major) || ((MajorVersion == major && MinorVersion >= minor)))
+				return true;
+			else
+				return false;
+		}
 	}
 
 	class DebugInfo {
@@ -308,7 +318,7 @@ namespace Mono.Debugger.Soft
 		 * with newer runtimes, and vice versa.
 		 */
 		public const int MAJOR_VERSION = 2;
-		public const int MINOR_VERSION = 2;
+		public const int MINOR_VERSION = 3;
 
 		enum WPSuspendPolicy {
 			NONE = 0,
