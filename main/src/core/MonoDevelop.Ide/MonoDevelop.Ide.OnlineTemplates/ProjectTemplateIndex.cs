ead6d5ef2eacf75ef58e4db21ca52b0eafacf506
Michael Hutchinson <mhutchinson@novell.com>
2011-03-15 23:05:01 -0400

    [Ide] Sync online template classes from index tool

--- main/src/core/MonoDevelop.Ide/MonoDevelop.Ide.OnlineTemplates/ProjectTemplateIndex.cs
+++ main/src/core/MonoDevelop.Ide/MonoDevelop.Ide.OnlineTemplates/ProjectTemplateIndex.cs
@@ -119,5 +119,46 @@ namespace MonoDevelop.Ide.OnlineTemplates
 			);
 		}
 	}
+	
+	class ProjectTemplateManifest
+	{
+		const int FORMAT_VERSION = 1;
+		
+		public string Name { get; set; }
+		public string Summary { get; set; }
+		public string Description { get; set; }
+		public string Author { get; set; }
+		public string Tags { get; set; }
+		public string IconFile { get; set; }
+		public string ScreenshotFile { get; set; }
+		public string MinimumVersion { get; set; }
+		
+		public static ProjectTemplateManifest Load (XDocument doc)
+		{
+			var root = doc.Root;
+			if (root.Name != "ProjectTemplate")
+				throw new Exception (string.Format ("Root element was {0}, expected '{1}'", root.Name, "ProjectTemplate"));
+			if ((int) root.Attribute ("format") != FORMAT_VERSION)
+				throw new Exception ("Invalid format version");
+			
+			return new ProjectTemplateManifest {
+				Name = GetRequiredStringElement (root, "Name"),
+				Summary = (string) root.Element ("Summary"),
+				Description = GetRequiredStringElement (root, "Description"),
+				Author = GetRequiredStringElement (root, "Author"),
+				Tags = GetRequiredStringElement (root, "Tags"),
+				IconFile = (string) root.Element ("IconFile"),
+				ScreenshotFile = (string) root.Element ("ScreenshotFile"),
+			};
+		}
+		
+		static string GetRequiredStringElement (XElement parent, string name)
+		{
+			string value = (string) parent.Element (name);
+			if (string.IsNullOrWhiteSpace (value))
+				throw new Exception (string.Format ("The '{0}' element cannot be empty", name));
+			return value;
+		}
+	}
 }
 
