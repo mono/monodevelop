92451a19f5986e44672f5219dca3df8e56995f3a
Mike Krüger <mkrueger@novell.com>
2011-04-27 11:09:04 +0200

    Clear internal error when starting new test run (part of 'Bug 677228 - RemotingException isn't counted as failure').

--- main/src/addins/NUnit/Gui/TestResultsPad.cs
+++ main/src/addins/NUnit/Gui/TestResultsPad.cs
@@ -241,6 +241,10 @@ namespace MonoDevelop.NUnit
 		public void OnTestSuiteChanged (object sender, EventArgs e)
 		{
 			results.Clear ();
+			
+			error = null;
+			errorMessage = null;
+			
 			failuresStore.Clear ();
 			outputView.Buffer.Clear ();
 			outIters.Clear ();
@@ -286,6 +290,10 @@ namespace MonoDevelop.NUnit
 			rootTest = test;
 			results.Clear ();
 			testsToRun = test.CountTestCases ();
+			
+			error = null;
+			errorMessage = null;
+			
 			progressBar.Fraction = 0;
 			progressBar.Text = "";
 			progressBar.Text = "0 / " + testsToRun;
