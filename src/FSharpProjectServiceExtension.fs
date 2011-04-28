e892cbacf40cbe96b7b3632f89fa52f05371441a
Lluis Sanchez <lluis@novell.com>
2011-04-28 12:05:45 +0200

    Added new extensions
    The solution pad extensions sorts the files in the tree using the user
    defined order. The project extension deploys the f# core assembly
    together with the project output.

---
+++ src/FSharpProjectServiceExtension.fs
@@ -0,0 +1,23 @@
+﻿namespace FSharp.MonoDevelop
+
+open System
+open System.IO
+open MonoDevelop.Core
+open MonoDevelop.Ide
+open MonoDevelop.Projects
+open Mono.Addins
+
+type FSharpProjectServiceExtension() =
+  inherit ProjectServiceExtension()
+
+  override x.SupportsItem(item:IBuildTarget) =
+    // Extend any F# project
+    (item :? DotNetProject) && (item :?> DotNetProject).LanguageName = "F#"
+
+  override x.PopulateSupportFileList(project:Project, list:FileCopySet, configuration:ConfigurationSelector) =
+    base.PopulateSupportFileList (project, list, configuration)
+    let outDir = (project.GetConfiguration (configuration) :?> ProjectConfiguration).OutputDirectory
+    let added = list.Add(new FilePath (AddinManager.CurrentAddin.GetFilePath("FSharp.Core.dll")), true)
+    ()
+    
+    
