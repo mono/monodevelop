d202cf380a8919a1ebf4e94c8c0f061f6fc48e7a
Lluis Sanchez <lluis@xamarin.com>
2011-06-10 12:22:33 +0200

    Bump MD version

--- main/src/core/MonoDevelop.Ide/BuildVariables.cs
+++ main/src/core/MonoDevelop.Ide/BuildVariables.cs
@@ -2,8 +2,8 @@ namespace MonoDevelop.Ide
 {
 	internal class BuildVariables
 	{
-		public static string PackageVersion = "2.5.91";
-		public static string PackageVersionLabel = "2.6 Beta 2";
+		public static string PackageVersion = "2.6";
+		public static string PackageVersionLabel = "2.6";
 	}
 }
 
