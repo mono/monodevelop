mono/monodevelop

commit d7ea43557766de3973ae39c011cb79fda870983f

Moved the old nrefactory to another namespace. The new nrefactory uses the same namespace - this'll prevent namespace clashes and will make sure that that version is outdated and'll be replaced over time.

main/src/tools/mdmonitor/gtk-gui/Mono.Instrumentation.Monitor.TimeLineViewWindow.cs
@@ -86,12 +86,7 @@ namespace Mono.Instrumentation.Monitor
 			global::Gtk.Viewport w5 = new global::Gtk.Viewport ();
 			w5.ShadowType = ((global::Gtk.ShadowType)(0));
 			// Container child GtkViewport.Gtk.Container+ContainerChild
-			this.timeView = new global::Mono.Instrumentation.Monitor.TimeLineView ();
-			this.timeView.Name = "timeView";
-			this.timeView.SingleThread = false;
-			this.timeView.TimeScale = 0;
-			this.timeView.Zoom = 0;
-			this.timeView.Scale = 0;
+			this.timeView = null;
 			w5.Add (this.timeView);
 			this.GtkScrolledWindow.Add (w5);
 			this.hbox2.Add (this.GtkScrolledWindow);

commit e04846cc4da03ecb604f616d37f7dafa4cdf3d1a

[mdmonitor] Fix the timeline view
It was broken in commit d7ea43557766de3973ae39c011cb79fda870983f

main/src/tools/mdmonitor/gtk-gui/Mono.Instrumentation.Monitor.TimeLineViewWindow.cs
@@ -86,7 +86,12 @@ namespace Mono.Instrumentation.Monitor
 			global::Gtk.Viewport w5 = new global::Gtk.Viewport ();
 			w5.ShadowType = ((global::Gtk.ShadowType)(0));
 			// Container child GtkViewport.Gtk.Container+ContainerChild
-			this.timeView = null;
+			this.timeView = new global::Mono.Instrumentation.Monitor.TimeLineView ();
+			this.timeView.Name = "timeView";
+			this.timeView.SingleThread = false;
+			this.timeView.TimeScale = 0;
+			this.timeView.Zoom = 0;
+			this.timeView.Scale = 0;
 			w5.Add (this.timeView);
 			this.GtkScrolledWindow.Add (w5);
 			this.hbox2.Add (this.GtkScrolledWindow);