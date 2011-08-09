60814e2c314e83ab083119f0085ad7189abd904e
Lluis Sanchez <lluis@xamarin.com>
2011-08-09 14:15:30 +0200

    Update MonoDevelop version

--- main/src/core/MonoDevelop.Ide/BuildVariables.cs
+++ main/src/core/MonoDevelop.Ide/BuildVariables.cs
@@ -2,8 +2,8 @@ namespace MonoDevelop.Ide
 {
 	internal class BuildVariables
 	{
-		public static string PackageVersion = "2.5.92";
-		public static string PackageVersionLabel = "2.6 Beta 3";
+		public static string PackageVersion = "2.5.93";
+		public static string PackageVersionLabel = "2.6 RC1";
 	}
 }
 
