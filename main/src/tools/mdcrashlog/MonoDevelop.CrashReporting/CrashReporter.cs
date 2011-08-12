693d009ffc9d09b49ab1e22e4c4077a7c63d607c
Alan McGovern <alan.mcgovern@gmail.com>
2011-08-12 19:07:37 +0100

    [CrashMonitor] Update the license headers on all the files.

--- main/src/tools/mdcrashlog/MonoDevelop.CrashReporting/CrashReporter.cs
+++ main/src/tools/mdcrashlog/MonoDevelop.CrashReporting/CrashReporter.cs
@@ -1,10 +1,10 @@
 // 
-// CrashLogger.cs
+// CrashReporter.cs
 //  
 // Author:
-//       alanmcgovern <>
+//       Alan McGovern <alan@xamarin.com>
 // 
-// Copyright (c) 2011 alanmcgovern
+// Copyright (c) Xamarin (http://xamarin.com)
 // 
 // Permission is hereby granted, free of charge, to any person obtaining a copy
 // of this software and associated documentation files (the "Software"), to deal
@@ -23,6 +23,8 @@
 // LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
 // OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
 // THE SOFTWARE.
+
+
 using System;
 using System.IO;
 using System.IO.Compression;
