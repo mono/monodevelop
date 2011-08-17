2ae11cfc6f1a7183da59bebd1ff00e45920153d2
Alex Corrado <alexc@xamarin.com>
2011-08-17 14:03:37 -0400

    2.8 Alpha 1 (2.7)

--- main/src/core/MonoDevelop.Ide/BuildVariables.cs
+++ main/src/core/MonoDevelop.Ide/BuildVariables.cs
@@ -3,7 +3,7 @@ namespace MonoDevelop.Ide
 	internal class BuildVariables
 	{
 		public static string PackageVersion = "2.7";
-		public static string PackageVersionLabel = "2.8 Preview 1";
+		public static string PackageVersionLabel = "2.8 Alpha 1";
 	}
 }
 
