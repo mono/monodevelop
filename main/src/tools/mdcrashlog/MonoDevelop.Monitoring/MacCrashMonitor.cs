80d1b5dc13bee86f59f941d655f6177a97237a6b
Alan McGovern <alan.mcgovern@gmail.com>
2011-08-18 15:34:58 +0100

    [CrashMonitor] Add missing license headers and fix existing ones.

--- main/src/tools/mdcrashlog/MonoDevelop.Monitoring/MacCrashMonitor.cs
+++ main/src/tools/mdcrashlog/MonoDevelop.Monitoring/MacCrashMonitor.cs
@@ -4,7 +4,7 @@
 // Author:
 //       Alan McGovern <alan@xamarin.com>
 // 
-// Copyright (c) Xamarin (http://xamarin.com)
+// Copyright 2011, Xamarin Inc.
 // 
 // Permission is hereby granted, free of charge, to any person obtaining a copy
 // of this software and associated documentation files (the "Software"), to deal
