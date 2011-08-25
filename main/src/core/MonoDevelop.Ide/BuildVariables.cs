6b3ce5e029744b3de73bcb04e3a6a72743883eab
Alex Corrado <alexc@xamarin.com>
2011-08-25 00:20:39 -0400

    The real 2.8 Alpha 2 (2.7.11)

--- main/src/core/MonoDevelop.Ide/BuildVariables.cs
+++ main/src/core/MonoDevelop.Ide/BuildVariables.cs
@@ -2,7 +2,7 @@ namespace MonoDevelop.Ide
 {
 	internal class BuildVariables
 	{
-		public static string PackageVersion = "2.7.1";
+		public static string PackageVersion = "2.7.11";
 		public static string PackageVersionLabel = "2.8 Alpha 2";
 	}
 }
