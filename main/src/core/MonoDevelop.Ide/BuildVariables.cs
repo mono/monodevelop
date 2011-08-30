c95074e92466f526209f657ea42713e3f376cce9
Alex Corrado <alexc@xamarin.com>
2011-08-29 22:12:38 -0400

    2.6

--- main/src/core/MonoDevelop.Ide/BuildVariables.cs
+++ main/src/core/MonoDevelop.Ide/BuildVariables.cs
@@ -2,8 +2,8 @@ namespace MonoDevelop.Ide
 {
 	internal class BuildVariables
 	{
-		public static string PackageVersion = "2.5.93.1";
-		public static string PackageVersionLabel = "2.6 RC2";
+		public static string PackageVersion = "2.6";
+		public static string PackageVersionLabel = "2.6";
 	}
 }
 
