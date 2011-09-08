2b47224db44de14247a8276fe79d042c73bb4acf
Jeffrey Stedfast <jeff@xamarin.com>
2011-09-08 15:08:14 -0400

    Don't add frameworks here, add them in the ios code instead

--- main/src/addins/MonoDevelop.MacDev/XcodeIntegration/XcodeProject.cs
+++ main/src/addins/MonoDevelop.MacDev/XcodeIntegration/XcodeProject.cs
@@ -129,10 +129,6 @@ namespace MonoDevelop.MacDev.XcodeIntegration
 
 			this.files.Add (target);
 			this.project.AddNativeTarget (nativeTarget);
-
-			AddFramework ("UIKit");
-			AddFramework ("Foundation");
-			AddFramework ("CoreGraphics");
 		}
 		
 		public string Name { get { return name; } }
