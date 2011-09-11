65e47ec873f0805a616052be24de9c801917feff
Michael Hutchinson <mhutch@xamarin.com>
2011-09-11 17:29:34 +0100

    [Ide] Move entry code to Monodevelop.Ide

--- main/src/core/MonoDevelop.Ide/MonoDevelop.Ide/IdeStartup.cs
+++ main/src/core/MonoDevelop.Ide/MonoDevelop.Ide/IdeStartup.cs
@@ -476,6 +476,71 @@ namespace MonoDevelop.Ide
 				
 			return (int)(hash % 1000);
 		}
+		
+		public static int Main (string[] args)
+		{
+			bool retry = false;
+
+			EnableFileLogging ();
+			
+			do {
+				try {
+					Runtime.SetProcessName (BrandingService.ApplicationName);
+					var app = new IdeStartup ();
+					return app.Run (args);
+				} catch (Exception ex) {
+					if (!retry && AddinManager.IsInitialized) {
+						LoggingService.LogWarning (BrandingService.ApplicationName + " failed to start. Rebuilding addins registry.");
+						AddinManager.Registry.Rebuild (new Mono.Addins.ConsoleProgressStatus (true));
+						LoggingService.LogInfo ("Addin registry rebuilt. Restarting MonoDevelop.");
+						retry = true;
+					} else {
+						LoggingService.LogFatalError (BrandingService.ApplicationName + " failed to start. Some of the assemblies required to run MonoDevelop (for example gtk-sharp, gnome-sharp or gtkhtml-sharp) may not be properly installed in the GAC.", ex);
+						retry = false;
+					}
+				} finally {
+					Runtime.Shutdown ();
+				}
+			}
+			while (retry);
+
+			if (logFile != null)
+				logFile.Close ();
+
+			return -1;
+		}
+
+		static StreamWriter logFile;
+
+		static void EnableFileLogging ( )
+		{
+			if (Path.DirectorySeparatorChar != '\\')
+				return;
+
+			// On Windows log all output to a log file
+
+			FilePath logDir = UserProfile.Current.LogDir;
+			if (!Directory.Exists (logDir))
+				Directory.CreateDirectory (logDir);
+
+			string file = logDir.Combine ("log.txt");
+			try {
+				logFile = new StreamWriter (file);
+				logFile.AutoFlush = true;
+
+				var tw = new MonoDevelop.Core.ProgressMonitoring.LogTextWriter ();
+				tw.ChainWriter (logFile);
+				tw.ChainWriter (Console.Out);
+				Console.SetOut (tw);
+
+				tw = new MonoDevelop.Core.ProgressMonitoring.LogTextWriter ();
+				tw.ChainWriter (logFile);
+				tw.ChainWriter (Console.Error);
+				Console.SetError (tw);
+			}
+			catch {
+			}
+		}
 	}
 	
 #pragma warning disable 0618
