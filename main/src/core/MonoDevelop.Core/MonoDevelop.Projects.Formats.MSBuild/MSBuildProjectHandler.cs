ce96dc18fd8675e4452054af619bcc71c6181342
Jeffrey Stedfast <jeff@xamarin.com>
2011-09-22 15:15:35 -0400

    Revert previous 2 commits, I was confused

--- main/src/core/MonoDevelop.Core/MonoDevelop.Projects.Formats.MSBuild/MSBuildProjectHandler.cs
+++ main/src/core/MonoDevelop.Core/MonoDevelop.Projects.Formats.MSBuild/MSBuildProjectHandler.cs
@@ -241,7 +241,7 @@ namespace MonoDevelop.Projects.Formats.MSBuild
 			MSBuildFileFormat format = null;
 			if (expectedFormat != null) {
 				if (p.ToolsVersion != expectedFormat.ToolsVersion) {
-					monitor.ReportWarning (string.Format (GettextCatalog.GetString ("Project '{0}' has different ToolsVersion than the containing solution."), Path.GetFileNameWithoutExtension (fileName)));
+					monitor.ReportWarning (GettextCatalog.GetString ("Project '{0}' has different ToolsVersion than the containing solution.", Path.GetFileNameWithoutExtension (fileName)));
 				} else {
 					format = expectedFormat;
 				}
