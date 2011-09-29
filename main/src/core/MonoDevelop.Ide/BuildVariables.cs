1b5e424433882f5f74ee73f457ef106bee499562
Xamarin Release Manager <release-manager@xamarin.com>
2011-09-29 13:12:24 -0400

    Updated package version to 2.8

--- main/src/core/MonoDevelop.Ide/BuildVariables.cs
+++ main/src/core/MonoDevelop.Ide/BuildVariables.cs
@@ -2,8 +2,8 @@ namespace MonoDevelop.Ide
 {
 	internal class BuildVariables
 	{
-		public static string PackageVersion = "2.7.22";
-		public static string PackageVersionLabel = "2.8 Beta 3";
+		public static string PackageVersion = "2.8";
+		public static string PackageVersionLabel = "2.8";
 	}
 }
 
