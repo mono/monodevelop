959cfff628910484572ad6465d28855a0518769f
Alan McGovern <alan.mcgovern@gmail.com>
2011-12-06 14:14:43 +0000

    [MacCore] Add a missing String.Format ()
    I accidentally missed the String.Format here and ended up not
    appending the file name to the error message. Not sure how I missed
    that when I tested the behaviour. Whoops.

--- main/src/addins/MonoDevelop.MacDev/XcodeInterfaceBuilderDisplayBinding.cs
+++ main/src/addins/MonoDevelop.MacDev/XcodeInterfaceBuilderDisplayBinding.cs
@@ -70,7 +70,7 @@ namespace MonoDevelop.MacDev
 		public override void Launch (params string[] files)
 		{
 			if (!AppleSdkSettings.IsXcode42)
-				throw new UserException ("XCode 4.2 is required to open file {0}", files [0]);
+				throw new UserException (string.Format ("XCode 4.2 is required to open file {0}", files [0]));
 
 			project.XcodeProjectTracker.OpenDocument (files[0]);
 		}
