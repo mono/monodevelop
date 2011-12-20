6024dcf49f0b921676ec9525d9b1b3ab06d87f8d
Michael Hutchinson <m.j.hutchinson@gmail.com>
2011-12-19 20:36:34 -0500

    [MacPlatform] Fix #1206 - Accessory view in Mac dialogs is mispositioned

--- main/src/addins/MacPlatform/Dialogs/MacOpenFileDialogHandler.cs
+++ main/src/addins/MacPlatform/Dialogs/MacOpenFileDialogHandler.cs
@@ -158,6 +158,15 @@ namespace MonoDevelop.MacIntegration
 							slnViewerSelected = true;
 						}
 						box.Layout ();
+						
+						//re-center the accessory view in its parent, Cocoa does this for us initially and after
+						//resizing the window, but we need to do it again after altering its layout
+						var superFrame = box.View.Superview.Frame;
+						var frame = box.View.Frame;
+						//not sure why it's ceiling, but this matches the Cocoa layout
+						frame.X = (float) Math.Ceiling ((superFrame.Width - frame.Width) / 2);
+						frame.Y = (float) Math.Ceiling ((superFrame.Height - frame.Height) / 2);
+						box.View.Frame = frame;
 					} 
 					if (encodingSelector != null)
 						encodingSelector.Enabled = !slnViewerSelected;
