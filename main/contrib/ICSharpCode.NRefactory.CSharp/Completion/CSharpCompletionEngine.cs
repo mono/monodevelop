aedc31fae2d5ef26351e6fe905e50f593b72828b
Mike Krüger <mkrueger@xamarin.com>
2012-01-16 12:01:48 +0100

    Fixed "Bug 2800 - Finalize is offered as a valid completion target".

--- main/contrib/ICSharpCode.NRefactory.CSharp/Completion/CSharpCompletionEngine.cs
+++ main/contrib/ICSharpCode.NRefactory.CSharp/Completion/CSharpCompletionEngine.cs
@@ -870,6 +870,9 @@ namespace ICSharpCode.NRefactory.CSharp.Completion
 				if (this.currentMember != null) {
 					var def = ctx.CurrentTypeDefinition ?? Compilation.MainAssembly.GetTypeDefinition (currentType);
 					foreach (var member in def.GetMembers ()) {
+						if (member is IMethod && ((IMethod)member).FullName == "System.Object.Finalize")
+							continue;
+											
 						if (memberPred == null || memberPred (member))
 							wrapper.AddMember (member);
 					}
@@ -1774,6 +1777,10 @@ namespace ICSharpCode.NRefactory.CSharp.Completion
 						//					Console.WriteLine ("skip non static member: " + member.FullName);
 						continue;
 					}
+					
+					if (member is IMethod && ((IMethod)member).FullName == "System.Object.Finalize")
+						continue;
+					
 					//				Console.WriteLine ("add : "+ member.FullName + " --- " + member.IsStatic);
 					result.AddMember (member);
 				}
