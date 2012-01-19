mono/monodevelop

commit ff093f475867194e0c659be702639eed2c25cf10

[Core] Preserve the BOM when saving MSBuildProject files.
Fixes bug #2909.

main/src/core/MonoDevelop.Core/MonoDevelop.Projects.Text/TextFile.cs
@@ -334,7 +334,99 @@ namespace MonoDevelop.Projects.Text
 			WriteFile (name, text.ToString (), sourceEncoding, HadBOM);
 			modified = false;
 		}
+		
+		public static void WriteFile (FilePath fileName, byte[] content, string encoding, ByteOrderMark bom, bool onlyIfChanged)
+		{
+			int contentLength = content.Length + (bom != null ? bom.Length : 0);
+			byte[] converted;
+			
+			if (encoding != null)
+				converted = ConvertToBytes (content, content.LongLength, encoding, "UTF-8");
+			else
+				converted = content;
+			
+			if (onlyIfChanged) {
+				FileInfo finfo = new FileInfo (fileName);
+				if (finfo.Length == contentLength) {
+					bool changed = false;
+					
+					// Open the file on disk and compare them byte by byte...
+					using (FileStream stream = finfo.Open (FileMode.Open, FileAccess.Read, FileShare.Read)) {
+						byte[] buf = new byte [4096];
+						int bomOffset = 0;
+						int offset = 0;
+						int nread;
+						int i;
+						
+						while (!changed && (nread = stream.Read (buf, 0, buf.Length)) > 0) {
+							i = 0;
+							
+							if (bom != null && bomOffset < bom.Length) {
+								while (i < nread && bomOffset < bom.Length) {
+									if (bom.Bytes[bomOffset] != buf[i]) {
+										changed = true;
+										break;
+									}
+									
+									bomOffset++;
+									i++;
+								}
+								
+								if (changed)
+									break;
+							}
+							
+							while (i < nread && offset < converted.Length) {
+								if (converted[offset] != buf[i]) {
+									changed = true;
+									break;
+								}
+								
+								offset++;
+								i++;
+							}
+							
+							if (offset == converted.Length && i < nread)
+								changed = true;
+						}
+						
+						if (offset < converted.Length)
+							changed = true;
+					}
+					
+					if (!changed)
+						return;
+				}
+				
+				// Content has changed...
+			}
+			
+			string tempName = Path.GetDirectoryName (fileName) + 
+				Path.DirectorySeparatorChar + ".#" + Path.GetFileName (fileName);
+			FileStream fs = new FileStream (tempName, FileMode.Create, FileAccess.Write);
+			
+			if (bom != null)
+				fs.Write (bom.Bytes, 0, bom.Length);
+			
+			fs.Write (converted, 0, converted.Length);
+			fs.Flush ();
+			fs.Close ();
 
+			FileService.SystemRename (tempName, fileName);
+		}
+		
+		public static void WriteFile (FilePath fileName, string content, string encoding, ByteOrderMark bom, bool onlyIfChanged)
+		{
+			byte[] buf = Encoding.UTF8.GetBytes (content);
+			
+			WriteFile (fileName, buf, encoding, bom, onlyIfChanged);
+		}
+		
+		public static void WriteFile (FilePath fileName, string content, ByteOrderMark bom, bool onlyIfChanged)
+		{
+			WriteFile (fileName, content, bom != null ? bom.Name : null, bom, onlyIfChanged);
+		}
+		
 		public static void WriteFile (FilePath fileName, string content, string encoding)
 		{
 			WriteFile (fileName, content, encoding, false);
@@ -342,24 +434,9 @@ namespace MonoDevelop.Projects.Text
 		
 		public static void WriteFile (FilePath fileName, string content, string encoding, bool saveBOM)
 		{
-			byte[] buf = Encoding.UTF8.GetBytes (content);
-			ByteOrderMark bom;
+			ByteOrderMark bom = saveBOM && encoding != null ? ByteOrderMark.GetByName (encoding) : null;
 			
-			if (encoding != null)
-				buf = ConvertToBytes (buf, buf.LongLength, encoding, "UTF-8");
-			
-			string tempName = Path.GetDirectoryName (fileName) + 
-				Path.DirectorySeparatorChar + ".#" + Path.GetFileName (fileName);
-			FileStream fs = new FileStream (tempName, FileMode.Create, FileAccess.Write);
-			
-			if (saveBOM && (bom = ByteOrderMark.GetByName (encoding)) != null)
-				fs.Write (bom.Bytes, 0, bom.Length);
-			
-			fs.Write (buf, 0, buf.Length);
-			fs.Flush ();
-			fs.Close ();
-
-			FileService.SystemRename (tempName, fileName);
+			WriteFile (fileName, content, encoding, bom, false);
 		}
 	}
 	

main/src/core/MonoDevelop.Core/MonoDevelop.Projects.Formats.MSBuild/MSBuildProject.cs
@@ -32,6 +32,9 @@ using System.Collections.Generic;
 using System.Xml;
 using System.Text;
 
+using MonoDevelop.Projects.Utility;
+using MonoDevelop.Projects.Text;
+
 namespace MonoDevelop.Projects.Formats.MSBuild
 {
 	public class MSBuildProject
@@ -45,6 +48,7 @@ namespace MonoDevelop.Projects.Formats.MSBuild
 		
 		bool endsWithEmptyLine;
 		string newLine = Environment.NewLine;
+		ByteOrderMark bom;
 		
 		internal static XmlNamespaceManager XmlNamespaceManager {
 			get {
@@ -65,51 +69,89 @@ namespace MonoDevelop.Projects.Formats.MSBuild
 		
 		public void Load (string file)
 		{
-			string xml = File.ReadAllText (file);
-			LoadXml (xml);
-		}
-		
-		public void LoadXml (string xml)
-		{
+			using (FileStream fs = File.OpenRead (file)) {
+				byte[] buf = new byte [1024];
+				int nread, i;
+				
+				if ((nread = fs.Read (buf, 0, buf.Length)) <= 0)
+					return;
+				
+				if (ByteOrderMark.TryParse (buf, nread, out bom))
+					i = bom.Length;
+				else
+					i = 0;
+				
+				do {
+					// Read to the first newline to figure out which line endings this file is using
+					while (i < nread) {
+						if (buf[i] == '\r') {
+							newLine = "\r\n";
+							break;
+						} else if (buf[i] == '\n') {
+							newLine = "\n";
+							break;
+						}
+						
+						i++;
+					}
+					
+					if (newLine == null) {
+						if ((nread = fs.Read (buf, 0, buf.Length)) <= 0) {
+							newLine = "\n";
+							break;
+						}
+						
+						i = 0;
+					}
+				} while (newLine == null);
+				
+				// Check for a blank line at the end
+				endsWithEmptyLine = fs.Seek (-1, SeekOrigin.End) > 0 && fs.ReadByte () == (int) '\n';
+			}
+			
+			// Load the XML document
 			doc = new XmlDocument ();
 			doc.PreserveWhitespace = false;
+			
+			// HACK: XmlStreamReader will fail if the file is encoded in UTF-8 but has <?xml version="1.0" encoding="utf-16"?>
+			// To work around this, we load the XML content into a string and use XmlDocument.LoadXml() instead.
+			string xml = File.ReadAllText (file);
+			
 			doc.LoadXml (xml);
-			newLine = CountNewLines ("\r\n", xml) > (CountNewLines ("\n", xml) / 2) ? "\r\n" : "\n";
-			if (xml.EndsWith (newLine))
-				endsWithEmptyLine = true;
 		}
 		
-		class Utf8Writer: StringWriter
+		class ProjectWriter : StringWriter
 		{
+			Encoding encoding;
+			
+			public ProjectWriter (ByteOrderMark bom)
+			{
+				encoding = bom != null ? Encoding.GetEncoding (bom.Name) : null;
+				ByteOrderMark = bom;
+			}
+			
+			public ByteOrderMark ByteOrderMark {
+				get; private set;
+			}
+			
 			public override Encoding Encoding {
-				get { return Encoding.UTF8; }
+				get { return encoding ?? Encoding.UTF8; }
 			}
 		}
 		
-		public string Save ()
+		public void Save (string fileName)
 		{
 			// StringWriter.Encoding always returns UTF16. We need it to return UTF8, so the
 			// XmlDocument will write the UTF8 header.
-			Utf8Writer sw = new Utf8Writer ();
+			ProjectWriter sw = new ProjectWriter (bom);
 			sw.NewLine = newLine;
 			doc.Save (sw);
-			string txt = sw.ToString ();
-			if (endsWithEmptyLine && !txt.EndsWith (newLine))
-				txt += newLine;
-			return txt;
-		}
-		
-		int CountNewLines (string nl, string text)
-		{
-			int i = -1;
-			int c = -1;
-			do {
-				c++;
-				i++;
-				i = text.IndexOf (nl, i);
-			}
-			while (i != -1);
-			return c;
+			
+			string content = sw.ToString ();
+			if (endsWithEmptyLine && !content.EndsWith (newLine))
+				content += newLine;
+			
+			TextFile.WriteFile (fileName, content, bom, true);
 		}
 		
 		public string DefaultTargets {

main/src/core/MonoDevelop.Core/MonoDevelop.Projects.Formats.MSBuild/MSBuildProjectHandler.cs
@@ -46,7 +46,6 @@ namespace MonoDevelop.Projects.Formats.MSBuild
 {
 	public class MSBuildProjectHandler: MSBuildHandler, IResourceHandler, IPathHandler, IAssemblyReferenceHandler
 	{
-		string fileContent;
 		List<string> targetImports = new List<string> ();
 		IResourceHandler customResourceHandler;
 		List<string> subtypeGuids = new List<string> ();
@@ -57,6 +56,7 @@ namespace MonoDevelop.Projects.Formats.MSBuild
 		ITimeTracker timer;
 		bool useXBuild;
 		MSBuildVerbosity verbosity;
+		string fileName;
 		
 		struct ItemInfo {
 			public MSBuildItem Item;
@@ -241,8 +241,8 @@ namespace MonoDevelop.Projects.Formats.MSBuild
 			
 			timer.Trace ("Reading project file");
 			MSBuildProject p = new MSBuildProject ();
-			fileContent = File.ReadAllText (fileName);
-			p.LoadXml (fileContent);
+			this.fileName = fileName;
+			p.Load (fileName);
 			
 			//determine the file format
 			MSBuildFileFormat format = null;
@@ -339,8 +339,7 @@ namespace MonoDevelop.Projects.Formats.MSBuild
 							throw new Exception ("Unable to correct flavor GUID");
 						var gg = string.Join (";", subtypeGuids) + ";" + TypeGuid;
 						p.GetGlobalPropertyGroup ().SetPropertyValue ("ProjectTypeGuids", gg.ToUpper ());
-						fileContent = p.Save ();
-						MonoDevelop.Projects.Text.TextFile.WriteFile (fileName, fileContent, "UTF-8");
+						p.Save (fileName);
 					}
 					st.UpdateImports ((SolutionEntityItem)item, targetImports);
 				} else
@@ -449,8 +448,8 @@ namespace MonoDevelop.Projects.Formats.MSBuild
 			
 			foreach (var t in toRemove)
 				msproject.RemoveItem (t);
-			fileContent = msproject.Save ();
-			MonoDevelop.Projects.Text.TextFile.WriteFile (fileName, fileContent, "UTF-8");
+			
+			msproject.Save (fileName);
 		}
 		
 		void Load (IProgressMonitor monitor, MSBuildProject msproject)
@@ -781,8 +780,8 @@ namespace MonoDevelop.Projects.Formats.MSBuild
 			DotNetProject dotNetProject = Item as DotNetProject;
 			
 			MSBuildProject msproject = new MSBuildProject ();
-			if (fileContent != null) {
-				msproject.LoadXml (fileContent);
+			if (fileName != null) {
+				msproject.Load (fileName);
 			} else {
 				msproject.DefaultTargets = "Build";
 				newProject = true;
@@ -856,7 +855,7 @@ namespace MonoDevelop.Projects.Formats.MSBuild
 				langParams = dotNetProject.LanguageParameters;
 			}
 			
-			if (fileContent == null)
+			if (fileName == null)
 				ser.InternalItemProperties.ItemData.Sort (globalConfigOrder);
 
 			WritePropertyGroupMetadata (globalGroup, ser.InternalItemProperties.ItemData, ser, Item, langParams);
@@ -1031,16 +1030,11 @@ namespace MonoDevelop.Projects.Formats.MSBuild
 			} else
 				msproject.RemoveProjectExtensions ("MonoDevelop");
 			
-			string txt = msproject.Save ();
-			
 			// Don't save the file to disk if the content did not change
-			if (txt != fileContent) {
-				MonoDevelop.Projects.Text.TextFile.WriteFile (eitem.FileName, txt, "UTF-8");
-				fileContent = txt;
-				
-				if (projectBuilder != null)
-					projectBuilder.Refresh ();
-			}
+			msproject.Save (eitem.FileName);
+			
+			if (projectBuilder != null)
+				projectBuilder.Refresh ();
 		}
 		
 		void ForceDefaultValueSerialization (MSBuildSerializer ser, MSBuildPropertySet baseGroup, object ob)

main/src/core/MonoDevelop.Core/MonoDevelop.Projects.Formats.MSBuild/MSBuildProjectService.cs
@@ -615,7 +615,7 @@ namespace MonoDevelop.Projects.Formats.MSBuild
 		static string LoadProjectTypeGuids (string fileName)
 		{
 			MSBuildProject project = new MSBuildProject ();
-			project.LoadXml (File.ReadAllText (fileName));
+			project.Load (fileName);
 			
 			MSBuildPropertySet globalGroup = project.GetGlobalPropertyGroup ();
 			if (globalGroup == null)

commit 5885cac80546a324199c31965cbfeea984773536

[Core] Fix a regression from ff093f47
Only check the file length if it exists on disk.

main/src/core/MonoDevelop.Core/MonoDevelop.Projects.Text/TextFile.cs
@@ -347,7 +347,7 @@ namespace MonoDevelop.Projects.Text
 			
 			if (onlyIfChanged) {
 				FileInfo finfo = new FileInfo (fileName);
-				if (finfo.Length == contentLength) {
+				if (finfo.Exists && finfo.Length == contentLength) {
 					bool changed = false;
 					
 					// Open the file on disk and compare them byte by byte...