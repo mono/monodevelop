33b8ae44cea1813619c53197386c88594abaa5a7
Michael Hutchinson <m.j.hutchinson@gmail.com>
2012-02-02 17:55:06 -0500

    [Project] Remove unnecessary tasks in build output
    They didn't add much, and made the build output wordier and
    more deeply indented. They also made the status bar less useful
    because it did not report *which* project was being built.

--- main/src/core/MonoDevelop.Core/MonoDevelop.Projects/Project.cs
+++ main/src/core/MonoDevelop.Core/MonoDevelop.Projects/Project.cs
@@ -433,7 +433,7 @@ namespace MonoDevelop.Projects
 			//copy references and files marked to "CopyToOutputDirectory"
 			CopySupportFiles (monitor, configuration);
 		
-			monitor.BeginTask (GettextCatalog.GetString ("Performing main compilation..."), 0);
+			monitor.Log.WriteLine ("Performing main compilation...");
 			
 			BuildResult res = DoBuild (monitor, configuration);
 
@@ -445,7 +445,6 @@ namespace MonoDevelop.Projects
 
 				monitor.Log.WriteLine (GettextCatalog.GetString ("Build complete -- ") + errorString + ", " + warningString);
 			}
-			monitor.EndTask ();
 
 			return res;
 		}
@@ -654,7 +653,7 @@ namespace MonoDevelop.Projects
 				return;
 			}
 			
-			monitor.BeginTask (GettextCatalog.GetString ("Performing clean..."), 0);
+			monitor.Log.WriteLine ("Removing output files...");
 			
 			// Delete generated files
 			foreach (FilePath file in GetOutputFiles (configuration)) {
@@ -668,9 +667,7 @@ namespace MonoDevelop.Projects
 			DeleteSupportFiles (monitor, configuration);
 			
 			DoClean (monitor, config.Selector);
-			monitor.Log.WriteLine ();
 			monitor.Log.WriteLine (GettextCatalog.GetString ("Clean complete"));
-			monitor.EndTask ();
 		}
 
 		protected virtual void DoClean (IProgressMonitor monitor, ConfigurationSelector configuration)
