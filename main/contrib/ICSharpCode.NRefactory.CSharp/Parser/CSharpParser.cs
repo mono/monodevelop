a6cc99ec027f459a80dafe7f33219ccec296b3e8
Mike Krüger <mkrueger@xamarin.com>
2012-02-10 16:16:49 +0100

    Fixed "Bug 3373 - Error while converting :StringLiteral ("") - unknown type name".

--- main/contrib/ICSharpCode.NRefactory.CSharp/Parser/CSharpParser.cs
+++ main/contrib/ICSharpCode.NRefactory.CSharp/Parser/CSharpParser.cs
@@ -247,8 +247,6 @@ namespace ICSharpCode.NRefactory.CSharp
 						return new PrimitiveType ("new", Convert (sce.Location));
 					}
 				}
-				System.Console.WriteLine ("Error while converting :" + typeName + " - unknown type name");
-				System.Console.WriteLine (Environment.StackTrace);
 				return new SimpleType ("unknown");
 			}
 			
