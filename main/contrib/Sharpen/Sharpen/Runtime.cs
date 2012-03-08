6379d09af700fb5451ee53636490b628f9ad5162
Alan McGovern <alan.mcgovern@gmail.com>
2012-03-08 10:14:06 +0000

    [NGit] Set a default value for jgit.fs.debug to prevent an exception
    Set this to false by default so that a call to boolean.Parse does not
    explode when NGit is invoked on windows.

--- main/contrib/Sharpen/Sharpen/Runtime.cs
+++ main/contrib/Sharpen/Sharpen/Runtime.cs
@@ -84,6 +84,7 @@ namespace Sharpen
 		{
 			if (properties == null) {
 				properties = new Hashtable ();
+				properties ["jgit.fs.debug"] = "false";
 				properties ["user.home"] = Environment.GetFolderPath (Environment.SpecialFolder.Personal);
 				properties ["java.library.path"] = Environment.GetEnvironmentVariable ("PATH");
 				if (Path.DirectorySeparatorChar != '\\')
