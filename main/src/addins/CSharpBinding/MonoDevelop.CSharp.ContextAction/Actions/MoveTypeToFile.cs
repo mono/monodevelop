1d279d22c1a6d63dca2d049bc4b63e30109355dc
Mike Krüger <mkrueger@xamarin.com>
2012-03-09 15:07:13 +0100

    [CSharpBinding] MoveTypeToFile action now saves the project.

--- main/src/addins/CSharpBinding/MonoDevelop.CSharp.ContextAction/Actions/MoveTypeToFile.cs
+++ main/src/addins/CSharpBinding/MonoDevelop.CSharp.ContextAction/Actions/MoveTypeToFile.cs
@@ -34,6 +34,7 @@ using MonoDevelop.Refactoring;
 using System.IO;
 using System.Text;
 using MonoDevelop.Ide.StandardHeader;
+using MonoDevelop.Core.ProgressMonitoring;
 
 namespace MonoDevelop.CSharp.ContextAction
 {
@@ -61,7 +62,8 @@ namespace MonoDevelop.CSharp.ContextAction
 			string correctFileName = GetCorrectFileName (context, type);
 			if (IsSingleType (context)) {
 				FileService.RenameFile (context.Document.FileName, correctFileName);
-//				context.Do (new RenameFileChange ());
+				if (context.Document.Project != null)
+					context.Document.Project.Save (new NullProgressMonitor ());
 				return;
 			}
 			
