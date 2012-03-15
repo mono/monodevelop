bc23098475c1652ad68c1f975284ffa36dfecf75
Jeffrey Stedfast <jeff@xamarin.com>
2012-03-15 16:48:53 -0400

    [Debugger] Fixed uncaught exception dialog's display of stack traces.
    Fixes bug #3919.

--- main/src/addins/VersionControl/MonoDevelop.VersionControl/gtk-gui/MonoDevelop.VersionControl.Views.DiffWidget.cs
+++ main/src/addins/VersionControl/MonoDevelop.VersionControl/gtk-gui/MonoDevelop.VersionControl.Views.DiffWidget.cs
@@ -57,7 +57,7 @@ namespace MonoDevelop.VersionControl.Views
 			this.buttonDiff.CanFocus = true;
 			this.buttonDiff.Name = "buttonDiff";
 			this.buttonDiff.UseUnderline = true;
-			this.buttonDiff.Label = global::Mono.Unix.Catalog.GetString ("Patch");
+			this.buttonDiff.Label = global::Mono.Unix.Catalog.GetString ("Unified Diff");
 			this.hbox2.Add (this.buttonDiff);
 			global::Gtk.Box.BoxChild w3 = ((global::Gtk.Box.BoxChild)(this.hbox2 [this.buttonDiff]));
 			w3.PackType = ((global::Gtk.PackType)(1));
