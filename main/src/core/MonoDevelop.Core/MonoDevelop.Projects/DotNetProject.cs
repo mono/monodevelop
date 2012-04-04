mono/monodevelop

commit 9fb07d423fabe7cd898f1a68f4d156a8c6b9fb9d

[Core] Fix #4144 - MonoDevelop sets default namespace if empty

main/src/core/MonoDevelop.Core/MonoDevelop.Projects/DotNetProject.cs
@@ -149,9 +149,9 @@ namespace MonoDevelop.Projects
 			if (projectCreateInfo != null) {
 				Name = projectCreateInfo.ProjectName;
 				binPath = projectCreateInfo.BinPath;
-				defaultNamespace = SanitisePotentialNamespace (projectCreateInfo.ProjectName);
-			} else
+			} else {
 				binPath = ".";
+			}
 
 			foreach (DotNetProjectConfiguration dotNetProjectConfig in Configurations) {
 				dotNetProjectConfig.OutputDirectory = Path.Combine (binPath, dotNetProjectConfig.Name);
@@ -241,7 +241,10 @@ namespace MonoDevelop.Projects
 					languageParameters.ParentProject = this;
 			}
 		}
-
+		
+		/// <summary>
+		/// Default namespace setting. May be empty, use GetDefaultNamespace to get a usable value.
+		/// </summary>
 		public string DefaultNamespace {
 			get { return defaultNamespace; }
 			set {
@@ -896,7 +899,11 @@ namespace MonoDevelop.Projects
 				return false;
 			return LanguageBinding.IsSourceCodeFile (fileName);
 		}
-
+		
+		/// <summary>
+		/// Gets the default namespace for the file, according to the naming policy.
+		/// </summary>
+		/// <remarks>Always returns a valid namespace, even if the fileName is null.</remarks>
 		public virtual string GetDefaultNamespace (string fileName)
 		{
 			DotNetNamingPolicy pol = Policies.Get<DotNetNamingPolicy> ();
@@ -906,6 +913,10 @@ namespace MonoDevelop.Projects
 			string defaultNmspc = !string.IsNullOrEmpty (DefaultNamespace)
 				? DefaultNamespace
 				: SanitisePotentialNamespace (Name) ?? "Application";
+			
+			if (string.IsNullOrEmpty (fileName)) {
+				return defaultNmspc;
+			}
 
 			string dirname = Path.GetDirectoryName (fileName);
 			string relativeDirname = null;
@@ -1032,9 +1043,6 @@ namespace MonoDevelop.Projects
 			IResourceHandler handler = ItemHandler as IResourceHandler;
 			if (handler != null)
 				MigrateResourceIds (handler, ResourceHandler);
-			
-			if (String.IsNullOrEmpty (defaultNamespace))
-				defaultNamespace = SanitisePotentialNamespace (Name);
 
 			base.OnEndLoad ();
 		}

main/src/addins/MonoDevelop.WebReferences/MonoDevelop.WebReferences/WebServiceDiscoveryResult.cs
@@ -72,6 +72,11 @@ namespace MonoDevelop.WebReferences
 		
 		public virtual void GenerateFiles (DotNetProject project, string namspace, string referenceName)
 		{
+			//make sure we have a valid value for the namespace
+			if (string.IsNullOrEmpty (namspace)) {
+				namspace = project.GetDefaultNamespace (null);
+			}
+			
 			// Create the base directory if it does not exists
 			FilePath basePath = GetReferencePath (project, referenceName).CanonicalPath;
 			if (!Directory.Exists (basePath))

commit fc199538aaad5587dd7ffeddf17adfbedb90b891

[Core] Fix a regression introduced in 9fb07d423
We should set a default value for RootNamespace when creating a
new project. We still need to be able to handle a null/empty value
though.

Fixes bug #4144.

main/src/core/MonoDevelop.Core/MonoDevelop.Projects/DotNetProject.cs
@@ -148,6 +148,7 @@ namespace MonoDevelop.Projects
 			if (projectCreateInfo != null) {
 				Name = projectCreateInfo.ProjectName;
 				binPath = projectCreateInfo.BinPath;
+				defaultNamespace = SanitisePotentialNamespace (projectCreateInfo.ProjectName);
 			} else {
 				binPath = ".";
 			}