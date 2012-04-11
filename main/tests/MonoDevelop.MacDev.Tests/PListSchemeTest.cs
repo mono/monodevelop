86dc4f420f264b4f7dde4a22255435811c8dbdb4
Alan McGovern <alan.mcgovern@gmail.com>
2012-04-11 18:23:41 +0100

    [MacPlatform] Add tests to cover the previous fix and fix a race condition
    Populating the mime type database is racy, so fix this by using a Lazy<T>.
    This allows the unit tests to run as expected and will prevent any potential
    mime type matching failures due to the platform being queried before the
    database has parsed.

--- main/tests/MonoDevelop.MacDev.Tests/PListSchemeTest.cs
+++ main/tests/MonoDevelop.MacDev.Tests/PListSchemeTest.cs
@@ -2,9 +2,9 @@
 // PListSchemeTest.cs
 //  
 // Author:
-//       alanmcgovern <${AuthorEmail}>
+//       Alan McGovenrn <alan@xamarin.com>
 // 
-// Copyright (c) 2012 alanmcgovern
+// Copyright (c) 2012 Xamarin Inc.
 // 
 // Permission is hereby granted, free of charge, to any person obtaining a copy
 // of this software and associated documentation files (the "Software"), to deal
