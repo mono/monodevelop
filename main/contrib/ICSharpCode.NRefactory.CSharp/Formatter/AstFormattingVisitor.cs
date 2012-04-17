0db3c18e9d62e84a6d92cfc3d53f643ba39ea2d2
Mike Krüger <mkrueger@xamarin.com>
2012-04-17 22:43:23 +0200

    [TextEditor] Fixed wrapping column.

--- main/contrib/ICSharpCode.NRefactory.CSharp/Formatter/AstFormattingVisitor.cs
+++ main/contrib/ICSharpCode.NRefactory.CSharp/Formatter/AstFormattingVisitor.cs
@@ -785,7 +785,7 @@ namespace ICSharpCode.NRefactory.CSharp
 		}
 
 
-		void FormatParameters(AstNode node)
+		void FormatParameters (AstNode node)
 		{
 			Wrapping methodCallArgumentWrapping;
 			bool newLineAferMethodCallOpenParentheses;
@@ -842,27 +842,28 @@ namespace ICSharpCode.NRefactory.CSharp
 			}
 			
 
-			bool wrapMethodCall = DoWrap(methodCallArgumentWrapping, rParToken);
-			if (wrapMethodCall && parameters.Any()) {
+			bool wrapMethodCall = DoWrap (methodCallArgumentWrapping, rParToken);
+			if (wrapMethodCall && parameters.Any ()) {
 				if (newLineAferMethodCallOpenParentheses) {
-					curIndent.Push(IndentType.Continuation);
+					curIndent.Push (IndentType.Continuation);
 					foreach (var arg in parameters) {
-						FixStatementIndentation(arg.StartLocation);
+						FixStatementIndentation (arg.StartLocation);
 					}
-					curIndent.Pop();
+					curIndent.Pop ();
 				} else {
-					int extraSpaces = parameters.First().StartLocation.Column - 1 - curIndent.IndentString.Length;
+					int extraSpaces = parameters.First ().StartLocation.Column - 1 - curIndent.IndentString.Length;
 					curIndent.ExtraSpaces += extraSpaces;
 					foreach (var arg in parameters.Skip(1)) {
-						FixStatementIndentation(arg.StartLocation);
+						FixStatementIndentation (arg.StartLocation);
 					}
 					curIndent.ExtraSpaces -= extraSpaces;
 				}
 				if (methodClosingParenthesesOnNewLine)
-					FixStatementIndentation(rParToken.StartLocation);
+					FixStatementIndentation (rParToken.StartLocation);
 			} else {
 				foreach (var arg in parameters) {
-					ForceSpacesBeforeRemoveNewLines(arg, spaceAfterMethodCallParameterComma && arg.PrevSibling.Role == Roles.Comma);
+					if (methodCallArgumentWrapping == Wrapping.DoNotWrap)
+						ForceSpacesBeforeRemoveNewLines (arg, spaceAfterMethodCallParameterComma && arg.PrevSibling.Role == Roles.Comma);
 					arg.AcceptVisitor(this);
 				}
 				ForceSpacesBeforeRemoveNewLines(rParToken, spaceWithinMethodCallParentheses);
@@ -1686,13 +1687,13 @@ namespace ICSharpCode.NRefactory.CSharp
 			}
 		}
 
-		bool DoWrap(Wrapping wrapping, AstNode wrapNode)
+		bool DoWrap (Wrapping wrapping, AstNode wrapNode)
 		{
 			return wrapping == Wrapping.WrapAlways || 
 				options.WrapLineLength > 0 && wrapping == Wrapping.WrapIfTooLong && wrapNode.StartLocation.Column >= options.WrapLineLength;
 		}
 
-		void FormatArguments(AstNode node)
+		void FormatArguments (AstNode node)
 		{
 			Wrapping methodCallArgumentWrapping;
 			bool newLineAferMethodCallOpenParentheses;
@@ -1738,27 +1739,28 @@ namespace ICSharpCode.NRefactory.CSharp
 				arguments = invocationExpression.Arguments;
 			}
 
-			bool wrapMethodCall = DoWrap(methodCallArgumentWrapping, rParToken);
-			if (wrapMethodCall && arguments.Any()) {
+			bool wrapMethodCall = DoWrap (methodCallArgumentWrapping, rParToken);
+			if (wrapMethodCall && arguments.Any ()) {
 				if (newLineAferMethodCallOpenParentheses) {
-					curIndent.Push(IndentType.Continuation);
+					curIndent.Push (IndentType.Continuation);
 					foreach (var arg in arguments) {
-						FixStatementIndentation(arg.StartLocation);
+						FixStatementIndentation (arg.StartLocation);
 					}
-					curIndent.Pop();
+					curIndent.Pop ();
 				} else {
-					int extraSpaces = arguments.First().StartLocation.Column - 1 - curIndent.IndentString.Length;
+					int extraSpaces = arguments.First ().StartLocation.Column - 1 - curIndent.IndentString.Length;
 					curIndent.ExtraSpaces += extraSpaces;
 					foreach (var arg in arguments.Skip(1)) {
-						FixStatementIndentation(arg.StartLocation);
+						FixStatementIndentation (arg.StartLocation);
 					}
 					curIndent.ExtraSpaces -= extraSpaces;
 				}
 				if (methodClosingParenthesesOnNewLine)
-					FixStatementIndentation(rParToken.StartLocation);
+					FixStatementIndentation (rParToken.StartLocation);
 			} else {
 				foreach (var arg in arguments) {
-					ForceSpacesBeforeRemoveNewLines(arg, spaceAfterMethodCallParameterComma && arg.PrevSibling.Role == Roles.Comma);
+					if (methodCallArgumentWrapping == Wrapping.DoNotWrap)
+						ForceSpacesBeforeRemoveNewLines (arg, spaceAfterMethodCallParameterComma && arg.PrevSibling.Role == Roles.Comma);
 					arg.AcceptVisitor(this);
 				}
 				ForceSpacesBeforeRemoveNewLines(rParToken, spaceWithinMethodCallParentheses);
