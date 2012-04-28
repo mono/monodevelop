aa50191f9a5e406eab3788074ea28000295b3fc0
Scott Stephens <stephens.js@gmail.com>
2012-04-28 14:17:20 -0500

    Remove PopulateSupportFileList, which was removed as an extension point.

--- src/FSharpProjectServiceExtension.fs
+++ src/FSharpProjectServiceExtension.fs
@@ -14,10 +14,4 @@ type FSharpProjectServiceExtension() =
     // Extend any F# project
     (item :? DotNetProject) && (item :?> DotNetProject).LanguageName = "F#"
 
-  override x.PopulateSupportFileList(project:Project, list:FileCopySet, configuration:ConfigurationSelector) =
-    base.PopulateSupportFileList (project, list, configuration)
-    let outDir = (project.GetConfiguration (configuration) :?> ProjectConfiguration).OutputDirectory
-    let added = list.Add(new FilePath (AddinManager.CurrentAddin.GetFilePath("FSharp.Core.dll")), true)
-    ()
-    
     
