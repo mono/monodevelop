1f72727b1688f30b0fff3d47039dac6a079c3988
Mike Krüger <mkrueger@xamarin.com>
2012-05-08 23:06:09 +0200

    [NRefactory] Fixed check if parameter is null code action bug.

--- main/contrib/ICSharpCode.NRefactory.CSharp/Refactoring/CodeActions/CheckIfParameterIsNullAction.cs
+++ main/contrib/ICSharpCode.NRefactory.CSharp/Refactoring/CodeActions/CheckIfParameterIsNullAction.cs
@@ -35,15 +35,19 @@ namespace ICSharpCode.NRefactory.CSharp.Refactoring
 	[ContextAction("Check if parameter is null", Description = "Checks function parameter is not null.")]
 	public class CheckIfParameterIsNullAction : SpecializedCodeAction<ParameterDeclaration>
 	{
-		protected override CodeAction GetAction(RefactoringContext context, ParameterDeclaration parameter)
+		protected override CodeAction GetAction (RefactoringContext context, ParameterDeclaration parameter)
 		{
-			var bodyStatement = parameter.Parent.GetChildByRole(Roles.Body);
-			if (bodyStatement == null)
+			BlockStatement bodyStatement;
+			if (parameter.Parent is LambdaExpression) {
+				bodyStatement = parameter.Parent.GetChildByRole (LambdaExpression.BodyRole) as BlockStatement;
+			} else {
+				bodyStatement = parameter.Parent.GetChildByRole (Roles.Body);
+			}
+			if (bodyStatement == null || bodyStatement.IsNull)
 				return null;
-			var type = context.ResolveType(parameter.Type);
-			if (type.IsReferenceType == false || HasNullCheck(parameter)) 
+			var type = context.ResolveType (parameter.Type);
+			if (type.IsReferenceType == false || HasNullCheck (parameter)) 
 				return null;
-			
 			return new CodeAction (context.TranslateString("Add null check for parameter"), script => {
 				var statement = new IfElseStatement () {
 					Condition = new BinaryOperatorExpression (new IdentifierExpression (parameter.Name), BinaryOperatorType.Equality, new NullReferenceExpression ()),
