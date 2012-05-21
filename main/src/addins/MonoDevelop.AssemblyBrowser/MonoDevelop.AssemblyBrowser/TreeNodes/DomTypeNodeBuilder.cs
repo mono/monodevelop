415ebc2d14cbdcd58b3058667483884555c62994
Mike Krüger <mkrueger@xamarin.com>
2012-05-21 13:52:41 +0200

    Fixed 'Bug 5023 - Assembly Browser fails to show nested classes.'.

--- main/src/addins/MonoDevelop.AssemblyBrowser/MonoDevelop.AssemblyBrowser/TreeNodes/DomTypeNodeBuilder.cs
+++ main/src/addins/MonoDevelop.AssemblyBrowser/MonoDevelop.AssemblyBrowser/TreeNodes/DomTypeNodeBuilder.cs
@@ -125,9 +125,10 @@ namespace MonoDevelop.AssemblyBrowser
 			var list = new System.Collections.ArrayList ();
 			list.Add (new BaseTypeFolder (type));
 			bool publicOnly = builder.Options ["PublicApiOnly"];
-			foreach (var t in type.NestedTypes.Where (m => !m.IsSynthetic && !(publicOnly && (m.IsPublic || m.IsProtected) )))
+			foreach (var t in type.NestedTypes.Where (m => !m.IsSynthetic && (m.IsPublic || m.IsProtected || !publicOnly))) {
 				list.Add (t);
-			foreach (var m in type.Members.Where (m => !m.IsSynthetic && !(publicOnly && !(m.IsProtected || m.IsPublic || m.IsInternal))))
+			}
+			foreach (var m in type.Members.Where (m => !m.IsSynthetic && (m.IsPublic || m.IsProtected || !publicOnly)))
 				list.Add (m);
 			builder.AddChildren (list);
 		}
