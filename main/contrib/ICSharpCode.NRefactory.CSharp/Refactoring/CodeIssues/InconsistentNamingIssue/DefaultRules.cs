c17ef352c6df03351e4f691c869710952ad8e8d6
Mike Krüger <mkrueger@xamarin.com>
2012-06-07 08:57:09 +0200

    Fixed 'Bug 5554 - Default naming policy should only affect non-private symbols '.

--- main/contrib/ICSharpCode.NRefactory.CSharp/Refactoring/CodeIssues/InconsistentNamingIssue/DefaultRules.cs
+++ main/contrib/ICSharpCode.NRefactory.CSharp/Refactoring/CodeIssues/InconsistentNamingIssue/DefaultRules.cs
@@ -39,24 +39,28 @@ namespace ICSharpCode.NRefactory.CSharp.Refactoring
 			
 			yield return new NamingRule(AffectedEntity.Class | AffectedEntity.Struct | AffectedEntity.Enum | AffectedEntity.Delegate) {
 				Name = "Types",
+				VisibilityMask = Modifiers.Public,
 				NamingStyle = NamingStyle.PascalCase
 			};
 
 			yield return new NamingRule(AffectedEntity.Interface) {
 				Name = "Interfaces",
 				NamingStyle = NamingStyle.PascalCase,
+				VisibilityMask = Modifiers.Public,
 				RequiredPrefixes = new [] { "I" }
 			};
 
 			yield return new NamingRule(AffectedEntity.CustomAttributes) {
 				Name = "Attributes",
 				NamingStyle = NamingStyle.PascalCase,
+				VisibilityMask = Modifiers.Public,
 				RequiredSuffixes = new [] { "Attribute" }
 			};
 			
 			yield return new NamingRule(AffectedEntity.CustomEventArgs) {
 				Name = "Event Arguments",
 				NamingStyle = NamingStyle.PascalCase,
+				VisibilityMask = Modifiers.Public,
 				RequiredSuffixes = new [] { "EventArgs" }
 			};
 			
@@ -68,50 +72,45 @@ namespace ICSharpCode.NRefactory.CSharp.Refactoring
 
 			yield return new NamingRule(AffectedEntity.Methods) {
 				Name = "Methods",
+				VisibilityMask = Modifiers.Public | Modifiers.Protected,
 				NamingStyle = NamingStyle.PascalCase
 			};
 
 			yield return new NamingRule(AffectedEntity.ReadonlyField) {
 				Name = "Static Readonly Fields",
-				VisibilityMask = Modifiers.Public | Modifiers.Protected | Modifiers.Internal,
+				VisibilityMask = Modifiers.Public | Modifiers.Protected,
 				NamingStyle = NamingStyle.PascalCase,
 				IncludeInstanceMembers = false
 			};
 
 			yield return new NamingRule(AffectedEntity.Field) {
-				Name = "Fields (Public & Protected)",
+				Name = "Fields",
 				NamingStyle = NamingStyle.PascalCase,
 				VisibilityMask = Modifiers.Public | Modifiers.Protected
 			};
 			
 			yield return new NamingRule(AffectedEntity.ReadonlyField) {
-				Name = "ReadOnly Fields (Public & Protected)",
+				Name = "ReadOnly Fields",
 				NamingStyle = NamingStyle.PascalCase,
 				VisibilityMask = Modifiers.Public | Modifiers.Protected,
 				IncludeStaticEntities = false
 			};
 
-			yield return new NamingRule(AffectedEntity.Field) {
-				Name = "Static Fields (Private)",
-				NamingStyle = NamingStyle.CamelCase,
-				VisibilityMask = Modifiers.Private,
-				IncludeStaticEntities = true,
-				IncludeInstanceMembers = false
-			};
-			
 			yield return new NamingRule(AffectedEntity.ConstantField) {
 				Name = "Constant Fields",
 				NamingStyle = NamingStyle.PascalCase,
-				VisibilityMask = Modifiers.Public | Modifiers.Protected | Modifiers.Internal | Modifiers.Private
+				VisibilityMask = Modifiers.Public | Modifiers.Protected
 			};
 
 			yield return new NamingRule(AffectedEntity.Property) {
 				Name = "Properties",
+				VisibilityMask = Modifiers.Public | Modifiers.Protected,
 				NamingStyle = NamingStyle.PascalCase
 			};
 
 			yield return new NamingRule(AffectedEntity.Event) {
 				Name = "Events",
+				VisibilityMask = Modifiers.Public | Modifiers.Protected,
 				NamingStyle = NamingStyle.PascalCase
 			};
 
