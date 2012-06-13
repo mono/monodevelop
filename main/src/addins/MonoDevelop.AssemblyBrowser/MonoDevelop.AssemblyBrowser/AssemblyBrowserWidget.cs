1560ca66d0b4837d6583a5ac99463af6bf17f95a
Mike Krüger <mkrueger@xamarin.com>
2012-06-13 14:25:43 +0200

    Fixed 'Bug 5623 - Navigating to and within the assembly browser often fails '.

--- main/src/addins/MonoDevelop.AssemblyBrowser/MonoDevelop.AssemblyBrowser/AssemblyBrowserWidget.cs
+++ main/src/addins/MonoDevelop.AssemblyBrowser/MonoDevelop.AssemblyBrowser/AssemblyBrowserWidget.cs
@@ -355,9 +355,9 @@ namespace MonoDevelop.AssemblyBrowser
 		{
 			if (type is ArrayTypeReference) {
 				var array = (ArrayTypeReference)type;
-				AppendTypeReference (result, array.ElementType);
+  				AppendTypeReference (result, array.ElementType);
 				result.Append ("[");
-				result.Append (new string (',', array.Dimensions));
+				result.Append (new string (',', array.Dimensions  - 1));
 				result.Append ("]");
 				return;
 			}
@@ -368,7 +368,13 @@ namespace MonoDevelop.AssemblyBrowser
 				result.Append ("*");
 				return;
 			}
-			
+
+			if (type is GetClassTypeReference){
+				var r = (GetClassTypeReference)type;
+				result.Append (r.Namespace + "." + r.Name);
+				return;
+			}
+
 			if (type is IUnresolvedTypeDefinition)
 				result.Append (((IUnresolvedTypeDefinition)type).FullName);
 		}
@@ -474,8 +480,9 @@ namespace MonoDevelop.AssemblyBrowser
 				if (member is IUnresolvedTypeDefinition)
 					return GetIdString (member) == helpUrl;
 			} else {
-				if (member is IUnresolvedMember)
+				if (member is IUnresolvedMember) {
 					return GetIdString (member) == helpUrl;
+				}
 			}
 			return false;
 		}
