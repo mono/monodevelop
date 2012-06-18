bf0e82d61e984665e1798e883ec239edd576552b
Ihor Sihuta <segamanable@gmail.com>
2012-06-18 18:22:30 +0300

    renamed source directory

---
+++ FSharp.MonoDevelop/Gui/FSharpSettingsWidget.cs
@@ -0,0 +1,23 @@
+using System;
+namespace FSharp.MonoDevelop.Gui
+{
+	[System.ComponentModel.ToolboxItem(true)]
+	public partial class FSharpSettingsWidget : Gtk.Bin
+	{
+		public FSharpSettingsWidget ()
+		{
+			this.Build ();
+		}
+		
+		public Gtk.CheckButton CheckInteractiveUseDefault { get { return checkInteractiveUseDefault; } }
+    	public Gtk.Button ButtonBrowse { get { return buttonBrowse; } }
+    	public Gtk.Entry EntryArguments { get { return entryArguments; } }
+	    public Gtk.Entry EntryPath { get { return entryPath; } }
+	    public Gtk.FontButton FontInteractive { get { return fontbutton1; } }
+		public Gtk.CheckButton CheckCompilerUseDefault { get { return checkCompilerUseDefault; } }
+		public Gtk.Button ButtonCompilerBrowse { get { return buttonCompilerBrowse; } }
+		public Gtk.Entry EntryCompilerPath { get { return entryCompilerPath; } }
+		
+	}
+}
+
