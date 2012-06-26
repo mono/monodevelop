da33277e1ce636e454f4c057cd6343e53b93736c
Ihor Sihuta <segamanable@gmail.com>
2012-06-26 13:34:29 +0300

    Implemented GetLanguageItem method, some code fixes

--- MonoDevelop.FSharpBinding/FSharpResolverProvider.fs
+++ MonoDevelop.FSharpBinding/FSharpResolverProvider.fs
@@ -5,33 +5,36 @@
 namespace MonoDevelop.FSharp
 
 open System;
-open MonoDevelop.Ide.Gui.Content;
-open MonoDevelop.Core;
-open Mono.TextEditor;
 open System.Text;
-open MonoDevelop.Ide.Gui;
-open MonoDevelop.Ide;
-open ICSharpCode.NRefactory.CSharp.Resolver;
-open ICSharpCode.NRefactory.TypeSystem;
-open MonoDevelop.Ide.TypeSystem;
-open ICSharpCode.NRefactory.CSharp;
 open System.Linq;
 open System.Collections.Generic;
-open ICSharpCode.NRefactory.Semantics;
-open ICSharpCode.NRefactory;
-open ICSharpCode.NRefactory.CSharp.TypeSystem;
 open System.Threading;
+
+open Mono.TextEditor;
+
+open MonoDevelop.Core;
+open MonoDevelop.Ide;
+open MonoDevelop.Ide.Gui;
+open MonoDevelop.Ide.Gui.Content
+open MonoDevelop.Ide.TypeSystem;
 open MonoDevelop.Refactoring;
+
+open ICSharpCode.NRefactory;
+open ICSharpCode.NRefactory.Semantics;
+open ICSharpCode.NRefactory.TypeSystem;
+open ICSharpCode.NRefactory.CSharp;
+open ICSharpCode.NRefactory.CSharp.Resolver;
+open ICSharpCode.NRefactory.CSharp.TypeSystem;
+
+open Microsoft.FSharp.Compiler.SourceCodeServices
 
 /// Result of F# identifier resolution at the specified location
 /// Stores the data tip that we get from the language service
 /// (this is passed to MonoDevelop, which asks as about tooltip later)
-//type internal FSharpResolveResult(tip:DataTipText) = 
-//  inherit ResolveResult(new ResolveAtLocation().ResolveAtLocation()  
-//
-//  override x.GetDefinitionRegion(dom:DomRegion, memb:IMember) =
-//    seq { do () }
-//  member x.DataTip = tip
+type internal FSharpResolveResult(tip:DataTipText) = 
+  inherit ResolveResult(SpecialType.UnknownType)
+
+  member x.DataTip = tip
   
 
 /// Implements "resolution" - looks for tool-tips at current locations
@@ -43,17 +46,18 @@ type FSharpResolverProvider() =
       let mutable dt = new DomRegion()
       if offset < 0 then dt <- DomRegion.Empty
       let loc = RefactoringService.GetCorrectResolveLocation(doc, doc.Editor.OffsetToLocation(offset))
-      let (success, result, node) = doc.TryResolveAt(loc)
-      if not success then region <- DomRegion.Empty
+      let (success, result:ResolveResult, node:AstNode) = doc.TryResolveAt(loc)
+      if not success then dt <- DomRegion.Empty
       null
-      
+      dt <- new DomRegion(node.StartLocation, node.EndLocation)
+      result
     /// Whatever this is, we don't support it!  
-    member x.GetLanguageItem(dom:Document, data, offset, expression) : ResolveResult =
-      null
+    //member x.GetLanguageItem(dom:Document, data, offset, expression) : ResolveResult =
+      //null
 
     /// Returns string with tool-tip from 'FSharpResolveResult'
     /// (which we generated in the previous method - so we simply run formatter)
-    member x.CreateTooltip(dom:Document, unit, result, errorInformation, ambience, modifierState) : string = 
+    member x.CreateTooltip(unit, result, errorInformation, ambience, modifierState) : string = 
       match result with
       | :? FSharpResolveResult as res -> TipFormatter.formatTipWithHeader(res.DataTip)
       | _ -> null
