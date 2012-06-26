mono/monodevelop

commit 0e97e0e4e5702bf12ef782be545e56a84981403c

[Core] Fix a bug and a speed issue in DotNetProject.PopulateSupportFileList
Firstly we were using an initializer on a ThreadStatic variable. This is
incorrect (as per documentation) as the initializer is only run *once*,
not once per thread. Therefore the 'main' thread would view the variable
as having a default value of -1 and everything else would view it as
having a value of 0. Removing the initializer fixes the LocalCopy unit
test.

We now also use a hashset to track which projects have been walked when
calculating the list of support files. This greatly speeds up the case
where there are dozens of projects in a solution and they all reference
each other.

main/src/core/MonoDevelop.Core/MonoDevelop.Projects/DotNetProject.cs
@@ -487,15 +487,22 @@ namespace MonoDevelop.Projects
 		}
 		
 		[ThreadStatic]
-		static int supportReferDistance = -1;
+		static int supportReferDistance;
+		[ThreadStatic]
+		static HashSet<DotNetProject> processedProjects;
 
 		internal protected override void PopulateSupportFileList (FileCopySet list, ConfigurationSelector configuration)
 		{
 			try {
+				if (supportReferDistance == 0)
+					processedProjects = new HashSet<DotNetProject> ();
 				supportReferDistance++;
+
 				PopulateSupportFileListInternal (list, configuration);
 			} finally {
 				supportReferDistance--;
+				if (supportReferDistance == 0)
+					processedProjects = null;
 			}
 		}
 
@@ -537,8 +544,9 @@ namespace MonoDevelop.Projects
 
 					//VS COMPAT: recursively copy references's "local copy" files
 					//but only copy the "copy to output" files from the immediate references
-					foreach (var f in p.GetSupportFileList (configuration))
-						list.Add (f.Src, f.CopyOnlyIfNewer, f.Target);
+					if (processedProjects.Add (p))
+						foreach (var f in p.GetSupportFileList (configuration))
+							list.Add (f.Src, f.CopyOnlyIfNewer, f.Target);
 
 					DotNetProjectConfiguration refConfig = p.GetConfiguration (configuration) as DotNetProjectConfiguration;
 

commit 1d0fc6ac835ecd0baaea17ecb5194efa49eaf239

Fix a bug introduced in 0e97e0e4e5.
If a project was previously referenced by an immediate reference of the current project, it was added to processedProjects. When the immediate reference of the current project was processed after, it would be skipped and "copy to output" files would not be copied.

Fixes bug #5812

main/src/core/MonoDevelop.Core/MonoDevelop.Projects/DotNetProject.cs
@@ -544,7 +544,7 @@ namespace MonoDevelop.Projects
 
 					//VS COMPAT: recursively copy references's "local copy" files
 					//but only copy the "copy to output" files from the immediate references
-					if (processedProjects.Add (p))
+					if (processedProjects.Add (p) || supportReferDistance == 1)
 						foreach (var f in p.GetSupportFileList (configuration))
 							list.Add (f.Src, f.CopyOnlyIfNewer, f.Target);
 