3d53e1366933298a3fe0cdc6d0f7d00c9916564c
Xamarin Release Manager <release-manager@xamarin.com>
2012-07-27 18:17:29 +0000

    Updated package version to 3.0.3.5

--- main/src/core/MonoDevelop.Ide/BuildVariables.cs
+++ main/src/core/MonoDevelop.Ide/BuildVariables.cs
@@ -2,8 +2,8 @@ namespace MonoDevelop.Ide
 {
 	internal class BuildVariables
 	{
-		public static string PackageVersion = "3.0.3.4";
-		public static string PackageVersionLabel = "3.0.3.4";
+		public static string PackageVersion = "3.0.3.5";
+		public static string PackageVersionLabel = "3.0.3.5";
 	}
 }
 
