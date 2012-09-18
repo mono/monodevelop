413379e448de87245f05b978a21532857fbbb98d
Jeffrey Stedfast <jeff@xamarin.com>
2012-09-18 13:55:36 -0400

    [Debugger] Don't default to the Field icon for unknown identifiers
    Fixes bug #7100.

--- main/src/addins/MonoDevelop.Debugger/MonoDevelop.Debugger/ObjectValueTreeView.cs
+++ main/src/addins/MonoDevelop.Debugger/MonoDevelop.Debugger/ObjectValueTreeView.cs
@@ -758,20 +758,22 @@ namespace MonoDevelop.Debugger
 			string stic = (flags & ObjectValueFlags.Global) != 0 ? "static-" : string.Empty;
 			
 			switch (flags & ObjectValueFlags.OriginMask) {
-				case ObjectValueFlags.Property: source = "property"; break;
-				case ObjectValueFlags.Type: source = "class"; stic = string.Empty; break;
-				case ObjectValueFlags.Literal: return "md-literal";
-				case ObjectValueFlags.Namespace: return "md-name-space";
-				case ObjectValueFlags.Group: return "md-open-resource-folder";
-				default: source = "field"; break;
+			case ObjectValueFlags.Property: source = "property"; break;
+			case ObjectValueFlags.Type: source = "class"; stic = string.Empty; break;
+			case ObjectValueFlags.Literal: return "md-literal";
+			case ObjectValueFlags.Namespace: return "md-name-space";
+			case ObjectValueFlags.Group: return "md-open-resource-folder";
+			case ObjectValueFlags.Field: source = "field"; break;
+			default: return "md-empty";
 			}
+
 			string access;
 			switch (flags & ObjectValueFlags.AccessMask) {
-				case ObjectValueFlags.Private: access = "private-"; break;
-				case ObjectValueFlags.Internal: access = "internal-"; break;
-				case ObjectValueFlags.InternalProtected:
-				case ObjectValueFlags.Protected: access = "protected-"; break;
-				default: access = string.Empty; break;
+			case ObjectValueFlags.Private: access = "private-"; break;
+			case ObjectValueFlags.Internal: access = "internal-"; break;
+			case ObjectValueFlags.InternalProtected:
+			case ObjectValueFlags.Protected: access = "protected-"; break;
+			default: access = string.Empty; break;
 			}
 			
 			return "md-" + access + stic + source;
