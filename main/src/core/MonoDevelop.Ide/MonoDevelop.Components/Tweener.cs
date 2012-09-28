06343fa44cc8c7d62a3fb8551909dd27b612d20b
Jason Smith <jassmith@gmail.com>
2012-09-28 01:20:03 -0700

    [UIRefresh] Implement most common easing operations

--- main/src/core/MonoDevelop.Ide/MonoDevelop.Components/Tweener.cs
+++ main/src/core/MonoDevelop.Ide/MonoDevelop.Components/Tweener.cs
@@ -34,7 +34,15 @@ namespace MonoDevelop.Components
 	static class Easing
 	{
 		public static readonly Func<float, float> Linear = x => x;
-		public static readonly Func<float, float> SinInOut = x => (float)Math.Sin (x * Math.PI * 0.5f);
+
+		public static readonly Func<float, float> SinOut = x => (float)Math.Sin (x * Math.PI * 0.5f);
+		public static readonly Func<float, float> SinIn = x => 1.0f - (float)Math.Cos (x * Math.PI * 0.5f);
+		public static readonly Func<float, float> SinInOut = x => -(float)Math.Cos (Math.PI * x) / 2.0f + 0.5f;
+
+		public static readonly Func<float, float> CubicIn = x => x * x * x;
+		public static readonly Func<float, float> CubicOut = x => (float)Math.Pow (x - 1.0f, 3.0f) + 1.0f;
+		public static readonly Func<float, float> CubicInOut = x => x < 0.5f ? (float)Math.Pow (x * 2.0f, 3.0f) / 2.0f :
+																			   (float)(Math.Pow ((x-1)*2.0f, 3.0f) + 2.0f) / 2.0f;
 	}
 
 	static class Animation
