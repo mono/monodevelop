fc26637c3dd21e231f2316cde3c1a1b5c402f825
Jason Smith <jassmith@gmail.com>
2012-09-28 00:19:58 -0700

    Fix header

--- main/src/core/MonoDevelop.Ide/MonoDevelop.Components/Tweener.cs
+++ main/src/core/MonoDevelop.Ide/MonoDevelop.Components/Tweener.cs
@@ -4,7 +4,7 @@
 // Author:
 //       Jason Smith <jason.smith@xamarin.com>
 //
-// Copyright (c) 2012 Jason Smith
+// Copyright (c) 2012 Xamarin Inc.
 //
 // Permission is hereby granted, free of charge, to any person obtaining a copy
 // of this software and associated documentation files (the "Software"), to deal
