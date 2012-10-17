7d82bfd32e4de87955fcdc047edb06d2bc0e9e70
Mike Krüger <mkrueger@xamarin.com>
2012-10-17 13:29:30 +0200

    [TextEditor] TextEditor now supports Html clipboard targets beside RTF. Now colored cut & paste works in more applications. (I had problems with my e-mail client thunderbird that didn't acceppt RTF - but Html).

---
+++ main/src/core/Mono.Texteditor/Mono.TextEditor.Utils/HtmlWriter.cs
@@ -0,0 +1,113 @@
+//
+// HtmlWriter.cs
+//
+// Author:
+//       Mike Krüger <mkrueger@xamarin.com>
+//
+// Copyright (c) 2012 Xamarin Inc. (http://xamarin.com)
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in
+// all copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+// THE SOFTWARE.
+using System;
+using System.Text;
+
+namespace Mono.TextEditor.Utils
+{
+	/// <summary>
+	/// This class is used for converting a highlighted document to html.
+	/// </summary>
+	public static class HtmlWriter
+	{
+		public static string GenerateHtml (TextDocument doc, Mono.TextEditor.Highlighting.ISyntaxMode mode, Mono.TextEditor.Highlighting.ColorScheme style, ITextEditorOptions options)
+		{
+			var htmlText = new StringBuilder ();
+
+			htmlText.Append (@"<!DOCTYPE HTML PUBLIC ""-//W3C//DTD HTML 4.0 Transitional//EN""><HTML><BODY>");
+
+			var selection = new TextSegment (0, doc.TextLength);
+			int startLineNumber = doc.OffsetToLineNumber (selection.Offset);
+			int endLineNumber = doc.OffsetToLineNumber (selection.EndOffset);
+			htmlText.Append ("<FONT face = '" + options.Font.Family + "'>");
+			bool first = true;
+			foreach (var line in doc.GetLinesBetween (startLineNumber, endLineNumber)) {
+				if (!first) {
+					htmlText.Append ("<BR/>");
+				} else {
+					first = false;
+				}
+
+				if (mode == null) {
+					AppendHtmlText (htmlText, doc, options, System.Math.Max (selection.Offset, line.Offset), System.Math.Min (line.EndOffset, selection.EndOffset));
+					continue;
+				}
+
+				foreach (var chunk in mode.GetChunks (style, line, line.Offset, line.Length)) {
+					int start = System.Math.Max (selection.Offset, chunk.Offset);
+					int end = System.Math.Min (chunk.EndOffset, selection.EndOffset);
+					var chunkStyle = style.GetChunkStyle (chunk);
+					if (start < end) {
+						htmlText.Append ("<SPAN style = '");
+						if (chunkStyle.Bold)
+							htmlText.Append ("font-weight:bold;");
+						if (chunkStyle.Italic)
+							htmlText.Append ("font-style:italic;");
+						htmlText.Append ("color:" + ((HslColor)chunkStyle.Color).ToPangoString () + ";");
+						htmlText.Append ("' >");
+						AppendHtmlText (htmlText, doc, options, start, end);
+						htmlText.Append ("</SPAN>");
+					}
+				}
+			}
+			htmlText.Append ("</FONT>");
+			htmlText.Append ("</BODY></HTML>");
+			return htmlText.ToString ();
+		}
+
+		static void AppendHtmlText (StringBuilder htmlText, TextDocument doc, ITextEditorOptions options, int start, int end)
+		{
+			for (int i = start; i < end; i++) {
+				char ch = doc.GetCharAt (i);
+				switch (ch) {
+				case ' ':
+					htmlText.Append ("&nbsp;");
+					break;
+				case '\t':
+					for (int i2 = 0; i2 < options.TabSize; i2++)
+						htmlText.Append ("&nbsp;");
+					break;
+				case '<':
+					htmlText.Append ("&lt;");
+					break;
+				case '>':
+					htmlText.Append ("&gt;");
+					break;
+				case '&':
+					htmlText.Append ("&amp;");
+					break;
+				case '"':
+					htmlText.Append ("&quot;");
+					break;
+				default:
+					htmlText.Append (ch);
+					break;
+				}
+			}
+		}
+	}
+}
+
--- main/src/core/Mono.Texteditor/Mono.TextEditor/Actions/ClipboardActions.cs
+++ main/src/core/Mono.Texteditor/Mono.TextEditor/Actions/ClipboardActions.cs
@@ -65,16 +65,19 @@ namespace Mono.TextEditor
 		public class CopyOperation
 		{
 			public const int TextType     = 1;
-			public const int RichTextType = 2;
-			public const int MonoTextType = 3;
-			
+			public const int HTMLTextType = 2;
+			public const int RichTextType = 3;
+
+			public const int MonoTextType = 99;
+
 			const int UTF8_FORMAT = 8;
 			
 			public static readonly Gdk.Atom CLIPBOARD_ATOM        = Gdk.Atom.Intern ("CLIPBOARD", false);
 			public static readonly Gdk.Atom PRIMARYCLIPBOARD_ATOM = Gdk.Atom.Intern ("PRIMARY", false);
 			public static readonly Gdk.Atom RTF_ATOM;
 			public static readonly Gdk.Atom MD_ATOM  = Gdk.Atom.Intern ("text/monotext", false);
-			
+			public static readonly Gdk.Atom HTML_ATOM;
+
 			public CopyOperation ()	
 			{
 			}
@@ -95,6 +98,9 @@ namespace Mono.TextEditor
 					break;
 				case RichTextType:
 					selection_data.Set (RTF_ATOM, UTF8_FORMAT, System.Text.Encoding.UTF8.GetBytes (RtfWriter.GenerateRtf (copiedDocument, mode, docStyle, options)));
+					break;
+				case HTMLTextType:
+					selection_data.Set (HTML_ATOM, UTF8_FORMAT, System.Text.Encoding.UTF8.GetBytes (HtmlWriter.GenerateHtml (copiedDocument, mode, docStyle, options)));
 					break;
 				case MonoTextType:
 					byte[] rawText = System.Text.Encoding.UTF8.GetBytes (monoDocument.Text);
@@ -129,20 +135,24 @@ namespace Mono.TextEditor
 			Mono.TextEditor.Highlighting.ISyntaxMode mode;
 
 			public static Gtk.TargetList targetList;
-			
+
 			static CopyOperation ()
 			{
 				if (Platform.IsMac) {
 					RTF_ATOM = Gdk.Atom.Intern ("NSRTFPboardType", false); //TODO: use public.rtf when dep on MacOS 10.6
+					const string NSHTMLPboardType = "Apple HTML pasteboard type";
+					HTML_ATOM = Gdk.Atom.Intern (NSHTMLPboardType, false);
 				} else {
 					RTF_ATOM = Gdk.Atom.Intern ("text/rtf", false);
+					HTML_ATOM = Gdk.Atom.Intern ("text/html", false);
 				}
-				
+
 				targetList = new Gtk.TargetList ();
+				targetList.Add (HTML_ATOM, /* FLAGS */0, HTMLTextType);
 				targetList.Add (RTF_ATOM, /* FLAGS */0, RichTextType);
 				targetList.Add (MD_ATOM, /* FLAGS */0, MonoTextType);
 				targetList.AddTextTargets (TextType);
-				
+
 				//HACK: work around gtk_selection_data_set_text causing crashes on Mac w/ QuickSilver, Clipbard History etc.
 				if (Platform.IsMac) {
 					targetList.Remove ("COMPOUND_TEXT");
