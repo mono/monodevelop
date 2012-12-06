3d24f7bb0cd9a5d2657e266603c2392a206fa837
Mike Krüger <mkrueger@xamarin.com>
2012-12-06 13:13:31 +0100

    [Ide] Fixed list scrolling.

--- main/src/core/MonoDevelop.Ide/MonoDevelop.Ide.CodeCompletion/ListWidget.cs
+++ main/src/core/MonoDevelop.Ide/MonoDevelop.Ide.CodeCompletion/ListWidget.cs
@@ -335,8 +335,12 @@ namespace MonoDevelop.Ide.CodeCompletion
 		{
 			var area = GetRowArea (SelectedItem);
 			double newValue;
-			if (area.Y < vadj.Value || vadj.Value + vadj.PageSize < area.Bottom) {
+			if (vadj.PageSize == 1.0) {
 				newValue = Math.Min (vadj.Upper - vadj.PageSize, area.Y);
+			} else if (area.Y < vadj.Value) {
+				newValue = Math.Min (vadj.Upper - vadj.PageSize, area.Y);
+			} else if (vadj.Value + vadj.PageSize < area.Bottom) {
+				newValue = Math.Min (vadj.Upper - vadj.PageSize, area.Bottom - vadj.PageSize + 1);
 			} else {
 				return;
 			}
