5164571a6f371a61e6ca78328a4875b139a6ade1
Robin Neatherway <robin.neatherway@gmail.com>
2013-01-07 00:23:34 +0000

    Minor alterations

--- FSharp.AutoComplete/ProjectParser.fs
+++ FSharp.AutoComplete/ProjectParser.fs
@@ -50,9 +50,13 @@ module ProjectParser =
 
   let getFileName (p: ProjectResolver) : string = p.project.FullFileName
 
+  let getDirectory (p: ProjectResolver) : string =
+    IO.Path.GetDirectoryName p.project.FullFileName
+
   let getFiles (p: ProjectResolver) : string array =
-    let fs = p.project.GetEvaluatedItemsByName("Compile")
-    [| for f in fs do yield f.FinalItemSpec |]
+    let fs  = p.project.GetEvaluatedItemsByName("Compile")
+    let dir = getDirectory p
+    [| for f in fs do yield IO.Path.Combine(dir, f.FinalItemSpec) |]
 
   let getReferences (p: ProjectResolver) : string array =
     let convert (bi: BuildItem) : ITaskItem =
@@ -62,15 +66,16 @@ module ProjectParser =
       ti :> ITaskItem
 
     let pwd = Environment.CurrentDirectory
-    do Environment.CurrentDirectory <- IO.Path.GetDirectoryName p.project.FullFileName
+    do Environment.CurrentDirectory <- getDirectory p
     let refs = p.project.GetEvaluatedItemsByName "Reference"
     p.rar.Assemblies <- [| for r in refs do yield convert r |]
     p.rar.TargetProcessorArchitecture <- p.project.GetEvaluatedProperty "PlatformTarget"
     // TODO: Execute may fail
     ignore <| p.rar.Execute ()
     do Environment.CurrentDirectory <- pwd
-    [| for f in p.rar.ResolvedFiles do yield f.ItemSpec |]
-
+    //Array.append [|"-r:/Library/Frameworks/Mono.framework/Versions/3.0.0/lib/mono/4.0/mscorlib.dll"|]
+    //             [| for f in p.rar.ResolvedFiles do yield "-r:" + f.ItemSpec |]
+    [| for f in p.rar.ResolvedFiles do yield "-r:" + f.ItemSpec |]
 
   let getOptions (p: ProjectResolver) : string array =
     let getprop s = p.project.GetEvaluatedProperty s
