15b2b1b8dd34737895cf30fb1414033045417763
Robin Neatherway <robin.neatherway@gmail.com>
2013-01-12 18:03:19 +0000

    Correct test typos

--- FSharp.AutoComplete/test/RobustCommands/CompleteBadPositionRunner.fsx
+++ FSharp.AutoComplete/test/RobustCommands/CompleteBadPositionRunner.fsx
@@ -10,8 +10,8 @@ let p = new FSharpAutoCompleteWrapper()
 
 p.project "Project/Test1.fsproj"
 p.parse "Program.fs"
-p.complete "Program.fs" 50 0
-p.complete "Program.fs" 1 100
+p.completion "Program.fs" 50 0
+p.completion "Program.fs" 1 100
 p.send "quit\n"
 let output = p.finalOutput ()
 File.WriteAllText("completebadposition.txt", output)
