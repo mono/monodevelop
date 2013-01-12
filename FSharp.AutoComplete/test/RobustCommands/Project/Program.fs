f0716eee55e6eca7e994ce22bc7fedbfc79f6fa5
Robin Neatherway <robin.neatherway@gmail.com>
2013-01-12 13:50:42 +0000

    Add a few more sanity check tests

---
+++ FSharp.AutoComplete/test/RobustCommands/Project/Program.fs
@@ -0,0 +1,13 @@
+module X =
+  let func x = x + 1
+
+let testval = FileTwo.NewObjectType()
+
+let val2 = X.func 2
+
+let val3 = testval.Terrific val2
+
+[<EntryPoint>]
+let main args =
+    printfn "Hello %d" val2
+    0
