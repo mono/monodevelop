38e7734f835d305b9dd97fb0014ef4308582f933
Robin Neatherway <robin.neatherway@gmail.com>
2013-01-17 15:57:15 +0000

    Remove absolute paths from tests for portability

--- FSharp.AutoComplete/test/runtests.sh
+++ FSharp.AutoComplete/test/runtests.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
 
+cd `dirname $0`
 find . -name "*Runner.fsx" | xargs -L1 -P8 -t fsharpi --exec
+find . -name "*.txt" | xargs perl -p -i -e "s/File '.*\/test\/(.*)'/File 'test\/\$1'/"
 git status .
