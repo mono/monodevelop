5157c4ebc622a50392498da694a9dfd5c90ea367
Robin Neatherway <robin.neatherway@gmail.com>
2013-02-05 17:22:14 +0000

    Update AutoComplete tests to remove absolute paths

--- FSharp.AutoComplete/test/runtests.sh
+++ FSharp.AutoComplete/test/runtests.sh
@@ -2,5 +2,5 @@
 
 cd `dirname $0`
 find . -name "*Runner.fsx" | xargs -L1 -P8 -t fsharpi --exec
-find . -name "*.txt" | xargs perl -p -i -e "s/File '.*\/test\/(.*)'/File 'test\/\$1'/"
+find . -name "*.txt" | xargs perl -p -i -e "s/\/.*FSharp.AutoComplete\/test\/(.*)/<absolute path removed>\/test\/\$1/"
 git status .
