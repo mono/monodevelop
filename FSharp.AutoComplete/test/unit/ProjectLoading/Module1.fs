2b72f33cc715e13700c23083f4dcfa84ba2276e7
Robin Neatherway <robin.neatherway@gmail.com>
2013-02-13 14:46:42 +0000

    Rework unit test with FsUnit and automated runner

--- FSharp.AutoComplete/test/unit/ProjectLoading/Module1.fs
+++ FSharp.AutoComplete/test/unit/ProjectLoading/Module1.fs
@@ -1,23 +1,24 @@
 module Module1
 
-open Xunit
+open NUnit.Framework
+open FsUnit
 
 open FSharp.InteractiveAutocomplete
 open System.IO
 
-[<Fact>]
+[<Test>]
 let TestProjectLibraryResolution () =
   let p  = ProjectParser.load "../../data/Test1.fsproj"
-  Assert.True(Option.isSome p)
+  Option.isSome p |> should be True
   let rs = ProjectParser.getReferences p.Value
-  Assert.True(4  = rs.Length)
+  rs |> should haveLength 4
 
-[<Fact>]
+[<Test>]
 let Test2ndLevelDepsResolution () =
   let p  = ProjectParser.load "../../data/Test2.fsproj"
-  Assert.True(Option.isSome p)
+  Option.isSome p |> should be True
   let rs = ProjectParser.getReferences p.Value
-  Assert.True(6  = rs.Length, sprintf "%A" rs)
-  Assert.True(Array.exists (fun (r: string) -> r.Contains("Test1.dll")) rs)
-  
+  rs |> should haveLength 6
+  rs |> Array.map Path.GetFileName
+     |> should contain "Test1.dll"
 
