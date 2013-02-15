9220cab6f10eec9ecfa6d605b72afa5d35524fd5
Mike Krüger <mkrueger@xamarin.com>
2013-02-15 08:20:43 +0100

    Fixed 'Bug 10310 - Indenter does not update after policy change'.

--- main/src/addins/CSharpBinding/MonoDevelop.CSharp.Formatting/CSharpTextEditorIndentation.cs
+++ main/src/addins/CSharpBinding/MonoDevelop.CSharp.Formatting/CSharpTextEditorIndentation.cs
@@ -52,8 +52,26 @@ namespace MonoDevelop.CSharp.Formatting
 				return document.Editor;
 			}
 		}
-		CSharpFormattingPolicy policy;
-		TextStylePolicy textStylePolicy;
+
+		IEnumerable<string> types = MonoDevelop.Ide.DesktopService.GetMimeTypeInheritanceChain (CSharpFormatter.MimeType);
+
+		CSharpFormattingPolicy Policy {
+			get {
+				if (Document != null && Document.Project != null && Document.Project.Policies != null) {
+					return base.Document.Project.Policies.Get<CSharpFormattingPolicy> (types);
+				}
+				return MonoDevelop.Projects.Policies.PolicyService.GetDefaultPolicy<CSharpFormattingPolicy> (types);
+			}
+		}
+
+		TextStylePolicy TextStylePolicy {
+			get {
+				if (Document != null && Document.Project != null && Document.Project.Policies != null) {
+					return base.Document.Project.Policies.Get<TextStylePolicy> (types);
+				}
+				return MonoDevelop.Projects.Policies.PolicyService.GetDefaultPolicy<TextStylePolicy> (types);
+			}
+		}
 
 		char lastCharInserted;
 
@@ -189,38 +207,21 @@ namespace MonoDevelop.CSharp.Formatting
 			}
 		}
 
-		public CSharpTextEditorIndentation ()
-		{
-			IEnumerable<string> types = MonoDevelop.Ide.DesktopService.GetMimeTypeInheritanceChain (CSharpFormatter.MimeType);
-			policy = MonoDevelop.Projects.Policies.PolicyService.GetDefaultPolicy<CSharpFormattingPolicy> (types);
-			textStylePolicy = MonoDevelop.Projects.Policies.PolicyService.GetDefaultPolicy<TextStylePolicy> (types);
-		}
-
 		public override void Initialize ()
 		{
 			base.Initialize ();
 
-			IEnumerable<string> types = MonoDevelop.Ide.DesktopService.GetMimeTypeInheritanceChain (CSharpFormatter.MimeType);
-			if (base.Document.Project != null && base.Document.Project.Policies != null) {
-				policy = base.Document.Project.Policies.Get<CSharpFormattingPolicy> (types);
-				textStylePolicy = base.Document.Project.Policies.Get<TextStylePolicy> (types);
-			}
 
 			if (textEditorData != null) {
 				textEditorData.Options.Changed += delegate {
-					var project = base.Document.Project;
-					if (project != null) {
-						policy = project.Policies.Get<CSharpFormattingPolicy> (types);
-						textStylePolicy = project.Policies.Get<TextStylePolicy> (types);
-					}
 					textEditorData.IndentationTracker = new IndentVirtualSpaceManager (
 						textEditorData,
-						new DocumentStateTracker<CSharpIndentEngine> (new CSharpIndentEngine (policy, textStylePolicy), textEditorData)
+						new DocumentStateTracker<CSharpIndentEngine> (new CSharpIndentEngine (Policy, TextStylePolicy), textEditorData)
 					);
 				};
 				textEditorData.IndentationTracker = new IndentVirtualSpaceManager (
 					textEditorData,
-					new DocumentStateTracker<CSharpIndentEngine> (new CSharpIndentEngine (policy, textStylePolicy), textEditorData)
+					new DocumentStateTracker<CSharpIndentEngine> (new CSharpIndentEngine (Policy, TextStylePolicy), textEditorData)
 				);
 			}
 
@@ -240,7 +241,7 @@ namespace MonoDevelop.CSharp.Formatting
 
 		void InitTracker ()
 		{
-			stateTracker = new DocumentStateTracker<CSharpIndentEngine> (new CSharpIndentEngine (policy, textStylePolicy), textEditorData);
+			stateTracker = new DocumentStateTracker<CSharpIndentEngine> (new CSharpIndentEngine (Policy, TextStylePolicy), textEditorData);
 		}
 
 		internal DocumentStateTracker<CSharpIndentEngine> StateTracker { get { return stateTracker; } }
