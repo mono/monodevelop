9492450865ae833654de98bf0889a606f749a11a
Michael Hutchinson <m.j.hutchinson@gmail.com>
2013-02-15 13:15:23 -0500

    [Core] Finish workarounds for Mono file enumeration bug BXC2975 - System.IO.Directory.EnumerateFiles doesn't return any file

--- main/src/core/MonoDevelop.Core/MonoDevelop.Core.Assemblies/TargetFramework.cs
+++ main/src/core/MonoDevelop.Core/MonoDevelop.Core.Assemblies/TargetFramework.cs
@@ -383,6 +383,9 @@ namespace MonoDevelop.Core.Assemblies
 							ainfo.InGac = reader.ReadContentAsBoolean ();
 					} while (reader.ReadToFollowing ("File"));
 				} else {
+
+					// HACK: we were using EnumerateFiles but it's broken in some Mono releases
+					// https://bugzilla.xamarin.com/show_bug.cgi?id=2975
 					var files = System.IO.Directory.GetFiles (dir, "*.dll");
 					foreach (var f in files) {
 						try {
