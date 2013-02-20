ba45a99c3859967b2437c863dc8e53d853b43735
Chris Barrett <chris.d.barrett@me.com>
2013-02-20 21:11:05 +1300

    add tests for finding solutions

--- emacs/test/fsharp-mode-tests.el
+++ emacs/test/fsharp-mode-tests.el
@@ -1,3 +1,5 @@
+;;; Test registration of auto modes.
+
 (defmacro check-automode (extension)
   `(check ,(concat "uses fsharp-mode for " extension " files")
      (using-temp-file ,extension
@@ -7,3 +9,17 @@
 (check-automode ".fsx")
 (check-automode ".fsi")
 (check-automode ".fsl")
+
+;;; Test our ability to find SLN files and projects.
+;;; This is tricky to test comprehensively because there is a sln at the
+;;; root of this repo.
+
+(check "does not find fsharp project if none present"
+  (should-not (fsharp-mode/find-sln-or-fsproj "/bin/")))
+
+(check "should find sln at base of repo given a subdir"
+  (should-match "Binding.sln$" (fsharp-mode/find-sln-or-fsproj test-dir)))
+
+(check "should find sln at base of repo given a file in subdir"
+  (should-match "Binding.sln$" (fsharp-mode/find-sln-or-fsproj
+                                (concat test-dir "file.fs"))))
