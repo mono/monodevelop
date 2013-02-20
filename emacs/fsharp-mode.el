86a440522b0a40b6a2aecd49380a639494d23cb5
Chris Barrett <chris.d.barrett@me.com>
2013-02-20 22:00:52 +1300

    require namespaces

--- emacs/fsharp-mode.el
+++ emacs/fsharp-mode.el
@@ -25,7 +25,7 @@
 ;; the Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor,
 ;; Boston, MA 02110-1301, USA.
 
-;;user customizable variables
+(require 'namespaces)
 
 (namespace fsharp-mode
   :export
