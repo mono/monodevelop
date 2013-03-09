8d99664c4339cc5dd24f6c42147ffc7afe23eed1
Jeffrey Stedfast <fejj@gnome.org>
2013-03-09 18:17:09 -0500

    [Debugger] Attempt to limit potential race conditions in DebuggerService.Cleanup()
    Fixes bug #11016

--- main/src/addins/MonoDevelop.Debugger/MonoDevelop.Debugger/DebuggingService.cs
+++ main/src/addins/MonoDevelop.Debugger/MonoDevelop.Debugger/DebuggingService.cs
@@ -344,68 +344,72 @@ namespace MonoDevelop.Debugger
 			});
 
 		}
-		
+
+		static object cleanup_lock = new object ();
 		static void Cleanup ()
 		{
+			DebuggerSession currentSession;
+			StatusBarIcon currentIcon;
+			IConsole currentConsole;
+
+			lock (cleanup_lock) {
+				if (!IsDebugging)
+					return;
+
+				currentIcon = busyStatusIcon;
+				currentSession = session;
+				currentConsole = console;
+
+				currentBacktrace = null;
+				busyStatusIcon = null;
+				session = null;
+				console = null;
+			}
+
 			if (oldLayout != null) {
 				string layout = oldLayout;
 				oldLayout = null;
-				// Dispatch asynchronously to avoid start/stop races
+
+				// Dispatch synchronously to avoid start/stop races
 				DispatchService.GuiSyncDispatch (delegate {
 					IdeApp.Workbench.HideCommandBar ("Debug");
 					if (IdeApp.Workbench.CurrentLayout == "Debug")
 						IdeApp.Workbench.CurrentLayout = layout;
 				});
 			}
-			
-			currentBacktrace = null;
-			
-			if (!IsDebugging)
-				return;
 
-			DispatchService.GuiSyncDispatch (delegate {
-				HideExceptionCaughtDialog ();
-			});
+			currentSession.BusyStateChanged -= OnBusyStateChanged;
+			currentSession.TargetEvent -= OnTargetEvent;
+			currentSession.TargetStarted -= OnStarted;
 
-			if (busyStatusIcon != null) {
-				busyStatusIcon.Dispose ();
-				busyStatusIcon = null;
+			currentSession.BreakpointTraceHandler = null;
+			currentSession.GetExpressionEvaluator = null;
+			currentSession.TypeResolverHandler = null;
+			currentSession.OutputWriter = null;
+			currentSession.LogWriter = null;
+			
+			if (currentConsole != null) {
+				currentConsole.CancelRequested -= OnCancelRequested;
+				currentConsole.Dispose ();
 			}
 			
-			session.TargetEvent -= OnTargetEvent;
-			session.TargetStarted -= OnStarted;
-			session.OutputWriter = null;
-			session.LogWriter = null;
-			session.BusyStateChanged -= OnBusyStateChanged;
-			session.TypeResolverHandler = null;
-			session.BreakpointTraceHandler = null;
-			session.GetExpressionEvaluator = null;
-			
-			// Dispose the session at the end, since it may take a while.
-			DebuggerSession oldSession = session;
-			session = null;
-			
 			DispatchService.GuiDispatch (delegate {
+				HideExceptionCaughtDialog ();
+
+				if (currentIcon != null) {
+					currentIcon.Dispose ();
+					currentIcon = null;
+				}
+
 				if (StoppedEvent != null)
 					StoppedEvent (null, new EventArgs ());
-			});
-			
-			if (console != null) {
-				console.CancelRequested -= OnCancelRequested;
-				console.Dispose ();
-				console = null;
-			}
-			
-			DispatchService.GuiDispatch (delegate {
+
 				NotifyCallStackChanged ();
 				NotifyCurrentFrameChanged ();
 				NotifyLocationChanged ();
 			});
-			
-			if (oldSession != null) {
-				oldSession.BusyStateChanged -= OnBusyStateChanged;
-				oldSession.Dispose ();
-			}
+
+			currentSession.Dispose ();
 		}
 
 		public static bool IsDebugging {
