mono/monodevelop

commit e0cf14834e92967001a8a765cf43fcda45fc7ec0

[MSBuild] Don't remove framework attributes
[2/2] Fix BXC7914 - MonoDevelop changes csproj unnecessarily

main/src/core/MonoDevelop.Core/MonoDevelop.Projects.Formats.MSBuild/MSBuildProjectHandler.cs
@@ -1104,22 +1104,15 @@ namespace MonoDevelop.Projects.Formats.MSBuild
 				bool isDefaultVersion = isDefaultIdentifier && def.Version == moniker.Version;
 				bool isDefaultProfile = isDefaultVersion && def.Profile == moniker.Profile;
 
-				// If the format only supports one fx version, or the version is the default, there is no need to store it
-				if (!isDefaultVersion && supportsMultipleFrameworks)
-					SetGroupProperty (globalGroup, "TargetFrameworkVersion", "v" + moniker.Version);
-				else
-					globalGroup.RemoveProperty ("TargetFrameworkVersion");
+				// If the format only supports one fx version, or the version is the default, there is no need to store it.
+				// However, is there is already a value set, do not remove it.
+				if (supportsMultipleFrameworks) {
+					SetIfPresentOrNotDefaultValue (globalGroup, "TargetFrameworkVersion", "v" + moniker.Version, "v" + def.Version);
+				}
 				
 				if (TargetFormat.SupportsMonikers) {
-					if (!isDefaultIdentifier && def.Identifier != moniker.Identifier)
-						SetGroupProperty (globalGroup, "TargetFrameworkIdentifier", moniker.Identifier);
-					else
-						globalGroup.RemoveProperty ("TargetFrameworkIdentifier");
-					
-					if (!isDefaultProfile && def.Profile != moniker.Profile)
-						SetGroupProperty (globalGroup, "TargetFrameworkProfile", moniker.Profile);
-					else
-						globalGroup.RemoveProperty ("TargetFrameworkProfile");
+					SetIfPresentOrNotDefaultValue (globalGroup, "TargetFrameworkIdentifier", moniker.Identifier, def.Identifier);
+					SetIfPresentOrNotDefaultValue (globalGroup, "TargetFrameworkProfile", moniker.Profile, def.Profile);
 				}
 			}
 
@@ -1156,6 +1149,16 @@ namespace MonoDevelop.Projects.Formats.MSBuild
 			
 			if (projectBuilder != null)
 				projectBuilder.Refresh ();
+		}
+
+		void SetIfPresentOrNotDefaultValue (MSBuildPropertySet propGroup, string name, string value, string defaultValue)
+		{
+			var prop = propGroup.GetProperty (name);
+			if (prop != null) {
+				prop.Value = value;
+			} else if (value != defaultValue) {
+				propGroup.SetPropertyValue (name, value, false);
+			}
 		}
 		
 		void ForceDefaultValueSerialization (MSBuildSerializer ser, MSBuildPropertySet baseGroup, object ob)
@@ -1562,17 +1565,6 @@ namespace MonoDevelop.Projects.Formats.MSBuild
 					configData.Add (new ConfigData (conf, platform, cgrp));
 			}
 			return configData;
-		}
-		
-		MSBuildProperty SetGroupProperty (MSBuildPropertySet propGroup, string name, string value, bool preserveExistingCase = false)
-		{
-			if (preserveExistingCase) {
-				var existing = propGroup.GetProperty (name);
-				if (existing != null && string.Equals (existing.Value, value, StringComparison.OrdinalIgnoreCase)) {
-					return existing;
-				}
-			}
-			return propGroup.SetPropertyValue (name, value, preserveExistingCase);
 		}
 		
 		ConfigData FindPropertyGroup (List<ConfigData> configData, SolutionItemConfiguration config)

commit d61210fd7f3d31073b82abe3a4aaba4587a3854c

[Core] Fix a regression from e0cf1483 when saving csproj
If both the old value and new value are the same we should do nothing.
We should probably also be careful in case the new value is null. In
this scenario that probably means we should remove the xelement but
currently that might still trigger the bug in
https://bugzilla.xamarin.com/show_bug.cgi?id=11039

main/src/core/MonoDevelop.Core/MonoDevelop.Projects.Formats.MSBuild/MSBuildProjectHandler.cs
@@ -1155,7 +1155,8 @@ namespace MonoDevelop.Projects.Formats.MSBuild
 		{
 			var prop = propGroup.GetProperty (name);
 			if (prop != null) {
-				prop.Value = value;
+				if (value != defaultValue)
+					prop.Value = value;
 			} else if (value != defaultValue) {
 				propGroup.SetPropertyValue (name, value, false);
 			}