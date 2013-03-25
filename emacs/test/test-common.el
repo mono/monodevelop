36cd2e99548402dabd3aa44e7eb7190989d971a4
Chris Barrett <chris.d.barrett@me.com>
2013-03-25 22:10:16 +1300

    remove json packages

--- emacs/test/test-common.el
+++ emacs/test/test-common.el
@@ -73,7 +73,7 @@
 (defconst tests-load-path
   (mapcar 'expand-file-name `(,@load-path "." ".." "./tests")))
 
-(defconst default-dependencies '(popup s dash pos-tip auto-complete json))
+(defconst default-dependencies '(popup s dash pos-tip auto-complete))
 
 (defun load-packages ()
   "Load package dependencies for fsharp-mode."
