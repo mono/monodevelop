429104476df66ec9f3839ff096f22315b752a236
Michael Hutchinson <m.j.hutchinson@gmail.com>
2013-04-09 20:41:05 -0400

    [Ide] Make Linux process name consistent again

--- main/src/core/MonoDevelop.Ide/MonoDevelop.Ide/IdeStartup.cs
+++ main/src/core/MonoDevelop.Ide/MonoDevelop.Ide/IdeStartup.cs
@@ -527,6 +527,8 @@ namespace MonoDevelop.Ide
 			do {
 				try {
 					var exename = Path.GetFileNameWithoutExtension (Assembly.GetEntryAssembly ().Location);
+					if (!Platform.IsMac && !Platform.IsWindows)
+						exename = exename.ToLower ();
 					Runtime.SetProcessName (exename);
 					var app = new IdeStartup ();
 					ret = app.Run (options);
