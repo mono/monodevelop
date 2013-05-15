ce47555367e5e472f648b7dd065cff3b9bcbbd1f
Xamarin Release Manager <release-manager@xamarin.com>
2013-05-15 18:02:24 +0000

    Updated package version to 4.0.7

--- main/src/core/MonoDevelop.Ide/BuildVariables.cs
+++ main/src/core/MonoDevelop.Ide/BuildVariables.cs
@@ -2,8 +2,8 @@ namespace MonoDevelop.Ide
 {
 	internal class BuildVariables
 	{
-		public static string PackageVersion = "4.0.6";
-		public static string PackageVersionLabel = "4.0.6";
+		public static string PackageVersion = "4.0.7";
+		public static string PackageVersionLabel = "4.0.7";
 	}
 }
 
