baac668139462b210b24dbfafb044e9a55ff0a92
Mike Krüger <mkrueger@xamarin.com>
2013-06-24 13:50:50 +0200

    [DesignerSupport] Fixed null reference exception on IDE quit.

--- main/src/addins/MonoDevelop.DesignerSupport/MonoDevelop.DesignerSupport.Toolbox/ToolboxWidget.cs
+++ main/src/addins/MonoDevelop.DesignerSupport/MonoDevelop.DesignerSupport.Toolbox/ToolboxWidget.cs
@@ -771,13 +771,17 @@ namespace MonoDevelop.DesignerSupport.Toolbox
 		protected override void OnSetScrollAdjustments (Adjustment hAdjustement, Adjustment vAdjustement)
 		{
 			this.hAdjustement = hAdjustement;
-			this.hAdjustement.ValueChanged += delegate {
-				this.QueueDraw ();
-			};
+			if (this.hAdjustement != null) {
+				this.hAdjustement.ValueChanged += delegate {
+					this.QueueDraw ();
+				};
+			}
 			this.vAdjustement = vAdjustement;
-			this.vAdjustement.ValueChanged += delegate {
-				this.QueueDraw ();
-			};
+			if (this.vAdjustement != null) {
+				this.vAdjustement.ValueChanged += delegate {
+					this.QueueDraw ();
+				};
+			}
 		}
 		#endregion
 		
