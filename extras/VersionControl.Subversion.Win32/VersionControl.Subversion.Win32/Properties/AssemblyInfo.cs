f28289d45f28a2bce59327471ee77b058e1c6190
Marius Ungureanu <teromario@yahoo.com>
2013-06-26 10:47:32 +0300

    Readded Subversion 1.6 support through another addin.

--- extras/VersionControl.Subversion.Win32/VersionControl.Subversion.Win32/Properties/AssemblyInfo.cs
+++ extras/VersionControl.Subversion.Win32/VersionControl.Subversion.Win32/Properties/AssemblyInfo.cs
@@ -9,7 +9,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration ("")]
 [assembly: AssemblyCompany ("")]
 [assembly: AssemblyProduct ("SubversionAddinWindows")]
-[assembly: AssemblyCopyright ("Copyright ©  2009")]
+[assembly: AssemblyCopyright ("Copyright © Xamarin Inc. 2013")]
 [assembly: AssemblyTrademark ("")]
 [assembly: AssemblyCulture ("")]
 
