05b518cb93acae66d4f35e756462f569cd6b959d
Lluis Sanchez <lluis@xamarin.com>
2013-06-27 18:49:34 +0200

    Reduce check timeout for command update handlers

--- main/src/core/MonoDevelop.Ide/MonoDevelop.Components.Commands/CommandManager.cs
+++ main/src/core/MonoDevelop.Ide/MonoDevelop.Components.Commands/CommandManager.cs
@@ -49,7 +49,7 @@ namespace MonoDevelop.Components.Commands
 		DateTime lastUserInteraction;
 		KeyboardShortcut[] chords;
 		string chord;
-		internal const int SlowCommandWarningTime = 200;
+		internal const int SlowCommandWarningTime = 50;
 		
 		Dictionary<object,Command> cmds = new Dictionary<object,Command> ();
 		Hashtable handlerInfo = new Hashtable ();
