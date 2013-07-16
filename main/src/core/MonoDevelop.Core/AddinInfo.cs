cedec81618aafc5e15d524e36583f03a7765011f
Lluis Sanchez <lluis@xamarin.com>
2013-07-16 20:10:37 +0200

    Revert "Add assembly prefix to assembly attributes"
    This reverts commit 6e4fe8e5d7d9f6c9e3e652d1ab93ba1d67309c64.

--- main/src/core/MonoDevelop.Core/AddinInfo.cs
+++ main/src/core/MonoDevelop.Core/AddinInfo.cs
@@ -2,11 +2,11 @@
 using System;
 using Mono.Addins;
 
-[assembly:AddinRoot ("Core", 
+[AddinRoot ("Core", 
         Namespace = "MonoDevelop", 
         Version = MonoDevelop.BuildInfo.Version,
         CompatVersion = MonoDevelop.BuildInfo.CompatVersion,
         Category = "MonoDevelop Core")]
 
-[assembly:AddinName ("MonoDevelop Runtime")]
-[assembly:AddinDescription ("Provides the core services of the MonoDevelop platform")]
+[AddinName ("MonoDevelop Runtime")]
+[AddinDescription ("Provides the core services of the MonoDevelop platform")]
--- main/src/core/MonoDevelop.Ide/AddinInfo.cs
+++ main/src/core/MonoDevelop.Ide/AddinInfo.cs
@@ -2,13 +2,13 @@
 using System;
 using Mono.Addins;
 
-[assembly:AddinRoot ("Ide", 
+[AddinRoot ("Ide", 
             Namespace = "MonoDevelop", 
             Version = MonoDevelop.BuildInfo.Version,
             CompatVersion = MonoDevelop.BuildInfo.CompatVersion,
             Category = "MonoDevelop Core")]
 
-[assembly:AddinName ("MonoDevelop Ide")]
-[assembly:AddinDescription ("The MonoDevelop IDE application")]
+[AddinName ("MonoDevelop Ide")]
+[AddinDescription ("The MonoDevelop IDE application")]
 
-[assembly:AddinDependency ("Core", MonoDevelop.BuildInfo.Version)]
+[AddinDependency ("Core", MonoDevelop.BuildInfo.Version)]
