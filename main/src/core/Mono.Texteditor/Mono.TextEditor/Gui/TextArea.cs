ee98851e0e0f3b94837b1402cc255160c3d60586
Mike Krüger <mkrueger@xamarin.com>
2013-07-18 07:15:20 +0200

    Fixed 'Bug 13309 - Parts of the text editor blacked out when window re-focused'.

--- main/src/core/Mono.Texteditor/Mono.TextEditor/Gui/TextArea.cs
+++ main/src/core/Mono.Texteditor/Mono.TextEditor/Gui/TextArea.cs
@@ -290,7 +290,6 @@ namespace Mono.TextEditor
 		{
 			GtkWorkarounds.FixContainerLeak (this);
 			this.Events = EventMask.PointerMotionMask | EventMask.ButtonPressMask | EventMask.ButtonReleaseMask | EventMask.EnterNotifyMask | EventMask.LeaveNotifyMask | EventMask.VisibilityNotifyMask | EventMask.FocusChangeMask | EventMask.ScrollMask | EventMask.KeyPressMask | EventMask.KeyReleaseMask;
-			this.DoubleBuffered = true;
 			base.CanFocus = true;
 
 			// This is required to properly handle resizing and rendering of children
