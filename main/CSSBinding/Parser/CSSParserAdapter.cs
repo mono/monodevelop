d470fb41787b5af445b93a2fa2ca844086263b6a
diyoda_sajjana <diyoda.sajjana@gmail.com>
2013-07-22 03:14:23 +0530

    Added CSSParserAdapther

---
+++ main/CSSBinding/Parser/CSSParserAdapter.cs
@@ -0,0 +1,58 @@
+//
+// CSSParserAdapter.cs
+//
+// Author:
+//       Diyoda Sajjana <>
+//
+// Copyright (c) 2013 Diyoda Sajjana
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in
+// all copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+// THE SOFTWARE.
+using System;
+using MonoDevelop.Ide.TypeSystem;
+
+namespace Parser
+{
+	public class CSSParserAdapter : TypeSystemParser
+	{
+		public CSSParserAdapter ()
+		{
+		}
+
+		public override ParsedDocument Parse (bool storeAst, string fileName, TextReader content, Project project = null)
+		{
+			ParsedTemplate template = new ParsedTemplate (fileName);
+			try {
+				var tk = new Tokeniser (fileName, content.ReadToEnd ());
+				template.ParseWithoutIncludes (tk);
+			} catch (ParserException ex) {
+				template.LogError (ex.Message, ex.Location);
+			}
+
+			var errors = new List<Error> ();
+			foreach (System.CodeDom.Compiler.CompilerError err in template.Errors) {
+				errors.Add (new Error (err.IsWarning ? ErrorType.Warning : ErrorType.Error, err.ErrorText, err.Line, err.Column));
+			}
+			var doc = new T4ParsedDocument (fileName, template.RawSegments, errors);
+			doc.Flags |= ParsedDocumentFlags.NonSerializable;
+
+			return doc;
+		}
+	}
+}
+
