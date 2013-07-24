355c3590036e3dd69c187c0f4ab8b51ded52c67e
Alan McGovern <alan@xamarin.com>
2013-07-24 02:12:22 -0400

    WIP: Implement an ultra basic alternative to the existing debug tooltip logic
    This test demonstrates the mkrueger approved method of getting the information
    we need for the debugger tooltips.
    
    The code in 'ResolveExpression' is what the debug tooltips should use to figure
    out the expression that needs to be passed to the debugger. It seems to work
    for the few cases I've added to the testsuite but it's by no means comprehensive
    test suite yet! :p

--- main/tests/UnitTests/MonoDevelop.SourceEditor/DebugTooltipTests.cs
+++ main/tests/UnitTests/MonoDevelop.SourceEditor/DebugTooltipTests.cs
@@ -23,58 +23,107 @@
 // OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
 // THE SOFTWARE.
 
-//using System;
-//using NUnit.Framework;
-//using Mono.TextEditor;
-//using ICSharpCode.NRefactory.Semantics;
-//using ICSharpCode.NRefactory.TypeSystem;
-//
-//namespace MonoDevelop.SourceEditor
-//{
-//	[TestFixture ()]
-//	public class DebugTooltipTests
-//	{
-//		static TextEditorData CreateEditorData (string content)
-//		{
-//			return new TextEditorData (new TextDocument (content));
-//		}
-//
-//		static string ResolveExpression (TextEditorData data, int offset)
-//		{
-//			string expression = null;
-//			ResolveResult result;
-//			DomRegion region;
-//
-//			//result = data.GetLanguageItem (offset, out region);
-//			result = null;
-//			if (result != null && !result.IsError && result.GetType () != typeof (ResolveResult)) {
-////				if (!DebugValueTooltipProvider.TryResolveExpression (data, result, region, out expression))
-////					return null;
-//			}
-//
-//			return expression;
-//		}
-//
-//		[Test ()]
-//		public void TestLocalVariables ()
-//		{
-//			var content = @"// Test local variables
-//using System;
-//
-//namespace DebuggerTooltipTests
-//{
-//	class MainClass
-//	{
-//		public static void Main (string[] args)
-//		{
-//			int basicLocalVariable = 5;
-//		}
-//	}
-//}
-//";
-//			var editor = CreateEditorData (content);
-//
-//			Assert.AreEqual ("basicLocalVariable", ResolveExpression (editor, content.IndexOf ("basicLocalVariable") + 1));
-//		}
-//	}
-//}
+using System;
+using NUnit.Framework;
+using Mono.TextEditor;
+using ICSharpCode.NRefactory.Semantics;
+using ICSharpCode.NRefactory.TypeSystem;
+using MonoDevelop.CSharp.Resolver;
+using MonoDevelop.CSharpBinding;
+using MonoDevelop.CSharpBinding.Tests;
+using MonoDevelop.CSharp.Completion;
+using MonoDevelop.Ide.Gui;
+using ICSharpCode.NRefactory.CSharp;
+using ICSharpCode.NRefactory.CSharp.TypeSystem;
+using ICSharpCode.NRefactory.CSharp.Resolver;
+using MonoDevelop.CSharp;
+
+namespace MonoDevelop.SourceEditor
+{
+	[TestFixture ()]
+	public class DebugTooltipTests
+	{
+		static Document Setup (string input)
+		{
+			var tww = new TestWorkbenchWindow ();
+			var content = new TestViewContent ();
+			tww.ViewContent = content;
+			content.ContentName = "a.cs";
+			content.GetTextEditorData ().Document.MimeType = "text/x-csharp";
+			var doc = new Document (tww);
+
+			var text = input;
+			int endPos = text.IndexOf ('$');
+			if (endPos >= 0)
+				text = text.Substring (0, endPos) + text.Substring (endPos + 1);
+
+			content.Text = text;
+			content.CursorPosition = Math.Max (0, endPos);
+
+			var compExt = new CSharpCompletionTextEditorExtension ();
+			compExt.Initialize (doc);
+			content.Contents.Add (compExt);
+
+			doc.UpdateParseDocument ();
+			return doc;
+		}
+
+		static string ResolveExpression (Document doc, int offset)
+		{
+			var editor = doc.Editor;
+			var unit = doc.ParsedDocument.GetAst<SyntaxTree> ();
+			if (unit == null)
+				return null;
+
+			var file = doc.ParsedDocument.ParsedFile as CSharpUnresolvedFile;
+			if (file == null)
+				return null;
+
+			ResolveResult result;
+			AstNode node;
+			var loc = editor.OffsetToLocation (offset);
+			if (!doc.TryResolveAt (loc, out result, out node))
+				return null;
+			if (result is LocalResolveResult)
+				return ((LocalResolveResult)result).Variable.Name;
+			return editor.GetTextBetween (node.StartLocation, node.EndLocation);
+		}
+
+		[Test ()]
+		public void TestLocalVariables ()
+		{
+			var content = @"// Test local variables
+using System;
+
+namespace DebuggerTooltipTests
+{
+	class MainClass
+	{
+		public int MyField {
+			get; set;
+		}
+
+		public static void Main (string[] args)
+		{
+			int basicLocalVariable = 5;
+			int localVarFromMethod = Foo ();
+			int castingLocalVariable = ((Foo) args [0]).FooValue;
+			int localVarFromProperties = this.FirstProperty.SecondProperty;
+			int propertyFromBase = base.BaseProperty;
+
+			MyField = 7;
+		}
+	}
+}
+";
+			var document = Setup (content);
+			Assert.AreEqual ("basicLocalVariable", ResolveExpression (document, content.IndexOf ("basicLocalVariable") + 1));
+			Assert.AreEqual ("localVarFromMethod", ResolveExpression (document, content.IndexOf ("localVarFromMethod") + 1));
+			Assert.AreEqual ("((Foo) args [0]).FooValue", ResolveExpression (document, content.IndexOf ("FooValue") + 1));
+			Assert.AreEqual ("this.FirstProperty", ResolveExpression (document, content.IndexOf ("FirstProperty") + 1));
+			Assert.AreEqual ("this.FirstProperty.SecondProperty", ResolveExpression (document, content.IndexOf ("SecondProperty") + 1));
+			Assert.AreEqual ("base.BaseProperty", ResolveExpression (document, content.IndexOf ("BaseProperty") + 1));
+			Assert.AreEqual ("MyField", ResolveExpression (document, content.LastIndexOf ("MyField") + 1));
+		}
+	}
+}
