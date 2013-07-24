79d0ae2d353024902b34b3bd8fe39c92477075c2
Andrés G. Aragoneses <knocte@gmail.com>
2013-07-24 16:46:51 +0200

    [WebReferences] Nitpick in add-in description.
    Languages are better referred as "C#" and "VB.NET" instead of
    "CSharp" and "VB".

--- main/src/addins/MonoDevelop.WebReferences/AddinInfo.cs
+++ main/src/addins/MonoDevelop.WebReferences/AddinInfo.cs
@@ -9,7 +9,7 @@ using Mono.Addins.Description;
         Category = "Web Development")]
 
 [assembly:AddinName ("Project Web References")]
-[assembly:AddinDescription ("Provides support for adding and maintaining Web References for CSharp and VB projects")]
+[assembly:AddinDescription ("Provides support for adding and maintaining Web References for C# and VB.NET projects")]
 
 [assembly:AddinDependency ("Core", MonoDevelop.BuildInfo.Version)]
 [assembly:AddinDependency ("Ide", MonoDevelop.BuildInfo.Version)]
