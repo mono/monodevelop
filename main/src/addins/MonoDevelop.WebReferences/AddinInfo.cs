3efd0290201484d2a88eeb2cdb56cd1aa6948137
Andrés G. Aragoneses <knocte@gmail.com>
2013-07-24 16:44:41 +0200

    [WebReferences] Fix typo in add-in description
    "maintianing" -> "maintaining"

--- main/src/addins/MonoDevelop.WebReferences/AddinInfo.cs
+++ main/src/addins/MonoDevelop.WebReferences/AddinInfo.cs
@@ -9,7 +9,7 @@ using Mono.Addins.Description;
         Category = "Web Development")]
 
 [assembly:AddinName ("Project Web References")]
-[assembly:AddinDescription ("Provides support for adding and maintianing Web References for CSharp and VB projects")]
+[assembly:AddinDescription ("Provides support for adding and maintaining Web References for CSharp and VB projects")]
 
 [assembly:AddinDependency ("Core", MonoDevelop.BuildInfo.Version)]
 [assembly:AddinDependency ("Ide", MonoDevelop.BuildInfo.Version)]
