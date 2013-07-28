544d983947538a0ffc70c77d9ea78ee8d57b4d93
Luís Reis <luiscubal@gmail.com>
2013-07-28 16:00:14 +0100

    MonoDevelop.Core: Remove commented code left behind in previous commit.

--- main/src/core/MonoDevelop.Core/MonoDevelop.Core.FileSystem/FileSystemExtension.cs
+++ main/src/core/MonoDevelop.Core/MonoDevelop.Core.FileSystem/FileSystemExtension.cs
@@ -70,8 +70,6 @@ namespace MonoDevelop.Core.FileSystem
 		
 		public virtual void MoveFile (FilePath source, FilePath dest)
 		{
-			//GetNextForPath (source, false).MoveFile (source, dest);
-			
 			FileSystemExtension srcExt = GetNextForPath (source, false);
 			FileSystemExtension dstExt = GetNextForPath (dest, false);
 			
