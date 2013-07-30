mono/monodevelop

commit 13cb995ad6fd8188f532131c76f5c154329b801f

[NUnit] Add better support for streaming updates from mdtool/guiunit
If we get streaming results from these test runners we need to clear
out the test results pad before we parse the results xml. Some information
is only available in the results xml (currently) so to avoid having each
test appear twice in the output pad we clear it out after we receive
all the streaming updates and then re-populate it from the results
xml file.

main/src/addins/NUnit/Services/NUnitAssemblyTestSuite.cs
@@ -470,11 +470,13 @@ namespace MonoDevelop.NUnit
 				if (!string.IsNullOrEmpty (cmd.Arguments))
 					cmd.Arguments += " ";
 				cmd.Arguments += "\"-xml=" + outFile + "\" " + AssemblyPath;
+
+				bool automaticUpdates = cmd.Command.Contains ("GuiUnit") || (cmd.Command.Contains ("mdtool.exe") && cmd.Arguments.Contains ("run-md-tests"));
 				if (!string.IsNullOrEmpty (testName))
 					cmd.Arguments += " -run=" + suiteName + "." + testName;
 				else if (!string.IsNullOrEmpty (suiteName))
 					cmd.Arguments += " -run=" + suiteName;
-				if (cmd.Command.Contains ("GuiUnit") || (cmd.Command.Contains ("mdtool.exe") && cmd.Arguments.Contains ("run-md-tests"))) {
+				if (automaticUpdates) {
 					var tcpListener = new MonoDevelop.NUnit.External.TcpTestListener (localMonitor);
 					cmd.Arguments += " -port=" + tcpListener.Port;
 				}
@@ -487,9 +489,13 @@ namespace MonoDevelop.NUnit
 				
 				if (new FileInfo (outFile).Length == 0)
 					throw new Exception ("Command failed");
-
+				
 				XDocument doc = XDocument.Load (outFile);
+
 				if (doc.Root != null) {
+					if (automaticUpdates)
+						testContext.ResultsPad.InitializeTestRun (test);
+
 					var root = doc.Root.Elements ("test-suite").FirstOrDefault ();
 					if (root != null) {
 						cons.SetDone ();

main/src/addins/NUnit/Gui/TestResultsPad.cs
@@ -44,7 +44,7 @@ using System.Text.RegularExpressions;
 
 namespace MonoDevelop.NUnit
 {
-	class TestResultsPad: IPadContent, ITestProgressMonitor
+	public class TestResultsPad: IPadContent, ITestProgressMonitor
 	{
 		NUnitService testService = NUnitService.Instance;
 		

main/src/addins/NUnit/Services/NUnitProjectTestSuite.cs
@@ -28,6 +28,7 @@
 
 
 using System.IO;
+using System.Linq;
 using System.Collections.Generic;
 
 using MonoDevelop.Projects;
@@ -127,6 +128,12 @@ namespace MonoDevelop.NUnit
 			var r = project.ExtendedProperties ["TestRunnerCommand"];
 			command = r != null ? project.BaseDirectory.Combine (r.ToString ()).ToString () : null;
 			args = (string)project.ExtendedProperties ["TestRunnerArgs"];
+			if (command == null && args == null) {
+				var guiUnit = project.References.FirstOrDefault (pref => pref.ReferenceType == ReferenceType.Assembly && Path.GetFileName (pref.Reference) == "GuiUnit.exe");
+				if (guiUnit != null) {
+					command = guiUnit.Reference;
+				}
+			}
 		}
 
 		protected override string AssemblyPath {

main/src/addins/NUnit/Services/NUnitService.cs
@@ -333,12 +333,14 @@ namespace MonoDevelop.NUnit
 		bool success;
 		ManualResetEvent waitEvent;
 		IExecutionHandler context;
-		
+		TestResultsPad resultsPad;
+
 		public TestSession (UnitTest test, IExecutionHandler context, TestResultsPad resultsPad)
 		{
 			this.test = test;
 			this.context = context;
 			this.monitor = new TestMonitor (resultsPad);
+			this.resultsPad = resultsPad;
 			resultsPad.InitializeTestRun (test);
 		}
 		
@@ -355,7 +357,7 @@ namespace MonoDevelop.NUnit
 			try {
 				NUnitService.ResetResult (test);
 
-				TestContext ctx = new TestContext (monitor, context, DateTime.Now);
+				TestContext ctx = new TestContext (monitor, resultsPad, context, DateTime.Now);
 				test.Run (ctx);
 				test.SaveResults ();
 				success = true;

main/src/addins/NUnit/Services/TestContext.cs
@@ -40,10 +40,11 @@ namespace MonoDevelop.NUnit
 		DateTime testDate;
 		object contextData;
 		IExecutionHandler executionContext;
-		
-		public TestContext (ITestProgressMonitor monitor, IExecutionHandler executionContext, DateTime testDate)
+
+		public TestContext (ITestProgressMonitor monitor, TestResultsPad resultsPad, IExecutionHandler executionContext, DateTime testDate)
 		{
 			this.monitor = monitor;
+			ResultsPad = resultsPad;
 			if (executionContext == null)
 				executionContext = Runtime.ProcessService.DefaultExecutionHandler;
 			this.executionContext = executionContext;
@@ -66,6 +67,10 @@ namespace MonoDevelop.NUnit
 		
 		public IExecutionHandler ExecutionContext {
 			get { return executionContext; }
+		}
+
+		internal TestResultsPad ResultsPad {
+			get; private set;
 		}
 	}
 }

commit 89ad427b1e8cca5f495469b063ea3d8aae3e115c

[NUnit] Don't reset the test results pad on a background thread
Fixes a crasher introduced in 13cb995ad6

main/src/addins/NUnit/Services/NUnitAssemblyTestSuite.cs
@@ -493,8 +493,11 @@ namespace MonoDevelop.NUnit
 				XDocument doc = XDocument.Load (outFile);
 
 				if (doc.Root != null) {
-					if (automaticUpdates)
-						testContext.ResultsPad.InitializeTestRun (test);
+					if (automaticUpdates) {
+						DispatchService.GuiDispatch (delegate {
+							testContext.ResultsPad.InitializeTestRun (test);
+						});
+					}
 
 					var root = doc.Root.Elements ("test-suite").FirstOrDefault ();
 					if (root != null) {