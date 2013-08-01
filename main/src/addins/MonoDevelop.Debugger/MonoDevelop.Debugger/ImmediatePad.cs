3a880291ef339756a1cd04daab1ce907f7f0ab80
Jeffrey Stedfast <fejj@gnome.org>
2013-08-01 16:09:02 -0400

    [Debugger] More code-completion improvements to the ImmediatePad

--- main/src/addins/MonoDevelop.Debugger/MonoDevelop.Debugger/ImmediatePad.cs
+++ main/src/addins/MonoDevelop.Debugger/MonoDevelop.Debugger/ImmediatePad.cs
@@ -25,25 +25,25 @@
 // THE SOFTWARE.
 
 using System;
+using System.Linq;
+using System.Collections.Generic;
+
 using Mono.Debugging.Client;
-using MonoDevelop.Ide;
+
 using MonoDevelop.Core;
 using MonoDevelop.Ide.Gui;
 using MonoDevelop.Components;
-using MonoDevelop.Ide.CodeCompletion;
-using System.Linq;
-using System.Collections.Generic;
 
 namespace MonoDevelop.Debugger
 {
 	public class ImmediatePad: IPadContent
 	{
 		static object locker = new object();
-		ImmediateConsoleView view;
+		DebuggerConsoleView view;
 		
 		public void Initialize (IPadWindow container)
 		{
-			view = new ImmediateConsoleView ();
+			view = new DebuggerConsoleView ();
 			view.ConsoleInput += OnViewConsoleInput;
 			view.ShadowType = Gtk.ShadowType.None;
 			view.ShowAll ();
@@ -264,276 +264,6 @@ namespace MonoDevelop.Debugger
 
 		public void Dispose ()
 		{
-		}
-	}
-
-	class ImmediateConsoleView : ConsoleView, ICompletionWidget
-	{
-		const string tokenBeginChars = "([<,";
-
-		Mono.Debugging.Client.CompletionData currentCompletionData;
-		bool isLiteralString = false;
-		Gtk.TextMark tokenBeginMark;
-		CodeCompletionContext ctx;
-		Gdk.ModifierType modifier;
-		bool keyHandled = false;
-		uint keyValue;
-		char keyChar;
-		Gdk.Key key;
-
-		public ImmediateConsoleView ()
-		{
-			SetFont (IdeApp.Preferences.CustomOutputPadFont);
-
-			TextView.KeyReleaseEvent += OnEditKeyRelease;
-
-			IdeApp.Preferences.CustomOutputPadFontChanged += OnCustomOutputPadFontChanged;
-			CompletionWindowManager.WindowClosed += OnCompletionWindowClosed;
-		}
-
-		static bool IsCompletionChar (char c)
-		{
-			return (char.IsLetterOrDigit (c) || char.IsPunctuation (c) || char.IsSymbol (c) || char.IsWhiteSpace (c));
-		}
-
-		static Mono.Debugging.Client.CompletionData GetCompletionData (string exp)
-		{
-			if (DebuggingService.CurrentFrame != null)
-				return DebuggingService.CurrentFrame.GetExpressionCompletionData (exp);
-
-			return null;
-		}
-
-		void OnCompletionWindowClosed (object sender, EventArgs e)
-		{
-			currentCompletionData = null;
-		}
-
-		void PopupCompletion ()
-		{
-			Gtk.Application.Invoke (delegate {
-				char c = (char) Gdk.Keyval.ToUnicode (keyValue);
-				if (currentCompletionData == null && IsCompletionChar (c)) {
-					string expr = Buffer.GetText (TokenBegin, Cursor, false);
-					currentCompletionData = GetCompletionData (expr);
-					if (currentCompletionData != null) {
-						DebugCompletionDataList dataList = new DebugCompletionDataList (currentCompletionData);
-						ctx = ((ICompletionWidget) this).CreateCodeCompletionContext (expr.Length - currentCompletionData.ExpressionLength);
-						CompletionWindowManager.ShowWindow (null, c, dataList, this, ctx);
-					} else {
-						currentCompletionData = null;
-					}
-				}
-			});
-		}
-
-		void OnEditKeyRelease (object sender, Gtk.KeyReleaseEventArgs args)
-		{
-			if (!isLiteralString && (tokenBeginChars.IndexOf (keyChar) != -1 || (keyChar == ' ' && TokenBegin.Offset + 1 == Cursor.Offset)))
-				Buffer.MoveMark (tokenBeginMark, Cursor);
-
-			if (keyHandled)
-				return;
-
-			string text = TokenText;
-
-			if (ctx != null)
-				text = text.Substring (Math.Max (0, Math.Min (ctx.TriggerOffset, text.Length)));
-
-			CompletionWindowManager.UpdateWordSelection (text);
-			CompletionWindowManager.PostProcessKeyEvent (key, keyChar, modifier);
-			PopupCompletion ();
-		}
-
-		protected override bool ProcessKeyPressEvent (Gtk.KeyPressEventArgs args)
-		{
-			keyHandled = false;
-
-			keyChar = (char) args.Event.Key;
-			keyValue = args.Event.KeyValue;
-			modifier = args.Event.State;
-			key = args.Event.Key;
-
-			if (keyChar == '"')
-				isLiteralString = !isLiteralString;
-
-			if ((args.Event.Key == Gdk.Key.Down || args.Event.Key == Gdk.Key.Up)) {
-				keyChar = '\0';
-			}
-
-			if (currentCompletionData != null) {
-				if ((keyHandled = CompletionWindowManager.PreProcessKeyEvent (key, keyChar, modifier)))
-					return true;
-			}
-
-			return base.ProcessKeyPressEvent (args);
-		}
-
-		protected override void UpdateInputLineBegin ()
-		{
-			if (tokenBeginMark == null)
-				tokenBeginMark = Buffer.CreateMark (null, Buffer.EndIter, true);
-			else
-				Buffer.MoveMark (tokenBeginMark, Buffer.EndIter);
-
-			base.UpdateInputLineBegin ();
-			isLiteralString = false;
-		}
-
-		string TokenText {
-			get { return Buffer.GetText (TokenBegin, TokenEnd, false); }
-			set {
-				var start = TokenBegin;
-				var end = TokenEnd;
-
-				Buffer.Delete (ref start, ref end);
-				start = TokenBegin;
-				Buffer.Insert (ref start, value);
-			}
-		}
-
-		Gtk.TextIter TokenBegin {
-			get { return Buffer.GetIterAtMark (tokenBeginMark); }
-		}
-
-		Gtk.TextIter TokenEnd {
-			get { return InputLineEnd; }
-		}
-
-		int Position {
-			get { return Cursor.Offset - TokenBegin.Offset; }
-		}
-
-		#region ICompletionWidget implementation
-
-		CodeCompletionContext ICompletionWidget.CurrentCodeCompletionContext {
-			get {
-				return ((ICompletionWidget) this).CreateCodeCompletionContext (Position);
-			}
-		}
-
-		EventHandler completionContextChanged;
-
-		event EventHandler ICompletionWidget.CompletionContextChanged {
-			add { completionContextChanged += value; }
-			remove { completionContextChanged -= value; }
-		}
-
-		string ICompletionWidget.GetText (int startOffset, int endOffset)
-		{
-			var text = TokenText;
-
-			if (startOffset < 0 || startOffset > text.Length) startOffset = 0;
-			if (endOffset > text.Length) endOffset = text.Length;
-
-			return text.Substring (startOffset, endOffset - startOffset);
-		}
-
-		void ICompletionWidget.Replace (int offset, int count, string text)
-		{
-			if (count > 0)
-				TokenText = TokenText.Remove (offset, count);
-			if (!string.IsNullOrEmpty (text))
-				TokenText = TokenText.Insert (offset, text);
-		}
-
-		int ICompletionWidget.CaretOffset {
-			get {
-				return Position;
-			}
-		}
-
-		char ICompletionWidget.GetChar (int offset)
-		{
-			string text = TokenText;
-
-			if (offset >= text.Length)
-				return (char) 0;
-
-			return text[offset];
-		}
-
-		CodeCompletionContext ICompletionWidget.CreateCodeCompletionContext (int triggerOffset)
-		{
-			CodeCompletionContext c = new CodeCompletionContext ();
-			c.TriggerLine = 0;
-			c.TriggerOffset = triggerOffset;
-			c.TriggerLineOffset = c.TriggerOffset;
-			c.TriggerWordLength = currentCompletionData.ExpressionLength;
-
-			int height, lineY, x, y;
-			TextView.GdkWindow.GetOrigin (out x, out y);
-			TextView.GetLineYrange (Cursor, out lineY, out height);
-
-			var rect = GetIterLocation (Cursor);
-
-			c.TriggerYCoord = y + lineY + height;
-			c.TriggerXCoord = x + rect.X;
-			c.TriggerTextHeight = height;
-
-			return c;
-		}
-
-		string ICompletionWidget.GetCompletionText (CodeCompletionContext ctx)
-		{
-			return TokenText.Substring (ctx.TriggerOffset, ctx.TriggerWordLength);
-		}
-
-		void ICompletionWidget.SetCompletionText (CodeCompletionContext ctx, string partial_word, string complete_word)
-		{
-			int sp = Position - partial_word.Length;
-
-			var start = Buffer.GetIterAtOffset (TokenBegin.Offset + sp);
-			var end = Buffer.GetIterAtOffset (start.Offset + partial_word.Length);
-			Buffer.Delete (ref start, ref end);
-			Buffer.Insert (ref start, complete_word);
-			Buffer.PlaceCursor (start);
-		}
-
-		void ICompletionWidget.SetCompletionText (CodeCompletionContext ctx, string partial_word, string complete_word, int offset)
-		{
-			int sp = Position - partial_word.Length;
-
-			var start = Buffer.GetIterAtOffset (TokenBegin.Offset + sp);
-			var end = Buffer.GetIterAtOffset (start.Offset + partial_word.Length);
-			Buffer.Delete (ref start, ref end);
-			Buffer.Insert (ref start, complete_word);
-
-			var cursor = Buffer.GetIterAtOffset (start.Offset + offset);
-			Buffer.PlaceCursor (cursor);
-		}
-
-		int ICompletionWidget.TextLength {
-			get {
-				return TokenText.Length;
-			}
-		}
-
-		int ICompletionWidget.SelectedLength {
-			get {
-				return 0;
-			}
-		}
-
-		Gtk.Style ICompletionWidget.GtkStyle {
-			get {
-				return Style;
-			}
-		}
-
-		#endregion
-
-		void OnCustomOutputPadFontChanged (object sender, EventArgs e)
-		{
-			SetFont (IdeApp.Preferences.CustomOutputPadFont);
-		}
-
-		protected override void OnDestroyed ()
-		{
-			IdeApp.Preferences.CustomOutputPadFontChanged -= OnCustomOutputPadFontChanged;
-			CompletionWindowManager.WindowClosed -= OnCompletionWindowClosed;
-			CompletionWindowManager.HideWindow ();
-			base.OnDestroyed ();
 		}
 	}
 }
