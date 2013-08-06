bcca6d3f661d440bef37fa7bc9cdbb9caf5dbd12
Therzok <teromario@yahoo.com>
2013-08-06 17:04:20 +0300

    [Version Control] Enable update test on Unix.

--- main/src/addins/VersionControl/MonoDevelop.VersionControl.Git.Tests/BaseRepositoryTests.cs
+++ main/src/addins/VersionControl/MonoDevelop.VersionControl.Git.Tests/BaseRepositoryTests.cs
@@ -96,6 +96,7 @@ namespace MonoDevelop.VersionControl.Tests
 		}
 
 		[Test]
+		[Platform (Exclude = "Win")]
 		public virtual void UpdateIsDone ()
 		{
 			string added = rootCheckout + "testfile";
