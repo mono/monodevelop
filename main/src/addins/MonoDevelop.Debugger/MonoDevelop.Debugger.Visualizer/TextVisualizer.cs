abc6bfff973ae68657515157ac4f839b402617ff
Jeffrey Stedfast <fejj@gnome.org>
2013-08-12 20:44:23 -0400

    [Debugger] Modified TextVisualizer to support byte[], sbyte[], and char[]

--- main/src/addins/MonoDevelop.Debugger/MonoDevelop.Debugger.Visualizer/TextVisualizer.cs
+++ main/src/addins/MonoDevelop.Debugger/MonoDevelop.Debugger.Visualizer/TextVisualizer.cs
@@ -25,8 +25,11 @@
 // THE SOFTWARE.
 
 using System;
-using Mono.Debugging.Client;
+using System.Text;
+
 using Gtk;
+
+using Mono.Debugging.Client;
 using MonoDevelop.Core;
 
 namespace MonoDevelop.Debugger.Visualizer
@@ -37,9 +40,7 @@ namespace MonoDevelop.Debugger.Visualizer
 		
 		TextView textView;
 		RawValueString raw;
-#pragma warning disable 414
 		ObjectValue value;
-#pragma warning restore 414
 		uint idle_id;
 		int length;
 		int offset;
@@ -49,14 +50,58 @@ namespace MonoDevelop.Debugger.Visualizer
 		}
 		
 		public string Name {
-			get {
-				return GettextCatalog.GetString ("Text");
-			}
+			get { return GettextCatalog.GetString ("Text"); }
 		}
 		
 		public bool CanVisualize (ObjectValue val)
 		{
-			return val.TypeName == "string";
+			switch (val.TypeName) {
+			case "sbyte[]": return true;
+			case "byte[]": return true;
+			case "char[]": return true;
+			case "string": return true;
+			default: return false;
+			}
+		}
+
+		static void AppendByte (StringBuilder text, byte c)
+		{
+			switch (c) {
+			case 0x00: text.Append ("\\0"); break;
+			case 0x07: text.Append ("\\a"); break;
+			case 0x08: text.Append ("\\b"); break;
+			case 0x09: text.Append ("\\t"); break;
+			case 0x0A: text.Append ("\\n"); break;
+			case 0x0B: text.Append ("\\v"); break;
+			case 0x0D: text.Append ("\\r"); break;
+			default:
+				if (c < 020 || c > 0x7e) {
+					text.AppendFormat ("\\x{0:x,2}", c);
+				} else {
+					text.Append ((char) c);
+				}
+				break;
+			}
+		}
+
+		static string ByteArrayToCString (byte[] buf)
+		{
+			StringBuilder text = new StringBuilder ();
+
+			for (int i = 0; i < buf.Length; i++)
+				AppendByte (text, buf[i]);
+
+			return text.ToString ();
+		}
+
+		static string SByteArrayToCString (sbyte[] buf)
+		{
+			StringBuilder text = new StringBuilder ();
+
+			for (int i = 0; i < buf.Length; i++)
+				AppendByte (text, (byte) buf[i]);
+
+			return text.ToString ();
 		}
 		
 		bool GetNextStringChunk ()
@@ -77,57 +122,98 @@ namespace MonoDevelop.Debugger.Visualizer
 			return false;
 		}
 
+		void PopulateTextView ()
+		{
+			if (value.TypeName == "string") {
+				EvaluationOptions ops = DebuggingService.DebuggerSession.EvaluationOptions.Clone ();
+				ops.ChunkRawStrings = true;
+
+				raw = value.GetRawValue (ops) as RawValueString;
+				length = raw.Length;
+				offset = 0;
+
+				if (length > 0) {
+					idle_id = GLib.Idle.Add (GetNextStringChunk);
+					textView.Destroyed += delegate {
+						if (idle_id != 0) {
+							GLib.Source.Remove (idle_id);
+							idle_id = 0;
+						}
+					};
+				}
+			} else {
+				var array = value.GetRawValue () as RawValueArray;
+				var iter = textView.Buffer.EndIter;
+				string text;
+
+				switch (value.TypeName) {
+				case "sbyte[]":
+					text = SByteArrayToCString (array.ToArray () as sbyte[]);
+					break;
+				case "byte[]":
+					text = ByteArrayToCString (array.ToArray () as byte[]);
+					break;
+				case "char[]":
+					text = new string (array.ToArray () as char[]);
+					break;
+				default:
+					text = string.Empty;
+					break;
+				}
+
+				textView.Buffer.Insert (ref iter, text);
+			}
+		}
+
 		public Widget GetVisualizerWidget (ObjectValue val)
 		{
-			VBox box = new VBox (false, 6);
 			textView = new TextView () { WrapMode = WrapMode.Char };
-			ScrolledWindow scrolled = new ScrolledWindow ();
-			scrolled.HscrollbarPolicy = PolicyType.Automatic;
-			scrolled.VscrollbarPolicy = PolicyType.Automatic;
-			scrolled.ShadowType = ShadowType.In;
+
+			var scrolled = new ScrolledWindow () {
+				HscrollbarPolicy = PolicyType.Automatic,
+				VscrollbarPolicy = PolicyType.Automatic,
+				ShadowType = ShadowType.In
+			};
 			scrolled.Add (textView);
-			box.PackStart (scrolled, true, true, 0);
-			CheckButton cb = new CheckButton (GettextCatalog.GetString ("Wrap text"));
-			cb.Active = true;
-			cb.Toggled += delegate {
-				if (cb.Active)
+
+			var check = new CheckButton (GettextCatalog.GetString ("Wrap text"));
+			check.Active = true;
+			check.Toggled += delegate {
+				if (check.Active)
 					textView.WrapMode = WrapMode.Char;
 				else
 					textView.WrapMode = WrapMode.None;
 			};
-			box.PackStart (cb, false, false, 0);
+
+			var box = new VBox (false, 6);
+			box.PackStart (scrolled, true, true, 0);
+			box.PackStart (check, false, false, 0);
 			box.ShowAll ();
-			
-			EvaluationOptions ops = DebuggingService.DebuggerSession.EvaluationOptions.Clone ();
-			ops.ChunkRawStrings = true;
-			
-			raw = val.GetRawValue (ops) as RawValueString;
-			length = raw.Length;
+
 			value = val;
-			offset = 0;
-			
-			if (length > 0) {
-				idle_id = GLib.Idle.Add (GetNextStringChunk);
-				textView.Destroyed += delegate {
-					if (idle_id != 0) {
-						GLib.Source.Remove (idle_id);
-						idle_id = 0;
-					}
-				};
-			}
+
+			PopulateTextView ();
 			
 			return box;
 		}
 		
 		public bool StoreValue (ObjectValue val)
 		{
-			val.SetRawValue (textView.Buffer.Text);
-			return true;
+			switch (val.TypeName) {
+			case "char[]":
+				val.SetRawValue (textView.Buffer.Text.ToCharArray ());
+				return true;
+			case "string":
+				val.SetRawValue (textView.Buffer.Text);
+				return true;
+			default:
+				return false;
+			}
 		}
 		
 		public bool CanEdit (ObjectValue val)
 		{
-			return true;
+			return val.TypeName == "string" || val.TypeName == "char[]";
 		}
 	}
 }
