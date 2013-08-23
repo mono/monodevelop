a5d188fd598a9b560abd8872623f11137461c39f
Mike Krüger <mkrueger@xamarin.com>
2013-08-23 18:33:57 +0200

    [TextEditor] Fixed fold marker margin drawing bug.

--- main/src/core/Mono.Texteditor/Mono.TextEditor/Gui/FoldMarkerMargin.cs
+++ main/src/core/Mono.Texteditor/Mono.TextEditor/Gui/FoldMarkerMargin.cs
@@ -370,7 +370,7 @@ namespace Mono.TextEditor
 				}
 			}
 
-			if (editor.Options.ShowFoldMargin && line < editor.Document.LineCount) {
+			if (editor.Options.ShowFoldMargin && line <= editor.Document.LineCount) {
 				double foldSegmentYPos = y + System.Math.Floor (editor.LineHeight - foldSegmentSize) / 2;
 				double xPos = x + System.Math.Floor (marginWidth / 2) + 0.5;
 				
