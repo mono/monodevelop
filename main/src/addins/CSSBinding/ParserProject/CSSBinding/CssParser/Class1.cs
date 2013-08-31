eeff0017eee0ebeed9dc0780395685a01fd510cd
dioode <diyoda.sajjana@gmail.com>
2013-08-31 21:28:54 +0530

    Added ANTLR 3 version of the Grammar and the parser implimentation

---
+++ main/src/addins/CSSBinding/ParserProject/CSSBinding/CssParser/Class1.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.IO;
+using Antlr.Runtime;
+
+
+namespace CssParser
+{
+	class Program
+	{
+		public static void Main(string[] args)
+		{
+			Stream inputStream = Console.OpenStandardInput();
+			ANTLRInputStream input = new ANTLRInputStream(inputStream);
+			CSSLexer lexer = new CSSLexer(input);
+			CommonTokenStream tokens = new CommonTokenStream(lexer);
+			CSSParser parser = new CSSParser(tokens);
+			parser.styleSheet();
+
+			Console.Read();
+			
+		}
+	}
+
+	public partial class CSSLexer
+	{
+		public override void ReportError(RecognitionException e)
+		{
+			base.ReportError(e);
+			Console.WriteLine("Error in lexer at line " + e.Line + ":" + e.CharPositionInLine + e.Message);
+		}
+
+	}
+
+
+	public partial class CSSParser
+	{
+		public override void ReportError(RecognitionException e)
+		{
+			base.ReportError(e);
+			int x = e.CharPositionInLine;
+			Console.WriteLine("Error in lexer at line " + e.Line + ":" + x);
+		}
+
+	}
+}
+
+
+
