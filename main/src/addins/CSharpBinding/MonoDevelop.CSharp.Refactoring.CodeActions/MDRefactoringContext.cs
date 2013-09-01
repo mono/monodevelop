5295539729a38126d73dcbe88bf5284005f14b3b
Mike Krüger <mkrueger@xamarin.com>
2013-09-01 15:38:01 +0200

    [CSharpBinding] Corrected location for refactoring context.

--- main/src/addins/CSharpBinding/MonoDevelop.CSharp.Refactoring.CodeActions/MDRefactoringContext.cs
+++ main/src/addins/CSharpBinding/MonoDevelop.CSharp.Refactoring.CodeActions/MDRefactoringContext.cs
@@ -182,7 +182,7 @@ namespace MonoDevelop.CSharp.Refactoring.CodeActions
 			this.ParsedDocument = document.ParsedDocument;
 			this.Project = document.Project as DotNetProject;
 			this.formattingOptions = document.GetFormattingOptions ();
-			this.location = RefactoringService.GetCorrectResolveLocation (document, loc);
+			this.location = loc;
 			var policy = document.HasProject ? Project.Policies.Get<NameConventionPolicy> () : MonoDevelop.Projects.Policies.PolicyService.GetDefaultPolicy<NameConventionPolicy> ();
 			Services.AddService (typeof(NamingConventionService), policy.CreateNRefactoryService ());
 		}
