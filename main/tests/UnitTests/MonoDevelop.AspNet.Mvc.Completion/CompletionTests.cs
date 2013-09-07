bec09285fb19ae71fea9c3e9fa3b3242921297d8
Michael Hutchinson <mhutch@xamarin.com>
2013-09-07 19:38:44 -0400

    [Razor] Re-enable Razor completion tests
    They work again now the bug in the C# extension is fixed.

--- main/tests/UnitTests/MonoDevelop.AspNet.Mvc.Completion/CompletionTests.cs
+++ main/tests/UnitTests/MonoDevelop.AspNet.Mvc.Completion/CompletionTests.cs
@@ -48,7 +48,6 @@ namespace UnitTests.MonoDevelop.AspNet.Mvc.Completion
 			Assert.IsNotNull (provider.Find ("li"));
 		}
 
-		[Ignore("ASP.NET completion broken.")]
 		[Test]
 		public void RazorDirectivesAndStatementsCompletion ()
 		{
@@ -72,7 +71,6 @@ namespace UnitTests.MonoDevelop.AspNet.Mvc.Completion
 			Assert.IsNotNull (provider.Find ("try"));
 		}
 
-		[Ignore("ASP.NET completion broken.")]
 		[Test]
 		public void CSharpIdentifiersCompletion ()
 		{
@@ -82,7 +80,6 @@ namespace UnitTests.MonoDevelop.AspNet.Mvc.Completion
 			Assert.IsNotNull (provider.Find ("var"));
 		}
 
-		[Ignore("ASP.NET completion broken.")]
 		[Test]
 		public void CSharpIdentifiersCtrlSpaceCompletion ()
 		{
@@ -92,7 +89,6 @@ namespace UnitTests.MonoDevelop.AspNet.Mvc.Completion
 			Assert.IsNotNull (provider.Find ("var"));
 		}
 
-		[Ignore("ASP.NET completion broken.")]
 		[Test]
 		public void CSharpMembersCompletion ()
 		{
@@ -101,7 +97,6 @@ namespace UnitTests.MonoDevelop.AspNet.Mvc.Completion
 			Assert.IsNotNull (provider.Find ("IsLetter"));
 		}
 
-		[Ignore("ASP.NET completion broken.")]
 		[Test]
 		public void CSharpMembersCtrlSpaceCompletion ()
 		{
