a857484d60790c9b070e712393e8234f27d3a4eb
Lluis Sanchez <lluis@xamarin.com>
2013-10-07 15:51:03 +0200

    [NUnit] Improve support for guiunit
    When running on guiunit there is no need to parse the resulting xml
    file, since all results come through the tcp connection.
    
    Improved TcpTestListener. It now properly reports the status for all
    the sections (nodes) of a test suite.
    
    Fixed 15165 - Xamarin Studio cannot run single tests using an external
    test runner correctly.

--- main/src/addins/NUnit/Services/TcpTestListener.cs
+++ main/src/addins/NUnit/Services/TcpTestListener.cs
@@ -34,11 +34,18 @@ using System.Threading.Tasks;
 using System.Xml.Linq;
 using System.Collections.Generic;
 using System.Linq;
+using MonoDevelop.Core;
 
 namespace MonoDevelop.NUnit.External
 {
 	class TcpTestListener
 	{
+		string testSuiteName;
+		string rootTestName;
+
+		List<Tuple<string,UnitTestResult>> suiteStack = new List<Tuple<string, UnitTestResult>> ();
+		IRemoteEventListener listener;
+
 		TcpListener TcpListener {
 			get; set;
 		}
@@ -48,8 +55,12 @@ namespace MonoDevelop.NUnit.External
 		}
 
 
-		public TcpTestListener (IRemoteEventListener listener)
+		public TcpTestListener (IRemoteEventListener listener, string suiteName)
 		{
+			this.testSuiteName = suiteName;
+			this.listener = listener;
+			bool rootSuiteStarted = false;
+
 			TcpListener = new TcpListener (new IPEndPoint (IPAddress.Loopback, 0));
 			TcpListener.Start ();
 			Task.Factory.StartNew (() => {
@@ -61,27 +72,112 @@ namespace MonoDevelop.NUnit.External
 						string line = null;
 						while ((line = reader.ReadLine ()) != null) {
 							var element = XElement.Parse (line);
+							string testName = element.Attribute ("name").Value;
+							var action = element.Name.LocalName;
 
-							Gtk.Application.Invoke (delegate {
-								var testName = element.Attribute ("name").Value;
-								if (element.Name.LocalName == "suite-started") {
-									listener.SuiteStarted (testName);
-								} else if (element.Name.LocalName == "test-started") {
-									listener.TestStarted (testName);
-								} else if (element.Name.LocalName == "test-finished") {
-									listener.TestFinished (testName, CreateResult (element));
-								} else if (element.Name.LocalName == "suite-finished") {
-									listener.SuiteFinished (testName, CreateResult (element));
+							if (testSuiteName.Length == 0 && !rootSuiteStarted) {
+								// Running the whole assembly
+								rootTestName = testName;
+								rootSuiteStarted = true;
+								continue;
+							}
+							if (testSuiteName == testName && !rootSuiteStarted) {
+								// Running a test suite
+								rootTestName = testName;
+								rootSuiteStarted = true;
+								listener.SuiteStarted ("<root>");
+								continue;
+							}
+
+							if (!rootSuiteStarted)
+								continue;
+
+							switch (action) {
+							case "suite-started":
+								UpdateTestSuiteStatus (testName, false); break;
+							case "test-started":
+								UpdateTestSuiteStatus (testName, true);
+								listener.TestStarted (testName); break;
+							case "test-finished":
+								var res = CreateResult (element);
+								AddTestResult (res);
+								listener.TestFinished (testName, res); break;
+							case "suite-finished":
+								if (testName == rootTestName) {
+									FinishSuites (0);
+									listener.SuiteFinished ("<root>", CreateResult (element));
+									rootSuiteStarted = false;
 								}
-							});
+								break;
+							}
 						}
 					}
-				} catch {
-
+				} catch (Exception ex) {
+					LoggingService.LogError ("Exception in test listener", ex);
 				} finally {
 					TcpListener.Stop ();
 				}
 			});
+		}
+
+		void UpdateTestSuiteStatus (string name, bool isTest)
+		{
+			if (testSuiteName.Length > 0)
+				name = name.Substring (testSuiteName.Length + 1);
+			string[] parts = name.Split ('.');
+			for (int n = 0; n < parts.Length; n++) {
+				if (n >= suiteStack.Count) {
+					StartSuite (parts[n]);
+				} else if (parts [n] != suiteStack [n].Item1) {
+					FinishSuites (n);
+					StartSuite (parts[n]);
+				}
+			}
+		}
+
+		void FinishSuites (int stackLevel)
+		{
+			if (stackLevel + 1 < suiteStack.Count)
+				FinishSuites (stackLevel + 1);
+
+			if (stackLevel >= suiteStack.Count)
+				return;
+
+			var tname = GetTestSuiteName (stackLevel);
+			var res = suiteStack [stackLevel].Item2;
+
+			suiteStack.RemoveAt (stackLevel);
+
+			listener.SuiteFinished (tname, res);
+		}
+
+		void StartSuite (string name)
+		{
+			suiteStack.Add (new Tuple<string, UnitTestResult> (name, new UnitTestResult ()));
+			name = GetTestSuiteName (suiteStack.Count - 1);
+			listener.SuiteStarted (name);
+		}
+
+		void AddTestResult (UnitTestResult res)
+		{
+			foreach (var r in suiteStack)
+				r.Item2.Add (res);
+		}
+
+		string GetTestSuiteName (int stackLevel)
+		{
+			var info = suiteStack [stackLevel];
+
+			string name;
+			if (stackLevel > 0) {
+				var prefix = string.Join (".", suiteStack.Select (s => s.Item1).Take (stackLevel));
+				name = prefix + "." + info.Item1;
+			} else
+				name = info.Item1;
+
+			if (testSuiteName.Length > 0)
+				name = testSuiteName + "." + name;
+			return name;
 		}
 
 		UnitTestResult CreateResult (XElement element)
@@ -92,12 +188,17 @@ namespace MonoDevelop.NUnit.External
 			var ignored = int.Parse (element.Attribute ("ignored").Value);
 			var inconclusive = int.Parse (element.Attribute ("inconclusive").Value);
 
+			var message = (string)element.Attribute ("message");
+			var stackTrace = (string)element.Attribute ("stack-trace");
+
 			return new UnitTestResult {
 				Status = result,
 				Passed = passed,
 				Failures = failures,
 				Ignored = ignored,
-				Inconclusive = inconclusive
+				Inconclusive = inconclusive,
+				Message = message,
+				StackTrace = stackTrace
 			};
 		}
 	}
