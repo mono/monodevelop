3574bbd32834b7212b8a59742322c7a2006a8cb2
Xamarin Release Manager <release-manager@xamarin.com>
2013-10-17 18:58:47 +0000

    Updated package version to 4.0.14

--- main/src/core/MonoDevelop.Core/BuildVariables.cs
+++ main/src/core/MonoDevelop.Core/BuildVariables.cs
@@ -2,8 +2,8 @@ namespace MonoDevelop
 {
 	public static class BuildInfo
 	{
-		public const string Version = "4.0.13";
-		public const string VersionLabel = "4.0.13";
+		public const string Version = "4.0.14";
+		public const string VersionLabel = "4.0.14";
 		public const string CompatVersion = "4.0";
 	}
 }
