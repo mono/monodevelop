abd09cce5668f915ef874d8ad4488f6bdf2417af
Therzok <teromario@yahoo.com>
2013-10-24 18:14:33 +0300

    [wintest.sh] Append /build/tests/ to the assembly name if it isn't there.

--- main/wintest.sh
+++ main/wintest.sh
@@ -3,6 +3,9 @@ if [ $# -lt 1 ]; then
 	build/bin/mdtool run-md-tests build/tests/*.Tests.dll
 else
 	for arg in $@; do
+		if [[ $arg != build/tests/* ]]; then
+			arg="build/tests/$arg"
+		fi
 		build/bin/mdtool run-md-tests $arg
 	done
 fi
