1dae2e7467f01ea8272758313eae3dd088e86f21
Therzok <teromario@yahoo.com>
2013-10-24 18:30:56 +0300

    [WinTest.sh] Fix this file more.

--- main/wintest.sh
+++ main/wintest.sh
@@ -1,11 +1,15 @@
 if [ $# -lt 1 ]; then
 	build/bin/mdtool run-md-tests build/tests/UnitTests.dll
 	build/bin/mdtool run-md-tests build/tests/*.Tests.dll
+	build/bin/mdtool run-md-tests external/nrefactory/bin/Debug/ICSharpCode.NRefactory.Tests.dll
 else
 	for arg in $@; do
-		if [[ $arg != build/tests/* ]]; then
+		if [[ $arg == "ICSharpCode.NRefactory.Tests.dll" ]]; then
+			arg="external/nrefactory/bin/Debug/ICSharpCode.NRefactory.Tests.dll"
+		elif [[ $arg != build/tests/* ]]; then
 			arg="build/tests/$arg"
 		fi
-		build/bin/mdtool run-md-tests $arg
+
+		(build/bin/mdtool run-md-tests $arg) || exit $?
 	done
 fi
