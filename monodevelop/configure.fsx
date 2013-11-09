6facf2405466e678bc88b848450c78d555141c98
Don Syme <donsyme@fastmail.fm>
2013-11-09 17:54:52 +0000

    update version number to 3.2.20

--- monodevelop/configure.fsx
+++ monodevelop/configure.fsx
@@ -12,7 +12,7 @@ open System.IO
 open System.Diagnostics
 open System.Text.RegularExpressions
 
-let FSharpVersion = "3.2.19"
+let FSharpVersion = "3.2.20"
 
 let UnixPaths = 
     [ "/usr/lib/monodevelop"
