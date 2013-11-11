0d5e1f22c49bb1a38d83ba17510f6a01fa8eada4
Alan McGovern <alan@xamarin.com>
2013-11-11 16:14:58 -0500

    [NUnit] Fix the execution of single tests
    We need to use the TestId as that is the thing which uniquely identifes
    both single tests and also test fixtures.

--- main/src/addins/NUnit/Services/NUnitAssemblyTestSuite.cs
+++ main/src/addins/NUnit/Services/NUnitAssemblyTestSuite.cs
@@ -474,7 +474,7 @@ namespace MonoDevelop.NUnit
 
 				bool automaticUpdates = cmd.Command.Contains ("GuiUnit") || (cmd.Command.Contains ("mdtool.exe") && cmd.Arguments.Contains ("run-md-tests"));
 				if (!string.IsNullOrEmpty(pathName))
-					cmd.Arguments += " -run=" + pathName;
+					cmd.Arguments += " -run=" + test.TestId;
 				if (automaticUpdates) {
 					tcpListener = new MonoDevelop.NUnit.External.TcpTestListener (localMonitor, suiteName);
 					cmd.Arguments += " -port=" + tcpListener.Port;
