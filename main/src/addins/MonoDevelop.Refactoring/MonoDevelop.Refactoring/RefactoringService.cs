5183410ee2c6f40cd4a60b6ca1809a81d9d8ac2d
Mike Krüger <mkrueger@xamarin.com>
2013-11-20 12:21:30 +0100

    [Refactoring] Added instrumentation timer for background analysis.

--- main/src/addins/MonoDevelop.Refactoring/MonoDevelop.Refactoring/RefactoringService.cs
+++ main/src/addins/MonoDevelop.Refactoring/MonoDevelop.Refactoring/RefactoringService.cs
@@ -41,6 +41,7 @@ using MonoDevelop.CodeIssues;
 using Mono.TextEditor;
 using MonoDevelop.Ide.TypeSystem;
 using System.Diagnostics;
+using MonoDevelop.Core.Instrumentation;
 
 namespace MonoDevelop.Refactoring
 {
@@ -204,49 +205,26 @@ namespace MonoDevelop.Refactoring
 			string disabledNodes = editor != null ? PropertyService.Get ("ContextActions." + editor.MimeType, "") ?? "" : "";
 			return Task.Factory.StartNew (delegate {
 				var result = new List<CodeAction> ();
+				var timer = InstrumentationService.CreateTimerCounter ("Source analysis background task", "Source analysis");
+				timer.BeginTiming ();
 				try {
-					#if PROFILE
-					var runList = new List<Tuple<long, string>> ();
-					var globalClock = new Stopwatch();
-					globalClock.Start ();
-
-					#endif
-
 					var parsedDocument = doc.ParsedDocument;
 					if (editor != null && parsedDocument != null && parsedDocument.CreateRefactoringContext != null) {
 						var ctx = parsedDocument.CreateRefactoringContext (doc, cancellationToken);
 						if (ctx != null) {
 							foreach (var provider in contextActions.Where (fix => disabledNodes.IndexOf (fix.IdString, StringComparison.Ordinal) < 0)) {
-								#if PROFILE
-								var clock = new Stopwatch();
-								clock.Start ();
-								#endif
 								try {
 									result.AddRange (provider.GetActions (doc, ctx, loc, cancellationToken));
 								} catch (Exception ex) {
 									LoggingService.LogError ("Error in context action provider " + provider.Title, ex);
 								}
-								#if PROFILE
-								clock.Stop ();
-								lock (runList) {
-									runList.Add (Tuple.Create (clock.ElapsedMilliseconds, provider.Title)); 
-								}
-								#endif
 							}
 						}
 					}
-					#if PROFILE
-					globalClock.Stop ();
-					runList.Sort ();
-					Console.WriteLine ("All: " + globalClock.ElapsedMilliseconds +"ms");
-					foreach (var item in runList) {
-						if (item.Item1 == 0)
-							continue;
-						Console.WriteLine (item.Item1 +"ms\t: " + item.Item2);
-					}
-					#endif
 				} catch (Exception ex) {
 					LoggingService.LogError ("Error in analysis service", ex);
+				} finally {
+					timer.EndTiming ();
 				}
 				return (IEnumerable<CodeAction>)result;
 			}, cancellationToken);
