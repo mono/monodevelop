dbc02cc718dd3c142491852bf7f0be13eff59eca
Vaclav Vancura <vaclav@vancura.org>
2013-11-29 17:32:39 +0100

    [Icons] File Overlays icons

--- main/src/addins/VersionControl/MonoDevelop.VersionControl/MonoDevelop.VersionControl/VersionControlService.cs
+++ main/src/addins/VersionControl/MonoDevelop.VersionControl/MonoDevelop.VersionControl/VersionControlService.cs
@@ -55,22 +55,22 @@ namespace MonoDevelop.VersionControl
 		{
 			IdeApp.Initialized += delegate {
 				try {
-					overlay_modified = Xwt.Drawing.Image.FromResource("modified-overlay-16.png");
-					overlay_removed = Xwt.Drawing.Image.FromResource("removed-overlay-16.png");
-					overlay_conflicted = Xwt.Drawing.Image.FromResource("conflict-overlay-16.png");
-					overlay_added = Xwt.Drawing.Image.FromResource("added-overlay-16.png");
-					overlay_controled = Xwt.Drawing.Image.FromResource("versioned-overlay-16.png");
-					overlay_unversioned = Xwt.Drawing.Image.FromResource("unversioned-overlay-16.png");
-					overlay_protected = Xwt.Drawing.Image.FromResource("lock-required-overlay-16.png");
-					overlay_unlocked = Xwt.Drawing.Image.FromResource("unlocked-overlay-16.png");
-					overlay_locked = Xwt.Drawing.Image.FromResource("locked-overlay-16.png");
+					overlay_modified = Xwt.Drawing.Image.FromResource("modified-overlay-light-16.png");
+					overlay_removed = Xwt.Drawing.Image.FromResource("removed-overlay-light-16.png");
+					overlay_conflicted = Xwt.Drawing.Image.FromResource("conflict-overlay-light-16.png");
+					overlay_added = Xwt.Drawing.Image.FromResource("added-overlay-light-16.png");
+					overlay_controled = Xwt.Drawing.Image.FromResource("versioned-overlay-light-16.png");
+					overlay_unversioned = Xwt.Drawing.Image.FromResource("unversioned-overlay-light-16.png");
+					overlay_protected = Xwt.Drawing.Image.FromResource("lock-required-overlay-light-16.png");
+					overlay_unlocked = Xwt.Drawing.Image.FromResource("unlocked-overlay-light-16.png");
+					overlay_locked = Xwt.Drawing.Image.FromResource("locked-overlay-light-16.png");
                     //overlay_normal = Gdk.Pixbuf.LoadFromResource("normal-overlay-16.png");
 			
 					icon_modified = ImageService.GetIcon ("gtk-edit", Gtk.IconSize.Menu);
 					icon_removed = ImageService.GetIcon (Gtk.Stock.Remove, Gtk.IconSize.Menu);
 					icon_conflicted = ImageService.GetIcon (Gtk.Stock.DialogWarning, Gtk.IconSize.Menu);
 					icon_added = ImageService.GetIcon (Gtk.Stock.Add, Gtk.IconSize.Menu);
-					icon_controled = Xwt.Drawing.Image.FromResource("versioned-overlay-16.png");
+					icon_controled = Xwt.Drawing.Image.FromResource("versioned-overlay-light-16.png");
 				} catch (Exception e) {
 					LoggingService.LogError ("Error while loading icons.", e);
 				}
