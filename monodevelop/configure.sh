040282980415ed0eacd101ff64ab776a26f434d5
Robin Neatherway <robin.neatherway@gmail.com>
2013-12-18 11:55:29 +0000

    Pass on configure options to configure.fsx

--- monodevelop/configure.sh
+++ monodevelop/configure.sh
@@ -1,6 +1,6 @@
 #! /bin/bash -e
 echo "Configuring..."
-fsharpi configure.fsx
+fsharpi configure.fsx "$@"
 echo "Getting nuget packages..."
 mozroots --import --sync --quiet
 (cd MonoDevelop.FSharpBinding && mono ../../lib/nuget/NuGet.exe install -OutputDirectory packages)
