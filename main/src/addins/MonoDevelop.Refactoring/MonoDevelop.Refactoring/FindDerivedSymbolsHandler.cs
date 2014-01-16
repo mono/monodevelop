d05e5fbcbc39aefa78cbf0f227f8064e927c7267
Mike Krüger <mkrueger@xamarin.com>
2014-01-16 14:09:19 +0100

    [Refactoring] FindDerivedSymbolsHandler no longer blocks the UI thread.

--- main/src/addins/MonoDevelop.Refactoring/MonoDevelop.Refactoring/FindDerivedSymbolsHandler.cs
+++ main/src/addins/MonoDevelop.Refactoring/MonoDevelop.Refactoring/FindDerivedSymbolsHandler.cs
@@ -32,6 +32,10 @@ using ICSharpCode.NRefactory.Analysis;
 using MonoDevelop.Ide.TypeSystem;
 using System.Collections.Generic;
 using System.Linq;
+using System.Threading.Tasks;
+using MonoDevelop.Projects;
+using System.Threading;
+using MonoDevelop.Core;
 
 namespace MonoDevelop.Refactoring
 {
@@ -40,75 +44,83 @@ namespace MonoDevelop.Refactoring
 		//Ide.Gui.Document doc;
 		readonly IMember entity;
 
+
 		public FindDerivedSymbolsHandler (Ide.Gui.Document doc, IMember entity)
 		{
 			//this.doc = doc;
 			this.entity = entity;
 		}
 
-		static HashSet<IAssembly> GetAllAssemblies ()
+		Task<HashSet<IAssembly>> GetAllAssemblies ()
 		{
-			var assemblies = new HashSet<IAssembly> ();
-			foreach (var project in IdeApp.ProjectOperations.CurrentSelectedSolution.GetAllProjects ()) {
-				var comp = TypeSystemService.GetCompilation (project);
-				if (comp == null)
-					continue;
-				foreach (var asm in comp.Assemblies)
-					assemblies.Add (asm);
-			}
-			return assemblies;
+			var solution = IdeApp.ProjectOperations.CurrentSelectedSolution;
+			return Task.Factory.StartNew (delegate {
+				var assemblies = new HashSet<IAssembly> ();
+				foreach (var project in solution.GetAllProjects ()) {
+					var comp = TypeSystemService.GetCompilation (project);
+					if (comp == null)
+						continue;
+					foreach (var asm in comp.Assemblies)
+						assemblies.Add (asm);
+				}
+				return assemblies;
+			});
 		}
 
 		public bool IsValid {
 			get {
-				var assemblies = GetAllAssemblies ();
-				var tg = new TypeGraph (assemblies);
-				var node = tg.GetNode (entity.DeclaringTypeDefinition); 
-				return node != null;
+				return true;
 			}
 		}
 
 		public void Run ()
 		{
 			var assemblies = GetAllAssemblies ();
-			var tg = new TypeGraph (assemblies);
-			var node = tg.GetNode (entity.DeclaringTypeDefinition); 
-			if (node == null)
-				return;
-			using (var monitor = IdeApp.Workbench.ProgressMonitors.GetSearchProgressMonitor (true, true)) {
-				Stack<IList<TypeGraphNode>> derivedTypes = new Stack<IList<TypeGraphNode>> ();
-				derivedTypes.Push (node.DerivedTypes); 
-				HashSet<ITypeDefinition> visitedType = new HashSet<ITypeDefinition> ();
-				while (derivedTypes.Count > 0) {
-					foreach (var derived in derivedTypes.Pop ()) {
-						if (visitedType.Contains (derived.TypeDefinition))
-							continue;
-						derivedTypes.Push (tg.GetNode (derived.TypeDefinition).DerivedTypes);
-						visitedType.Add (derived.TypeDefinition);
-						var impMember = derived.TypeDefinition.Compilation.Import (entity);
-						if (impMember == null)
-							continue;
-						IMember derivedMember;
-						if (entity.DeclaringTypeDefinition.Kind == TypeKind.Interface) {
-							derivedMember = derived.TypeDefinition.GetMembers (null, GetMemberOptions.IgnoreInheritedMembers).FirstOrDefault (
-								m => m.ImplementedInterfaceMembers.Any (im => im.Region == entity.Region)
-							);
-						} else {
-							derivedMember = InheritanceHelper.GetDerivedMember (impMember, derived.TypeDefinition);
-						}
-						if (derivedMember == null || string.IsNullOrEmpty (derivedMember.Region.FileName))
-							continue;
-						var tf = TextFileProvider.Instance.GetReadOnlyTextEditorData (derivedMember.Region.FileName);
-						var start = tf.LocationToOffset (derivedMember.Region.Begin); 
-						tf.SearchRequest.SearchPattern = derivedMember.Name;
-						var sr = tf.SearchForward (start); 
-						if (sr != null)
-							start = sr.Offset;
+			assemblies.ContinueWith (delegate(Task<HashSet<IAssembly>> arg) {
+				using (var monitor = IdeApp.Workbench.ProgressMonitors.GetSearchProgressMonitor (true, true)) {
+					monitor.BeginTask (GettextCatalog.GetString ("Building type graph in solution ..."), 1); 
+					var tg = new TypeGraph (arg.Result);
+					var node = tg.GetNode (entity.DeclaringTypeDefinition); 
+					monitor.EndTask ();
+					if (node == null)
+						return;
+					Gtk.Application.Invoke (delegate {
+							Stack<IList<TypeGraphNode>> derivedTypes = new Stack<IList<TypeGraphNode>> ();
+							derivedTypes.Push (node.DerivedTypes); 
+							HashSet<ITypeDefinition> visitedType = new HashSet<ITypeDefinition> ();
+							while (derivedTypes.Count > 0) {
+								foreach (var derived in derivedTypes.Pop ()) {
+									if (visitedType.Contains (derived.TypeDefinition))
+										continue;
+									derivedTypes.Push (tg.GetNode (derived.TypeDefinition).DerivedTypes);
+									visitedType.Add (derived.TypeDefinition);
+									var impMember = derived.TypeDefinition.Compilation.Import (entity);
+									if (impMember == null)
+										continue;
+									IMember derivedMember;
+									if (entity.DeclaringTypeDefinition.Kind == TypeKind.Interface) {
+										derivedMember = derived.TypeDefinition.GetMembers (null, GetMemberOptions.IgnoreInheritedMembers).FirstOrDefault (
+											m => m.ImplementedInterfaceMembers.Any (im => im.Region == entity.Region)
+										);
+									} else {
+										derivedMember = InheritanceHelper.GetDerivedMember (impMember, derived.TypeDefinition);
+									}
+									if (derivedMember == null || string.IsNullOrEmpty (derivedMember.Region.FileName))
+										continue;
+									var tf = TextFileProvider.Instance.GetReadOnlyTextEditorData (derivedMember.Region.FileName);
+									var start = tf.LocationToOffset (derivedMember.Region.Begin); 
+									tf.SearchRequest.SearchPattern = derivedMember.Name;
+									var sr = tf.SearchForward (start); 
+									if (sr != null)
+										start = sr.Offset;
 
-						monitor.ReportResult (new MemberReference (derivedMember, derivedMember.Region, start, derivedMember.Name.Length));
-					}
+									monitor.ReportResult (new MemberReference (derivedMember, derivedMember.Region, start, derivedMember.Name.Length));
+								}
+							}
+					});
 				}
-			}
+			});
+	
 		}
 	}
 }
