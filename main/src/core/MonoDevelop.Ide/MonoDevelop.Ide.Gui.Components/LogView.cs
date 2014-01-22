03973cc3af77655ce2312762f6e71ecec2876189
Jeffrey Stedfast <jeff@xamarin.com>
2014-01-22 15:48:33 -0500

    Change it back to 4k

--- main/src/core/MonoDevelop.Ide/MonoDevelop.Ide.Gui.Components/LogView.cs
+++ main/src/core/MonoDevelop.Ide/MonoDevelop.Ide.Gui.Components/LogView.cs
@@ -58,7 +58,7 @@ namespace MonoDevelop.Ide.Gui.Components
 		GLib.TimeoutHandler outputDispatcher;
 		bool outputDispatcherRunning = false;
 		
-		const int MAX_BUFFER_LENGTH = 16000 * 1024;
+		const int MAX_BUFFER_LENGTH = 4000 * 1024;
 
 		/// <summary>
 		/// The log text view allows the user to jump to the source of an error/warning
