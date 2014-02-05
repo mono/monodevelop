81cdcdce32801cff89bbf9d9b68f75520972d119
Mike Krüger <mkrueger@xamarin.com>
2014-02-05 09:43:55 +0100

    [SourceEditor] Added json indentation engine.

---
+++ main/src/addins/MonoDevelop.SourceEditor2/MonoDevelop.SourceEditor.JSon/JSonTextEditorExtension.cs
@@ -0,0 +1,131 @@
+﻿//
+// JSonTextEditorExtension.cs
+//
+// Author:
+//       Mike Krüger <mkrueger@xamarin.com>
+//
+// Copyright (c) 2014 Xamarin Inc. (http://xamarin.com)
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in
+// all copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+// THE SOFTWARE.
+using MonoDevelop.Ide.Gui.Content;
+using ICSharpCode.NRefactory.CSharp;
+using Gdk;
+using Mono.TextEditor;
+using System;
+using MonoDevelop.Core;
+using MonoDevelop.Ide.CodeCompletion;
+
+namespace MonoDevelop.SourceEditor.JSon
+{
+	class JSonTextEditorExtension : TextEditorExtension
+	{
+		CacheIndentEngine stateTracker;
+
+		TextEditorData textEditorData {
+			get {
+				return document.Editor;
+			}
+		}
+
+		public override void Initialize ()
+		{
+			base.Initialize ();
+			IStateMachineIndentEngine indentEngine;
+			indentEngine = new JSonIndentEngine (document.Editor);
+			stateTracker = new CacheIndentEngine (indentEngine);
+			document.Editor.IndentationTracker = new JSonIndentationTracker (document.Editor, stateTracker);
+		}
+
+
+		public override bool KeyPress (Key key, char keyChar, ModifierType modifier)
+		{
+			var result = base.KeyPress (key, keyChar, modifier);
+
+			if (key == Key.Return) {
+				if (textEditorData.Options.IndentStyle == IndentStyle.Virtual) {
+					if (textEditorData.GetLine (textEditorData.Caret.Line).Length == 0)
+						textEditorData.Caret.Column = textEditorData.IndentationTracker.GetVirtualIndentationColumn (textEditorData.Caret.Location);
+				} else {
+					DoReSmartIndent ();
+				}
+			}
+
+			return result;
+		}
+
+		void DoReSmartIndent ()
+		{
+			DoReSmartIndent (textEditorData.Caret.Offset);
+		}
+
+		void DoReSmartIndent (int cursor)
+		{
+			SafeUpdateIndentEngine (cursor);
+			if (stateTracker.LineBeganInsideVerbatimString || stateTracker.LineBeganInsideMultiLineComment)
+				return;
+			var line = textEditorData.Document.GetLineByOffset (cursor);
+
+			// Get context to the end of the line w/o changing the main engine's state
+			var curTracker = stateTracker.Clone ();
+			try {
+				for (int max = cursor; max < line.EndOffset; max++) {
+					curTracker.Push (textEditorData.Document.GetCharAt (max));
+				}
+			} catch (Exception e) {
+				LoggingService.LogError ("Exception during indentation", e);
+			}
+
+			int pos = line.Offset;
+			string curIndent = line.GetIndentation (textEditorData.Document);
+			int nlwsp = curIndent.Length;
+			int offset = cursor > pos + nlwsp ? cursor - (pos + nlwsp) : 0;
+			if (!stateTracker.LineBeganInsideMultiLineComment || (nlwsp < line.LengthIncludingDelimiter && textEditorData.Document.GetCharAt (line.Offset + nlwsp) == '*')) {
+				// Possibly replace the indent
+				string newIndent = curTracker.ThisLineIndent;
+				int newIndentLength = newIndent.Length;
+				if (newIndent != curIndent) {
+					if (CompletionWindowManager.IsVisible) {
+						if (pos < CompletionWindowManager.CodeCompletionContext.TriggerOffset)
+							CompletionWindowManager.CodeCompletionContext.TriggerOffset -= nlwsp;
+					}
+
+					newIndentLength = textEditorData.Replace (pos, nlwsp, newIndent);
+					textEditorData.Document.CommitLineUpdate (textEditorData.Caret.Line);
+					CompletionWindowManager.HideWindow ();
+				}
+				pos += newIndentLength;
+			} else {
+				pos += curIndent.Length;
+			}
+
+			pos += offset;
+
+			textEditorData.FixVirtualIndentation ();
+		}
+		internal void SafeUpdateIndentEngine (int offset)
+		{
+			try {
+				stateTracker.Update (offset);
+			} catch (Exception e) {
+				LoggingService.LogError ("Error while updating the indentation engine", e);
+			}
+		}
+
+	}
+}
