4d1f2ab6b605a913d361374f0e51c8ae87143ce9
Therzok <teromario@yahoo.com>
2014-02-10 17:24:08 +0200

    [VersionControl] Fix SubversionException.
    Another Subversion Operation is already in progress is an error when we have 2
    concurrent subversion operations called.
    
    StatusView and a tree refresh could collide without this patch causing that error.

--- main/src/addins/VersionControl/MonoDevelop.VersionControl/MonoDevelop.VersionControl/Repository.cs
+++ main/src/addins/VersionControl/MonoDevelop.VersionControl/MonoDevelop.VersionControl/Repository.cs
@@ -205,12 +205,17 @@ namespace MonoDevelop.VersionControl
 		public VersionInfo[] GetDirectoryVersionInfo (FilePath localDirectory, bool getRemoteStatus, bool recursive)
 		{
 			try {
-				if (recursive)
-					return OnGetDirectoryVersionInfo (localDirectory, getRemoteStatus, recursive);
+				var viList = new List<VersionInfo> ();
+				if (recursive) {
+					foreach (var subdir in Directory.GetDirectories (localDirectory))
+						viList.AddRange (OnGetDirectoryVersionInfo (subdir, getRemoteStatus, recursive));
+				}
 
 				var status = infoCache.GetDirectoryStatus (localDirectory);
-				if (status != null && !status.RequiresRefresh && (!getRemoteStatus || status.HasRemoteStatus))
-					return status.FileInfo;
+				if (status != null && !status.RequiresRefresh && (!getRemoteStatus || status.HasRemoteStatus)) {
+					viList.AddRange (status.FileInfo);
+					return viList.ToArray ();
+				}
 
 				// If there is no cached status, query it asynchronously
 				DirectoryInfoQuery q = new DirectoryInfoQuery () {
@@ -222,8 +227,10 @@ namespace MonoDevelop.VersionControl
 				// If we have a status value but the value was invalidated (RequiresRefresh == true)
 				// then we return the invalidated value while we start an async query to get the new one
 
-				if (status != null && status.RequiresRefresh && (!getRemoteStatus || status.HasRemoteStatus))
-					return status.FileInfo;
+				if (status != null && status.RequiresRefresh && (!getRemoteStatus || status.HasRemoteStatus)) {
+					viList.AddRange (status.FileInfo);
+					return viList.ToArray ();
+				}
 				return new VersionInfo[0];
 			} finally {
 				//Console.WriteLine ("GetDirectoryVersionInfo " + localDirectory + " - " + (DateTime.Now - now).TotalMilliseconds);
