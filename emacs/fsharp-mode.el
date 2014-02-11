9cdcbc56b790e0d6de965af2dd76c35a2169721b
Robin Neatherway <robin.neatherway@gmail.com>
2014-02-11 21:43:06 +0000

    Bump version numbers for release of 1.1.0

--- emacs/fsharp-mode.el
+++ emacs/fsharp-mode.el
@@ -6,7 +6,7 @@
 ;;         2010-2011 Laurent Le Brun <laurent@le-brun.eu>
 ;; Maintainer: Robin Neatherway <robin.neatherway@gmail.com>
 ;; Keywords: languages
-;; Version: 1.0.2
+;; Version: 1.1.0
 
 ;; This file is not part of GNU Emacs.
 
