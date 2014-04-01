c063a2372868cca03c32082551165973cb4da298
Lluis Sanchez <lluis@xamarin.com>
2014-04-01 13:01:48 +0200

    [Core] Improve support for shared assets projects
    Moved classes for handling shared projects to its own namespace.
    Added extensions for handling shared project references.

---
+++ main/src/core/MonoDevelop.Core/MonoDevelop.Projects.SharedAssetsProjects/SharedAssetsProject.cs
@@ -0,0 +1,128 @@
+﻿//
+// SharedProject.cs
+//
+// Author:
+//       Lluis Sanchez <lluis@xamarin.com>
+//
+// Copyright (c) 2014 Xamarin Inc
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in
+// all copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+// THE SOFTWARE.
+
+using System;
+using System.Linq;
+using System.Collections.Generic;
+using MonoDevelop.Core;
+using System.IO;
+
+namespace MonoDevelop.Projects.SharedAssetsProjects
+{
+	public class SharedAssetsProject: Project
+	{
+		Solution currentSolution;
+
+		public SharedAssetsProject ()
+		{
+		}
+
+		public string DefaultNamespace { get; set; }
+
+		public override string ProjectType {
+			get {
+				return "SharedAssets";
+			}
+		}
+
+		public override string[] SupportedLanguages {
+			get {
+				return new [] {"C#"};
+			}
+		}
+
+		protected override BuildResult OnBuild (MonoDevelop.Core.IProgressMonitor monitor, ConfigurationSelector configuration)
+		{
+			return new BuildResult ();
+		}
+
+		internal protected override bool OnGetCanRunTarget (string target, ConfigurationSelector configuration)
+		{
+			return false;
+		}
+
+		protected override void OnBoundToSolution ()
+		{
+			if (currentSolution != null) {
+				currentSolution.ReferenceAddedToProject -= HandleReferenceAddedToProject;
+				currentSolution.ReferenceRemovedFromProject -= HandleReferenceRemovedFromProject;
+			}
+
+			base.OnBoundToSolution ();
+
+			ParentSolution.ReferenceAddedToProject += HandleReferenceAddedToProject;
+			ParentSolution.ReferenceRemovedFromProject += HandleReferenceRemovedFromProject;
+			currentSolution = ParentSolution;
+		}
+
+		public override void Dispose ()
+		{
+			base.Dispose ();
+			if (currentSolution != null) {
+				currentSolution.ReferenceAddedToProject -= HandleReferenceAddedToProject;
+				currentSolution.ReferenceRemovedFromProject -= HandleReferenceRemovedFromProject;
+			}
+		}
+
+		void HandleReferenceRemovedFromProject (object sender, ProjectReferenceEventArgs e)
+		{
+			if (e.ProjectReference.ReferenceType == ReferenceType.Project && e.ProjectReference.Reference == Name) {
+				foreach (var f in Files) {
+					var pf = e.Project.GetProjectFile (f.FilePath);
+					if ((pf.Flags & ProjectItemFlags.DontPersist) != 0)
+						e.Project.Files.Remove (pf.FilePath);
+				}
+			}
+		}
+
+		void HandleReferenceAddedToProject (object sender, ProjectReferenceEventArgs e)
+		{
+			if (e.ProjectReference.ReferenceType == ReferenceType.Project && e.ProjectReference.Reference == Name) {
+				e.ProjectReference.Flags = ProjectItemFlags.DontPersist;
+				foreach (var f in Files) {
+					var cf = (ProjectFile) f.Clone ();
+					cf.Flags |= ProjectItemFlags.DontPersist | ProjectItemFlags.Hidden;
+					e.Project.Files.Add (cf);
+					e.ProjectReference.SetItemsProjectPath (Path.ChangeExtension (FileName, ".projitems"));
+				}
+			}
+		}
+	}
+
+	internal static class SharedAssetsProjectExtensions
+	{
+		public static string GetItemsProjectPath (this ProjectReference r)
+		{
+			return (string) r.ExtendedProperties ["MSBuild.SharedAssetsProject"];
+		}
+
+		public static void SetItemsProjectPath (this ProjectReference r, string path)
+		{
+			r.ExtendedProperties ["MSBuild.SharedAssetsProject"] = path;
+		}
+	}
+}
+
---
+++ main/src/core/MonoDevelop.Core/MonoDevelop.Projects.SharedAssetsProjects/SharedAssetsProjectMSBuildHandler.cs
@@ -0,0 +1,129 @@
+﻿//
+// SharedProjectMSBuildHandler.cs
+//
+// Author:
+//       Lluis Sanchez <lluis@xamarin.com>
+//
+// Copyright (c) 2014 Xamarin Inc
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in
+// all copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+// THE SOFTWARE.
+using System;
+using System.Linq;
+using System.Xml;
+using System.IO;
+using MonoDevelop.Core;
+using System.Collections.Generic;
+using MonoDevelop.Projects.Formats.MSBuild;
+
+namespace MonoDevelop.Projects.SharedAssetsProjects
+{
+	class SharedAssetsProjectMSBuildHandler: MSBuildProjectHandler
+	{
+		string projitemsFile;
+
+		public SharedAssetsProjectMSBuildHandler ()
+		{
+		}
+
+		protected override void LoadProject (IProgressMonitor monitor, MSBuildProject msproject)
+		{
+			var doc = msproject.Document;
+			projitemsFile = null;
+			foreach (var no in doc.DocumentElement.ChildNodes) {
+				var im = no as XmlElement;
+				if (im != null && im.LocalName == "Import" && im.GetAttribute ("Label") == "Shared") {
+					projitemsFile = im.GetAttribute ("Project");
+					break;
+				}
+			}
+			if (projitemsFile == null)
+				return;
+
+			projitemsFile = Path.Combine (Path.GetDirectoryName (msproject.FileName), projitemsFile);
+
+			MSBuildProject p = new MSBuildProject ();
+			p.Load (projitemsFile);
+
+			MSBuildSerializer ser = CreateSerializer ();
+			ser.SerializationContext.BaseFile = EntityItem.FileName;
+			ser.SerializationContext.ProgressMonitor = monitor;
+
+			Item.SetItemHandler (this);
+
+			var cp = p.PropertyGroups.FirstOrDefault (g => g.Label == "Configuration");
+			if (cp != null)
+				((SharedAssetsProject)EntityItem).DefaultNamespace = cp.GetPropertyValue ("Import_RootNamespace");
+
+			LoadProjectItems (p, ser, ProjectItemFlags.None);
+		}
+
+		protected override MSBuildProject SaveProject (IProgressMonitor monitor)
+		{
+			MSBuildSerializer ser = CreateSerializer ();
+			ser.SerializationContext.BaseFile = EntityItem.FileName;
+			ser.SerializationContext.ProgressMonitor = monitor;
+
+			MSBuildProject projitemsProject = new MSBuildProject ();
+			MSBuildProject msproject = new MSBuildProject ();
+
+			var newProject = EntityItem.FileName == null || !File.Exists (EntityItem.FileName);
+			if (newProject) {
+				var import = msproject.AddNewImport (@"$(MSBuildExtensionsPath)\$(MSBuildToolsVersion)\Microsoft.Common.props");
+				import.Condition = @"Exists('$(MSBuildExtensionsPath)\$(MSBuildToolsVersion)\Microsoft.Common.props')";
+				msproject.AddNewImport (@"$(MSBuildExtensionsPath32)\Microsoft\VisualStudio\v$(VisualStudioVersion)\CodeSharing\Microsoft.CodeSharing.Common.Default.props");
+				msproject.AddNewImport (@"$(MSBuildExtensionsPath32)\Microsoft\VisualStudio\v$(VisualStudioVersion)\CodeSharing\Microsoft.CodeSharing.Common.props");
+				import = msproject.AddNewImport (Path.ChangeExtension (EntityItem.FileName.FileName, ".projitems"));
+				import.Label = "Shared";
+				msproject.AddNewImport (@"$(MSBuildExtensionsPath32)\Microsoft\VisualStudio\v$(VisualStudioVersion)\CodeSharing\Microsoft.CodeSharing.CSharp.targets");
+			} else {
+				msproject.Load (EntityItem.FileName);
+			}
+
+			// having no ToolsVersion is equivalent to 2.0, roundtrip that correctly
+			if (ToolsVersion != "2.0")
+				msproject.ToolsVersion = ToolsVersion;
+			else if (string.IsNullOrEmpty (msproject.ToolsVersion))
+				msproject.ToolsVersion = null;
+			else
+				msproject.ToolsVersion = "2.0";
+
+			if (projitemsFile == null)
+				projitemsFile = Path.ChangeExtension (EntityItem.FileName, ".projitems");
+			if (File.Exists (projitemsFile)) {
+				projitemsProject.Load (projitemsFile);
+			} else {
+				var grp = projitemsProject.AddNewPropertyGroup (true);
+				grp.SetPropertyValue ("MSBuildAllProjects", "$(MSBuildAllProjects);$(MSBuildThisFileFullPath)", false);
+				grp.SetPropertyValue ("HasSharedItems", "true", false);
+				grp.SetPropertyValue ("SharedGUID", EntityItem.ItemId, false);
+
+				grp = projitemsProject.AddNewPropertyGroup (true);
+				grp.Label = "Configuration";
+				grp.SetPropertyValue ("Import_RootNamespace", ((SharedAssetsProject)EntityItem).DefaultNamespace, false);
+			}
+
+			SaveProjectItems (monitor, new MSBuildFileFormatVS12 (), ser, projitemsProject, "$(MSBuildThisFileDirectory)");
+
+			projitemsProject.Save (projitemsFile);
+
+			return msproject;
+		}
+	}
+}
+
