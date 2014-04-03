360db26da80db1eba07da243e744e3bea37d1a10
Jeffrey Stedfast <jeff@xamarin.com>
2014-04-03 17:22:09 -0400

    [Debugger] Make the DebugValueTooltipProvider more extensible for other languages

--- main/src/addins/MonoDevelop.SourceEditor2/MonoDevelop.SourceEditor/DebugValueTooltipProvider.cs
+++ main/src/addins/MonoDevelop.SourceEditor2/MonoDevelop.SourceEditor/DebugValueTooltipProvider.cs
@@ -78,144 +78,6 @@ namespace MonoDevelop.SourceEditor
 
 		#region ITooltipProvider implementation
 
-		static string GetIdentifierName (TextEditorData editor, Identifier id, out int startOffset)
-		{
-			startOffset = editor.LocationToOffset (id.StartLocation.Line, id.StartLocation.Column);
-
-			return editor.GetTextBetween (id.StartLocation, id.EndLocation);
-		}
-
-		public static string ResolveExpression (TextEditorData editor, ResolveResult result, AstNode node, out int startOffset)
-		{
-			//Console.WriteLine ("result is a {0}", result.GetType ().Name);
-			startOffset = -1;
-
-			if (result is NamespaceResolveResult ||
-			    result is ConversionResolveResult ||
-			    result is ConstantResolveResult ||
-			    result is ForEachResolveResult ||
-			    result is TypeIsResolveResult ||
-			    result is TypeOfResolveResult ||
-			    result is ErrorResolveResult)
-				return null;
-
-			if (result.IsCompileTimeConstant)
-				return null;
-
-			startOffset = editor.LocationToOffset (node.StartLocation.Line, node.StartLocation.Column);
-
-			if (result is InvocationResolveResult) {
-				var ir = (InvocationResolveResult) result;
-				if (ir.Member.Name == ".ctor") {
-					// if the user is hovering over something like "new Abc (...)", we want to show them type information for Abc
-					return ir.Member.DeclaringType.FullName;
-				}
-
-				// do not support general method invocation for tooltips because it could cause side-effects
-				return null;
-			} else if (result is LocalResolveResult) {
-				if (node is ParameterDeclaration) {
-					// user is hovering over a method parameter, but we don't want to include the parameter type
-					var param = (ParameterDeclaration) node;
-
-					return GetIdentifierName (editor, param.NameToken, out startOffset);
-				}
-
-				if (node is VariableInitializer) {
-					// user is hovering over something like "int fubar = 5;", but we don't want the expression to include the " = 5"
-					var variable = (VariableInitializer) node;
-
-					return GetIdentifierName (editor, variable.NameToken, out startOffset);
-				}
-			} else if (result is MemberResolveResult) {
-				var mr = (MemberResolveResult) result;
-
-				if (node is PropertyDeclaration) {
-					var prop = (PropertyDeclaration) node;
-					var name = GetIdentifierName (editor, prop.NameToken, out startOffset);
-
-					// if the property is static, then we want to return "Full.TypeName.Property"
-					if (prop.Modifiers.HasFlag (Modifiers.Static))
-						return mr.Member.DeclaringType.FullName + "." + name;
-
-					// otherwise we want to return "this.Property" so that it won't conflict with anything else in the local scope
-					return "this." + name;
-				}
-
-				if (node is FieldDeclaration) {
-					var field = (FieldDeclaration) node;
-					var name = GetIdentifierName (editor, field.NameToken, out startOffset);
-
-					// if the field is static, then we want to return "Full.TypeName.Field"
-					if (field.Modifiers.HasFlag (Modifiers.Static))
-						return mr.Member.DeclaringType.FullName + "." + name;
-
-					// otherwise we want to return "this.Field" so that it won't conflict with anything else in the local scope
-					return "this." + name;
-				}
-
-				if (node is VariableInitializer) {
-					// user is hovering over a field declaration that includes initialization
-					var variable = (VariableInitializer) node;
-					var name = GetIdentifierName (editor, variable.NameToken, out startOffset);
-
-					// walk up the AST to find the FieldDeclaration so that we can determine if it is static or not
-					var field = variable.GetParent<FieldDeclaration> ();
-
-					// if the field is static, then we want to return "Full.TypeName.Field"
-					if (field.Modifiers.HasFlag (Modifiers.Static))
-						return mr.Member.DeclaringType.FullName + "." + name;
-
-					// otherwise we want to return "this.Field" so that it won't conflict with anything else in the local scope
-					return "this." + name;
-				}
-
-				if (node is NamedExpression) {
-					// user is hovering over 'Property' in an expression like: var fubar = new Fubar () { Property = baz };
-					var variable = node.GetParent<VariableInitializer> ();
-					if (variable != null) {
-						var variableName = GetIdentifierName (editor, variable.NameToken, out startOffset);
-						var name = GetIdentifierName (editor, ((NamedExpression) node).NameToken, out startOffset);
-
-						return variableName + "." + name;
-					}
-				}
-			} else if (result is TypeResolveResult) {
-				return ((TypeResolveResult) result).Type.FullName;
-			}
-
-			return editor.GetTextBetween (node.StartLocation, node.EndLocation);
-		}
-
-		static bool TryResolveAt (Document doc, DocumentLocation loc, out ResolveResult result, out AstNode node)
-		{
-			if (doc == null)
-				throw new ArgumentNullException ("doc");
-
-			result = null;
-			node = null;
-
-			var parsedDocument = doc.ParsedDocument;
-			if (parsedDocument == null)
-				return false;
-
-			var unit = parsedDocument.GetAst<SyntaxTree> ();
-			var parsedFile = parsedDocument.ParsedFile as CSharpUnresolvedFile;
-
-			if (unit == null || parsedFile == null)
-				return false;
-
-			try {
-				result = ResolveAtLocation.Resolve (new Lazy<ICompilation> (() => doc.Compilation), parsedFile, unit, loc, out node);
-				if (result == null || node is Statement)
-					return false;
-			} catch {
-				return false;
-			}
-
-			return true;
-		}
-
 		public override TooltipItem GetItem (TextEditor editor, int offset)
 		{
 			if (offset >= editor.Document.TextLength)
@@ -240,14 +102,11 @@ namespace MonoDevelop.SourceEditor
 				if (doc == null || doc.ParsedDocument == null)
 					return null;
 
-				ResolveResult result;
-				AstNode node;
-
-				var loc = editor.OffsetToLocation (offset);
-				if (!TryResolveAt (doc, loc, out result, out node))
+				var resolver = doc.GetContent<IDebuggerExpressionResolver> ();
+				if (resolver == null)
 					return null;
 
-				expression = ResolveExpression (editor.GetTextEditorData (), result, node, out startOffset);
+				expression = resolver.ResolveExpression (editor.GetTextEditorData (), doc, offset, out startOffset);
 			}
 			
 			if (string.IsNullOrEmpty (expression))
