ca97a99d789558623da22cc481d5f1ef902bb534
Michael Hutchinson <m.j.hutchinson@gmail.com>
2014-04-03 18:22:15 -0400

    Add Windows XP specific gtkrc

--- main/src/core/MonoDevelop.Ide/MonoDevelop.Ide/IdeStartup.cs
+++ main/src/core/MonoDevelop.Ide/MonoDevelop.Ide/IdeStartup.cs
@@ -347,6 +347,8 @@ namespace MonoDevelop.Ide
 				var gtkrc = "gtkrc";
 				if (Platform.IsWindows) {
 					gtkrc += ".win32";
+					if (Environment.OSVersion.Version.Major < 6)
+						gtkrc += "-xp";
 				} else if (Platform.IsMac) {
 					gtkrc += ".mac";
 				}
