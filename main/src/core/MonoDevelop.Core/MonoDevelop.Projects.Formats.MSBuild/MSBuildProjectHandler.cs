55621f20f57e739cc53e6ca289e39c6fa29248ec
Lluis Sanchez <lluis@xamarin.com>
2014-04-04 19:21:46 +0200

    Fix build
    Remove duplicated method

--- main/src/core/MonoDevelop.Core/MonoDevelop.Projects.Formats.MSBuild/MSBuildProjectHandler.cs
+++ main/src/core/MonoDevelop.Core/MonoDevelop.Projects.Formats.MSBuild/MSBuildProjectHandler.cs
@@ -1651,11 +1651,6 @@ namespace MonoDevelop.Projects.Formats.MSBuild
 			}
 		}
 
-		IEnumerable<MSBuildExtension> GetMSBuildExtensions ()
-		{
-			return AddinManager.GetExtensionObjects<MSBuildExtension> ("/MonoDevelop/ProjectModel/MSBuildExtensions");
-		}
-
 		void ReadBuildItemMetadata (DataSerializer ser, MSBuildItem buildItem, object dataItem, Type extendedType)
 		{
 			DataItem ditem = new DataItem ();
