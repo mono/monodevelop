mono/monodevelop

commit 5c4da579f6ab35c5c7fd2735f3f4f617e93b43f7

Fixed 'Bug 12911 - NUnit addin munges testcases containing data with dots'.

main/src/addins/NUnit/NUnitRunner/NUnitTestRunner.cs
@@ -119,7 +119,9 @@ namespace MonoDevelop.NUnit.External
 			// The name of inherited tests include the base class name as prefix.
 			// That prefix has to be removed
 			string tname = test.TestName.Name;
-			int i = tname.LastIndexOf ('.');
+			// Find the last index of the dot character that is not a part of the test parameters
+			int j = tname.IndexOf ('(');
+			int i = tname.LastIndexOf ('.', (j == -1) ? (tname.Length - 1) : j);
 			if (i != -1)
 				tname = tname.Substring (i + 1);
 			if (test.FixtureType != null) {

commit ed5af81c38d7689c4e94ef6a20f5b503718bdd66

Fix regression introduced in 5c4da579

main/src/addins/NUnit/NUnitRunner/NUnitTestRunner.cs
@@ -121,7 +121,7 @@ namespace MonoDevelop.NUnit.External
 			string tname = test.TestName.Name;
 			// Find the last index of the dot character that is not a part of the test parameters
 			int j = tname.IndexOf ('(');
-			int i = tname.LastIndexOf ('.', (j == -1) ? (tname.Length - 1) : j);
+			int i = tname.LastIndexOf ('.', (j == -1) ? 0 : j);
 			if (i != -1)
 				tname = tname.Substring (i + 1);
 			if (test.FixtureType != null) {