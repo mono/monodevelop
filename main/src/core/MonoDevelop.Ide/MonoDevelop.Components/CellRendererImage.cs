d647dd2d30d78aaa89dc9726c28f700eaba0c7de
Lluis Sanchez <lluis@xamarin.com>
2014-04-08 13:48:59 +0200

    [Ide] Fix icon padding in new project dialog

--- main/src/core/MonoDevelop.Ide/MonoDevelop.Components/CellRendererImage.cs
+++ main/src/core/MonoDevelop.Ide/MonoDevelop.Components/CellRendererImage.cs
@@ -122,37 +122,39 @@ namespace MonoDevelop.Components
 
 		protected override void Render (Gdk.Drawable window, Gtk.Widget widget, Gdk.Rectangle background_area, Gdk.Rectangle cell_area, Gdk.Rectangle expose_area, Gtk.CellRendererState flags)
 		{
-			var img = GetImate ();
+			var img = GetImage ();
 			if (img == null)
 				return;
 
 			using (var ctx = Gdk.CairoHelper.Create (window)) {
-				var x = Xpad + cell_area.X + cell_area.Width / 2 - (int)(img.Width / 2);
-				var y = Ypad + cell_area.Y + cell_area.Height / 2 - (int)(img.Height / 2);
+				var x = cell_area.X + cell_area.Width / 2 - (int)(img.Width / 2);
+				var y = cell_area.Y + cell_area.Height / 2 - (int)(img.Height / 2);
 				ctx.DrawImage (widget, img, x, y);
 			}
 		}
 
 		protected void GetImageInfo (Gdk.Rectangle cell_area, out Image img, out int x, out int y)
 		{
-			img = GetImate ();
-			x = (int)(Xpad + cell_area.X + cell_area.Width / 2 - (int)(img.Width / 2));
-			y = (int)(Ypad + cell_area.Y + cell_area.Height / 2 - (int)(img.Height / 2));
+			img = GetImage ();
+			x = (int)(cell_area.X + cell_area.Width / 2 - (int)(img.Width / 2));
+			y = (int)(cell_area.Y + cell_area.Height / 2 - (int)(img.Height / 2));
 		}
 
 		public override void GetSize (Gtk.Widget widget, ref Gdk.Rectangle cell_area, out int x_offset, out int y_offset, out int width, out int height)
 		{
-			var img = GetImate ();
+			var img = GetImage ();
 			if (img != null) {
 				width = (int)img.Width;
 				height = (int)img.Height;
 			} else
 				width = height = 0;
 
+			width += (int)Xpad * 2;
+			height += (int)Ypad * 2;
 			x_offset = y_offset = 0;
 		}
 
-		Xwt.Drawing.Image GetImate ()
+		Xwt.Drawing.Image GetImage ()
 		{
 			if (icon.IsNull)
 				return IsExpanded ? (imageOpen ?? image) : (imageClosed ?? image);
