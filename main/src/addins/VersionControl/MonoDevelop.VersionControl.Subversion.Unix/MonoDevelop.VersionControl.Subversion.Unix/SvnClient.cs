83947f60f36b26ed17a92d1e796edf003619ebd6
Marius Ungureanu <marius.ungureanu@xamarin.com>
2014-04-28 21:59:01 +0300

    [Svn] Fix Unix lock permissions.

--- main/src/addins/VersionControl/MonoDevelop.VersionControl.Subversion.Unix/MonoDevelop.VersionControl.Subversion.Unix/SvnClient.cs
+++ main/src/addins/VersionControl/MonoDevelop.VersionControl.Subversion.Unix/MonoDevelop.VersionControl.Subversion.Unix/SvnClient.cs
@@ -1106,6 +1106,36 @@ namespace MonoDevelop.VersionControl.Subversion.Unix
 			}
 		}
 
+		public override bool HasNeedLock (FilePath file)
+		{
+			IntPtr hash_item, hash_name, hash_val;
+			int length;
+			IntPtr result;
+			IntPtr props_ptr = IntPtr.Zero;
+			StringBuilder props = new StringBuilder ();
+			string new_path;
+			LibSvnClient.svn_string_t new_props;
+			LibSvnClient.Rev rev = LibSvnClient.Rev.Working;
+
+			var localpool = TryStartOperation (null);
+			try {
+				new_path = NormalizePath (file, localpool);
+				CheckError (svn.client_propget (out result, "svn:needs-lock", new_path,
+					ref rev, false, ctx, localpool));
+				hash_item = apr.hash_first (localpool, result);
+				while (hash_item != IntPtr.Zero) {
+					apr.hash_this (hash_item, out hash_name, out length, out hash_val);
+					new_props = (LibSvnClient.svn_string_t) Marshal.PtrToStructure (hash_val, typeof (LibSvnClient.svn_string_t));
+					props.Append (Marshal.PtrToStringAnsi (new_props.data));
+					hash_item = apr.hash_next (hash_item);
+				}
+				return props.Length != 0;
+			} finally {
+				apr.pool_destroy (localpool);
+				TryEndOperation ();
+			}
+		}
+
 		public override string GetTextBase (string sourcefile)
 		{
 			MemoryStream data = new MemoryStream ();
