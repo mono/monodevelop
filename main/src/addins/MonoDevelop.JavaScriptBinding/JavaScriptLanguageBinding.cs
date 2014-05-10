d124cf3cd6101ffcccb39953c44e04b63fbf285f
Harsimran Bath <harsimranb@outlook.com>
2014-05-10 01:17:37 -0700

    Added language binding for javascript

---
+++ main/src/addins/MonoDevelop.JavaScriptBinding/JavaScriptLanguageBinding.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using MonoDevelop.Projects;
+
+namespace MonoDevelop.JavaScript
+{
+	class JavaScriptLanguageBinding : ILanguageBinding
+	{
+		public string Language {
+			get { return "JavaScript"; }
+		}
+
+		public string SingleLineCommentTag {
+			get { return "//"; }
+		}
+
+		public string BlockCommentStartTag {
+			get { return "/*"; }
+		}
+
+		public string BlockCommentEndTag {
+			get { return "*/"; }
+		}
+
+		public bool IsSourceCodeFile (Core.FilePath fileName)
+		{
+			return fileName.ToString ().EndsWith (".js", StringComparison.OrdinalIgnoreCase);
+		}
+
+		public Core.FilePath GetFileName (Core.FilePath fileNameWithoutExtension)
+		{
+			return fileNameWithoutExtension + ".js";
+		}
+	}
+}
