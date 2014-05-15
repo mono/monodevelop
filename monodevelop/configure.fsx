f931c98c94cb19f8bbf10e17154d2204720fc804
7sharp9 <kukulcanenator@googlemail.com>
2014-05-15 12:14:50 +0100

    bumped version

--- monodevelop/configure.fsx
+++ monodevelop/configure.fsx
@@ -10,7 +10,7 @@ open System.IO
 open System.Diagnostics
 open System.Text.RegularExpressions
 
-let FSharpVersion = "5.0.1"
+let FSharpVersion = "5.0.2"
 
 let UnixPaths = 
     [ "/usr/lib/monodevelop"
