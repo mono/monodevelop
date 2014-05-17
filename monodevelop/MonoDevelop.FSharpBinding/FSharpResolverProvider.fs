ccc4a40e00597625e4dc4c09d17f5e3e00bf9d66
7sharp9 <kukulcanenator@googlemail.com>
2014-05-17 21:09:23 +0100

    Split TooltipProvider into its own file and renamed from FSharpLanguageItemTooltipProvider to FSharpTooltipProvider Added LoggingService based logging for ResolverProvider and TooltipProvider

--- monodevelop/MonoDevelop.FSharpBinding/FSharpResolverProvider.fs
+++ monodevelop/MonoDevelop.FSharpBinding/FSharpResolverProvider.fs
@@ -1,149 +1,21 @@
 ﻿// --------------------------------------------------------------------------------------
-// Provides tool tips with F# hints for MonoDevelop
-// (this file implements MonoDevelop interfaces and calls 'LanguageService')
+// Resolves locations to NRefactory Symbols and is used for Highlight usages and goto declaration
 // --------------------------------------------------------------------------------------
+
 namespace MonoDevelop.FSharp
 
 open System
-open System.Diagnostics
-open System.Text
-open System.Linq
-open System.Collections.Generic
-open System.Threading
 open FSharp.CompilerBinding
-
-open Mono.TextEditor
 open MonoDevelop.Core
-open MonoDevelop.Ide
-open MonoDevelop.Ide.CodeCompletion
 open MonoDevelop.Ide.Gui
 open MonoDevelop.Ide.Gui.Content
-open MonoDevelop.Ide.TypeSystem
-open MonoDevelop.Refactoring
-
 open ICSharpCode.NRefactory
 open ICSharpCode.NRefactory.Semantics
 open ICSharpCode.NRefactory.TypeSystem
-
 open Microsoft.FSharp.Compiler.SourceCodeServices
-
-/// Resolves locations to tooltip items, and orchestrates their display.
-///
-/// We resolve language items to an NRefactory symbol.
-type FSharpLanguageItemTooltipProvider() = 
-    inherit Mono.TextEditor.TooltipProvider()
-
-    // Keep the last result and tooltip window cached
-    let mutable lastResult = None : TooltipItem option
-    static let mutable lastWindow = None
-
-    let killTooltipWindow() =
-       match lastWindow with
-       | Some(w:TooltipInformationWindow) -> w.Destroy()
-       | None -> ()
-
-    override x.GetItem (editor, offset) =
-      try
-        let extEditor = editor :?> MonoDevelop.SourceEditor.ExtensibleTextEditor 
-        let docText = editor.Text
-        if docText = null || offset >= docText.Length || offset < 0 then null else
-        let proj = extEditor.Project :?> MonoDevelop.Projects.DotNetProject
-        let files = CompilerArguments.getSourceFiles(extEditor.Project.Items) |> Array.ofList
-        let args = CompilerArguments.getArgumentsFromProject(proj)
-        let framework = CompilerArguments.getTargetFramework(proj.TargetFramework.Id)
-        let tyResOpt =
-            MDLanguageService.Instance.GetTypedParseResultWithTimeout
-                 (extEditor.Project.FileName.ToString(),
-                  editor.FileName, 
-                  docText, 
-                  files,
-                  args,
-                  AllowStaleResults.MatchingSource,
-                  ServiceSettings.blockingTimeout,
-                  framework) |> Async.RunSynchronously
-        Debug.WriteLine (sprintf "TooltipProvider: Getting tool tip")
-        match tyResOpt with
-        | None -> null
-        | Some tyRes ->
-        // Get tool-tip from the language service
-        let line, col, lineStr = MonoDevelop.getLineInfoFromOffset(offset, editor.Document)
-        let tip = tyRes.GetToolTip(line, col, lineStr) |> Async.RunSynchronously
-        match tip with
-        | None -> null
-        | Some (ToolTipText(elems),_) when elems |> List.forall (function ToolTipElementNone -> true | _ -> false) -> 
-            Debug.WriteLine ("TooltipProvider: No data found")
-            null
-        | Some(tiptext,(col1,col2)) -> 
-            Debug.WriteLine("TooltipProvider: Got data")
-            //check to see if the last result is the same tooltipitem, if so return the previous tooltipitem
-            match lastResult with
-            | Some(tooltipItem) when
-                tooltipItem.Item :?> ToolTipText = tiptext && 
-                tooltipItem.ItemSegment = TextSegment(editor.LocationToOffset (line, col1 + 1), col2 - col1) -> tooltipItem
-            //If theres no match or previous cached result generate a new tooltipitem
-            | Some(_) | None -> 
-                let line = editor.Document.OffsetToLineNumber offset
-                let segment = TextSegment(editor.LocationToOffset (line, col1 + 1), col2 - col1)
-                let tooltipItem = TooltipItem (tiptext, segment)
-                lastResult <- Some(tooltipItem)
-                tooltipItem
-      with x -> null
-
-    override x.CreateTooltipWindow (editor, offset, modifierState, item) = 
-        let doc = IdeApp.Workbench.ActiveDocument
-        if (doc = null) then null else
-        match item.Item with 
-        | :? ToolTipText as titem ->
-            let tooltip = TipFormatter.formatTip(titem)
-            let (signature, comment) = 
-                match tooltip with
-                | [signature,comment] -> signature,comment
-                //With multiple tips just take the head.  
-                //This shouldnt happen anyway as we split them in the resolver provider
-                | multiple -> multiple |> List.head
-            //dont show a tooltip if there is no content
-            if String.IsNullOrEmpty(signature) then null 
-            else            
-                let result = new TooltipInformationWindow(ShowArrow = true)
-                let toolTipInfo = new TooltipInformation(SignatureMarkup = signature)
-                if not (String.IsNullOrEmpty(comment)) then toolTipInfo.SummaryMarkup <- comment
-                result.AddOverload(toolTipInfo)
-                result.RepositionWindow ()                  
-                result :> _
-        | _ -> Debug.WriteLine("** not a FSharpLocalResolveResult!")
-               null
-    
-    override x.ShowTooltipWindow (editor, offset, modifierState, mouseX, mouseY, item) =
-        match (lastResult, lastWindow) with
-        | Some(lastRes), Some(lastWin) when item.Item = lastRes.Item && lastWin.IsRealized ->
-            lastWin :> _                   
-        | _ -> killTooltipWindow()
-               match x.CreateTooltipWindow (editor, offset, modifierState, item) with
-               | :? TooltipInformationWindow as tipWindow ->
-                   let positionWidget = editor.TextArea
-                   let region = item.ItemSegment.GetRegion(editor.Document)
-                   let p1, p2 = editor.LocationToPoint(region.Begin), editor.LocationToPoint(region.End)
-                   let caret = Gdk.Rectangle (int p1.X - positionWidget.Allocation.X, 
-                                              int p2.Y - positionWidget.Allocation.Y, 
-                                              int (p2.X - p1.X), 
-                                              int editor.LineHeight)
-                   //For debug this is usful for visualising the tooltip location
-                   // editor.SetSelection(item.ItemSegment.Offset, item.ItemSegment.EndOffset)
-               
-                   tipWindow.ShowPopup(positionWidget, caret, MonoDevelop.Components.PopupPosition.Top)
-                   tipWindow.EnterNotifyEvent.Add(fun _ -> editor.HideTooltip (false))
-                   //cache last window shown
-                   lastWindow <- Some(tipWindow)
-                   lastResult <- Some(item)
-                   tipWindow :> _
-               | _ -> null
-            
-    interface IDisposable with
-        member x.Dispose() = killTooltipWindow()
 
 /// Resolves locations to NRefactory symbols and ResolveResult objects.
 type FSharpResolverProvider() =
-  do Debug.WriteLine ("Resolver: Creating FSharpResolverProvider")
 
   interface ITextEditorResolverProvider with
   
@@ -151,12 +23,12 @@ type FSharpResolverProvider() =
     member x.GetLanguageItem(doc:Document, offset:int, region:DomRegion byref) : ResolveResult =
 
       try 
-        Debug.WriteLine ("Resolver: In GetLanguageItem")
+        LoggingService.LogInfo "ResolverProvider: In GetLanguageItem"
         if doc.Editor = null || doc.Editor.Document = null then null else
         let docText = doc.Editor.Text
         if docText = null || offset >= docText.Length || offset < 0 then null else
 
-        Debug.WriteLine("Resolver: Getting results of type checking")
+        LoggingService.LogInfo "ResolverProvider: Getting results of type checking"
         // Try to get typed result - with the specified timeout
         let proj = doc.Project :?> MonoDevelop.Projects.DotNetProject
         let files = CompilerArguments.getSourceFiles(doc.Project.Items) |> Array.ofList
@@ -173,46 +45,45 @@ type FSharpResolverProvider() =
                   ServiceSettings.blockingTimeout,
                   framework) |> Async.RunSynchronously
 
-        Debug.WriteLine("getting declaration location...")
+        LoggingService.LogInfo "ResolverProvider: Getting declaration location"
         match tyResOpt with
         | None -> null
         | Some tyRes ->
         // Get the declaration location from the language service
         let line, col, lineStr = MonoDevelop.getLineInfoFromOffset(offset, doc.Editor.Document)
 
-        let fsSymbolOpt = tyRes.GetSymbol(line, col, lineStr) |> Async.RunSynchronously
+        let fsSymbolUse = tyRes.GetSymbol(line, col, lineStr) |> Async.RunSynchronously
 
-        match fsSymbolOpt with 
+        match fsSymbolUse with 
         | None ->  null
         | Some fsSymbolUse ->
-            let loc = tyRes.GetDeclarationLocation(line, col, lineStr) |> Async.RunSynchronously
+            let findDeclarationResult = tyRes.GetDeclarationLocation(line, col, lineStr) |> Async.RunSynchronously
             let lastIdent = 
                 match FSharp.CompilerBinding.Parsing.findLongIdents(col, lineStr) with 
                 | Some(_, identIsland) -> Seq.last identIsland
                 | None -> ""
-            let reg = 
-                match loc with
+            let domRegion = 
+                match findDeclarationResult with
                 | FindDeclResult.DeclFound(m) -> 
-                    Debug.WriteLine("found, line = {0}, col = {1}, file = {2}", m.StartLine, m.StartColumn, m.FileName)
+                    LoggingService.LogInfo("ResolverProvider: found, line = {0}, col = {1}, file = {2}", m.StartLine, m.StartColumn, m.FileName)
                     DomRegion(m.FileName,m.StartLine,m.StartColumn+1)
                 | FindDeclResult.DeclNotFound(notfound) -> 
                     match notfound with 
-                    | FindDeclFailureReason.Unknown           -> Debug.WriteLine("DeclNotFound: Unknown")
-                    | FindDeclFailureReason.NoSourceCode      -> Debug.WriteLine("DeclNotFound: No Source Code")
-                    | FindDeclFailureReason.ProvidedType(t)   -> Debug.WriteLine("DeclNotFound: ProvidedType")
-                    | FindDeclFailureReason.ProvidedMember(m) -> Debug.WriteLine("DeclNotFound: ProvidedMember")
+                    | FindDeclFailureReason.Unknown           -> LoggingService.LogWarning "Declaration not found: Unknown"
+                    | FindDeclFailureReason.NoSourceCode      -> LoggingService.LogWarning "Declaration not found: No Source Code"
+                    | FindDeclFailureReason.ProvidedType(t)   -> LoggingService.LogWarning("Declaration not found: ProvidedType {0}", t)
+                    | FindDeclFailureReason.ProvidedMember(m) -> LoggingService.LogWarning("Declaration not found: ProvidedMember {0}", m)
                     DomRegion.Empty
-            region <- reg
+            region <- domRegion
             // This is the NRefactory symbol for the item - the Region is used for goto-definition
-            let resolveResult = NRefactory.createResolveResult(doc.ProjectContent, fsSymbolUse.Symbol, lastIdent, reg)
+            let resolveResult = NRefactory.createResolveResult(doc.ProjectContent, fsSymbolUse.Symbol, lastIdent, domRegion)
             resolveResult
 
       with exn -> 
-        Debug.WriteLine (sprintf "Resolver: Exception: '%s'" (exn.ToString()))
+        LoggingService.LogError("ResolverProvider: Exception while retrieving resolve result", exn)
         null
 
     member x.GetLanguageItem(doc:Document, offset:int, identifier:string) : ResolveResult =
-      do Debug.WriteLine (sprintf "Resolver: in GetLanguageItem#2")
       let (result, region) = (x :> ITextEditorResolverProvider).GetLanguageItem(doc, offset)
       result
 
---
+++ monodevelop/MonoDevelop.FSharpBinding/FSharpTooltipProvider.fs
@@ -0,0 +1,127 @@
+﻿// --------------------------------------------------------------------------------------
+// Provides tool tips with F# hints for MonoDevelop
+// (this file implements MonoDevelop interfaces and calls 'LanguageService')
+// --------------------------------------------------------------------------------------
+namespace MonoDevelop.FSharp
+
+open System
+open FSharp.CompilerBinding
+open Mono.TextEditor
+open MonoDevelop.Core
+open MonoDevelop.Ide
+open MonoDevelop.Ide.CodeCompletion
+open Microsoft.FSharp.Compiler.SourceCodeServices
+
+/// Resolves locations to tooltip items, and orchestrates their display.
+/// We resolve language items to an NRefactory symbol.
+type FSharpTooltipProvider() = 
+    inherit Mono.TextEditor.TooltipProvider()
+
+    // Keep the last result and tooltip window cached
+    let mutable lastResult = None : TooltipItem option
+    static let mutable lastWindow = None
+
+    let killTooltipWindow() =
+       match lastWindow with
+       | Some(w:TooltipInformationWindow) -> w.Destroy()
+       | None -> ()
+
+    override x.GetItem (editor, offset) =
+      try
+        let extEditor = editor :?> MonoDevelop.SourceEditor.ExtensibleTextEditor 
+        let docText = editor.Text
+        if docText = null || offset >= docText.Length || offset < 0 then null else
+        let proj = extEditor.Project :?> MonoDevelop.Projects.DotNetProject
+        let files = CompilerArguments.getSourceFiles(extEditor.Project.Items) |> Array.ofList
+        let args = CompilerArguments.getArgumentsFromProject(proj)
+        let framework = CompilerArguments.getTargetFramework(proj.TargetFramework.Id)
+        let tyResOpt =
+            MDLanguageService.Instance.GetTypedParseResultWithTimeout
+                 (extEditor.Project.FileName.ToString(),
+                  editor.FileName, 
+                  docText, 
+                  files,
+                  args,
+                  AllowStaleResults.MatchingSource,
+                  ServiceSettings.blockingTimeout,
+                  framework) |> Async.RunSynchronously
+        LoggingService.LogInfo "TooltipProvider: Getting tool tip"
+        match tyResOpt with
+        | None -> null
+        | Some tyRes ->
+        // Get tool-tip from the language service
+        let line, col, lineStr = MonoDevelop.getLineInfoFromOffset(offset, editor.Document)
+        let tip = tyRes.GetToolTip(line, col, lineStr) |> Async.RunSynchronously
+        match tip with
+        | None -> null
+        | Some (ToolTipText(elems),_) when elems |> List.forall (function ToolTipElementNone -> true | _ -> false) -> 
+            LoggingService.LogWarning "TooltipProvider: No data found"
+            null
+        | Some(tiptext,(col1,col2)) -> 
+            LoggingService.LogInfo "TooltipProvider: Got data"
+            //check to see if the last result is the same tooltipitem, if so return the previous tooltipitem
+            match lastResult with
+            | Some(tooltipItem) when
+                tooltipItem.Item :?> ToolTipText = tiptext && 
+                tooltipItem.ItemSegment = TextSegment(editor.LocationToOffset (line, col1 + 1), col2 - col1) -> tooltipItem
+            //If theres no match or previous cached result generate a new tooltipitem
+            | Some(_) | None -> 
+                let line = editor.Document.OffsetToLineNumber offset
+                let segment = TextSegment(editor.LocationToOffset (line, col1 + 1), col2 - col1)
+                let tooltipItem = TooltipItem (tiptext, segment)
+                lastResult <- Some(tooltipItem)
+                tooltipItem
+      with x -> null
+
+    override x.CreateTooltipWindow (editor, offset, modifierState, item) = 
+        let doc = IdeApp.Workbench.ActiveDocument
+        if (doc = null) then null else
+        match item.Item with 
+        | :? ToolTipText as titem ->
+            let tooltip = TipFormatter.formatTip(titem)
+            let (signature, comment) = 
+                match tooltip with
+                | [signature,comment] -> signature,comment
+                //With multiple tips just take the head.  
+                //This shouldnt happen anyway as we split them in the resolver provider
+                | multiple -> multiple |> List.head
+            //dont show a tooltip if there is no content
+            if String.IsNullOrEmpty(signature) then null 
+            else            
+                let result = new TooltipInformationWindow(ShowArrow = true)
+                let toolTipInfo = new TooltipInformation(SignatureMarkup = signature)
+                if not (String.IsNullOrEmpty(comment)) then toolTipInfo.SummaryMarkup <- comment
+                result.AddOverload(toolTipInfo)
+                result.RepositionWindow ()                  
+                result :> _
+        | _ -> LoggingService.LogError "TooltipProvider: Type mismatch, not a FSharpLocalResolveResult"
+               null
+    
+    override x.ShowTooltipWindow (editor, offset, modifierState, mouseX, mouseY, item) =
+        match (lastResult, lastWindow) with
+        | Some(lastRes), Some(lastWin) when item.Item = lastRes.Item && lastWin.IsRealized ->
+            lastWin :> _                   
+        | _ -> killTooltipWindow()
+               match x.CreateTooltipWindow (editor, offset, modifierState, item) with
+               | :? TooltipInformationWindow as tipWindow ->
+                   let positionWidget = editor.TextArea
+                   let region = item.ItemSegment.GetRegion(editor.Document)
+                   let p1, p2 = editor.LocationToPoint(region.Begin), editor.LocationToPoint(region.End)
+                   let caret = Gdk.Rectangle (int p1.X - positionWidget.Allocation.X, 
+                                              int p2.Y - positionWidget.Allocation.Y, 
+                                              int (p2.X - p1.X), 
+                                              int editor.LineHeight)
+                   //For debug this is usful for visualising the tooltip location
+                   // editor.SetSelection(item.ItemSegment.Offset, item.ItemSegment.EndOffset)
+               
+                   tipWindow.ShowPopup(positionWidget, caret, MonoDevelop.Components.PopupPosition.Top)
+                   tipWindow.EnterNotifyEvent.Add(fun _ -> editor.HideTooltip (false))
+                   //cache last window shown
+                   lastWindow <- Some(tipWindow)
+                   lastResult <- Some(item)
+                   tipWindow :> _
+               | _ -> LoggingService.LogError "TooltipProvider: Type mismatch, not a TooltipInformationWindow"
+                      null
+            
+    interface IDisposable with
+        member x.Dispose() = killTooltipWindow()
