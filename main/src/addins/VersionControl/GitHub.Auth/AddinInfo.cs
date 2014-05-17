e769da60be5be54fb513507ba5f24a6536ab605b
Marius Ungureanu <marius.ungureanu@xamarin.com>
2014-05-17 16:32:38 +0300

    [GitHub] Add Authentication Service.
    No UI available yet. If user and password are set in your local build you are
    able to authenticate with those credentials. otherwise, you'll be
    connected anonymously.

---
+++ main/src/addins/VersionControl/GitHub.Auth/AddinInfo.cs
@@ -0,0 +1,14 @@
+﻿using Mono.Addins;
+using Mono.Addins.Description;
+
+[assembly:Addin ("GitHub.Auth", 
+	Namespace = "MonoDevelop",
+	Version = MonoDevelop.BuildInfo.Version,
+	Category = "Version Control")]
+
+[assembly:AddinName ("GitHub Authentication support")]
+[assembly:AddinDescription ("A MonoDevelop addin for authenticating with GitHub")]
+[assembly:AddinFlags (AddinFlags.Hidden)]
+
+[assembly:AddinDependency ("Core", MonoDevelop.BuildInfo.Version)]
+[assembly:AddinDependency ("Ide", MonoDevelop.BuildInfo.Version)]
---
+++ main/src/addins/VersionControl/GitHub.Auth/AssemblyInfo.cs
@@ -0,0 +1,7 @@
+using System.Reflection;
+
+[assembly: AssemblyProduct ("MonoDevelop")]
+[assembly: AssemblyTitle ("GitHub.Auth")]
+[assembly: AssemblyDescription ("A MonoDevelop addin for authenticating with GitHub")]
+[assembly: AssemblyVersion ("2.6")]
+[assembly: AssemblyCopyright ("LGPL")]
