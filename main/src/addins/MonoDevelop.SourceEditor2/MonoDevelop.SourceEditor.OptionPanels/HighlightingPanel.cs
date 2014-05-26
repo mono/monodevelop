8b4ee81a40153da12d0473a54a6c256427afb7a4
sheff146 <alexandre.shevchenko@gmail.com>
2014-05-26 21:43:06 +0600

    Created new ColorSchemeEditor component via Xwt. It still needs to be completed.

--- main/src/addins/MonoDevelop.SourceEditor2/MonoDevelop.SourceEditor.OptionPanels/HighlightingPanel.cs
+++ main/src/addins/MonoDevelop.SourceEditor2/MonoDevelop.SourceEditor.OptionPanels/HighlightingPanel.cs
@@ -109,10 +109,13 @@ namespace MonoDevelop.SourceEditor.OptionPanels
 		{
 			TreeIter selectedIter;
 			if (styleTreeview.Selection.GetSelected (out selectedIter)) {
-				var editor = new ColorShemeEditor (this);
-				var colorScheme = (Mono.TextEditor.Highlighting.ColorScheme)this.styleStore.GetValue (selectedIter, 1);
-				editor.SetSheme (colorScheme);
-				MessageService.ShowCustomDialog (editor, dialog);
+				var editor = new XwtColorSchemeEditor ();
+				var runCommand = editor.Run ();
+				editor.Dispose ();
+				//var editor = new ColorShemeEditor (this);
+				//var colorScheme = (Mono.TextEditor.Highlighting.ColorScheme)this.styleStore.GetValue (selectedIter, 1);
+				//editor.SetSheme (colorScheme);
+				//MessageService.ShowCustomDialog (editor, dialog);
 			}
 		}
 		
