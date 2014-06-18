2c3f570b3307286f9efa60086db94712b0a97e6c
praveenan <praveena.nimani@gmail.com>
2014-06-18 18:34:07 +0530

    * AddinInfo.cs: Created project and addded basic functionality
    * packages.config:
    * AssemblyInfo.cs:
    * GitHubPad.cs:
    * OctokitHelper.cs:
    * GitHub.Repository.addin.xml:
    * pad-unit-test-16.png:
    * pad-unit-test-16@2x.png:

---
+++ main/src/addins/GitHubIntegration/GitHub.Auth/GitHub.Repository/AddinInfo.cs
@@ -0,0 +1,14 @@
+﻿using Mono.Addins;
+using Mono.Addins.Description;
+
+[assembly:Addin ("GitHub.Repository", 
+	Namespace = "MonoDevelop",
+	Version = MonoDevelop.BuildInfo.Version,
+	Category = "GitHub Integration")]
+
+[assembly:AddinName ("GitHub Repository integration support")]
+[assembly:AddinDescription ("A MonoDevelop addin for authenticating with GitHub")]
+[assembly:AddinFlags (AddinFlags.Hidden)]
+
+[assembly:AddinDependency ("Core", MonoDevelop.BuildInfo.Version)]
+[assembly:AddinDependency ("Ide", MonoDevelop.BuildInfo.Version)]
