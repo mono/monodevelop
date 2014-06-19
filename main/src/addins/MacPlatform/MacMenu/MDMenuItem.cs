00d9ac91ba709acf9c0d7d10a6c12019bb62e6c8
Lluis Sanchez <lluis@xamarin.com>
2014-06-19 17:12:38 +0200

    [Mac] Properly enable/disable menu items

--- main/src/addins/MacPlatform/MacMenu/MDMenuItem.cs
+++ main/src/addins/MacPlatform/MacMenu/MDMenuItem.cs
@@ -155,9 +155,7 @@ namespace MonoDevelop.MacIntegration.MacMenu
 		void SetItemValues (NSMenuItem item, CommandInfo info)
 		{
 			item.SetTitleWithMnemonic (GetCleanCommandText (info));
-			if (commandSource != CommandSource.ContextMenu) {
-				item.Enabled = !IsGloballyDisabled && info.Enabled;
-			}
+			item.Enabled = info.Enabled && (!IsGloballyDisabled || commandSource != CommandSource.ContextMenu);
 			item.Hidden = !info.Visible;
 			SetAccel (item, info.AccelKey);
 
