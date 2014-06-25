ec43138f1c1d814bfd627ff783d6184514db0f25
praveenan <praveena.nimani@gmail.com>
2014-06-25 22:51:28 +0530

    in-progress gisting

--- main/src/addins/GitHubIntegration/GitHub.Repository/Core/OctokitHelper.cs
+++ main/src/addins/GitHubIntegration/GitHub.Repository/Core/OctokitHelper.cs
@@ -54,6 +54,14 @@ namespace GitHub.Repository.Core
 		}
 
 
+		//in progress
+		public bool GistAFile()
+		{
+			Octokit.Gist gist = new Octokit.Gist ();
+			Octokit.GistFile file = new Octokit.GistFile ();
+			gist.Files.Add (file);
+			GitHubService.Client.Gist.Create (gist).Result;
+		}
 			
 	}
 }
