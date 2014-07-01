576b2c574edef126e387379aef77aaca6862d5a5
7sharp9 <kukulcanenator@googlemail.com>
2014-07-01 19:17:17 +0100

    Fantomas code formatting

---
+++ monodevelop/MonoDevelop.FSharpBinding/FSharpFormattingPolicyPanel.fs
@@ -0,0 +1,21 @@
+﻿namespace MonoDevelop.FSharp
+
+open Gtk
+open MonoDevelop.Ide.Gui.Dialogs
+
+type FSharpFormattingPolicyPanel() =
+    inherit MimeTypePolicyOptionsPanel<FSharpFormattingPolicy>()
+    let mutable policy = FSharpFormattingPolicy()
+    let mutable panel = new FSharpFormattingPolicyPanelWidget()
+    override __.CreatePanelWidget() =
+        panel <- new FSharpFormattingPolicyPanelWidget()
+        panel.Initialize()
+        panel :> Widget
+
+    override __.LoadFrom(p : FSharpFormattingPolicy) =
+        policy <- p.Clone()
+        panel.SetFormat(policy)
+        
+    override __.GetPolicy() =
+        panel.CommitPendingChanges ()
+        policy
