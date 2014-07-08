d8da75f6a74e781f8fe9e4c3f118700d56dd0c09
Alan McGovern <alan@xamarin.com>
2014-07-08 08:24:06 -0400

    [build] Try to continue if mozroots fails
    mozroots will fail if you are disconnected from the internet.
    However if the nuget packages are already in the download cache,
    you don't need the internet anyway!

--- monodevelop/configure.sh
+++ monodevelop/configure.sh
@@ -2,5 +2,5 @@
 echo "Configuring..."
 fsharpi configure.fsx "$@"
 echo "Getting nuget packages..."
-mozroots --import --sync --quiet
+mozroots --import --sync --quiet || echo 'Could not import mozroots, proceeding anyway'
 (cd MonoDevelop.FSharpBinding && mono ../../lib/nuget/NuGet.exe restore MonoDevelop.FSharp.mac-linux.sln)
