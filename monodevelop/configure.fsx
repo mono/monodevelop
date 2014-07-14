315beb18d1ac35a4b6c0bbd27f512e7a40f75e4f
7sharp9 <kukulcanenator@googlemail.com>
2014-07-14 10:35:21 +0100

    Incremented version

--- monodevelop/configure.fsx
+++ monodevelop/configure.fsx
@@ -10,7 +10,7 @@ open System.IO
 open System.Diagnostics
 open System.Text.RegularExpressions
 
-let FSharpVersion = "5.3.0"
+let FSharpVersion = "5.3.1"
 
 let UnixPaths = 
     [ "/usr/lib/monodevelop"
