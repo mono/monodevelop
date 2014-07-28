mono/monodevelop

commit cdabd0caaab5438e4285d9ef1c9514cbe9708fac

[Ide] Fix crash when loading pads
When registering a new pad as a result of the installation of a new add-in
the corresponding DockItem was not created. This is now done.

Fixes bug 20600 - Installing an addin that creates a new pad throws a null
reference exception and crashes Xamarin Studio.

main/src/core/MonoDevelop.Ide/MonoDevelop.Ide.Gui/DefaultWorkbench.cs
@@ -422,7 +422,7 @@ namespace MonoDevelop.Ide.Gui
 		{
 			if (node is PadCodon) {
 				PadCodon pad = (PadCodon) node;
-				RegisterPad (pad);
+				AddPad (pad, pad.DefaultPlacement, pad.DefaultStatus);
 			}
 			else if (node is CategoryNode) {
 				foreach (ExtensionNode cn in node.ChildNodes)
@@ -784,10 +784,6 @@ namespace MonoDevelop.Ide.Gui
 		{
 			AddinManager.AddExtensionNodeHandler (stockLayoutsPath, OnLayoutsExtensionChanged);
 			
-			ExtensionNodeList padCodons = AddinManager.GetExtensionNodes (viewContentPath);
-			foreach (ExtensionNode node in padCodons)
-				ShowPadNode (node);
-			
 			CreateComponents ();
 			
 			// Subscribe to changes in the extension
@@ -930,9 +926,10 @@ namespace MonoDevelop.Ide.Gui
 			}
 
 			// create DockItems for all the pads
-			foreach (PadCodon content in padContentCollection)
-				AddPad (content, content.DefaultPlacement, content.DefaultStatus);
-			
+			ExtensionNodeList padCodons = AddinManager.GetExtensionNodes (viewContentPath);
+			foreach (ExtensionNode node in padCodons)
+				ShowPadNode (node);
+
 			try {
 				if (System.IO.File.Exists (configFile)) {
 					dock.LoadLayouts (configFile);

commit 7e65004dfa76080271b166e6e5bb391cabc68039

[Ide] Fix pad registration issue
Fixes regression introduced by commit cdabd0caaa

main/src/core/MonoDevelop.Ide/MonoDevelop.Ide.Gui/DefaultWorkbench.cs
@@ -451,8 +451,6 @@ namespace MonoDevelop.Ide.Gui
 				return;
 			}
 
-			RegisterPad (content);
-			
 			if (item != null) {
 				if (show)
 					item.Visible = true;
@@ -1292,6 +1290,8 @@ namespace MonoDevelop.Ide.Gui
 		
 		void AddPad (PadCodon padCodon, string placement, DockItemStatus defaultStatus)
 		{
+			RegisterPad (padCodon);
+
 			PadWindow window = new PadWindow (this, padCodon);
 			window.Icon = padCodon.Icon;
 			padWindows [padCodon] = window;
@@ -1331,9 +1331,6 @@ namespace MonoDevelop.Ide.Gui
 				else
 					window.NotifyContentHidden ();
 			};
-			
-			if (!padContentCollection.Contains (padCodon))
-				padContentCollection.Add (padCodon);
 		}
 		
 		void UpdatePad (object source, EventArgs args)