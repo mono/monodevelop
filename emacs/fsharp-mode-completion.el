243fbe2babd45093b9df30cc69140a8456baf40f
Robin Neatherway <robin.neatherway@gmail.com>
2014-10-14 14:12:56 +0100

    Add missing require 'cl for Emacs

--- emacs/fsharp-mode-completion.el
+++ emacs/fsharp-mode-completion.el
@@ -23,6 +23,7 @@
 ;; the Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor,
 ;; Boston, MA 02110-1301, USA.
 
+(with-no-warnings (require 'cl))
 (require 's)
 (require 'dash)
 (require 'auto-complete)
