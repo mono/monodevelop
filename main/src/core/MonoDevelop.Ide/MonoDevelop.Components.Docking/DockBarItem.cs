mono/monodevelop

commit 1fdcc70515cdb917fe620aeba216824dcd0c0643

[Ide] Fix pad hidding issue
When presenting a pad, make sure the window that contains it gets the
top level focus, otherwise it will be closed shortly after being shown.

main/src/core/MonoDevelop.Ide/MonoDevelop.Components.Docking/DockBarItem.cs
@@ -274,6 +274,8 @@ namespace MonoDevelop.Components.Docking
 				GLib.Timeout.Add (200, delegate {
 					// Using a small delay because AutoShow uses an animation and setting focus may
 					// not work until the item is visible
+					if (autoShowFrame != null && autoShowFrame.ContainerWindow != (Gtk.Window)Toplevel)
+						autoShowFrame.ContainerWindow.Present ();
 					it.SetFocus ();
 					ScheduleAutoHide (false);
 					return false;

commit ce857076f888f87f2e4cc71a0715abb48a96622c

Fix for bug introduced in commit 1fdcc70515cdb917fe620aeba216824dcd0c0643. IDE is throwing following exception... System.NullReferenceException: Object reference not set to an instance of an object at MonoDevelop.Components.Docking.DockBarItem.<Present>m__2 () [0x00027] in main/src/core/MonoDevelop.Ide/MonoDevelop.Components.Docking/DockBarItem.cs:278 at GLib.Timeout+TimeoutProxy.Handler () [0x00000] in <filename unknown>:0

main/src/core/MonoDevelop.Ide/MonoDevelop.Components.Docking/DockBarItem.cs
@@ -274,7 +274,7 @@ namespace MonoDevelop.Components.Docking
 				GLib.Timeout.Add (200, delegate {
 					// Using a small delay because AutoShow uses an animation and setting focus may
 					// not work until the item is visible
-					if (autoShowFrame != null && autoShowFrame.ContainerWindow != (Gtk.Window)Toplevel)
+					if (autoShowFrame != null && autoShowFrame.ContainerWindow != null && autoShowFrame.ContainerWindow != (Gtk.Window)Toplevel)
 						autoShowFrame.ContainerWindow.Present ();
 					it.SetFocus ();
 					ScheduleAutoHide (false);