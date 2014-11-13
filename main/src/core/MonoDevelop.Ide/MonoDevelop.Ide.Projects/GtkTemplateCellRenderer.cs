ce831de294f256beaf204e68771607f2bbc4bbcc
Matt Ward <ward.matt@gmail.com>
2014-11-13 18:02:56 +0000

    [Ide] Align second row for category and template in New Project dialog.
    The second row for the list of categories and the list of templates
    now have their top edge aligned in the New Project dialog.

--- main/src/core/MonoDevelop.Ide/MonoDevelop.Ide.Projects/GtkTemplateCellRenderer.cs
+++ main/src/core/MonoDevelop.Ide/MonoDevelop.Ide.Projects/GtkTemplateCellRenderer.cs
@@ -46,8 +46,8 @@ namespace MonoDevelop.Ide.Projects
 		const int languageRightHandPadding = 4;
 		const int languageLeftHandPadding = 9;
 		const int iconTextPadding = 9;
-		const int groupTemplateHeadingTotalYPadding = 18;
-		const int groupTemplateHeadingYOffset = 7;
+		const int groupTemplateHeadingTotalYPadding = 24;
+		const int groupTemplateHeadingYOffset = 4;
 		const int categoryTextPaddingX = 4;
 
 		int minLanguageRectWidth;
