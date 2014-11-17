00b218efb22aab4b91db99de7182dd464c715756
guillermooo <guillermo.lopez@outlook.com>
2014-11-17 22:19:57 +0100

    improve Windows build scripts

--- sublimetext/bin/Build.ps1
+++ sublimetext/bin/Build.ps1
@@ -1,3 +1,4 @@
+param([switch]$Clean)
 <#
 .DESCRIPTION
     Publishes the project locally as a directory.
@@ -5,8 +6,23 @@
 
 $script:thisDir = split-path $MyInvocation.MyCommand.Path -parent
 
-# todo: use config file for devs
-$targetDir = "~/Utilities/Sublime Text 3/Data/Packages/Fsharp"
+$msg = @"
+You need to set `$global:STDataPath to Sublime Text 3's data path.
+
+PS> `$STDataPath = ...
+
+(Note this is a PowerShell variable, not an environment variable.)
+
+For more information on Sublime Text's data path, see:
+http://docs.sublimetext.info/en/latest/basic_concepts.html#the-data-directory
+"@
+
+if (-not (test-path variable:\STDataPath) -or
+    ($global:STDataPath -eq $null) -or
+    -not (test-path $global:STDataPath)) {
+    throw $msg
+    exit 1
+}
 
 "restarting Sublime Text..."
 
@@ -18,9 +34,16 @@ catch [Exception] {
     # ignore
 }
 
+$fsharpPackageDir = "$global:STDataPath\Packages\FSharp"
+[void] (new-item -itemtype 'directory' $fsharpPackageDir -force -erroraction stop)
+
+if ($Clean) {
+    [void] (remove-item "$fsharpPackageDir/*" -recurse -force -erroraction stop)
+}
+
 # after st has been closed; resources should have been released now
-push-location "../Fsharp"
-    copy-item * $targetDir -force -recurse -erroraction stop
+push-location "$thisDir/../FSharp"
+    copy-item * $fsharpPackageDir -force -recurse -erroraction stop
 pop-location
 
 'done'
