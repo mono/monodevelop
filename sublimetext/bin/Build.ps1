e4c72e139f3406e5b0bcb8d7c109ba1600a25b41
guillermooo <guillermo.lopez@outlook.com>
2014-11-18 08:11:16 +0100

    improve build scripts
    - Add parameters to control several aspects
    - Use verbose stream (4) for progress information
    - Use debug stream (5) for debugging
    
    To show the verbose stream's output, use the -Verbose parameter.
    To show the debug stream's output, use the -Debug parameter.
    
    To redirect streams (example):
    
    ./bin/Build.ps1 4>&1
    
    Redirecting streams is useful, for example, to show output within
    Sublime Text if so desired.

--- sublimetext/bin/Build.ps1
+++ sublimetext/bin/Build.ps1
@@ -1,8 +1,16 @@
-param([switch]$Clean)
 <#
-.DESCRIPTION
+    .DESCRIPTION
     Publishes the project locally as a directory.
+
+    .PARAMETER Clean
+    Whether to erase the target folder's content first.
+
+    .PARAMETER Restart
+    Whether to restart Sublime Text.
 #>
+[CmdletBinding()]
+param([switch]$Clean, [switch]$Restart)
+
 
 $script:thisDir = split-path $MyInvocation.MyCommand.Path -parent
 
@@ -24,28 +32,41 @@ if (-not (test-path variable:\STDataPath) -or
     exit 1
 }
 
-"restarting Sublime Text..."
+write-debug "path to Sublime Text 3 Data is: '$global:STDataPath'"
 
-try {
-    get-process "sublime_text" -erroraction silentlycontinue | stop-process -erroraction silentlycontinue
-    start-sleep -milliseconds 250
-}
-catch [Exception] {
-    # ignore
+if ($Restart) {
+    try {
+        write-verbose "stopping Sublime Text..."
+        get-process "sublime_text" -erroraction silentlycontinue | stop-process -erroraction silentlycontinue
+        start-sleep -milliseconds 250
+    }
+    catch [Exception] {
+        write-debug "error occurred while stopping sublime text"
+        # ignore
+    }
 }
 
 $fsharpPackageDir = "$global:STDataPath\Packages\FSharp"
+write-debug "target path is: $fsharpPackageDir"
+write-verbose "creating '$fsharpPackageDir' directory..."
 [void] (new-item -itemtype 'directory' $fsharpPackageDir -force -erroraction stop)
 
 if ($Clean) {
+    write-verbose "erasing '$fsharpPackageDir''s content..."
     [void] (remove-item "$fsharpPackageDir/*" -recurse -force -erroraction stop)
 }
 
 # after st has been closed; resources should have been released now
 push-location "$thisDir/../FSharp"
+    write-debug "source directory is: $(get-location)"
+    write-verbose "copying files from $(get-location) to '$fsharpPackageDir'..."
     copy-item * $fsharpPackageDir -force -recurse -erroraction stop
 pop-location
 
-'done'
+if ($Restart) {
+    write-verbose "restarting Sublime Text..."
+    write-debug "path to Sublime Text 3 executable is: $((get-command sublime_text).path)"
+    sublime_text
+}
 
-sublime_text
+write-verbose 'done'
