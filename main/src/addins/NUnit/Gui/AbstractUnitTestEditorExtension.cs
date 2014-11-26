127d1c84775111d8a8ecb8be57b6c058c7945150
Mike Krüger <mkrueger@xamarin.com>
2014-11-26 11:44:36 +0100

    [NUnit] Handle thread cancellation.

--- main/src/addins/NUnit/Gui/AbstractUnitTestEditorExtension.cs
+++ main/src/addins/NUnit/Gui/AbstractUnitTestEditorExtension.cs
@@ -79,21 +79,26 @@ namespace MonoDevelop.NUnit
 			ThreadPool.QueueUserWorkItem (delegate {
 				if (token.IsCancellationRequested)
 					return;
-				var foundTests = GatherUnitTests (token).Result;
-				if (foundTests == null)
-					return;
-				Application.Invoke (delegate {
-					foreach (var oldMarker in currentMarker)
-						Editor.RemoveMarker (oldMarker);
-					currentMarker = new List<IUnitTestMarker> ();
-					foreach (var foundTest in foundTests) {
-						if (token.IsCancellationRequested)
-							return;
-						var unitTestMarker = TextMarkerFactory.CreateUnitTestMarker (Editor, new UnitTestMarkerHostImpl (this), foundTest);
-						currentMarker.Add (unitTestMarker);
-						Editor.AddMarker (foundTest.LineNumber, unitTestMarker);
-					}
-				});
+				GatherUnitTests (token).ContinueWith (task => {
+					var foundTests = task.Result;
+					if (foundTests == null)
+						return;
+					Application.Invoke (delegate {
+						foreach (var oldMarker in currentMarker)
+							Editor.RemoveMarker (oldMarker);
+						currentMarker = new List<IUnitTestMarker> ();
+						foreach (var foundTest in foundTests) {
+							if (token.IsCancellationRequested)
+								return;
+							var unitTestMarker = TextMarkerFactory.CreateUnitTestMarker (Editor, new UnitTestMarkerHostImpl (this), foundTest);
+							currentMarker.Add (unitTestMarker);
+							Editor.AddMarker (foundTest.LineNumber, unitTestMarker);
+						}
+					});
+
+				}, TaskContinuationOptions.ExecuteSynchronously | 
+					TaskContinuationOptions.NotOnCanceled | 
+					TaskContinuationOptions.NotOnFaulted);
 			});
 		}
 
