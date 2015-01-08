3dda97987471b040d01eee733cca9827673026c2
xplicit <svg@ngs.ru>
2015-01-08 11:03:22 +0600

    [T4] all templates processing for Solution/Project
    This is new feature, which allows to update all T4 templates in Solution or selected Project.
    To use it, select in Solution View Solution or Project node, right click to open context menu, select Tools/Process Teplates.
    
    It's very usefull when you have base template and alot of derived templates from it.
    Before this feature is implemented you have to change every derived T4 template and resave it to regenerate the content after changing base template.
    With this feature you can do it in one click.
    
    Knowing issue: after regeneration "Tool output" window is created for every templat was processed.
    It would be much better, that all information about tool generation process will be created in one "Tool output" window, but I don't know now how to do it inside monodevelop. If someone point me how it can be done, I'll do it.

---
+++ main/src/addins/TextTemplating/MonoDevelop.TextTemplating/GenerateCommandHandler.cs
@@ -0,0 +1,76 @@
+﻿//
+// GenerateCommand.cs
+//
+// Author:
+//       Sergey Zhukov <>
+//
+// Copyright (c) 2015 Sergey Zhukov
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in
+// all copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+// THE SOFTWARE.
+using System;
+using MonoDevelop.Components.Commands;
+using MonoDevelop.Projects;
+using MonoDevelop.Ide;
+using MonoDevelop.Ide.CustomTools;
+
+namespace MonoDevelop.TextTemplating
+{
+	public enum Commands
+	{
+		Generate
+	}
+
+	public class GenerateCommandHandler : CommandHandler
+	{
+		protected override void Run ()
+		{
+			IWorkspaceObject wob = IdeApp.ProjectOperations.CurrentSelectedItem as IWorkspaceObject;
+
+			Solution solution = wob as Solution;
+			if (solution != null) {
+				foreach(Project p in solution.GetAllProjects ())
+					UpdateFilesInProject(p);
+			} else {
+				Project proj = wob as Project;
+
+				if (proj != null) {
+					UpdateFilesInProject (proj);
+				}
+			}
+
+			base.Run ();
+
+		}
+
+		private void UpdateFilesInProject(Project project)
+		{
+			foreach (ProjectFile file in project.Files) {
+				if (file.Generator == typeof(TextTemplatingFileGenerator).Name
+					|| file.Generator == typeof(TextTemplatingFilePreprocessor).Name) {
+					CustomToolService.Update (file, true);
+				}
+			}
+		}
+
+		protected override void Update (CommandInfo info)
+		{
+			base.Update (info);
+		}
+	}
+}
