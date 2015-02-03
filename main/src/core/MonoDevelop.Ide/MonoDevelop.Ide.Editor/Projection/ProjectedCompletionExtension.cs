87e7dec1041a4a9a0df30aedd4ec79e4ba1a3116
Mike Krüger <mkrueger@xamarin.com>
2015-02-03 13:00:30 +0100

    [Ide] Implemented basic completion projection.

--- main/src/core/MonoDevelop.Ide/MonoDevelop.Ide.Editor/Projection/ProjectedCompletionExtension.cs
+++ main/src/core/MonoDevelop.Ide/MonoDevelop.Ide.Editor/Projection/ProjectedCompletionExtension.cs
@@ -34,7 +34,6 @@ namespace MonoDevelop.Ide.Editor.Projection
 	sealed class ProjectedCompletionExtension : CompletionTextEditorExtension
 	{
 		IReadOnlyList<Projection> projections;
-		CompletionTextEditorExtension projectedExtension;
 
 		public ProjectedCompletionExtension (IReadOnlyList<Projection> projections)
 		{
@@ -54,88 +53,198 @@ namespace MonoDevelop.Ide.Editor.Projection
 
 		protected override void Initialize ()
 		{
-		//	projectedExtension = ProjectedDocumentContext.ProjectedEditor.GetContent<CompletionTextEditorExtension> ();
+		}
+
+		Projection GetProjectionAt (int offset)
+		{
+			foreach (var projection in projections) {
+				foreach (var seg in projection.ProjectedSegments) {
+					if (seg.ContainsOriginal (offset)) {
+						projection.ProjectedEditor.CaretOffset = seg.FromOriginalToProjected (offset);
+						return projection;
+					}
+				}
+			}
+			return null;
+		}
+
+		CompletionTextEditorExtension GetExtensionAt (int offset)
+		{
+			var projection = GetProjectionAt (offset);
+			if (projection != null)			
+				return projection.ProjectedEditor.GetContent<CompletionTextEditorExtension> ();
+			return null;
+		}
+
+		CompletionTextEditorExtension GetCurrentExtension ()
+		{
+			return GetExtensionAt (Editor.CaretOffset);
 		}
 
 		public override bool CanRunCompletionCommand ()
 		{
+			var projectedExtension = GetCurrentExtension ();
+			if (projectedExtension == null)
+				return false;
 			return projectedExtension.CanRunCompletionCommand ();
 		}
 
 		public override MonoDevelop.Ide.CodeCompletion.ICompletionDataList CodeCompletionCommand (MonoDevelop.Ide.CodeCompletion.CodeCompletionContext completionContext)
 		{
-			return projectedExtension.CodeCompletionCommand (completionContext);
+			var projectedExtension = GetExtensionAt (completionContext.TriggerOffset);
+			if (projectedExtension == null)
+				return null;
+			return projectedExtension.CodeCompletionCommand (ConvertContext (completionContext));
 		}
 
 		public override bool CanRunParameterCompletionCommand ()
 		{
+			var projectedExtension = GetCurrentExtension ();
+			if (projectedExtension == null)
+				return false;
 			return projectedExtension.CanRunParameterCompletionCommand ();
 		}
 
 		public override string CompletionLanguage {
 			get {
+				var projectedExtension = GetCurrentExtension ();
+				if (projectedExtension == null)
+					return base.CompletionLanguage;
 				return projectedExtension.CompletionLanguage;
 			}
 		}
 
 		public override bool GetCompletionCommandOffset (out int cpos, out int wlen)
 		{
+			var projectedExtension = GetCurrentExtension ();
+			if (projectedExtension == null) {
+				cpos = 0;
+				wlen = 0;
+				return false;
+			}
 			return projectedExtension.GetCompletionCommandOffset (out cpos, out wlen);
 		}
 
 		public override int GetCurrentParameterIndex (int startOffset)
 		{
+			var projectedExtension = GetExtensionAt (startOffset);
+			if (projectedExtension == null)
+				return -1;
 			return projectedExtension.GetCurrentParameterIndex (startOffset);
 		}
 
 		public override int GuessBestMethodOverload (ICSharpCode.NRefactory6.CSharp.Completion.ParameterHintingResult provider, int currentOverload)
 		{
+			var projectedExtension = GetCurrentExtension ();
+			if (projectedExtension == null)
+				return -1;
 			return projectedExtension.GuessBestMethodOverload (provider, currentOverload);
 		}
 
 		public override System.Threading.Tasks.Task<MonoDevelop.Ide.CodeCompletion.ICompletionDataList> HandleCodeCompletionAsync (MonoDevelop.Ide.CodeCompletion.CodeCompletionContext completionContext, char completionChar, System.Threading.CancellationToken token)
 		{
-			return projectedExtension.HandleCodeCompletionAsync (completionContext, completionChar, token);
+			var projectedExtension = GetExtensionAt (completionContext.TriggerOffset);
+			if (projectedExtension == null)
+				return null;
+
+			return projectedExtension.HandleCodeCompletionAsync (ConvertContext (completionContext), completionChar, token);
 		}
 
 		public override System.Threading.Tasks.Task<ICSharpCode.NRefactory6.CSharp.Completion.ParameterHintingResult> HandleParameterCompletionAsync (MonoDevelop.Ide.CodeCompletion.CodeCompletionContext completionContext, char completionChar, System.Threading.CancellationToken token)
 		{
-			return projectedExtension.HandleParameterCompletionAsync (completionContext, completionChar, token);
+			var projectedExtension = GetExtensionAt (completionContext.TriggerOffset);
+			if (projectedExtension == null)
+				return null;
+			return projectedExtension.HandleParameterCompletionAsync (ConvertContext (completionContext), completionChar, token);
 		}
 
 		public override bool KeyPress (KeyDescriptor descriptor)
 		{
-			return projectedExtension.KeyPress (descriptor);
+			var projectedExtension = GetCurrentExtension();
+			if (projectedExtension != null)
+				projectedExtension.KeyPress (descriptor);
+			return base.KeyPress (descriptor);
 		}
 
 		public override ICSharpCode.NRefactory6.CSharp.Completion.ParameterHintingResult ParameterCompletionCommand (MonoDevelop.Ide.CodeCompletion.CodeCompletionContext completionContext)
 		{
-			return projectedExtension.ParameterCompletionCommand (completionContext);
+			var projectedExtension = GetExtensionAt (completionContext.TriggerOffset);
+			if (projectedExtension == null)
+				return null;
+			return projectedExtension.ParameterCompletionCommand (ConvertContext (completionContext));
 		}
 
 		public override void RunCompletionCommand ()
 		{
+			Console.WriteLine ("run completion command !!!!");
+			var projectedExtension = GetCurrentExtension();
+			if (projectedExtension == null)
+				return;
+			Console.WriteLine ("found ext : "+ projectedExtension);
 			projectedExtension.RunCompletionCommand ();
 		}
 
 		public override void RunParameterCompletionCommand ()
 		{
+			var projectedExtension = GetCurrentExtension();
+			if (projectedExtension == null)
+				return;
+			
 			projectedExtension.RunParameterCompletionCommand ();
 		}
 
 		public override void RunShowCodeTemplatesWindow ()
 		{
+			var projectedExtension = GetCurrentExtension();
+			if (projectedExtension == null)
+				return;
 			projectedExtension.RunShowCodeTemplatesWindow ();
 		}
 
 		public override MonoDevelop.Ide.CodeCompletion.ICompletionDataList ShowCodeSurroundingsCommand (MonoDevelop.Ide.CodeCompletion.CodeCompletionContext completionContext)
 		{
-			return projectedExtension.ShowCodeSurroundingsCommand (completionContext);
+			var projectedExtension = GetExtensionAt (completionContext.TriggerOffset);
+			if (projectedExtension == null)
+				return null;
+			return projectedExtension.ShowCodeSurroundingsCommand (ConvertContext (completionContext));
 		}
 
 		public override MonoDevelop.Ide.CodeCompletion.ICompletionDataList ShowCodeTemplatesCommand (MonoDevelop.Ide.CodeCompletion.CodeCompletionContext completionContext)
 		{
-			return projectedExtension.ShowCodeTemplatesCommand (completionContext);
+			var projectedExtension = GetExtensionAt (completionContext.TriggerOffset);
+			if (projectedExtension == null)
+				return null;
+			return projectedExtension.ShowCodeTemplatesCommand (ConvertContext (completionContext));
+		}
+
+		MonoDevelop.Ide.CodeCompletion.CodeCompletionContext ConvertContext (MonoDevelop.Ide.CodeCompletion.CodeCompletionContext completionContext)
+		{
+			var projection = GetProjectionAt (completionContext.TriggerOffset);
+
+			int offset = completionContext.TriggerOffset;
+			int line = completionContext.TriggerLine;
+			int lineOffset = completionContext.TriggerLineOffset;
+
+			if (projection != null) {
+				foreach (var seg in projection.ProjectedSegments) {
+					if (seg.ContainsOriginal (offset)) {
+						offset = seg.FromOriginalToProjected (offset);
+						var loc = projection.ProjectedEditor.OffsetToLocation (offset);
+						line = loc.Line;
+						lineOffset = loc.Column - 1;
+					}
+				}
+			}
+
+			return new MonoDevelop.Ide.CodeCompletion.CodeCompletionContext {
+				TriggerOffset = offset,
+				TriggerLine = line,
+				TriggerLineOffset  = lineOffset,
+				TriggerXCoord  = completionContext.TriggerXCoord,
+				TriggerYCoord  = completionContext.TriggerYCoord,
+				TriggerTextHeight  = completionContext.TriggerTextHeight,
+				TriggerWordLength  = completionContext.TriggerWordLength
+			};
 		}
 	}
 }
