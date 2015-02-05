b28bf19ec28f34e2bebe3260479af1e635cda4d1
Lluis Sanchez Gual <lluis@xamarin.com>
2015-02-05 01:45:08 +0100

    Add missing test files

--- main/tests/UnitTests/MonoDevelop.Projects/ItemMetadataTests.cs
+++ main/tests/UnitTests/MonoDevelop.Projects/ItemMetadataTests.cs
@@ -24,13 +24,122 @@
 // OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
 // THE SOFTWARE.
 using System;
+using NUnit.Framework;
+using UnitTests;
+using System.Xml;
+using System.Threading.Tasks;
+using MonoDevelop.Core;
+using System.Linq;
+using System.IO;
 
 namespace MonoDevelop.Projects
 {
+	[TestFixture]
 	public class ItemMetadataTests
 	{
+		XmlNamespaceManager nm;
+
 		public ItemMetadataTests ()
 		{
+			XmlDocument doc = new XmlDocument ();
+			nm = new XmlNamespaceManager (doc.NameTable);
+			nm.AddNamespace ("n","http://schemas.microsoft.com/developer/msbuild/2003");
+		}
+
+		DotNetProject CreateProject ()
+		{
+			var p = Services.ProjectService.CreateDotNetProject ("C#");
+			p.FileName = Path.GetTempFileName ();
+			File.Delete (p.FileName);
+			return p;
+		}
+
+		XmlElement LoadElement (string file, string itemInclude)
+		{
+			XmlDocument doc = new XmlDocument ();
+			doc.Load (file);
+			return (XmlElement) doc.SelectSingleNode ("n:Project/n:ItemGroup/n:Compile[contains(@Include, '" + itemInclude + "')]", nm);
+		}
+
+		async Task<XmlElement> Save (ProjectItem it)
+		{
+			Project p = CreateProject ();
+			p.Items.Add (it);
+			try {
+				await p.SaveAsync (new MonoDevelop.Core.ProgressMonitor ());
+				return LoadElement (p.FileName, it.Include);
+			} finally {
+				System.IO.File.Delete (p.FileName);
+			}
+		}
+
+		void AssertHasMetadata (XmlElement elem, string prop, string val)
+		{
+			var p = (XmlElement)elem.SelectSingleNode ("n:" + prop, nm);
+			Assert.IsNotNull (p, "Property " + prop + " not found");
+			Assert.AreEqual (val, p.InnerXml);
+		}
+
+		[Test]
+		public async Task String ()
+		{
+			ProjectItem item = new ProjectFile ("t1");
+			item.Metadata.SetValue ("a1", "v1");
+			var e = await Save (item);
+			Assert.NotNull (e);
+			Assert.AreEqual (1, e.ChildNodes.Count);
+			AssertHasMetadata (e, "a1", "v1");
+		}
+
+		[Test]
+		public async Task StringWithDefaultValue ()
+		{
+			ProjectItem item = new ProjectFile ("t1");
+			item.Metadata.SetValue ("a1", "v1", "v2");
+			item.Metadata.SetValue ("a2", "v2", "v2");
+			var e = await Save (item);
+			Assert.NotNull (e);
+			Assert.AreEqual (1, e.ChildNodes.Count);
+			AssertHasMetadata (e, "a1", "v1");
+		}
+
+		[Test]
+		public async Task StringPreserveCase ()
+		{
+			var p = CreateProject ();
+			try {
+				ProjectItem item = new ProjectFile ("t1");
+				item.Metadata.SetValue ("a1", "v1");
+				item.Metadata.SetValue ("a2", "V2");
+				item.Metadata.SetValue ("a3", "v3");
+				item.Metadata.SetValue ("a4", "V4");
+				p.Items.Add (item);
+				await p.SaveAsync (new ProgressMonitor ());
+
+				var p2 = (DotNetProject) await Services.ProjectService.ReadSolutionItem (new ProgressMonitor (), p.FileName);
+				p.Dispose ();
+
+				item = p2.Files [0];
+
+				item.Metadata.SetValue ("a1", "V1", preserveExistingCase: true);
+				item.Metadata.SetValue ("a2", "v2", preserveExistingCase: true);
+				item.Metadata.SetValue ("a3", "V3", preserveExistingCase: false);
+				item.Metadata.SetValue ("a4", "v4", preserveExistingCase: false);
+
+				await p2.SaveAsync (new ProgressMonitor ());
+
+				var e = LoadElement (p2.FileName, "t1");
+
+				Assert.NotNull (e);
+				Assert.AreEqual (4, e.ChildNodes.Count);
+				AssertHasMetadata (e, "a1", "v1");
+				AssertHasMetadata (e, "a2", "V2");
+				AssertHasMetadata (e, "a3", "V3");
+				AssertHasMetadata (e, "a4", "v4");
+			} finally {
+				if (File.Exists (p.FileName))
+					File.Delete (p.FileName);
+			}
 		}
 	}
 }
