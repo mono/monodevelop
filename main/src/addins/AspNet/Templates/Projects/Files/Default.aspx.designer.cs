0cd344c76f0a64715eeb784b89f20883f65cf97b
Michael Hutchinson <m.j.hutchinson@gmail.com>
2015-02-08 00:29:49 -0500

    [AspNet] Use wizard for project templates

---
+++ main/src/addins/AspNet/Templates/Projects/Files/Default.aspx.designer.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Web;
+using System.Web.UI;
+
+namespace ${Namespace} {
+
+	public partial class ${EscapedIdentifier}
+	{
+		protected System.Web.UI.WebControls.Button button1;
+	}
+}
