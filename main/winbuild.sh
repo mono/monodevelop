558b8720ce839b4f9e0c9b91c07ce5ae97ec2c15
Michael Hutchinson <m.j.hutchinson@gmail.com>
2015-02-09 15:55:27 -0500

    Use MSBuild 14 in Windows build script

--- main/winbuild.sh
+++ main/winbuild.sh
@@ -1,3 +1,3 @@
-export PATH="/c/Windows/Microsoft.NET/Framework/v4.0.30319:$PATH"
+export PATH="/c/Program Files (x86)/MSBuild/14.0/Bin:$PATH"
 pushd ..; git submodule update --init --recursive || exit 1; popd
 MSBuild.exe Main.sln -p:Configuration=DebugWin32 $*
