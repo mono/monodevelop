3906de4c86b760545588771a6ba1bdbbef4684cf
7sharp9 <dave.thomas@xamarin.com>
2015-02-09 13:04:00 +0000

    Ported Highlight usages to XS6-editor-roslyn branch

--- monodevelop/MonoDevelop.FSharpBinding/FSharpHighlightUsagesExtension.fs
+++ monodevelop/MonoDevelop.FSharpBinding/FSharpHighlightUsagesExtension.fs
@@ -5,6 +5,7 @@ open Mono.TextEditor
 open MonoDevelop.Core
 open MonoDevelop.Ide
 open MonoDevelop.Ide.FindInFiles
+open MonoDevelop.Ide.Editor.Extension
 open MonoDevelop.Projects
 open ICSharpCode.NRefactory.Semantics
 open ICSharpCode.NRefactory.TypeSystem
@@ -13,43 +14,42 @@ open Microsoft.FSharp.Compiler.SourceCodeServices
 open FSharp.CompilerBinding
 
 /// MD/XS extension for highlighting the usages of a symbol within the current buffer.
-type HighlightUsagesExtension() as this =
-    inherit MonoDevelop.SourceEditor.AbstractUsagesExtension<ResolveResult>()
+type HighlightUsagesExtension() =
+    inherit AbstractUsagesExtension<(string * FSharpSymbolUse []) option>()
 
     override x.Initialize() =
         base.Initialize ()
-        let syntaxMode = new FSharpSyntaxMode (this.Document)
-        this.TextEditorData.Document.SyntaxMode <- syntaxMode
+        //let syntaxMode = new FSharpSyntaxMode (this.Editor, this.DocumentContext)
+        //this.Editor.SemanticHighlighting <- syntaxMode
+
+    override x.ResolveAsync (token) =
+        Async.StartAsTask (
+            cancellationToken = token,
+            computation = async {
+            try
+                let line, col, lineStr = MonoDevelop.getLineInfoFromOffset(x.Editor.CaretOffset, x.Editor)
+                let currentFile = x.DocumentContext.Name
+                let source = x.Editor.Text
+
+                let projectFilename, files, args = MonoDevelop.getCheckerArgs(x.DocumentContext.Project, currentFile)
+
+                let! symbolReferences = MDLanguageService.Instance.GetUsesOfSymbolAtLocationInFile (projectFilename, currentFile, source, files, line, col, lineStr, args)
+                return symbolReferences
+            with
+            | :? OperationCanceledException -> return None
+            | exn -> LoggingService.LogError("Unhandled Exception in F# HighlightingUsagesExtension", exn)
+                     return None })
             
-    override x.TryResolve(resolveResult) =
-        true
-
-    override x.GetReferences(_, token) =
-        try
-            let line, col, lineStr = MonoDevelop.getLineInfoFromOffset(this.Editor.Caret.Offset, this.Editor.Document)
-            let currentFile = this.FileName.FullPath.ToString()
-            let source = this.Editor.Text
-            let projectContent = this.Document.ProjectContent
-
-            let projectFilename, files, args = MonoDevelop.getCheckerArgs(this.Document.Project, currentFile)
-
-            let symbolReferences =
-                try Async.RunSynchronously(async{return! MDLanguageService.Instance.GetUsesOfSymbolAtLocationInFile(projectFilename, currentFile, source, files, line, col, lineStr, args)},
-                                           timeout = ServiceSettings.blockingTimeout,
-                                           cancellationToken = token)
-                with
-                | :? TimeoutException -> LoggingService.LogWarning ("Highlight usages timed out")
-                                         None
-                | exn -> LoggingService.LogError ("Highlight usages error", exn)
-                         None
-
-            match symbolReferences with
-            | Some(fsSymbolName, references) -> 
-                seq{for symbolUse in references do
-                      yield NRefactory.createMemberReference(projectContent, symbolUse, currentFile, source, fsSymbolName) }
-            | _ -> Seq.empty
-                            
-        with
-        | :? OperationCanceledException -> Seq.empty
-        | exn -> LoggingService.LogError("Unhandled Exception in F# HighlightingUsagesExtension", exn)
-                 Seq.empty       
+    override x.GetReferences(resolveResult, token) =
+        if token.IsCancellationRequested then Seq.empty else
+            try
+                match resolveResult with
+                | Some(fsSymbolName, references) -> 
+                    references
+                    |> Seq.map (fun symbolUse -> NRefactory.createMemberReference(x.Editor, x.DocumentContext, symbolUse, fsSymbolName))
+                | _ -> Seq.empty
+                                
+            with
+            | :? OperationCanceledException -> Seq.empty
+            | exn -> LoggingService.LogError("Unhandled Exception in F# HighlightingUsagesExtension", exn)
+                     Seq.empty  
