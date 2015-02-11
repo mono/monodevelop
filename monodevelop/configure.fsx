71b02427e73cbd23aceb3e651eb545ff06db0cea
Dave Thomas <7sharp9@users.noreply.github.com>
2015-02-11 12:29:28 +0000

    bump version number
    5.8 now in monodevelop master

--- monodevelop/configure.fsx
+++ monodevelop/configure.fsx
@@ -10,7 +10,7 @@ open System.IO
 open System.Diagnostics
 open System.Text.RegularExpressions
 
-let FSharpVersion = "5.8.0"
+let FSharpVersion = "5.8.1"
 
 let UnixPaths = 
     [ "/usr/lib/monodevelop"
