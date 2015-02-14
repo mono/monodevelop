61e59d4acb1f87a9e3cad2fe5980bfb370046c52
Marius Ungureanu <marius.ungureanu@xamarin.com>
2015-02-14 03:07:43 +0200

    [Addins] Fix broken PlatformCondition.

--- main/src/core/MonoDevelop.Core/MonoDevelop.Core.AddIns/PlatformCondition.cs
+++ main/src/core/MonoDevelop.Core/MonoDevelop.Core.AddIns/PlatformCondition.cs
@@ -57,10 +57,10 @@ namespace MonoDevelop.Core.AddIns
 
 			Version version;
 			if (Version.TryParse (conditionNode.GetAttribute ("minVersion"), out version))
-				result |= (Platform.IsMac ? MacSystemInformation.OsVersion : Environment.OSVersion.Version) >= version;
+				result &= (Platform.IsMac ? MacSystemInformation.OsVersion : Environment.OSVersion.Version) >= version;
 
 			if (Version.TryParse (conditionNode.GetAttribute ("maxVersion"), out version))
-				result |= (Platform.IsMac ? MacSystemInformation.OsVersion : Environment.OSVersion.Version) <= version;
+				result &= (Platform.IsMac ? MacSystemInformation.OsVersion : Environment.OSVersion.Version) <= version;
 
 			return negate ? !result : result;
 		}
