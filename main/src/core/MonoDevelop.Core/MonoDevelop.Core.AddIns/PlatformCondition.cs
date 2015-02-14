aa4f4b3c483c41b48efedc0ca44277b1144418b2
Marius Ungureanu <marius.ungureanu@xamarin.com>
2015-02-14 02:44:58 +0200

    [Addins] PlatformCondition fixup
    Use OS Version, not Darwin Version for checks.

--- main/src/core/MonoDevelop.Core/MonoDevelop.Core.AddIns/PlatformCondition.cs
+++ main/src/core/MonoDevelop.Core/MonoDevelop.Core.AddIns/PlatformCondition.cs
@@ -57,7 +57,7 @@ namespace MonoDevelop.Core.AddIns
 
 			Version version;
 			if (Version.TryParse (conditionNode.GetAttribute ("minVersion"), out version))
-				result |= Environment.OSVersion.Version >= version;
+				result |= (Platform.IsMac ? MacSystemInformation.OsVersion : Environment.OSVersion.Version) >= version;
 
 			return negate ? !result : result;
 		}
