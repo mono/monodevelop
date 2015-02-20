5be253a49e51740c830e9f82de321ba1bd481156
David Karlaš <david.karlas@xamarin.com>
2015-02-20 12:08:43 +0100

    Fixed NRE when right clicking in Assembly Browser

--- main/src/addins/MonoDevelop.SourceEditor2/MonoDevelop.SourceEditor/ExtensibleTextEditor.cs
+++ main/src/addins/MonoDevelop.SourceEditor2/MonoDevelop.SourceEditor/ExtensibleTextEditor.cs
@@ -604,7 +604,7 @@ namespace MonoDevelop.SourceEditor
 			ParameterInformationWindowManager.HideWindow (null, view);
 			HideTooltip ();
 			const string menuPath = "/MonoDevelop/SourceEditor2/ContextMenu/Editor";
-			var ctx = view.WorkbenchWindow.ExtensionContext ?? AddinManager.AddinEngine;
+			var ctx = view.WorkbenchWindow?.ExtensionContext ?? AddinManager.AddinEngine;
 
 			CommandEntrySet cset = IdeApp.CommandService.CreateCommandEntrySet (ctx, menuPath);
 
