9be42d92db6ea8e8a15de7966b7c6b2ba5c68e62
Mike Krüger <mkrueger@xamarin.com>
2015-03-06 16:16:19 +0100

    [Refactoring] Implemented a more lightweight background analysis runner approach.

--- main/src/addins/MonoDevelop.Refactoring/MonoDevelop.CodeIssues/CodeAnalysisRunner.cs
+++ main/src/addins/MonoDevelop.Refactoring/MonoDevelop.CodeIssues/CodeAnalysisRunner.cs
@@ -43,6 +43,7 @@ using Mono.TextEditor;
 using ICSharpCode.NRefactory.Refactoring;
 using MonoDevelop.CodeActions;
 using System.Diagnostics;
+using MonoDevelop.Ide.TypeSystem;
 
 namespace MonoDevelop.CodeIssues
 {
@@ -55,7 +56,7 @@ namespace MonoDevelop.CodeIssues
 			return new BaseCodeIssueProvider[] { p };
 		}
 
-		public static IEnumerable<Result> Check (Document input, CancellationToken cancellationToken)
+		public static IEnumerable<Result> Check (Document input, ParsedDocument parsedDocument, CancellationToken cancellationToken)
 		{
 			if (!QuickTaskStrip.EnableFancyFeatures || input.Project == null || !input.IsCompileableInProject)
 				return Enumerable.Empty<Result> ();
@@ -70,8 +71,9 @@ namespace MonoDevelop.CodeIssues
 			var result = new BlockingCollection<Result> ();
 		
 			var codeIssueProvider = RefactoringService.GetInspectors (editor.Document.MimeType).ToArray ();
-			var context = input.ParsedDocument.CreateRefactoringContext != null ?
-				input.ParsedDocument.CreateRefactoringContext (input, cancellationToken) : null;
+
+			var context = parsedDocument.CreateRefactoringContext != null ?
+				parsedDocument.CreateRefactoringContext (input, cancellationToken) : null;
 			Parallel.ForEach (codeIssueProvider, (parentProvider) => {
 				try {
 					#if PROFILE
