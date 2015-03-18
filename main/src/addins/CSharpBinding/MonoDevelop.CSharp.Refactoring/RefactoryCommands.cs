06e734c99bb4f910192ef45a962b977e0173a40d
Mike Krüger <mkrueger@xamarin.com>
2015-03-18 16:51:49 +0100

    [CSharpBinding] Find refs work now on symbols from referenced assemblies.

--- main/src/addins/CSharpBinding/MonoDevelop.CSharp.Refactoring/RefactoryCommands.cs
+++ main/src/addins/CSharpBinding/MonoDevelop.CSharp.Refactoring/RefactoryCommands.cs
@@ -192,19 +192,18 @@ namespace MonoDevelop.CSharp.Refactoring
 				added = true;
 			}
 
-			if (canRename) {
-				var sym = info.Symbol ?? info.DeclaredSymbol;
-				ainfo.Add (IdeApp.CommandService.GetCommandInfo (RefactoryCommands.FindReferences), new System.Action (() => FindReferencesHandler.FindRefs (sym)));
-				if (doc.HasProject && sym != null) {
-					try {
-						if (Microsoft.CodeAnalysis.FindSymbols.SymbolFinder.FindSimilarSymbols (sym, semanticModel.Compilation).Count () > 1)
-							ainfo.Add (IdeApp.CommandService.GetCommandInfo (RefactoryCommands.FindAllReferences), new System.Action (() => FindAllReferencesHandler.FindRefs (info.Symbol, semanticModel.Compilation)));
-					} catch (Exception) {
-						// silently ignore roslyn bug.
-					}
+			var sym = info.Symbol ?? info.DeclaredSymbol;
+			ainfo.Add (IdeApp.CommandService.GetCommandInfo (RefactoryCommands.FindReferences), new System.Action (() => FindReferencesHandler.FindRefs (sym)));
+			if (doc.HasProject && sym != null) {
+				try {
+					if (Microsoft.CodeAnalysis.FindSymbols.SymbolFinder.FindSimilarSymbols (sym, semanticModel.Compilation).Count () > 1)
+						ainfo.Add (IdeApp.CommandService.GetCommandInfo (RefactoryCommands.FindAllReferences), new System.Action (() => FindAllReferencesHandler.FindRefs (info.Symbol, semanticModel.Compilation)));
+				} catch (Exception) {
+					// silently ignore roslyn bug.
 				}
-				added = true;
 			}
+			added = true;
+
 			if (info.DeclaredSymbol != null) {
 				string description;
 				if (FindDerivedSymbolsHandler.CanFindDerivedSymbols (info.DeclaredSymbol, out description)) {
