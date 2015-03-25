2aba089545e4d351d48e90963501c241e1bd2c08
Michael Hutchinson <m.j.hutchinson@gmail.com>
2015-03-25 13:45:46 -0400

    Update winbuild.sh
    Need to restore NuGet packages now for the solution
    to be buildable.

--- main/winbuild.sh
+++ main/winbuild.sh
@@ -1,3 +1,4 @@
+./external/nuget-binary/NuGet.exe restore Main.sln
 export PATH="/c/Windows/Microsoft.NET/Framework/v4.0.30319:$PATH"
 pushd ..; git submodule update --init --recursive || exit 1; popd
 MSBuild.exe Main.sln -p:Configuration=DebugWin32 $*
