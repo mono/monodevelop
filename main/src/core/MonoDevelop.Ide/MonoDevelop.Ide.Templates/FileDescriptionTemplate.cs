680ae39ac8898822e78d629de742051b0760b3d2
Anubhav singh <gitexperience@users.noreply.github.com>
2015-04-15 09:59:32 -0700

    [Ide] check for invalid characters for filename
    Provides creation of filename with only valid characters.

--- main/src/core/MonoDevelop.Ide/MonoDevelop.Ide.Templates/FileDescriptionTemplate.cs
+++ main/src/core/MonoDevelop.Ide/MonoDevelop.Ide.Templates/FileDescriptionTemplate.cs
@@ -77,9 +77,11 @@ namespace MonoDevelop.Ide.Templates
 
 		internal string CreateCondition { get; private set; }
 		
+		static readonly char [] InvalidFileNameCharacters = "&<*;?>%:#|".ToCharArray();
+		
 		public virtual bool IsValidName (string name, string language)
 		{
-			return name.Length > 0 && name.IndexOfAny (Path.GetInvalidFileNameChars ()) == -1;
+			return name.Length > 0 && name.IndexOfAny (InvalidFileNameCharacters) == -1;
 /*			if (name.Length > 0) {
 				if (language != null && language.Length > 0) {
 					IDotNetLanguageBinding binding = LanguageBindingService.GetBindingPerLanguageName (language) as IDotNetLanguageBinding;
