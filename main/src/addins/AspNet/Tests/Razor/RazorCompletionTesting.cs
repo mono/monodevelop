5bd283f5867af6e41f0905140d179bfc05a6f9e5
Lluis Sanchez Gual <lluis@xamarin.com>
2015-04-23 20:18:08 +0200

    Track api changes

--- main/src/addins/AspNet/Tests/Razor/RazorCompletionTesting.cs
+++ main/src/addins/AspNet/Tests/Razor/RazorCompletionTesting.cs
@@ -124,7 +124,7 @@ namespace MonoDevelop.AspNet.Tests.Razor
 				cursorPosition = endPos - 1;
 			}
 
-			var project = new AspNetAppProject ("C#");
+			var project = Services.ProjectService.CreateProject ("C#", "AspNetApp");
 
 			project.FileName = UnitTests.TestBase.GetTempFile (".csproj");
 			string file = UnitTests.TestBase.GetTempFile (extension);
