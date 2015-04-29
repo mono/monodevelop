d73e2bd97072d7706340f127b81982b60be5fe56
Marius Ungureanu <marius.ungureanu@xamarin.com>
2015-04-29 19:27:23 +0300

    [Core] PasswordService cleanup.

--- main/src/core/MonoDevelop.Core/MonoDevelop.Core/PasswordService.cs
+++ main/src/core/MonoDevelop.Core/MonoDevelop.Core/PasswordService.cs
@@ -32,31 +32,35 @@ namespace MonoDevelop.Core
 {
 	public static class PasswordService
 	{
-		const string PasswordProvidersPath = "/MonoDevelop/Core/PasswordProvider";
+		static IPasswordProvider GetPasswordProvider ()
+		{
+			const string PasswordProvidersPath = "/MonoDevelop/Core/PasswordProvider";
+			return AddinManager.GetExtensionObjects <IPasswordProvider> (PasswordProvidersPath).FirstOrDefault ();
+		}
 
 		public static void AddWebPassword (Uri url, string password)
 		{
-			var provider = AddinManager.GetExtensionObjects <IPasswordProvider> (PasswordProvidersPath).FirstOrDefault ();
+			var provider = GetPasswordProvider ();
 			if (provider != null)
 				provider.AddWebPassword (url, password);
 		}
 
 		public static void AddWebUserNameAndPassword (Uri url, string username, string password)
 		{
-			var provider = AddinManager.GetExtensionObjects<IPasswordProvider> (PasswordProvidersPath).FirstOrDefault ();
+			var provider = GetPasswordProvider ();
 			if (provider != null)
 				provider.AddWebUserNameAndPassword (url, username, password);
 		}
 
 		public static string GetWebPassword (Uri url)
 		{
-			var provider = AddinManager.GetExtensionObjects<IPasswordProvider> (PasswordProvidersPath).FirstOrDefault ();
+			var provider = GetPasswordProvider ();
 			return provider != null ? provider.GetWebPassword (url) : null;
 		}
 
 		public static Tuple<string, string> GetWebUserNameAndPassword (Uri url)
 		{
-			var provider = AddinManager.GetExtensionObjects<IPasswordProvider> (PasswordProvidersPath).FirstOrDefault ();
+			var provider = GetPasswordProvider ();
 			return provider != null ? provider.GetWebUserNameAndPassword (url) : null;
 		}
 	}
