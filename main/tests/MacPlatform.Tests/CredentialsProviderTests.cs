3b33c55af76fb40f4f53dac76fdd294538046a2a
Marius Ungureanu <marius.ungureanu@xamarin.com>
2015-04-29 19:38:46 +0300

    [Core] Windows and Mac implementation of PasswordProvider removal.

--- main/tests/MacPlatform.Tests/CredentialsProviderTests.cs
+++ main/tests/MacPlatform.Tests/CredentialsProviderTests.cs
@@ -1,11 +1,10 @@
 ﻿//
-// MacKeychainPasswordProvider.cs
+// CredentialsProviderTests.cs
 //
 // Author:
-//       Alan McGovern <alan@xamarin.com>
-//       Bojan Rajkovic <brajkovic@xamarin.com>
+//       Marius Ungureanu <marius.ungureanu@xamarin.com>
 //
-// Copyright (c) 2012 - 2013, Xamarin Inc.
+// Copyright (c) 2015 Xamarin, Inc (http://www.xamarin.com)
 //
 // Permission is hereby granted, free of charge, to any person obtaining a copy
 // of this software and associated documentation files (the "Software"), to deal
@@ -24,43 +23,18 @@
 // LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
 // OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
 // THE SOFTWARE.
-
 using System;
-using MonoDevelop.Core;
-using MonoDevelop.MacInterop;
+using NUnit.Framework;
 
-namespace MonoDevelop.MacIntegration
+namespace MacPlatform.Tests
 {
-	public class MacKeychainPasswordProvider : IPasswordProvider
+	[TestFixture]
+	public class CredentialsProviderTests : MonoDevelop.Core.BaseCredentialsProviderTests
 	{
-		public void AddWebPassword (Uri uri, string password)
+		protected override MonoDevelop.Core.IPasswordProvider GetPasswordProvider ()
 		{
-			Keychain.AddInternetPassword (uri, password);
-		}
-
-		public string GetWebPassword (Uri uri)
-		{
-			return Keychain.FindInternetPassword (uri);
-		}
-
-		public void AddWebUserNameAndPassword (Uri url, string username, string password)
-		{
-			Keychain.AddInternetPassword (url, username, password);
-		}
-
-		public Tuple<string, string> GetWebUserNameAndPassword (Uri url)
-		{
-			return Keychain.FindInternetUserNameAndPassword (url);
-		}
-
-		public void RemoveWebPassword (Uri uri)
-		{
-			Keychain.RemoveInternetPassword (uri);
-		}
-
-		public void RemoveWebUserNameAndPassword (Uri uri)
-		{
-			Keychain.RemoveInternetUserNameAndPassword (uri);
+			return new MonoDevelop.MacIntegration.MacKeychainPasswordProvider ();
 		}
 	}
 }
+
