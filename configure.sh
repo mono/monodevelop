3d01c09a45641030c1a10fa11419db0f05e2611a
7sharp9 <dave.thomas@xamarin.com>
2015-05-06 17:47:11 +0100

    Updated nuget management to use paket

--- configure.sh
+++ configure.sh
@@ -1,6 +1,6 @@
 #! /bin/bash -e
 echo "Configuring..."
 fsharpi configure.fsx "$@"
-echo "Getting nuget packages..."
 mozroots --import --sync --quiet || echo 'Could not import mozroots, proceeding anyway'
-mono .nuget/NuGet.exe restore MonoDevelop.FSharp.mac-linux.sln
+echo "Restoring nuget packages with paket..."
+mono .paket/paket.exe restore
