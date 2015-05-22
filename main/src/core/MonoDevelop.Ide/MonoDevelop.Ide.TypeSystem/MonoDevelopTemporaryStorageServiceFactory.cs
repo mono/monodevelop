790fa2b50fc6d8daf40b69beb3fd86fc8f58d9b7
Mike Krüger <mkrueger@xamarin.com>
2015-05-22 13:45:02 +0200

    [Ide] Implemented custom ITemporaryStorageService factory. That should reduce roslyn memory consumption.

--- main/src/core/MonoDevelop.Ide/MonoDevelop.Ide.TypeSystem/MonoDevelopTemporaryStorageServiceFactory.cs
+++ main/src/core/MonoDevelop.Ide/MonoDevelop.Ide.TypeSystem/MonoDevelopTemporaryStorageServiceFactory.cs
@@ -24,12 +24,125 @@
 // OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
 // THE SOFTWARE.
 using System;
+using System.Composition;
+using System.IO;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.CodeAnalysis.Host;
+using Microsoft.CodeAnalysis.Host.Mef;
+using Microsoft.CodeAnalysis.Text;
+using MonoDevelop.Core.Text;
+
 namespace MonoDevelop.Ide.TypeSystem
 {
-	public class MonoDevelopTemporaryStorageServiceFactory
+	[ExportWorkspaceServiceFactory(typeof(ITemporaryStorageService), ServiceLayer.Host), Shared]
+	sealed class MonoDevelopTemporaryStorageServiceFactory : IWorkspaceServiceFactory
 	{
-		public MonoDevelopTemporaryStorageServiceFactory ()
+		public IWorkspaceService CreateService(HostWorkspaceServices workspaceServices)
 		{
+			return new TemporaryStorageService();
+		}
+
+		class TemporaryStorageService : ITemporaryStorageService
+		{
+			public ITemporaryStreamStorage CreateTemporaryStreamStorage (CancellationToken cancellationToken = default(CancellationToken))
+			{
+				return new StreamStorage(Path.GetTempFileName ());
+			}
+
+			public ITemporaryTextStorage CreateTemporaryTextStorage (CancellationToken cancellationToken = default(CancellationToken))
+			{
+				return new TemporaryTextStorage(Path.GetTempFileName ());
+			}
+		}
+
+		class TemporaryTextStorage : ITemporaryTextStorage
+		{
+			string fileName;
+
+			public TemporaryTextStorage (string fileName)
+			{
+				this.fileName = fileName;
+			}
+
+			public void Dispose()
+			{
+				try {
+					Console.WriteLine ("remove : "+ fileName);
+					File.Delete (fileName);
+				} catch (Exception) {}
+			}
+
+			public SourceText ReadText(CancellationToken cancellationToken = default(CancellationToken))
+			{
+				var src = StringTextSource.ReadFrom (fileName);
+				Console.WriteLine (fileName +" read text : "+ src.Length);
+				return SourceText.From(src.Text, src.Encoding);
+			}
+
+			public Task<SourceText> ReadTextAsync(CancellationToken cancellationToken = default(CancellationToken))
+			{
+				return Task.Run(delegate { return ReadText (cancellationToken); });
+			}
+
+			public void WriteText(SourceText text, CancellationToken cancellationToken = default(CancellationToken))
+			{
+				Console.WriteLine ("write text :" + text.Length);
+				using (var writer = new StreamWriter(fileName, false, text.Encoding))
+					text.Write (writer, cancellationToken);
+			}
+
+			Task ITemporaryTextStorage.WriteTextAsync(SourceText text, CancellationToken cancellationToken)
+			{
+				return Task.Run (delegate {
+					WriteText (text, cancellationToken);
+				});
+			}
+		}
+
+		class StreamStorage : ITemporaryStreamStorage
+		{
+			string fileName;
+			Stream stream;
+
+			public StreamStorage (string fileName)
+			{
+				this.fileName = fileName;
+			}
+
+			public void Dispose()
+			{
+				stream.Close ();
+				try {
+					File.Delete (fileName);
+				} catch (Exception) {}
+
+			}
+
+			public Stream ReadStream(CancellationToken cancellationToken = default(CancellationToken))
+			{
+				return File.Open (fileName, FileMode.Open, FileAccess.Read, FileShare.Read);
+			}
+
+			public Task<Stream> ReadStreamAsync(CancellationToken cancellationToken = default(CancellationToken))
+			{
+				return Task.FromResult((Stream)File.Open (fileName, FileMode.Open, FileAccess.Read, FileShare.Read));
+			}
+
+			public void WriteStream(Stream stream, CancellationToken cancellationToken = default(CancellationToken))
+			{
+				using (var newStream = File.Open (fileName, FileMode.CreateNew, FileAccess.Write, FileShare.Write)) {
+					stream.CopyTo(newStream);
+				}
+			}
+
+			public async Task WriteStreamAsync(Stream stream, CancellationToken cancellationToken = default(CancellationToken))
+			{
+				using (var newStream = File.Open (fileName, FileMode.CreateNew, FileAccess.Write, FileShare.Write)) {
+					await stream.CopyToAsync(newStream).ConfigureAwait(false);
+				}
+			}
 		}
 	}
 }
