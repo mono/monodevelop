cb859bd6cb0f46dbd4811e5570cacc3de6971bdd
iain holmes <iain@xamarin.com>
2015-05-27 17:35:00 +0100

    [AutoTest] Handle stock ids when checking button labels

--- main/src/core/MonoDevelop.Ide/MonoDevelop.Components.AutoTest.Results/GtkWidgetResult.cs
+++ main/src/core/MonoDevelop.Ide/MonoDevelop.Components.AutoTest.Results/GtkWidgetResult.cs
@@ -113,6 +113,22 @@ namespace MonoDevelop.Components.AutoTest.Results
 			pinfo = resultWidget.GetType ().GetProperty ("Label");
 			if (pinfo != null) {
 				string propText = (string)pinfo.GetValue (resultWidget);
+
+				Button button = resultWidget as Button;
+				// If resultWidget is a button then the label may not be the actual label that is displayed
+				// but rather a stock ID. So we may need to translate it
+				if (button != null && button.UseStock && propText != null) {
+					StockItem item = Stock.Lookup (propText);
+					propText = item.Label;
+				}
+
+				if (button != null && button.UseUnderline) {
+					int indexOfUnderline = propText.IndexOf ("_");
+					if (indexOfUnderline > -1) {
+						propText = propText.Remove (indexOfUnderline, 1);
+					}
+				}
+
 				if (CheckForText (propText, text, exact)) {
 					return this;
 				}
