a7d60080415f06d7e17bca9566f3392abe332df4
Mike Krüger <mkrueger@xamarin.com>
2015-05-27 10:01:52 +0200

    [Ide] MonoDevelopSourceTextContainer now uses the Disposable pattern. This way it's easier to control the correct order of the disposal. The document may've already been disposed when the finalizer runs.

--- main/src/core/MonoDevelop.Ide/MonoDevelop.Ide.TypeSystem/MonoDevelopWorkspace.cs
+++ main/src/core/MonoDevelop.Ide/MonoDevelop.Ide.TypeSystem/MonoDevelopWorkspace.cs
@@ -621,7 +621,7 @@ namespace MonoDevelop.Ide.TypeSystem
 			lock (openDocuments) {
 				var openDoc = openDocuments.FirstOrDefault (d => d.Id == documentId);
 				if (openDoc != null) {
-//					openDoc.TextChanged -= HandleTextChanged;
+					openDoc.Dispose ();
 					openDocuments.Remove (openDoc);
 				}
 			}
