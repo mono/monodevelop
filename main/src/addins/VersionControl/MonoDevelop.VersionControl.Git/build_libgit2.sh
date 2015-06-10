8b53df47145fca6a3a6684c9e166749e2e5e6080
Marius Ungureanu <marius.ungureanu@xamarin.com>
2015-06-10 16:44:18 +0300

    Update linux script.

--- main/src/addins/VersionControl/MonoDevelop.VersionControl.Git/build_libgit2.sh
+++ main/src/addins/VersionControl/MonoDevelop.VersionControl.Git/build_libgit2.sh
@@ -1,28 +1,28 @@
 #!/bin/bash
 
-pushd ../../../../external/libgit2sharp/
-LIBGIT2SHA=`cat LibGit2Sharp/libgit2_hash.txt`
+pushd ../../../../external/libgit2/
+LIBGIT2SHA=`cat ../libgit-binary/libgit2_hash.txt`
 SHORTSHA=${LIBGIT2SHA:0:7}
 
-if [[ -d libgit2/build ]]
+if [[ -d build ]]
 then
-    pushd libgit2/build
+    pushd build
 
     if [[ -n $(ls libgit2-${SHORTSHA}.*) ]]
     then
         exit 0
     else
         popd
-        rm -rf libgit2/build
+        rm -rf build
     fi
 fi
 
-cp libgit2/CMakeLists.txt libgit2/.CMakeLists.txt.mdcopy
-echo 'SET(CMAKE_SKIP_RPATH TRUE)' > libgit2/CMakeLists.txt
-cat libgit2/.CMakeLists.txt.mdcopy >> libgit2/CMakeLists.txt
+cp CMakeLists.txt .CMakeLists.txt.mdcopy
+echo 'SET(CMAKE_SKIP_RPATH TRUE)' > CMakeLists.txt
+cat .CMakeLists.txt.mdcopy >> CMakeLists.txt
 
-mkdir libgit2/build
-pushd libgit2/build
+mkdir build
+pushd build
 
 cmake -DCMAKE_BUILD_TYPE:STRING=RelWithDebInfo \
       -DTHREADSAFE:BOOL=ON \
@@ -35,5 +35,5 @@ cmake -DCMAKE_BUILD_TYPE:STRING=RelWithDebInfo \
 cmake --build .
 popd
 
-mv libgit2/.CMakeLists.txt.mdcopy libgit2/CMakeLists.txt
+mv .CMakeLists.txt.mdcopy CMakeLists.txt
 popd
