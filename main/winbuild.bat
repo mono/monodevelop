b4b5b0424224aadf1acd247dc70efeef4ff45b71
David Karlaš <david.karlas@xamarin.com>
2015-06-22 15:25:48 +0200

    WinBuild.bat/sh added restore RefactoringEssentials packages so RefactoringEssential.csproj hintpaths have .dll as expected

--- main/winbuild.bat
+++ main/winbuild.bat
@@ -1,2 +1,3 @@
 ".nuget\NuGet.exe" restore Main.sln
+".nuget\NuGet.exe" restore external\RefactoringEssentials\RefactoringEssentials.sln
 "C:\Program Files (x86)\MSBuild\14.0\Bin\MSBuild.exe" Main.sln /m /p:Configuration=DebugWin32 /p:Platform="Any CPU"
