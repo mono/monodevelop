mono/monodevelop

commit 0e2cabf935f0940a315e5302396c8b2cdf5152e4

Changed font size of menu on code actions tooltip

main/src/addins/MonoDevelop.Refactoring/MonoDevelop.CodeActions/CodeActionEditorExtension.cs
@@ -433,7 +433,9 @@ namespace MonoDevelop.CodeActions
 
 		AppKit.NSMenu CreateNSMenu (FixMenuDescriptor entrySet)
 		{
-			var menu = new AppKit.NSMenu ();
+			var menu = new AppKit.NSMenu {
+				Font = NSFont.MenuFontOfSize (12),
+			};
 			foreach (var item in entrySet.Items) {
 				if (item == FixMenuEntry.Separator) {
 					menu.AddItem (AppKit.NSMenuItem.SeparatorItem);

commit a35a80aac57d657655b103b2050061810d12df2b

Fix build breakage caused by 0e2cabf935f0940a315e5302396c8b2cdf5152e4

main/src/addins/MonoDevelop.Refactoring/MonoDevelop.CodeActions/CodeActionEditorExtension.cs
@@ -434,7 +434,7 @@ namespace MonoDevelop.CodeActions
 		AppKit.NSMenu CreateNSMenu (FixMenuDescriptor entrySet)
 		{
 			var menu = new AppKit.NSMenu {
-				Font = NSFont.MenuFontOfSize (12),
+				Font = AppKit.NSFont.MenuFontOfSize (12),
 			};
 			foreach (var item in entrySet.Items) {
 				if (item == FixMenuEntry.Separator) {