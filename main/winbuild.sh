a30d9f39eb14ad2f74c091ef4914550b73809002
Michael Hutchinson <m.j.hutchinson@gmail.com>
2015-07-18 23:01:02 -0400

    Fix winbuild.sh

--- main/winbuild.sh
+++ main/winbuild.sh
@@ -1,5 +1,5 @@
 export PATH="/c/Program Files (x86)/MSBuild/14.0/Bin:$PATH"
 pushd ..; git submodule update --init --recursive || exit 1; popd
 ./.nuget/NuGet.exe restore Main.sln
-./.nuget/NuGet.exe restore restore external/RefactoringEssentials/RefactoringEssentials.sln
+./.nuget/NuGet.exe restore external/RefactoringEssentials/RefactoringEssentials.sln
 MSBuild.exe -m Main.sln -p:Configuration=DebugWin32 $*
