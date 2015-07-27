83b20708a7dd1d09cb365e6129a99e952b537334
Lluis Sanchez <lluis@xamarin.com>
2015-07-27 13:44:29 +0200

    Optimize setting the startup project
    When changing the startup project, only save the user properties,
    not the whole solution.
    
    Don't read the startup project from the solution file, since it is
    now stored as user property.
    
    Fixes bug #32389 - Changing startup project takes 15+ seconds

--- main/src/core/MonoDevelop.Ide/MonoDevelop.Ide.Gui.Pads.ProjectPad/ProjectNodeBuilder.cs
+++ main/src/core/MonoDevelop.Ide/MonoDevelop.Ide.Gui.Pads.ProjectPad/ProjectNodeBuilder.cs
@@ -402,9 +402,12 @@ namespace MonoDevelop.Ide.Gui.Pads.ProjectPad
 		public void SetAsStartupProject ()
 		{
 			Project project = CurrentNode.DataItem as Project;
-			project.ParentSolution.SingleStartup = true;
 			project.ParentSolution.StartupItem = project;
-			IdeApp.ProjectOperations.Save (project.ParentSolution);
+			if (!project.ParentSolution.SingleStartup) {
+				project.ParentSolution.SingleStartup = true;
+				IdeApp.ProjectOperations.Save (project.ParentSolution);
+			} else
+				project.ParentSolution.SaveUserProperties ();
 		}
 		
 		public override void DeleteItem ()
