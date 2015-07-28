20b69cc721bed1d473fbf212af36a164b73a4bc7
iain holmes <iain@xamarin.com>
2015-07-28 11:58:53 +0100

    [Tests] Add more tests to the MacPlatform test suite

--- main/tests/MacPlatform.Tests/AppleScriptTests.cs
+++ main/tests/MacPlatform.Tests/AppleScriptTests.cs
@@ -1,21 +1,21 @@
-// 
-// MacPlatformTest.cs
-//  
+﻿//
+// AppleScriptTests.cs
+//
 // Author:
-//       Alan McGovenrn <alan@xamarin.com>
-// 
-// Copyright (c) 2012 Xamarin Inc.
-// 
+//       iain holmes <iain@xamarin.com>
+//
+// Copyright (c) 2015 Xamarin, Inc
+//
 // Permission is hereby granted, free of charge, to any person obtaining a copy
 // of this software and associated documentation files (the "Software"), to deal
 // in the Software without restriction, including without limitation the rights
 // to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
 // copies of the Software, and to permit persons to whom the Software is
 // furnished to do so, subject to the following conditions:
-// 
+//
 // The above copyright notice and this permission notice shall be included in
 // all copies or substantial portions of the Software.
-// 
+//
 // THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
 // IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
 // FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
@@ -24,36 +24,20 @@
 // OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
 // THE SOFTWARE.
 using System;
-using NUnit.Framework;
-using MonoDevelop.MacIntegration;
 using MonoDevelop.MacInterop;
-using MonoDevelop.Ide;
-using UnitTests;
+using NUnit.Framework;
 
 namespace MacPlatform.Tests
 {
-	[TestFixture]
-	public class MacPlatformTest : TestBase
+	public class AppleScriptTests
 	{
 		[Test]
-		public void GetMimeType_text ()
+		public void TestAppleScript ()
 		{
-			// Verify no exception is thrown
-			DesktopService.GetMimeTypeForUri ("test.txt");
-		}
-
-		[Test]
-		public void GetMimeType_NoExtension ()
-		{
-			// Verify no exception is thrown
-			DesktopService.GetMimeTypeForUri ("test");
-		}
-
-		[Test]
-		public void GetMimeType_Null ()
-		{
-			// Verify no exception is thrown
-			DesktopService.GetMimeTypeForUri (null);
+			// Include the quotes in the test string because AppleScript is a bit janky
+			string testStr = "\"hello world\"";
+			string result = AppleScript.Run ("return {0}", testStr);
+			Assert.AreEqual (result, testStr);
 		}
 	}
 }
