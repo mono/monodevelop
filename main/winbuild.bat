f084d138a4a431d9d9a64941fc91a805ff4c6456
Marius Ungureanu <marius.ungureanu@xamarin.com>
2015-09-01 16:06:54 +0300

    [Windows] Prefer MSBuild14 if available.

--- main/winbuild.bat
+++ main/winbuild.bat
@@ -1,1 +1,7 @@
-"%WinDir%\Microsoft.NET\Framework\v4.0.30319\MSBuild.exe" Main.sln /m /p:Configuration=DebugWin32 /p:Platform="Any CPU"
+set filename=%ProgramFiles(x86)%\MSBuild\14.0\Bin\MSBuild.exe
+
+if not exist filename (
+	filename=%WinDir%\Microsoft.NET\Framework\v4.0.30319\MSBuild.exe
+)
+
+"%filename%" Main.sln /m /p:Configuration=DebugWin32 /p:Platform="Any CPU"
