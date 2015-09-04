3e8246fe731c3651da6ac5ab86d094b6d74a5cd8
Marius Ungureanu <teromario@yahoo.com>
2015-09-04 11:48:13 +0300

    [Windows] Fix another issue in the build script.

--- main/winbuild.bat
+++ main/winbuild.bat
@@ -1,7 +1,7 @@
 set filename=%ProgramFiles(x86)%\MSBuild\14.0\Bin\MSBuild.exe
 
 if not exist %filename% (
-	filename=%WinDir%\Microsoft.NET\Framework\v4.0.30319\MSBuild.exe
+	set filename=%WinDir%\Microsoft.NET\Framework\v4.0.30319\MSBuild.exe
 )
 
 "%filename%" Main.sln /m /p:Configuration=DebugWin32 /p:Platform="Any CPU"
