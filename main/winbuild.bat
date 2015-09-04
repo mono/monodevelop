06213ae9b84946171daad99d0a0ed5f94924de95
Marius Ungureanu <teromario@yahoo.com>
2015-09-04 11:47:26 +0300

    [Windows] Fix issue in build script
    Thanks @mrward!

--- main/winbuild.bat
+++ main/winbuild.bat
@@ -1,6 +1,6 @@
 set filename=%ProgramFiles(x86)%\MSBuild\14.0\Bin\MSBuild.exe
 
-if not exist filename (
+if not exist %filename% (
 	filename=%WinDir%\Microsoft.NET\Framework\v4.0.30319\MSBuild.exe
 )
 
