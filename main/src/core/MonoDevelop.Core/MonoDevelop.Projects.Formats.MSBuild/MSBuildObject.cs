3c85d2ac0974846419fe0f3dfd324c5da28a66d6
Lluis Sanchez <lluis@xamarin.com>
2015-09-04 12:05:01 +0200

    Disable some debug code
    Fixes bug #33639 - [Roslyn] Index out of range exception when opening projects

--- main/src/core/MonoDevelop.Core/MonoDevelop.Projects.Formats.MSBuild/MSBuildObject.cs
+++ main/src/core/MonoDevelop.Core/MonoDevelop.Projects.Formats.MSBuild/MSBuildObject.cs
@@ -24,7 +24,7 @@
 // OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
 // THE SOFTWARE.
 
-#define ATTR_STATS
+//#define ATTR_STATS
 
 using System;
 using System.Collections.Generic;
