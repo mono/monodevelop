fd8ab6fe55a103f10e5da56eea2785c3ee565355
nosami <jasonimison@gmail.com>
2015-09-09 12:41:16 +0100

    gtk code for outline pad

--- MonoDevelop.FSharpBinding/FSharpOutlineTextEditorExtension.fs
+++ MonoDevelop.FSharpBinding/FSharpOutlineTextEditorExtension.fs
@@ -26,11 +26,12 @@ type FSharpOutlineTextEditorExtension() =
 //        let documentParsed  = x.DocumentContext.DocumentParsed.Subscribe(fun o e -> x.PathUpdated())
         x.DocumentContext.DocumentParsed.Add(x.UpdateDocumentOutline)
 
-    //override x.IsValidInContext _ =
+    override x.IsValidInContext _ =
+        true
     //    IdeApp.Workbench.ActiveDocument <> null && IdeApp.Workbench.ActiveDocument.Name = x.DocumentContext.Name
 
     member private x.UpdateDocumentOutline _ =
-        let ast = 
+        let ast =
             maybe {let! context = x.DocumentContext |> Option.ofNull
                    let! parsedDocument = context.ParsedDocument |> Option.ofNull
                    let! ast = parsedDocument.Ast |> Option.tryCast<ParseAndCheckResults>
@@ -39,15 +40,19 @@ type FSharpOutlineTextEditorExtension() =
         DispatchService.AssertGuiThread ()
         Gdk.Threads.Enter ()
         ast |> Option.iter (fun ast ->
-            treeStore.AppendValues("2") |> ignore
+            //treeStore.AppendValues(TreeIter.Zero, [|"1"|]) |> ignore
+            treeStore.AppendValues("1") |> ignore
 
             match treeView with
-            | Some(treeView) -> treeView.ExpandAll ()
+            | Some(treeView) -> 
+                treeView.ExpandAll ()
+                //treeView.Selection.SelectIter (TreeIter.Zero)
             | None -> ()
             ()
+
         )
         Gdk.Threads.Leave ()
- 
+
         ()
 
     override x.Dispose () =
@@ -62,15 +67,23 @@ type FSharpOutlineTextEditorExtension() =
                     let setCellIcon (column: TreeViewColumn) (cellRenderer: CellRenderer) (treeModel: TreeModel) (iter: TreeIter) =
                         ()
 
-                    let refillTree _ = 
+                    let setCellText (column: TreeViewColumn) (cellRenderer: CellRenderer) (treeModel: TreeModel) (iter: TreeIter) =
+                      Console.WriteLine ("F# tree func");
+
+                      let renderer = cellRenderer :?> CellRendererText
+                      let o = treeModel.GetValue (iter, 0)
+                      renderer.Text <- (string o)
+                      ()
+
+                    let refillTree _ =
                         DispatchService.AssertGuiThread ()
                         Gdk.Threads.Enter ()
-                        treeStore.AppendValues("1") |> ignore
+                        treeStore.AppendValues("23") |> ignore
                         Gdk.Threads.Leave ()
                         ()
-                    let treeStore = new TreeStore(typedefof<Object>)
+                    treeStore <- new TreeStore(typedefof<obj>)
                     let padTreeView = new PadTreeView(treeStore)
-                    let treeView = Some padTreeView
+                    treeView <- Some padTreeView
                     let pixRenderer = new CellRendererImage ()
                     pixRenderer.Xpad <- 0u
                     pixRenderer.Ypad <- 0u
@@ -79,24 +92,26 @@ type FSharpOutlineTextEditorExtension() =
                     padTreeView.TextRenderer.Ypad <- 0u
 
                     let treeCol = new TreeViewColumn ()
+                    treeCol.Title <- "HI I AM A COLUMN"
                     treeCol.PackStart (pixRenderer, false)
+                    // treeCol.AddAttribute (padTreeView.TextRenderer,"text",1)
 
                     treeCol.SetCellDataFunc (pixRenderer, new TreeCellDataFunc (setCellIcon))
                     treeCol.PackStart (padTreeView.TextRenderer, true);
 
-                    treeCol.SetCellDataFunc (padTreeView.TextRenderer, new TreeCellDataFunc (setCellIcon))
+                    treeCol.SetCellDataFunc (padTreeView.TextRenderer, new TreeCellDataFunc (setCellText))
                     padTreeView.AppendColumn (treeCol) |> ignore
 
-                    padTreeView.HeadersVisible <- false
-                    padTreeView.Realized.Add (refillTree)
+                    padTreeView.HeadersVisible <- true
+                    padTreeView.Realized.Add refillTree
                     let sw = new CompactScrolledWindow()
+                    //padTreeView.Model <- treeStore
                     sw.Add padTreeView
                     sw.ShowAll ()
                     sw :> Widget
-        
+
 
         member x.GetToolbarWidgets () =
             List.empty<Widget> :> _
         member x.ReleaseOutlineWidget () =
             ()
-        
