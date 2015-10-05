506e9df18ab681bdb6746eb4fccc67a990a57132
Mike Krüger <mkrueger@xamarin.com>
2015-10-05 07:33:49 +0200

    Fixed 'Bug 34540 - Auto complete / resolve / types completely hosed on new XM due to corrupted cache'.
    Bumping version numbers will force to create a new cache.

--- main/src/core/MonoDevelop.Ide/MonoDevelop.Ide.TypeSystem/TypeSystemService.cs
+++ main/src/core/MonoDevelop.Ide/MonoDevelop.Ide.TypeSystem/TypeSystemService.cs
@@ -160,7 +160,7 @@ namespace MonoDevelop.Ide.TypeSystem
 
 	public static class TypeSystemService
 	{
-		const string CurrentVersion = "1.1.8";
+		const string CurrentVersion = "1.1.9";
 		static readonly List<TypeSystemParserNode> parsers;
 		static string[] filesSkippedInParseThread = new string[0];
 
