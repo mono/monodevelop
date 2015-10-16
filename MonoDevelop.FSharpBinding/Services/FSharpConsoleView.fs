f9c82807fd917f9f8f1ac309d3e51cc5170d2e01
7sharp9 <dave.thomas@xamarin.com>
2015-10-16 17:53:36 +0100

    Initial fsi syntax highlighting.
    None the “send to” commands have been tested.

--- MonoDevelop.FSharpBinding/Services/FSharpConsoleView.fs
+++ MonoDevelop.FSharpBinding/Services/FSharpConsoleView.fs
@@ -2,11 +2,34 @@
 open System
 open System.Reflection
 open System.Collections.Generic
+open System.Linq
 open Gtk
 open MonoDevelop.Core
 open MonoDevelop.Ide.Execution
 open Pango
+open Microsoft.FSharp.Compiler.SourceCodeServices
+open Mono.TextEditor
+open Mono.TextEditor.Highlighting
+open MonoDevelop.Ide
  
+type TokeniserOutput =
+  | Token of FSharpTokenInfo * string | EndState of FSharpTokenizerLexState
+
+[<RequireQualifiedAccess>]
+type Prompt = Normal | Multiline | None
+
+type Tags =
+  | Freezer
+  | Keyword
+  | User
+  | String
+  | Comment
+  | Inactive
+  | Number
+  | Operator
+  | Preprocessor
+  | PlainText
+
 type FSharpConsoleView() as x = 
   inherit ScrolledWindow()
 
@@ -14,40 +37,169 @@ type FSharpConsoleView() as x =
   let mutable scriptLines = ""
   let commandHistoryPast = new Stack<string> ()
   let commandHistoryFuture = new Stack<string> ()
-  let mutable inBlock = false
-  let mutable blockText = ""
   let textView = new TextView (WrapMode = Gtk.WrapMode.Word)
   let buffer = textView.Buffer
   let inputBeginMark = buffer.CreateMark (null, buffer.EndIter, true)
-  // The 'Freezer' tag is used to keep everything except the input line from being editable
-  let freezerTag = new TextTag ("Freezer", Editable = false)
-  let errorTag   = new TextTag ("error", Background = "#dc3122", Foreground = "white", Weight = Weight.Bold)
-  let warningTag = new TextTag ("warning", Foreground = "black", Background = "yellow")
-  let debugTag   = new TextTag ("debug", Foreground = "darkgrey")
 
-  do x.Add (textView)
+  let getTextTag (chunkStyle:ChunkStyle) =
+    new TextTag (chunkStyle.Name,
+                 Foreground=ColorScheme.ColorToMarkup chunkStyle.Foreground,
+                 Background=ColorScheme.ColorToMarkup chunkStyle.Background) 
+  
+  let mutable lastLineState = 0L //used as last lines state
+  let mutable tempState = 0L //used as the last stet for an in process line
+  let tags = Dictionary<_,_>()
+
+  let getTokensForLine file defines (line:string) state = 
+    // Get defined directives
+    let defines = defines |> Option.map (fun (s:string) -> s.Split([| ' '; ';'; ',' |], StringSplitOptions.RemoveEmptyEntries) |> List.ofSeq)
+    // Create source tokenizer
+    let sourceTok = SourceTokenizer(defaultArg defines ["INTERACTIVE";"EDITING"], file)
+    // Parse lines using the tokenizer
+    let tokenizer = sourceTok.CreateLineTokenizer(line)
+    let rec parseLine state = 
+        [ match tokenizer.ScanToken(state) with
+          | Some(tok), nstate ->
+              let str = line.Substring(tok.LeftColumn, tok.RightColumn - tok.LeftColumn + 1)
+              yield Token(tok, str)
+              yield! parseLine nstate
+          | None, nstate ->
+            yield EndState nstate ]
+    parseLine state
+
+  let getTokensFromLines file defines (lines:string[]) state =
+    [ for line in lines do
+        yield getTokensForLine file defines line state ]
+
+  let inputBeingProcessed = ref false
+  let startInputProcessing() =
+    inputBeingProcessed := true
+    { new IDisposable with
+      member x.Dispose() = inputBeingProcessed := false }
+
+  let applyToken (txt:string) (tag:TextTag) =
+    let mutable startIter = buffer.EndIter
+    buffer.InsertWithTags(&startIter, txt, [|tag|])
+
+  let eraseCurrentLine() =
+    let mutable start = x.InputLineBegin
+    let mutable end' = x.InputLineEnd
+    buffer.Delete (&start, &end')
+
+  let highlightLine line state =
+    let tokens = getTokensForLine "temp.fsx" None line state
+    tokens |> 
+    List.fold (fun state t ->
+               match t with
+               | Token( t,txt) ->
+                   match t.ColorClass with
+                   | FSharpTokenColorKind.Keyword -> applyToken txt tags.[Keyword]; state
+                   | FSharpTokenColorKind.Identifier -> applyToken txt tags.[User]; state
+                   | FSharpTokenColorKind.UpperIdentifier -> applyToken txt tags.[User]; state
+                   | FSharpTokenColorKind.Comment -> applyToken txt tags.[Comment]; state
+                   | FSharpTokenColorKind.String -> applyToken txt tags.[String]; state
+                   | FSharpTokenColorKind.Text -> applyToken txt tags.[String]; state
+                   | FSharpTokenColorKind.InactiveCode -> applyToken txt tags.[Inactive]; state
+                   | FSharpTokenColorKind.Number -> applyToken txt tags.[Number]; state
+                   | FSharpTokenColorKind.Operator -> applyToken txt tags.[Operator]; state
+                   | FSharpTokenColorKind.PreprocessorKeyword -> applyToken txt tags.[Preprocessor]; state
+                   | FSharpTokenColorKind.Default
+                   | _ -> applyToken txt tags.[PlainText]; state
+               | EndState state -> state) 0L
+  
+  let disposables = ResizeArray()
+
+  let createDisposable f =
+    { new IDisposable with 
+        member x.Dispose() = f()}
+
+  let addDisposable =
+    disposables.Add
+
+  do x.UpdateColors()
+     x.Add (textView)
      x.ShowAll ()
-     buffer.TagTable.Add (freezerTag)
-     buffer.TagTable.Add (errorTag)
-     buffer.TagTable.Add (warningTag)
-     buffer.TagTable.Add (debugTag)
 
+  member x.UpdateColors() =
+    let addTag (k:Tags) v = 
+      tags.Add(k,v)
+      buffer.TagTable.Add v
+
+    let scheme = SyntaxModeService.GetColorStyle (IdeApp.Preferences.ColorScheme.Value)
+    tags |> Seq.iter (fun (KeyValue(k,v)) -> buffer.TagTable.Remove(v); v.Dispose())
+    tags.Clear()
+    addTag Tags.Keyword (getTextTag scheme.KeywordTypes)
+    addTag Tags.User (getTextTag scheme.UserTypes)
+    addTag Tags.String (getTextTag scheme.String)
+    addTag Tags.Comment (getTextTag scheme.CommentTags)
+    addTag Tags.Inactive (getTextTag scheme.ExcludedCode)
+    addTag Tags.Number (getTextTag scheme.Number)
+    addTag Tags.Operator (getTextTag scheme.Punctuation)
+    addTag Tags.Preprocessor (getTextTag scheme.Preprocessor)
+    addTag Tags.PlainText (getTextTag scheme.PlainText)
+    addTag Tags.Freezer (new TextTag ("Freezer", Editable = false))
+  
   member x.InitialiseEvents() =
+    disposables.Add(
+      PropertyService.PropertyChanged.Subscribe
+        (fun _ (eventArgs:PropertyChangedEventArgs) -> 
+           if eventArgs.Key = "ColorScheme" && eventArgs.OldValue <> eventArgs.NewValue then
+             x.UpdateColors ()))
+
     let handleKeyPressDelegate =
       let mainType = typeof<FSharpConsoleView>
-      let meth = mainType.GetMethod("HandleKeyPress")
-      Delegate.CreateDelegate(typeof<KeyPressEventHandler>, x, meth) :?> KeyPressEventHandler
+      let keyPress = mainType.GetMethod("HandleKeyPress")
+      Delegate.CreateDelegate(typeof<KeyPressEventHandler>, x, keyPress) :?> KeyPressEventHandler
+
+//    let handleCopyDelegate =
+//      let mainType = typeof<FSharpConsoleView>
+//      let copy = mainType.GetMethod("HandleCopy")
+//      Delegate.CreateDelegate(typeof<EventHandler>, x, copy) :?> EventHandler
+//    textView.add_CopyClipboard(handleCopyDelegate)
+
+    textView.Buffer.InsertText.Subscribe(x.TextInserted)
+    |> addDisposable
 
     textView.add_KeyPressEvent(handleKeyPressDelegate)
-    textView.PopulatePopup.AddHandler(x.TextViewPopulatePopup)
+    createDisposable (fun () -> textView.remove_KeyPressEvent(handleKeyPressDelegate))
+    |> addDisposable
+
+    textView.PopulatePopup.Subscribe(x.TextViewPopulatePopup)
+    |> addDisposable
+
+  member x.TextInserted e =
+    if not !inputBeingProcessed then
+      using (startInputProcessing()) (fun _ ->
+      let line : string = x.InputLine
+      eraseCurrentLine()
+      let lines = line.Split([|'\n'|], StringSplitOptions.None)
+      if lines.Length = 1 then tempState <- highlightLine line lastLineState
+      else 
+        lines
+        |> Array.iteri
+             (fun i line ->
+                if i < lines.Length-1 then
+                  lastLineState <- highlightLine line lastLineState
+                  x.ProcessReturn()
+                else 
+                  tempState <- highlightLine line lastLineState))
 
   [<GLib.ConnectBeforeAttribute>]
   member x.HandleKeyPress(_o:obj, args) =
     if (x.ProcessKeyPressEvent (args)) then
         args.RetVal <- true
+
+  //fired on context menu copy
+  [<GLib.ConnectBeforeAttribute>]
+  member x.HandleCopy(_o:obj, e: EventArgs) =
+    ()
+
+  //fired on context menu paste
+  [<GLib.ConnectBeforeAttribute>]
+  member x.HandlePaste(_o:obj, e:EventArgs) =
+    ()
   
-  member x.TextViewPopulatePopup =
-    PopulatePopupHandler(fun sender args ->
+  member x.TextViewPopulatePopup sender args =
     let item = new MenuItem (Mono.Unix.Catalog.GetString ("Clear"))
     let sep = new SeparatorMenuItem ()
 
@@ -56,64 +208,38 @@ type FSharpConsoleView() as x =
     sep.Show ()
 
     args.Menu.Add (sep)
-    args.Menu.Add (item))
+    args.Menu.Add (item)
 
-//  member x.ClearActivated (e) = x.Clear ()
   member x.SetFont (font) = textView.ModifyFont (font)
   member x.TextView = textView
   member val PromptString = "> " with get, set
+  member val PromptMultiLineString = "- " with get, set
   member val AutoIndent = false with get, set
-  member val PromptMultiLineString = ">> " with get, set
 
   member x.ProcessReturn () =
-      if inBlock then
-        if x.InputLine = "" then
-          x.ProcessInput (blockText)
-          blockText <- ""
-          inBlock <- false
-        else 
-          blockText <- blockText + "\n" + x.InputLine
-          let mutable whiteSpace = null
-          if x.AutoIndent then
-            let r = Text.RegularExpressions.Regex (@"^(\s+).*")
-            whiteSpace <- r.Replace (x.InputLine, "$1")
-            if x.InputLine.EndsWith (x.BlockStart, StringComparison.InvariantCulture) then
-              whiteSpace <- whiteSpace + "\t"
-          
-          x.Prompt (true, true)
-          if x.AutoIndent then
-            x.InputLine <- x.InputLine + whiteSpace
-      else
-        // Special case for start of new code block
-        if not (String.IsNullOrEmpty (x.BlockStart) && x.InputLine.Trim().EndsWith (x.BlockStart, StringComparison.InvariantCulture)) then
-          inBlock <- true
-          blockText <- x.InputLine
-          x.Prompt (true, true)
-          if x.AutoIndent then
-            x.InputLine <- x.InputLine + "\t"
-        
+    // Bookkeeping
+    if (x.InputLine <> "") then
 
-        // Bookkeeping
-        if (x.InputLine <> "") then
-          // Everything but the last item (which was input),
-          //in the future stack needs to get put back into the
-          // past stack
-          while (commandHistoryFuture.Count > 1) do
-            commandHistoryPast.Push (commandHistoryFuture.Pop())
-          // Clear the pesky junk input line
-          commandHistoryFuture.Clear()
+      // Everything but the last item (which was input), in the future stack needs to get put back into the past stack
+      while (commandHistoryFuture.Count > 1) do
+        commandHistoryPast.Push (commandHistoryFuture.Pop())
+      // Clear the pesky junk input line
+      commandHistoryFuture.Clear()
 
-          // Record our input line
-          commandHistoryPast.Push(x.InputLine)
-          if scriptLines = "" then
-            scriptLines <- scriptLines + x.InputLine
-          else
-            scriptLines <- scriptLines + "\n" + x.InputLine
+      // Record our input line
+      commandHistoryPast.Push(x.InputLine)
+      if scriptLines = "" then scriptLines <- scriptLines + x.InputLine
+      else scriptLines <- scriptLines + "\n" + x.InputLine
 
-          x.ProcessInput (x.InputLine)
+      x.ProcessInput (x.InputLine)
+      lastLineState <- tempState
+      let prompt, newLine =
+        if x.InputLine.EndsWith(";;\n") then Prompt.None, true
+        else Prompt.Multiline, false
+      x.Prompt(newLine, prompt)
 
   member x.ProcessCommandHistoryUp () =
-      if not inBlock && commandHistoryPast.Count > 0 then
+      if commandHistoryPast.Count > 0 then
         if commandHistoryFuture.Count = 0 then
           commandHistoryFuture.Push (x.InputLine)
         else
@@ -122,21 +248,19 @@ type FSharpConsoleView() as x =
         x.InputLine <- commandHistoryPast.Peek ()
 
   member x.ProcessCommandHistoryDown () =
-      if not inBlock && commandHistoryFuture.Count > 0 then
+      if commandHistoryFuture.Count > 0 then
         if commandHistoryFuture.Count = 1 then
           x.InputLine <- commandHistoryFuture.Pop ()
         else
           commandHistoryPast.Push (commandHistoryFuture.Pop ())
           x.InputLine <- commandHistoryPast.Peek ()
 
-  member x.InputLineBegin = buffer.GetIterAtMark(inputBeginMark) :TextIter
-  member x.InputLineEnd = buffer.EndIter :TextIter
-  member x.Cursor = buffer.GetIterAtMark (buffer.InsertMark) : TextIter
-  member x.Buffer = textView.Buffer
-  member x.GetIterLocation ( iter:TextIter) = textView.GetIterLocation (iter)
+  member x.InputLineBegin = buffer.GetIterAtMark(inputBeginMark)
+  member x.InputLineEnd = buffer.EndIter
+  member x.Cursor = buffer.GetIterAtMark (buffer.InsertMark)
 
   member x.ProcessKeyPressEvent ( args:KeyPressEventArgs) =
-    let returnCode = 
+    let returnCode =
       // Short circuit to avoid getting moved back to the input line
       // when paging up and down in the shell output
       if args.Event.Key = Gdk.Key.Page_Up || args.Event.Key = Gdk.Key.Page_Down then false
@@ -165,7 +289,10 @@ type FSharpConsoleView() as x =
           // Move the selection mark too, if shift isn't held
           if not (args.Event.State.HasFlag (Gdk.ModifierType.ShiftMask)) then
             buffer.MoveMark (buffer.SelectionBound, x.InputLineBegin)
-        true
+            true
+          else false
+        elif x.Cursor.Compare (x.InputLineBegin) <= 0 then true
+        else false
       | Gdk.Key.KP_Home | Gdk.Key.Home ->
         buffer.MoveMark (buffer.InsertMark, x.InputLineBegin)
 
@@ -180,10 +307,27 @@ type FSharpConsoleView() as x =
           // Move the selection mark too, if shift isn't held
           if not (args.Event.State.HasFlag (Gdk.ModifierType.ShiftMask)) then
             buffer.MoveMark (buffer.SelectionBound, x.InputLineBegin)
+          true
+        else false
+      | Gdk.Key.BackSpace | Gdk.Key.Delete -> false
+      | _ ->
+        //do our syntax highlighting
+        using (startInputProcessing()) (fun _ ->
+        let nextKey = char (Gdk.Keyval.ToUnicode(args.Event.KeyValue))
+        let line = x.InputLine + nextKey.ToString()
+        eraseCurrentLine()
+        let lines = line.Split([|'\n'|], StringSplitOptions.None)
+        if lines.Length = 1 then tempState <- highlightLine line lastLineState
+        else 
+          lines
+          |> Array.iteri
+               (fun i line ->
+                  if i < lines.Length-1 then
+                    lastLineState <- highlightLine line lastLineState
+                    x.ProcessReturn()
+                  else 
+                    tempState <- highlightLine line lastLineState))
         true
-
-      | Gdk.Key.period -> false
-      | _ -> false
 
     returnCode
 
@@ -191,79 +335,80 @@ type FSharpConsoleView() as x =
   member x.InputLine
     with get() =  buffer.GetText (x.InputLineBegin, x.InputLineEnd, false)
     and set(v) =
+        using (startInputProcessing()) (fun _ ->
         let mutable start = x.InputLineBegin
         let mutable end' = x.InputLineEnd
         buffer.Delete (&start, &end')
         start <- x.InputLineBegin
-        buffer.Insert (&start, v)
+        tempState <- highlightLine v lastLineState)
 
   member x.ProcessInput (line:string) =
-    x.WriteOutput ("\n")
+    x.WriteOutput("\n", false)
     console.Trigger(line)
 
-  member x.WriteOutput (line) =
-    x.WriteOutput (line, LogLevel.Default)
-
-  member x.WriteOutput (line, logLevel) =
-    let tag = x.GetTag (logLevel)
-    let mutable start = buffer.EndIter
-
-    if (tag = null) then
+  member x.WriteOutput (line:string, highlight) =
+    using (startInputProcessing()) (fun _ ->
+    if highlight then
+      let tokens = getTokensFromLines "temp.fs" None (line.Split([|'\n';'\r'|], StringSplitOptions.RemoveEmptyEntries)) 0L
+      tokens |>
+      List.iter 
+        (List.iter (function
+                    | Token( t,txt) ->
+                        match t.ColorClass with
+                        | FSharpTokenColorKind.Keyword -> applyToken txt tags.[Keyword]
+                        | FSharpTokenColorKind.Identifier -> applyToken txt tags.[User]
+                        | FSharpTokenColorKind.UpperIdentifier -> applyToken txt tags.[User]
+                        | FSharpTokenColorKind.Comment -> applyToken txt tags.[Comment]
+                        | FSharpTokenColorKind.String -> applyToken txt tags.[String]
+                        | FSharpTokenColorKind.Text -> applyToken txt tags.[String]
+                        | FSharpTokenColorKind.InactiveCode -> applyToken txt tags.[Inactive]
+                        | FSharpTokenColorKind.Number -> applyToken txt tags.[Number]
+                        | FSharpTokenColorKind.Operator -> applyToken txt tags.[Operator]
+                        | FSharpTokenColorKind.PreprocessorKeyword -> applyToken txt tags.[Preprocessor]
+                        | FSharpTokenColorKind.Default
+                        | _ -> applyToken txt tags.[PlainText]
+                    | EndState _ ->
+                        let mutable end' = buffer.EndIter
+                        buffer.Insert (&end', "\n")))
+    else
+      let mutable start = buffer.EndIter
       buffer.Insert(&start, line)
-    else
-      buffer.InsertWithTags(&start, line, tag)
-    
+
     buffer.PlaceCursor (buffer.EndIter)
-    textView.ScrollMarkOnscreen (buffer.InsertMark)
+    textView.ScrollMarkOnscreen (buffer.InsertMark))
 
-  member x.GetTag (logLevel:LogLevel) =
-    match (logLevel) with
-    | LogLevel.Critical
-    | LogLevel.Error
-      -> [|errorTag|]
-    | LogLevel.Warning
-      -> [|warningTag|]
-    | LogLevel.Debug
-      -> [|debugTag|]
-    | _ -> null
-
-
-  member x.Prompt (newLine) =
-    x.Prompt (newLine, false)
-
-  member x.Prompt (newLine, multiline) =
+  member x.Prompt (newLine, prompt:Prompt) =
+    using (startInputProcessing()) (fun _ ->
     let mutable end' = buffer.EndIter
-    if newLine then
-      buffer.Insert (&end', "\n")
-    if multiline then
-      buffer.Insert (&end', x.PromptMultiLineString)
-    else
-      buffer.Insert (&end', x.PromptString)
+
+    if newLine then buffer.Insert (&end', "\n")
+
+    match prompt with
+    | Prompt.Normal -> bueffer.Insert (&end', x.PromptString)
+    | Prompt.Multiline -> buffer.Insert (&end', x.PromptMultiLineString)
+    | Prompt.None -> ()
 
     buffer.PlaceCursor (buffer.EndIter)
     textView.ScrollMarkOnscreen (buffer.InsertMark)
 
-    x.UpdateInputLineBegin ()
+    buffer.MoveMark (inputBeginMark, buffer.EndIter)
 
     // Freeze all the text except our input line
-    buffer.ApplyTag(freezerTag, buffer.StartIter, x.InputLineBegin)
+    buffer.ApplyTag(tags.[Freezer], buffer.StartIter, x.InputLineBegin))
 
-  member x.UpdateInputLineBegin () =
-    // Record the end of where we processed, used to calculate start
-    // of next input line
-    buffer.MoveMark (inputBeginMark, buffer.EndIter)
 
   member x.Clear () =
     buffer.Text <- ""
     scriptLines <- ""
-    x.Prompt (false)
+    x.Prompt (false, Prompt.Normal)
 
   member x.ClearHistory () =
     commandHistoryFuture.Clear ()
     commandHistoryPast.Clear ()
 
-  member val BlockStart = "" with get, set
-  member val BlockEnd   = "" with get, set
-
   [<CLIEvent>]
   member x.ConsoleInput = console.Publish
+
+  interface IDisposable with 
+    member x.Dispose() =
+      disposables |> Seq.iter (fun d -> d.Dispose() )
