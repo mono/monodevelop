e7c01116f267ff4d53fef297cac65358df860309
Marius Ungureanu <marius.ungureanu@xamarin.com>
2015-10-16 09:42:50 -0400

    [Build] Add support for testing fsharp stuff.

--- main/wintest.sh
+++ main/wintest.sh
@@ -1,10 +1,15 @@
 if [ $# -lt 1 ]; then
 	build/bin/mdtool run-md-tests build/tests/UnitTests.dll
 	build/bin/mdtool run-md-tests build/tests/*.Tests.dll
-	if [ ! -f "$external/nrefactory/bin/Debug/ICSharpCode.NRefactory.Tests.dll" ]; then
+	if [ ! -f "external/nrefactory/bin/Debug/ICSharpCode.NRefactory.Tests.dll" ]; then
 		build/bin/mdtool run-md-tests external/nrefactory/bin/Debug/ICSharpCode.NRefactory.Tests.dll
 	else
 		build/bin/mdtool run-md-tests external/nrefactory/bin/Release/ICSharpCode.NRefactory.Tests.dll
+	fi
+	if [ ! -f "external/fsharpbinding/MonoDevelop.FSharp.Tests/bin/Debug/MonoDevelop.FSharp.Tests.dll" ]; then
+		build/bin/mdtool run-md-tests external/fsharpbinding/MonoDevelop.FSharp.Tests/bin/Debug/MonoDevelop.FSharp.Tests.dll
+	else
+		build/bin/mdtool run-md-tests external/fsharpbinding/MonoDevelop.FSharp.Tests/bin/Release/MonoDevelop.FSharp.Tests.dll
 	fi
 else
 	for arg in $@; do
@@ -12,6 +17,11 @@ else
 			arg="external/nrefactory/bin/Debug/ICSharpCode.NRefactory.Tests.dll"
 			if [ ! -f "$arg" ]; then
 				arg="external/nrefactory/bin/Release/ICSharpCode.NRefactory.Tests.dll"
+			fi
+		elif [[ $arg == *MonoDevelop.FSharp.Tests.dll ]]; then
+			arg="external/fsharpbinding/MonoDevelop.FSharp.Tests/bin/Debug/MonoDevelop.FSharp.Tests.dll"
+			if [ ! -f "$arg" ]; then
+				arg="external/fsharpbinding/MonoDevelop.FSharp.Tests/bin/Release/MonoDevelop.FSharp.Tests.dll"
 			fi
 		elif [[ $arg != build/tests/* ]]; then
 			arg="build/tests/$arg"
