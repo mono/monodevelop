864916f8459e05ae6335d81495a9243ad6638ad3
Marius Ungureanu <marius.ungureanu@xamarin.com>
2015-10-22 08:16:40 -0700

    [Windows] Get rid of titlebar custom impl

--- main/src/addins/WindowsPlatform/WindowsPlatform/MainToolbar/TitleBar.xaml.cs
+++ main/src/addins/WindowsPlatform/WindowsPlatform/MainToolbar/TitleBar.xaml.cs
@@ -26,55 +26,5 @@ namespace WindowsPlatform.MainToolbar
         {
             InitializeComponent();
         }
-
-		void AlwaysCanExecute (object sender, CanExecuteRoutedEventArgs args)
-		{
-			args.CanExecute = true;
-		}
-
-		void CloseExecuted (object sender, ExecutedRoutedEventArgs e)
-		{
-			IdeApp.Exit ();
-		}
-
-		void MaximizeExecuted (object sender, ExecutedRoutedEventArgs e)
-		{
-			if (IdeApp.Workbench.RootWindow.GdkWindow.State == Gdk.WindowState.Maximized)
-				IdeApp.Workbench.RootWindow.Unmaximize ();
-			else {
-				IdeApp.Workbench.RootWindow.Maximize ();
-				IdeApp.Workbench.RootWindow.Resize ((int)SystemParameters.WorkArea.Width, (int)SystemParameters.WorkArea.Height);
-			}
-		}
-
-		void MinimizeExecuted (object sender, ExecutedRoutedEventArgs e)
-		{
-			IdeApp.Workbench.RootWindow.Iconify ();
-		}
-
-		protected override void OnVisualParentChanged (DependencyObject oldParent)
-		{
-			base.OnVisualParentChanged (oldParent);
-
-			window = Window.GetWindow (this);
-		}
-
-		int initX, initY;
-		bool inDrag;
-		Window window;
-
-		void OnDragStart (object sender, MouseButtonEventArgs e)
-		{
-			if (e.MouseDevice.DirectlyOver.GetType () != typeof (Border))
-				return;
-
-			if (e.ClickCount == 2)
-				MaximizeExecuted (this, null);
-			else {
-				int wndX, wndY;
-				IdeApp.Workbench.RootWindow.GetPosition (out wndX, out wndY);
-				IdeApp.Workbench.RootWindow.BeginMoveDrag (1, wndX, wndY, (uint)e.Timestamp);
-			}
-		}
 	}
 }
