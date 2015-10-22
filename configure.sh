05f20e6fdc400e2a46fc1d1f128ee0d9abdde336
nosami <jasonimison@gmail.com>
2015-10-22 10:53:12 +0100

    Remove paket.exe

--- configure.sh
+++ configure.sh
@@ -3,4 +3,5 @@ echo "Configuring..."
 fsharpi configure.fsx "$@"
 mozroots --import --sync --quiet || echo 'Could not import mozroots, proceeding anyway'
 echo "Restoring nuget packages with paket..."
+mono .paket/paket.bootstraper.exe
 mono .paket/paket.exe restore
