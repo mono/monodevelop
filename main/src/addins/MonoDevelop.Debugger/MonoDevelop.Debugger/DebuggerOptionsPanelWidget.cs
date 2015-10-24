4efb14f86e60d82863fb7bf9709b36f8f99b9054
David Karlaš <david.karlas@xamarin.com>
2015-10-24 04:28:16 -0400

    [Debugger] Converted OptionsPanel from GTK# to XWT

--- main/src/addins/MonoDevelop.Debugger/MonoDevelop.Debugger/DebuggerOptionsPanelWidget.cs
+++ main/src/addins/MonoDevelop.Debugger/MonoDevelop.Debugger/DebuggerOptionsPanelWidget.cs
@@ -1,4 +1,4 @@
-// 
+﻿// 
 // DebuggerOptionsPanelWidget.cs
 //  
 // Author:
@@ -24,30 +24,75 @@
 // OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
 // THE SOFTWARE.
 
+using System;
 using Mono.Debugging.Client;
 using MonoDevelop.Ide.Gui.Dialogs;
+using Xwt;
+using MonoDevelop.Core;
 
 namespace MonoDevelop.Debugger
 {
-	public class DebuggerOptionsPanel: OptionsPanel
+	public class DebuggerOptionsPanel : OptionsPanel
 	{
 		DebuggerOptionsPanelWidget w;
-		
+
 		public override Gtk.Widget CreatePanelWidget ()
 		{
-			return w = new DebuggerOptionsPanelWidget ();
+			w = new DebuggerOptionsPanelWidget ();
+
+			return (Gtk.Widget)Toolkit.CurrentEngine.GetNativeWidget (w);
 		}
-		
+
 		public override void ApplyChanges ()
 		{
 			w.Store ();
 		}
 	}
 
-	[System.ComponentModel.ToolboxItem(true)]
-	public partial class DebuggerOptionsPanelWidget : Gtk.Bin
+	public class DebuggerOptionsPanelWidget : VBox
 	{
 		DebuggerSessionOptions options;
+		CheckBox checkProjectCodeOnly;
+		CheckBox checkStepOverPropertiesAndOperators;
+		CheckBox checkAllowEval;
+		CheckBox checkAllowToString;
+		CheckBox checkShowBaseGroup;
+		CheckBox checkGroupPrivate;
+		CheckBox checkGroupStatic;
+		SpinButton spinTimeout;
+
+		void Build ()
+		{
+			checkProjectCodeOnly = new CheckBox (GettextCatalog.GetString ("Debug project code only; do not step into framework code."));
+			PackStart (checkProjectCodeOnly);
+			checkStepOverPropertiesAndOperators = new CheckBox (GettextCatalog.GetString ("Step over properties and operators"));
+			PackStart (checkStepOverPropertiesAndOperators);
+			checkAllowEval = new CheckBox (GettextCatalog.GetString ("Allow implicit property evaluation and method invocation"));
+			checkAllowEval.Toggled += OnCheckAllowEvalToggled;
+			PackStart (checkAllowEval);
+			checkAllowToString = new CheckBox (GettextCatalog.GetString ("Call string-conversion function on objects in variables windows"));
+			checkAllowToString.MarginLeft = 18;
+			PackStart (checkAllowToString);
+			checkShowBaseGroup = new CheckBox (GettextCatalog.GetString ("Show inherited class members in a base class group"));
+			PackStart (checkShowBaseGroup);
+			checkGroupPrivate = new CheckBox (GettextCatalog.GetString ("Group non-public members"));
+			PackStart (checkGroupPrivate);
+			checkGroupStatic = new CheckBox (GettextCatalog.GetString ("Group static members"));
+			PackStart (checkGroupStatic);
+			var hbox = new HBox ();
+			hbox.PackStart (new Label (GettextCatalog.GetString ("Evaluation Timeout:")));
+			spinTimeout = new SpinButton ();
+			spinTimeout.ClimbRate = 100;
+			spinTimeout.Digits = 0;
+			spinTimeout.IncrementValue = 100;
+			spinTimeout.MaximumValue = 1000000;
+			spinTimeout.MinimumValue = 0;
+			spinTimeout.Wrap = false;
+			spinTimeout.WidthRequest = 80;
+			hbox.PackStart (spinTimeout);
+			hbox.PackStart (new Label (GettextCatalog.GetString ("ms")));
+			PackStart (hbox);
+		}
 
 		public DebuggerOptionsPanelWidget ()
 		{
@@ -74,7 +119,7 @@ namespace MonoDevelop.Debugger
 			ops.FlattenHierarchy = !checkShowBaseGroup.Active;
 			ops.GroupPrivateMembers = checkGroupPrivate.Active;
 			ops.GroupStaticMembers = checkGroupStatic.Active;
-			ops.EvaluationTimeout = (int) spinTimeout.Value;
+			ops.EvaluationTimeout = (int)spinTimeout.Value;
 
 			options.StepOverPropertiesAndOperators = checkStepOverPropertiesAndOperators.Active;
 			options.ProjectAssembliesOnly = checkProjectCodeOnly.Active;
