7616ce723ce4fe040ed39655b94056ec909bf76f
Matt Ward <matt.ward@xamarin.com>
2015-10-28 16:09:26 +0000

    [Ide] Fix current format in Export Solution dialog.
    The currently used file format was just showing as MSBuild. Now
    it shows the full production description.

--- main/src/core/MonoDevelop.Ide/MonoDevelop.Ide.Projects/ExportSolutionDialog.cs
+++ main/src/core/MonoDevelop.Ide/MonoDevelop.Ide.Projects/ExportSolutionDialog.cs
@@ -43,14 +43,11 @@ namespace MonoDevelop.Ide.Projects
 		{
 			this.Build();
 			
-			labelNewFormat.Text = item.FileFormat.Name;
+			labelNewFormat.Text = GetFormatText (item.FileFormat);
 			
 			formats = MSBuildFileFormat.GetSupportedFormats (item).ToArray ();
 			foreach (var format in formats) {
-				if (!string.IsNullOrEmpty (format.ProductDescription))
-					comboFormat.AppendText (string.Format ("{0} ({1})", format.Name, format.ProductDescription));
-				else 
-					comboFormat.AppendText (format.Name);
+				comboFormat.AppendText (GetFormatText (format));
 			}
 
 			int sel = Array.IndexOf (formats, selectedFormat);
@@ -72,6 +69,13 @@ namespace MonoDevelop.Ide.Projects
 			
 			folderEntry.Path = item.ItemDirectory;
 			UpdateControls ();
+		}
+
+		static string GetFormatText (MSBuildFileFormat format)
+		{
+			if (!string.IsNullOrEmpty (format.ProductDescription))
+				return string.Format ("{0} ({1})", format.Name, format.ProductDescription);
+			return format.Name;
 		}
 		
 		public MSBuildFileFormat Format {
