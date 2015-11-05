mono/monodevelop

commit d993bea002a14e8d139565568a142ee2ce939401

[VCS] Fail-safe mechanism to requery repo when disposed.
Bug 35073 - [Roslyn] Git doesn't appear to be working (blame, changes,
log, etc)

main/src/addins/VersionControl/MonoDevelop.VersionControl/MonoDevelop.VersionControl/VersionControlItem.cs
@@ -48,10 +48,17 @@ namespace MonoDevelop.VersionControl
 			get;
 			private set;
 		}
-		
+
+		Repository repository;
 		public Repository Repository {
-			get;
-			private set;
+			get {
+				if (repository.Disposed)
+					repository = VersionControlService.GetRepository (WorkspaceObject);
+				return repository;
+			}
+			internal set {
+				repository = value;
+			}
 		}
 		
 		public FilePath Path {

main/src/addins/VersionControl/MonoDevelop.VersionControl/MonoDevelop.VersionControl.Views/VersionControlDocumentInfo.cs
@@ -58,8 +58,8 @@ namespace MonoDevelop.VersionControl.Views
 		}
 		
 		public Repository Repository {
-			get;
-			set;
+			get { return Item.Repository; }
+			set { Item.Repository = value; }
 		}
 		
 		public bool Started {
@@ -70,7 +70,7 @@ namespace MonoDevelop.VersionControl.Views
 		{
 			this.Document = document;
 			this.Item = item;
-			this.Repository = repository;
+			item.Repository = repository;
 		}
 
 		public void Start (bool rerun = false)

main/src/addins/VersionControl/MonoDevelop.VersionControl/MonoDevelop.VersionControl/Repository.cs
@@ -95,8 +95,10 @@ namespace MonoDevelop.VersionControl
 				Dispose ();
 		}
 
+		internal bool Disposed { get; private set; }
 		public virtual void Dispose ()
 		{
+			Disposed = true;
 			if (!queryRunning)
 				return;
 

commit 5715622d487d0714f2c2f6341efd9d249f81b781

[VCS] Fix regression introduced in d993bea002a14e8d139565568a142ee2ce939401
Fixes a NRE that was caused by an unconditional check.

main/src/addins/VersionControl/MonoDevelop.VersionControl/MonoDevelop.VersionControl/VersionControlItem.cs
@@ -52,7 +52,7 @@ namespace MonoDevelop.VersionControl
 		Repository repository;
 		public Repository Repository {
 			get {
-				if (repository.Disposed)
+				if (repository != null && repository.Disposed)
 					repository = VersionControlService.GetRepository (WorkspaceObject);
 				return repository;
 			}