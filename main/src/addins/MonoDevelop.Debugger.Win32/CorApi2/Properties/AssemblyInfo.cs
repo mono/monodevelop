3dc3c20c6167496de329a108be91b937004895d8
Nerzhul 500 <nerzhulart@gmail.com>
2015-11-18 22:52:24 +0300

    Version update

--- main/src/addins/MonoDevelop.Debugger.Win32/CorApi2/Properties/AssemblyInfo.cs
+++ main/src/addins/MonoDevelop.Debugger.Win32/CorApi2/Properties/AssemblyInfo.cs
@@ -26,5 +26,5 @@ using System.Runtime.InteropServices;
 //      Build Number
 //      Revision
 //
-[assembly: AssemblyVersion("1.0.0.12")]
-[assembly: AssemblyFileVersion("1.0.0.12")]
+[assembly: AssemblyVersion("1.0.0.13")]
+[assembly: AssemblyFileVersion("1.0.0.13")]
