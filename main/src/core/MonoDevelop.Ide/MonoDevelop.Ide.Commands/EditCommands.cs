1051866d1f93b090bb5e22c8306c3d2479ff580d
Lluis Sanchez <lluis@xamarin.com>
2015-11-23 20:41:39 +0100

    [Ide] Fix more NREs in Edit command handlers

--- main/src/core/MonoDevelop.Ide/MonoDevelop.Ide.Commands/EditCommands.cs
+++ main/src/core/MonoDevelop.Ide/MonoDevelop.Ide.Commands/EditCommands.cs
@@ -184,7 +184,7 @@ namespace MonoDevelop.Ide.Commands
 			object focus = IdeApp.Workbench.RootWindow.HasToplevelFocus ? IdeApp.Workbench.RootWindow.Focus : null;
 			info.Enabled = (focus is Gtk.Editable || focus is Gtk.TextView);
 			#if MAC
-			var macfocus = AppKit.NSApplication.SharedApplication.KeyWindow.FirstResponder;
+			var macfocus = AppKit.NSApplication.SharedApplication?.KeyWindow?.FirstResponder;
 			info.Enabled |= macfocus is AppKit.NSText;
 			#endif
 			info.Bypass = !info.Enabled;
@@ -228,7 +228,7 @@ namespace MonoDevelop.Ide.Commands
 				info.Enabled = false;
 
 			#if MAC
-			var macfocus = AppKit.NSApplication.SharedApplication.KeyWindow.FirstResponder;
+			var macfocus = AppKit.NSApplication.SharedApplication?.KeyWindow?.FirstResponder;
 			info.Enabled |= macfocus is AppKit.NSText;
 			#endif
 
@@ -273,7 +273,7 @@ namespace MonoDevelop.Ide.Commands
 				info.Enabled = false;
 
 			#if MAC
-			var macfocus = AppKit.NSApplication.SharedApplication.KeyWindow.FirstResponder;
+			var macfocus = AppKit.NSApplication.SharedApplication?.KeyWindow?.FirstResponder;
 			info.Enabled |= macfocus is AppKit.NSText;
 			#endif
 
