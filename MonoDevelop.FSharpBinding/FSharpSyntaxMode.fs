c8f80ef1a1c557f6dacea772d27b1355ce17ec46
7sharp9 <dave.thomas@xamarin.com>
2015-11-26 08:24:15 +0000

    Switch to a cheaper field enum check

--- MonoDevelop.FSharpBinding/FSharpSyntaxMode.fs
+++ MonoDevelop.FSharpBinding/FSharpSyntaxMode.fs
@@ -127,7 +127,7 @@ module Patterns =
     match ts with
     | IdentifierSymbol symbolUse -> 
       match symbolUse with
-      | SymbolUse.Field f -> Some (symbolUse.IsFromDefinition, f.IsMutable && not f.DeclaringEntity.IsEnum)
+      | SymbolUse.Field f -> Some (symbolUse.IsFromDefinition, f.IsMutable && not f.IsLiteral)
       | _ -> None
     | _ -> None
   
