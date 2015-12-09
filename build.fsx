699e934842051084e5382cfb4ccdfab563e0b396
nosami <jasonimison@gmail.com>
2015-12-09 10:01:26 +0000

    move make tasks to FAKE so they can be ran on Windows

--- build.fsx
+++ build.fsx
@@ -1,13 +1,40 @@
 #r @"FakeLib.dll"
 open Fake
+open System.IO
 
-Target "Build" (fun _ ->
+let isWindows = (Path.DirectorySeparatorChar = '\\')
+let config = "Release"
+
+Target "Default" (fun _ ->
   MSBuildWithDefaults "Build" ["./MonoDevelop.FSharp.sln"]
   |> Log "AppBuild-Output: "
 )
 
+let mdpath = "../../build/bin/mdtool.exe"
+
+let mdtool args =
+  let result = 
+    if isWindows then
+      Shell.Exec (mdpath, args)
+    else
+      Shell.Exec ("mono", mdpath + " " + args)
+  result |> ignore
+
 let test() =
-  Shell.Exec ("mono", "../../build/bin/mdtool.exe run-md-tests ../../external/fsharpbinding/MonoDevelop.FSharp.Tests/bin/Release/MonoDevelop.FSharp.Tests.dll -labels") |> ignore
+  mdtool ("run-md-tests ../../external/fsharpbinding/MonoDevelop.FSharp.Tests/bin/" + config + "/MonoDevelop.FSharp.Tests.dll -labels")
+
+Target "Pack" (fun _ ->
+  let dir = "pack/" + config
+  Directory.Delete (dir, true)
+  Directory.CreateDirectory dir |> ignore
+  mdtool ("setup pack bin/FSharpBinding.dll -d:pack/" + config)
+)
+
+Target "Install" (fun _ ->
+  let versionConfig = File.ReadAllLines("../../../version.config")
+  let version = versionConfig.[0].Replace("Version=", "")
+  mdtool ("setup install -y pack/" + config + "/MonoDevelop.FSharpBinding_" + version + ".mpack")
+)
 
 Target "BuildAndTest" (fun _ ->
   test()
@@ -21,10 +48,13 @@ Target "Run" (fun _ ->
   Shell.Exec ("make", "run", "../..") |> ignore
 )
 
-"Build"
+"Default"
   ==> "BuildAndTest"
 
-"Build"
+"Default"
   ==> "Run"
 
-RunTargetOrDefault "Build"
+"Pack" 
+  ==> "Install"
+
+RunTargetOrDefault "Default"
