ae0f094e3a991e8eb3514647553e422bcc10df87
Mike Krüger <mkrueger@xamarin.com>
2015-12-12 09:10:02 +0100

    [CSharpBinding] Ported encapsulate field refactorings.
    Older versions of RefactoringEssentials provided the action but it got
    removed because of duplicates in VS.NET.
    Now we're using the same action than VS.NET for that operation.

--- main/src/addins/CSharpBinding/MonoDevelop.CSharp.CodeFixes/EncapsulateField/AbstractEncapsulateFieldRefactoringProvider.cs
+++ main/src/addins/CSharpBinding/MonoDevelop.CSharp.CodeFixes/EncapsulateField/AbstractEncapsulateFieldRefactoringProvider.cs
@@ -1,21 +1,20 @@
 ﻿// Copyright (c) Microsoft.  All Rights Reserved.  Licensed under the Apache License, Version 2.0.  See License.txt in the project root for license information.
 
 using System.Threading.Tasks;
+using ICSharpCode.NRefactory6.CSharp.CodeRefactorings.EncapsulateField;
 using Microsoft.CodeAnalysis.Shared.Extensions;
-using Microsoft.CodeAnalysis.CodeRefactorings;
-using ICSharpCode.NRefactory.CSharp;
 
-namespace ICSharpCode.NRefactory6.CSharp.CodeRefactorings.EncapsulateField
+namespace Microsoft.CodeAnalysis.CodeRefactorings.EncapsulateField
 {
-	public abstract class AbstractEncapsulateFieldRefactoringProvider : CodeRefactoringProvider
+	internal abstract class AbstractEncapsulateFieldRefactoringProvider : CodeRefactoringProvider
 	{
 		CSharpEncapsulateFieldService service = new CSharpEncapsulateFieldService ();
 
 		public sealed override async Task ComputeRefactoringsAsync(CodeRefactoringContext context)
 		{
 			var actions = await service.GetEncapsulateFieldCodeActionsAsync(context.Document, context.Span, context.CancellationToken).ConfigureAwait(false);
-
-			context.RegisterRefactorings(actions);
+			foreach (var action in actions)
+				context.RegisterRefactoring(action);
 		}
 	}
 }
