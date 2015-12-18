21577f0009cc30d866e3fdfe3b13897d010620c8
Vsevolod Kukol <sevoku@xamarin.com>
2015-12-18 10:54:59 +0100

    [Ide] Fix crompletion tooltip pager flickering
    Completion tooltip does not need an update
    when switching overlays.

--- main/src/core/MonoDevelop.Ide/MonoDevelop.Ide.CodeCompletion/CompletionListWindow.cs
+++ main/src/core/MonoDevelop.Ide/MonoDevelop.Ide.CodeCompletion/CompletionListWindow.cs
@@ -284,7 +284,6 @@ namespace MonoDevelop.Ide.CodeCompletion
 							declarationviewwindow.OverloadLeft ();
 						else
 							declarationviewwindow.OverloadRight ();
-						UpdateDeclarationView ();
 					} else {
 						CompletionWindowManager.HideWindow ();
 						OnWindowClosed (EventArgs.Empty);
