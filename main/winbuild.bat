6d603e5dbffdc4447785bbabc2b68b2dae23f29b
Mikhail Filippov <mikhail@filippov.me>
2015-12-30 15:26:54 +0300

    Add packages restore for mono-addins in winbuild.bat

--- main/winbuild.bat
+++ main/winbuild.bat
@@ -1,3 +1,4 @@
 ".nuget\NuGet.exe" restore Main.sln
 "external\RefactoringEssentials\.nuget\NuGet.exe" restore external\RefactoringEssentials\RefactoringEssentials.sln
+".nuget\NuGet.exe" restore external\mono-addins\Mono.Addins.sln
 "C:\Program Files (x86)\MSBuild\14.0\Bin\MSBuild.exe" Main.sln /m /p:Configuration=ReleaseWin32 /p:Platform="Any CPU"
