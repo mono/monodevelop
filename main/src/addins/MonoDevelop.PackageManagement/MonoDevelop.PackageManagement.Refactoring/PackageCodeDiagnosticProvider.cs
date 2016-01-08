28c9b5a79d28b9849fb52fc60a3e7793b635457f
Mike Krüger <mkrueger@xamarin.com>
2016-01-08 09:50:09 +0100

    Revert "Revert "Revert "[Refactoring] Added loading of roslyn analyzers."""
    This reverts commit 308d381e63ca2fa4b82d5e86d929242ffcda0a4d.

--- main/src/addins/MonoDevelop.PackageManagement/MonoDevelop.PackageManagement.Refactoring/PackageCodeDiagnosticProvider.cs
+++ main/src/addins/MonoDevelop.PackageManagement/MonoDevelop.PackageManagement.Refactoring/PackageCodeDiagnosticProvider.cs
@@ -37,9 +37,6 @@ using System.Threading.Tasks;
 using MonoDevelop.Ide.Editor;
 using MonoDevelop.Ide.TypeSystem;
 using MonoDevelop.Ide;
-using System.Security.Cryptography;
-using System.Text;
-using MonoDevelop.Core;
 
 namespace MonoDevelop.PackageManagement.Refactoring
 {
@@ -74,73 +71,34 @@ namespace MonoDevelop.PackageManagement.Refactoring
 			var diags = await GetProjectDiagnosticsAsync (document.Project, language, cancellationToken).ConfigureAwait (false);
 			return diags.Refactorings;
 		}
-		static MD5 md5 = MD5.Create (); 
 
-		public static string GetMD5 (byte[] data)
-		{
-			var result = new StringBuilder();
-			foreach (var b in md5.ComputeHash (data)) {
-				result.Append(b.ToString("X2"));
-			}
-			return result.ToString();
-		}
-
-		internal static ConfigurationProperty<string> loadAnalyzers = ConfigurationProperty.Create ("LoadAnalyzers", "");
-		internal static ConfigurationProperty<string> knownAnalyzers = ConfigurationProperty.Create ("KnownAnalyzers", "");
-
-		async static Task<AnalyzersFromAssembly> GetProjectDiagnosticsAsync (Project project, string language, CancellationToken cancellationToken)
+		static Task<AnalyzersFromAssembly> GetProjectDiagnosticsAsync (Project project, string language, CancellationToken cancellationToken)
 		{
 			if (project == null)
-				return AnalyzersFromAssembly.Empty;
+				return Task.FromResult (AnalyzersFromAssembly.Empty);
 			AnalyzersFromAssembly result;
 			if (diagnosticCache.TryGetValue(project, out result)) 
-				return result;
+				return Task.FromResult (result);
 
 			result = new AnalyzersFromAssembly ();
 
 			var dotNetProject = project as DotNetProject;
 			if (dotNetProject != null) {
 				var proxy = new DotNetProjectProxy (dotNetProject);
-				var analyzingDomain = AppDomain.CreateDomain ("analyzerDomain");
 				if (proxy.HasPackages ()) {
 					var packagesPath = new SolutionPackageRepositoryPath (proxy, PackageManagementServices.Options);
 					foreach (var file in Directory.EnumerateFiles (packagesPath.PackageRepositoryPath, "*.dll", SearchOption.AllDirectories)) {
 						cancellationToken.ThrowIfCancellationRequested ();
 						try {
-							var assembly = File.ReadAllBytes (file);
-							var analyzers = new AnalyzersFromAssembly ();
-							analyzers.AddAssembly (analyzingDomain.Load (assembly));
-							if (analyzers.Fixes.Count + analyzers.Analyzers.Count + analyzers.Refactorings.Count == 0)
-								continue;
-
-							var md5 = GetMD5 (assembly);
-							if (!knownAnalyzers.Value.Contains (md5)) {
-								knownAnalyzers.Value += md5 + "," + Path.GetFileName (file) + ",";
-								await Runtime.RunInMainThread (delegate {
-									var button = MessageService.AskQuestion (GettextCatalog.GetString ("The assembly {0} contains analyzers. Load them ?", Path.GetFileName (file)), AlertButton.Yes, AlertButton.No);
-									if (button == AlertButton.Yes) {
-										loadAnalyzers.Value += md5 + ",";
-										AddAssembly (result, file, md5);
-									}
-								});
-							} else {
-								if (loadAnalyzers.Value.Contains (md5))
-									AddAssembly (result, file, md5);
-							}
+							var asm = Assembly.LoadFrom (file);
+							result.AddAssembly (asm);
 						} catch (Exception) {
 						}
 					}
-					AppDomain.Unload (analyzingDomain); 
 				}
 			}
 			diagnosticCache[project] = result;
-			return result;
-		}
-
-		static void AddAssembly (AnalyzersFromAssembly result, string file, string md5)
-		{
-			var asm = Assembly.LoadFrom (file);
-			result.AddAssembly (asm);
+			return Task.FromResult (result);
 		}
 	}
 }
