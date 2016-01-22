3c41fde39ab17c7275db6a261f5621615cf7e51e
nosami <jasonimison@gmail.com>
2016-01-22 10:30:25 +0000

    working but slow

--- MonoDevelop.FSharpBinding/FSharpNavigationTextEditorExtension.fs
+++ MonoDevelop.FSharpBinding/FSharpNavigationTextEditorExtension.fs
@@ -1,25 +1,48 @@
 ﻿namespace MonoDevelop.FSharp
+
+open System.Threading.Tasks
 open MonoDevelop
+open MonoDevelop.Core
 open MonoDevelop.Ide
 open MonoDevelop.Ide.Editor
 open MonoDevelop.Ide.Editor.Extension
+open Microsoft.FSharp.Compiler
 
 type FSharpNavigationTextEditorExtension() =
     inherit AbstractNavigationExtension()
 
     override x.RequestLinksAsync(offset, length, token) =
+        // return all links for the line at offset
         let editor = base.Editor
         let doc = base.DocumentContext
-        let y =
-            async {
-                match doc.ParsedDocument.TryGetAst () with
-                | None -> return Seq.empty
-                | Some ast ->
-                    match Refactoring.getSymbolAndLineInfoAtCaret ast editor with
-                    | (_line, col, lineTxt), Some symbolUse when Refactoring.Operations.canJump symbolUse editor.FileName doc.Project.ParentSolution ->
-                        let offset = editor.LocationToOffset (DocumentLocation(symbolUse.RangeAlternate.StartLine, symbolUse.RangeAlternate.StartColumn))
-                        let seg = AbstractNavigationExtension.NavigationSegment(offset, length, (fun x -> ()))
-                        return seq { yield seg }
-                    | _ -> return Seq.empty
+
+        match doc.ParsedDocument.TryGetAst () with
+        | None -> Task.FromResult Seq.empty
+        | Some ast ->
+            let getOffset(pos: Range.pos) =
+                editor.LocationToOffset (DocumentLocation(pos.Line, pos.Column + 1))
+            
+            let (line, col, lineStr) = editor.GetLineInfoFromOffset offset
+            LoggingService.LogDebug(sprintf "%d %d %d %s" length line col lineStr)
+            let tokens = Lexer.tokenizeLine lineStr [||] 0 lineStr Lexer.queryLexState
+            let links =
+                asyncSeq {
+                    for token in tokens do
+                        let! symbolUse = ast.GetSymbolAtLocation (line, token.LeftColumn, lineStr)
+                        match symbolUse with
+                        | Some symbol when Refactoring.Operations.canJump symbol editor.FileName doc.Project.ParentSolution -> 
+                            let range = symbol.RangeAlternate
+                            let startOffset = getOffset range.Start
+                            let endOffset = getOffset range.End
+                            LoggingService.LogDebug(symbol.Symbol.DisplayName)
+                            let seg = 
+                                AbstractNavigationExtension.NavigationSegment(startOffset, endOffset - startOffset, 
+                                    (fun () -> Refactoring.jumpToDeclaration(editor, doc, symbol)))
+                            yield seg
+                        | _ -> ()
+                } 
+
+            let computation = async {
+                return links |> AsyncSeq.toSeq
             }
-        y |> Async.StartAsTask
+            Async.StartAsTask(computation, cancellationToken = token)
