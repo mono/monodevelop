905794a0fa935559c621669abbdcac5cb5ef9c35
nosami <jasonimison@gmail.com>
2016-01-22 11:29:27 +0000

    use the symbol cache

--- MonoDevelop.FSharpBinding/FSharpNavigationTextEditorExtension.fs
+++ MonoDevelop.FSharpBinding/FSharpNavigationTextEditorExtension.fs
@@ -7,42 +7,41 @@ open MonoDevelop.Ide
 open MonoDevelop.Ide.Editor
 open MonoDevelop.Ide.Editor.Extension
 open Microsoft.FSharp.Compiler
+open Microsoft.FSharp.Compiler.SourceCodeServices
 
 type FSharpNavigationTextEditorExtension() =
     inherit AbstractNavigationExtension()
 
-    override x.RequestLinksAsync(offset, length, token) =
+    override x.RequestLinksAsync(offset, _length, token) =
         // return all links for the line at offset
         let editor = base.Editor
-        let doc = base.DocumentContext
+        let documentContext = base.DocumentContext
 
-        match doc.ParsedDocument.TryGetAst () with
-        | None -> Task.FromResult Seq.empty
-        | Some ast ->
-            let getOffset(pos: Range.pos) =
-                editor.LocationToOffset (DocumentLocation(pos.Line, pos.Column + 1))
-            
-            let (line, col, lineStr) = editor.GetLineInfoFromOffset offset
-            LoggingService.LogDebug(sprintf "%d %d %d %s" length line col lineStr)
-            let tokens = Lexer.tokenizeLine lineStr [||] 0 lineStr Lexer.queryLexState
-            let links =
-                asyncSeq {
-                    for token in tokens do
-                        let! symbolUse = ast.GetSymbolAtLocation (line, token.LeftColumn, lineStr)
-                        match symbolUse with
-                        | Some symbol when Refactoring.Operations.canJump symbol editor.FileName doc.Project.ParentSolution -> 
-                            let range = symbol.RangeAlternate
-                            let startOffset = getOffset range.Start
-                            let endOffset = getOffset range.End
-                            LoggingService.LogDebug(symbol.Symbol.DisplayName)
-                            let seg = 
-                                AbstractNavigationExtension.NavigationSegment(startOffset, endOffset - startOffset, 
-                                    (fun () -> Refactoring.jumpToDeclaration(editor, doc, symbol)))
-                            yield seg
-                        | _ -> ()
-                } 
+        let computation = async {
+            let doc = documentContext.ParsedDocument :?> FSharpParsedDocument
+            match doc.TryGetAst () with
+            | None -> return Seq.empty
+            | Some _ast ->
+                let getOffset(pos: Range.pos) =
+                    editor.LocationToOffset (DocumentLocation(pos.Line, pos.Column + 1))
+                
+                let line = editor.OffsetToLineNumber offset
 
-            let computation = async {
-                return links |> AsyncSeq.toSeq
-            }
-            Async.StartAsTask(computation, cancellationToken = token)
+                let segmentFromSymbol (symbol: FSharpSymbolUse) =
+                    let range = symbol.RangeAlternate
+                    let startOffset = getOffset range.Start
+                    let endOffset = getOffset range.End
+                     
+                    AbstractNavigationExtension.NavigationSegment(startOffset, endOffset - startOffset, 
+                        (fun () -> Refactoring.jumpToDeclaration(editor, documentContext, symbol)))
+
+                let filterSymbols (symbol: FSharpSymbolUse) =
+                    symbol.RangeAlternate.StartLine = line
+                    && Refactoring.Operations.canJump symbol editor.FileName documentContext.Project.ParentSolution
+
+                return doc.AllSymbolsKeyed.Values
+                       |> Seq.filter filterSymbols
+                       |> Seq.map segmentFromSymbol
+        }
+
+        Async.StartAsTask(computation, cancellationToken = token)
