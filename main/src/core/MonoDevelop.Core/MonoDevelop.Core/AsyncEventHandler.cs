5d39760ac676d7496a24c92060bb845aaf0294da
Lluis Sanchez <lluis@xamarin.com>
2016-01-28 10:33:31 +0100

    Don't allow closing a solution while one is still loading
    The fix is to delay the UI unlock until solution status has been
    fully restored. Solution preferences restoration is now an async operation.
    
    Fixes bug #37982 - Workspace documents still open after solution closing

--- main/src/core/MonoDevelop.Core/MonoDevelop.Core/AsyncEventHandler.cs
+++ main/src/core/MonoDevelop.Core/MonoDevelop.Core/AsyncEventHandler.cs
@@ -23,14 +23,14 @@
 // LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
 // OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
 // THE SOFTWARE.
+
 using System;
+using System.Threading.Tasks;
+
 namespace MonoDevelop.Core
 {
-	public class AsyncEventHandler
-	{
-		public AsyncEventHandler ()
-		{
-		}
-	}
+	public delegate Task AsyncEventHandler (object sender, EventArgs args);
+
+	public delegate Task AsyncEventHandler<T> (object sender, T args);
 }
 
