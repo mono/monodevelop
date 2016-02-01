af14ae4621239f8c8f93f6f2707d3bcdd6d6df51
iain holmes <iain@xamarin.com>
2016-02-01 17:02:34 +0000

    [MacToolbar] Fix the offset for the statusbar so it always lands on a pixel

--- main/src/addins/MacPlatform/MainToolbar/AwesomeBar.cs
+++ main/src/addins/MacPlatform/MainToolbar/AwesomeBar.cs
@@ -78,7 +78,7 @@ namespace MonoDevelop.MacIntegration.MainToolbar
 			// We only need to work out the width on the left side of the window because the statusbar is centred
 			// Gap + RunButton.Width + Gap + ButtonBar.Width + Gap + Half of StatusBar.Width
 			var spaceLeft = (Frame.Width / 2) - (toolbarPadding + runButtonWidth + toolbarPadding + ButtonBarContainer.Frame.Width + toolbarPadding + (statusbarWidth / 2));
-			StatusBar.Frame = new CGRect ((Frame.Width - statusbarWidth) / 2 + 0.5, 0, statusbarWidth, ToolbarWidgetHeight);
+			StatusBar.Frame = new CGRect (Math.Round ((Frame.Width - statusbarWidth) / 2), 0, statusbarWidth, ToolbarWidgetHeight);
 			SearchBar.Frame = new CGRect (Frame.Width - searchbarWidth - 10, 0, searchbarWidth, ToolbarWidgetHeight);
 
 			var selectorSize = SelectorView.SizeThatFits (new CGSize (spaceLeft, ToolbarWidgetHeight));
