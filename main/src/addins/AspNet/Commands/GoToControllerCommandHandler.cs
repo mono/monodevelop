f4442c665fd6208df3db8f2fdf1623e841217538
Matt Ward <matt.ward@xamarin.com>
2016-02-09 12:39:24 +0000

    [AspNet] Fix potential UI hang in go to controller command
    The command was using GetCompilationAsync (project).Result on the UI
    thread which could block.

--- main/src/addins/AspNet/Commands/GoToControllerCommandHandler.cs
+++ main/src/addins/AspNet/Commands/GoToControllerCommandHandler.cs
@@ -26,6 +26,7 @@
 
 using System;
 using System.Linq;
+using System.Threading.Tasks;
 using Microsoft.CodeAnalysis;
 using MonoDevelop.AspNet.Projects;
 using MonoDevelop.Components.Commands;
@@ -55,21 +56,21 @@ namespace MonoDevelop.AspNet.Commands
 				info.Enabled = info.Visible = false;
 		}
 
-		protected override void Run ()
+		protected override async void Run ()
 		{
 			var doc = IdeApp.Workbench.ActiveDocument;
 			var name = doc.FileName.ParentDirectory.FileName;
-			var controller = FindController (doc.Project, name);
+			var controller = await FindController (doc.Project, name);
 
 			if (controller != null)
-				RefactoringService.RoslynJumpToDeclaration (controller, doc.Project);
+				await RefactoringService.RoslynJumpToDeclaration (controller, doc.Project);
 			else
 				MessageService.ShowError ("Matching controller cannot be found.");
 		}
 
-		INamedTypeSymbol FindController (MonoDevelop.Projects.Project project, string name)
+		async Task<INamedTypeSymbol> FindController (MonoDevelop.Projects.Project project, string name)
 		{
-			var compilation = TypeSystemService.GetCompilationAsync (project).Result;
+			var compilation = await TypeSystemService.GetCompilationAsync (project);
 			if (compilation == null)
 				return null;
 
