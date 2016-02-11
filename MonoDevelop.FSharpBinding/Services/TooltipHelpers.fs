03ef3fa8258814bed06fc7aca6a12761baad8c15
nosami <jasonimison@gmail.com>
2016-02-11 09:56:29 +0000

    indent

--- MonoDevelop.FSharpBinding/Services/TooltipHelpers.fs
+++ MonoDevelop.FSharpBinding/Services/TooltipHelpers.fs
@@ -121,144 +121,144 @@ module TooltipsXml =
         if par = null then None else Some((elementValue addStyle par).ToString())
 
 module TooltipXmlDoc =
-  let rawGenerator = new Monodoc.Generators.RawGenerator()
-  ///lru based memoize
-  let private memoize f n =
-      let lru = ref (ExtCore.Caching.LruCache.create n)
-      fun x -> match (!lru).TryFind x with
-               | Some entry, cache ->
-                   lru := cache
-                   entry
-               | None, cache ->
-                   let res = f x
-                   lru := cache.Add (x, res)
-                   res
-
-  /// Memoize the objects that manage access to XML files, keeping only 20 most used
-  // @todo consider if this needs to be a weak table in some way
-  let private xmlDocProvider =
-      memoize (fun x ->
-          try Some (ICSharpCode.NRefactory.Documentation.XmlDocumentationProvider(x))
-          with exn -> None) 20u
-
-  let private tryExt file ext = Option.condition File.Exists (Path.ChangeExtension(file,ext))
-
-  /// Return the XmlDocumentationProvider for an assembly
-  let findXmlDocProviderForAssembly file  =
-      maybe {let! xmlFile = Option.coalesce (tryExt file "xml") (tryExt file "XML")
-             return! xmlDocProvider xmlFile }
-
-  let findXmlDocProviderForEntity (file, key:string)  =
-      maybe {let! docReader = findXmlDocProviderForAssembly file
-             let doc = docReader.GetDocumentation key
-             if String.IsNullOrEmpty doc then return! None
-             else return doc}
-
-  ///check helpxml exist
-  let tryGetDoc key =
-    let helpTree = MonoDevelop.Projects.HelpService.HelpTree
-    if helpTree = null then None else
-    try
-        let rendered = helpTree.RenderUrl(key, rawGenerator)
-        if rendered = null then
-            None
-        else
-            let doc = XmlDocument()
-            doc.LoadXml rendered
-            Some doc
-    with ex ->
-        LoggingService.LogError ("GetHelpXml failed for key {0}", key, ex)
-        None
-
-  let (|MemberName|_|) (name:string) =
-      let dotRight = name.LastIndexOf '.'
-      if dotRight < 1 || dotRight >= name.Length - 1 then None else
-      let typeName = name.[0..dotRight-1]
-      let elemName = name.[dotRight+1..]
-      Some ("T:" + typeName, elemName)
-
-  let (|Method|_|) (key:string) =
-     if key.StartsWith "M:" then
-         let key = key.[2..]
-         let name,count,args =
-             if not (key.Contains "(") then key, 0, [| |] else
-
-             let pieces = key.Split( [|'('; ')' |], StringSplitOptions.RemoveEmptyEntries)
-             if pieces.Length < 2 then key, 0, [| |] else
-             let nameAndCount = pieces.[0]
-             let argsText = pieces.[1].Replace(")","")
-             let args = argsText.Split(',')
-             if nameAndCount.Contains "`" then
-                 let ps = nameAndCount.Split( [| '`' |],StringSplitOptions.RemoveEmptyEntries)
-                 let noArgs =
-                     try int (ps.[1].Split([| '.' |], StringSplitOptions.RemoveEmptyEntries).[0] )
-                     with _ -> 0
-
-                 nameAndCount, noArgs, args
-             else
-                 nameAndCount, 0, args
-
-         match name with
-         | MemberName(typeName,elemName) -> Some (typeName, elemName, count, args)
-         | _ -> None
-     else None
-
-  let (|FieldPropertyOrEvent|_|) (key:string) =
-     if key.StartsWith "P:" || key.StartsWith "F:" || key.StartsWith "E:" then
-         let name = key.[2..]
-         match name with
-         | MemberName(typeName,elemName) -> Some (typeName, elemName)
-         | _ -> None
-     else None
-
-  let (|Type|_|) (key:string) =
-     if key.StartsWith "T:" then
-        Some key
-     else None
-
-  let private trySelectOverload (nodes: XmlNodeList, argsFromKey:string[]) =
-      if (nodes.Count = 1) then Some nodes.[0] else
-
-      let result =
-        [ for x in nodes -> x ] |> Seq.tryFind (fun curNode ->
-          let paramList = curNode.SelectNodes ("Parameters/*")
-          let paramTypes = [| for p in paramList -> p.Attributes.GetNamedItem("Type").Value |]
-          (paramList <> null) && (argsFromKey.Length = paramList.Count) && paramTypes = argsFromKey )
-
-      match result with
-      | None -> None
-      | Some node ->
-          let docs = node.SelectSingleNode ("Docs")
-          if docs = null then None else Some docs
-
-  let private typeMemberFormatter name =
-    if name = "#ctor" then "/Type/Members/Member[@MemberName='.ctor']"
-    else "/Type/Members/Member[@MemberName='" + name + "']"
-
-  /// Try to find the MonoDoc documentation for a file/key pair representing an entity with documentation
-  let findMonoDocProviderForEntity (_file, key) =
-      match key with
-      | Type(typ) ->
-          maybe {let! docXml = tryGetDoc typ
-                 return docXml.OuterXml}
-      | FieldPropertyOrEvent (parentId, name) ->
-          maybe {let! doc = tryGetDoc (parentId)
-                 let docXml = doc.SelectSingleNode (typeMemberFormatter name)
-                 return docXml.OuterXml }
-      | Method(parentId, name, _count, args) ->
-          maybe {
-                  let! doc = tryGetDoc (parentId)
-                  let nodeXmls = doc.SelectNodes (typeMemberFormatter name)
-                  let! docXml = trySelectOverload (nodeXmls, args)
-                  return docXml.OuterXml }
-      | _ -> LoggingService.LogWarning ("findMonoDocProviderForEntity, No match for key: {0}", key)
-             None
-
-  /// Find the documentation for a file/key pair representing an entity with documentation
-  let findDocForEntity (file, key)  =
-      match findXmlDocProviderForEntity (file, key) with
-      | Some doc -> Some doc
-      | None -> findMonoDocProviderForEntity (file, key)
+    let rawGenerator = new Monodoc.Generators.RawGenerator()
+    ///lru based memoize
+    let private memoize f n =
+        let lru = ref (ExtCore.Caching.LruCache.create n)
+        fun x -> match (!lru).TryFind x with
+                 | Some entry, cache ->
+                     lru := cache
+                     entry
+                 | None, cache ->
+                     let res = f x
+                     lru := cache.Add (x, res)
+                     res
+    
+    /// Memoize the objects that manage access to XML files, keeping only 20 most used
+    // @todo consider if this needs to be a weak table in some way
+    let private xmlDocProvider =
+        memoize (fun x ->
+            try Some (ICSharpCode.NRefactory.Documentation.XmlDocumentationProvider(x))
+            with exn -> None) 20u
+    
+    let private tryExt file ext = Option.condition File.Exists (Path.ChangeExtension(file,ext))
+    
+    /// Return the XmlDocumentationProvider for an assembly
+    let findXmlDocProviderForAssembly file  =
+        maybe {let! xmlFile = Option.coalesce (tryExt file "xml") (tryExt file "XML")
+               return! xmlDocProvider xmlFile }
+    
+    let findXmlDocProviderForEntity (file, key:string)  =
+        maybe {let! docReader = findXmlDocProviderForAssembly file
+               let doc = docReader.GetDocumentation key
+               if String.IsNullOrEmpty doc then return! None
+               else return doc}
+    
+    ///check helpxml exist
+    let tryGetDoc key =
+      let helpTree = MonoDevelop.Projects.HelpService.HelpTree
+      if helpTree = null then None else
+      try
+          let rendered = helpTree.RenderUrl(key, rawGenerator)
+          if rendered = null then
+              None
+          else
+              let doc = XmlDocument()
+              doc.LoadXml rendered
+              Some doc
+      with ex ->
+          LoggingService.LogError ("GetHelpXml failed for key {0}", key, ex)
+          None
+    
+    let (|MemberName|_|) (name:string) =
+        let dotRight = name.LastIndexOf '.'
+        if dotRight < 1 || dotRight >= name.Length - 1 then None else
+        let typeName = name.[0..dotRight-1]
+        let elemName = name.[dotRight+1..]
+        Some ("T:" + typeName, elemName)
+    
+    let (|Method|_|) (key:string) =
+       if key.StartsWith "M:" then
+           let key = key.[2..]
+           let name,count,args =
+               if not (key.Contains "(") then key, 0, [| |] else
+    
+               let pieces = key.Split( [|'('; ')' |], StringSplitOptions.RemoveEmptyEntries)
+               if pieces.Length < 2 then key, 0, [| |] else
+               let nameAndCount = pieces.[0]
+               let argsText = pieces.[1].Replace(")","")
+               let args = argsText.Split(',')
+               if nameAndCount.Contains "`" then
+                   let ps = nameAndCount.Split( [| '`' |],StringSplitOptions.RemoveEmptyEntries)
+                   let noArgs =
+                       try int (ps.[1].Split([| '.' |], StringSplitOptions.RemoveEmptyEntries).[0] )
+                       with _ -> 0
+    
+                   nameAndCount, noArgs, args
+               else
+                   nameAndCount, 0, args
+    
+           match name with
+           | MemberName(typeName,elemName) -> Some (typeName, elemName, count, args)
+           | _ -> None
+       else None
+    
+    let (|FieldPropertyOrEvent|_|) (key:string) =
+       if key.StartsWith "P:" || key.StartsWith "F:" || key.StartsWith "E:" then
+           let name = key.[2..]
+           match name with
+           | MemberName(typeName,elemName) -> Some (typeName, elemName)
+           | _ -> None
+       else None
+    
+    let (|Type|_|) (key:string) =
+       if key.StartsWith "T:" then
+          Some key
+       else None
+    
+    let private trySelectOverload (nodes: XmlNodeList, argsFromKey:string[]) =
+        if (nodes.Count = 1) then Some nodes.[0] else
+    
+        let result =
+          [ for x in nodes -> x ] |> Seq.tryFind (fun curNode ->
+            let paramList = curNode.SelectNodes ("Parameters/*")
+            let paramTypes = [| for p in paramList -> p.Attributes.GetNamedItem("Type").Value |]
+            (paramList <> null) && (argsFromKey.Length = paramList.Count) && paramTypes = argsFromKey )
+    
+        match result with
+        | None -> None
+        | Some node ->
+            let docs = node.SelectSingleNode ("Docs")
+            if docs = null then None else Some docs
+    
+    let private typeMemberFormatter name =
+      if name = "#ctor" then "/Type/Members/Member[@MemberName='.ctor']"
+      else "/Type/Members/Member[@MemberName='" + name + "']"
+    
+    /// Try to find the MonoDoc documentation for a file/key pair representing an entity with documentation
+    let findMonoDocProviderForEntity (_file, key) =
+        match key with
+        | Type(typ) ->
+            maybe {let! docXml = tryGetDoc typ
+                   return docXml.OuterXml}
+        | FieldPropertyOrEvent (parentId, name) ->
+            maybe {let! doc = tryGetDoc (parentId)
+                   let docXml = doc.SelectSingleNode (typeMemberFormatter name)
+                   return docXml.OuterXml }
+        | Method(parentId, name, _count, args) ->
+            maybe {
+                    let! doc = tryGetDoc (parentId)
+                    let nodeXmls = doc.SelectNodes (typeMemberFormatter name)
+                    let! docXml = trySelectOverload (nodeXmls, args)
+                    return docXml.OuterXml }
+        | _ -> LoggingService.LogWarning ("findMonoDocProviderForEntity, No match for key: {0}", key)
+               None
+    
+    /// Find the documentation for a file/key pair representing an entity with documentation
+    let findDocForEntity (file, key)  =
+        match findXmlDocProviderForEntity (file, key) with
+        | Some doc -> Some doc
+        | None -> findMonoDocProviderForEntity (file, key)
 
 /// Formatting of TooltipElement information displayed in tooltips and autocompletion
 module TooltipFormatting =
