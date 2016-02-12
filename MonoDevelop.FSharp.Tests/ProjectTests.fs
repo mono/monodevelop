9b51edb3ee60eff33c08a614f316045dec3ab3cd
nosami <jasonimison@gmail.com>
2016-02-12 00:53:40 +0000

    fix windows tests

--- MonoDevelop.FSharp.Tests/ProjectTests.fs
+++ MonoDevelop.FSharp.Tests/ProjectTests.fs
@@ -46,17 +46,18 @@ type ProjectTests() =
 
     [<Test>]
     member this.``Adds desktop conditional FSharp targets``() =
-        let project = Services.ProjectService.CreateDotNetProject ("F#") :?> FSharpProject
-        Project.addConditionalTargets project.MSBuildProject
-        let s = project.MSBuildProject.SaveToString()
-        s |> shouldEqualIgnoringLineEndings
-            """<?xml version="1.0" encoding="utf-8"?>
-<Project xmlns="http://schemas.microsoft.com/developer/msbuild/2003">
-  <PropertyGroup />
-  <PropertyGroup>
-    <FSharpTargetsPath>$(MSBuildExtensionsPath32)\Microsoft\VisualStudio\v$(VisualStudioVersion)\FSharp\Microsoft.FSharp.Targets</FSharpTargetsPath>
-  </PropertyGroup>
-  <PropertyGroup Condition="'$(VisualStudioVersion)' == '10.0' OR '$(VisualStudioVersion)' == '11.0'">
-    <FSharpTargetsPath>$(MSBuildExtensionsPath32)\..\Microsoft SDKs\F#\3.0\Framework\v4.0\Microsoft.FSharp.Targets</FSharpTargetsPath>
-  </PropertyGroup>
-</Project>"""
+        if not MonoDevelop.Core.Platform.IsWindows then
+            let project = Services.ProjectService.CreateDotNetProject ("F#") :?> FSharpProject
+            Project.addConditionalTargets project.MSBuildProject
+            let s = project.MSBuildProject.SaveToString()
+            s |> shouldEqualIgnoringLineEndings
+                """<?xml version="1.0" encoding="utf-8"?>
+    <Project xmlns="http://schemas.microsoft.com/developer/msbuild/2003">
+      <PropertyGroup />
+      <PropertyGroup>
+        <FSharpTargetsPath>$(MSBuildExtensionsPath32)\Microsoft\VisualStudio\v$(VisualStudioVersion)\FSharp\Microsoft.FSharp.Targets</FSharpTargetsPath>
+      </PropertyGroup>
+      <PropertyGroup Condition="'$(VisualStudioVersion)' == '10.0' OR '$(VisualStudioVersion)' == '11.0'">
+        <FSharpTargetsPath>$(MSBuildExtensionsPath32)\..\Microsoft SDKs\F#\3.0\Framework\v4.0\Microsoft.FSharp.Targets</FSharpTargetsPath>
+      </PropertyGroup>
+    </Project>"""
