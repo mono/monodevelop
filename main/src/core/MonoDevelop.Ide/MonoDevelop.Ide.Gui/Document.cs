mono/monodevelop

commit fa99d4d91344139f3f00d787b89f18a272da7269

[Ide] Editor file name is used for parsing with priority.
This makes the android designer editor view easier.

main/src/core/MonoDevelop.Ide/MonoDevelop.Ide.Gui/Document.cs
@@ -740,7 +740,7 @@ namespace MonoDevelop.Ide.Gui
 		{
 			try {
 				await EnsureAnalysisDocumentIsOpen ();
-				string currentParseFile = FileName;
+				string currentParseFile = GetCurrentParseFileName();
 				var editor = Editor;
 				if (editor == null || string.IsNullOrEmpty (currentParseFile))
 					return null;
@@ -874,7 +874,7 @@ namespace MonoDevelop.Ide.Gui
 
 		internal void StartReparseThread ()
 		{
-			string currentParseFile = adhocProject != null ? adHocFile : FileName;
+			string currentParseFile = GetCurrentParseFileName ();
 			if (string.IsNullOrEmpty (currentParseFile))
 				return;
 			CancelParseTimeout ();
@@ -884,6 +884,12 @@ namespace MonoDevelop.Ide.Gui
 				parseTimeout = 0;
 				return false;
 			});
+		}
+
+		string GetCurrentParseFileName ()
+		{
+			string result = adhocProject != null ? adHocFile : Editor.FileName;
+			return result ?? FileName;
 		}
 
 		async void StartReparseThreadDelayed (FilePath currentParseFile)

commit 1b081e7a8b0368bb971913dd504718014966ce5a

[Ide] Fix null reference when document has no editor.
Fixed bug #38833  - Getting error Error adding to project: object
reference not set to an instance of an object when adding component.
https://bugzilla.xamarin.com/show_bug.cgi?id=38833

On adding a Xamarin Component to a project a reference may be added
to the project. This then triggers a reparse of the document,
including the document associated with the component's view content
which has no associated editor so a null reference occurred when the
editor was used without checking if it was null. This problem was
introduced by commit fa99d4d91344139f3f00d787b89f18a272da7269

System.NullReferenceException: Object reference not set to an instance of an object

Server stack trace:
   at MonoDevelop.Ide.Gui.Document.GetCurrentParseFileName () in monodevelop/main/src/core/MonoDevelop.Ide/MonoDevelop.Ide.Gui/Document.cs:891
   at MonoDevelop.Ide.Gui.Document.StartReparseThread () in monodevelop/main/src/core/MonoDevelop.Ide/MonoDevelop.Ide.Gui/Document.cs:877
   at MonoDevelop.Ide.Gui.Document.HandleProjectModified (System.Object sender, MonoDevelop.Projects.SolutionItemModifiedEventArgs e) in monodevelop/main/src/core/MonoDevelop.Ide/MonoDevelop.Ide.Gui/Document.cs:730
   at (wrapper delegate-invoke) <Module>:invoke_void_object_SolutionItemModifiedEventArgs (object,MonoDevelop.Projects.SolutionItemModifiedEventArgs)
   at MonoDevelop.Projects.SolutionFolderItem.OnModified (MonoDevelop.Projects.SolutionItemModifiedEventArgs args) in monodevelop/main/src/core/MonoDevelop.Core/MonoDevelop.Projects/SolutionFolderItem.cs:445
   at MonoDevelop.Projects.Project.OnModified (MonoDevelop.Projects.SolutionItemModifiedEventArgs args) in monodevelop/main/src/core/MonoDevelop.Core/MonoDevelop.Projects/Project.cs:320
   at MonoDevelop.Projects.SolutionFolderItem.NotifyModified (System.String hint) in monodevelop/main/src/core/MonoDevelop.Core/MonoDevelop.Projects/SolutionFolderItem.cs:432

main/src/core/MonoDevelop.Ide/MonoDevelop.Ide.Gui/Document.cs
@@ -888,7 +888,8 @@ namespace MonoDevelop.Ide.Gui
 
 		string GetCurrentParseFileName ()
 		{
-			string result = adhocProject != null ? adHocFile : Editor.FileName;
+			var editor = Editor;
+			string result = adhocProject != null ? adHocFile : editor?.FileName;
 			return result ?? FileName;
 		}
 