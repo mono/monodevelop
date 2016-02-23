77a72a0f653570875444f6d8a3778dfde7f2b307
Mike Krüger <mkrueger@xamarin.com>
2016-02-23 08:32:26 +0100

    Fixed 'Bug 39015 - New keyword missing from completion'

--- main/tests/MonoDevelop.CSharpBinding.Tests/Features/CodeCompletion/NR6/MiscTests.cs
+++ main/tests/MonoDevelop.CSharpBinding.Tests/Features/CodeCompletion/NR6/MiscTests.cs
@@ -140,5 +140,38 @@ namespace TestProject
 			Assert.IsNull (provider.Find ("TestProject.TestClass.FooBar.Bar"));
 			Assert.IsNotNull (provider.Find ("TestClass.FooBar.Bar"));
 		}
+
+		/// <summary>
+		/// Bug 39015 - New keyword missing from completion
+		/// </summary>
+		[Test]
+		public void TestBug39015 ()
+		{
+			var provider = CreateProvider (
+				@"
+using System;
+
+namespace Foo
+{
+	enum TestEnum { A, B }
+	public class Test
+	{
+		void FooBar (int i, ConsoleKey t)
+		{
+		}
+		void FooBar (int i, object o)
+		{
+		}
+
+		void TestMe ()
+		{
+			$this.FooBar (12, $
+			
+		}
+	}
+}", usePreviousCharAsTrigger: true);
+			Assert.IsNotNull (provider, "provider was not created.");
+			Assert.IsNotNull (provider.Find ("new"));
+		}
 	}
 }
