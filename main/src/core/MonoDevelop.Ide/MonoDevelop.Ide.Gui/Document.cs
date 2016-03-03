c03437808204162bcdad773f952d2f7d1e760242
Mike Krüger <mkrueger@xamarin.com>
2016-03-03 07:53:12 +0100

    Fixed 'Bug 39287 - [Regression] XS does not detect external changes to source files'.
    TrackFileChanges is not used in roslyn anymore and was broken.

--- main/src/core/MonoDevelop.Ide/MonoDevelop.Ide.Gui/Document.cs
+++ main/src/core/MonoDevelop.Ide/MonoDevelop.Ide.Gui/Document.cs
@@ -368,7 +368,6 @@ namespace MonoDevelop.Ide.Gui
 		{
 			// suspend type service "check all file loop" since we have already a parsed document.
 			// Or at least one that updates "soon".
-			TypeSystemService.TrackFileChanges = false;
 			try {
 				// Freeze the file change events. There can be several such events, and sending them all together
 				// is more efficient
@@ -425,7 +424,6 @@ namespace MonoDevelop.Ide.Gui
 						LoggingService.LogWarning ("Exception while getting the write time from " + fileName, e); 
 					}
 				}
-				TypeSystemService.TrackFileChanges = true;
 			}
 		}
 
