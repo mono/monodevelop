6d00f85bb1c5fa0b532ef26c709815438133bff9
Vaclav Vancura <vaclav@vancura.org>
2016-03-30 13:13:47 +0200

    [PackageManagement] Fixing selected URL colors in sources preferences

--- main/src/addins/MonoDevelop.PackageManagement/MonoDevelop.PackageManagement.Gui/Styles.cs
+++ main/src/addins/MonoDevelop.PackageManagement/MonoDevelop.PackageManagement.Gui/Styles.cs
@@ -24,6 +24,7 @@
 // OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
 // THE SOFTWARE.
 using MonoDevelop.Ide;
+using MonoDevelop.Core;
 
 namespace MonoDevelop.PackageManagement
 {
@@ -67,11 +68,14 @@ namespace MonoDevelop.PackageManagement
 			CellSelectionColor = Ide.Gui.Styles.BaseSelectionBackgroundColor;
 			CellTextSelectionColor = Ide.Gui.Styles.BaseSelectionTextColor;
 
-			PackageSourceUrlSelectedTextColor = PackageSourceUrlTextColor;
-			PackageSourceErrorSelectedTextColor = PackageSourceErrorTextColor;
 			PackageInfoBackgroundColor = Ide.Gui.Styles.SecondaryBackgroundLighterColor;
 			PackageSourceErrorTextColor = Ide.Gui.Styles.ErrorForegroundColor;
 			PackageSourceUrlTextColor = Ide.Gui.Styles.DimTextColor;
+
+			PackageSourceErrorSelectedTextColor = PackageSourceErrorTextColor;
+
+			// Blue selection text color only on Mac
+			PackageSourceUrlSelectedTextColor = Platform.IsMac ? Xwt.Drawing.Color.FromName ("#ffffff") : Ide.Gui.Styles.DimTextColor;
 
 			LineBorderColor = Ide.Gui.Styles.SeparatorColor;
 
