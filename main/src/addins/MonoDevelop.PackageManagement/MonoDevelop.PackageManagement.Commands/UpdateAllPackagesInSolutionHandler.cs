b65885e1aa626ed99d29d7d053ae3d2070295120
Matt Ward <matt.ward@xamarin.com>
2016-04-01 14:13:14 +0100

    [NuGet] Updating all packages in solution now uses NuGet 3.
    No restore is run before doing the update.
    No updates available message is not shown in the status bar.

--- main/src/addins/MonoDevelop.PackageManagement/MonoDevelop.PackageManagement.Commands/UpdateAllPackagesInSolutionHandler.cs
+++ main/src/addins/MonoDevelop.PackageManagement/MonoDevelop.PackageManagement.Commands/UpdateAllPackagesInSolutionHandler.cs
@@ -28,7 +28,6 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using MonoDevelop.Components.Commands;
-using MonoDevelop.Core;
 
 namespace MonoDevelop.PackageManagement.Commands
 {
@@ -37,35 +36,28 @@ namespace MonoDevelop.PackageManagement.Commands
 		protected override void Run ()
 		{
 			try {
-				IPackageManagementSolution solution = GetPackageManagementSolution ();
-				UpdateAllPackagesInSolution updateAllPackages = CreateUpdateAllPackagesInSolution (solution);
-				ProgressMonitorStatusMessage progressMessage = ProgressMonitorStatusMessageFactory.CreateUpdatingPackagesInSolutionMessage (updateAllPackages.Projects);
-				RestoreBeforeUpdateAction.Restore (solution, updateAllPackages.Projects, () => {
-					Runtime.RunInMainThread (() => {
-						Update (updateAllPackages, progressMessage);
-					}).Wait ();
-				});
+				// TODO: Restore before update.
+				//RestoreBeforeUpdateAction.Restore (solution, updateAllPackages.Projects, () => {
+				//	Runtime.RunInMainThread (() => {
+				//		Update (updateAllPackages, progressMessage);
+				//	}).Wait ();
+				//});
+				Update ();
 			} catch (Exception ex) {
 				ProgressMonitorStatusMessage progressMessage = ProgressMonitorStatusMessageFactory.CreateUpdatingPackagesInSolutionMessage ();
 				PackageManagementServices.BackgroundPackageActionRunner.ShowError (progressMessage, ex);
 			}
 		}
 
-		void Update (UpdateAllPackagesInSolution updateAllPackages, ProgressMonitorStatusMessage progressMessage)
+		void Update ()
 		{
-			try {
-				List<UpdatePackageAction> updateActions = updateAllPackages.CreateActions ().ToList ();
-				PackageManagementServices.BackgroundPackageActionRunner.Run (progressMessage, updateActions);
-			} catch (Exception ex) {
-				PackageManagementServices.BackgroundPackageActionRunner.ShowError (progressMessage, ex);
-			}
-		}
+			var solutionManager = new MonoDevelopSolutionManager (GetSelectedSolution ());
+			var updateAllPackages = new UpdateAllNuGetPackagesInSolution (solutionManager);
+			List<IPackageAction> updateActions = updateAllPackages.CreateActions ().ToList ();
 
-		UpdateAllPackagesInSolution CreateUpdateAllPackagesInSolution (IPackageManagementSolution solution)
-		{
-			return new UpdateAllPackagesInSolution (
-				solution,
-				PackageManagementServices.PackageRepositoryCache.CreateAggregateRepository ());
+			// TODO: No updates available message.
+			ProgressMonitorStatusMessage progressMessage = ProgressMonitorStatusMessageFactory.CreateUpdatingPackagesInSolutionMessage ();
+			PackageManagementServices.BackgroundPackageActionRunner.Run (progressMessage, updateActions);
 		}
 
 		protected override void Update (CommandInfo info)
