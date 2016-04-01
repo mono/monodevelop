979deea612976ed0ad8de5c1f2c5f45b65aebb41
Mike Krüger <mkrueger@xamarin.com>
2016-04-01 07:53:19 +0200

    Fixed 'Bug 40014 - warning and breakpoint icons collision'

--- main/src/addins/MonoDevelop.SourceEditor2/MonoDevelop.SourceEditor/SourceEditorView.cs
+++ main/src/addins/MonoDevelop.SourceEditor2/MonoDevelop.SourceEditor/SourceEditorView.cs
@@ -584,7 +584,7 @@ namespace MonoDevelop.SourceEditor
 							oldMarker.AddError (marker.Task, marker.Task.Severity == TaskSeverity.Error, marker.Task.Description);
 						} else {
 							marker.LineSegment = lineSegment;
-							widget.Document.AddMarker (lineSegment, marker, false);
+							widget.Document.AddMarker (lineSegment, marker, false, 0);
 							newErrorMarkers.Add (marker);
 						}
 					}
