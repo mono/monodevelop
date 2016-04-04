0bceafc1a793ad124de7252369ad74eb4a635cd1
Vsevolod Kukol <sevoku@xamarin.com>
2016-04-04 10:18:28 +0200

    [UnitTesting] Update Cell foreground colors on style change

--- main/src/addins/MonoDevelop.UnitTesting/Gui/TestResultsPad.cs
+++ main/src/addins/MonoDevelop.UnitTesting/Gui/TestResultsPad.cs
@@ -127,6 +127,8 @@ namespace MonoDevelop.UnitTesting
 			col.AddAttribute (tr, "markup", 1);
 			failuresTreeView.AppendColumn (col);
 			failuresTreeView.Model = failuresStore;
+			failuresTreeView.StyleSet += (o, args) => 
+				tr.Foreground = Styles.BaseForegroundColor.ToHexString (false);
 		
 			var sw = new MonoDevelop.Components.CompactScrolledWindow ();
 			sw.ShadowType = ShadowType.None;
