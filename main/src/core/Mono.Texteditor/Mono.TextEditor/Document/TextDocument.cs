33df84dddf46f386dab505cfa7e92da96635ac80
Mike Krüger <mkrueger@xamarin.com>
2016-04-04 10:05:39 +0200

    [TextEditor] Removed wait update in atomic undo.
    Shouldn't be needed any longer.

--- main/src/core/Mono.Texteditor/Mono.TextEditor/Document/TextDocument.cs
+++ main/src/core/Mono.Texteditor/Mono.TextEditor/Document/TextDocument.cs
@@ -969,10 +969,6 @@ namespace Mono.TextEditor
 		internal void BeginAtomicUndo (OperationType operationType = OperationType.Undefined)
 		{
 			currentAtomicUndoOperationType.Push (operationType);
-			if (atomicUndoLevel == 0) {
-				if (this.syntaxMode != null && !SuppressHighlightUpdate)
-					Mono.TextEditor.Highlighting.SyntaxModeService.WaitUpdate (this);
-			}
 			if (currentAtomicOperation == null) {
 				Debug.Assert (atomicUndoLevel == 0); 
 				currentAtomicOperation = new AtomicUndoOperation (operationType);
