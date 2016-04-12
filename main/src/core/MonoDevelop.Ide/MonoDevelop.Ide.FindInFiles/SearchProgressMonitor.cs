0b9da61c9e836de36a703423d46f3c5ccfbf8cc7
Vsevolod Kukol <sevoku@xamarin.com>
2016-04-12 10:31:47 +0200

    [Ide] Focus Search result pad on search dialog closed
    (fixes bug #37577)

--- main/src/core/MonoDevelop.Ide/MonoDevelop.Ide.FindInFiles/SearchProgressMonitor.cs
+++ main/src/core/MonoDevelop.Ide/MonoDevelop.Ide.FindInFiles/SearchProgressMonitor.cs
@@ -39,6 +39,12 @@ namespace MonoDevelop.Ide.FindInFiles
 	{
 		SearchResultPad outputPad;
 
+		internal SearchResultPad ResultPad {
+			get {
+				return outputPad;
+			}
+		}
+
 		internal SearchProgressMonitor (Pad pad, CancellationTokenSource cancellationTokenSource = null): base (Runtime.MainSynchronizationContext, cancellationTokenSource)
 		{
 			AddFollowerMonitor (IdeApp.Workbench.ProgressMonitors.GetStatusProgressMonitor (GettextCatalog.GetString ("Searching..."), Stock.StatusSearch, false, true, false, pad));
