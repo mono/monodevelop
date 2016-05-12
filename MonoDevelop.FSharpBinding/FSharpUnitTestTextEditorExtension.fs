f82fdf28bebb1dbd39c30d009f5fe20d1c6c3c14
Lluis Sanchez <lluis@xamarin.com>
2016-05-12 13:41:38 +0200

    Track API changes

--- MonoDevelop.FSharpBinding/FSharpUnitTestTextEditorExtension.fs
+++ MonoDevelop.FSharpBinding/FSharpUnitTestTextEditorExtension.fs
@@ -99,8 +99,8 @@ module unitTestGatherer =
                 dnp.GetReferencedAssemblies(MonoDevelop.getConfig())
                 |> Async.AwaitTask
                 |> Async.RunSynchronously
-                |> Seq.exists (fun r -> r.EndsWith ("nunit.framework.dll", StringComparison.InvariantCultureIgnoreCase)
-                                        || r.EndsWith ("GuiUnit.exe", StringComparison.InvariantCultureIgnoreCase)) 
+                |> Seq.exists (fun r -> r.FilePath.ToString().EndsWith ("nunit.framework.dll", StringComparison.InvariantCultureIgnoreCase)
+                                        || r.FilePath.ToString().EndsWith ("GuiUnit.exe", StringComparison.InvariantCultureIgnoreCase)) 
             with ex ->
                 MonoDevelop.Core.LoggingService.LogInternalError ("FSharpUnitTestTextEditorExtension: GatherUnitTests failed", ex)
                 false
