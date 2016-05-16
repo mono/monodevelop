mono/monodevelop

commit 7410601578fda27f6bba68e40ea9fec6e80415aa

WIP (#1419)

main/src/addins/CSharpBinding/MonoDevelop.CSharp.Diagnostics/InconsistentNaming/NameConventionPanelWidget.cs
@@ -55,19 +55,13 @@ namespace MonoDevelop.CSharp.Diagnostics.InconsistentNaming
 			var ct1 = new CellRendererText ();
 			var col1 = treeviewConventions.AppendColumn (GettextCatalog.GetString ("Rule"), ct1);
 			col1.Expand = true;
-			col1.SetCellDataFunc (ct1, delegate (TreeViewColumn column, CellRenderer cell, TreeModel model, TreeIter iter) {
-				var rule = (NameConventionRule)model.GetValue (iter, 0);
-				ct1.Text = rule.Name;
-			});
+			col1.SetCellDataFunc (ct1, NameConventionRuleNameDataFunc);
 			
 			
 			var ct2 = new CellRendererText ();
 			var col2 = treeviewConventions.AppendColumn (GettextCatalog.GetString ("Example"), ct2);
 			col2.Expand = true;
-			col2.SetCellDataFunc (ct2, delegate (TreeViewColumn column, CellRenderer cell, TreeModel model, TreeIter iter) {
-				var rule = (NameConventionRule)model.GetValue (iter, 0);
-				ct2.Text = rule.GetPreview ();
-			});
+			col2.SetCellDataFunc (ct2, NameConventionRuleNameDataFunc);
 			
 			treeviewConventions.Model = treeStore;
 			treeviewConventions.Selection.Changed += HandleSelectionChanged;
@@ -77,6 +71,12 @@ namespace MonoDevelop.CSharp.Diagnostics.InconsistentNaming
 			buttonAdd.Clicked += (o, s) => AddEntry ();
 
 			HandleSelectionChanged (null, null);
+		}
+
+		static void NameConventionRuleNameDataFunc (TreeViewColumn column, CellRenderer cell, TreeModel model, TreeIter iter)
+		{
+			var rule = (NameConventionRule)model.GetValue (iter, 0);
+			((CellRendererText)cell).Text = rule.Name;
 		}
 
 		void HandleSelectionChanged (object sender, EventArgs e)

main/src/addins/CSharpBinding/MonoDevelop.CSharp.Formatting/CSharpFormattingProfileDialog.cs
@@ -696,7 +696,7 @@ namespace MonoDevelop.CSharp.Formatting
 			return info.GetValue (profile, null);
 		}
 		
-		static void RenderIcon (TreeViewColumn col, CellRenderer cell, TreeModel model, TreeIter iter) 
+		static void RenderIcon (TreeViewColumn col, CellRenderer cell, TreeModel model, TreeIter iter)
 		{
 			var pixbufCellRenderer = (CellRendererImage)cell;
 			if (model.IterHasChild (iter)) {
@@ -706,7 +706,7 @@ namespace MonoDevelop.CSharp.Formatting
 			}
 		}
 		
-		void ComboboxDataFunc (TreeViewColumn col, CellRenderer cell, TreeModel model, TreeIter iter) 
+		static void ComboboxDataFunc (TreeViewColumn col, CellRenderer cell, TreeModel model, TreeIter iter)
 		{
 			var cellRenderer = (CellRendererCombo)cell;
 			var info = GetProperty (model, iter);
@@ -714,17 +714,21 @@ namespace MonoDevelop.CSharp.Formatting
 				cellRenderer.Text = "<invalid>";
 				return;
 			}
+
+			var profile = ((CSharpFormattingProfileDialog)col.TreeView.Toplevel).profile;
 			object value = info.GetValue (profile, null);
 			
 			cellRenderer.Text = value is Enum ? TranslateValue (value) : value.ToString ();
 		}
 		
-		void ToggleDataFunc (TreeViewColumn col, CellRenderer cell, TreeModel model, TreeIter iter) 
+		static void ToggleDataFunc (TreeViewColumn col, CellRenderer cell, TreeModel model, TreeIter iter)
 		{
 			var cellRenderer = (CellRendererToggle)cell;
 			var info = GetProperty (model, iter);
 			if (info == null || info.PropertyType != typeof(bool)) 
 				return;
+
+			var profile = ((CSharpFormattingProfileDialog)col.TreeView.Toplevel).profile;
 			bool value = (bool)info.GetValue (profile, null);
 			cellRenderer.Active = value;
 		}

main/src/addins/MonoDevelop.Debugger/MonoDevelop.Debugger/ObjectValueTreeView.cs
@@ -329,26 +329,7 @@ namespace MonoDevelop.Debugger
 			valueCol.AddAttribute (evaluateStatusCell, "image", EvaluateStatusIconColumn);
 			var crColorPreview = new CellRendererColorPreview ();
 			valueCol.PackStart (crColorPreview, false);
-			valueCol.SetCellDataFunc (crColorPreview, new TreeCellDataFunc ((tree_column, cell, model, iter) => {
-				var val = (ObjectValue) model.GetValue (iter, ObjectColumn);
-				Xwt.Drawing.Color? color;
-
-				if (val != null && !val.IsNull && DebuggingService.HasGetConverter<Xwt.Drawing.Color> (val)) {
-					try {
-						color = DebuggingService.GetGetConverter<Xwt.Drawing.Color> (val).GetValue (val);
-					} catch (Exception) {
-						color = null;
-					}
-				} else {
-					color = null;
-				}
-				if (color != null) {
-					((CellRendererColorPreview) cell).Color = (Xwt.Drawing.Color) color;
-					cell.Visible = true;
-				} else {
-					cell.Visible = false;
-				}
-			}));
+			valueCol.SetCellDataFunc (crColorPreview, ValueDataFunc);
 			crpButton = new CellRendererRoundedButton ();
 			valueCol.PackStart (crpButton, false);
 			valueCol.AddAttribute (crpButton, "visible", ValueButtonVisibleColumn);
@@ -416,6 +397,28 @@ namespace MonoDevelop.Debugger
 			horizontal_separator = (int)this.StyleGetProperty ("horizontal-separator");
 			grid_line_width = (int)this.StyleGetProperty ("grid-line-width");
 			focus_line_width = (int)this.StyleGetProperty ("focus-line-width") * 2;//we just use *2 version in GetMaxWidth
+		}
+
+		static void ValueDataFunc (Gtk.TreeViewColumn tree_column, Gtk.CellRenderer cell, Gtk.TreeModel model, Gtk.TreeIter iter)
+		{
+			var val = (ObjectValue)model.GetValue (iter, ObjectColumn);
+			Xwt.Drawing.Color? color;
+
+			if (val != null && !val.IsNull && DebuggingService.HasGetConverter<Xwt.Drawing.Color> (val)) {
+				try {
+					color = DebuggingService.GetGetConverter<Xwt.Drawing.Color> (val).GetValue (val);
+				} catch (Exception) {
+					color = null;
+				}
+			} else {
+				color = null;
+			}
+			if (color != null) {
+				((CellRendererColorPreview)cell).Color = (Xwt.Drawing.Color)color;
+				cell.Visible = true;
+			} else {
+				cell.Visible = false;
+			}
 		}
 
 		int expanderSize;

main/src/addins/MonoDevelop.Refactoring/MonoDevelop.CodeIssues/CodeIssuePanelWidget.cs
@@ -253,15 +253,7 @@ namespace MonoDevelop.CodeIssues
 			treeviewInspections.AppendColumn (titleCol);
 			titleCol.PackStart (toggleRenderer, false);
 			titleCol.Sizing = TreeViewColumnSizing.Autosize;
-			titleCol.SetCellDataFunc (toggleRenderer, delegate (TreeViewColumn treeColumn, CellRenderer cell, TreeModel model, TreeIter iter) {
-				var provider = (Tuple<CodeDiagnosticDescriptor, DiagnosticDescriptor>)treeStore.GetValue (iter, 1);
-				if (provider == null) {
-					toggleRenderer.Visible = false;
-					return;
-				}
-				toggleRenderer.Visible = true;
-				toggleRenderer.Active = enableState[provider];
-			});
+			titleCol.SetCellDataFunc (toggleRenderer, TitleColDataFunc);
 
 
 			var cellRendererText = new CellRendererText {
@@ -317,25 +309,43 @@ namespace MonoDevelop.CodeIssues
 				} while (comboBoxStore.IterNext (ref storeIter));
 			};
 			
-			col.SetCellDataFunc (comboRenderer, delegate (TreeViewColumn treeColumn, CellRenderer cell, TreeModel model, TreeIter iter) {
-				var provider = (Tuple<CodeDiagnosticDescriptor, DiagnosticDescriptor>)treeStore.GetValue (iter, 1);
-				if (provider == null) {
-					comboRenderer.Visible = false;
-					return;
-				}
-				var severity = severities[provider];
-				if (!severity.HasValue) {
-					comboRenderer.Visible = false;
-					return;
-				}
-				comboRenderer.Visible = true;
-				comboRenderer.Text = GetDescription (severity.Value);
-				comboRenderer.Icon = GetIcon (severity.Value);
-			});
+			col.SetCellDataFunc (comboRenderer, ComboDataFunc);
 			treeviewInspections.HeadersVisible = false;
 			treeviewInspections.Model = treeStore;
 			GetAllSeverities ();
 			FillInspectors (null);
+		}
+
+		// TODO: Make static.
+		void TitleColDataFunc (TreeViewColumn treeColumn, CellRenderer cell, TreeModel model, TreeIter iter)
+		{
+			var provider = (Tuple<CodeDiagnosticDescriptor, DiagnosticDescriptor>)model.GetValue (iter, 1);
+			if (provider == null) {
+				cell.Visible = false;
+				return;
+			}
+			cell.Visible = true;
+			((CellRendererToggle)cell).Active = enableState [provider];
+		}
+
+		// TODO: Make static.
+		void ComboDataFunc (TreeViewColumn treeColumn, CellRenderer cell, TreeModel model, TreeIter iter)
+		{
+			var provider = (Tuple<CodeDiagnosticDescriptor, DiagnosticDescriptor>)treeStore.GetValue (iter, 1);
+			if (provider == null) {
+				cell.Visible = false;
+				return;
+			}
+			var severity = severities [provider];
+			if (!severity.HasValue) {
+				cell.Visible = false;
+				return;
+			}
+			cell.Visible = true;
+
+			var combo = (CustomCellRenderer)cell;
+			combo.Text = GetDescription (severity.Value);
+			combo.Icon = GetIcon (severity.Value);
 		}
 
 		void ApplyFilter (object sender, EventArgs e)

main/src/addins/MonoDevelop.RegexToolkit/MonoDevelop.RegexToolkit/ElementHelpWidget.cs
@@ -91,7 +91,7 @@ namespace MonoDevelop.RegexToolkit
 			Show ();
 		}
 		
-		void ElementDescriptionFunc (TreeViewColumn column, CellRenderer cell, TreeModel model, TreeIter iter)
+		static void ElementDescriptionFunc (TreeViewColumn column, CellRenderer cell, TreeModel model, TreeIter iter)
 		{
 			string str = (string)model.GetValue (iter, 2);
 			if (string.IsNullOrEmpty (str)) {

main/src/addins/MonoDevelop.SourceEditor2/MonoDevelop.SourceEditor.OptionPanels/HighlightingPanel.cs
@@ -41,7 +41,7 @@ namespace MonoDevelop.SourceEditor.OptionPanels
 	{
 		string schemeName;
 		ListStore styleStore = new ListStore (typeof (string), typeof (Mono.TextEditor.Highlighting.ColorScheme), typeof(bool));
-		Lazy<Gdk.Pixbuf> errorPixbuf = new Lazy<Gdk.Pixbuf> (() => ImageService.GetIcon (Stock.DialogError, IconSize.Menu).ToPixbuf ());
+		static Lazy<Gdk.Pixbuf> errorPixbuf = new Lazy<Gdk.Pixbuf> (() => ImageService.GetIcon (Stock.DialogError, IconSize.Menu).ToPixbuf ());
 
 		public HighlightingPanel ()
 		{
@@ -49,11 +49,7 @@ namespace MonoDevelop.SourceEditor.OptionPanels
 			var col = new TreeViewColumn ();
 			var crpixbuf = new CellRendererPixbuf ();
 			col.PackStart (crpixbuf, false);
-			col.SetCellDataFunc (crpixbuf, (TreeViewColumn tree_column, CellRenderer cell, TreeModel tree_model, TreeIter iter) => {
-				var isError = (bool)styleStore.GetValue (iter, 2);
-				crpixbuf.Visible = isError;
-				crpixbuf.Pixbuf = isError ? errorPixbuf.Value : null;
-			});
+			col.SetCellDataFunc (crpixbuf, ImageDataFunc);
 			var crtext = new CellRendererText ();
 			col.PackEnd (crtext, true);
 			col.SetAttributes (crtext, "markup", 0);
@@ -61,6 +57,15 @@ namespace MonoDevelop.SourceEditor.OptionPanels
 			styleTreeview.Model = styleStore;
 			schemeName = DefaultSourceEditorOptions.Instance.ColorScheme;
 			MonoDevelop.Ide.Gui.Styles.Changed += HandleThemeChanged;
+		}
+
+		static void ImageDataFunc (TreeViewColumn tree_column, CellRenderer cell, TreeModel tree_model, TreeIter iter)
+		{
+
+			var isError = (bool)tree_model.GetValue (iter, 2);
+			var crpixbuf = (CellRendererPixbuf)cell;
+			crpixbuf.Visible = isError;
+			crpixbuf.Pixbuf = isError ? errorPixbuf.Value : null;
 		}
 
 		void HandleThemeChanged (object sender, EventArgs e)

main/src/core/MonoDevelop.Ide/MonoDevelop.Ide.CodeTemplates/CodeTemplatePanel.cs
@@ -149,31 +149,33 @@ namespace MonoDevelop.Ide.CodeTemplates
 			CodeTemplateService.Templates = templates;
 		}
 		
-		void RenderIcon (Gtk.TreeViewColumn column, Gtk.CellRenderer cell, Gtk.TreeModel model, Gtk.TreeIter iter)
+		static void RenderIcon (Gtk.TreeViewColumn column, Gtk.CellRenderer cell, Gtk.TreeModel model, Gtk.TreeIter iter)
 		{
-			CodeTemplate template = (CodeTemplate)templateStore.GetValue (iter, 0);
-			
+			CodeTemplate template = (CodeTemplate)model.GetValue (iter, 0);
+
+			var cri = (CellRendererImage)cell;
 			if (template == null) {
-				pixbufCellRenderer.Image = ImageService.GetIcon (treeviewCodeTemplates.GetRowExpanded (templateStore.GetPath (iter)) ? MonoDevelop.Ide.Gui.Stock.OpenFolder : MonoDevelop.Ide.Gui.Stock.ClosedFolder, IconSize.Menu);
+				cri.Image = ImageService.GetIcon (((TreeView)column.TreeView).GetRowExpanded (model.GetPath (iter)) ? MonoDevelop.Ide.Gui.Stock.OpenFolder : MonoDevelop.Ide.Gui.Stock.ClosedFolder, IconSize.Menu);
 			} else {
-				pixbufCellRenderer.Image = ImageService.GetIcon (template.Icon, IconSize.Menu);
+				cri.Image = ImageService.GetIcon (template.Icon, IconSize.Menu);
 			}
 				
 		}
 		
 		void RenderTemplateName (Gtk.TreeViewColumn column, Gtk.CellRenderer cell, Gtk.TreeModel model, Gtk.TreeIter iter)
 		{
-			CodeTemplate template = (CodeTemplate)templateStore.GetValue (iter, 0);
+			CodeTemplate template = (CodeTemplate)model.GetValue (iter, 0);
+			var crt = (CellRendererText)cell;
 			if (template == null) {
-				templateCellRenderer.Markup = (string)templateStore.GetValue (iter, 2);
+				crt.Markup = (string)model.GetValue (iter, 2);
 				return;
 			}
 			
-			if (treeviewCodeTemplates.Selection.IterIsSelected (iter)) {
-				templateCellRenderer.Markup = GLib.Markup.EscapeText (template.Shortcut) + " (" + 
+			if (((TreeView)column.TreeView).Selection.IterIsSelected (iter)) {
+				crt.Markup = GLib.Markup.EscapeText (template.Shortcut) + " (" + 
 					GLib.Markup.EscapeText (GettextCatalog.GetString (template.Description)) + ")";
 			} else {
-				templateCellRenderer.Markup =  GLib.Markup.EscapeText (template.Shortcut) + " <span foreground=\"" + 
+				crt.Markup =  GLib.Markup.EscapeText (template.Shortcut) + " <span foreground=\"" + 
 					GetColorString (Style.Text (StateType.Insensitive)) + "\">(" 
 					+ GLib.Markup.EscapeText (GettextCatalog.GetString (template.Description)) + ")</span>";
 			}

main/src/core/MonoDevelop.Ide/MonoDevelop.Ide.Gui.Components/ExtensibleTreeView.cs
@@ -299,7 +299,7 @@ namespace MonoDevelop.Ide.Gui.Components
 		}
 #endif
 
-		void SetIconCellData (Gtk.TreeViewColumn col, Gtk.CellRenderer renderer, Gtk.TreeModel model, Gtk.TreeIter it)
+		static void SetIconCellData (Gtk.TreeViewColumn col, Gtk.CellRenderer renderer, Gtk.TreeModel model, Gtk.TreeIter it)
 		{
 			if (model == null)
 				return;
@@ -317,7 +317,7 @@ namespace MonoDevelop.Ide.Gui.Components
 			cell.OverlayTopRight = info.OverlayTopRight;
 		}
 
-		void SetTextCellData (Gtk.TreeViewColumn col, Gtk.CellRenderer renderer, Gtk.TreeModel model, Gtk.TreeIter it)
+		static void SetTextCellData (Gtk.TreeViewColumn col, Gtk.CellRenderer renderer, Gtk.TreeModel model, Gtk.TreeIter it)
 		{
 			if (model == null)
 				return;

commit 74a0b7dcb9c5a3253289466c3441730d98a9cb56

[Ide] Fixup issue introduced in 7410601578fda27f6bba68e40ea9fec6e80415aa

main/src/addins/CSharpBinding/MonoDevelop.CSharp.Diagnostics/InconsistentNaming/NameConventionPanelWidget.cs
@@ -61,7 +61,7 @@ namespace MonoDevelop.CSharp.Diagnostics.InconsistentNaming
 			var ct2 = new CellRendererText ();
 			var col2 = treeviewConventions.AppendColumn (GettextCatalog.GetString ("Example"), ct2);
 			col2.Expand = true;
-			col2.SetCellDataFunc (ct2, NameConventionRuleNameDataFunc);
+			col2.SetCellDataFunc (ct2, NameConventionRulePreviewDataFunc);
 			
 			treeviewConventions.Model = treeStore;
 			treeviewConventions.Selection.Changed += HandleSelectionChanged;
@@ -77,6 +77,12 @@ namespace MonoDevelop.CSharp.Diagnostics.InconsistentNaming
 		{
 			var rule = (NameConventionRule)model.GetValue (iter, 0);
 			((CellRendererText)cell).Text = rule.Name;
+		}
+
+		static void NameConventionRulePreviewDataFunc (TreeViewColumn column, CellRenderer cell, TreeModel model, TreeIter iter)
+		{
+			var rule = (NameConventionRule)model.GetValue (iter, 0);
+			((CellRendererText)cell).Text = rule.GetPreview ();
 		}
 
 		void HandleSelectionChanged (object sender, EventArgs e)