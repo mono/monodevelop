d08e68e6384736450bb5b0292460cecf88abfc99
nosami <jasonimison@gmail.com>
2016-06-07 20:40:22 +0100

    some fsi commands should be available for all langs

--- MonoDevelop.FSharpBinding/FSharpInteractivePad.fs
+++ MonoDevelop.FSharpBinding/FSharpInteractivePad.fs
@@ -482,17 +482,21 @@ type FSharpFsiEditorCompletion() =
             result
         | _ -> base.KeyPress (descriptor)
 
+  
   type InteractiveCommand(command) =
     inherit CommandHandler()
-
-    override x.Update(info:CommandInfo) =
-        info.Enabled <- true
-        info.Visible <- FileService.isInsideFSharpFile()
 
     override x.Run() =
         FSharpInteractivePad.Fsi
         |> Option.iter (fun fsi -> command fsi
                                    FSharpInteractivePad.BringToFront(false))
+
+  type FSharpFileInteractiveCommand(command) =
+    inherit InteractiveCommand(command)
+
+    override x.Update(info:CommandInfo) =
+        info.Enabled <- true
+        info.Visible <- FileService.isInsideFSharpFile()
 
   type ShowFSharpInteractive() =
       inherit InteractiveCommand(ignore)
@@ -510,16 +514,16 @@ type FSharpFsiEditorCompletion() =
       inherit InteractiveCommand(fun fsi -> fsi.Paste())
 
   type SendSelection() =
-      inherit InteractiveCommand(fun fsi -> fsi.SendSelection())
+      inherit FSharpFileInteractiveCommand(fun fsi -> fsi.SendSelection())
 
   type SendLine() =
-      inherit InteractiveCommand(fun fsi -> fsi.SendLine())
+      inherit FSharpFileInteractiveCommand(fun fsi -> fsi.SendLine())
 
   type SendFile() =
-      inherit InteractiveCommand(fun fsi -> fsi.SendFile())
+      inherit FSharpFileInteractiveCommand(fun fsi -> fsi.SendFile())
 
   type SendReferences() =
-      inherit InteractiveCommand(fun fsi -> fsi.LoadReferences())
+      inherit FSharpFileInteractiveCommand(fun fsi -> fsi.LoadReferences())
 
   type RestartFsi() =
       inherit InteractiveCommand(fun fsi -> fsi.RestartFsi())
