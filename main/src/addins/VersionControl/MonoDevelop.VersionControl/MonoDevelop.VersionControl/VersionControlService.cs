mono/monodevelop

commit f1b3d9b2b96fd68ffc9f36bc48ec01ce825c7090

[Icons] [Ide] New pad icons: * md-pad-download (now used instead of old md-status-download which is meant to be used only in toolbar) * md-pad-device-deployment (previously md-status-download, a device icon makes more sense) * md-pad-upload

main/src/addins/VersionControl/MonoDevelop.VersionControl/MonoDevelop.VersionControl/VersionControlService.cs
@@ -601,8 +601,8 @@ namespace MonoDevelop.VersionControl
 		{
 			IconId icon;
 			switch (op) {
-			case VersionControlOperationType.Pull: icon = Stock.StatusDownload; break;
-			case VersionControlOperationType.Push: icon = Stock.StatusUpload; break;
+			case VersionControlOperationType.Pull: icon = Stock.PadDownload; break;
+			case VersionControlOperationType.Push: icon = Stock.PadUpload; break;
 			default: icon = "md-version-control"; break;
 			}
 

main/src/addins/MonoDevelop.WebReferences/MonoDevelop.WebReferences.Commands/WebReferenceCommandHandler.cs
@@ -87,7 +87,7 @@ namespace MonoDevelop.WebReferences.Commands
 				return;
 			try {
 				updatingReferences = true;
-				using (var monitor = IdeApp.Workbench.ProgressMonitors.GetStatusProgressMonitor (GettextCatalog.GetPluralString ("Updating web reference", "Updating web references", items.Count), MonoDevelop.Ide.Gui.Stock.StatusDownload, true)) {
+				using (var monitor = IdeApp.Workbench.ProgressMonitors.GetStatusProgressMonitor (GettextCatalog.GetPluralString ("Updating web reference", "Updating web references", items.Count), MonoDevelop.Ide.Gui.Stock.PadDownload, true)) {
 					monitor.BeginTask (GettextCatalog.GetPluralString ("Updating web reference", "Updating web references", items.Count), items.Count);
 
 					for (int i = 0; i < items.Count; i++) {

main/src/core/MonoDevelop.Ide/ExtensionModel/StockIcons.addin.xml
@@ -257,6 +257,9 @@
 	<StockIcon stockid="md-solution-folder-open" resource="folder-solution-16.png" size="Menu" />
 	<StockIcon stockid="md-component-folder-open" resource="folder-component-16.png" size="Menu" />
 	<StockIcon stockid="md-web-search" resource="web-search-16.png" size="Menu" />
+	<StockIcon stockid="md-pad-download" resource="pad-download-16.png" size="Menu" />
+	<StockIcon stockid="md-pad-upload" resource="pad-upload-16.png" size="Menu" />
+	<StockIcon stockid="md-pad-device-deployment" resource="pad-device-deployment-16.png" size="Menu" />
 
 	<!-- All sizes icons -->
 	<StockIcon stockid="gtk-dialog-error" resource="error-16.png" />

main/src/core/MonoDevelop.Ide/MonoDevelop.Ide.Gui/StockIcons.cs
@@ -164,5 +164,8 @@ namespace MonoDevelop.Ide.Gui
 		public static readonly IconId Execute = Gtk.Stock.Execute;
 		public static readonly IconId SearchboxSearch = "md-searchbox-search";
 		public static readonly IconId Updates = "md-updates";
+		public static readonly IconId PadDownload = "md-pad-download";
+		public static readonly IconId PadUpload = "md-pad-upload";
+		public static readonly IconId PadDeviceDeployment = "md-pad-device-deployment";
 	}
 }

main/src/core/MonoDevelop.Ide/MonoDevelop.Ide.csproj
@@ -3622,6 +3622,114 @@
     <EmbeddedResource Include="icons\pad-error-list-warnings-16~dark~sel%402x.png">
       <LogicalName>pad-error-list-warnings-16~dark~sel@2x.png</LogicalName>
     </EmbeddedResource>
+    <EmbeddedResource Include="icons\pad-device-deployment-16.png">
+      <LogicalName>pad-device-deployment-16.png</LogicalName>
+    </EmbeddedResource>
+    <EmbeddedResource Include="icons\pad-device-deployment-16%402x.png">
+      <LogicalName>pad-device-deployment-16@2x.png</LogicalName>
+    </EmbeddedResource>
+    <EmbeddedResource Include="icons\pad-device-deployment-16~dark.png">
+      <LogicalName>pad-device-deployment-16~dark.png</LogicalName>
+    </EmbeddedResource>
+    <EmbeddedResource Include="icons\pad-device-deployment-16~dark%402x.png">
+      <LogicalName>pad-device-deployment-16~dark@2x.png</LogicalName>
+    </EmbeddedResource>
+    <EmbeddedResource Include="icons\pad-device-deployment-16~sel.png">
+      <LogicalName>pad-device-deployment-16~sel.png</LogicalName>
+    </EmbeddedResource>
+    <EmbeddedResource Include="icons\pad-device-deployment-16~sel%402x.png">
+      <LogicalName>pad-device-deployment-16~sel@2x.png</LogicalName>
+    </EmbeddedResource>
+    <EmbeddedResource Include="icons\pad-device-deployment-16~dark~sel.png">
+      <LogicalName>pad-device-deployment-16~dark~sel.png</LogicalName>
+    </EmbeddedResource>
+    <EmbeddedResource Include="icons\pad-device-deployment-16~dark~sel%402x.png">
+      <LogicalName>pad-device-deployment-16~dark~sel@2x.png</LogicalName>
+    </EmbeddedResource>
+    <EmbeddedResource Include="icons\pad-device-deployment-16~error.png">
+      <LogicalName>pad-device-deployment-16~error.png</LogicalName>
+    </EmbeddedResource>
+    <EmbeddedResource Include="icons\pad-device-deployment-16~error%402x.png">
+      <LogicalName>pad-device-deployment-16~error@2x.png</LogicalName>
+    </EmbeddedResource>
+    <EmbeddedResource Include="icons\pad-device-deployment-16~dark~error.png">
+      <LogicalName>pad-device-deployment-16~dark~error.png</LogicalName>
+    </EmbeddedResource>
+    <EmbeddedResource Include="icons\pad-device-deployment-16~dark~error%402x.png">
+      <LogicalName>pad-device-deployment-16~dark~error@2x.png</LogicalName>
+    </EmbeddedResource>
+    <EmbeddedResource Include="icons\pad-download-16.png">
+      <LogicalName>pad-download-16.png</LogicalName>
+    </EmbeddedResource>
+    <EmbeddedResource Include="icons\pad-download-16%402x.png">
+      <LogicalName>pad-download-16@2x.png</LogicalName>
+    </EmbeddedResource>
+    <EmbeddedResource Include="icons\pad-download-16~dark.png">
+      <LogicalName>pad-download-16~dark.png</LogicalName>
+    </EmbeddedResource>
+    <EmbeddedResource Include="icons\pad-download-16~dark%402x.png">
+      <LogicalName>pad-download-16~dark@2x.png</LogicalName>
+    </EmbeddedResource>
+    <EmbeddedResource Include="icons\pad-download-16~sel.png">
+      <LogicalName>pad-download-16~sel.png</LogicalName>
+    </EmbeddedResource>
+    <EmbeddedResource Include="icons\pad-download-16~sel%402x.png">
+      <LogicalName>pad-download-16~sel@2x.png</LogicalName>
+    </EmbeddedResource>
+    <EmbeddedResource Include="icons\pad-download-16~dark~sel.png">
+      <LogicalName>pad-download-16~dark~sel.png</LogicalName>
+    </EmbeddedResource>
+    <EmbeddedResource Include="icons\pad-download-16~dark~sel%402x.png">
+      <LogicalName>pad-download-16~dark~sel@2x.png</LogicalName>
+    </EmbeddedResource>
+    <EmbeddedResource Include="icons\pad-download-16~error.png">
+      <LogicalName>pad-download-16~error.png</LogicalName>
+    </EmbeddedResource>
+    <EmbeddedResource Include="icons\pad-download-16~error%402x.png">
+      <LogicalName>pad-download-16~error@2x.png</LogicalName>
+    </EmbeddedResource>
+    <EmbeddedResource Include="icons\pad-download-16~dark~error.png">
+      <LogicalName>pad-download-16~dark~error.png</LogicalName>
+    </EmbeddedResource>
+    <EmbeddedResource Include="icons\pad-download-16~dark~error%402x.png">
+      <LogicalName>pad-download-16~dark~error@2x.png</LogicalName>
+    </EmbeddedResource>
+    <EmbeddedResource Include="icons\pad-upload-16.png">
+      <LogicalName>pad-upload-16.png</LogicalName>
+    </EmbeddedResource>
+    <EmbeddedResource Include="icons\pad-upload-16%402x.png">
+      <LogicalName>pad-upload-16@2x.png</LogicalName>
+    </EmbeddedResource>
+    <EmbeddedResource Include="icons\pad-upload-16~dark.png">
+      <LogicalName>pad-upload-16~dark.png</LogicalName>
+    </EmbeddedResource>
+    <EmbeddedResource Include="icons\pad-upload-16~dark%402x.png">
+      <LogicalName>pad-upload-16~dark@2x.png</LogicalName>
+    </EmbeddedResource>
+    <EmbeddedResource Include="icons\pad-upload-16~sel.png">
+      <LogicalName>pad-upload-16~sel.png</LogicalName>
+    </EmbeddedResource>
+    <EmbeddedResource Include="icons\pad-upload-16~sel%402x.png">
+      <LogicalName>pad-upload-16~sel@2x.png</LogicalName>
+    </EmbeddedResource>
+    <EmbeddedResource Include="icons\pad-upload-16~dark~sel.png">
+      <LogicalName>pad-upload-16~dark~sel.png</LogicalName>
+    </EmbeddedResource>
+    <EmbeddedResource Include="icons\pad-upload-16~dark~sel%402x.png">
+      <LogicalName>pad-upload-16~dark~sel@2x.png</LogicalName>
+    </EmbeddedResource>
+    <EmbeddedResource Include="icons\pad-upload-16~error.png">
+      <LogicalName>pad-upload-16~error.png</LogicalName>
+    </EmbeddedResource>
+    <EmbeddedResource Include="icons\pad-upload-16~error%402x.png">
+      <LogicalName>pad-upload-16~error@2x.png</LogicalName>
+    </EmbeddedResource>
+    <EmbeddedResource Include="icons\pad-upload-16~dark~error.png">
+      <LogicalName>pad-upload-16~dark~error.png</LogicalName>
+    </EmbeddedResource>
+    <EmbeddedResource Include="icons\pad-upload-16~dark~error%402x.png">
+      <LogicalName>pad-upload-16~dark~error@2x.png</LogicalName>
+    </EmbeddedResource>
     <EmbeddedResource Include="icons\element-variable-16.png">
       <LogicalName>element-variable-16.png</LogicalName>
     </EmbeddedResource>

commit 89a14349734af153657d93837b2512d09931beeb

[VCS] Toolbar VCS icon now animates.
Bug 41662 - Arrow is not showing progress in Xamarin Studio status bar while publishing the project.
Fixed regression from f1b3d9b2b96fd68ffc9f36bc48ec01ce825c7090.

main/src/addins/VersionControl/MonoDevelop.VersionControl/MonoDevelop.VersionControl/VersionControlService.cs
@@ -605,19 +605,31 @@ namespace MonoDevelop.VersionControl
 		
 		public static ProgressMonitor GetProgressMonitor (string operation, VersionControlOperationType op)
 		{
-			IconId icon;
+			IconId padIcon, statusIcon;
 			bool cancellable;
 			switch (op) {
-			case VersionControlOperationType.Pull: icon = Stock.PadDownload; cancellable = true; break;
-			case VersionControlOperationType.Push: icon = Stock.PadUpload; cancellable = true; break;
-			default: icon = "md-version-control"; cancellable = false; break;
+			case VersionControlOperationType.Pull:
+				padIcon = Stock.PadDownload;
+				statusIcon = Stock.StatusDownload;
+				cancellable = true;
+				break;
+			case VersionControlOperationType.Push:
+				padIcon = Stock.PadUpload;
+				statusIcon = Stock.StatusUpload;
+				cancellable = true;
+				break;
+			default:
+				padIcon = "md-version-control";
+				statusIcon = "md-version-control";
+				cancellable = false;
+				break;
 			}
 
 			ProgressMonitor monitor = IdeApp.Workbench.ProgressMonitors.GetOutputProgressMonitor ("MonoDevelop.VersionControlOutput", GettextCatalog.GetString ("Version Control"), "md-version-control", false, true);
 			Pad outPad = IdeApp.Workbench.ProgressMonitors.GetPadForMonitor (monitor);
 			
 			AggregatedProgressMonitor mon = new AggregatedProgressMonitor (monitor);
-			mon.AddFollowerMonitor (IdeApp.Workbench.ProgressMonitors.GetStatusProgressMonitor (operation, icon, true, true, false, outPad, cancellable));
+			mon.AddFollowerMonitor (IdeApp.Workbench.ProgressMonitors.GetStatusProgressMonitor (operation, statusIcon, true, true, false, outPad, cancellable));
 			return mon;
 		}
 		