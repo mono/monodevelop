mono/monodevelop

commit ed8070cd874497cc1303b82e527238c013a0e87b

[Nuget] Help gettext crawler a bit here.

main/src/addins/MonoDevelop.PackageManagement/MonoDevelop.PackageManagement.Gui/AddPackagesDialog.cs
@@ -660,14 +660,9 @@ namespace MonoDevelop.PackageManagement
 
 		void UpdateAddPackagesButton ()
 		{
-			if (PackagesCheckedCount > 1) {
-				addPackagesButton.Label = Catalog.GetString ("Add Packages");
-			} else {
-				if (OlderPackageInstalledThanPackageSelected ()) {
-					addPackagesButton.Label = Catalog.GetString ("Update Package");
-				} else {
-					addPackagesButton.Label = Catalog.GetString ("Add Package");
-				}
+			string label = Catalog.GetPluralString ("Add Package", "Add Packages", PackagesCheckedCount);
+			if (PackagesCheckedCount <= 1 && OlderPackageInstalledThanPackageSelected ()) {
+				label = Catalog.GetString ("Update Package");
 			}
 			addPackagesButton.Sensitive = IsAddPackagesButtonEnabled ();
 		}

commit 81446f1843f35719f40d766efced30680096a636

[NuGet] Fix incorrect button label in Add Packages dialog.
Fixed bug #42042 - When updating a Nuget package via
"Packages Dialog", the button is not changing to "Update Package"
https://bugzilla.xamarin.com/show_bug.cgi?id=42042

The text for the button was being created but the button's label was
not being updated. Also the plural string was being shown when no
packages were checked. This was broken in commit:

ed8070cd874497cc1303b82e527238c013a0e87b

main/src/addins/MonoDevelop.PackageManagement/MonoDevelop.PackageManagement.Gui/AddPackagesDialog.cs
@@ -650,11 +650,20 @@ namespace MonoDevelop.PackageManagement
 
 		void UpdateAddPackagesButton ()
 		{
-			string label = Catalog.GetPluralString ("Add Package", "Add Packages", PackagesCheckedCount);
+			string label = Catalog.GetPluralString ("Add Package", "Add Packages", GetPackagesCountForAddPackagesButtonLabel ());
 			if (PackagesCheckedCount <= 1 && OlderPackageInstalledThanPackageSelected ()) {
 				label = Catalog.GetString ("Update Package");
 			}
+			addPackagesButton.Label = label;
 			addPackagesButton.Sensitive = IsAddPackagesButtonEnabled ();
+		}
+
+		int GetPackagesCountForAddPackagesButtonLabel ()
+		{
+			if (PackagesCheckedCount > 1)
+				return PackagesCheckedCount;
+
+			return 1;
 		}
 
 		void UpdatePackageListViewSelectionColor ()