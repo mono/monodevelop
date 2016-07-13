4781fb790be99b7d100b8da3a016df8facc5da85
Mike Krüger <mkrueger@xamarin.com>
2016-07-13 14:22:14 +0200

    [Ide] Compile sublime3 regexes to the .NET format.
    The ruby regex engine is not quite the same than the .NET one. This is
    just the first step for the translation (tested with various sublime3
    styles)

--- main/src/core/MonoDevelop.Ide/MonoDevelop.Ide.Editor.Highlighting/Sublime3Format.cs
+++ main/src/core/MonoDevelop.Ide/MonoDevelop.Ide.Editor.Highlighting/Sublime3Format.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.IO;
 using YamlDotNet.RepresentationModel;
 using System.Linq;
+using System.Text;
+using MonoDevelop.Core;
 
 namespace MonoDevelop.Ide.Editor.Highlighting
 {
@@ -13,12 +15,25 @@ namespace MonoDevelop.Ide.Editor.Highlighting
 			input.ReadLine ();
 			input.ReadLine ();
 			string name = null, scope = null, firstLineMatch = null;
+			var variables = new Dictionary<string, string> ();
+			
 			var extensions = new List<string> ();
 			var contexts = new List<SyntaxContext> ();
 
 			var yaml = new YamlStream ();
 			yaml.Load (input);
 			var mapping = (YamlMappingNode)yaml.Documents [0].RootNode;
+
+			foreach (var entry in mapping.Children) {
+				switch (((YamlScalarNode)entry.Key).Value) {
+				case "variables":
+					foreach (var captureEntry in ((YamlMappingNode)entry.Value).Children) {
+						variables [((YamlScalarNode)captureEntry.Key).Value] = ((YamlScalarNode)captureEntry.Value).Value;
+					}
+					break;
+				}
+			}
+
 			foreach (var entry in mapping.Children) {
 				switch (((YamlScalarNode)entry.Key).Value) {
 				case "name":
@@ -33,11 +48,11 @@ namespace MonoDevelop.Ide.Editor.Highlighting
 					scope = ((YamlScalarNode)entry.Value).Value;
 					break;
 				case "first_line_match":
-					firstLineMatch = ((YamlScalarNode)entry.Value).Value;
+					firstLineMatch = CompileRegex (((YamlScalarNode)entry.Value).Value);
 					break;
 				case "contexts":
 					foreach (var contextMapping in ((YamlMappingNode)entry.Value).Children) {
-						contexts.Add (ReadContext (contextMapping));
+						contexts.Add (ReadContext (contextMapping, variables));
 					}
 					break;
 				}
@@ -46,24 +61,25 @@ namespace MonoDevelop.Ide.Editor.Highlighting
 		}
 
 
-		internal static SyntaxContext ReadContext (KeyValuePair<YamlNode, YamlNode> mapping)
+		internal static SyntaxContext ReadContext (KeyValuePair<YamlNode, YamlNode> mapping, Dictionary<string, string> variables)
 		{
 			var result = new SyntaxContext (((YamlScalarNode)mapping.Key).Value);
-			result.ParseMapping (mapping.Value as YamlSequenceNode);
+			result.ParseMapping (mapping.Value as YamlSequenceNode, variables);
 			return result;
 		}
 
-		internal static SyntaxMatch ReadMatch (YamlMappingNode mapping)
+		internal static SyntaxMatch ReadMatch (YamlMappingNode mapping, Dictionary<string, string> variables)
 		{
 			string match = null, scope = null;
-			var captures = new List<Tuple<int, string>> ();
+			var captures  = new List<Tuple<int, string>> ();
+
 			ContextReference push = null, set = null;
 			bool pop = false;
 
 			foreach (var entry in mapping.Children) {
 				switch (((YamlScalarNode)entry.Key).Value) {
 				case "match":
-					match = ((YamlScalarNode)entry.Value).Value;
+					match = CompileRegex (ReplaceVariables (((YamlScalarNode)entry.Value).Value, variables));
 					break;
 				case "scope":
 					scope = ((YamlScalarNode)entry.Value).Value;
@@ -79,14 +95,14 @@ namespace MonoDevelop.Ide.Editor.Highlighting
 					}
 					break;
 				case "push":
-					push = ReadContextReference (entry.Value);
+					push = ReadContextReference (entry.Value, variables);
 					break;
 				case "pop":
 					// according to the spec the only accepted value
 					pop = true;
 					break;
 				case "set":
-					set = ReadContextReference (entry.Value);
+					set = ReadContextReference (entry.Value, variables);
 					break;
 				case "syntax":
 					// ?
@@ -96,23 +112,157 @@ namespace MonoDevelop.Ide.Editor.Highlighting
 			return new SyntaxMatch (match, scope, captures, push, pop, set);
 		}
 
-		internal static ContextReference ReadContextReference (YamlNode value)
+		internal static ContextReference ReadContextReference (YamlNode value, Dictionary<string, string> variables)
 		{
 			var seq = value as YamlSequenceNode;
 			if (seq != null)
-				return ReadAnonymousMatchContextReference (seq);
+				return ReadAnonymousMatchContextReference (seq, variables);
 			return null;
 		}
 
-		internal static ContextReference ReadAnonymousMatchContextReference (YamlSequenceNode seq)
+		internal static ContextReference ReadAnonymousMatchContextReference (YamlSequenceNode seq, Dictionary<string, string> variables)
 		{
-			var ctx = new SyntaxContext (null);
+			var ctx = new SyntaxContext ("__Anonymous__");
 
 			foreach (var c in seq.Children.OfType<YamlMappingNode> ()) {
-				ctx.ParseMapping (c);
+				ctx.ParseMapping (c, variables);
 			}
 
 			return new AnonymousMatchContextReference (ctx);
+		}
+
+		static string ReplaceVariables (string str, Dictionary<string, string> variables)
+		{
+			if (string.IsNullOrEmpty (str))
+				return str;
+
+			var result = new StringBuilder ();
+			var wordBuilder = new StringBuilder ();
+			bool recordWord = false;
+			for (int i = 0; i < str.Length - 1; i++) {
+				var ch = str [i];
+				var next = str [i + 1];
+				if (ch == '{' && next == '{') {
+					i++;
+					recordWord = true;
+					continue;
+				}
+				if (ch == '}' && next == '}' && recordWord) {
+					i++;
+					recordWord = false;
+					string replacement;
+					if (variables.TryGetValue (wordBuilder.ToString (), out replacement)) {
+						result.Append (replacement);
+					} else { 
+						LoggingService.LogWarning ("Sublime3Format: Can't find variable " + wordBuilder.ToString ());
+					}
+					wordBuilder.Length = 0;
+					if (i >= str.Length - 1)
+						return result.ToString ();
+					continue;
+				}
+
+				if (recordWord) {
+					wordBuilder.Append (ch);
+				} else {
+					result.Append (ch);
+				}
+			}
+			result.Append (str [str.Length - 1]);
+
+			return result.ToString ();
+		}
+
+
+		// translates ruby regex -> .NET regexes
+		static string CompileRegex (string regex)
+		{
+			if (string.IsNullOrEmpty (regex))
+				return regex;
+			var result = new StringBuilder ();
+			var wordBuilder = new StringBuilder ();
+			bool recordWord = false, inCharacterClass = false;
+
+			for (int i = 0; i < regex.Length - 1; i++) {
+				var ch = regex [i];
+				var next = regex [i + 1];
+				switch (ch) {
+				case '\\':
+					if (next == 'h') {
+						result.Append ("[0-9a-fA-F]");
+						i++;
+						continue;
+					}
+					if (next == 'H') {
+						result.Append ("[^0-9a-fA-F]");
+						i++;
+						continue;
+					}
+					break;
+				case '[':
+					if (next == ':') {
+						recordWord = true;
+						i++;
+						continue;
+					}
+					inCharacterClass = true;
+					break;
+				case ']':
+					inCharacterClass = false;
+					break;
+
+				case ':':
+					if (next == ']' && recordWord) {
+						result.Append (ConvertUnicodeCategory (wordBuilder.ToString (), inCharacterClass));
+						i++;
+						if (i >= regex.Length - 1)
+							return result.ToString ();
+						wordBuilder.Length = 0;
+						recordWord = false;
+						continue;
+					}
+					break;
+				}
+				if (recordWord) {
+					wordBuilder.Append (ch);
+				} else {
+					result.Append (ch);
+				}
+			}
+			result.Append (regex [regex.Length - 1]);
+			return result.ToString ();
+		}
+
+		static string ConvertUnicodeCategory (string category, bool inCharacterClass)
+		{
+			switch (category) {
+			case "alnum": // Alphabetic and numeric character
+				return inCharacterClass ? "\\w\\d": "[\\w\\d]";
+			case "alpha": // Alphabetic character
+				return "\\w";
+			case "blank": // Space or tab
+				return inCharacterClass ? " \t" : "[ \t]";
+			case "cntrl": // Control character
+				return "\\W"; // TODO
+			case "digit": // Digit
+				return "\\d";
+			case "graph": // Non - blank character (excludes spaces, control characters, and similar)
+				return "\\S";
+			case "lower": // Lowercase alphabetical character
+				return inCharacterClass ? "a-z" : "[a-z]";
+			case "print": // Like [:graph:], but includes the space character
+				return inCharacterClass ? "\\S\\ " : "[\\S\\ ]";
+			case "punct": // Punctuation character
+				return "\\W"; // TODO
+			case "space": // Whitespace character ([:blank:], newline, carriage return, etc.)
+				return "\\s";
+			case "upper": // Uppercase alphabetical
+				return inCharacterClass ? "A-Z" : "[A-Z]";
+			case "xdigit": // Digit allowed in a hexadecimal number (i.e., 0 - 9a - fA - F)
+				return inCharacterClass ? "0-9a-fA-F" : "[0-9a-fA-F]";
+			}
+			LoggingService.LogWarning ("unknown unicode category : " + category);
+			return "";
 		}
 	}
 }
