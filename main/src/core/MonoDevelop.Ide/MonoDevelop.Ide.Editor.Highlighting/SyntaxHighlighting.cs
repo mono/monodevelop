7a47d5cab476eae633dce772ce04fba04038bf89
Mike Krüger <mkrueger@xamarin.com>
2016-07-20 15:56:38 -0400

    [Ide] Remove code duplication in syntax highlighting.
    Still not the final algorithm but it needs to work first.

--- main/src/core/MonoDevelop.Ide/MonoDevelop.Ide.Editor.Highlighting/SyntaxHighlighting.cs
+++ main/src/core/MonoDevelop.Ide/MonoDevelop.Ide.Editor.Highlighting/SyntaxHighlighting.cs
@@ -33,187 +33,153 @@ namespace MonoDevelop.Ide.Editor.Highlighting
 		public IEnumerable<ColoredSegment> GetColoredSegments (IDocumentLine line, int offset, int length)
 		{
 			if (Document == null) {
-				yield return new ColoredSegment (offset, length, ImmutableStack<string>.Empty.Push (""));
-				yield break;
+				return new [] { new ColoredSegment (offset, length, ImmutableStack<string>.Empty.Push ("")) };
 			}
+			var high = new Highlighter (this);
 			var cur = Document.GetLine (1);
-			var contextStack = ImmutableStack<SyntaxContext>.Empty;
-			contextStack = contextStack.Push (GetContext ("main"));
-			var scopeStack = ImmutableStack<string>.Empty;
-			scopeStack = scopeStack.Push (this.definition.Scope);
-			SyntaxContext currentContext;
-			Match match = null;
-			SyntaxMatch curMatch = null;
-			bool deep = true;
-
-			var matchStack = ImmutableStack<SyntaxMatch>.Empty;
+			
 			if (cur != null && cur.Offset < line.Offset) {
 				do {
-					int co = cur.Offset;
-					int cl = cur.Length;
-				restart2:
-					currentContext = contextStack.Peek ();
-					match = null;
-					curMatch = null;
-					foreach (var m in currentContext.GetMatches (this)) {
-						var r = m.GetRegex ();
-						if (r == null)
-							continue;
-
-						var possibleMatch = r.Match (this.Document, co, cl);
-						if (possibleMatch.Success) {
-							if (match == null || possibleMatch.Index < match.Index) {
-								match = possibleMatch;
-								curMatch = m;
-							}
-						}
-					}
-					if (match != null) {
-						var matchEndOffset = match.Index + match.Length;
-
-						if (curMatch.Pop || curMatch.Set != null) {
-							if (curMatch.Pop || contextStack.Count () > 1) {
-								contextStack = contextStack.Pop ();
-								scopeStack = scopeStack.Pop ();
-							}
-						}
-
-						if (curMatch.Push != null || curMatch.Set != null) {
-							var nextContexts = (curMatch.Push ?? curMatch.Set).GetContexts (this);
-							if (nextContexts != null) {
-								foreach (var nextContext in nextContexts) {
-									contextStack = contextStack.Push (nextContext);
-									scopeStack = scopeStack.Push (curMatch.Scope ??
-																  nextContext.MetaContentScope ??
-																  nextContext.MetaScope ??
-																  scopeStack.Peek ());
-								}
-								// deep = false;
-								cl -= matchEndOffset - co;
-								co = matchEndOffset;
-
-								goto restart2;
-							}
-						}
-						cl -= matchEndOffset - co;
-						co = matchEndOffset;
-						if (cl > 0) {
-							deep = true;
-							goto restart2;
-						}
-					}
+					high.GetColoredSegments (cur.Offset, cur.Length).Count ();
 					cur = cur.NextLine;
 				} while (cur != null && cur.Offset < line.Offset);
 			}
-			int curSegmentOffset = line.Offset;
-			deep = true;
-		restart:
-			currentContext = contextStack.Peek ();
-			match = null;
-			curMatch = null;
-			foreach (var m in currentContext.GetMatches (this)) {
-				var r = m.GetRegex ();
-				if (r == null)
-					continue;
-
-				var possibleMatch = r.Match (this.Document, offset, length);
-				if (possibleMatch.Success && possibleMatch.Length > 0) {
-					if (match == null || possibleMatch.Index < match.Index) {
-						match = possibleMatch;
-						curMatch = m;
-						// Console.WriteLine (match.Index + "possible match : " + m+ "/" + possibleMatch.Index + "-" + possibleMatch.Length);
-					} else {
-						// Console.WriteLine (match.Index + "skip match : " + m + "/" + possibleMatch.Index + "-" + possibleMatch.Length);
-					}
-				} else {
-					// Console.WriteLine ("fail match : " + m);
-				}
+			if (offset - line.Offset > 0) {
+				high.GetColoredSegments (line.Offset, offset - line.Offset).Count ();
 			}
-
-			if (match != null) {
-				var matchEndOffset = match.Index + match.Length;
-				if (curSegmentOffset < match.Index) {
-					yield return new ColoredSegment (curSegmentOffset, match.Index - curSegmentOffset, scopeStack);
-					curSegmentOffset = match.Index;
-				}
-				if (curMatch.Scope != null) {
-					scopeStack = scopeStack.Push (curMatch.Scope);
-				}
-				if (curMatch.Captures.Count > 0) {
-					var captureList = new List<ColoredSegment> ();
-					foreach (var capture in curMatch.Captures) {
-						var grp = match.Groups [capture.Item1];
-						if (grp.Length == 0)
-							continue;
-						if (curSegmentOffset < grp.Index) {
-							Insert (captureList, new ColoredSegment (curSegmentOffset, grp.Index - curSegmentOffset, scopeStack));
-						}
-						Insert (captureList, new ColoredSegment (grp.Index, grp.Length, scopeStack.Push (capture.Item2)));
-						curSegmentOffset = grp.Index + grp.Length;
-					}
-					foreach (var item in captureList)
-						yield return item;	
-				}
-
-				if (curMatch.Pop || curMatch.Set != null) {
-					if (matchEndOffset - curSegmentOffset > 0) {
-						yield return new ColoredSegment (curSegmentOffset, matchEndOffset - curSegmentOffset, scopeStack);
-					}
-					curSegmentOffset = matchEndOffset;
-					if (curMatch.Pop || contextStack.Count () > 1) {
-						contextStack = contextStack.Pop ();
-						if (!matchStack.IsEmpty) {
-							if (matchStack.Peek ()?.Scope != null)
-								scopeStack = scopeStack.Pop ();
-							matchStack = matchStack.Pop ();
-						}
-						if (currentContext.MetaScope != null)
-							scopeStack = scopeStack.Pop ();
-						if (currentContext.MetaContentScope != null)
-							scopeStack = scopeStack.Pop ();
-					}
-				}
-				if (curMatch.Scope != null && curSegmentOffset < matchEndOffset) {
-					yield return new ColoredSegment (curSegmentOffset, matchEndOffset - curSegmentOffset, scopeStack);
-					curSegmentOffset = matchEndOffset;
-				}
-
-				if (curMatch.Push != null || curMatch.Set != null) {
-					var nextContexts = (curMatch.Push ?? curMatch.Set).GetContexts (this);
-					if (nextContexts != null) {
-						bool first = true;
-						foreach (var nextContext in nextContexts) {
-							if (first) {
-								matchStack = matchStack.Push (curMatch);
-								if (curMatch.Scope != null) {
-									scopeStack = scopeStack.Push (curMatch.Scope);
-								}
-
-							} else {
-								matchStack = matchStack.Push (null);
-							}
-							contextStack = contextStack.Push (nextContext);
-							if (nextContext.MetaScope != null)
-								scopeStack = scopeStack.Push (nextContext.MetaScope);
-							if (nextContext.MetaContentScope != null)
-								scopeStack = scopeStack.Push (nextContext.MetaContentScope);
-						}
-
-					}
-				}
-				if (curMatch.Scope != null) {
-					scopeStack = scopeStack.Pop ();
-				}
-				length -= curSegmentOffset - offset;
-				offset = curSegmentOffset;
-				goto restart;
-			}
-		
-			if (line.EndOffset - curSegmentOffset > 0) {
-				yield return new ColoredSegment (curSegmentOffset, line.EndOffset - curSegmentOffset, scopeStack);
-			}
+			return high.GetColoredSegments (offset, length);
 		}
 
+		class Highlighter
+		{
+			ImmutableStack<SyntaxContext> contextStack = ImmutableStack<SyntaxContext>.Empty;
+			ImmutableStack<SyntaxMatch> matchStack = ImmutableStack<SyntaxMatch>.Empty;
+			ImmutableStack<string> scopeStack = ImmutableStack<string>.Empty;
+			SyntaxHighlighting highlighting;
+
+			public Highlighter (SyntaxHighlighting highlighting)
+			{
+				this.highlighting = highlighting;
+				contextStack = contextStack.Push (highlighting.GetContext ("main"));
+				scopeStack = scopeStack.Push (highlighting.definition.Scope);
+			}
+
+			public IEnumerable<ColoredSegment> GetColoredSegments (int offset, int length)
+			{
+				SyntaxContext currentContext;
+				Match match = null;
+				SyntaxMatch curMatch = null;
+
+				int curSegmentOffset = offset;
+				int endOffset = offset + length;
+			restart:
+				currentContext = contextStack.Peek ();
+				match = null;
+				curMatch = null;
+				foreach (var m in currentContext.GetMatches (highlighting)) {
+					var r = m.GetRegex ();
+					if (r == null)
+						continue;
+
+					var possibleMatch = r.Match (highlighting.Document, offset, length);
+					if (possibleMatch.Success && possibleMatch.Length > 0) {
+						if (match == null || possibleMatch.Index < match.Index) {
+							match = possibleMatch;
+							curMatch = m;
+							// Console.WriteLine (match.Index + "possible match : " + m+ "/" + possibleMatch.Index + "-" + possibleMatch.Length);
+						} else {
+							// Console.WriteLine (match.Index + "skip match : " + m + "/" + possibleMatch.Index + "-" + possibleMatch.Length);
+						}
+					} else {
+						// Console.WriteLine ("fail match : " + m);
+					}
+				}
+
+				if (match != null) {
+					var matchEndOffset = match.Index + match.Length;
+					if (curSegmentOffset < match.Index) {
+						yield return new ColoredSegment (curSegmentOffset, match.Index - curSegmentOffset, scopeStack);
+						curSegmentOffset = match.Index;
+					}
+					if (curMatch.Scope != null) {
+						scopeStack = scopeStack.Push (curMatch.Scope);
+					}
+					if (curMatch.Captures.Count > 0) {
+						var captureList = new List<ColoredSegment> ();
+						foreach (var capture in curMatch.Captures) {
+							var grp = match.Groups [capture.Item1];
+							if (grp.Length == 0)
+								continue;
+							if (curSegmentOffset < grp.Index) {
+								Insert (captureList, new ColoredSegment (curSegmentOffset, grp.Index - curSegmentOffset, scopeStack));
+							}
+							Insert (captureList, new ColoredSegment (grp.Index, grp.Length, scopeStack.Push (capture.Item2)));
+							curSegmentOffset = grp.Index + grp.Length;
+						}
+						foreach (var item in captureList)
+							yield return item;
+					}
+
+					if (curMatch.Pop || curMatch.Set != null) {
+						if (matchEndOffset - curSegmentOffset > 0) {
+							yield return new ColoredSegment (curSegmentOffset, matchEndOffset - curSegmentOffset, scopeStack);
+						}
+						curSegmentOffset = matchEndOffset;
+						if (curMatch.Pop || contextStack.Count () > 1) {
+							contextStack = contextStack.Pop ();
+							if (!matchStack.IsEmpty) {
+								if (matchStack.Peek ()?.Scope != null)
+									scopeStack = scopeStack.Pop ();
+								matchStack = matchStack.Pop ();
+							}
+							if (currentContext.MetaScope != null)
+								scopeStack = scopeStack.Pop ();
+							if (currentContext.MetaContentScope != null)
+								scopeStack = scopeStack.Pop ();
+						}
+					}
+					if (curMatch.Scope != null && curSegmentOffset < matchEndOffset) {
+						yield return new ColoredSegment (curSegmentOffset, matchEndOffset - curSegmentOffset, scopeStack);
+						curSegmentOffset = matchEndOffset;
+					}
+
+					if (curMatch.Push != null || curMatch.Set != null) {
+						var nextContexts = (curMatch.Push ?? curMatch.Set).GetContexts (highlighting);
+						if (nextContexts != null) {
+							bool first = true;
+							foreach (var nextContext in nextContexts) {
+								if (first) {
+									matchStack = matchStack.Push (curMatch);
+									if (curMatch.Scope != null) {
+										scopeStack = scopeStack.Push (curMatch.Scope);
+									}
+									first = false;
+								} else {
+									matchStack = matchStack.Push (null);
+								}
+								contextStack = contextStack.Push (nextContext);
+								if (nextContext.MetaScope != null)
+									scopeStack = scopeStack.Push (nextContext.MetaScope);
+								if (nextContext.MetaContentScope != null)
+									scopeStack = scopeStack.Push (nextContext.MetaContentScope);
+							}
+
+						}
+					}
+					if (curMatch.Scope != null) {
+						scopeStack = scopeStack.Pop ();
+					}
+					length -= curSegmentOffset - offset;
+					offset = curSegmentOffset;
+					goto restart;
+				}
+
+				if (endOffset - curSegmentOffset > 0) {
+					yield return new ColoredSegment (curSegmentOffset, endOffset - curSegmentOffset, scopeStack);
+				}
+			}
+		}
+	
 		static void Insert (List<ColoredSegment> list, ColoredSegment newSegment)
 		{
 			if (list.Count == 0) {
