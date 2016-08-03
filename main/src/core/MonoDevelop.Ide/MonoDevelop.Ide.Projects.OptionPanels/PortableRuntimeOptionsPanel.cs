97d2df05dd856953227a609ed13703e4648a2af2
Mikayla Hutchinson <m.j.hutchinson@gmail.com>
2016-08-02 22:20:18 -0400

    [Ide] Build UI for netstandard/pcl toggle

--- main/src/core/MonoDevelop.Ide/MonoDevelop.Ide.Projects.OptionPanels/PortableRuntimeOptionsPanel.cs
+++ main/src/core/MonoDevelop.Ide/MonoDevelop.Ide.Projects.OptionPanels/PortableRuntimeOptionsPanel.cs
@@ -59,6 +59,22 @@ namespace MonoDevelop.Ide.Projects.OptionPanels
 		DotNetProject project;
 		TargetFramework target;
 
+		string[] KnownNetStandardVersions = new [] {
+			"netstandard1.0",
+			"netstandard1.1",
+			"netstandard1.2",
+			"netstandard1.3",
+			"netstandard1.4",
+			"netstandard1.5",
+			"netstandard1.6",
+		};
+
+		readonly ComboBox netStandardCombo;
+		readonly Entry targetFrameworkEntry;
+		readonly RadioButton netstandardRadio;
+		readonly RadioButton pclRadio;
+		readonly Button frameworkPickerButton;
+
 		public PortableRuntimeOptionsPanelWidget (DotNetProject project, IEnumerable<ItemConfiguration> configurations)
 		{
 			this.project = project;
@@ -66,11 +82,62 @@ namespace MonoDevelop.Ide.Projects.OptionPanels
 
 			Spacing = 6;
 
-			var frameworkPickerButton = new Button (GettextCatalog.GetString ("Change Targets..."));
-			PackStart (frameworkPickerButton);
+			PackStart (new Label { Markup = string.Format ("<b>{0}</b>", GettextCatalog.GetString ("Target Framework")), Xalign = 0f });
+
+			var fxAlignment = new Alignment (0f, 0f, 1f, 1f) { LeftPadding = 12 };
+			PackStart (fxAlignment);
+			var radioBox = new VBox { Spacing = 10 };
+			fxAlignment.Add (radioBox);
+
+			var netstandardPickerHbox = new HBox { Spacing = 10 };
+			radioBox.PackStart (netstandardPickerHbox);
+			netstandardRadio = new RadioButton (GettextCatalog.GetString (".NET Standard Platform:"));
+			netstandardPickerHbox.PackStart (netstandardRadio, false, false, 0);
+			netstandardPickerHbox.PackStart (netStandardCombo = ComboBox.NewText (), false, false, 0);
+
+			var netstandardDesc = new Label { Markup = GettextCatalog.GetString ("Your library will be compatible with all frameworks that support the selected <a href='{0}'>.NET Standard</a> version.", "netstandard"), Xalign = 0f };
+			radioBox.PackStart (new Alignment (0f, 0f, 1f, 1f) { Child = netstandardDesc, LeftPadding = 24 });
+
+			var pclPickerHbox = new HBox { Spacing = 10 };
+			radioBox.PackStart (pclPickerHbox);
+			pclRadio = new RadioButton (netstandardRadio, GettextCatalog.GetString (".NET Portable:"));
+			pclPickerHbox.PackStart (pclRadio, false, false, 0);
+			pclPickerHbox.PackStart (targetFrameworkEntry = new Entry { IsEditable = false, WidthChars = 20 }, false, false, 0);
+			frameworkPickerButton = new Button (GettextCatalog.GetString ("Change..."));
+			pclPickerHbox.PackStart (frameworkPickerButton, false, false, 0);
+
+			var pclDesc = new Label { Markup = GettextCatalog.GetString ("Your library will be compatible with the frameworks supported by the selected <a href='{0}'>PCL profile</a>.", "pcl"), Xalign = 0f };
+			radioBox.PackStart (new Alignment (0f, 0f, 1f, 1f) { Child = pclDesc, LeftPadding = 24 });
+
 			frameworkPickerButton.Clicked += PickFramework;
 
+			foreach (var val in KnownNetStandardVersions) {
+				netStandardCombo.AppendText (val);
+			};
+			netStandardCombo.Active = KnownNetStandardVersions.Length - 1;
+
+			targetFrameworkEntry.Text = string.Format ("PCL {0} - {1}", target.Id.Version, target.Id.Profile);
+
+			// both toggle when we switch between them, only need to subscribe to one event
+			netstandardRadio.Toggled += RadioToggled;
+
+			UpdateSensitivity ();
+
 			ShowAll ();
+		}
+
+		void RadioToggled (object sender, EventArgs e)
+		{
+			UpdateSensitivity ();
+		}
+
+		void UpdateSensitivity()
+		{
+			bool pcl = pclRadio.Active;
+
+			netStandardCombo.Sensitive = !pcl;
+			targetFrameworkEntry.Sensitive = pcl;
+			frameworkPickerButton.Sensitive = pcl;
 		}
 
 		void PickFramework (object sender, EventArgs e)
