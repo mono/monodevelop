3886502bce8a848cfeddea8ab66406d5973a3176
Matt Ward <matt.ward@xamarin.com>
2016-08-05 15:07:50 +0100

    [Packaging] Add empty addin project

---
+++ main/src/addins/MonoDevelop.Packaging/Properties/AddinInfo.cs
@@ -0,0 +1,14 @@
+﻿
+using Mono.Addins;
+using Mono.Addins.Description;
+
+[assembly:Addin ("Packaging", 
+	Namespace = "MonoDevelop",
+	Version = MonoDevelop.BuildInfo.Version,
+	Category = "IDE extensions")]
+
+[assembly:AddinName ("NuGet Packaging")]
+[assembly:AddinDescription ("Provides support for creating NuGet packages.")]
+
+[assembly:AddinDependency ("Core", MonoDevelop.BuildInfo.Version)]
+[assembly:AddinDependency ("Ide", MonoDevelop.BuildInfo.Version)]
