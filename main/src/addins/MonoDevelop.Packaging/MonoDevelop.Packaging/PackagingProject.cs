737b33b08e098d73210a64f830980b7b636c73ac
Matt Ward <matt.ward@xamarin.com>
2016-08-09 14:32:09 +0100

    [Packaging] Add .nuproj project template
    Creates a .nuproj project with a readme.txt file. The readme.txt file
    is included so the project can compile without any errors since
    nuget pack will fail if there are no files or no dependencies.
    The project template does not use a project template wizard so
    some of the initial NuGet package metadata values are hard coded.

--- main/src/addins/MonoDevelop.Packaging/MonoDevelop.Packaging/PackagingProject.cs
+++ main/src/addins/MonoDevelop.Packaging/MonoDevelop.Packaging/PackagingProject.cs
@@ -26,19 +26,34 @@
 
 using System;
 using System.Collections.Generic;
+using System.Linq;
+using System.Xml;
 using MonoDevelop.Core;
 using MonoDevelop.Core.Assemblies;
+using MonoDevelop.Core.Serialization;
 using MonoDevelop.Projects;
 using MonoDevelop.Projects.MSBuild;
 
 namespace MonoDevelop.Packaging
 {
-	public class PackagingProject : DotNetProject
+	class PackagingProject : DotNetProject
 	{
 		public PackagingProject ()
 		{
 			UsePartialTypes = false;
 		}
+
+		[ItemProperty ("Id")]
+		string id;
+
+		[ItemProperty ("Version")]
+		string version;
+
+		[ItemProperty ("Authors")]
+		string authors;
+
+		[ItemProperty ("Description")]
+		string description;
 
 		protected override DotNetCompilerParameters OnCreateCompilationParameters (
 			DotNetProjectConfiguration config,
@@ -61,9 +76,9 @@ namespace MonoDevelop.Packaging
 				return result;
 
 			if (framework.Id.Identifier == ".NETFramework") {
-				Version version = null;
-				if (System.Version.TryParse (framework.Id.Version, out version)) {
-					return version.Major >= 4 && version.Minor >= 5;
+				Version frameworkVersion = null;
+				if (System.Version.TryParse (framework.Id.Version, out frameworkVersion)) {
+					return frameworkVersion.Major >= 4 && frameworkVersion.Minor >= 5;
 				}
 			}
 
@@ -89,6 +104,54 @@ namespace MonoDevelop.Packaging
 			foreach (KeyValuePair<string, string> property in provider.GetGlobalProperties ()) {
 				globalGroup.SetValue (property.Key, property.Value, property.Value);
 			}
+		}
+
+		protected override void OnGetDefaultImports (List<string> imports)
+		{
+			imports.Add (@"$(NuGetPackagingPath)\NuGet.Packaging.targets");
+		}
+
+		protected override void OnWriteProject (ProgressMonitor monitor, MSBuildProject msproject)
+		{
+			base.OnWriteProject (monitor, msproject);
+
+			bool newProject = FileName == null || msproject.IsNewProject;
+			if (newProject) {
+				AddPackagingPropsImport (msproject);
+			}
+		}
+
+		void AddPackagingPropsImport (MSBuildProject msproject)
+		{
+			MSBuildObject insertBefore = msproject.GetAllObjects ().FirstOrDefault ();
+			msproject.AddNewImport (
+				@"$(NuGetPackagingPath)\NuGet.Packaging.props",
+				@"Exists('$(NuGetPackagingPath)\NuGet.Packaging.props')",
+				insertBefore);
+		}
+
+		protected override string OnGetDefaultTargetPlatform (ProjectCreateInformation projectCreateInfo)
+		{
+			return String.Empty;
+		}
+
+		protected override void OnInitializeFromTemplate (ProjectCreateInformation projectCreateInfo, XmlElement projectOptions)
+		{
+			base.OnInitializeFromTemplate (projectCreateInfo, projectOptions);
+
+			CompileTarget = CompileTarget.Package;
+
+			// The following package metadata will eventually be provided by the
+			// project template wizard.
+			id = Name;
+			version = "1.0.0";
+			authors = AuthorInformation.Name;
+			description = Name;
+		}
+
+		protected override string OnGetDefaultBuildAction (string fileName)
+		{
+			return "NuGetFile";
 		}
 	}
 }
