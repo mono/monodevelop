mono/monodevelop

commit 02850812ea52f66fc5820f668575dd92da9afbc1

Improve default build configuration when invoking xbuild directly

main/before.Main.sln.targets
@@ -1,0 +1,19 @@
+<Project xmlns="http://schemas.microsoft.com/developer/msbuild/2003">
+
+	<!--
+	Ensure that when a configuration is not provided, we use a good default
+	for the current OS.
+	-->
+	<Choose Condition="'$(Configuration)'==''">
+		<When Condition="'$(OS)'!='Unix'">
+			<PropertyGroup>
+				<Configuration>DebugWin32</Configuration>
+			</PropertyGroup>
+		</When>
+		<When Condition="Exists('/System/Library/Frameworks/CoreFoundation.framework')">
+			<PropertyGroup>
+				<Configuration>DebugMac</Configuration>
+			</PropertyGroup>
+		</When>
+	</Choose>
+</Project>

commit 10d1ed523fac33df56f715fc362654f443b1c4ab

[Build] Fix build issue introduced in 02850812ea52f66fc5820f668575dd92da9afbc1

main/before.Main.sln.targets
@@ -4,16 +4,18 @@
 	Ensure that when a configuration is not provided, we use a good default
 	for the current OS.
 	-->
-	<Choose Condition="'$(Configuration)'==''">
-		<When Condition="'$(OS)'!='Unix'">
-			<PropertyGroup>
-				<Configuration>DebugWin32</Configuration>
-			</PropertyGroup>
-		</When>
-		<When Condition="Exists('/System/Library/Frameworks/CoreFoundation.framework')">
-			<PropertyGroup>
-				<Configuration>DebugMac</Configuration>
-			</PropertyGroup>
+	<Choose Condition>
+		<When Condition="'$(Configuration)'==''">
+			<When Condition="'$(OS)'!='Unix'">
+				<PropertyGroup>
+					<Configuration>DebugWin32</Configuration>
+				</PropertyGroup>
+			</When>
+			<When Condition="Exists('/System/Library/Frameworks/CoreFoundation.framework')">
+				<PropertyGroup>
+					<Configuration>DebugMac</Configuration>
+				</PropertyGroup>
+			</When>
 		</When>
 	</Choose>
 </Project>

main/src/addins/VersionControl/MonoDevelop.VersionControl/MonoDevelop.VersionControl.Views/CellRendererDiff.cs
@@ -41,6 +41,8 @@ namespace MonoDevelop.VersionControl.Views
 		protected override void OnDestroyed ()
 		{
 			isDisposed = true;
+			FontService.RemoveCallback (OnDiffFontChanged);
+			IdeApp.Preferences.CustomOutputPadFont.Changed -= OnTextFontChanged;
 			DisposeLayout ();
 			base.OnDestroyed ();
 		}
@@ -58,7 +60,7 @@ namespace MonoDevelop.VersionControl.Views
 			this.lines = lines;
 			this.diffMode = diffMode;
 			this.path = path;
-			
+
 			if (diffMode) {
 				if (lines != null && lines.Length > 0) {
 					int maxlen = -1;
@@ -86,18 +88,39 @@ namespace MonoDevelop.VersionControl.Views
 				layout = CreateLayout (container, string.Join (Environment.NewLine, lines));
 				layout.GetPixelSize (out width, out height);
 			}
+			FontService.RegisterFontChangedCallback ("Editor", OnDiffFontChanged);
+			IdeApp.Preferences.CustomOutputPadFont.Changed += OnTextFontChanged;
+		}
+
+		void UpdateFont (Pango.Layout forLayout)
+		{
+			forLayout = forLayout ?? layout;
+			if (diffMode)
+				layout.FontDescription = Pango.FontDescription.FromString (MonoDevelop.Ide.Editor.DefaultSourceEditorOptions.Instance.FontName);
+			else
+				layout.FontDescription = IdeApp.Preferences.CustomOutputPadFont;
+		}
+
+		void OnDiffFontChanged ()
+		{
+			UpdateFont (forLayout: null);
+		}
+
+		void OnTextFontChanged (object sender, EventArgs args)
+		{
+			UpdateFont (forLayout: null);
 		}
 		
 		Pango.Layout CreateLayout (Widget container, string text)
 		{
 			Pango.Layout layout = new Pango.Layout (container.PangoContext);
 			layout.SingleParagraphMode = false;
+			UpdateFont (layout);
 			if (diffMode) {
-				layout.FontDescription = FontService.MonospaceFont;
 				layout.SetText (text);
-			}
-			else
+			} else {
 				layout.SetMarkup (text);
+			}
 			return layout;
 		}
 		

main/src/addins/VersionControl/MonoDevelop.VersionControl/MonoDevelop.VersionControl.Views/LogWidget.cs
@@ -34,6 +34,7 @@ using System.Threading;
 using MonoDevelop.Components;
 using Mono.TextEditor;
 using System.Linq;
+using MonoDevelop.Ide.Fonts;
 
 namespace MonoDevelop.VersionControl.Views
 {
@@ -63,6 +64,7 @@ namespace MonoDevelop.VersionControl.Views
 		CellRendererDiff diffRenderer = new CellRendererDiff ();
 		CellRendererText messageRenderer = new CellRendererText ();
 		CellRendererText textRenderer = new CellRendererText ();
+		CellRendererText contentRenderer = new CellRendererText ();
 		CellRendererImage pixRenderer = new CellRendererImage ();
 		
 		bool currentRevisionShortened;
@@ -172,8 +174,7 @@ namespace MonoDevelop.VersionControl.Views
 			treeviewLog.AppendColumn (colRevMessage);
 			colRevMessage.MinWidth = 350;
 			colRevMessage.Resizable = true;
-			
-			
+
 			TreeViewColumn colRevDate = new TreeViewColumn (GettextCatalog.GetString ("Date"), textRenderer);
 			colRevDate.SetCellDataFunc (textRenderer, DateFunc);
 			colRevDate.Resizable = true;
@@ -210,25 +211,24 @@ namespace MonoDevelop.VersionControl.Views
 			
 			TreeViewColumn colChangedFile = new TreeViewColumn ();
 			var crp = new CellRendererImage ();
-			var crt = new CellRendererText ();
 			colChangedFile.Title = GettextCatalog.GetString ("File");
 			colChangedFile.PackStart (crp, false);
-			colChangedFile.PackStart (crt, true);
+			colChangedFile.PackStart (contentRenderer, true);
 			colChangedFile.AddAttribute (crp, "image", 2);
-			colChangedFile.AddAttribute (crt, "text", 3);
+			colChangedFile.AddAttribute (contentRenderer, "text", 3);
 			treeviewFiles.AppendColumn (colChangedFile);
 			
 			TreeViewColumn colOperation = new TreeViewColumn ();
 			colOperation.Title = GettextCatalog.GetString ("Operation");
 			colOperation.PackStart (crp, false);
-			colOperation.PackStart (crt, true);
+			colOperation.PackStart (contentRenderer, true);
 			colOperation.AddAttribute (crp, "image", 0);
-			colOperation.AddAttribute (crt, "text", 1);
+			colOperation.AddAttribute (contentRenderer, "text", 1);
 			treeviewFiles.AppendColumn (colOperation);
 			
 			TreeViewColumn colChangedPath = new TreeViewColumn ();
 			colChangedPath.Title = GettextCatalog.GetString ("Path");
-			
+
 			diffRenderer.DrawLeft = true;
 			colChangedPath.PackStart (diffRenderer, true);
 			colChangedPath.SetCellDataFunc (diffRenderer, SetDiffCellData);
@@ -238,7 +238,7 @@ namespace MonoDevelop.VersionControl.Views
 			treeviewFiles.Events |= Gdk.EventMask.PointerMotionMask;
 			
 			textviewDetails.WrapMode = Gtk.WrapMode.Word;
-			
+
 			labelAuthor.Text = "";
 			labelDate.Text = "";
 			labelRevision.Text = "";
@@ -256,6 +256,25 @@ namespace MonoDevelop.VersionControl.Views
 
 			UpdateStyle ();
 			Ide.Gui.Styles.Changed += HandleStylesChanged;
+
+			IdeApp.Preferences.CustomOutputPadFont.Changed += OnFontChanged;
+			IdeApp.Preferences.CustomPadFont.Changed += OnFontChanged;
+
+			OnFontChanged (null, null);
+		}
+
+		void OnFontChanged (object sender, EventArgs args)
+		{
+			var outputFont = IdeApp.Preferences.CustomOutputPadFont;
+			textRenderer.FontDesc = outputFont;
+			messageRenderer.FontDesc = outputFont;
+			contentRenderer.FontDesc = outputFont;
+
+			var padFont = IdeApp.Preferences.CustomPadFont.Value;
+			labelAuthor.ModifyFont (padFont);
+			labelDate.ModifyFont (padFont);
+			labelRevision.ModifyFont (padFont);
+			label3.ModifyFont (padFont);
 		}
 
 		protected override void OnRealized ()
@@ -500,6 +519,8 @@ namespace MonoDevelop.VersionControl.Views
 			revertToButton.Clicked -= RevertToRevisionClicked;
 			refreshButton.Clicked -= RefreshClicked;
 			Ide.Gui.Styles.Changed -= HandleStylesChanged;
+			IdeApp.Preferences.CustomPadFont.Changed -= OnFontChanged;
+			IdeApp.Preferences.CustomOutputPadFont.Changed -= OnFontChanged;
 
 			logstore.Dispose ();
 			changedpathstore.Dispose ();