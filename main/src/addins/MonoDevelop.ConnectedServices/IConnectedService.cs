0d3bc676e404a0fbde7b9bc3cf65c96413be3ace
Greg Munn <greg@sgmunn.com>
2016-09-02 10:35:10 -0400

    [ConnectedServices] ServiceStatus comments

--- main/src/addins/MonoDevelop.ConnectedServices/IConnectedService.cs
+++ main/src/addins/MonoDevelop.ConnectedServices/IConnectedService.cs
@@ -120,11 +120,29 @@ namespace MonoDevelop.ConnectedServices
 		Task<bool> RemoveFromProject ();
 	}
 
+	/// <summary>
+	/// Defines the various states that a service be in
+	/// </summary>
 	public enum ServiceStatus
 	{
+		/// <summary>
+		/// The service is not added to the project
+		/// </summary>
 		NotAdded,
+
+		/// <summary>
+		/// The service has been added to the project
+		/// </summary>
 		Added,
+
+		/// <summary>
+		/// The service is ciurrently being added to the project
+		/// </summary>
 		Adding,
+
+		/// <summary>
+		/// The service is currently being removed from the project
+		/// </summary>
 		Removing,
 	}
 }
