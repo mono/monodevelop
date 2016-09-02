0c860f322ae568cb36f2996c7c9f8d6203e34d3e
Václav Vančura <vaclav@vancura.org>
2016-09-02 15:52:12 +0200

    [WelcomePage] *very* minor fix to the remove button alignment

--- main/src/core/MonoDevelop.Ide/MonoDevelop.Ide.WelcomePage/WelcomePageListButton.cs
+++ main/src/core/MonoDevelop.Ide/MonoDevelop.Ide.WelcomePage/WelcomePageListButton.cs
@@ -301,7 +301,7 @@ namespace MonoDevelop.Ide.WelcomePage
 
 			if (hasRemoveButton && (mouseOver || !ItemAccessible)) {
 				x = Allocation.Right - InternalPadding - 2;
-				y = Allocation.Y + (int)(Allocation.Height / 2) - (int)(removeIcon.Height / 2);
+				y = Allocation.Y + (int)(Allocation.Height / 2) - (int)(removeIcon.Height / 2) - 1;
 				ctx.DrawImage (this, removeIcon, x, y);
 				removeRect = new Gdk.Rectangle (x, y, (int)removeIcon.Width, (int)removeIcon.Height);
 			}
