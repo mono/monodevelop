mono/monodevelop

commit b1ab403a063af7b8ef489ab198feff18e20cd02b

[Ide] GetWorkspace is now public - see 'Bug 40422 - The roslyn workspace can't be queried while it's loading'

main/src/core/MonoDevelop.Ide/MonoDevelop.Ide/RootWorkspace.cs
@@ -887,10 +887,10 @@ namespace MonoDevelop.Ide
 		
 #region Event handling
 		
-		internal void NotifyItemAdded (WorkspaceItem item)
+		internal async void NotifyItemAdded (WorkspaceItem item)
 		{
 			try {
-				MonoDevelop.Ide.TypeSystem.TypeSystemService.Load (item, null);
+				await MonoDevelop.Ide.TypeSystem.TypeSystemService.Load (item, null);
 			} catch (Exception ex) {
 				LoggingService.LogError ("Could not load parser database.", ex);
 			}

main/src/core/MonoDevelop.Ide/MonoDevelop.Ide.TypeSystem/MonoDevelopWorkspace.cs
@@ -48,10 +48,10 @@ using System.Collections.Immutable;
 namespace MonoDevelop.Ide.TypeSystem
 {
 
-	class MonoDevelopWorkspace : Workspace
+	public class MonoDevelopWorkspace : Workspace
 	{
 		readonly static HostServices services;
-		public readonly WorkspaceId Id;
+		internal readonly WorkspaceId Id;
 
 		CancellationTokenSource src = new CancellationTokenSource ();
 		MonoDevelop.Projects.Solution currentMonoDevelopSolution;
@@ -88,7 +88,7 @@ namespace MonoDevelop.Ide.TypeSystem
 			services = Microsoft.CodeAnalysis.Host.Mef.MefHostServices.Create (assemblies);
 		}
 
-		public MonoDevelopWorkspace () : base (services, ServiceLayer.Desktop)
+		internal MonoDevelopWorkspace () : base (services, ServiceLayer.Desktop)
 		{
 			this.Id = WorkspaceId.Next ();
 			if (IdeApp.Workspace != null) {
@@ -223,13 +223,13 @@ namespace MonoDevelop.Ide.TypeSystem
 			});
 		}
 
-		public Task<SolutionInfo> TryLoadSolution (MonoDevelop.Projects.Solution solution, CancellationToken cancellationToken = default(CancellationToken))
+		internal Task<SolutionInfo> TryLoadSolution (MonoDevelop.Projects.Solution solution, CancellationToken cancellationToken = default(CancellationToken))
 		{
 			this.currentMonoDevelopSolution = solution;
 			return CreateSolutionInfo (solution, cancellationToken);
 		}
 
-		public void UnloadSolution ()
+		internal void UnloadSolution ()
 		{
 			OnSolutionRemoved (); 
 		}
@@ -267,7 +267,7 @@ namespace MonoDevelop.Ide.TypeSystem
 			return null;
 		}
 
-		public bool Contains (ProjectId projectId) 
+		internal bool Contains (ProjectId projectId) 
 		{
 			return projectDataMap.ContainsKey (projectId);
 		}
@@ -705,7 +705,7 @@ namespace MonoDevelop.Ide.TypeSystem
 //			}
 //		}
 
-		public void InformDocumentClose (DocumentId analysisDocument, string filePath)
+		internal void InformDocumentClose (DocumentId analysisDocument, string filePath)
 		{
 			try {
 				var loader = new MonoDevelopTextLoader (filePath);
@@ -1053,7 +1053,7 @@ namespace MonoDevelop.Ide.TypeSystem
 		}
 		#endregion
 
-		public Document GetDocument (DocumentId documentId, CancellationToken cancellationToken = default(CancellationToken))
+		internal Document GetDocument (DocumentId documentId, CancellationToken cancellationToken = default(CancellationToken))
 		{
 			var project = CurrentSolution.GetProject (documentId.ProjectId);
 			if (project == null)
@@ -1061,7 +1061,7 @@ namespace MonoDevelop.Ide.TypeSystem
 			return project.GetDocument (documentId);
 		}
 
-		public async void UpdateFileContent (string fileName, string text)
+		internal async void UpdateFileContent (string fileName, string text)
 		{
 			SourceText newText = SourceText.From (text);
 			foreach (var kv in this.projectDataMap) {
@@ -1083,7 +1083,7 @@ namespace MonoDevelop.Ide.TypeSystem
 			}
 		}
 
-		public void RemoveProject (MonoDevelop.Projects.Project project)
+		internal void RemoveProject (MonoDevelop.Projects.Project project)
 		{
 			var id = GetProjectId (project); 
 			if (id != null) {

main/src/core/MonoDevelop.Ide/MonoDevelop.Ide.TypeSystem/TypeSystemService_WorkspaceHandling.cs
@@ -90,7 +90,7 @@ namespace MonoDevelop.Ide.TypeSystem
 		}
 
 
-		internal static MonoDevelopWorkspace GetWorkspace (MonoDevelop.Projects.Solution solution)
+		public static MonoDevelopWorkspace GetWorkspace (MonoDevelop.Projects.Solution solution)
 		{
 			if (solution == null)
 				throw new ArgumentNullException ("solution");

commit 72c6c4b892b6d1b483e95eeb01589d3334059535

[Ide] Don't wait for the typesystem to finish parsing to load a solution
This fixes an issue introduced in
b1ab403a063 which caused the workspace to have to wait for the typesystem to display the solution pad.

main/src/core/MonoDevelop.Ide/MonoDevelop.Ide/RootWorkspace.cs
@@ -887,10 +887,10 @@ namespace MonoDevelop.Ide
 		
 #region Event handling
 		
-		internal async void NotifyItemAdded (WorkspaceItem item)
+		internal void NotifyItemAdded (WorkspaceItem item)
 		{
 			try {
-				await MonoDevelop.Ide.TypeSystem.TypeSystemService.Load (item, null);
+				MonoDevelop.Ide.TypeSystem.TypeSystemService.Load (item, null);
 			} catch (Exception ex) {
 				LoggingService.LogError ("Could not load parser database.", ex);
 			}