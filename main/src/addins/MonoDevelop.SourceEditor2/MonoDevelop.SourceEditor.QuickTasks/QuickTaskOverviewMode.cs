e629509d3a4e4c5aa0f540375d988a6956550a45
Marius Ungureanu <marius.ungureanu@xamarin.com>
2016-09-17 17:18:12 +0300

    [Editor] Add sanity cancellation to quicktask idle updates.

--- main/src/addins/MonoDevelop.SourceEditor2/MonoDevelop.SourceEditor.QuickTasks/QuickTaskOverviewMode.cs
+++ main/src/addins/MonoDevelop.SourceEditor2/MonoDevelop.SourceEditor.QuickTasks/QuickTaskOverviewMode.cs
@@ -880,7 +880,7 @@ namespace MonoDevelop.SourceEditor.QuickTasks
 			return true;
 		}
 
-		CancellationTokenSource src = new CancellationTokenSource ();
+		CancellationTokenSource src;
 
 		class IdleUpdater
 		{
@@ -988,26 +988,29 @@ namespace MonoDevelop.SourceEditor.QuickTasks
 		void DrawIndicatorSurface(uint timeout = 250)
 		{
 			RemoveIndicatorIdleHandler ();
-			GLib.TimeoutHandler timeoutHandler = delegate {
-				indicatorIdleTimout = 0;
-				if (!IsRealized)
-					return false;
-				var allocation = Allocation;
-				src.Cancel ();
-				src = new CancellationTokenSource ();
-				new IdleUpdater (this, src.Token).Start ();
-				return false;
-			};
 			if (timeout == 0) {
-				timeoutHandler ();
+				IndicatorSurfaceTimeoutHandler ();
 			} else {
-				indicatorIdleTimout = GLib.Timeout.Add (timeout, timeoutHandler);
+				indicatorIdleTimout = GLib.Timeout.Add (timeout, IndicatorSurfaceTimeoutHandler);
 			}
+		}
+
+		bool IndicatorSurfaceTimeoutHandler ()
+		{
+			indicatorIdleTimout = 0;
+			if (!IsRealized)
+				return false;
+			var allocation = Allocation;
+			src?.Cancel ();
+			src = new CancellationTokenSource ();
+			new IdleUpdater (this, src.Token).Start ();
+			return false;
 		}
 
 		void RemoveIndicatorIdleHandler ()
 		{
 			if (indicatorIdleTimout > 0) {
+				src?.Cancel ();
 				GLib.Source.Remove (indicatorIdleTimout);
 				indicatorIdleTimout = 0;
 			}
