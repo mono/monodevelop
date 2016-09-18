d8a2935775fe410b688d798c41187fdecaf9ce97
Marius Ungureanu <marius.ungureanu@xamarin.com>
2016-09-18 17:02:04 +0300

    [Windows] Fix batch files, remove bash-isms.

--- main/winbuild.bat
+++ main/winbuild.bat
@@ -2,6 +2,6 @@
 "external\RefactoringEssentials\.nuget\NuGet.exe" restore external\RefactoringEssentials\RefactoringEssentials.sln
 "C:\Program Files (x86)\MSBuild\14.0\Bin\MSBuild.exe" Main.sln /m /p:Configuration=ReleaseWin32 /p:Platform="Any CPU"
 
-if exist "C:\Program Files (x86)\Microsoft Visual Studio 11.0\VC\bin\editbin.exe" then (
+if exist "C:\Program Files (x86)\Microsoft Visual Studio 11.0\VC\bin\editbin.exe" (
 	"C:\Program Files (x86)\Microsoft Visual Studio 11.0\VC\bin\editbin.exe" /largeaddressaware build/bin/MonoDevelop.exe
 )
--- main/windebugbuild.bat
+++ main/windebugbuild.bat
@@ -2,6 +2,6 @@
 "external\RefactoringEssentials\.nuget\NuGet.exe" restore external\RefactoringEssentials\RefactoringEssentials.sln
 "C:\Program Files (x86)\MSBuild\14.0\Bin\MSBuild.exe" Main.sln /m /p:Configuration=DebugWin32 /p:Platform="Any CPU"
 
-if exist "C:\Program Files (x86)\Microsoft Visual Studio 11.0\VC\bin\editbin.exe" then (
+if exist "C:\Program Files (x86)\Microsoft Visual Studio 11.0\VC\bin\editbin.exe" (
 	"C:\Program Files (x86)\Microsoft Visual Studio 11.0\VC\bin\editbin.exe" /largeaddressaware Xamarin.Startup/bin/Debug/XamarinStudio.exe
 )
