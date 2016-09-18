b9c3e68061e16c9f6743336590f4a73716fbc8e8
Marius Ungureanu <marius.ungureanu@xamarin.com>
2016-09-18 21:17:41 +0300

    [Windows] Fix LAA flag setting in debug batch file.

--- main/windebugbuild.bat
+++ main/windebugbuild.bat
@@ -3,5 +3,5 @@
 "C:\Program Files (x86)\MSBuild\14.0\Bin\MSBuild.exe" Main.sln /m /p:Configuration=DebugWin32 /p:Platform="Any CPU"
 
 if exist "C:\Program Files (x86)\Microsoft Visual Studio 11.0\VC\bin\editbin.exe" (
-	"C:\Program Files (x86)\Microsoft Visual Studio 11.0\VC\bin\editbin.exe" /largeaddressaware Xamarin.Startup/bin/Debug/XamarinStudio.exe
+	"C:\Program Files (x86)\Microsoft Visual Studio 11.0\VC\bin\editbin.exe" /largeaddressaware build/bin/MonoDevelop.exe
 )
