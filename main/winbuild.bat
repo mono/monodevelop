5532394e1a663844e79a9e8f177c9f12fa392e66
Marius Ungureanu <marius.ungureanu@xamarin.com>
2016-09-18 15:36:18 +0300

    [Windows] Compile XS with largeaddressaware support.
    This allows XS on Windows to go over 1.2gb memory usage.

--- main/winbuild.bat
+++ main/winbuild.bat
@@ -1,3 +1,4 @@
 ".nuget\NuGet.exe" restore Main.sln
 "external\RefactoringEssentials\.nuget\NuGet.exe" restore external\RefactoringEssentials\RefactoringEssentials.sln
 "C:\Program Files (x86)\MSBuild\14.0\Bin\MSBuild.exe" Main.sln /m /p:Configuration=ReleaseWin32 /p:Platform="Any CPU"
+"C:\Program Files (x86)\Microsoft Visual Studio 11.0\VC\bin\editbin.exe" /largeaddressaware build/bin/MonoDevelop.exe
