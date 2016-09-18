c4f5b2b8d30e9ac5e0b9f44b60f1c4d644dde7e9
Marius Ungureanu <marius.ungureanu@xamarin.com>
2016-09-18 18:43:13 +0300

    [Ide] Mark addins being hidden via AddinFlags attribute.
    not sure why, but the version before this change causes the addins to not be discoverable via AddinRegistry.GetAddin(name)

--- main/src/addins/MacPlatform/AddinInfo.cs
+++ main/src/addins/MacPlatform/AddinInfo.cs
@@ -6,11 +6,11 @@ using Mono.Addins.Description;
 [assembly:Addin ("MacPlatform", 
         Namespace = "MonoDevelop",
         Version = MonoDevelop.BuildInfo.Version,
-        Flags = AddinFlags.Hidden,
         Category = "Platform Support")]
 
 [assembly:AddinName ("MonoDevelop Mac Platform Support")]
 [assembly:AddinDescription ("Mac Platform Support for MonoDevelop")]
+[assembly: AddinFlags (AddinFlags.Hidden)]
 
 [assembly:AddinDependency ("Ide", MonoDevelop.BuildInfo.Version)]
 [assembly:AddinDependency ("Core", MonoDevelop.BuildInfo.Version)]
