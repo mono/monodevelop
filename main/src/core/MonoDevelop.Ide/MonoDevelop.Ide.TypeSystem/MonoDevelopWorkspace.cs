mono/monodevelop

commit c5e0ab0e26472a5f0b31f625c3b8b65947542f01

[Ide] Add tooltip to Typesystem parser status icon.
Mirrors the text from CompletionDataWindow.

main/src/core/MonoDevelop.Ide/MonoDevelop.Ide.TypeSystem/MonoDevelopWorkspace.cs
@@ -156,6 +156,7 @@ namespace MonoDevelop.Ide.TypeSystem
 				if (statusIcon != null)
 					return;
 				statusIcon = IdeApp.Workbench?.StatusBar.ShowStatusIcon (ImageService.GetIcon ("md-parser"));
+				statusIcon.ToolTip = GettextCatalog.GetString ("Gathering class information");
 			});
 		}
 

commit ae9ab201506b7b20a474d8355cd8abb4fadef194

[Ide] Fix NRE caused by c5e0ab0e26472a5f0b31f625c3b8b65947542f01 in a test context.

main/src/core/MonoDevelop.Ide/MonoDevelop.Ide.TypeSystem/MonoDevelopWorkspace.cs
@@ -156,7 +156,8 @@ namespace MonoDevelop.Ide.TypeSystem
 				if (statusIcon != null)
 					return;
 				statusIcon = IdeApp.Workbench?.StatusBar.ShowStatusIcon (ImageService.GetIcon ("md-parser"));
-				statusIcon.ToolTip = GettextCatalog.GetString ("Gathering class information");
+				if (statusIcon != null)
+					statusIcon.ToolTip = GettextCatalog.GetString ("Gathering class information");
 			});
 		}
 