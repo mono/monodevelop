7fa49d3fca18c6bdfd758508c3c129c0f35678e3
Mike Krüger <mkrueger@xamarin.com>
2016-10-11 14:59:49 +0200

    Fixed windows build batch files.
    We've moved the nuget to a submodule.

--- main/winbuild.bat
+++ main/winbuild.bat
@@ -1,4 +1,4 @@
-".nuget\NuGet.exe" restore Main.sln
+"external\nuget-binary\NuGet.exe" restore Main.sln
 "external\RefactoringEssentials\.nuget\NuGet.exe" restore external\RefactoringEssentials\RefactoringEssentials.sln
 "C:\Program Files (x86)\MSBuild\14.0\Bin\MSBuild.exe" Main.sln /m /p:Configuration=ReleaseWin32 /p:Platform="Any CPU"
 
--- main/windebugbuild.bat
+++ main/windebugbuild.bat
@@ -1,4 +1,4 @@
-".nuget\NuGet.exe" restore Main.sln
+"external\nuget-binary\NuGet.exe" restore Main.sln
 "external\RefactoringEssentials\.nuget\NuGet.exe" restore external\RefactoringEssentials\RefactoringEssentials.sln
 "C:\Program Files (x86)\MSBuild\14.0\Bin\MSBuild.exe" Main.sln /m /p:Configuration=DebugWin32 /p:Platform="Any CPU"
 
