c2d17002bea5ed597abad2a2e8f293e3bcada572
nosami <jasonimison@gmail.com>
2016-10-13 14:03:50 +0100

    Update F# theme

--- main/external/fsharpbinding/MonoDevelop.FSharp.Tests/SyntaxHighlighting.fs
+++ main/external/fsharpbinding/MonoDevelop.FSharp.Tests/SyntaxHighlighting.fs
@@ -9,13 +9,20 @@ open MonoDevelop.Ide.Editor
 open FsUnit
 open System.Threading
 open System.IO
-//open System.Linq
 
+//open System.Linq
 [<TestFixture>]
+//type A = A of int
+//type A = A of a:int
+//type B = B of int
+//w11.Position <- 0
+//let _x, y = 1,2
+//let docs = openDocument()
 type SyntaxHighlighting() =
     do
         FixtureSetup.initialiseMonoDevelop()
 
+    let mutable x = 1
     let assertStyle (input:string, expectedStyle:string) =
         //Assert.Fail()
         //editor.SyntaxHighlighting.GetHighlightedLineAsync
@@ -25,6 +32,7 @@ type SyntaxHighlighting() =
         //Syntax
         let offset = input.IndexOf("$")
         let length = input.LastIndexOf("$") - offset - 1
+
         let input = input.Replace("$", "")
         let data = new TextEditorData (new TextDocument (input))
         let markup = data.GetMarkup(offset, length, false, true, false, false)
@@ -32,10 +40,12 @@ type SyntaxHighlighting() =
         use reader = File.OpenText("/Users/jason/src/monodevelop/main/src/core/MonoDevelop.Ide/MonoDevelop.Ide.Editor.Highlighting/syntaxes/FSharp/F#.sublime-syntax")
         let highlighting = Sublime3Format.ReadHighlighting(reader)
         highlighting.PrepareMatches()
-        editor.Text <- input
+        editor.Text <- " " + input
         editor.SyntaxHighlighting <- MonoDevelop.Ide.Editor.Highlighting.SyntaxHighlighting(highlighting, editor)
+        //let offset = if length = 1 then offset else offset+1
         let stack = editor.GetScopeStackAsync(offset+1, CancellationToken.None) |> Async.AwaitTask |> Async.RunSynchronously
         let first = stack |> Seq.head
+
         Assert.AreEqual(expectedStyle, first)
         //let syntaxMode = SyntaxModeService.GetSyntaxMode (data.Document, "text/x-fsharp")
         //let style = SyntaxModeService.GetColorStyle ("Gruvbox")
@@ -62,81 +72,86 @@ type SyntaxHighlighting() =
         //    chunk.Offset + chunk.Length
 
         //Seq.fold assertOffsets 0 chunks |> ignore
+    [<TestCase("let $_x$, y = 1,2", "source.fs")>]
+    [<TestCase("$yield$ x", "keyword.source.fs")>]
+    [<TestCase("$let$ (|Event|_|) = function", "keyword.source.fs")>]
     [<TestCase("$namespace$ MonoDevelop", "keyword.source.fs")>]
+    [<TestCase("namespace $MonoDevelop$", "source.fs")>]
     [<TestCase("let simpleBinding = $1$", "constant.numeric.source.fs")>]
     [<TestCase("$let$ simpleBinding = 1", "keyword.source.fs")>]
     [<TestCase("$let!$ simpleBinding = 1", "keyword.source.fs")>]
     [<TestCase("let $simpleBinding$ = 1", "entity.name.field")>]
     [<TestCase("let $offset$ = 1", "entity.name.field")>]
-    [<TestCase("let $add$ x y = x + y", "User Method Declaration")>]
+    [<TestCase("let $add$ x y = x + y", "entity.name.function")>]
+    [<TestCase("let add $param$ y = param + y", "entity.name.field")>]
     [<TestCase("let simpleBinding$ = $1", "source.fs")>]
     [<TestCase("$open$ MonoDevelop", "keyword.source.fs")>]
     [<TestCase("open$ MonoDevelop$", "source.fs")>]
     [<TestCase("open$ Mono.Text$", "source.fs")>]
-    [<TestCase("Seq.$find$ (", "User Method Declaration")>]
-    [<TestCase("SyntaxModeService.$GetColorStyle$ (\"Gruvbox\")", "User Method Declaration")>]
-    [<TestCase("Seq.find ($fun$ c", "Keyword(Jump)")>]
-    [<TestCase("$type$ SyntaxHighlighting() =", "Keyword(Namespace)")>]
+    [<TestCase("Seq.$find$ (", "entity.name.function")>]
+    [<TestCase("SyntaxModeService.$GetColorStyle$ (\"Gruvbox\")", "entity.name.function")>]
+    [<TestCase("Seq.find ($fun$ c", "keyword.source.fs")>]
+    [<TestCase("$type$ SyntaxHighlighting() =", "keyword.source.fs")>]
     [<TestCase("type $SyntaxHighlighting$ () =", "entity.name.class")>]
     [<TestCase("type $``Completion Tests``$ () =", "entity.name.class")>]
     [<TestCase("$module$ MyModule =", "keyword.source.fs")>]
     [<TestCase("module $MyModule$ =", "entity.name.class")>]
     [<TestCase("[<$TestCase$(", "entity.name.class")>]
-    [<TestCase("$[<$TestCase(", "Punctuation(Brackets)")>]
+    [<TestCase("$[<$TestCase(", "punctuation.section.brackets")>]
     [<TestCase("inherits $SyntaxHighlighting$ () =", "entity.name.class")>]
     [<TestCase("new $DefaultBraceMatcher$()", "entity.name.class")>]
     [<TestCase("$match$ (startOffset, endOffset) with", "keyword.source.fs")>]
     [<TestCase("$else$", "keyword.source.fs")>]
     [<TestCase("let x (y: $string$", "entity.name.class")>]
-    [<TestCase("string.$Length$", "User Property Declaration")>]
-    [<TestCase("$($", "Punctuation(Brackets)")>]
-    [<TestCase("$<$", "Punctuation(Brackets)")>]
-    [<TestCase("$[$", "Punctuation(Brackets)")>]
-    [<TestCase("${$", "Punctuation(Brackets)")>]
-    [<TestCase("do Something() |> $ignore$", "User Method Declaration")>]
+    [<TestCase("string.$Length$", "entity.name.property")>]
+    [<TestCase("$($", "punctuation.section.brackets")>]
+    [<TestCase("$<$", "punctuation.section.brackets")>]
+    [<TestCase("$[$", "punctuation.section.brackets")>]
+    [<TestCase("${$", "punctuation.section.brackets")>]
+    [<TestCase("do Something() |> $ignore$", "entity.name.function")>]
     [<TestCase("let $mutable$ x   = 1", "keyword.source.fs")>]
     [<TestCase("let mutable  $x$ = 1", "entity.name.field")>]
     [<TestCase("let mutable x$ = $1", "source.fs")>]
     [<TestCase("c.Style$ |> $should equal", "source.fs")>]
-    [<TestCase("c.Style |> $should$ equal", "User Method Declaration")>]
+    [<TestCase("c.Style |> $should$ equal", "entity.name.function")>]
     [<TestCase("match $x$ with", "entity.name.field")>]
     [<TestCase("Unchecked.defaultof<$_$>", "source.fs")>]
-    [<TestCase("Seq.$add$", "User Method Declaration")>]
-    [<TestCase("let inline$ add$ x y = x + y", "User Method Declaration")>]
+    [<TestCase("Seq.$add$", "entity.name.function")>]
+    [<TestCase("let inline$ add$ x y = x + y", "entity.name.function")>]
     [<TestCase("$override$ x.Something()", "keyword.source.fs")>]
     [<TestCase("member x.$``some identifier``$ = 1", "entity.name.field")>]
-    [<TestCase("member x.$``some identifier``$ () = 1", "User Method Declaration")>]
+    [<TestCase("member x.$``some identifier``$ () = 1", "entity.name.function")>]
     [<TestCase("let mutable $vbox4$ : Gtk.VBox = null", "entity.name.field")>]
     [<TestCase("$return$ x", "keyword.source.fs")>]
     [<TestCase("$return!$ x", "keyword.source.fs")>]
     [<TestCase("member val IndentOnTryWith = false with $get, set$", "source.fs")>]
     [<TestCase("| Some $funion$ -> ", "entity.name.field")>]
-    [<TestCase("yield $sprintf$ \"%A\"", "User Method Declaration")>]
+    [<TestCase("yield $sprintf$ \"%A\"", "entity.name.function")>]
     [<TestCase("$doc$.Editor", "entity.name.field")>]
     [<TestCase(":> $SomeType$", "entity.name.class")>]
-    [<TestCase("($'c'$)", "String")>]
+    [<TestCase("($'c'$)", "string.quoted.single.source.fs")>]
     [<TestCase("| Type of $string$", "entity.name.class")>]
     [<TestCase("$DisplayFlags$ = DisplayFlags.DescriptionHasMarkup", "entity.name.field")>]
     [<TestCase("let shouldEqual (x: $'a$) (y: 'a) =", "entity.name.class")>]
     [<TestCase("| :? $string$", "entity.name.class")>]
-    [<TestCase("let inline $private$ is expr s =", "Keyword(Modifiers)")>]
-    [<TestCase("let inline private$ is$ expr s =", "User Method Declaration")>]
-    [<TestCase("override x.$CanHandle$ editor", "User Method Declaration")>]
+    [<TestCase("let inline $private$ is expr s =", "keyword.source.fs")>]
+    [<TestCase("let inline private$ is$ expr s =", "entity.name.function")>]
+    [<TestCase("override x.$CanHandle$ editor", "entity.name.function")>]
     [<TestCase("let addEdge ((n1, n2): 'n * $'n$)", "entity.name.class")>]
-    [<TestCase("Map<'n, Set<'n$>>$", "Punctuation(Brackets)")>]
-    [<TestCase("let docs = $openDocuments$()", "User Method Declaration")>]
-    [<TestCase("let x = $true$", "Keyword(Constants)")>]
+    [<TestCase("Map<'n, Set<'n$>>$", "punctuation.section.brackets")>]
+    [<TestCase("let docs = $openDocuments$()", "entity.name.function")>]
+    [<TestCase("let x = $true$", "constant.language.source.fs")>]
     [<TestCase("let $``simple binding``$ = 1", "entity.name.field")>]
-    [<TestCase("let inline$ ``add number``$ x y = x + y", "User Method Declaration")>]
-    [<TestCase("$|>$ Option.bind", "Punctuation(Brackets)")>]
+    [<TestCase("let inline$ ``add number``$ x y = x + y", "entity.name.function")>]
+    [<TestCase("$|>$ Option.bind", "punctuation.section.brackets")>]
     [<TestCase("$typeof$<int>", "entity.name.field")>]
-    [<TestCase("editor.CaretOffset $<-$ offset", "Punctuation(Brackets)")>]
+    [<TestCase("editor.CaretOffset $<-$ offset", "punctuation.section.brackets")>]
     [<TestCase("let x$  = $1", "source.fs")>]
     [<TestCase("let $x$     = ", "entity.name.field")>]
-    [<TestCase(@"type thing = TP<$""$32""", "String")>]
+    [<TestCase(@"type thing = TP<$""$32""", "string.quoted.double.source.fs")>]
     [<TestCase(@"type thing = TP<$MyParam$=""32"", MyOtherParam=42>", "entity.name.field")>]
     [<TestCase(@"let rec computeSomeFunction $x$ =", "entity.name.field")>]
-    [<TestCase(@"Option.tryCast<MonoTextEditor$>($fun e", "Punctuation(Brackets)")>]
+    [<TestCase(@"Option.tryCast<MonoTextEditor$>($fun e", "punctuation.section.brackets")>]
     [<TestCase(@"let mutable session$ = $setupSession()", "source.fs")>]
     [<TestCase(@"$0b010101$", "constant.numeric.source.fs")>]
     [<TestCase(@"w11.Position <- $0$", "constant.numeric.source.fs")>]
@@ -157,8 +172,8 @@ type SyntaxHighlighting() =
     member x.``Syntax highlighting``(source, expectedStyle) =
         assertStyle (source, expectedStyle)
 
-    [<TestCase(@"static member $GetDefaultConfiguration$ :", "User Method Declaration")>]
-    [<TestCase(@"member $``Syntax Highlighting``$ :", "User Method Declaration")>]
+    [<TestCase(@"static member $GetDefaultConfiguration$ :", "entity.name.function")>]
+    [<TestCase(@"member $``Syntax Highlighting``$ :", "entity.name.function")>]
     [<TestCase(@"-> $^T$ :", "entity.name.class")>]
     [<TestCase(@"   -> $seq$<'T>", "entity.name.class")>]
     [<TestCase(@"   $list$ :", "entity.name.field")>]
