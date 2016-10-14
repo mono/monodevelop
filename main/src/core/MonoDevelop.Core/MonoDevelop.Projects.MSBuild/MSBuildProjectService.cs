0bda0fbb064441269ae0d0da5b6ed1a177f0aee8
Lluis Sanchez <lluis@xamarin.com>
2016-10-14 16:36:17 +0200

    [Core] Fix project builder sharing issue
    Don't share builders that have different requeriments wrt use of MSBuild

--- main/src/core/MonoDevelop.Core/MonoDevelop.Projects.MSBuild/MSBuildProjectService.cs
+++ main/src/core/MonoDevelop.Core/MonoDevelop.Projects.MSBuild/MSBuildProjectService.cs
@@ -943,7 +943,7 @@ namespace MonoDevelop.Projects.MSBuild
 				}
 
 				//one builder per solution
-				string builderKey = runtime.Id + " # " + solutionFile + " # " + customId;
+				string builderKey = runtime.Id + " # " + solutionFile + " # " + customId + " # " + requiresMicrosoftBuild;
 
 				RemoteBuildEngine builder = null;
 
