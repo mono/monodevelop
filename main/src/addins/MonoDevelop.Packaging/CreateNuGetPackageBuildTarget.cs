83d1e3d652e058552022825c0dc1130915e46169
Matt Ward <matt.ward@xamarin.com>
2016-10-14 12:14:11 +0100

    [Packaging] Fix Create NuGet Package not creating a .nupkg
    Setting the PackOnBuild to true as a global MSBuild property does not
    work with MSBuild but does with XBuild. The property does not seem to
    have any affect on properties in the project or imported. As a
    workaround the Pack target is being called directly. Also still using
    the IdeApp.ProjectOperations.Build method so build errors are reported
    in the Errors window. Running the Pack target directly on the project
    without using ProjectOperations.Build results in no information being
    reported in the IDE.

---
+++ main/src/addins/MonoDevelop.Packaging/CreateNuGetPackageBuildTarget.cs
@@ -0,0 +1,88 @@
+﻿//
+// CreateNuGetPackageBuildTarget.cs
+//
+// Author:
+//       Matt Ward <matt.ward@xamarin.com>
+//
+// Copyright (c) 2016 Xamarin Inc. (http://xamarin.com)
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in
+// all copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+// THE SOFTWARE.
+
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using MonoDevelop.Core;
+using MonoDevelop.Projects;
+
+namespace MonoDevelop.Packaging
+{
+	class CreateNuGetPackageBuildTarget : IBuildTarget
+	{
+		DotNetProject project;
+
+		public CreateNuGetPackageBuildTarget (DotNetProject project)
+		{
+			this.project = project;
+		}
+
+		public string Name {
+			get { return project.Name; }
+		}
+
+		public async Task<BuildResult> Build (ProgressMonitor monitor, ConfigurationSelector configuration, bool buildReferencedTargets = false, OperationContext operationContext = null)
+		{
+			return (await project.RunTarget (monitor, "Pack", configuration, new TargetEvaluationContext (operationContext))).BuildResult;
+		}
+
+		public bool CanBuild (ConfigurationSelector configuration)
+		{
+			throw new NotImplementedException ();
+		}
+
+		public bool CanExecute (ExecutionContext context, ConfigurationSelector configuration)
+		{
+			throw new NotImplementedException ();
+		}
+
+		public Task<BuildResult> Clean (ProgressMonitor monitor, ConfigurationSelector configuration, OperationContext operationContext = null)
+		{
+			throw new NotImplementedException ();
+		}
+
+		public Task Execute (ProgressMonitor monitor, ExecutionContext context, ConfigurationSelector configuration)
+		{
+			throw new NotImplementedException ();
+		}
+
+		public IEnumerable<IBuildTarget> GetExecutionDependencies ()
+		{
+			throw new NotImplementedException ();
+		}
+
+		public bool NeedsBuilding (ConfigurationSelector configuration)
+		{
+			throw new NotImplementedException ();
+		}
+
+		public Task PrepareExecution (ProgressMonitor monitor, ExecutionContext context, ConfigurationSelector configuration)
+		{
+			throw new NotImplementedException ();
+		}
+	}
+}
--- main/src/addins/MonoDevelop.Packaging/MonoDevelop.Packaging/CreateNuGetPackageHandler.cs
+++ main/src/addins/MonoDevelop.Packaging/MonoDevelop.Packaging/CreateNuGetPackageHandler.cs
@@ -38,9 +38,12 @@ namespace MonoDevelop.Packaging
 			if (project == null)
 				return;
 
+			// Setting PackOnBuild as a global property does not work since instead
+			// a dummy build target is created that calls the Pack target instead.
 			var context = new ProjectOperationContext ();
 			context.GlobalProperties.SetValue ("PackOnBuild", "true");
-			IdeApp.ProjectOperations.Build (project, operationContext: context);
+			var buildTarget = new CreateNuGetPackageBuildTarget (project);
+			IdeApp.ProjectOperations.Build (buildTarget, operationContext: context);
 		}
 	}
 }
