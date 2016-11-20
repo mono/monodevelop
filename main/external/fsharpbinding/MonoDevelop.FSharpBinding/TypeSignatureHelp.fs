d60f2e707b5ab6ca3d22eb58ec3977c4dbe0a01c
nosami <jasonimison@gmail.com>
2016-11-20 12:25:19 +0000

    main/external/fsharpbinding/MonoDevelop.FSharpBinding/TypeSignatureHelp.fs

--- main/external/fsharpbinding/MonoDevelop.FSharpBinding/TypeSignatureHelp.fs
+++ main/external/fsharpbinding/MonoDevelop.FSharpBinding/TypeSignatureHelp.fs
@@ -13,30 +13,37 @@ open MonoDevelop.Ide.Editor.Extension
 open Microsoft.FSharp.Compiler.SourceCodeServices
 open Microsoft.FSharp.Compiler
 open System.Reactive.Linq
-type SignatureHelpMarker(document, text, font, lineNr) =
+
+type SignatureHelpMarker(document, text, font, lineNr, line) =
     inherit TextLineMarker()
+    let mutable text' = text
+    //let mutable layout: Pango.Layout option = None
+    let tag = obj()
     static member FontScale = 0.8
-
-    member x.Text = text
-
-    override this.Draw(editor, g, _metrics) =
-        g.SetSourceRGB(0.5, 0.5, 0.5)
-        use layout = new Pango.Layout(editor.PangoContext, FontDescription=font)
-        let line = editor.GetLine lineNr
-        layout.SetText text
-        let x = editor.ColumnToX (line, (line.GetIndentation(document).Length+1)) - editor.HAdjustment.Value + editor.TextViewMargin.XOffset + (editor.TextViewMargin.TextStartPosition |> float)
-        let y = (editor.LineToY lineNr) - editor.VAdjustment.Value
-
-        let currentPoint = g.CurrentPoint
-        g.MoveTo(x, y + editor.LineHeight * (1.0 - SignatureHelpMarker.FontScale))
-        g.MoveTo(x, y + 5.0)
-        g.ShowLayout layout
-        g.MoveTo currentPoint
+    member x.Text with get() = text' and set(value) = text' <- value
+    //override this.Draw(editor, g, _metrics) = ()
+    interface ITextLineMarker with
+        member x.Line with get() = line
+        member x.IsVisible with get() = true and set(value) = ()
+        member x.Tag with get() = tag and set(value) = ()
 
     interface IExtendingTextLineMarker with
         member x.IsSpaceAbove with get() = true
         member x.GetLineHeight editor = editor.LineHeight * (1.0 + SignatureHelpMarker.FontScale)
-        member x.Draw(_editor, _g, _lineNr, _lineArea) = ()
+        member x.Draw(editor, g, lineNr, _lineArea) =
+            g.SetSourceRGB(0.5, 0.5, 0.5)
+            let line = editor.GetLine lineNr
+
+            use layout = new Pango.Layout(editor.PangoContext, FontDescription=font)
+            layout.SetText text'
+            let x = editor.ColumnToX (line, (line.GetIndentation(document).Length+1)) - editor.HAdjustment.Value + editor.TextViewMargin.XOffset + (editor.TextViewMargin.TextStartPosition |> float)
+            let y = (editor.LineToY lineNr) - editor.VAdjustment.Value
+
+            let currentPoint = g.CurrentPoint
+            g.MoveTo(x, y + editor.LineHeight * (1.0 - SignatureHelpMarker.FontScale))
+            //g.MoveTo(x, y + 5.0)
+            g.ShowLayout layout
+            g.MoveTo currentPoint
 
 module signatureHelp =
     let getOffset (editor:TextEditor) (pos:Range.pos) =
@@ -97,41 +104,68 @@ module signatureHelp =
             
 
             // remove any markers that are in the wrong positions
-            markers.Keys
-            |> Seq.filter(fun lineNumber -> lineNumber >= topVisibleLine && lineNumber <= bottomVisibleLine)
-            |> Seq.iter(fun lineNr -> 
-                            if not (funs.ContainsKey lineNr) then
-                                document.RemoveMarker markers.[lineNr]
-                                markers.TryRemove(lineNr) |> ignore)
+            //markers.Keys
+            //|> Seq.filter(fun lineNumber -> lineNumber >= topVisibleLine && lineNumber <= bottomVisibleLine)
+            //|> Seq.iter(fun lineNr -> 
+            //                if not (funs.ContainsKey lineNr) then
+            //                    markers.TryRemove(lineNr) |> ignore)
+            let lineNumbersWithMarkersToRemove = 
+                [topVisibleLine..bottomVisibleLine] |> List.filter(not << funs.ContainsKey)
+            if not lineNumbersWithMarkersToRemove.IsEmpty then
+                for lineNr in lineNumbersWithMarkersToRemove do
+                    let line = editor.GetLine lineNr
+                    editor.GetLineMarkers line 
+                    |> Seq.iter(fun m -> Runtime.RunInMainThread(fun() -> editor.RemoveMarker m) |> ignore)
+                document.CommitUpdateAll()
+            //editor.GetLineMarkers(
 
-            let addMarker text lineNr =
-                let newMarker = SignatureHelpMarker(document, text, font, lineNr)
+            let addMarker text lineNr line =
+                let newMarker = SignatureHelpMarker(document, text, font, lineNr, line)
                 markers.TryAdd (lineNr, newMarker) |> ignore
-                Runtime.RunInMainThread(fun () -> document.AddMarker(lineNr, newMarker)) |> ignore
+                document.AddMarker(lineNr, newMarker)
+                newMarker :> ITextLineMarker
+                //let line = editor.GetLine lineNr
+
+                //let offset = line.Offset
+                //let location = DocumentLocation(lineNr, 0)
+                //// use a smart marker to store the signature marker
+                //// so that we don't need to keep track of markers separately
+                //let currentSmartTag = TextMarkerFactory.CreateSmartTagMarker (editor, offset, location)
+                ////currentSmartTag.CancelPopup += CurrentSmartTag_CancelPopup;
+                ////currentSmartTag.ShowPopup += CurrentSmartTag_ShowPopup;
+                //currentSmartTag.Tag <- newMarker
+                //currentSmartTag.IsVisible <- false
+                //editor.AddMarker (currentSmartTag)
 
             funs |> Map.iter(fun _l f ->
                 let range = f.RangeAlternate
                 let lineText = editor.GetLineText(range.StartLine)
 
-                let res, marker = markers.TryGetValue range.StartLine
-                if not res then
+                let line = editor.GetLine range.StartLine
+                //editor.sm
+                //let res, marker = markers.TryGetValue range.StartLine
+                let marker = editor.GetLineMarkers line |> Seq.tryHead
+
+                //if marker.IsNone then
                     //add placeholder
-                    addMarker "" range.StartLine
+                let marker = marker |> Option.getOrElse(fun() -> addMarker "" range.StartLine line)
                 if range.StartLine >= topVisibleLine && range.EndLine <= bottomVisibleLine then
                     async {
                         let! tooltip = ast.GetToolTip(range.StartLine, range.StartColumn, lineText)
-                        tooltip |> Option.iter(fun (tooltip, line) ->
+                        tooltip |> Option.iter(fun (tooltip, lineNr) ->
                             let text = extractSignature tooltip
-                            LoggingService.logDebug "Line %d - %s" line text
-                            let res, marker = markers.TryGetValue range.StartLine
-
-                            if res then 
-                                if marker.Text <> text then
-                                    document.RemoveMarker marker
-                                    markers.TryRemove(range.StartLine) |> ignore
-                                    addMarker text range.StartLine
-                            else
-                                addMarker text range.StartLine )
+                            LoggingService.logDebug "Line %d - %s" lineNr text
+                            //let res, marker = markers.TryGetValue range.StartLine
+                            //let line = editor.GetLine lineNr
+                            //|> Option.iter(fun marker ->
+                            match marker with
+                            | :? SignatureHelpMarker as sigMarker ->
+                                if sigMarker.Text <> text then
+                                     sigMarker.Text <- text
+                                     document.CommitLineUpdate lineNr
+                            | _ -> ())
+                            //else
+                                //addMarker text range.StartLine )
                     } |> Async.StartImmediate)
                 )
 
@@ -153,7 +187,7 @@ type SignatureHelp() =
         disposable <-
             Some
                 (Observable.merge x.Editor.VAdjustmentChanged x.DocumentContext.DocumentParsed
-                |> throttle (TimeSpan.FromMilliseconds 350.)
+                |> throttle (TimeSpan.FromMilliseconds 100.)
                 |> Observable.subscribe (fun _ -> signatureHelp.getUnusedOpens markers x.DocumentContext x.Editor data font))
 
     override x.Dispose() = disposable |> Option.iter (fun en -> en.Dispose ())
