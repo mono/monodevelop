d8efdcd767d08a813e56871d3284399b3ebf614c
Matt Ward <matt.ward@xamarin.com>
2016-11-23 17:20:53 +0000

    [NuGet] Allow Xamarin.Forms addin to use internal API
    Temporarily allow Xamarin.Forms addin to use internal API.

--- main/src/addins/MonoDevelop.PackageManagement/AssemblyInfo.cs
+++ main/src/addins/MonoDevelop.PackageManagement/AssemblyInfo.cs
@@ -11,3 +11,4 @@ using System.Runtime.CompilerServices;
 [assembly: InternalsVisibleTo ("MonoDevelop.PackageManagement.Cmdlets")]
 [assembly: InternalsVisibleTo ("MonoDevelop.PackageManagement.Extensions")]
 [assembly: InternalsVisibleTo ("MonoDevelop.PackageManagement.Tests")]
+[assembly: InternalsVisibleTo ("Xamarin.Forms.Addin")]
