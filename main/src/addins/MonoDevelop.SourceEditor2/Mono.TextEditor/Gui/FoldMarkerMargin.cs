2a16ed3d80b0e3914cccddfab55e5758e316bebb
Mike Krüger <mkrueger@xamarin.com>
2017-02-27 13:37:10 +0100

    [SourceEditor] Fixed possible null reference exception in fold marker margin.

--- main/src/addins/MonoDevelop.SourceEditor2/Mono.TextEditor/Gui/FoldMarkerMargin.cs
+++ main/src/addins/MonoDevelop.SourceEditor2/Mono.TextEditor/Gui/FoldMarkerMargin.cs
@@ -341,13 +341,15 @@ namespace Mono.TextEditor
 				startFoldings.Clear ();
 				containingFoldings.Clear ();
 				endFoldings.Clear ();
-				foreach (FoldSegment segment in editor.Document.GetFoldingContaining (lineSegment)) {
-					if (segment.GetStartLine (editor.Document)?.Offset == lineSegment.Offset) {
-						startFoldings.Add (segment);
-					} else if (segment.GetEndLine (editor.Document)?.Offset == lineSegment.Offset) {
-						endFoldings.Add (segment);
-					} else {
-						containingFoldings.Add (segment);
+				if (lineSegment != null) {
+					foreach (FoldSegment segment in editor.Document.GetFoldingContaining (lineSegment)) {
+						if (segment.GetStartLine (editor.Document)?.Offset == lineSegment.Offset) {
+							startFoldings.Add (segment);
+						} else if (segment.GetEndLine (editor.Document)?.Offset == lineSegment.Offset) {
+							endFoldings.Add (segment);
+						} else {
+							containingFoldings.Add (segment);
+						}
 					}
 				}
 				
