mono/monodevelop

commit ca287231cfe2d122fa55081964b6fbc3f6fae77a

Bug 41738 - The 'TextTemplatingFileGenerator' code generator crashed: Object reference not set to an instance of an object

main/src/addins/TextTemplating/Mono.TextTemplating/Mono.TextTemplating/TemplatingEngine.cs
@@ -173,24 +173,24 @@ namespace Mono.TextTemplating
 				pars.CompilerOptions = "/noconfig " + pars.CompilerOptions;
 			return settings.Provider.CompileAssemblyFromDom (pars, ccu);
 		}
-		
-		static HashSet<string> ProcessReferences (ITextTemplatingEngineHost host, ParsedTemplate pt, TemplateSettings settings)
+
+		static string [] ProcessReferences (ITextTemplatingEngineHost host, ParsedTemplate pt, TemplateSettings settings)
 		{
-			var resolved = new HashSet<string> ();
-			
+			var resolved = new Dictionary<string, string> ();
+
 			foreach (string assem in settings.Assemblies.Union (host.StandardAssemblyReferences)) {
-				if (resolved.Contains (assem))
+				if (resolved.Values.Contains (assem))
 					continue;
-				
+
 				string resolvedAssem = host.ResolveAssemblyReference (assem);
-				if (!string.IsNullOrEmpty (resolvedAssem)) {
-					resolved.Add (resolvedAssem);
+				if (!string.IsNullOrEmpty (resolvedAssem) && File.Exists (resolvedAssem)) {
+					resolved [AssemblyName.GetAssemblyName (resolvedAssem).FullName] = resolvedAssem;
 				} else {
 					pt.LogError ("Could not resolve assembly reference '" + assem + "'");
 					return null;
 				}
 			}
-			return resolved;
+			return resolved.Values.ToArray ();
 		}
 		
 		public static TemplateSettings GetSettings (ITextTemplatingEngineHost host, ParsedTemplate pt)

commit 6c3b5f18e2be03b405e8cedd2e930fa937a84d3f

Bug 52855 - Failure to resolve System.Core when executing a t4 script Problem started with ca287231cfe2d12 where I required resolving of full path of assembly to be able to remove duplicates by comparing FullName of assembly. Which works fine in case of running inside IDE but when TextTransform.exe is used engine doesn’t resolve to full path but leaves GAC assemblies unresolved, which is OK since compiler can resolve this just fine later in process.

main/src/addins/TextTemplating/Mono.TextTemplating/Mono.TextTemplating/TemplatingEngine.cs
@@ -183,8 +183,11 @@ namespace Mono.TextTemplating
 					continue;
 
 				string resolvedAssem = host.ResolveAssemblyReference (assem);
-				if (!string.IsNullOrEmpty (resolvedAssem) && File.Exists (resolvedAssem)) {
-					resolved [AssemblyName.GetAssemblyName (resolvedAssem).FullName] = resolvedAssem;
+				if (!string.IsNullOrEmpty (resolvedAssem)) {
+					var assemblyName = resolvedAssem;
+					if (File.Exists (resolvedAssem))
+						assemblyName = AssemblyName.GetAssemblyName (resolvedAssem).FullName;
+					resolved [assemblyName] = resolvedAssem;
 				} else {
 					pt.LogError ("Could not resolve assembly reference '" + assem + "'");
 					return null;