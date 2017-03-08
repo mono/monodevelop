d014fc7d7b00e4673b6d6f9448b64e1d0540e328
Vsevolod Kukol <sevoku@microsoft.com>
2017-03-08 17:23:36 +0100

    [Ide] Xwt driven SearchPopupWindow

--- main/src/addins/CSharpBinding/MonoDevelop.CSharp/ProjectSearchCategory.cs
+++ main/src/addins/CSharpBinding/MonoDevelop.CSharp/ProjectSearchCategory.cs
@@ -64,7 +64,7 @@ namespace MonoDevelop.CSharp
 			sortOrder = FirstCategory;
 		}
 
-		public override void Initialize (Components.PopoverWindow popupWindow)
+		public override void Initialize (Components.XwtPopup popupWindow)
 		{
 			lastResult = new WorkerResult ();
 		}
