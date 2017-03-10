65ff95bf37f4beec9b08258940b2a49350b0a346
Mike Krüger <mkrueger@xamarin.com>
2017-03-10 12:46:38 +0100

    Fixed 'Bug 53213 - completion window pops up for text files '

--- main/src/core/MonoDevelop.Ide/MonoDevelop.Ide.Editor.TextMate/TextMateCompletionTextEditorExtension.cs
+++ main/src/core/MonoDevelop.Ide/MonoDevelop.Ide.Editor.TextMate/TextMateCompletionTextEditorExtension.cs
@@ -117,9 +117,10 @@ namespace MonoDevelop.Ide.Editor.TextMate
 		{
 			if (inactive)
 				return await base.HandleCodeCompletionAsync (completionContext, triggerInfo, token);
-
-			if (!IdeApp.Preferences.EnableAutoCodeCompletion)
+			if (Editor.MimeType == "text/plain")
 				return null;
+			if (!IdeApp.Preferences.EnableAutoCodeCompletion)
+					return null;
 			if (triggerInfo.CompletionTriggerReason != CompletionTriggerReason.CharTyped)
 				return null;
 			char completionChar = triggerInfo.TriggerCharacter.Value;
@@ -149,6 +150,8 @@ namespace MonoDevelop.Ide.Editor.TextMate
 				result.Add (new CompletionData (word));	
 			}
 
+			if (result.Count == 0)
+				return null;
 			return result;
 		}
 	}
