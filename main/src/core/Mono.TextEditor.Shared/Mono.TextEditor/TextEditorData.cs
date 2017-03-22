39c7efc9862e1f1e1ea8315fe53e51aef667a781
Todd Grunke <toddgrun@microsoft.com>
2017-03-22 18:28:15 +0000

    Fix for https://bugzilla.xamarin.com/show_bug.cgi?id=53679
    The underlying issue here is that the tab->spaces conversion shouldn't be happnening while undo operations are occuring

--- main/src/core/Mono.TextEditor.Shared/Mono.TextEditor/TextEditorData.cs
+++ main/src/core/Mono.TextEditor.Shared/Mono.TextEditor/TextEditorData.cs
@@ -278,7 +278,7 @@ namespace Mono.TextEditor
 		{
 			caret.UpdateCaretPosition (e);
 
-			if (Options.TabsToSpaces && document.IsTextSet) {
+			if (Options.TabsToSpaces && document.IsTextSet && !document.IsInUndo) {
 				string tabReplacement = new string (' ', Options.TabSize);
 				var newChanges = new List<Microsoft.CodeAnalysis.Text.TextChange> ();
 				foreach (var change in e.TextChanges) {
