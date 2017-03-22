a14edc049b41b41cfc0b3641faf20520c996f361
nosami <jasonimison@gmail.com>
2017-03-22 17:31:12 +0000

    [F#] Remove unused F# options from GUI

--- main/external/fsharpbinding/MonoDevelop.FSharp.Gui/FSharpSettingsWidget.cs
+++ main/external/fsharpbinding/MonoDevelop.FSharp.Gui/FSharpSettingsWidget.cs
@@ -9,18 +9,8 @@ namespace MonoDevelop.FSharp.Gui
 			this.Build ();
 		}
 		
-		public Gtk.CheckButton CheckInteractiveUseDefault { get { return checkInteractiveUseDefault; } }
-    	public Gtk.Button ButtonBrowse { get { return buttonBrowse; } }
-    	public Gtk.Entry EntryArguments { get { return entryArguments; } }
 		public Gtk.CheckButton AdvanceLine { get { return advanceToNextLineCheckbox; } }
-	    public Gtk.Entry EntryPath { get { return entryPath; } }
-	    public Gtk.FontButton FontInteractive { get { return fontbutton1; } }
-		public Gtk.CheckButton MatchThemeCheckBox { get { return matchThemeCheckbox; } }
-		public Gtk.HBox ColorsHBox { get { return hbox7; } }
-	    public Gtk.ColorButton BaseColorButton { get { return baseColorButton; } }
-	    public Gtk.ColorButton TextColorButton { get { return textColorButton; } }
 		public Gtk.CheckButton CheckCompilerUseDefault { get { return checkCompilerUseDefault; } }
-        public Gtk.Button ButtonCompilerBrowse { get { return buttonCompilerBrowse; } }
 		public Gtk.Entry EntryCompilerPath { get { return entryCompilerPath; } }
 		public Gtk.CheckButton CheckHighlightMutables { get { return checkHighlightMutables; } }
 		
