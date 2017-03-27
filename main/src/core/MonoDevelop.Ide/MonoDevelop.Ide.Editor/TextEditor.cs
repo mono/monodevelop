mono/monodevelop

commit ffc89f1c0b40e7883269fa5035d70e925991d6bb

Have MD TextDocument use and expose a VS TextDocument.

main/src/core/MonoDevelop.Ide/MonoDevelop.Ide.Editor/TextEditor.cs
@@ -366,16 +366,6 @@ namespace MonoDevelop.Ide.Editor
 			}
 		}
 
-		public bool UseBOM {
-			get {
-				return ReadOnlyTextDocument.UseBOM;
-			}
-			set {
-				Runtime.AssertMainThread ();
-				ReadWriteTextDocument.UseBOM = value;
-			}
-		}
-
 		public Encoding Encoding {
 			get {
 				return ReadOnlyTextDocument.Encoding;

main/src/addins/MonoDevelop.SourceEditor2/MonoDevelop.SourceEditor/EditorFactory.cs
@@ -42,11 +42,15 @@ namespace MonoDevelop.SourceEditor
 			return new TextDocument ();
 		}
 
-		ITextDocument ITextEditorFactory.CreateNewDocument (MonoDevelop.Core.Text.ITextSource textSource, string fileName, string mimeType)
+        ITextDocument ITextEditorFactory.CreateNewDocument(string fileName, string mimeType)
+        {
+            return new TextDocument(fileName, mimeType);
+        }
+
+        ITextDocument ITextEditorFactory.CreateNewDocument (MonoDevelop.Core.Text.ITextSource textSource, string fileName, string mimeType)
 		{
 			return new TextDocument (textSource.Text) {
 				Encoding = textSource.Encoding,
-				UseBOM = textSource.UseBOM,
 				MimeType = mimeType,
 				FileName = fileName
 			};
@@ -56,7 +60,6 @@ namespace MonoDevelop.SourceEditor
 		{
 			return new TextDocument (textSource.Text) {
 				Encoding = textSource.Encoding,
-				UseBOM = textSource.UseBOM,
 				IsReadOnly = true,
 				MimeType = mimeType,
 				FileName = fileName
@@ -68,7 +71,12 @@ namespace MonoDevelop.SourceEditor
 			return new SourceEditorView ();
 		}
 
-		ITextEditorImpl ITextEditorFactory.CreateNewEditor (IReadonlyTextDocument document)
+        ITextEditorImpl ITextEditorFactory.CreateNewEditor(string fileName, string mimeType)
+        {
+            return new SourceEditorView(fileName, mimeType);
+        }
+
+        ITextEditorImpl ITextEditorFactory.CreateNewEditor (IReadonlyTextDocument document)
 		{
 			return new SourceEditorView (document);
 		}

main/src/addins/MonoDevelop.SourceEditor2/MonoDevelop.SourceEditor/SourceEditorView.cs
@@ -170,20 +170,55 @@ namespace MonoDevelop.SourceEditor
 			});
 		}
 
-		public SourceEditorView (IReadonlyTextDocument document = null)
-		{
-			Counters.LoadedEditors++;
-			TextDocument doc;
-			if (document != null) {
-				var textDocument = document as TextDocument;
-				if (textDocument != null) {
-					doc = textDocument;
-				} else {
-					doc = new TextDocument (document.Text);
-				}
-			} else {
-				doc = new TextDocument ();
-			}
+        bool loadedInCtor = false;
+
+        public SourceEditorView(string fileName, string mimeType)
+            : this(new TextDocument(fileName, mimeType))
+        {
+            FileRegistry.Add(this);
+
+            loadedInCtor = true;
+        }
+
+        public SourceEditorView(IReadonlyTextDocument document = null)
+            : this(CreateTextDocumentFromReadonlyTextDocument(document))
+        {
+            if (document != null)
+            {
+                Document.MimeType = document.MimeType;
+                Document.FileName = document.FileName;
+                Document_MimeTypeChanged(this, EventArgs.Empty);
+            }
+            FileRegistry.Add(this);
+        }
+
+        private static TextDocument CreateTextDocumentFromReadonlyTextDocument(IReadonlyTextDocument document)
+        {
+            TextDocument doc;
+            if (document != null)
+            {
+                var textDocument = document as TextDocument;
+                if (textDocument != null)
+                {
+                    doc = textDocument;
+                }
+                else
+                {
+                    // TODO create the new TextDocument without duplicating the underlying text.
+                    doc = new TextDocument(document.Text);
+                }
+            }
+            else
+            {
+                doc = new TextDocument();
+            }
+
+            return doc;
+        }
+
+        private SourceEditorView(TextDocument doc)
+        {
+            Counters.LoadedEditors++;
 
 			widget = new SourceEditorWidget (this, doc);
 
@@ -236,12 +271,6 @@ namespace MonoDevelop.SourceEditor
 			IdeApp.Preferences.DefaultHideMessageBubbles.Changed += HandleIdeAppPreferencesDefaultHideMessageBubblesChanged;
 			// Document.AddAnnotation (this);
 			widget.TextEditor.Document.MimeTypeChanged += Document_MimeTypeChanged;
-			if (document != null) {
-				Document.MimeType = document.MimeType;
-				Document.FileName = document.FileName;
-				Document_MimeTypeChanged (this, EventArgs.Empty);
-			}
-			FileRegistry.Add (this);
 		}
 
 		void Document_MimeTypeChanged (object sender, EventArgs e)
@@ -639,17 +668,16 @@ namespace MonoDevelop.SourceEditor
 		
 		public override Task Save (FileSaveInformation fileSaveInformation)
 		{
-			return Save (fileSaveInformation.FileName, fileSaveInformation.Encoding ?? encoding);
+			return Save (fileSaveInformation.FileName, fileSaveInformation.Encoding);
 		}
 
 		public async Task Save (string fileName, Encoding encoding)
 		{
 			if (widget.HasMessageBar)
 				return;
-			if (encoding != null) {
-				this.encoding = encoding;
-				UpdateTextDocumentEncoding ();
-			}
+            if (encoding != null) {
+                this.Document.VsTextDocument.Encoding = encoding;
+            }
 			if (ContentName != fileName) {
 				FileService.RequestFileEdit ((FilePath) fileName);
 				writeAllowed = true;
@@ -723,22 +751,7 @@ namespace MonoDevelop.SourceEditor
 					}
 				}
 				try {
-					var writeEncoding = encoding;
-					var writeBom = hadBom;
-					var writeText = ProcessSaveText (Document.Text);
-					if (writeEncoding == null) {
-						if (this.encoding != null) {
-							writeEncoding = this.encoding;
-						} else { 
-							writeEncoding = Encoding.UTF8;
-							// Disabled. Shows up in the source control as diff, it's atm confusing for the users to see a change without
-							// changed files.
-							writeBom = false;
-	//						writeBom =!Mono.TextEditor.Utils.TextFileUtility.IsASCII (writeText);
-						}
-					}
-					await MonoDevelop.Core.Text.TextFileUtility.WriteTextAsync (fileName, writeText, writeEncoding, writeBom);
-					this.encoding = writeEncoding;
+                    this.Document.VsTextDocument.Save();
 				} catch (InvalidEncodingException) {
 					var result = MessageService.AskQuestion (GettextCatalog.GetString ("Can't save file with current codepage."), 
 						GettextCatalog.GetString ("Some unicode characters in this file could not be saved with the current encoding.\nDo you want to resave this file as Unicode ?\nYou can choose another encoding in the 'save as' dialog."),
@@ -746,10 +759,10 @@ namespace MonoDevelop.SourceEditor
 						AlertButton.Cancel,
 						new AlertButton (GettextCatalog.GetString ("Save as Unicode")));
 					if (result != AlertButton.Cancel) {
-						hadBom = true;
-						this.encoding = Encoding.UTF8;
-						MonoDevelop.Core.Text.TextFileUtility.WriteText (fileName, Document.Text, encoding, hadBom);
-					} else {
+                        this.Document.VsTextDocument.Encoding = Encoding.UTF8;
+                        this.Document.VsTextDocument.Save();
+                    }
+                    else {
 						return;
 					}
 				}
@@ -809,12 +822,6 @@ namespace MonoDevelop.SourceEditor
 			return text;
 		}
 
-		void UpdateTextDocumentEncoding ()
-		{
-			widget.Document.Encoding = encoding;
-			widget.Document.UseBOM = hadBom;
-		}
-
 		class MyExtendingLineMarker : TextLineMarker, IExtendingTextLineMarker
 		{
 			public bool IsSpaceAbove {
@@ -833,12 +840,12 @@ namespace MonoDevelop.SourceEditor
 			}
 		}
 
-		public Task Load (string fileName, Encoding loadEncoding, bool reload = false)
-		{
+        public Task Load(string fileName, Encoding loadEncoding, bool reload = false)
+        {
 			var document = Document;
-			if (document == null)
-				return TaskUtil.Default<object> ();
-			document.TextChanged -= OnTextReplaced;
+            if (document == null)
+                return TaskUtil.Default<object>();
+            document.TextChanged -= OnTextReplaced;
 			
 			if (warnOverwrite) {
 				warnOverwrite = false;
@@ -846,31 +853,45 @@ namespace MonoDevelop.SourceEditor
 				WorkbenchWindow.ShowNotification = false;
 			}
 			// Look for a mime type for which there is a syntax mode
-			UpdateMimeType (fileName);
-			string text = null;
 			bool didLoadCleanly;
 			if (!reload && AutoSave.AutoSaveExists (fileName)) {
 				widget.ShowAutoSaveWarning (fileName);
-				encoding = loadEncoding;
+				this.Document.VsTextDocument.Encoding = loadEncoding;
 				didLoadCleanly = false;
 			}
 			else {
-				if (loadEncoding == null) {
-					text = MonoDevelop.Core.Text.TextFileUtility.ReadAllText (fileName, out hadBom, out encoding);
-				} else {
-					encoding = loadEncoding;
-					text = MonoDevelop.Core.Text.TextFileUtility.ReadAllText (fileName, loadEncoding, out hadBom);
-				}
-				text = ProcessLoadText (text);
-				if (reload) {
-					document.ReplaceText (0, Document.Length, text);
-					document.DiffTracker.Reset ();
-				} else {
-					document.Text = text;
-					document.DiffTracker.SetBaseDocument (Document.CreateDocumentSnapshot ());
-				}
-				didLoadCleanly = true;
+                if (!this.loadedInCtor)
+                {
+                    UpdateMimeType(fileName);
+
+                     string text = null;
+                     bool hadBom;
+                     if (loadEncoding == null)
+                     {
+                         text = MonoDevelop.Core.Text.TextFileUtility.ReadAllText(fileName, out hadBom, out loadEncoding);
+                     }
+                     else
+                     {
+                         text = MonoDevelop.Core.Text.TextFileUtility.ReadAllText(fileName, loadEncoding, out hadBom);
+                     }
+                     this.Document.VsTextDocument.Encoding = loadEncoding;
+                     
+                     text = ProcessLoadText(text);
+                     if (reload)
+                     {
+                         document.ReplaceText(0, Document.Length, text);
+                         document.DiffTracker.Reset();
+                     }
+                     else
+                     {
+                         document.Text = text;
+                         document.DiffTracker.SetBaseDocument(Document.CreateDocumentSnapshot());
+                     }
+                }
+                didLoadCleanly = true;
 			}
+
+            this.loadedInCtor = false;
 			// TODO: Would be much easier if the view would be created after the containers.
 			ContentName = fileName;
 			lastSaveTimeUtc = File.GetLastWriteTimeUtc (ContentName);
@@ -885,13 +906,12 @@ namespace MonoDevelop.SourceEditor
 			if (didLoadCleanly) {
 				widget.EnsureCorrectEolMarker (fileName);
 			}
-			UpdateTextDocumentEncoding ();
 
 			document.TextChanged += OnTextReplaced;
-			return TaskUtil.Default<object> ();
-		}
-		
-		void HandleTextEditorVAdjustmentChanged (object sender, EventArgs e)
+            return TaskUtil.Default<object>();
+        }
+
+        void HandleTextEditorVAdjustmentChanged (object sender, EventArgs e)
 		{
 			widget.TextEditor.TextArea.SizeAllocated -= HandleTextEditorVAdjustmentChanged;
 			LoadSettings ();
@@ -931,8 +951,6 @@ namespace MonoDevelop.SourceEditor
 		}
 
 		bool warnOverwrite = false;
-		Encoding encoding;
-		bool hadBom = true;
 
 		internal void ReplaceContent (string fileName, string content, Encoding enc)
 		{
@@ -945,14 +963,13 @@ namespace MonoDevelop.SourceEditor
 			
 			Document.ReplaceText (0, Document.Length, content);
 			Document.DiffTracker.Reset ();
-			encoding = enc;
 			ContentName = fileName;
 			UpdateExecutionLocation ();
 			UpdateBreakpoints ();
 			UpdatePinnedWatches ();
 			LoadExtensions ();
 			IsDirty = false;
-			UpdateTextDocumentEncoding ();
+            this.Document.VsTextDocument.Encoding = enc;
 			InformLoadComplete ();
 		}
 	
@@ -962,7 +979,7 @@ namespace MonoDevelop.SourceEditor
 		}
 		
 		public Encoding SourceEncoding {
-			get { return encoding; }
+			get { return this.Document.VsTextDocument.Encoding; }
 		}
 
 		public override void Dispose ()

main/src/core/Mono.TextEditor.Platform/PlatformCatalog.cs
@@ -114,6 +114,9 @@ namespace Microsoft.VisualStudio.Platform
         internal ITextBufferFactoryService _textBufferFactoryService { get; private set; }
 
         [Import]
+        internal ITextDocumentFactoryService TextDocumentFactoryService { get; private set; }
+
+        [Import]
         internal ITextEditorFactoryService TextEditorFactoryService { get; private set; }
 
         [Import]
@@ -207,6 +210,190 @@ namespace Microsoft.VisualStudio.Platform
         }
 
         private Tuple<ImmutableDictionary<string, IContentType>, ImmutableDictionary<IContentType, string>> maps = Tuple.Create(ImmutableDictionary<string, IContentType>.Empty, ImmutableDictionary<IContentType, string>.Empty);
+    }
+
+    // Fold back into Text.Def.TextData.TextSnapshotToTextReader
+    public sealed class NewTextSnapshotToTextReader : TextReader
+    {
+        #region TextReader methods
+        /// <summary>
+        /// Closes the reader and releases any associated system resources.
+        /// </summary>
+        public override void Close()
+        {
+            _currentPosition = -1;
+            base.Close();
+        }
+
+        /// <summary>
+        /// Releases all resources used by the reader.
+        /// </summary>
+        /// <param name="disposing">Whether to release managed resources.</param>
+        protected override void Dispose(bool disposing)
+        {
+            _currentPosition = -1;
+            base.Dispose(disposing);
+        }
+
+        /// <summary>
+        /// Returns the next character without changing the state of the reader or the
+        /// character source.
+        /// </summary>
+        /// <returns>The next character to be read, or -1 if no more characters are available or the stream does not support seeking.</returns>
+        /// <exception cref="ObjectDisposedException">The reader is closed.</exception>
+        public override int Peek()
+        {
+            if (_currentPosition == -1)
+                throw new ObjectDisposedException("TextSnapshotToTextReader");
+
+            return (_currentPosition == _end) ? -1 : (int)(_snapshot[_currentPosition]);
+        }
+
+        /// <summary>
+        /// Reads the next character from the input stream and advances the character
+        /// position by one character.
+        /// </summary>
+        /// <returns>The next character from the input stream, or -1 if no more characters are available.</returns>
+        /// <exception cref="ObjectDisposedException">The reader is closed.</exception>
+        public override int Read()
+        {
+            if (_currentPosition == -1)
+                throw new ObjectDisposedException("TextSnapshotToTextReader");
+
+            return (_currentPosition == _end) ? -1 : (int)(_snapshot[_currentPosition++]);
+        }
+
+        /// <summary>
+        /// Reads the specified number of characters from the current stream and writes the
+        /// data to the buffer, beginning at the specified location.
+        /// </summary>
+        /// <param name="buffer">When this method returns, contains the specified character array from the current source.</param>
+        /// <param name="index">The place in buffer at which to begin writing.</param>
+        /// <param name="count">The maximum number of characters to read.</param>
+        /// <returns>The number of characters that have been read. The number will be less than
+        /// or equal to <paramref name="count"/>, depending on whether the data is available within the
+        /// stream. This method returns zero if called when no more characters are left to read.</returns>
+        /// <exception cref="ArgumentOutOfRangeException"><paramref name="index"/> or <paramref name="count"/> is negative, or
+        /// the buffer length minus index is less than <paramref name="count"/>.</exception>
+        /// <exception cref="ArgumentNullException"><paramref name="buffer"/> is null.</exception>
+        /// <exception cref="ObjectDisposedException">The reader is closed.</exception>
+        public override int Read(char[] buffer, int index, int count)
+        {
+            if (_currentPosition == -1)
+                throw new ObjectDisposedException("TextSnapshotToTextReader");
+            if (buffer == null)
+                throw new ArgumentNullException("buffer");
+            if (index < 0)
+                throw new ArgumentOutOfRangeException("index");
+            if (count < 0)
+                throw new ArgumentOutOfRangeException("count");
+            if (((index + count) < 0) || ((index + count) > buffer.Length))
+                throw new ArgumentOutOfRangeException("count");
+
+            int charactersToRead = System.Math.Min(_end - _currentPosition, count);
+            _snapshot.CopyTo(_currentPosition, buffer, index, charactersToRead);
+            _currentPosition += charactersToRead;
+
+            return charactersToRead;
+        }
+
+        /// <summary>
+        /// Reads a maximum of <paramref name="count"/> characters from the current stream and writes the
+        /// data to buffer, beginning at index.
+        /// </summary>
+        /// <param name="buffer">When this method returns, contains the specified character array from the current source.</param>
+        /// <param name="index">The place in buffer at which to begin writing.</param>
+        /// <param name="count">The maximum number of characters to read.</param>
+        /// <returns>The number of characters that have been read. The number will be less than
+        /// or equal to <paramref name="count"/>, depending on whether the data is available within the
+        /// stream. This method returns zero if called when no more characters are left to read.</returns>
+        /// <exception cref="ArgumentOutOfRangeException"><paramref name="index"/> or <paramref name="count"/> is negative, or
+        /// the buffer length minus index is less than <paramref name="count"/>.</exception>
+        /// <exception cref="ArgumentNullException"><paramref name="buffer"/> is null.</exception>
+        /// <exception cref="ObjectDisposedException">The reader is closed.</exception>
+        public override int ReadBlock(char[] buffer, int index, int count)
+        {
+            return Read(buffer, index, count);
+        }
+
+        /// <summary>Reads a line of characters from the current stream and returns the data as a string.</summary>
+        /// <returns>The next line from the input stream, or null if all characters have been read.</returns>
+        /// <exception cref="ObjectDisposedException">The <see cref="TextReader"/> is closed.</exception>
+        public override string ReadLine()
+        {
+            if (_currentPosition == -1)
+                throw new ObjectDisposedException("TextSnapshotToTextReader");
+
+            if (_readLastLine)
+                return null;
+
+            ITextSnapshotLine line = _snapshot.GetLineFromPosition(_currentPosition);
+
+            //Handle the case where the current position is between a \r\n without crashing (but returning an empty string instead).
+            string text = (line.End > _currentPosition)
+                          ? _snapshot.GetText(_currentPosition, line.End - _currentPosition)
+                          : string.Empty;
+
+            _currentPosition = line.EndIncludingLineBreak;
+
+            if (_currentPosition == _end)
+            {
+                //Do not read the last line in the buffer unless it contains some text.
+                _readLastLine = true;
+            }
+
+            return text;
+        }
+
+        /// <summary>Reads all the characters from the current position to the end of the reader and returns them as a string.</summary>
+        /// <returns>A string containing all the characters from the current position to the end of the reader.</returns>
+        /// <exception cref="ObjectDisposedException">The <see cref="TextReader"/> is closed.</exception>
+        public override string ReadToEnd()
+        {
+            if (_currentPosition == -1)
+                throw new ObjectDisposedException("TextSnapshotToTextReader");
+
+            string text = _snapshot.GetText(_currentPosition, _end - _currentPosition);
+            _currentPosition = _end;
+
+            return text;
+        }
+        #endregion
+
+        /// <summary>
+        /// Initializes a new instance of <see cref="TextSnapshotToTextReader"/> with the specified text snapshot.
+        /// </summary>
+        /// <param name="textSnapshot">The <see cref="ITextSnapshot"/> to expose as a reader.</param>
+        /// <exception cref="ArgumentNullException"><paramref name="textSnapshot"/> is null.</exception>
+        public NewTextSnapshotToTextReader(ITextSnapshot textSnapshot)
+        {
+            if (textSnapshot == null)
+                throw new ArgumentNullException("textSnapshot");
+
+            _snapshot = textSnapshot;
+            _end = textSnapshot.Length;
+        }
+
+        public NewTextSnapshotToTextReader(ITextSnapshot textSnapshot, int offset, int length)
+        {
+            if (textSnapshot == null)
+                throw new ArgumentNullException("textSnapshot");
+            if ((offset < 0) || (offset > textSnapshot.Length))
+                throw new ArgumentOutOfRangeException("offset");
+            int end = offset + length;
+            if ((end < offset) || (end > textSnapshot.Length))
+                throw new ArgumentOutOfRangeException("length");
+
+            _snapshot = textSnapshot;
+            _currentPosition = offset;
+            _end = length;
+        }
+
+        ITextSnapshot _snapshot;
+        int _currentPosition;
+        bool _readLastLine;
+
+        int _end;
     }
 
 #if false

main/src/core/Mono.TextEditor.Shared/Mono.TextEditor.Utils/ImmutableText.cs
@@ -92,7 +92,7 @@ namespace Mono.TextEditor.Utils
 
 		static readonly LeafNode EMPTY_NODE = new Leaf8BitNode (new byte [0]);
 
-		public static readonly ImmutableText Empty = new ImmutableText (EMPTY_NODE, true, null);
+		public static readonly ImmutableText Empty = new ImmutableText (EMPTY_NODE, null);
 
 		readonly Node root;
 
@@ -107,11 +107,6 @@ namespace Mono.TextEditor.Utils
 		}
 
 		public ITextSourceVersion Version {
-			get;
-			internal set;
-		}
-
-		public bool UseBOM {
 			get;
 			internal set;
 		}
@@ -152,10 +147,9 @@ namespace Mono.TextEditor.Utils
 			}
 		}
 
-		ImmutableText (Node node, bool useBom, Encoding encoding)
+		ImmutableText (Node node, Encoding encoding)
 		{
 			root = node;
-			UseBOM = useBom;
 			this.encoding = encoding;
 		}
 
@@ -179,7 +173,7 @@ namespace Mono.TextEditor.Utils
 		/// <returns><code>this + that</code></returns>
 		public ImmutableText Concat (ImmutableText that)
 		{
-			return that.Length == 0 ? this : Length == 0 ? that : new ImmutableText (ConcatNodes (EnsureChunked ().root, that.EnsureChunked ().root), UseBOM, encoding);
+			return that.Length == 0 ? this : Length == 0 ? that : new ImmutableText (ConcatNodes (EnsureChunked ().root, that.EnsureChunked ().root), encoding);
 		}
 
 		/// <summary>
@@ -231,7 +225,7 @@ namespace Mono.TextEditor.Utils
 				return Empty;
 			}
 
-			return new ImmutableText (root.SubNode (start, end), UseBOM, encoding);
+			return new ImmutableText (root.SubNode (start, end), encoding);
 		}
 
 		/// <summary>
@@ -428,7 +422,7 @@ namespace Mono.TextEditor.Utils
 		ImmutableText EnsureChunked ()
 		{
 			if (Length > BLOCK_SIZE && root is LeafNode) {
-				return new ImmutableText (NodeOf ((LeafNode)root, 0, Length), UseBOM, encoding);
+				return new ImmutableText (NodeOf ((LeafNode)root, 0, Length), encoding);
 			}
 			return this;
 		}

commit 3f7de6ccbe39f8519f9dd8b1dbb81de4c04659b6

Fix tooltips in editor Bug was caused in ffc89f1c0 with changes in main/src/core/MonoDevelop.Ide/MonoDevelop.Ide.Editor/TextEditorDisplayBinding.cs file which changed to set MimeType during construction instead after construction which resulted in MimeTypeChanged event not being triggered hence tooltip providers not set, hence no tooltips

main/src/core/MonoDevelop.Ide/MonoDevelop.Ide.Editor/TextEditor.cs
@@ -1036,6 +1036,7 @@ namespace MonoDevelop.Ide.Editor
 
 			FileNameChanged += TextEditor_FileNameChanged;
 			MimeTypeChanged += TextEditor_MimeTypeChanged;
+			TextEditor_MimeTypeChanged (null, null);
 		}
 
 		void TextEditor_FileNameChanged (object sender, EventArgs e)