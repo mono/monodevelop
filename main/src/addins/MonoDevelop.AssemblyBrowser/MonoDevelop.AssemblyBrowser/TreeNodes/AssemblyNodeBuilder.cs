bf96965dc41fb6e7bf0cce54d8415685a9d01ce0
Mike Krüger <mkrueger@xamarin.com>
2017-03-30 15:23:27 +0200

    Fixed 'Bug 43741 - Double clicking on search result or reference doesn't do anything for things without a namespace'
    There wasn't a reason that global types were put into the '-'
    namespace now they're ordered in the tree at the right position so the
    find algorithm can find them properly.

--- main/src/addins/MonoDevelop.AssemblyBrowser/MonoDevelop.AssemblyBrowser/TreeNodes/AssemblyNodeBuilder.cs
+++ main/src/addins/MonoDevelop.AssemblyBrowser/MonoDevelop.AssemblyBrowser/TreeNodes/AssemblyNodeBuilder.cs
@@ -82,7 +82,7 @@ namespace MonoDevelop.AssemblyBrowser
 			bool publicOnly = Widget.PublicApiOnly;
 			
 			foreach (var type in compilationUnit.UnresolvedAssembly.TopLevelTypeDefinitions) {
-				string namespaceName = string.IsNullOrEmpty (type.Namespace) ? "-" : type.Namespace;
+				string namespaceName = string.IsNullOrEmpty (type.Namespace) ? "" : type.Namespace;
 				if (!namespaces.ContainsKey (namespaceName))
 					namespaces [namespaceName] = new Namespace (namespaceName);
 				
@@ -90,7 +90,14 @@ namespace MonoDevelop.AssemblyBrowser
 				ns.Types.Add (type);
 			}
 
-			treeBuilder.AddChildren (namespaces.Values.Where (ns => !publicOnly || ns.Types.Any (t => t.IsPublic)));
+			treeBuilder.AddChildren (namespaces.Where (ns => ns.Key != "" && (!publicOnly || ns.Value.Types.Any (t => t.IsPublic))).Select (n => n.Value));
+			if (namespaces.ContainsKey ("")) {
+				foreach (var child in namespaces [""].Types) {
+					if (child.Name == "<Module>")
+						continue;
+					treeBuilder.AddChild (child);
+				}
+			}
 		}
 		
 		public override bool HasChildNodes (ITreeBuilder builder, object dataObject)
