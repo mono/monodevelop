80e9b2b8e63699e82981d678d22e00b6125d3c48
Lluis Sanchez <llsan@microsoft.com>
2017-04-25 17:10:40 +0200

    Revert "[Core] Optimize OnGetSourceFiles"
    This reverts commit 7369b8e0dff63c2d10f07b27460c442e7caff8b7.
    
    This commit broke several unit tests.

--- main/src/core/MonoDevelop.Core/MonoDevelop.Projects/Project.cs
+++ main/src/core/MonoDevelop.Core/MonoDevelop.Projects/Project.cs
@@ -549,15 +549,8 @@ namespace MonoDevelop.Projects
 
 			var buildActions = GetBuildActions ().Where (a => a != "Folder" && a != "--").ToArray ();
 
-			MSBuildProjectInstance pri = null;
-			var config = configuration != null ? (ProjectConfiguration)GetConfiguration (configuration) : null;
-			if (config != null) {
-				pri = config.ProjectInstance;
-			}
-
-			if (pri == null)
-				pri = await CreateProjectInstaceForConfigurationAsync (config?.Name, config?.Platform, false);
-
+			var config = configuration != null ? GetConfiguration (configuration) : null;
+			var pri = await CreateProjectInstaceForConfigurationAsync (config?.Name, config?.Platform, false);
 			foreach (var it in pri.EvaluatedItems.Where (i => buildActions.Contains (i.Name)))
 				results.Add (CreateProjectFile (it));
 
