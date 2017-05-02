539c17dc90c3b8b0763f32a75800b3e8f3a64aa5
Mikayla Hutchinson <m.j.hutchinson@gmail.com>
2017-05-01 20:23:56 -0400

    Add Azure Functions extension

---
+++ main/src/addins/MonoDevelop.AzureFunctions/Properties/AddinInfo.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using Mono.Addins;
+using Mono.Addins.Description;
+
+[assembly: Addin ("AzureFunctions",
+	Namespace = "MonoDevelop",
+	Version = MonoDevelop.BuildInfo.Version,
+	Category = "Ide")]
+
+[assembly: AddinName ("Azure Functions")]
+[assembly: AddinDescription ("Azure Functions")]
+
+[assembly: AddinDependency ("Core", MonoDevelop.BuildInfo.Version)]
+[assembly: AddinDependency ("Ide", MonoDevelop.BuildInfo.Version)]
+[assembly: AddinDependency ("DotNetCore", MonoDevelop.BuildInfo.Version)]
