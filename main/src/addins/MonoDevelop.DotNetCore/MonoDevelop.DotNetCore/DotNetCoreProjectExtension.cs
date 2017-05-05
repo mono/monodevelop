d2c8128794a963345c72d9c2ba0a15c6701f537d
Mikayla Hutchinson <m.j.hutchinson@gmail.com>
2017-05-05 00:52:07 -0400

    [DotNetCore] Allow Sdk projects to run on other runtimes
    Maybe we should split this into two extensions, one handling Sdk style
    projects and one handling .NET Core execution.

--- main/src/addins/MonoDevelop.DotNetCore/MonoDevelop.DotNetCore/DotNetCoreProjectExtension.cs
+++ main/src/addins/MonoDevelop.DotNetCore/MonoDevelop.DotNetCore/DotNetCoreProjectExtension.cs
@@ -130,7 +130,10 @@ namespace MonoDevelop.DotNetCore
 
 		protected override ExecutionCommand OnCreateExecutionCommand (ConfigurationSelector configSel, DotNetProjectConfiguration configuration, ProjectRunConfiguration runConfiguration)
 		{
-			return CreateDotNetCoreExecutionCommand (configSel, configuration, runConfiguration);
+			if (Project.TargetFramework.IsNetCoreApp ()) {
+				return CreateDotNetCoreExecutionCommand (configSel, configuration, runConfiguration);
+			}
+			return base.OnCreateExecutionCommand (configSel, configuration, runConfiguration);
 		}
 
 		DotNetCoreExecutionCommand CreateDotNetCoreExecutionCommand (ConfigurationSelector configSel, DotNetProjectConfiguration configuration, ProjectRunConfiguration runConfiguration)
@@ -188,7 +191,7 @@ namespace MonoDevelop.DotNetCore
 
 		protected override Task OnExecute (ProgressMonitor monitor, ExecutionContext context, ConfigurationSelector configuration, SolutionItemRunConfiguration runConfiguration)
 		{
-			if (DotNetCoreRuntime.IsMissing) {
+			if (Project.TargetFramework.IsNetCoreApp () && DotNetCoreRuntime.IsMissing) {
 				return ShowCannotExecuteDotNetCoreApplicationDialog ();
 			}
 
@@ -226,7 +229,15 @@ namespace MonoDevelop.DotNetCore
 			return Project.ParentSolution.ExtendedProperties.Contains (ShownDotNetCoreSdkInstalledExtendedPropertyName);
 		}
 
-		protected override async Task OnExecuteCommand (ProgressMonitor monitor, ExecutionContext context, ConfigurationSelector configuration, ExecutionCommand executionCommand)
+		protected override Task OnExecuteCommand (ProgressMonitor monitor, ExecutionContext context, ConfigurationSelector configuration, ExecutionCommand executionCommand)
+		{
+			if (Project.TargetFramework.IsNetCoreApp ()) {
+				return OnExecuteDotNetCoreCommand (monitor, context, configuration, executionCommand);
+			}
+			return base.OnExecuteCommand (monitor, context, configuration, executionCommand);
+		}
+
+		async Task OnExecuteDotNetCoreCommand (ProgressMonitor monitor, ExecutionContext context, ConfigurationSelector configuration, ExecutionCommand executionCommand)
 		{
 			bool externalConsole = false;
 			bool pauseConsole = false;
