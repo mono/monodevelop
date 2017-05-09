538d12fd59f5e741242121182a135ad85e71a9e2
Marius Ungureanu <therzok@gmail.com>
2017-05-09 03:13:16 +0300

    [C#] Initial stub for ParameterHintingData via SignatureHelp

---
+++ main/src/addins/CSharpBinding/MonoDevelop.CSharp.Features/ParameterHinting/SignatureHelpParameterHintingData.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ICSharpCode.NRefactory6.CSharp.Completion;
+using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.SignatureHelp;
+
+namespace CSharpBinding.MonoDevelop.CSharp.Features.ParameterHinting
+{
+
+	class SignatureHelpParameterHintingData : IParameterHintingData
+	{
+		SignatureHelpItem item;
+
+		public SignatureHelpParameterHintingData(SignatureHelpItem item)
+		{
+			this.item = item;
+		}
+
+		public ISymbol Symbol => null;
+
+		public SignatureHelpItem Item => item;
+
+		public int ParameterCount => item.Parameters.Length;
+
+		public bool IsParameterListAllowed => item.IsVariadic;
+
+		public string GetParameterName (int currentParameter)
+		{
+			return item.Parameters[currentParameter].Name;
+		}
+	}
+}
