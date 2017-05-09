e31ab83d39038cf10b14b0eedd8b38e53c6983ec
Marius Ungureanu <therzok@gmail.com>
2017-05-09 05:02:20 +0300

    [Test] Print out labels when running tests on Windows.

--- main/wintest.sh
+++ main/wintest.sh
@@ -1,15 +1,15 @@
 if [ $# -lt 1 ]; then
-	build/bin/mdtool run-md-tests build/tests/UnitTests.dll
-	build/bin/mdtool run-md-tests build/tests/*.Tests.dll
+	build/bin/mdtool run-md-tests -labels build/tests/UnitTests.dll
+	build/bin/mdtool run-md-tests -labels build/tests/*.Tests.dll
 	if [ ! -f "external/nrefactory/bin/Debug/ICSharpCode.NRefactory.Tests.dll" ]; then
-		build/bin/mdtool run-md-tests external/nrefactory/bin/Debug/ICSharpCode.NRefactory.Tests.dll
+		build/bin/mdtool run-md-tests -labels external/nrefactory/bin/Debug/ICSharpCode.NRefactory.Tests.dll
 	else
-		build/bin/mdtool run-md-tests external/nrefactory/bin/Release/ICSharpCode.NRefactory.Tests.dll
+		build/bin/mdtool run-md-tests -labels external/nrefactory/bin/Release/ICSharpCode.NRefactory.Tests.dll
 	fi
 	if [ ! -f "external/fsharpbinding/MonoDevelop.FSharp.Tests/bin/Debug/MonoDevelop.FSharp.Tests.dll" ]; then
-		build/bin/mdtool run-md-tests external/fsharpbinding/MonoDevelop.FSharp.Tests/bin/Debug/MonoDevelop.FSharp.Tests.dll
+		build/bin/mdtool run-md-tests -labels external/fsharpbinding/MonoDevelop.FSharp.Tests/bin/Debug/MonoDevelop.FSharp.Tests.dll
 	else
-		build/bin/mdtool run-md-tests external/fsharpbinding/MonoDevelop.FSharp.Tests/bin/Release/MonoDevelop.FSharp.Tests.dll
+		build/bin/mdtool run-md-tests -labels external/fsharpbinding/MonoDevelop.FSharp.Tests/bin/Release/MonoDevelop.FSharp.Tests.dll
 	fi
 else
 	for arg in $@; do
@@ -28,7 +28,7 @@ else
 		fi
 
 		if [ -f $arg ]; then
-			(build/bin/mdtool run-md-tests $arg) || exit $?
+			(build/bin/mdtool run-md-tests -labels $arg) || exit $?
 		fi
 	done
 fi
