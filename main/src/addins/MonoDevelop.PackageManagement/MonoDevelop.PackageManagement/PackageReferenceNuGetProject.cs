b39da95db6dba92ce01d0db42c5ba188d9a41154
Matt Ward <matt.ward@xamarin.com>
2017-05-21 13:20:24 +0100

    [NuGet] Support non .NET Core projects using PackageReference
    Non .NET Core Sdk style projects that use PackageReference in their
    MSBuild project file are now supported. Previously the Solution
    window would not show any packages for the project, would not allow
    restoring, and would install new NuGet packages using a packages.config
    file. Now if the project has a PackageReference the Packages folder
    shows the installed packages and can be used to update or install
    NuGet packages which will also create PackageReference MSBuild items.
    If the project has no PackageReferences then by default a
    packages.config file will be created. There is no UI option to opt-in
    to using PackageReferences currently so the project file will need
    to be edited to include at least one PackageReference before the
    default behaviour of using a packages.config file is overridden.

---
+++ main/src/addins/MonoDevelop.PackageManagement/MonoDevelop.PackageManagement/PackageReferenceNuGetProject.cs
@@ -0,0 +1,304 @@
+﻿//
+// PackageReferenceNuGetProject.cs
+//
+// Author:
+//       Matt Ward <matt.ward@xamarin.com>
+//
+// Copyright (c) 2016 Xamarin Inc. (http://xamarin.com)
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in
+// all copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+// THE SOFTWARE.
+
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using MonoDevelop.Core;
+using MonoDevelop.Projects;
+using NuGet.Frameworks;
+using NuGet.PackageManagement;
+using NuGet.Packaging;
+using NuGet.Packaging.Core;
+using NuGet.ProjectManagement;
+using NuGet.ProjectManagement.Projects;
+using NuGet.ProjectModel;
+using NuGet.Versioning;
+
+namespace MonoDevelop.PackageManagement
+{
+	/// <summary>
+	/// Supports projects that use PackageReference MSBuild items but do not use a SDK style
+	/// project used by .NET Core projects.
+	/// </summary>
+	class PackageReferenceNuGetProject : BuildIntegratedNuGetProject, IBuildIntegratedNuGetProject, IHasDotNetProject
+	{
+		DotNetProject project;
+		IPackageManagementEvents packageManagementEvents;
+		string msbuildProjectPath;
+		string projectName;
+
+		public PackageReferenceNuGetProject (DotNetProject project)
+			: this (project, PackageManagementServices.PackageManagementEvents)
+		{
+		}
+
+		public PackageReferenceNuGetProject (
+			DotNetProject project,
+			IPackageManagementEvents packageManagementEvents)
+		{
+			this.project = project;
+			this.packageManagementEvents = packageManagementEvents;
+
+			var targetFramework = NuGetFramework.Parse (project.TargetFramework.Id.ToString ());
+
+			InternalMetadata.Add (NuGetProjectMetadataKeys.TargetFramework, targetFramework);
+			InternalMetadata.Add (NuGetProjectMetadataKeys.Name, project.Name);
+			InternalMetadata.Add (NuGetProjectMetadataKeys.FullPath, project.BaseDirectory);
+
+			msbuildProjectPath = project.FileName;
+			projectName = project.Name;
+		}
+
+		internal DotNetProject DotNetProject {
+			get { return project; }
+		}
+
+		public override string ProjectName {
+			get { return projectName; }
+		}
+
+		public override string MSBuildProjectPath {
+			get { return msbuildProjectPath; }
+		}
+
+		public static NuGetProject Create (DotNetProject project)
+		{
+			if (project.Items.OfType<ProjectPackageReference> ().Any ())
+				return new PackageReferenceNuGetProject (project);
+
+			return null;
+		}
+
+		public virtual Task SaveProject ()
+		{
+			return project.SaveAsync (new ProgressMonitor ());
+		}
+
+		public override Task<IEnumerable<PackageReference>> GetInstalledPackagesAsync (CancellationToken token)
+		{
+			return Task.FromResult (GetPackageReferences ());
+		}
+
+		IEnumerable<PackageReference> GetPackageReferences ()
+		{
+			return project.Items.OfType<ProjectPackageReference> ()
+				.Select (projectItem => projectItem.CreatePackageReference ())
+				.ToList ();
+		}
+
+		public async override Task<bool> InstallPackageAsync (
+			string packageId,
+			VersionRange range,
+			INuGetProjectContext nuGetProjectContext,
+			BuildIntegratedInstallationContext installationContext,
+			CancellationToken token)
+		{
+			var packageIdentity = new PackageIdentity (packageId, range.MinVersion);
+
+			bool added = await Runtime.RunInMainThread (() => {
+				return AddPackageReference (packageIdentity, nuGetProjectContext);
+			});
+
+			if (added) {
+				await SaveProject ();
+			}
+
+			return added;
+		}
+
+		bool AddPackageReference (PackageIdentity packageIdentity, INuGetProjectContext context)
+		{
+			ProjectPackageReference packageReference = project.GetPackageReference (packageIdentity);
+			if (packageReference != null) {
+				context.Log (MessageLevel.Warning, GettextCatalog.GetString ("Package '{0}' already exists in project '{1}'", packageIdentity, project.Name));
+				return false;
+			}
+
+			RemoveExistingPackageReference (packageIdentity);
+
+			packageReference = ProjectPackageReference.Create (packageIdentity);
+			project.Items.Add (packageReference);
+
+			return true;
+		}
+
+		void RemoveExistingPackageReference (PackageIdentity packageIdentity)
+		{
+			ProjectPackageReference packageReference = project.GetPackageReference (packageIdentity, matchVersion: false);
+			if (packageReference != null) {
+				project.Items.Remove (packageReference);
+			}
+		}
+
+		public override async Task<bool> UninstallPackageAsync (
+			PackageIdentity packageIdentity,
+			INuGetProjectContext nuGetProjectContext,
+			CancellationToken token)
+		{
+			bool removed = await Runtime.RunInMainThread (() => {
+				return RemovePackageReference (packageIdentity, nuGetProjectContext);
+			});
+
+			if (removed) {
+				await SaveProject ();
+			}
+
+			return removed;
+		}
+
+		bool RemovePackageReference (PackageIdentity packageIdentity, INuGetProjectContext context)
+		{
+			ProjectPackageReference packageReference = project.GetPackageReference (packageIdentity, matchVersion: false);
+
+			if (packageReference == null) {
+				context.Log (MessageLevel.Warning, GettextCatalog.GetString ("Package '{0}' does not exist in project '{1}'", packageIdentity.Id, project.Name));
+				return false;
+			}
+
+			project.Items.Remove (packageReference);
+
+			return true;
+		}
+
+		public override Task<string> GetAssetsFilePathAsync ()
+		{
+			string assetsFilePath = project.GetNuGetAssetsFilePath ();
+			return Task.FromResult (assetsFilePath);
+		}
+
+		public override async Task<IReadOnlyList<PackageSpec>> GetPackageSpecsAsync (DependencyGraphCacheContext context)
+		{
+			PackageSpec existingPackageSpec = GetExistingProjectPackageSpec (context);
+			if (existingPackageSpec != null) {
+				return new [] { existingPackageSpec };
+			}
+
+			PackageSpec packageSpec = await CreateProjectPackageSpec (context);
+
+			if (context != null) {
+				AddToCache (context, packageSpec);
+			}
+
+			return new [] { packageSpec };
+		}
+
+		PackageSpec GetExistingProjectPackageSpec (DependencyGraphCacheContext context)
+		{
+			PackageSpec packageSpec = null;
+			if (context != null) {
+				if (context.PackageSpecCache.TryGetValue (MSBuildProjectPath, out packageSpec)) {
+					return packageSpec;
+				}
+			}
+			return packageSpec;
+		}
+
+		async Task<PackageSpec> CreateProjectPackageSpec (DependencyGraphCacheContext context)
+		{
+			PackageSpec packageSpec = await Runtime.RunInMainThread (() => CreateProjectPackageSpec (project, context));
+			return packageSpec;
+		}
+
+		static PackageSpec CreateProjectPackageSpec (DotNetProject project, DependencyGraphCacheContext context)
+		{
+			PackageSpec packageSpec = PackageSpecCreator.CreatePackageSpec (project, context.Logger);
+			return packageSpec;
+		}
+
+		void AddToCache (DependencyGraphCacheContext context, PackageSpec projectPackageSpec)
+		{
+			if (IsMissingFromCache (context, projectPackageSpec)) {
+				context.PackageSpecCache.Add (
+					projectPackageSpec.RestoreMetadata.ProjectUniqueName,
+					projectPackageSpec);
+			}
+		}
+
+		bool IsMissingFromCache (
+			DependencyGraphCacheContext context,
+			PackageSpec packageSpec)
+		{
+			PackageSpec ignore;
+			return !context.PackageSpecCache.TryGetValue (
+				packageSpec.RestoreMetadata.ProjectUniqueName,
+				out ignore);
+		}
+
+		public override Task<bool> ExecuteInitScriptAsync (
+			PackageIdentity identity,
+			string packageInstallPath,
+			INuGetProjectContext projectContext,
+			bool throwOnFailure)
+		{
+			// Not supported. This gets called for every NuGet package
+			// even if they do not have an init.ps1 so do not report this.
+			return Task.FromResult (false);
+		}
+
+		public override Task PostProcessAsync (INuGetProjectContext nuGetProjectContext, CancellationToken token)
+		{
+			Runtime.RunInMainThread (() => {
+				DotNetProject.NotifyModified ("References");
+			});
+
+			packageManagementEvents.OnFileChanged (project.GetNuGetAssetsFilePath ());
+
+			return base.PostProcessAsync (nuGetProjectContext, token);
+		}
+
+		public void OnBeforeUninstall (IEnumerable<NuGetProjectAction> actions)
+		{
+		}
+
+		public void OnAfterExecuteActions (IEnumerable<NuGetProjectAction> actions)
+		{
+		}
+
+		public void NotifyProjectReferencesChanged ()
+		{
+			Runtime.AssertMainThread ();
+
+			DotNetProject.RefreshProjectBuilder ();
+			DotNetProject.NotifyModified ("References");
+		}
+
+		/// <summary>
+		/// Always returns true so the project is re-evaluated after a restore.
+		/// This ensures any imports in the generated .nuget.g.targets are
+		/// re-evaluated. Without this custom MSBuild targets used by a NuGet package
+		/// that was restored into the local NuGet package cache would not be available
+		/// until the solution is closed and re-opened. Also handles the project file
+		/// being edited by hand and a new package reference being added that has a
+		/// custom MSBuild target.
+		/// </summary>
+		public bool ProjectRequiresReloadAfterRestore ()
+		{
+			return true;
+		}
+	}
+}
