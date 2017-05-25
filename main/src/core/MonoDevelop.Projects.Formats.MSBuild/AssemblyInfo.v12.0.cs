cc461bffe09dfd9ad5d524793ff1e89a3ecf3bc9
Mikayla Hutchinson <m.j.hutchinson@gmail.com>
2017-05-25 19:11:48 -0400

    [MSBuild] Remove unused builders

--- main/src/core/MonoDevelop.Projects.Formats.MSBuild/AssemblyInfo.v12.0.cs
+++
@@ -1,9 +0,0 @@
-[assembly: System.Reflection.AssemblyTitle ("MonoDevelop MSBuild 12.0 Builder")]
-
-namespace MonoDevelop.Projects.MSBuild
-{
-	static class MSBuildConsts
-	{
-		public const string Version = "12.0";
-	}
-}
--- main/src/core/MonoDevelop.Projects.Formats.MSBuild/AssemblyInfo.v14.0.cs
+++
@@ -1,9 +0,0 @@
-[assembly: System.Reflection.AssemblyTitle ("MonoDevelop MSBuild 14.0 Builder")]
-
-namespace MonoDevelop.Projects.MSBuild
-{
-	static class MSBuildConsts
-	{
-		public const string Version = "14.0";
-	}
-}
