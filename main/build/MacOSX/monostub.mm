b872cf5eb686a98eb92ed8fcada51d53b0f0cc7e
therzok <marius.ungureanu@xamarin.com>
2017-05-30 17:01:37 +0300

    [Mac] Fix loading xammac from app bundle.

--- main/build/MacOSX/monostub.mm
+++ main/build/MacOSX/monostub.mm
@@ -352,11 +352,13 @@ int main (int argc, char **argv)
 
 #if XM_REGISTRAR
 	libxammac = dlopen ("@loader_path/libxammac.dylib", RTLD_LAZY);
-
 	if (!libxammac) {
-		fprintf (stderr, "Failed to load libxammac.dylib: %s\n", dlerror ());
-		NSString *msg = @"This application requires Xamarin.Mac native library side-by-side.";
-		exit_with_message ((char *)[msg UTF8String], argv[0]);
+		libxammac = dlopen ("@loader_path/../Resources/lib/monodevelop/bin/libxammac.dylib", RTLD_LAZY);
+		if (!libxammac) {
+			fprintf (stderr, "Failed to load libxammac.dylib: %s\n", dlerror ());
+			NSString *msg = @"This application requires Xamarin.Mac native library side-by-side.";
+			exit_with_message ((char *)[msg UTF8String], argv[0]);
+		}
 	}
 
 	xamarin_create_classes_Xamarin_Mac ();
