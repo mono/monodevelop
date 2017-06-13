84f5a19ab5968dbabfd726da9e5f5ff16dfb85d6
nosami <jasonimison@gmail.com>
2017-06-13 10:25:40 +0100

    Fix indentation

--- main/external/fsharpbinding/MonoDevelop.FSharp.Gui/FSharpCompilerOptionsWidget.cs
+++ main/external/fsharpbinding/MonoDevelop.FSharp.Gui/FSharpCompilerOptionsWidget.cs
@@ -10,15 +10,15 @@ namespace MonoDevelop.FSharp.Gui
 		}
 		
 		public Gtk.Entry EntryCommandLine { get { return this.entryCustomParameters; } }
-        public Gtk.Entry EntryDefines { get { return this.symbolsEntry; } }
-        public Gtk.Entry EntryWarnings { get { return this.ignoreWarningsEntry; } }
+		public Gtk.Entry EntryDefines { get { return this.symbolsEntry; } }
+		public Gtk.Entry EntryWarnings { get { return this.ignoreWarningsEntry; } }
 		public Gtk.CheckButton CheckOptimize { get { return this.enableOptimizationCheckButton; } }
-        public Gtk.CheckButton CheckTailCalls { get { return this.checkTailCalls; } }
-        public Gtk.CheckButton CheckXmlDocumentation { get { return this.generateXmlOutputCheckButton; } }
-        public Gtk.ComboBox ComboDebugInformation { get { return this.comboDebug; } }
-        public Gtk.CheckButton CheckDebugInformation { get { return this.generateOverflowChecksCheckButton; } }
-        public Gtk.ComboBox ComboPlatforms { get { return this.comboPlatforms; } }
-        public Gtk.CheckButton CheckWarningsAsErrors { get { return this.warningsAsErrorsCheckButton; } }
+		public Gtk.CheckButton CheckTailCalls { get { return this.checkTailCalls; } }
+		public Gtk.CheckButton CheckXmlDocumentation { get { return this.generateXmlOutputCheckButton; } }
+		public Gtk.ComboBox ComboDebugInformation { get { return this.comboDebug; } }
+		public Gtk.CheckButton CheckDebugInformation { get { return this.generateOverflowChecksCheckButton; } }
+		public Gtk.ComboBox ComboPlatforms { get { return this.comboPlatforms; } }
+		public Gtk.CheckButton CheckWarningsAsErrors { get { return this.warningsAsErrorsCheckButton; } }
 		public Gtk.SpinButton WarningLevelSpinButton { get { return this.warningLevelSpinButton; } }
 	}
 }
