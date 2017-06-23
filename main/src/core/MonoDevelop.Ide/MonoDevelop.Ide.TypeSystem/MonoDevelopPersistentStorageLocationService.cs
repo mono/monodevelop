fbc2e97208da3db2381e834e9d57eb2fbf56dfda
Marius Ungureanu <therzok@gmail.com>
2017-06-23 21:14:18 +0300

    [TypeSystem] Make Microsoft.CodeAnalysis.Workspaces.Desktop work
    Since 2017-04, mono has fixed the mmap leaking handles issue. We can now use mmap'd files on Mac via
    roslyn temporary storage service.

---
+++ main/src/core/MonoDevelop.Ide/MonoDevelop.Ide.TypeSystem/MonoDevelopPersistentStorageLocationService.cs
@@ -0,0 +1,59 @@
+﻿//
+// MonoDevelopPersistentStorageLocationService.cs
+//
+// Author:
+//       Marius Ungureanu <maungu@microsoft.com>
+//
+// Copyright (c) 2017 Microsoft Inc.
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in
+// all copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+// THE SOFTWARE.
+
+using System;
+using System.Composition;
+using System.IO;
+using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.Host;
+using Microsoft.CodeAnalysis.Host.Mef;
+using Microsoft.CodeAnalysis.Options;
+using Microsoft.CodeAnalysis.SQLite;
+using Microsoft.CodeAnalysis.Storage;
+using Microsoft.CodeAnalysis.SolutionSize;
+using MonoDevelop.Core;
+using System.Text;
+using System.Collections.Generic;
+using System.Security.Cryptography;
+
+namespace MonoDevelop.Ide.TypeSystem
+{
+	[ExportWorkspaceService (typeof (IPersistentStorageLocationService), ServiceLayer.Host), Shared]
+	class MonoDevelopPersistentStorageLocationService : IPersistentStorageLocationService
+	{
+		public bool IsSupported (Workspace workspace) => workspace is MonoDevelopWorkspace;
+
+		public string GetStorageLocation (Solution solution)
+		{
+			if (solution.FilePath == null)
+				return null;
+			var solutionDirectory = Path.GetDirectoryName(solution.FilePath);
+			return Path.Combine (solutionDirectory, ".vs", "v15-mac");
+		}
+	}
+}
