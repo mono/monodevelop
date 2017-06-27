25894b9d43111ebf9bab01c8a7fd6f15b74feeb3
Marius Ungureanu <therzok@gmail.com>
2017-06-27 04:10:05 +0300

    [Perf] Add missing locks to StringInternPool usage.

--- main/src/core/MonoDevelop.Core/MonoDevelop.Projects.MSBuild/DefaultMSBuildEngine.cs
+++ main/src/core/MonoDevelop.Core/MonoDevelop.Projects.MSBuild/DefaultMSBuildEngine.cs
@@ -933,7 +933,8 @@ namespace MonoDevelop.Projects.MSBuild
 
 		MSBuildItemEvaluated CreateEvaluatedItem (MSBuildEvaluationContext context, ProjectInfo pinfo, MSBuildProject project, MSBuildItem sourceItem, string include, string evaluatedFile = null, string recursiveDir = null)
 		{
-			include = EngineManager.Pool.Add (include);
+			lock (EngineManager.Pool)
+				include = EngineManager.Pool.Add (include);
 
 			var it = new MSBuildItemEvaluated (project, sourceItem.Name, sourceItem.Include, include);
 			var md = new Dictionary<string,IMSBuildPropertyEvaluated> ();
