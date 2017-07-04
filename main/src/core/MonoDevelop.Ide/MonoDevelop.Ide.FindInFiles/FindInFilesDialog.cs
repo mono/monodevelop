8342d2839a2bdd7d9e66a0515ddafd934ca43ef5
Dmytro Ovcharov <dmytro.ovcharov@globallogic.com>
2017-07-04 17:01:05 +0300

    Line brakes changed

--- main/src/core/MonoDevelop.Ide/MonoDevelop.Ide.FindInFiles/FindInFilesDialog.cs
+++ main/src/core/MonoDevelop.Ide/MonoDevelop.Ide.FindInFiles/FindInFilesDialog.cs
@@ -24,7 +24,7 @@
 // OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
 // THE SOFTWARE.
 
-using System;
+using System; 
 using System.Linq;
 using System.Threading;
 using System.Text;
