58788c80ae4309cbd5b2ed1d231f892265ee41a2
Matt Ward <matt.ward@xamarin.com>
2017-07-11 18:54:32 +0100

    [NuGet] Re-enable NuGet package service

--- main/src/addins/MonoDevelop.PackageManagement/MonoDevelop.PackageManagement/PackageManagementServices.cs
+++ main/src/addins/MonoDevelop.PackageManagement/MonoDevelop.PackageManagement/PackageManagementServices.cs
@@ -70,7 +70,7 @@ namespace MonoDevelop.PackageManagement
 			nuGetConfigFileChangedMonitor.MonitorFileChanges ();
 
 			//analyzerPackageMonitor = new AnalyzerPackageMonitor ();
-			// MonoDevelop.Refactoring.PackageInstaller.PackageInstallerServiceFactory.PackageServices = new MonoDevelop.PackageManagement.Refactoring.NuGetPackageServicesProxy ();
+			MonoDevelop.Refactoring.PackageInstaller.PackageInstallerServiceFactory.PackageServices = new MonoDevelop.PackageManagement.Refactoring.NuGetPackageServicesProxy ();
 		}
 
 		internal static void InitializeCredentialService ()
