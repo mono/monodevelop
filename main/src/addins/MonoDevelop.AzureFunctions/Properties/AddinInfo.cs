9afeded03d40d954f0fac5ae51a1c1452f2027be
Jordan Matthiesen <jmatthie@hotmail.com>
2017-08-01 11:10:08 -0700

    Update metadata for an upcoming update

--- main/src/addins/MonoDevelop.AzureFunctions/Properties/AddinInfo.cs
+++ main/src/addins/MonoDevelop.AzureFunctions/Properties/AddinInfo.cs
@@ -7,8 +7,8 @@ using Mono.Addins.Description;
 	Version = MonoDevelop.BuildInfo.Version,
 	Category = "Ide")]
 
-[assembly: AddinName ("Azure Functions")]
-[assembly: AddinDescription ("Azure Functions")]
++[assembly: AddinName ("Azure Functions development (Preview)")]
+ +[assembly: AddinDescription ("Preview support for developing micro-services with Azure Functions.")]
 
 [assembly: AddinDependency ("Core", MonoDevelop.BuildInfo.Version)]
 [assembly: AddinDependency ("Ide", MonoDevelop.BuildInfo.Version)]
