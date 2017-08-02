976abada2c46a29cbe4b615e85bf0cf264b70f35
Mikayla Hutchinson <m.j.hutchinson@gmail.com>
2017-08-01 20:34:11 -0400

    Update AddinInfo.cs

--- main/src/addins/MonoDevelop.AzureFunctions/Properties/AddinInfo.cs
+++ main/src/addins/MonoDevelop.AzureFunctions/Properties/AddinInfo.cs
@@ -5,10 +5,10 @@ using Mono.Addins.Description;
 [assembly: Addin ("AzureFunctions",
 	Namespace = "MonoDevelop",
 	Version = MonoDevelop.BuildInfo.Version,
-	Category = "Ide")]
+	Category = "IDE extensions")]
 
 [assembly: AddinName ("Azure Functions development (Preview)")]
-[assembly: AddinDescription ("Preview support for developing micro-services with Azure Functions.")]
+[assembly: AddinDescription ("Preview support for developing microservices with Azure Functions.")]
 
 [assembly: AddinDependency ("Core", MonoDevelop.BuildInfo.Version)]
 [assembly: AddinDependency ("Ide", MonoDevelop.BuildInfo.Version)]
