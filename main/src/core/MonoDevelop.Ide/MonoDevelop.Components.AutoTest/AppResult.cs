c9cc52476356eeb7c342a180eb258af048dbe300
Manish Sinha <manish.sinha@xamarin.com>
2017-08-18 16:02:51 -0400

    [AutoTest] Split MatchProperty into smaller GetRecursiveObjectProperty
    This is important to get the property value in the case of recursion
    i.e. if we want to get the Editor property, then Caret, then Line of
    say Workbench, then we can ask for Editor.Caret.Line

--- main/src/core/MonoDevelop.Ide/MonoDevelop.Components.AutoTest/AppResult.cs
+++ main/src/core/MonoDevelop.Ide/MonoDevelop.Components.AutoTest/AppResult.cs
@@ -188,14 +188,20 @@ namespace MonoDevelop.Components.AutoTest
 
 		protected AppResult MatchProperty (string propertyName, object objectToCompare, object value)
 		{
-			foreach (var singleProperty in propertyName.Split (new [] { '.' })) {
-				objectToCompare = GetPropertyValue (singleProperty, objectToCompare);
-			}
+			var objectToCompare = GetRecursiveObjectProperty(propertyName, objectToCompare);
 			if (objectToCompare != null && value != null &&
-				CheckForText (objectToCompare.ToString (), value.ToString (), false)) {
+				CheckForText(objectToCompare.ToString(), value.ToString(), false)) {
 				return this;
 			}
 			return null;
+		}
+
+		protected object GetRecursiveObjectProperty (string propertyName, object obj)
+		{
+			foreach (var singleProperty in propertyName.Split(new[] { '.' })) {
+				obj = GetPropertyValue(singleProperty, obj);
+			}
+			return obj;
 		}
 
 		protected bool CheckForText (string haystack, string needle, bool exact)
