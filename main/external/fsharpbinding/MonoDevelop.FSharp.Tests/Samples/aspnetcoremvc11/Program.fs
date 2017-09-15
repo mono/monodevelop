43f715307c0e73c2a608d6f0849a3f2c82f28547
nosami <jasonimison@gmail.com>
2017-09-15 23:31:46 +0100

    Project file manipulation for VS for Windows fixes #59491  …
    Check IsImported flag and add add fixes for forward slashes.
    I also fixed some other issues that I spotted while fixing this
    
    Fix warnings
    
    Guard against MSBuild item Include being null
    
    Such as `<None Remove="**/*.fs" />`
    
    Add sample dotnet core web application template
    
    Add a build test for dotnet core MVC web ap
    
    Add IsImported asserts
    
    Switch MVC build test to netcore1.1

---
+++ main/external/fsharpbinding/MonoDevelop.FSharp.Tests/Samples/aspnetcoremvc11/Program.fs
@@ -0,0 +1,17 @@
+﻿namespace aspnetcoremvc11
+
+open System
+open System.Collections.Generic
+open System.IO
+open System.Linq
+open System.Threading.Tasks
+open Microsoft.AspNetCore.Hosting
+
+module Program = 
+    [<EntryPoint>]
+    let main args = 
+        let host = 
+            WebHostBuilder().UseKestrel().UseContentRoot(Directory.GetCurrentDirectory()).UseIISIntegration().UseStartup<Startup>()
+                .Build()
+        host.Run()
+        0
