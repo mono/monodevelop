63dfa852ee9465935c2e51827be0bf5514dc3e8c
Matt Ward <matt.ward@xamarin.com>
2017-10-09 17:26:02 +0100

    [Core] Fix incorrect SDK style project updates when moving a file
    Fixed bug #59418 - SDK project file not updated correctly when moving
    file to different folder
    https://bugzilla.xamarin.com/show_bug.cgi?id=59418
    
    Moving a Resource file from one folder to another would not update
    the project file correctly. Either the project file would not be
    modified or the items would be removed from the project file.
    One problem was that on moving a clone is taken of the original item
    and the evaluated MSBuild item was copied but was still referring
    back to the original location. Another problem was that MSBuild items
    were incorrectly being removed when matching a null include.

---
+++ main/tests/Ide.Tests/MonoDevelop.Ide.Projects/DotNetCoreProjectTests.cs
@@ -0,0 +1,64 @@
+﻿//
+// DotNetCoreProjectTests.cs
+//
+// Author:
+//       Matt Ward <matt.ward@xamarin.com>
+//
+// Copyright (c) 2017 Xamarin Inc. (http://xamarin.com)
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in
+// all copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+// THE SOFTWARE.
+
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using MonoDevelop.Core;
+using MonoDevelop.Projects;
+using NUnit.Framework;
+using UnitTests;
+
+namespace MonoDevelop.Ide.Projects
+{
+	[TestFixture]
+	public class DotNetCoreProjectTests : TestBase
+	{
+		[Test]
+		public async Task MoveResourceFilesToFolder ()
+		{
+			FilePath projFile = Util.GetSampleProject ("DotNetCoreResources", "NetStandardProject", "NetStandardProject.csproj");
+
+			var p = (DotNetProject)await Services.ProjectService.ReadSolutionItem (Util.GetMonitor (), projFile);
+			var resxFile = p.Files.Single (f => f.FilePath.FileName == "Resources.resx");
+
+			var newFolder = p.BaseDirectory.Combine ("NewFolder");
+			Directory.CreateDirectory (newFolder);
+
+			var resxFileTarget = newFolder.Combine ("Resources.resx");
+
+			bool move = true;
+			ProjectOperations.TransferFilesInternal (Util.GetMonitor (), p, resxFile.FilePath, p, resxFileTarget, move, true);
+
+			string expectedProjectXml = File.ReadAllText (p.FileName.ChangeName ("NetStandardProject-saved"));
+			await p.SaveAsync (Util.GetMonitor ());
+
+			string projectXml = File.ReadAllText (p.FileName);
+			Assert.AreEqual (expectedProjectXml, projectXml);
+			p.Dispose ();
+		}
+	}
+}
