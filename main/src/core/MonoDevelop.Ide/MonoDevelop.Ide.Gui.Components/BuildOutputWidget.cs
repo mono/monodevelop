2160b6dd0a2c78d7f383806d11c26676a56d5de2
Rodrigo Moya <rodrigo@gnome.org>
2017-11-01 22:56:44 +0100

    [Ide] Add code to process and display .binlog files

--- main/src/core/MonoDevelop.Ide/MonoDevelop.Ide.Gui.Components/BuildOutputWidget.cs
+++ main/src/core/MonoDevelop.Ide/MonoDevelop.Ide.Gui.Components/BuildOutputWidget.cs
@@ -29,6 +29,9 @@ using Gtk;
 using MonoDevelop.Ide.Editor;
 using MonoDevelop.Components;
 using MonoDevelop.Core;
+using Microsoft.Build.Logging;
+using Microsoft.Build.Framework;
+using System.Text;
 
 namespace MonoDevelop.Ide.Gui.Components
 {
@@ -40,23 +43,110 @@ namespace MonoDevelop.Ide.Gui.Components
 
 		public BuildOutputWidget (FilePath filename)
 		{
+			this.filename = filename;
+
 			editor = TextEditorFactory.CreateNewEditor ();
 			editor.IsReadOnly = true;
 			editor.FileName = filename;
-
-			editor.Text = "Hello BuildOutputWidget";
 
 			scrolledWindow = new CompactScrolledWindow ();
 			scrolledWindow.Add (editor);
 
 			PackStart (scrolledWindow);
 			ShowAll ();
+
+			ReadFile ();
 		}
 
 		protected override void OnDestroyed ()
 		{
 			editor.Dispose ();
 			base.OnDestroyed ();
+		}
+
+		StringBuilder buildOutput;
+
+		void ReadFile()
+		{
+			var binlogReader = new BinaryLogReplayEventSource ();
+			binlogReader.BuildStarted += BinLog_BuildStarted;
+			binlogReader.BuildFinished += BinLog_BuildFinished;
+			binlogReader.ErrorRaised += BinLog_ErrorRaised;
+			binlogReader.ProjectStarted += BinLog_ProjectStarted;
+			binlogReader.ProjectFinished += BinLog_ProjectFinished;
+			binlogReader.TargetStarted += BinLog_TargetStarted;
+			binlogReader.TargetFinished += BinLog_TargetFinished;
+			binlogReader.TaskStarted += BinLog_TaskStarted;
+			binlogReader.TaskFinished += BinLog_TaskFinished;
+
+			buildOutput = new StringBuilder();
+			binlogReader.Replay (filename.FullPath);
+			editor.Text = buildOutput.ToString();
+
+			binlogReader.BuildStarted -= BinLog_BuildStarted;
+			binlogReader.BuildFinished -= BinLog_BuildFinished;
+			binlogReader.ErrorRaised -= BinLog_ErrorRaised;
+			binlogReader.ProjectStarted -= BinLog_ProjectStarted;
+			binlogReader.ProjectFinished -= BinLog_ProjectFinished;
+			binlogReader.TargetStarted -= BinLog_TargetStarted;
+			binlogReader.TargetFinished -= BinLog_TargetFinished;
+			binlogReader.TaskStarted -= BinLog_TaskStarted;
+			binlogReader.TaskFinished -= BinLog_TaskFinished;
+		}
+
+		private string GetSuccessString (bool succeeded)
+		{
+			return succeeded ? "successfully" : "with errors";
+		}
+
+		private void InsertText (string text)
+		{
+			buildOutput.Append (text);
+		}
+
+		private void BinLog_BuildStarted(object sender, BuildStartedEventArgs e)
+		{
+			InsertText($"[{e.Timestamp}] Build started\n");
+		}
+
+		private void BinLog_BuildFinished(object sender, BuildFinishedEventArgs e)
+		{
+			InsertText($"[{e.Timestamp}] Build finished\n");
+		}
+
+		private void BinLog_ErrorRaised(object sender, BuildErrorEventArgs e)
+		{
+			InsertText($"[{e.Timestamp}] Error: {e.Code}: {e.Message} at {e.ProjectFile}#{e.LineNumber}\n");
+		}
+
+		private void BinLog_ProjectStarted(object sender, ProjectStartedEventArgs e)
+		{
+			InsertText($"[{e.Timestamp}]\tProject {e.ProjectFile} started\n");
+		}
+
+		private void BinLog_ProjectFinished(object sender, ProjectFinishedEventArgs e)
+		{
+			InsertText($"[{e.Timestamp}]\tProject {e.ProjectFile} finished {GetSuccessString(e.Succeeded)}\n");
+		}
+
+		private void BinLog_TargetStarted(object sender, TargetStartedEventArgs e)
+		{
+			InsertText($"[{e.Timestamp}]\t\tTarget {e.TargetName} started\n");
+		}
+
+		private void BinLog_TargetFinished(object sender, TargetFinishedEventArgs e)
+		{
+			InsertText($"[{e.Timestamp}]\t\tTarget {e.TargetName} finished {GetSuccessString(e.Succeeded)}\n");
+		}
+
+		private void BinLog_TaskStarted(object sender, TaskStartedEventArgs e)
+		{
+			InsertText($"[{e.Timestamp}]\t\t\tTask {e.TaskName} started\n");
+		}
+
+		private void BinLog_TaskFinished(object sender, TaskFinishedEventArgs e)
+		{
+			InsertText($"[{e.Timestamp}]\t\t\tTask {e.TaskName} finished {GetSuccessString(e.Succeeded)}\n");
 		}
 	}
 }
