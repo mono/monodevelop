62b6b6c099de658469e58b8111d6356034424f53
therzok <marius.ungureanu@xamarin.com>
2018-01-03 20:39:59 +0200

    [Core] Fix build.

--- main/src/core/MonoDevelop.Core/MonoDevelop.Projects.MSBuild.Conditions/ConditionTokenizer.cs
+++ main/src/core/MonoDevelop.Core/MonoDevelop.Projects.MSBuild.Conditions/ConditionTokenizer.cs
@@ -31,6 +31,7 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using System.Collections.Specialized;
+using System.Runtime.CompilerServices;
 using System.Text;
 
 namespace MonoDevelop.Projects.MSBuild.Conditions {
