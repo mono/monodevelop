c84a8a36385200a31f7770626cf01bf4e8fbbd90
Todd Grunke <toddgrun@microsoft.com>
2018-01-25 16:32:30 -0800

    Get rid of some exceptions that occur during project open

--- main/src/core/MonoDevelop.Core/MonoDevelop.Projects.MSBuild/RemoteBuildEngineManager.cs
+++ main/src/core/MonoDevelop.Core/MonoDevelop.Projects.MSBuild/RemoteBuildEngineManager.cs
@@ -611,7 +611,8 @@ namespace MonoDevelop.Projects.MSBuild
 				if (int.TryParse (Path.GetFileName (spid), out pid)) {
 					try {
 						// If there is a process running with this id it means the builder is still being used
-						if (Process.GetProcessById (pid) != null)
+						var runningProcesses = Process.GetProcesses();
+						if (runningProcesses.Any(p => p.Id == pid))
 							continue;
 					} catch {
 						// Ignore
