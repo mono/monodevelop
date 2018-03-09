749c33232b36eec4f154de93db44e67853daeace
David Karlaš <david.karlas@microsoft.com>
2018-03-09 14:45:58 +0100

    Completely changed RoslynCompletionPresenterSession to be it's own CompletionList implementation Unfortunately had to add reference to SourceEditor from CSharpBinding, reason for this is... in theory RoslynCompletionPresenterSession.cs should be in SourceEditor and this wouldn't be needed, but since APIs that RoslynCompletionPresenterSession.cs is using are Roslyn Internals API, it has to be in CSharpBinding project... Hence I needed to reference to access SourceEditor types needed...

--- main/src/addins/CSharpBinding/Razor/RoslynCompletionPresenter.cs
+++ main/src/addins/CSharpBinding/Razor/RoslynCompletionPresenter.cs
@@ -42,7 +42,7 @@ namespace MonoDevelop.Ide.CodeCompletion
                         if (TryGetLanguageNameFromContentType (subjectBuffer.ContentType, out languageName)) {
                             if (Workspace.TryGetWorkspace (subjectBuffer.AsTextContainer (), out var workspace)) {
                                 CompletionService completionService = workspace.Services.GetLanguageServices (languageName).GetService<CompletionService> ();
-                                return new RoslynCompletionPresenterSession (textView, subjectBuffer, completionDataProviderHandle.Value, completionService);
+								return new RoslynCompletionPresenterSession ((IMdTextView)textView, subjectBuffer, completionService);
                             }
                         }
                     }
