aa76753c3de963a6f21c6b4094991f9adbe2ea09
therzok <marius.ungureanu@xamarin.com>
2018-04-24 17:29:12 +0300

    [Ide] Move IFilePathRegistryService MEF export query out of static constructor
    Shaves some time off the IDE startup by delaying the query for the registry service (4s) in my case.

--- main/src/core/MonoDevelop.Ide/MonoDevelop.Ide.Desktop/PlatformService.cs
+++ main/src/core/MonoDevelop.Ide/MonoDevelop.Ide.Desktop/PlatformService.cs
@@ -297,11 +297,11 @@ namespace MonoDevelop.Ide.Desktop
 			}
 		}
 
-		static IFilePathRegistryService filePathRegistryService = CompositionManager.GetExportedValue<IFilePathRegistryService> ();
+		static Lazy<IFilePathRegistryService> filePathRegistryService = new Lazy<IFilePathRegistryService> (() => CompositionManager.GetExportedValue<IFilePathRegistryService> ());
 		MimeTypeNode FindMimeTypeForFile (string fileName)
 		{
 			try {
-				IContentType contentType = filePathRegistryService.GetContentTypeForPath (fileName);
+				IContentType contentType = filePathRegistryService.Value.GetContentTypeForPath (fileName);
 				if (contentType != PlatformCatalog.Instance.ContentTypeRegistryService.UnknownContentType) {
 					string mimeType = PlatformCatalog.Instance.MimeToContentTypeRegistryService.GetMimeType (contentType);
 					if (mimeType != null) {
