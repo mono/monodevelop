8e990b7f3c7bc5b34c1e44a47704809166547000
therzok <marius.ungureanu@xamarin.com>
2018-04-24 23:13:42 +0300

    [Perf] Optimize startup by doing MEF composition query on idle
    What happens here is that our Composition Catalog uses Mono.Addins behind the scenes
    to gather all the assemblies to load for composition.
    
    On the other hand, that _also_ loads the addins into the current engine.
    
    That means that MEF holds the addin lock, so IDE startup is inconsistent
    across sessions, depending on who gets to hold the lock first.
    
    This should fix the startup of the IDE by a big margin

--- main/src/core/MonoDevelop.Ide/MonoDevelop.Ide/IdeStartup.cs
+++ main/src/core/MonoDevelop.Ide/MonoDevelop.Ide/IdeStartup.cs
@@ -164,8 +164,6 @@ namespace MonoDevelop.Ide
 			Counters.Initialization.Trace ("Initializing Runtime");
 			Runtime.Initialize (true);
 
-			Composition.CompositionManager.InitializeAsync ().Ignore ();
-
 			IdeApp.Customizer.OnCoreInitialized ();
 
 			Counters.Initialization.Trace ("Initializing theme");
@@ -301,6 +299,7 @@ namespace MonoDevelop.Ide
 			startupTimer.Stop ();
 			Counters.Startup.Inc (GetStartupMetadata (startupInfo));
 
+			GLib.Idle.Add (OnIdle);
 			IdeApp.Run ();
 
 			IdeApp.Customizer.OnIdeShutdown ();
@@ -318,6 +317,12 @@ namespace MonoDevelop.Ide
 			MonoDevelop.Components.GtkWorkarounds.Terminate ();
 			
 			return 0;
+		}
+
+		static bool OnIdle ()
+		{
+			Composition.CompositionManager.InitializeAsync ().Ignore ();
+			return false;
 		}
 
 		static DateTime lastIdle;
