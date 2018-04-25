e114e12a760876e62c4aaf263328f9bfe7a9a9d6
therzok <marius.ungureanu@xamarin.com>
2018-04-25 15:03:47 +0300

    [MEF] Add a timer counter for MEF composition

--- main/src/core/MonoDevelop.Ide/MonoDevelop.Ide/Services.cs
+++ main/src/core/MonoDevelop.Ide/MonoDevelop.Ide/Services.cs
@@ -58,7 +58,7 @@ namespace MonoDevelop.Ide
 		internal static TimerCounter SaveAllTimer = InstrumentationService.CreateTimerCounter ("Save all documents", "IDE", id:"Ide.Shell.SaveAll");
 		internal static TimerCounter CloseWorkspaceTimer = InstrumentationService.CreateTimerCounter ("Workspace closed", "IDE", id:"Ide.Shell.CloseWorkspace");
 		internal static Counter Startup = InstrumentationService.CreateTimerCounter ("IDE Startup", "IDE", id:"Ide.Startup");
-
+		internal static TimerCounter Composition = InstrumentationService.CreateTimerCounter ("MEF Composition", "IDE", id:"Ide.Startup.Composition", logMessages:true);
 		internal static TimerCounter ProcessCodeCompletion = InstrumentationService.CreateTimerCounter ("Process Code Completion", "IDE", id: "Ide.ProcessCodeCompletion", logMessages:false);
 
 		internal static class ParserService {
