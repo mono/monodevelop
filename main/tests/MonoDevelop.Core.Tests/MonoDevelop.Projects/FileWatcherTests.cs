488bacb4df323a2a33a10b27faf857df769022b2
Matt Ward <matt.ward@microsoft.com>
2018-04-26 16:39:03 +0100

    [Core] Allow a solution to be monitored by the file watcher
    Added a simple way to register a solution so all files in its
    directories are monitored. Only file change events are passed to the
    FileService which are then raised as events by the FileService.
    Added some tests to verify that modify a project inside a solution
    or a file inside the project generates FileService FileChanged
    events. The tests that use Project.SaveAsync pass without the
    new file watcher since the FileService is called directly to
    notify file changes when SolutionItem.DoSave is called and
    MSBuildProject.SaveAsync is called, which uses TextFile.Write which
    generates the event. The direct calls to notify the FileService
    have not yet been removed.

--- main/tests/MonoDevelop.Core.Tests/MonoDevelop.Projects/FileWatcherTests.cs
+++ main/tests/MonoDevelop.Core.Tests/MonoDevelop.Projects/FileWatcherTests.cs
@@ -24,7 +24,13 @@
 // OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
 // THE SOFTWARE.
 
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 using MonoDevelop.Core;
+using MonoDevelop.Core.Text;
 using NUnit.Framework;
 using UnitTests;
 
@@ -33,10 +39,133 @@ namespace MonoDevelop.Projects
 	[TestFixture]
 	public class FileWatcherTests : TestBase
 	{
+		Solution sol;
+		List<FileEventInfo> fileChanges;
+		TaskCompletionSource<bool> fileChangesTask;
+		FilePath waitingForFileChangeFileName;
+
+		[SetUp]
+		public void Init ()
+		{
+			fileChanges = new List<FileEventInfo> ();
+			fileChangesTask = new TaskCompletionSource<bool> ();
+			FileService.FileChanged += OnFileChanged;
+		}
+
+		[TearDown]
+		public void TestTearDown ()
+		{
+			if (sol != null) {
+				FileWatcherService.Remove (sol);
+				sol.Dispose ();
+			}
+
+			FileService.FileChanged -= OnFileChanged;
+		}
+
+		void ClearFileChanges ()
+		{
+			fileChanges.Clear ();
+		}
+
+		void OnFileChanged (object sender, FileEventArgs e)
+		{
+			fileChanges.AddRange (e);
+
+			if (waitingForFileChangeFileName.IsNotNull) {
+				if (fileChanges.Any (file => file.FileName == waitingForFileChangeFileName)) {
+					fileChangesTask.SetResult (true);
+				}
+			}
+		}
+
+		void AssertFileChanged (FilePath fileName)
+		{
+			var files = fileChanges.Select (fileChange => fileChange.FileName);
+			Assert.That (files, Contains.Item (fileName));
+		}
+
+		Task WaitForFileChanged (FilePath fileName, int millisecondsTimeout = 2000)
+		{
+			return Task.WhenAny (Task.Delay (millisecondsTimeout), fileChangesTask.Task);
+		}
+
 		[Test]
 		public void IsNativeMacFileWatcher ()
 		{
 			Assert.AreEqual (Platform.IsMac, FSW.FileSystemWatcher.IsMac);
+		}
+
+		/// <summary>
+		/// Original code seems to generate the FileChanged event twice for the project file.
+		/// </summary>
+		[Test]
+		public async Task SaveProject_AfterModifying ()
+		{
+			string solFile = Util.GetSampleProject ("console-project", "ConsoleProject.sln");
+			sol = (Solution) await Services.ProjectService.ReadWorkspaceItem (Util.GetMonitor (), solFile);
+			var p = (DotNetProject) sol.Items [0];
+			p.DefaultNamespace = "Test";
+			ClearFileChanges ();
+			FileWatcherService.Add (sol);
+
+			await p.SaveAsync (Util.GetMonitor ());
+
+			AssertFileChanged (p.FileName);
+			Assert.IsFalse (p.NeedsReload);
+		}
+
+		[Test]
+		public async Task SaveProjectFileExternally ()
+		{
+			string solFile = Util.GetSampleProject ("console-project", "ConsoleProject.sln");
+			sol = (Solution) await Services.ProjectService.ReadWorkspaceItem (Util.GetMonitor (), solFile);
+			var p = (DotNetProject) sol.Items [0];
+			p.DefaultNamespace = "Test";
+			ClearFileChanges ();
+			FileWatcherService.Add (sol);
+
+			string xml = p.MSBuildProject.SaveToString ();
+			File.WriteAllText (p.FileName, xml);
+
+			await WaitForFileChanged (p.FileName);
+
+			AssertFileChanged (p.FileName);
+		}
+
+		[Test]
+		public async Task SaveFileInProjectExternally ()
+		{
+			string solFile = Util.GetSampleProject ("console-project", "ConsoleProject.sln");
+			sol = (Solution) await Services.ProjectService.ReadWorkspaceItem (Util.GetMonitor (), solFile);
+			var p = (DotNetProject) sol.Items [0];
+			var file = p.Files.First (f => f.FilePath.FileName == "Program.cs");
+			ClearFileChanges ();
+			FileWatcherService.Add (sol);
+
+			TextFileUtility.WriteText (file.FilePath, string.Empty, Encoding.UTF8);
+
+			await WaitForFileChanged (file.FilePath);
+
+			AssertFileChanged (file.FilePath);
+		}
+
+		[Test]
+		public async Task SaveFileInProjectExternallyAfterSolutionNotWatched_NoFileChangeEventsFired ()
+		{
+			string solFile = Util.GetSampleProject ("console-project", "ConsoleProject.sln");
+			sol = (Solution) await Services.ProjectService.ReadWorkspaceItem (Util.GetMonitor (), solFile);
+			var p = (DotNetProject) sol.Items [0];
+			var file = p.Files.First (f => f.FilePath.FileName == "Program.cs");
+			ClearFileChanges ();
+			FileWatcherService.Add (sol);
+			FileWatcherService.Remove (sol);
+
+			TextFileUtility.WriteText (file.FilePath, string.Empty, Encoding.UTF8);
+
+			await WaitForFileChanged (file.FilePath);
+
+			Assert.AreEqual (0, fileChanges.Count);
 		}
 	}
 }
