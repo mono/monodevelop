9f4f863cf5a4b0c7edb81c925873a3cd9b614f97
Lluis Sanchez <llsan@microsoft.com>
2018-05-09 18:20:35 +0200

    Improve unit test
    Remove hack for causing a builder crash, and use instead a project file
    that causes a crash.

--- main/src/core/MonoDevelop.Projects.Formats.MSBuild/MonoDevelop.Projects.Formats.MSBuild/ProjectBuilder.v4.0.cs
+++ main/src/core/MonoDevelop.Projects.Formats.MSBuild/MonoDevelop.Projects.Formats.MSBuild/ProjectBuilder.v4.0.cs
@@ -61,7 +61,6 @@ namespace MonoDevelop.Projects.MSBuild
 				throw new ArgumentException ("runTargets is empty");
 
 			MSBuildResult result = null;
-			CheckProperties (globalProperties);
 
 			BuildEngine.RunSTA (taskId, delegate {
 				Project project = null;
@@ -239,13 +238,6 @@ namespace MonoDevelop.Projects.MSBuild
 				BuildRequestData data = new BuildRequestData (pi, targets);
 				results = BuildManager.DefaultBuildManager.BuildRequest (data);
 			}
-		}
-
-		void CheckProperties (Dictionary<string, string> globalProperties)
-		{
-			// Used for unit testing
-			if (globalProperties != null && globalProperties.Any (prop => prop.Key == "__CRASH_ME__"))
-				System.Diagnostics.Process.GetCurrentProcess ().Kill ();
 		}
 	}
 }
