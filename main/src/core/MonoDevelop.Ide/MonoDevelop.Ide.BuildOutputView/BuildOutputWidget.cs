57fadfac24d0476f1451085ef0e96faf1254d693
Rodrigo Moya <rodrigo.moya@xamarin.com>
2018-05-27 20:23:32 +0200

    [BuildOutput] Keep track of normal/diagnostics selection

--- main/src/core/MonoDevelop.Ide/MonoDevelop.Ide.BuildOutputView/BuildOutputWidget.cs
+++ main/src/core/MonoDevelop.Ide/MonoDevelop.Ide.BuildOutputView/BuildOutputWidget.cs
@@ -659,6 +659,11 @@ namespace MonoDevelop.Ide.BuildOutputView
 														  $"{GettextCatalog.GetString ("Build Output")} {DateTime.Now.ToString ("h:mm tt yyyy-MM-dd")}.binlog" :
 														  (string)filePathLocation;
 						FileNameChanged?.Invoke (this, ViewContentName);
+						if (showDiagnostics) {
+							Counters.DiagnosticsViewSelected++;
+						} else {
+							Counters.NormalViewSelected++;
+						}
 					});
 				} catch (Exception ex) {
 					processingCompletion.TrySetException (ex);
