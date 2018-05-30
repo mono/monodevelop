9e58661ad3d279cf88d961dc9c77ef5d65fc1651
Matt Ward <matt.ward@microsoft.com>
2018-05-30 16:19:26 +0100

    [C#] Rely on file watcher for file change events
    Removed use of FileService.NotifyFileChange from the
    CodeGenerationService. Only code that seems to call this is the
    GtkCore addin in the ActionGroupDisplayBinding. That code does not
    seem to work - it fails to create a valid syntax tree in
    ActionGroupDisplayBinding.CreateClass, the C# code analysis
    SyntaxFactory.GetConstructorInitializerThisOrBaseKeywordKind throws
    an ArgumentOutOfRange exception. Also the names used are empty
    strings.

--- main/src/addins/CSharpBinding/MonoDevelop.CSharp.Refactoring/CodeGenerationService.cs
+++ main/src/addins/CSharpBinding/MonoDevelop.CSharp.Refactoring/CodeGenerationService.cs
@@ -285,7 +285,6 @@ namespace MonoDevelop.Refactoring
 			using (var sw = new StreamWriter (fileName)) {
 				sw.WriteLine (ns.ToString ());
 			}
-			FileService.NotifyFileChanged (fileName);
 			var roslynProject = MonoDevelop.Ide.TypeSystem.TypeSystemService.GetCodeAnalysisProject (project);
 			var id = MonoDevelop.Ide.TypeSystem.TypeSystemService.GetDocumentId (roslynProject.Id, fileName);
 			if (id == null)
