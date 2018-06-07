2670e7339b8c28851f8e97b3e292cc034538d715
Mike Krüger <mikkrg@microsoft.com>
2018-06-07 14:19:16 +0200

    Fixes VSTS Bug 627497: [Feedback] code snippet cursor in wrong location after setting condition
    https://devdiv.visualstudio.com/DevDiv/_workitems/edit/627497
    
    TextLink mode had no unit tests so I added some.

--- main/src/core/MonoDevelop.TextEditor.Tests/Mono.TextEditor.Tests/InsertionModeTests.cs
+++ main/src/core/MonoDevelop.TextEditor.Tests/Mono.TextEditor.Tests/InsertionModeTests.cs
@@ -30,7 +30,7 @@ using NUnit.Framework;
 
 namespace Mono.TextEditor.Tests
 {
-	[TestFixture()]
+	[TestFixture ()]
 	class InsertionModeTests : TextEditorTestBase
 	{
 		string CreateInsertionPoint (string input, string text, NewLineInsertion before, NewLineInsertion after)
@@ -42,8 +42,8 @@ namespace Mono.TextEditor.Tests
 			point.Insert (data, text);
 			return data.Document.Text;
 		}
-		
-		[Test()]
+
+		[Test ()]
 		public void TestInsertionPointClassStart ()
 		{
 			string test = CreateInsertionPoint (@"class Test
