35c2033335bf3183537e2a41c6ef207bd34459f7
therzok <marius.ungureanu@xamarin.com>
2018-06-12 00:12:44 +0300

    Fix more tests to properly dispose solutions

--- main/tests/MonoDevelop.CSharpBinding.Tests/MonoDevelop.CSharpBinding/OnTheFlyFormatterTests.cs
+++ main/tests/MonoDevelop.CSharpBinding.Tests/MonoDevelop.CSharpBinding/OnTheFlyFormatterTests.cs
@@ -131,13 +131,9 @@ namespace MonoDevelop.CSharpBinding
 			await doc.UpdateParseDocument ();
 			if (selectionStart >= 0 && selectionEnd >= 0)
 				content.GetTextEditorData ().SetSelection (selectionStart, selectionEnd);
-			try {
+
+			using (var testCase = new Ide.TextEditorExtensionTestCase (doc, content, tww, null, false)) {
 				act (content, ext);
-			} finally {
-				using (solution)
-					TypeSystemService.Unload (solution);
-				tww.CloseWindowSync ();
-				doc.DisposeDocument ();
 			}
 		}
 
