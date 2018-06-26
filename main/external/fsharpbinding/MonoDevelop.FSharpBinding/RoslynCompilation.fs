d6b35195590b18a5ae8a7b6ed39e0bd687ebcb80
nosami <jasonimison@gmail.com>
2018-06-26 15:13:42 +0100

    Update Roslyn.FSharp

---
+++ main/external/fsharpbinding/MonoDevelop.FSharpBinding/RoslynCompilation.fs
@@ -0,0 +1,50 @@
+﻿namespace MonoDevelop.FSharp
+
+open Microsoft.CodeAnalysis
+open Microsoft.FSharp.Compiler.SourceCodeServices
+open MonoDevelop.Core
+open Roslyn.FSharp
+
+type FSharpCompilation (checkProjectResults: FSharpCheckProjectResults, outputFile) =
+    let assemblySignature = checkProjectResults.AssemblySignature
+
+    interface IRoslynCompilation with
+        member x.GetTypeByMetadataName(fullyQualifiedMetadataName:string) =
+            let path =
+                fullyQualifiedMetadataName.Split '.'
+                |> Array.collect(fun s -> s.Split '+')
+                |> List.ofArray
+
+            let selfAndReferences =
+                seq {
+                    yield assemblySignature
+                    yield! checkProjectResults.ProjectContext.GetReferencedAssemblies()
+                           |> List.map(fun a -> a.Contents)
+                }
+
+            selfAndReferences
+            |> Seq.tryPick(fun a -> a.FindEntityByPath path)
+            |> Option.map(fun e -> FSharpNamedTypeSymbol(e) :> INamedTypeSymbol)
+            |> Option.toObj
+
+        member x.References =
+            checkProjectResults.ProjectContext.GetReferencedAssemblies()
+            //TODO: System.IO.FileNotFoundException: Could not load file or assembly 'FSharp.Core' or one of its dependencies
+            |> Seq.filter (fun a -> a.SimpleName <> "FSharp.Core") 
+            //TODO:System.IO.FileNotFoundException: Cannot resolve dependency to assembly because it has not been preloaded. When using the ReflectionOnly APIs, dependent assemblies must be pre-loaded or loaded on demand through the ReflectionOnlyAssemblyResolve event.
+            // File name: 'netstandard, Version=2.0.0.0, Culture=neutral, PublicKeyToken=cc7b13ffcd2ddd51'
+            |> Seq.filter (fun a -> a.SimpleName <> "netstandard") 
+            |> Seq.choose (fun asm -> asm.FileName)
+            |> Seq.map(fun fileName -> MetadataReference.CreateFromFile (fileName) :> MetadataReference)
+
+        member x.GetAssemblyOrModuleSymbol(reference) =
+            let fsharpAssembly =
+                checkProjectResults.ProjectContext.GetReferencedAssemblies()
+                |> List.find(fun a ->
+                    a.FileName
+                    |> Option.exists(fun f -> f = reference.Display))
+            //TODO: handle ModuleSymbol
+            FSharpAssemblySymbol(fsharpAssembly) :> ISymbol
+
+        member x.Assembly =
+            FSharpAssemblySymbol(assemblySignature, outputFile) :> _
---
+++ main/src/core/MonoDevelop.Core/MonoDevelop.Core/ICompilation.cs
@@ -0,0 +1,58 @@
+﻿//
+// ICompilation.cs
+//
+// Author:
+//       jason <jaimison@microsoft.com>
+//
+// Copyright (c) 2018 Microsoft
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in
+// all copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+// THE SOFTWARE.
+
+using Microsoft.CodeAnalysis;
+using System.Collections.Generic;
+
+namespace MonoDevelop.Core
+{
+	public interface IRoslynCompilation
+	{
+		INamedTypeSymbol GetTypeByMetadataName(string fullyQualifiedMetadataName);
+		IEnumerable<MetadataReference> References { get; }
+		ISymbol GetAssemblyOrModuleSymbol(MetadataReference reference);
+		IAssemblySymbol Assembly { get; }
+	}
+
+	class CompilationWrapper : IRoslynCompilation
+	{
+		private readonly Compilation compilation;
+
+		public CompilationWrapper (Compilation compilation)
+		{
+			this.compilation = compilation ?? throw new System.ArgumentNullException (nameof (compilation));
+		}
+		public IEnumerable<MetadataReference> References => compilation.References;
+
+		public IAssemblySymbol Assembly => compilation.Assembly;
+
+		public ISymbol GetAssemblyOrModuleSymbol (MetadataReference reference) =>
+			compilation.GetAssemblyOrModuleSymbol(reference);
+
+		public INamedTypeSymbol GetTypeByMetadataName (string fullyQualifiedMetadataName) =>
+			compilation.GetTypeByMetadataName(fullyQualifiedMetadataName);
+	}
+}
