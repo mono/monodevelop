3af51ed8b748998c50d43413ea1b955f0227817f
Jose Medrano <jose.medrano@microsoft.com>
2018-07-18 10:42:20 +0200

    [StructuredOutput] Click outside text area removes the current selection

--- main/src/core/MonoDevelop.Ide/MonoDevelop.Ide.BuildOutputView/BuildOutputTreeCellView.cs
+++ main/src/core/MonoDevelop.Ide/MonoDevelop.Ide.BuildOutputView/BuildOutputTreeCellView.cs
@@ -332,6 +332,7 @@ namespace MonoDevelop.Ide.BuildOutputView
 		const int MinLayoutWidth = 30;
 
 		const int DefaultExpandClickDelay = 250;
+
 		DateTime lastExpanderClick = DateTime.Now;
 
 		public Color StrongSelectionColor { get; set; }
@@ -818,11 +819,16 @@ namespace MonoDevelop.Ide.BuildOutputView
 					TextSelection.Stop ();
 					QueueDraw ();
 				} else if (TextSelection.State == TextSelectionState.Clicked) {
-					TextSelection = null;
-					QueueDraw ();
+					ClearSelection ();
 				}
 			}
 			base.OnButtonReleased (args);
+		}
+
+		public void ClearSelection ()
+		{
+			TextSelection = null;
+			QueueDraw ();
 		}
 
 		protected override void OnMouseExited ()
