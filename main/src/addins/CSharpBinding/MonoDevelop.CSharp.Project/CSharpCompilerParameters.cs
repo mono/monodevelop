8bedaa9c8f34c8a33e0a6e3694976ce2274eb3e7
therzok <marius.ungureanu@xamarin.com>
2018-08-30 17:19:17 +0300

    [Ide] Fix typo's in ruleset code

--- main/src/addins/CSharpBinding/MonoDevelop.CSharp.Project/CSharpCompilerParameters.cs
+++ main/src/addins/CSharpBinding/MonoDevelop.CSharp.Project/CSharpCompilerParameters.cs
@@ -170,7 +170,7 @@ namespace MonoDevelop.CSharp.Project
 			foreach (var warning in GetSuppressedWarnings ())
 				result [warning] = ReportDiagnostic.Suppress;
 
-			var globalRuleSet = Ide.TypeSystem.MonoDevelopRuleSetManager.GetGloabalRuleSet ();
+			var globalRuleSet = Ide.TypeSystem.MonoDevelopRuleSetManager.GetGlobalRuleSet ();
 			if (globalRuleSet != null) {
 				foreach (var kv in globalRuleSet.SpecificDiagnosticOptions) {
 					result [kv.Key] = kv.Value;
