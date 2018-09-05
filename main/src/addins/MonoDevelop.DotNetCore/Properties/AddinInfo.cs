8447fab59d218d1e94ed3856c418f7a0cb98121d
Matt Ward <matt.ward@microsoft.com>
2018-09-05 18:19:40 +0100

    [DotNetCore] Support alternative project type guid used by Visual Studio
    Visual Studio 2017 will sometimes change the project type guid used
    in a solution file from the default C# project type guid to
    9A19103F-16F7-4668-BE54-9A1E7A4F7556 due to a bug:
    
    https://github.com/dotnet/project-system/issues/1821
    
    Visual Studio for Mac uses the C# project type guid which results in
    the solution file being changed when it is saved. To avoid this a new
    MSBuildItemType has been added for the alternative project type guid so
    that the .NET Core project when loaded will have the correct project
    type guid and when the solution file is saved the project type guid
    will not be modified. Note that new C# .NET Core and .NET Standard
    projects will continue to use the project type guid used by C#
    projects AE04EC0-301F-11D3-BF4B-00C04F79EFBC which is also what Visual
    Studio 2017 on Windows uses for new projects.
    
    Fixes VSTS #677549 - Project GUID Changes When Building in VS2017 vs
    Building in VS for Mac

--- main/src/addins/MonoDevelop.DotNetCore/Properties/AddinInfo.cs
+++ main/src/addins/MonoDevelop.DotNetCore/Properties/AddinInfo.cs
@@ -13,6 +13,7 @@ using Mono.Addins.Description;
 
 [assembly:AddinDependency ("Core", MonoDevelop.BuildInfo.Version)]
 [assembly:AddinDependency ("Ide", MonoDevelop.BuildInfo.Version)]
+[assembly:AddinDependency ("CSharpBinding", MonoDevelop.BuildInfo.Version)]
 [assembly:AddinDependency ("Debugger", MonoDevelop.BuildInfo.Version)]
 [assembly:AddinDependency ("Debugger.VsCodeDebugProtocol", MonoDevelop.BuildInfo.Version)]
 [assembly:AddinDependency ("PackageManagement", MonoDevelop.BuildInfo.Version)]
