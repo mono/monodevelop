d23ce0f781c021cc9cc10c6f22f657b892167f5f
Matt Ward <matt.ward@microsoft.com>
2018-09-12 15:24:13 +0100

    [MacPlatform] Add HttpClient options panel
    Added a new options panel that can be used to switch between
    NSUrlSession and a managed (mono) implementation when using the
    HttpClient. This options panel is available from Other - Network
    and has a combo box that can switch between the two http client
    handler implementations. NSUrlSession is used by default. Currently
    this does not affect when http client handler is used by the IDE.

---
+++ main/src/addins/MacPlatform/Dialogs/HttpClientOptionsPanel.cs
@@ -0,0 +1,61 @@
+//
+// HttpClientOptionsPanel.cs
+//
+// Author:
+//       Matt Ward <matt.ward@microsoft.com>
+//
+// Copyright (c) 2018 Microsoft
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in
+// all copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+// THE SOFTWARE.
+
+using MonoDevelop.Components;
+using MonoDevelop.Ide.Gui.Dialogs;
+
+namespace MonoDevelop.MacIntegration
+{
+	class HttpClientOptionsPanel : OptionsPanel
+	{
+		Control control;
+		HttpClientOptionsWidget widget;
+
+		public override void ApplyChanges ()
+		{
+			widget.ApplyChanges ();
+		}
+
+		public override Control CreatePanelWidget ()
+		{
+			if (control == null) {
+				widget = new HttpClientOptionsWidget ();
+				control = widget.ToGtkWidget ();
+			}
+
+			return control;
+		}
+
+		public override void Dispose ()
+		{
+			if (control != null) {
+				control.Dispose ();
+				widget.Dispose ();
+			}
+			base.Dispose ();
+		}
+	}
+}
