a59c0f71f6c2e50d8769cc2c7e1e81f51ccb6cac
Jose Miguel Torres <josemiguel@xamarin.com>
2018-10-03 14:36:22 +0200

    [AspNetCore] Implements PublishToFolder

---
+++ main/src/addins/MonoDevelop.AspNetCore/MonoDevelop.AspNetCore.Commands/ProjectPublishProfile.cs
@@ -0,0 +1,24 @@
+﻿namespace MonoDevelop.AspNetCore.Commands
+{
+	using MonoDevelop.Core.Serialization;
+
+	[DataItem]
+	sealed public class ProjectPublishProfile
+	{
+		public const string ProjectPublishProfileKey = "PublishProfiles";
+
+		[ItemProperty]
+		public string Name { get; set; }
+
+		[ItemProperty]
+		public string FileName { get; set; }
+
+		public ProjectPublishProfile () {}
+		 
+		public ProjectPublishProfile (string name, string fileName)
+		{
+			this.FileName = fileName;
+			this.Name = name;
+		}
+	}
+}
---
+++ main/src/addins/MonoDevelop.AspNetCore/ProjectExtensions.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using MonoDevelop.AspNetCore.Commands;
+using MonoDevelop.Projects;
+
+namespace MonoDevelop.AspNetCore
+{
+	public static class ProjectExtensions
+	{
+		public static ProjectPublishProfile [] GetPublishProfiles (this DotNetProject project)
+		{
+			if (project == null || !project.UserProperties.HasValue (ProjectPublishProfile.ProjectPublishProfileKey)) {
+				return new ProjectPublishProfile [0];
+			}
+
+			var profiles = new List<ProjectPublishProfile> ();
+
+			foreach (var item in project.UserProperties.GetValue<ProjectPublishProfile []> (ProjectPublishProfile.ProjectPublishProfileKey)) {
+				profiles.Add (item);
+			}
+
+			return profiles.ToArray ();
+		}
+
+		public static void AddPublishProfiles (this DotNetProject project, ProjectPublishProfile newEntry)
+		{
+			var profiles = new List<ProjectPublishProfile> ();
+			if (project.UserProperties.HasValue (ProjectPublishProfile.ProjectPublishProfileKey)) {
+				foreach (var item in project.UserProperties.GetValue<ProjectPublishProfile []> (ProjectPublishProfile.ProjectPublishProfileKey)) {
+					if (item.Name.IndexOf (newEntry.Name, StringComparison.OrdinalIgnoreCase) < 0)
+						profiles.Add (item);
+				}
+			}
+			profiles.Add (newEntry);
+			project.UserProperties.SetValue<ProjectPublishProfile []> (ProjectPublishProfile.ProjectPublishProfileKey, profiles.ToArray());
+		}
+	}
+}
