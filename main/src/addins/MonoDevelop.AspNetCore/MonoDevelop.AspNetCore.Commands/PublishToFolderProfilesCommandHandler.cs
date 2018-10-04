a65391723ca6dcd0a6be39513cd2db399096c36c
Jose Miguel Torres <josemiguel@xamarin.com>
2018-10-04 12:36:53 +0200

    [AspNetCore] Splits PublishToFolder Command

---
+++ main/src/addins/MonoDevelop.AspNetCore/MonoDevelop.AspNetCore.Commands/PublishToFolderProfilesCommandHandler.cs
@@ -0,0 +1,31 @@
+﻿using MonoDevelop.Components.Commands;
+using MonoDevelop.Core;
+using MonoDevelop.Ide;
+using MonoDevelop.Projects;
+
+namespace MonoDevelop.AspNetCore.Commands
+{
+	sealed class PublishToFolderProfilesCommandHandler : PublishToFolderBaseCommandHandler
+	{
+		protected override void Update (CommandArrayInfo info)
+		{
+			base.Update (info);
+
+			project = IdeApp.ProjectOperations.CurrentSelectedProject as DotNetProject;
+
+			if (!ProjectSupportsAzurePublishing (project)) {
+				return;
+			}
+
+
+			var profiles = project.GetPublishProfiles ();
+			if (profiles.Length > 0) {
+				info.AddSeparator ();
+			}
+
+			foreach (var profile in profiles) {
+				info.Add (GettextCatalog.GetString ("Publish to {0} - Folder", profile.Name), new PublishCommandItem (project, profile));
+			}
+		}
+	}
+}
