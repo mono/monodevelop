9a516f32f8d5b86bc6c4befc29b736b1ea833df3
Mike Krüger <mikkrg@microsoft.com>
2018-10-09 09:10:13 +0200

    Fixes VSTS Bug 699450: [Feedback] Error creating file - NullReferenceException: Object reference not set to an instance of an object
    https://devdiv.visualstudio.com/DevDiv/_workitems/edit/699450

--- main/src/core/MonoDevelop.Ide/MonoDevelop.Ide.Editor/DocumentContext.cs
+++ main/src/core/MonoDevelop.Ide/MonoDevelop.Ide.Editor/DocumentContext.cs
@@ -39,7 +39,7 @@ namespace MonoDevelop.Ide.Editor
 	/// A document context puts a textual document in a semantic context inside a project and gives access
 	/// to the parse information of the textual document.
 	/// </summary>
-	public abstract class DocumentContext
+	public abstract class DocumentContext : IDisposable
 	{
 		/// <summary>
 		/// The name of the document. It's the file name for files on disc. 
@@ -186,5 +186,15 @@ namespace MonoDevelop.Ide.Editor
 		internal virtual void UpdateDocumentId (Microsoft.CodeAnalysis.DocumentId newId)
 		{
 		}
+
+		#region IDisposable Support
+
+		public bool IsDisposed { get; private set; }
+
+		public virtual void Dispose ()
+		{
+			IsDisposed = true;
+		}
+		#endregion
 	}
 }
