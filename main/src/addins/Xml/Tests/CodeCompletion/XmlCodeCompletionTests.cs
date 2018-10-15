0ac99f626d9e0b59ec1a067cfee7e6bd9b4a4970
David Karlaš <david.karlas@microsoft.com>
2018-10-15 11:14:32 +0200

    Reverted changed that caused C# unit tests to fail and set `CompletionWidget` from unit test instead

--- main/src/addins/Xml/Tests/CodeCompletion/XmlCodeCompletionTests.cs
+++ main/src/addins/Xml/Tests/CodeCompletion/XmlCodeCompletionTests.cs
@@ -18,7 +18,7 @@ namespace MonoDevelop.Xml.Tests.CodeCompletion
 			using (var testCase = await SetupTestCase ("<", 1)) {
 				var doc = testCase.Document;
 				var ext = doc.GetContent<BaseXmlEditorExtension> ();
-
+				ext.CompletionWidget = doc.Editor.GetViewContent().GetContent<ICompletionWidget> ();
 				await ext.TriggerCompletion (Ide.CodeCompletion.CompletionTriggerReason.CompletionCommand);
 				ext.KeyPress (KeyDescriptor.FromGtk (Gdk.Key.colon, ':', Gdk.ModifierType.None));
 				Assert.IsTrue (CompletionWindowManager.IsVisible);
