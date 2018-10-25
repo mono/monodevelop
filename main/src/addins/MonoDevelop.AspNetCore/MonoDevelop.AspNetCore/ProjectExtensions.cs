bb8c4fd00c1228df2acfe14a8008edcdb1e34df9
Jose Miguel Torres <josemiguel@xamarin.com>
2018-10-25 07:33:06 +0200

    [AspNetCore] Checks publish profiles directory exists before enumerating.
    - This prevents a DirectoryNotFound exception since we are always enumerating (and assuming) that such directory exists.

--- main/src/addins/MonoDevelop.AspNetCore/MonoDevelop.AspNetCore/ProjectExtensions.cs
+++ main/src/addins/MonoDevelop.AspNetCore/MonoDevelop.AspNetCore/ProjectExtensions.cs
@@ -1,9 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.IO;
-using System.Text;
-using System.Xml;
-using System.Xml.Serialization;
 using MonoDevelop.AspNetCore.Commands;
 using MonoDevelop.Ide;
 using MonoDevelop.Projects;
