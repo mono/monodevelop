6c4299a8a4cb562e35b35e3dcb757272e403f7fa
Jose Medrano <jose.medrano@microsoft.com>
2018-10-30 12:27:00 +0100

    Fixes visual issues in Expander button

--- main/src/addins/MonoDevelop.DesignerSupport/MonoDevelop.DesignerSupport.Toolbox/Styles.cs
+++ main/src/addins/MonoDevelop.DesignerSupport/MonoDevelop.DesignerSupport.Toolbox/Styles.cs
@@ -11,6 +11,29 @@ namespace MonoDevelop.DesignerSupport.Toolbox
 
 		public static NSColor CellBackgroundSelectedColor { get; private set; } = NSColor.SelectedTextBackground;
 		public static NSColor CellBackgroundColor { get; private set; } = NSColor.ControlBackground;
+		public static NSColor LabelSelectedForegroundColor { get; private set; }
+
+		public static NSColor HeaderBackgroundColor { get; private set; }
+		public static NSColor HeaderBorderBackgroundColor { get; private set; }
+
+		static Styles ()
+		{
+			LoadStyles ();
+			Ide.Gui.Styles.Changed += (o, e) => LoadStyles ();
+		}
+
+		public static void LoadStyles ()
+		{
+			if (IdeApp.Preferences.UserInterfaceTheme == Theme.Light) {
+				HeaderBackgroundColor = NSColor.FromRgb (0.98f, 0.98f, 0.98f);
+				HeaderBorderBackgroundColor = NSColor.FromRgb (0.96f, 0.96f, 0.96f);
+				LabelSelectedForegroundColor = NSColor.Highlight;
+			} else {
+				HeaderBackgroundColor = NSColor.FromRgb (0.29f, 0.29f, 0.29f);
+				HeaderBorderBackgroundColor = NSColor.FromRgb (0.29f, 0.29f, 0.29f);
+				LabelSelectedForegroundColor = NSColor.SelectedText;
+			}
+		}
 	}
 }
 #endif
