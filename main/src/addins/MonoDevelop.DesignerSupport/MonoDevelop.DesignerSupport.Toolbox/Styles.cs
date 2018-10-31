693085c4d5518f398bde9f896c373f9d1bd1c01f
Jose Medrano <jose.medrano@microsoft.com>
2018-10-31 01:46:40 +0100

    Fixes selection color and with lost/got focus

--- main/src/addins/MonoDevelop.DesignerSupport/MonoDevelop.DesignerSupport.Toolbox/Styles.cs
+++ main/src/addins/MonoDevelop.DesignerSupport/MonoDevelop.DesignerSupport.Toolbox/Styles.cs
@@ -9,7 +9,8 @@ namespace MonoDevelop.DesignerSupport.Toolbox
 		public static NSColor SectionForegroundColor { get; internal set; } = NSColor.Text;
 		public static NSColor SectionBackgroundColor { get; private set; } = NSColor.ControlLightHighlight;
 
-		public static NSColor CellBackgroundSelectedColor { get; private set; } = NSColor.SelectedTextBackground;
+		public static NSColor CellBackgroundSelectedColor { get; private set; } 
+		public static NSColor CellBackgroundUnfocusedSelectedColor { get; private set; } = NSColor.FromRgb (0.64f, 0.64f, 0.64f);
 		public static NSColor CellBackgroundColor { get; private set; } = NSColor.ControlBackground;
 		public static NSColor LabelSelectedForegroundColor { get; private set; }
 
@@ -31,7 +32,9 @@ namespace MonoDevelop.DesignerSupport.Toolbox
 				HeaderBorderBackgroundColor = NSColor.FromRgb (0.96f, 0.96f, 0.96f);
 				LabelSelectedForegroundColor = NSColor.Highlight;
 				ToolbarBackgroundColor = NSColor.White;
+				CellBackgroundSelectedColor = NSColor.FromRgb (0.36f, 0.54f, 0.90f);
 			} else {
+				CellBackgroundSelectedColor = NSColor.FromRgb (0.38f, 0.55f, 0.91f);
 				HeaderBackgroundColor = NSColor.FromRgb (0.29f, 0.29f, 0.29f);
 				HeaderBorderBackgroundColor = NSColor.FromRgb (0.29f, 0.29f, 0.29f);
 				LabelSelectedForegroundColor = NSColor.SelectedText;
