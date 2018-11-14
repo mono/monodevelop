90bb7a0b5d305f86fcb131899b8c76dcc95ee6b9
Kirill Osenkov <github@osenkov.com>
2018-11-14 14:53:33 -0800

    Add standalone Undo.

---
+++ main/src/addins/MonoDevelop.TextEditor/Undo/UndoHistoryRegistry.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.ComponentModel.Composition;
+using System.Runtime.CompilerServices;
+using Microsoft.VisualStudio.Text.Operations;
+
+namespace MonoDevelop.Ide.Text
+{
+    /// <summary>
+    /// This class is intended to be a very simple ITextUndoHistoryRegistry implementation for hosts that
+    /// don't have a built-in undo mechanism
+    /// </summary>
+    [Export(typeof(ITextUndoHistoryRegistry))]
+    internal sealed class TextUndoHistoryRegistry : ITextUndoHistoryRegistry
+    {
+        private readonly ConditionalWeakTable<object, ITextUndoHistory> _map = new ConditionalWeakTable<object, ITextUndoHistory>();
+
+        internal TextUndoHistoryRegistry()
+        {
+        }
+
+        private bool TryGetHistory(object context, out ITextUndoHistory undoHistory)
+        {
+            return _map.TryGetValue(context, out undoHistory);
+        }
+
+        #region ITextUndoHistoryRegistry
+
+        /// <summary>
+        /// Easy to implement but the Visual Studio implementation throws a NotSupportedException
+        /// </summary>
+        void ITextUndoHistoryRegistry.AttachHistory(object context, ITextUndoHistory history)
+        {
+            throw new NotSupportedException();
+        }
+
+        ITextUndoHistory ITextUndoHistoryRegistry.GetHistory(object context)
+        {
+            ITextUndoHistory history;
+            _map.TryGetValue(context, out history);
+            return history;
+        }
+
+        ITextUndoHistory ITextUndoHistoryRegistry.RegisterHistory(object context)
+        {
+            ITextUndoHistory history;
+            if (!_map.TryGetValue(context, out history))
+            {
+                history = new UndoHistory(context);
+                _map.Add(context, history);
+            }
+            return history;
+        }
+
+        void ITextUndoHistoryRegistry.RemoveHistory(ITextUndoHistory history)
+        {
+            var undoHistory = history as UndoHistory;
+            if (undoHistory != null)
+            {
+                _map.Remove(undoHistory.Context);
+                undoHistory.Clear();
+            }
+        }
+
+        bool ITextUndoHistoryRegistry.TryGetHistory(object context, out ITextUndoHistory history)
+        {
+            ITextUndoHistory undoHistory;
+            if (TryGetHistory(context, out undoHistory))
+            {
+                history = undoHistory;
+                return true;
+            }
+
+            history = null;
+            return false;
+        }
+
+        #endregion
+    }
+}
---
+++ main/src/addins/MonoDevelop.TextEditor/Undo/UndoTransaction.cs
@@ -0,0 +1,158 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.VisualStudio.Text.Operations;
+
+namespace MonoDevelop.Ide.Text
+{
+    internal sealed class UndoTransaction : ITextUndoTransaction
+    {
+        private readonly UndoHistory _textUndoHistory;
+        private readonly List<ITextUndoPrimitive> _primitiveList = new List<ITextUndoPrimitive>();
+        private bool _completed;
+
+        internal string Description
+        {
+            get;
+            set;
+        }
+
+        internal List<ITextUndoPrimitive> UndoPrimitives
+        {
+            get { return _primitiveList; }
+        }
+
+        internal bool CanRedo
+        {
+            get { return _primitiveList.All(x => x.CanRedo); }
+        }
+
+        internal bool CanUndo
+        {
+            get { return _primitiveList.All(x => x.CanUndo); }
+        }
+
+        internal IMergeTextUndoTransactionPolicy MergePolicy
+        {
+            get;
+            set;
+        }
+
+        internal UndoTransaction(UndoHistory textUndoHistory, string description)
+        {
+            _textUndoHistory = textUndoHistory;
+            Description = description;
+        }
+
+        internal void AddUndo(ITextUndoPrimitive undo)
+        {
+            System.Diagnostics.Debug.Assert(undo.CanUndo);
+            _primitiveList.Add(undo);
+            undo.Parent = this;
+        }
+
+        private void HandleCompleted()
+        {
+            if (_completed)
+            {
+                throw new InvalidOperationException();
+            }
+
+            _completed = true;
+        }
+
+        #region ITextUndoTransaction
+
+        void ITextUndoTransaction.AddUndo(ITextUndoPrimitive undo)
+        {
+            AddUndo(undo);
+        }
+
+        /// <summary>
+        /// Visual Studio implementation throw so duplicate here
+        /// </summary>
+        bool ITextUndoTransaction.CanRedo
+        {
+            get { return CanRedo; }
+        }
+
+        /// <summary>
+        /// Visual Studio implementation throw so duplicate here
+        /// </summary>
+        bool ITextUndoTransaction.CanUndo
+        {
+            get { return CanUndo; }
+        }
+
+        ITextUndoHistory ITextUndoTransaction.History
+        {
+            get { return _textUndoHistory; }
+        }
+
+        IMergeTextUndoTransactionPolicy ITextUndoTransaction.MergePolicy
+        {
+            get { return MergePolicy; }
+            set { MergePolicy = value; }
+        }
+
+        ITextUndoTransaction ITextUndoTransaction.Parent
+        {
+            get { throw new NotSupportedException(); }
+        }
+
+        IList<ITextUndoPrimitive> ITextUndoTransaction.UndoPrimitives
+        {
+            get { return UndoPrimitives; }
+        }
+
+        UndoTransactionState ITextUndoTransaction.State
+        {
+            get { throw new NotSupportedException(); }
+        }
+
+        string ITextUndoTransaction.Description
+        {
+            get { return Description; }
+            set { Description = value; }
+        }
+
+        void ITextUndoTransaction.Cancel()
+        {
+            HandleCompleted();
+            _textUndoHistory.OnTransactionClosed(this, didComplete: false);
+        }
+
+        void ITextUndoTransaction.Complete()
+        {
+            HandleCompleted();
+            _textUndoHistory.OnTransactionClosed(this, didComplete: true);
+        }
+
+        void ITextUndoTransaction.Do()
+        {
+            for (var i = 0; i < _primitiveList.Count; i++)
+            {
+                _primitiveList[i].Do();
+            }
+        }
+
+        void ITextUndoTransaction.Undo()
+        {
+            for (var i = _primitiveList.Count - 1; i >= 0; i--)
+            {
+                _primitiveList[i].Undo();
+            }
+        }
+
+        #endregion
+
+        #region IDisposable
+
+        void IDisposable.Dispose()
+        {
+            _textUndoHistory.OnTransactionClosed(this, didComplete: false);
+        }
+
+        #endregion
+    }
+}
