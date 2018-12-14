ed1d1c242cae9a7340f588d3425d3cfb299dd98c
Jose Medrano <jose.medrano@microsoft.com>
2018-12-14 18:53:42 +0100

    Adds DirectoryPathPropertyInfo to handle Directory Editors

--- main/src/addins/MonoDevelop.DesignerSupport/MonoDevelop.DesignerSupport/NativePropertyEditors/PropertyPadObjectEditor.cs
+++ main/src/addins/MonoDevelop.DesignerSupport/MonoDevelop.DesignerSupport/NativePropertyEditors/PropertyPadObjectEditor.cs
@@ -109,6 +109,10 @@ namespace MonoDevelop.DesignerSupport
 				return new FilePathPropertyInfo (propertyDescriptor, PropertyProvider, valueSources);
 			}
 
+			if (propertyDescriptor.PropertyType.IsAssignableFrom (typeof (Core.FilePath))) {
+				return new DirectoryPathPropertyInfo (propertyDescriptor, PropertyProvider, valueSources);
+			}
+
 			if (HasStandardValues (propertyDescriptor)) {
 				return new StringStandardValuesPropertyInfo (propertyDescriptor, PropertyProvider, valueSources);
 			}
