d0ad46d956c882bd7db64947e7e7dc52e3ba2e0f
Rodrigo Moya <rodrigo.moya@xamarin.com>
2018-12-24 11:15:01 +0100

    [Core] Allow passing a default value to FeatureSwitchService
    So that it can be used if no explicit checkers are found. Also, add tests
    for the new FeatureSwitchService

--- main/tests/MonoDevelop.Core.Tests/MonoDevelop.Core/FeatureSwitchServiceTests.cs
+++ main/tests/MonoDevelop.Core.Tests/MonoDevelop.Core/FeatureSwitchServiceTests.cs
@@ -27,72 +27,20 @@
 using System;
 using MonoDevelop.Core.FeatureConfiguration;
 using NUnit.Framework;
+using Mono.Addins;
+using UnitTests;
 
 namespace MonoDevelop.Core
 {
 	[TestFixture]
-	public class FeatureSwitchServiceTests
+	public class FeatureSwitchServiceTests : TestBase
 	{
-		[TestCase ("7.6", true)]
-		[TestCase (BuildInfo.FullVersion, true)]
-		[TestCase ("100.1", false)]
-		public void CanCheckForVersionCondition (string version, bool expected)
-		{
-			var cond = new VersionGreaterThanFeatureSwitchCondition (version);
-			Assert.That (cond.Evaluate (), Is.EqualTo (expected));
-		}
-
-		[TestCase ("FeatureSwitchServiceTests", null)]
-		[TestCase ("FeatureSwitchServiceTests", "value")]
-		public void CanCheckForEnvironmentVar (string variable, string value)
-		{
-			var cond = new EnvVarExistsFeatureSwitchCondition (variable, value);
-
-			// Check it returns false without the env var defined
-			Assert.False (cond.Evaluate ());
-
-			// Check the var exists and value is correct
-			Environment.SetEnvironmentVariable (variable, value ?? "True");
-			Assert.True (cond.Evaluate ());
-
-			// Check the value is compared
-			if (!string.IsNullOrEmpty (value)) {
-				Environment.SetEnvironmentVariable (variable, value.Insert (0, "insert"));
-				Assert.False (cond.Evaluate ());
-			}
-		}
-
 		[Test]
-		public void CanAggregateConditions ()
+		public void CanEnableWithEnvVar ()
 		{
-			Environment.SetEnvironmentVariable ("FeatureSwitchServiceTests", "1");
-
-			// Check when all conditions are met
-			var cond = new AggregatedFeatureSwitchCondition (true,
-				new VersionGreaterThanFeatureSwitchCondition (BuildInfo.FullVersion),
-				new EnvVarExistsFeatureSwitchCondition ("FeatureSwitchServiceTests", null)
-			);
-			Assert.True (cond.Evaluate ());
-
-			// Check when one condition is not met
-			Environment.SetEnvironmentVariable ("FeatureSwitchServiceTests", null);
-			Assert.False (cond.Evaluate ());
-
-			// Check when any condition is met
-			cond = new AggregatedFeatureSwitchCondition (false,
-				new VersionGreaterThanFeatureSwitchCondition (BuildInfo.FullVersion),
-				new EnvVarExistsFeatureSwitchCondition ("FeatureSwitchServiceTests", null)
-			);
-			Assert.True (cond.Evaluate ());
-		}
-
-		[TestCase (BuildInfo.FullVersion, true)]
-		[TestCase ("100.1", false)]
-		public void CanRegisterSwitches (string version, bool expected)
-		{
-			var feature = FeatureSwitchService.RegisterFeature (Guid.NewGuid ().ToString (),
-				new VersionGreaterThanFeatureSwitchCondition (version));
-			Assert.That (feature.IsEnabled, Is.EqualTo (expected));
+			Assert.False (FeatureSwitchService.IsFeatureEnabled ("MonoDevelop.Core.FeatureSwitchTests", false));
+			Environment.SetEnvironmentVariable ("MD_FEATURES_ENABLED", "MonoDevelop.Core.FeatureSwitchTests");
+			Assert.True (FeatureSwitchService.IsFeatureEnabled ("MonoDevelop.Core.FeatureSwitchTests", false));
 		}
 	}
 }
