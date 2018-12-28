d960f6aef950d5e87473dbf6fac712ca6f698875
Mikayla Hutchinson <m.j.hutchinson@gmail.com>
2018-12-28 00:25:20 -0500

    Add project values to custom tool standard values

--- main/src/addins/MonoDevelop.DesignerSupport/MonoDevelop.DesignerSupport.Projects/ProjectFileDescriptor.CustomToolStringsConverter.cs
+++ main/src/addins/MonoDevelop.DesignerSupport/MonoDevelop.DesignerSupport.Projects/ProjectFileDescriptor.CustomToolStringsConverter.cs
@@ -22,22 +22,75 @@
 //
 
 using System;
-using System.ComponentModel;
 using System.Collections;
-using MonoDevelop.Ide.Extensions;
-using Mono.Addins;
+using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
+using Mono.Addins;
+using MonoDevelop.Ide.Extensions;
 
 namespace MonoDevelop.DesignerSupport
 {
 	partial class ProjectFileDescriptor
 	{
+		[Components.PropertyGrid.PropertyEditors.StandardValuesSeparator ("--")]
 		class CustomToolStringsConverter : StandardStringsConverter
 		{
 			public override ICollection GetStandardStrings (ITypeDescriptorContext context)
 			{
+				var dedup = new HashSet<string> (StringComparer.OrdinalIgnoreCase);
+				var values = new List<string> ();
+
+				var file = (context?.Instance as ProjectFileDescriptor)?.file;
+
+				//add sections with values that other files in the project have
+				//first, a section with values that files with this file's extension have
+				//then,  a section with  values that other files have
+				if (file?.Project != null) {
+					var extension = file.FilePath.Extension;
+					var defer = new HashSet<string> ();
+					foreach (var f in file.Project.Files) {
+						if (!string.IsNullOrEmpty (f.Generator)) {
+							if (f.FilePath.HasExtension (extension)) {
+								if (dedup.Add (f.Generator)) {
+									values.Add (f.Generator);
+								}
+							} else {
+								defer.Add (f.Generator);
+							}
+						}
+					}
+					if (defer.Count > 0) {
+						bool addSep = values.Count > 0;
+						foreach (var v in defer) {
+							if (dedup.Add (v)) {
+								if (addSep) {
+									values.Add ("--");
+									addSep = false;
+								}
+								values.Add (v);
+							}
+						}
+					}
+				}
+
+				//add a section with values that extensions can handle
 				var nodes = AddinManager.GetExtensionNodes<CustomToolExtensionNode> ("/MonoDevelop/Ide/CustomTools");
-				return nodes.Select (n => n.Name).Distinct (StringComparer.OrdinalIgnoreCase).ToList ();
+				if (nodes.Count > 0) {
+					bool addSep = values.Count > 0;
+					foreach (var n in nodes) {
+						if (dedup.Add (n.Name)) {
+							if (addSep) {
+								values.Add ("--");
+								addSep = false;
+							}
+							values.Add (n.Name);
+						}
+					}
+
+				}
+
+				return values;
 			}
 		}
 	}
