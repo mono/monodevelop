e9646f8c329f9f4132263bc46e38a6d329f9bcdd
Sandy Armstrong <sandy@xamarin.com>
2019-01-11 16:05:52 -0800

    Experiments: Enable async completion when using new editor

---
+++ main/src/addins/CSharpBinding/MonoDevelop.Ide.Completion.Presentation/RoslynExperimentationService.cs
@@ -0,0 +1,45 @@
+//
+// RoslynExperimentationService.cs
+//
+// Author:
+//       Sandy Armstrong <sandy@xamarin.com>
+//
+// Copyright (c) 2019 Microsoft Corporation
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in
+// all copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+// THE SOFTWARE.
+
+using System.Composition;
+
+using Microsoft.CodeAnalysis.Experiments;
+using Microsoft.CodeAnalysis.Host.Mef;
+
+namespace MonoDevelop.Ide.Composition
+{
+	[ExportWorkspaceService (typeof (IExperimentationService), ServiceLayer.Editor), Shared]
+	class RoslynExperimentationService: IExperimentationService
+	{
+		readonly EditorExperimentationServiceInternal editorExperimentationService
+			= new EditorExperimentationServiceInternal ();
+
+		public bool IsExperimentEnabled (string experimentName)
+		{
+			return editorExperimentationService.IsCachedFlightEnabled (experimentName);
+		}
+	}
+}
--- main/src/core/MonoDevelop.Ide/MonoDevelop.Ide.Composition/EditorExperimentationServiceInternal.cs
+++ main/src/core/MonoDevelop.Ide/MonoDevelop.Ide.Composition/EditorExperimentationServiceInternal.cs
@@ -1,11 +1,15 @@
 using System.ComponentModel.Composition;
+
 using Microsoft.VisualStudio.Text.Utilities;
+
+using MonoDevelop.Ide.Editor;
 
 namespace MonoDevelop.Ide.Composition
 {
 	[Export (typeof (IExperimentationServiceInternal))]
 	internal class EditorExperimentationServiceInternal : IExperimentationServiceInternal
 	{
-		public bool IsCachedFlightEnabled (string flightName) => false;
+		public bool IsCachedFlightEnabled (string flightName)
+			=> flightName == "CompletionAPI" && DefaultSourceEditorOptions.Instance.EnableNewEditor;
 	}
 }
