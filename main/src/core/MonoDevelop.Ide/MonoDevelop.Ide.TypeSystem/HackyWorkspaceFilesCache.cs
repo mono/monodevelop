fb0a29a8421396fc6ffb2886a652d79d71cc8949
therzok <marius.ungureanu@xamarin.com>
2019-01-18 00:32:28 +0200

    Simplify

--- main/src/core/MonoDevelop.Ide/MonoDevelop.Ide.TypeSystem/HackyWorkspaceFilesCache.cs
+++ main/src/core/MonoDevelop.Ide/MonoDevelop.Ide.TypeSystem/HackyWorkspaceFilesCache.cs
@@ -39,9 +39,7 @@ namespace MonoDevelop.Ide.TypeSystem
 		readonly bool? enabled = FeatureSwitchService.IsFeatureEnabled ("HackyCache");
 
 		readonly FilePath cacheDir;
-		readonly FilePath mappingFile;
 
-		TypesystemCache controlCache = new TypesystemCache ();
 		Dictionary<FilePath, ProjectCache> cachedItems = new Dictionary<FilePath, ProjectCache> ();
 
 		public HackyWorkspaceFilesCache (Solution solution, ConfigurationSelector configuration)
@@ -52,55 +50,30 @@ namespace MonoDevelop.Ide.TypeSystem
 			cacheDir = solution.GetPreferencesDirectory ().Combine ("project-cache");
 			Directory.CreateDirectory (cacheDir);
 
-			mappingFile = cacheDir.Combine ("project-map.json");
-
 			LoadCache (solution);
 		}
 
 		void LoadCache (Solution sol)
 		{
-			try {
-				var serializer = new JsonSerializer ();
-				using (var sr = File.OpenText (mappingFile)) {
-					controlCache = (TypesystemCache)serializer.Deserialize (sr, typeof (TypesystemCache));
+			var solConfig = sol.GetConfiguration (IdeApp.Workspace.ActiveConfiguration);
+			var allProjects = sol.GetAllProjects ().ToDictionary (x => x.FileName, x => x);
 
-					var lastWrite = File.GetLastWriteTimeUtc (sol.FileName);
-					if (lastWrite != controlCache.SolutionTimestamp)
-						return;
-
-					var solConfig = sol.GetConfiguration (IdeApp.Workspace.ActiveConfiguration);
-					var allProjects = sol.GetAllProjects ().ToDictionary (x => x.FileName, x => x);
-
-					foreach (var project in controlCache.ProjectMap) {
-						if (!allProjects.TryGetValue (project.ProjectFilePath, out var mdProject))
-							continue;
-
-						var config = solConfig.GetMappedConfiguration (mdProject);
-						if (config != project.Configuration)
-							continue;
-
-						LoadProjectCacheItem (mdProject.FileName, project.CacheFilePath);
-					}
-				}
-			} catch (Exception ex) {
-				LoggingService.LogError ("Could not deserialize typesystem cache", ex);
-				TryDeleteFile (mappingFile);
-				return;
-			}
-		}
-
-		void LoadProjectCacheItem (FilePath projectFilePath, FilePath cacheFilePath)
-		{
 			var serializer = new JsonSerializer ();
+			foreach (var project in sol.GetAllProjects ()) {
+				var config = solConfig.GetMappedConfiguration (project);
 
-			try {
-				using (var sr = File.OpenText (cacheFilePath)) {
-					cachedItems [projectFilePath] = (ProjectCache)serializer.Deserialize (sr, typeof (ProjectCache));
+				var projectFilePath = project.FileName;
+				var cacheFilePath = GetProjectCacheFile (project, config);
+
+				try {
+					using (var sr = File.OpenText (cacheFilePath)) {
+						cachedItems [projectFilePath] = (ProjectCache)serializer.Deserialize (sr, typeof (ProjectCache));
+					}
+				} catch (Exception ex) {
+					LoggingService.LogError ("Could not deserialize project cache", ex);
+					TryDeleteFile (cacheFilePath);
+					return;
 				}
-			} catch (Exception ex) {
-				LoggingService.LogError ("Could not deserialize project cache", ex);
-				TryDeleteFile (cacheFilePath);
-				return;
 			}
 		}
 
@@ -131,12 +104,6 @@ namespace MonoDevelop.Ide.TypeSystem
 		{
 			var serializer = new JsonSerializer ();
 
-			// Write control file
-			using (var fs = File.Open (mappingFile, FileMode.Create))
-			using (var sw = new StreamWriter (fs)) {
-				serializer.Serialize (sw, controlCache);
-			}
-
 			// Write cache files
 			var solConfig = sol.GetConfiguration (IdeApp.Workspace.ActiveConfiguration);
 			var allProjects = sol.GetAllProjects ().ToDictionary (x => x.FileName, x => x);
@@ -148,7 +115,7 @@ namespace MonoDevelop.Ide.TypeSystem
 
 				using (var fs = File.Open (cacheFile, FileMode.Create))
 				using (var sw = new StreamWriter (fs)) {
-					serializer.Serialize (sw, controlCache);
+					serializer.Serialize (sw, item.Value);
 				}
 			}
 		}
@@ -164,22 +131,6 @@ namespace MonoDevelop.Ide.TypeSystem
 			files = cachedData.Files;
 			analyzers = cachedData.Analyzers;
 			return true;
-		}
-
-		[Serializable]
-		class TypesystemCache
-		{
-			public DateTime SolutionTimestamp;
-
-			public ProjectMapping [] ProjectMap = Array.Empty<ProjectMapping> ();
-		}
-
-		[Serializable]
-		class ProjectMapping
-		{
-			public string ProjectFilePath;
-			public string Configuration;
-			public string CacheFilePath;
 		}
 
 		[Serializable]
