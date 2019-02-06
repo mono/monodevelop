mono/monodevelop

commit 5f9a7806d7eafc91008bef6f5129803ce83d62d0

Cancel Project Run if WorkingDirectory doesn't exist and report (#7059)
Previously if a user had set their run configuration to use the "run in directory" with a path that did not exist, MonoDevelop would hang up upon attempting "Start Debugging" or "Start Without Debugging". It would not cancel or alert the user of why this was occurring which could be quite confusing to figure out.

Instead, during a call to OnExecuteCommand, the working directory is checked to see if it exists. If it does, it continues normally. If not it cancels the execution and reports a warning explaining what the problem is, indicating to the user where to go to fix the issue.

Fixes VSTS #569970

main/src/core/MonoDevelop.Core/MonoDevelop.Projects/DotNetProject.cs
@@ -1916,6 +1916,12 @@ namespace MonoDevelop.Projects
 			bool externalConsole = false, pauseConsole = false;
 
 			var dotNetExecutionCommand = executionCommand as DotNetExecutionCommand;
+
+			if (!Directory.Exists (dotNetExecutionCommand.WorkingDirectory)) {
+				monitor.ReportError (GettextCatalog.GetString ("Can not execute. The run configuration working directory doesn't exist at {0}", dotNetExecutionCommand.WorkingDirectory), null);
+				return;
+			}
+
 			if (dotNetExecutionCommand != null) {
 				dotNetExecutionCommand.UserAssemblyPaths = GetUserAssemblyPaths (configuration);
 				externalConsole = dotNetExecutionCommand.ExternalConsole;

commit fb3d93e2ec702a8a24a576dcfd981b9ec029a0a8

[C#] Fix NRE caused by #7059

main/src/core/MonoDevelop.Core/MonoDevelop.Projects/DotNetProject.cs
@@ -1915,14 +1915,14 @@ namespace MonoDevelop.Projects
 		{
 			bool externalConsole = false, pauseConsole = false;
 
-			var dotNetExecutionCommand = executionCommand as DotNetExecutionCommand;
-
-			if (!Directory.Exists (dotNetExecutionCommand.WorkingDirectory)) {
-				monitor.ReportError (GettextCatalog.GetString ("Can not execute. The run configuration working directory doesn't exist at {0}", dotNetExecutionCommand.WorkingDirectory), null);
-				return;
+			if (executionCommand is ProcessExecutionCommand processExecutionCommand) {
+				if (!Directory.Exists (processExecutionCommand.WorkingDirectory)) {
+					monitor.ReportError (GettextCatalog.GetString ("Can not execute. The run configuration working directory doesn't exist at {0}", processExecutionCommand.WorkingDirectory), null);
+					return;
+				}
 			}
 
-			if (dotNetExecutionCommand != null) {
+			if (executionCommand is DotNetExecutionCommand dotNetExecutionCommand) {
 				dotNetExecutionCommand.UserAssemblyPaths = GetUserAssemblyPaths (configuration);
 				externalConsole = dotNetExecutionCommand.ExternalConsole;
 				pauseConsole = dotNetExecutionCommand.PauseConsoleOutput;