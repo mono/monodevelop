d98b7eb29ea1e34db0d4e4d2f38f097b30e4f8a3
therzok <marius.ungureanu@xamarin.com>
2019-03-16 14:34:10 +0200

    Code cleanups

--- main/tests/StressTest/MonoDevelop.StressTest/Profiler/Heapshot.cs
+++ main/tests/StressTest/MonoDevelop.StressTest/Profiler/Heapshot.cs
@@ -19,12 +19,14 @@ namespace MonoDevelop.StressTest
 
 	public class Heapshot
 	{
+		// TODO: Make this into simple classId -> classInfo, objectId -> objectInfo
 		public readonly Dictionary<long, HeapRootRegisterEvent> Roots;
 		public readonly Dictionary<long, ClassLoadEvent> ClassInfos;
 		public readonly Dictionary<long, long> ObjectToType;
 		public readonly Dictionary<long, List<long>> TypeToObjectList;
 		public readonly Dictionary<string, (int, long)> ObjectCounts;
 
+		// TODO: Use a TaggedEdge where we add ObjectInfo
 		public readonly ReversedBidirectionalGraph<long, Edge<long>> Graph;
 
 		public Heapshot (NativeHeapshot nativeHeapshot)
@@ -56,42 +58,5 @@ namespace MonoDevelop.StressTest
 
 			return result;
 		}
-
-
-		//public static IEnumerable<(LogHeapRootSource, long[])> SearchRoots(this Heapshot heapshot, long objAddr)
-		//{
-		//	var queue = new Queue<long[]> ();
-		//	var visited = new HashSet<long> ();
-		//	visited.Add (objAddr);
-
-		//	var lessImportantRoots = new List<(LogHeapRootSource, long[])> ();
-		//	queue.Enqueue (new long[] { objAddr });
-
-		//	while (queue.Any ()) {
-		//		var cur = queue.Dequeue ();
-
-		//		var node = cur[cur.Length - 1];
-		//		if (heapshot.Roots.TryGetValue (node, out var root)) {
-		//			if (root.Source == LogHeapRootSource.Ephemeron || root.Source == LogHeapRootSource.FinalizerQueue)
-		//				lessImportantRoots.Add ((root.Source, cur));
-		//			else
-		//				yield return (root.Source, cur);
-		//		}
-
-		//		foreach (var child in GetReferencedFrom (node)) {
-		//			if (!visited.Add (child))
-		//				continue;
-
-		//			var newPath = new long[cur.Length + 1];
-		//			Array.Copy (cur, 0, newPath, 0, cur.Length);
-		//			newPath[cur.Length] = child;
-		//			queue.Enqueue (newPath);
-		//		}
-		//	}
-
-		//	foreach (var lir in lessImportantRoots) {
-		//		yield return lir;
-		//	}
-		//}
 	}
 }
