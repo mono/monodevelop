33762d7c89165e94b7cd5514f6321f1401e2169e
Jose Miguel Torres <jostor@microsoft.com>
2019-03-30 11:06:31 +0100

    [aspnetcore][tests] LaunchSettings API tests

---
+++ main/src/addins/MonoDevelop.AspNetCore/MonoDevelop.AspNetCore.Tests/MonoDevelop.AspNetCore.Tests/AspNetCoreExecutionCommandTests.cs
@@ -0,0 +1,41 @@
+//
+// AspNetCoreExecutionCommandTests.cs
+//
+// Author:
+//       josemiguel <jostor@microsoft.com>
+//
+// Copyright (c) 2019 ${CopyrightHolder}
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in
+// all copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+// THE SOFTWARE.
+using System;
+using NUnit.Framework;
+
+namespace MonoDevelop.AspNetCore.Tests
+{
+	[TestFixture]
+	public class AspNetCoreExecutionCommandTests
+	{
+		[TestCase ("http://localhost:5000;https://localhost:5001", "http://localhost:5000")]
+		[TestCase ("https://localhost:5001", "https://localhost:5001")]
+		public void GetFirstApplicationURL_returns_first_url (string appUrl, string expectedUrl)
+		{
+			Assert.That (AspNetCoreExecutionCommand.GetFirstApplicationURL (appUrl), Is.EqualTo (expectedUrl));
+		}
+	}
+}
