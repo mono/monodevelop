3097f1424feda7770898ffdbee2620b83e5b469a
Jose Miguel Torres <jostor@microsoft.com>
2019-03-30 11:01:42 +0100

    [aspnetcore] Adds LaunchProfile classes

---
+++ main/src/addins/MonoDevelop.AspNetCore/MonoDevelop.AspNetCore/LaunchProfileProvider.cs
@@ -0,0 +1,135 @@
+﻿//
+// LaunchProfileProvider.cs
+//
+// Author:
+//       José Miguel Torres <jostor@microsoft.com>
+//
+// Copyright (c) 2018 Microsoft
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in
+// all copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+// THE SOFTWARE.
+using System;
+using System.Collections.Generic;
+using System.IO;
+using MonoDevelop.Projects;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+
+namespace MonoDevelop.AspNetCore
+{
+	internal class LaunchProfileProvider
+	{
+		DotNetProject project;
+		readonly object fileLocker = new object ();
+		public IDictionary<string, JToken> GlobalSettings { get; private set; }
+		internal JObject ProfilesObject { get; private set; }
+		internal string launchSettingsJsonPath => Path.Combine (project.BaseDirectory, "Properties", "launchSettings.json");
+		const string DefaultGlobalSettings = @"{
+    						""windowsAuthentication"": false,
+    						""anonymousAuthentication"": true
+  							}";
+
+		public LaunchProfileProvider (Project project)
+		{
+			this.project = (DotNetProject) project;
+			GlobalSettings = new Dictionary<string, JToken> ();
+		}
+
+		public void LoadLaunchSettings ()
+		{
+			if (!File.Exists (launchSettingsJsonPath)) {
+				CreateAndAddDefaultLaunchSettings ();
+				return; 
+			}
+
+			var launchSettingsJson = TryParse ();
+
+			GlobalSettings.Clear ();
+			foreach (var token in launchSettingsJson) {
+				if (token.Key == "profiles") {
+					ProfilesObject = token.Value as JObject;
+					continue;
+				}
+				GlobalSettings.Add (token.Key, token.Value);
+			}
+		}
+
+		JObject TryParse ()
+		{
+			try {
+				return JObject.Parse (File.ReadAllText (launchSettingsJsonPath));
+			}
+			catch {
+				return new JObject ();
+			}
+		}
+
+		public void SaveLaunchSettings (IDictionary<string, Dictionary<string, object>> profilesData)
+		{
+			var doc = new JObject ();
+			var settings = new JsonSerializerSettings { NullValueHandling = NullValueHandling.Ignore };
+
+			if (profilesData == null)
+				return;
+
+			ProfilesObject = JObject.Parse (JsonConvert.SerializeObject (profilesData, Formatting.Indented, settings));
+
+			foreach (var set in GlobalSettings) {
+				doc.Add (set.Key, set.Value);
+			}
+
+			doc.Add ("profiles", ProfilesObject);
+
+			string jsonDocString = doc.ToString (Formatting.Indented);
+
+			string propertiesDirectory = Path.GetDirectoryName (launchSettingsJsonPath);
+			if (!Directory.Exists (propertiesDirectory)) {
+				Directory.CreateDirectory (propertiesDirectory);
+			}
+
+			lock (fileLocker) {
+				File.WriteAllText (launchSettingsJsonPath, jsonDocString);
+			};
+
+		}
+
+		public LaunchProfileData CreateDefaultProfile ()
+		{
+			var defaultProfile = new LaunchProfileData {
+				Name = project.DefaultNamespace,
+				CommandName = "Project",
+				LaunchBrowser = true,
+				EnvironmentVariables = new Dictionary<string, string> (StringComparer.Ordinal),
+				OtherSettings = new Dictionary<string, object> (StringComparer.Ordinal)
+			};
+
+			defaultProfile.EnvironmentVariables.Add ("ASPNETCORE_ENVIRONMENT", "Development");
+			defaultProfile.OtherSettings.Add ("applicationUrl", "https://localhost:5001;http://localhost:5000");
+
+			return defaultProfile;
+		}
+
+		void CreateAndAddDefaultLaunchSettings ()
+		{
+			GlobalSettings.Add ("iisSettings", JToken.Parse (DefaultGlobalSettings));
+			var profilesData = new Dictionary<string, LaunchProfileData> ();
+			profilesData.Add (project.DefaultNamespace, CreateDefaultProfile ());
+			SaveLaunchSettings (profilesData.ToSerializableForm ());
+		}
+	}
+}
