b13f238fc9c8db8cd73a021ff56677ebb607a5d7
David Karlaš <david.karlas@microsoft.com>
2019-04-26 13:37:00 +0200

    Initial support for snippets in new editor CSharpBinding MimeType for C# had wrong casing for ContentType ExpansionManager implements new interface by converting MD snippets to new editor snippets

---
+++ main/src/addins/MonoDevelop.TextEditor/MonoDevelop.TextEditor.Cocoa/ExpansionManager.cs
@@ -0,0 +1,79 @@
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.Composition;
+using System.IO;
+using Microsoft.VisualStudio.Text;
+using Microsoft.VisualStudio.Text.Editor;
+using Microsoft.VisualStudio.Text.Editor.Expansion;
+using Microsoft.VisualStudio.Utilities;
+using MonoDevelop.Ide;
+using MonoDevelop.Ide.CodeTemplates;
+
+namespace MonoDevelop.TextEditor.Cocoa
+{
+	[Export (typeof (IExpansionManager))]
+	class ExpansionManager : IExpansionManager
+	{
+		List<ExpansionTemplate> expansionTemplates;
+
+		public IEnumerable<ExpansionTemplate> EnumerateExpansions (IContentType contentType, bool shortcutOnly, string[] snippetTypes, bool includeNullType, bool includeDuplicates)
+		{
+			if (expansionTemplates != null)
+				return expansionTemplates;
+			expansionTemplates = new List<ExpansionTemplate> ();
+			foreach (var codeTemplate in CodeTemplateService.GetCodeTemplates (IdeServices.DesktopService.GetMimeTypeForContentType (contentType)))
+				expansionTemplates.Add (Convert (codeTemplate));
+			return expansionTemplates;
+		}
+
+		private ExpansionTemplate Convert (CodeTemplate codeTemplate)
+		{
+			var codeSnippet = new CodeSnippet ();
+			codeSnippet.Code = codeTemplate.Code;
+			codeSnippet.Description = codeTemplate.Description;
+			codeSnippet.Fields = new List<ExpansionField> ();
+			foreach (var variable in codeTemplate.Variables) {
+				var field = new ExpansionField ();
+				field.Editable = variable.IsEditable;
+				field.Function = ConvertFunctionName (variable.Function);
+				field.ToolTip = variable.ToolTip;
+				field.Default = variable.Default ?? "";
+				if ("GetConstructorModifier()" == variable.Function && string.IsNullOrEmpty (field.Default))
+					field.Default = "public ";
+				field.ID = variable.Name;
+				codeSnippet.Fields.Add (field);
+			}
+			codeSnippet.Language = codeTemplate.MimeType;
+			codeSnippet.Shortcut = codeTemplate.Shortcut;
+			codeSnippet.Title = codeTemplate.Shortcut;
+			return new ExpansionTemplate (codeSnippet);
+		}
+
+		private string ConvertFunctionName (string function)
+		{
+			if (function == null)
+				return function;
+			if (function == "GetCurrentClassName()")
+				return "ClassName()";
+			if (function.StartsWith ("GetSimpleTypeName", StringComparison.Ordinal))
+				return function.Replace ("GetSimpleTypeName", "SimpleTypeName").Replace ("#", ".").Replace ("\"", "");
+			return function;
+		}
+
+		public ExpansionTemplate GetTemplateByName (IExpansionClient expansionClient, IContentType contentType, string name, string filePath, ITextView textView, SnapshotSpan span, bool showDisambiguationUI)
+		{
+			foreach (var template in EnumerateExpansions (contentType, false, null, true, true))
+				if (template.Snippet.Title == name)
+					return template;
+			return null;
+		}
+
+		public ExpansionTemplate GetTemplateByShortcut (IExpansionClient expansionClient, string shortcut, IContentType contentType, ITextView textView, SnapshotSpan span, bool showDisambiguationUI)
+		{
+			foreach (var template in EnumerateExpansions (contentType, false, null, true, true))
+				if (template.Snippet.Shortcut == shortcut)
+					return template;
+			return null;
+		}
+	}
+}
