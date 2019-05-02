c8d6531f8b6866aeae6abd69efded61813609d89
Mike Krüger <mikkrg@microsoft.com>
2019-05-02 13:06:32 +0200

    [VBBinding] Removed redundant ParsedDocument infrastructure.
    It's not used for roslyn languages.

--- main/src/addins/VBNetBinding/Project/VBNetParsedDocument.cs
+++ main/src/addins/VBNetBinding/Project/VBNetParsedDocument.cs
@@ -47,10 +47,6 @@ namespace MonoDevelop.VBNetBinding
 			get;
 			set;
 		}
-		internal SyntaxTree ParsedUnit {
-			get;
-			set;
-		}
 
 		static VBNetParsedDocument ()
 		{
@@ -75,12 +71,10 @@ namespace MonoDevelop.VBNetBinding
 		#region implemented abstract members of ParsedDocument
 
 		IReadOnlyList<Comment> comments = new Comment[0];
-		SemaphoreSlim commentLock = new SemaphoreSlim (1, 1);
 
 		public override Task<IReadOnlyList<Comment>> GetCommentsAsync (CancellationToken cancellationToken = default(CancellationToken))
 		{
 			return Task.FromResult (comments);
-
 		}
 
 		// Tags are done via Ide.Tasks.CommentTasksProvider.
@@ -93,61 +87,12 @@ namespace MonoDevelop.VBNetBinding
 
 		public override Task<IReadOnlyList<FoldingRegion>> GetFoldingsAsync (CancellationToken cancellationToken = default (CancellationToken)) => foldings;
 
-		SemaphoreSlim errorLock = new SemaphoreSlim (1, 1);
-
 		static readonly IReadOnlyList<Error> emptyErrors = Array.Empty<Error> ();
+
 		public override async Task<IReadOnlyList<Error>> GetErrorsAsync (CancellationToken cancellationToken = default(CancellationToken))
 		{
-			if (Ide.IdeApp.Preferences.EnableSourceAnalysis || DocumentId is null)
-				return emptyErrors;
-
-			// FIXME: remove this fallback, error squiggles should always be handled via the source analysis mechanism
-			var document = IdeServices.TypeSystemService.GetCodeAnalysisDocument (DocumentId, cancellationToken);
-			var model = await document.GetSemanticModelAsync (cancellationToken);
-
-			bool locked = await errorLock.WaitAsync (Timeout.Infinite, cancellationToken).ConfigureAwait (false);
-			IReadOnlyList<Error> errors;
-			try {
-				try {
-					errors = model
-						.GetDiagnostics (null, cancellationToken)
-						.Select ((Diagnostic diag) => new Error (GetErrorType (diag.Severity), diag.Id, diag.GetMessage (), GetRegion (diag)) { Tag = diag })
-						.ToList ();
-				} catch (OperationCanceledException) {
-					errors = emptyErrors;
-				} catch (Exception e) {
-					LoggingService.LogError ("Error while getting diagnostics.", e);
-					errors = emptyErrors;
-				}
-			} finally {
-				if (locked)
-					errorLock.Release ();
-			}
-			
-			return errors;
+			return emptyErrors;
 		}
-
-		static DocumentRegion GetRegion (Diagnostic diagnostic)
-		{
-			try {
-				var lineSpan = diagnostic.Location.GetLineSpan ();
-				return new DocumentRegion (lineSpan.StartLinePosition, lineSpan.EndLinePosition);
-			} catch (Exception) {
-				return DocumentRegion.Empty;
-			}
-		}
-
-		static ErrorType GetErrorType (DiagnosticSeverity severity)
-		{
-			switch (severity) {
-			case DiagnosticSeverity.Error:
-				return ErrorType.Error;
-			case DiagnosticSeverity.Warning:
-				return ErrorType.Warning;
-			}
-			return ErrorType.Unknown;
-		}
-
 		#endregion
 	}
 }
