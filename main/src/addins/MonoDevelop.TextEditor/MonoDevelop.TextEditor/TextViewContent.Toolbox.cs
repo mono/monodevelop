ce1a3ab0091a6cd01eb3704a3077e33939a0a268
David Karlaš <david.karlas@xamarin.com>
2019-05-07 19:24:04 +0200

    Fixes 866554: Enable Snippets within Toolbox Changed logic in CodeTemplateToolboxProvider to not execute for new editor since code expects `document.Editor` to not be `null` Main change is in TextViewContent.Toolbox which now makes new editor ViewContent aware of `IToolboxConsumer` and in `CocoaTextViewContent` which actually converts templates that `IExpansionManager` returns to Toolbox items

---
+++ main/src/addins/MonoDevelop.TextEditor/MonoDevelop.TextEditor/TextViewContent.Toolbox.cs
@@ -0,0 +1,37 @@
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using Gdk;
+using Gtk;
+using Microsoft.VisualStudio.Text.Editor;
+using MonoDevelop.DesignerSupport.Toolbox;
+
+namespace MonoDevelop.TextEditor
+{
+	partial class TextViewContent<TView, TImports> :
+		IToolboxConsumer
+	{
+		TargetEntry [] IToolboxConsumer.DragTargets { get; } = new TargetEntry [0];
+
+		ToolboxItemFilterAttribute [] IToolboxConsumer.ToolboxFilterAttributes { get; } = new System.ComponentModel.ToolboxItemFilterAttribute [0];
+
+		string IToolboxConsumer.DefaultItemDomain => "Text";
+
+		void IToolboxConsumer.ConsumeItem (ItemToolboxNode item)
+		{
+			if (item is ITextToolboxNode tn) {
+				tn.InsertAtCaret (this.Document);
+				((ITextView3)TextView).Focus ();
+			}
+		}
+
+		bool IToolboxConsumer.CustomFilterSupports (ItemToolboxNode item)
+		{
+			return false;
+		}
+
+		void IToolboxConsumer.DragItem (ItemToolboxNode item, Widget source, DragContext ctx)
+		{
+		}
+	}
+}
