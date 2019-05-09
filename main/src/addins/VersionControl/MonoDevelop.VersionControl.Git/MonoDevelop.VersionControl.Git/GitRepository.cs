01a24e16a2f8e2778ab7308c864cfea15002cf78
Javier Suárez Ruiz <javiersuarezruiz@hotmail.com>
2019-05-09 12:23:19 +0200

    Fixes VSTS #825343 - Mismatches actual files commited vs selected when using git

--- main/src/addins/VersionControl/MonoDevelop.VersionControl.Git/MonoDevelop.VersionControl.Git/GitRepository.cs
+++ main/src/addins/VersionControl/MonoDevelop.VersionControl.Git/MonoDevelop.VersionControl.Git/GitRepository.cs
@@ -1256,17 +1256,56 @@ namespace MonoDevelop.VersionControl.Git
 				return;
 
 			var repo = (GitRepository)changeSet.Repository;
-			RunBlockingOperation (() => {
-				LibGit2Sharp.Commands.Stage (RootRepository, changeSet.Items.Select (i => i.LocalPath).ToPathStrings ());
+			var addedFiles = GetAddedLocalPathItems (changeSet);
 
-				if (changeSet.ExtendedProperties.Contains ("Git.AuthorName"))
-					RootRepository.Commit (message, new Signature (
-						(string)changeSet.ExtendedProperties ["Git.AuthorName"],
-						(string)changeSet.ExtendedProperties ["Git.AuthorEmail"],
-						DateTimeOffset.Now), sig);
-				else
-					RootRepository.Commit (message, sig, sig);
+			RunBlockingOperation (() => {
+				try {
+					// Unstage added files not included in the changeSet
+					if (addedFiles.Any ())
+						LibGit2Sharp.Commands.Unstage (RootRepository, addedFiles.ToPathStrings ());
+				} catch (Exception ex) {
+					LoggingService.LogInternalError ("Failed to commit.", ex);
+					return;
+				}
+				try {
+					// Commit
+					LibGit2Sharp.Commands.Stage (RootRepository, changeSet.Items.Select (i => i.LocalPath).ToPathStrings ());
+
+					if (changeSet.ExtendedProperties.Contains ("Git.AuthorName"))
+						RootRepository.Commit (message, new Signature (
+							(string)changeSet.ExtendedProperties ["Git.AuthorName"],
+							(string)changeSet.ExtendedProperties ["Git.AuthorEmail"],
+							DateTimeOffset.Now), sig);
+					else
+						RootRepository.Commit (message, sig, sig);
+				} catch (Exception ex) {
+					LoggingService.LogInternalError ("Failed to commit.", ex);
+				} finally {
+					// Always at the end, stage again the unstage added files not included in the changeSet
+					if (addedFiles.Any ())
+						LibGit2Sharp.Commands.Stage (RootRepository, addedFiles.ToPathStrings ());
+				}
 			});
+		}
+
+		HashSet<FilePath> GetAddedLocalPathItems (ChangeSet changeSet)
+		{
+			return readingOperationFactory.StartNew (() => {
+				var addedLocalPathItems = new HashSet<FilePath> ();
+				try {
+					var directoryVersionInfo = GetDirectoryVersionInfo (changeSet.BaseLocalPath, null, false, true);
+					const VersionStatus addedStatus = VersionStatus.Versioned | VersionStatus.ScheduledAdd;
+					var directoryVersionInfoItems = directoryVersionInfo.Where (vi => vi.Status == addedStatus);
+
+					foreach (var item in directoryVersionInfoItems)
+						foreach (var changeSetItem in changeSet.Items)
+							if (item.LocalPath != changeSetItem.LocalPath)
+								addedLocalPathItems.Add (item.LocalPath);
+				} catch (Exception ex) {
+					LoggingService.LogInternalError ("Could not get added VersionInfo items.", ex);
+				}
+				return addedLocalPathItems;
+			}).RunWaitAndCapture ();
 		}
 
 		public bool IsUserInfoDefault ()
