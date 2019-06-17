b20956982a41e99bb4fe8e7d1c337ed71ccae8bc
Rodrigo Moya <rodrigo@gnome-db.org>
2019-06-17 20:27:18 +0200

    [AspNetCore] Seal AspNetCoreNestingRulesProvider class
    Co-Authored-By: Marius Ungureanu <teromario@yahoo.com>

--- main/src/addins/MonoDevelop.AspNetCore/MonoDevelop.AspNetCore/AspNetCoreNestingRulesProvider.cs
+++ main/src/addins/MonoDevelop.AspNetCore/MonoDevelop.AspNetCore/AspNetCoreNestingRulesProvider.cs
@@ -30,7 +30,7 @@ using MonoDevelop.Projects.FileNesting;
 namespace MonoDevelop.AspNetCore
 {
 	[Extension]
-	class AspNetCoreNestingRulesProvider : NestingRulesProvider
+	sealed class AspNetCoreNestingRulesProvider : NestingRulesProvider
 	{
 		public AspNetCoreNestingRulesProvider ()
 		{
