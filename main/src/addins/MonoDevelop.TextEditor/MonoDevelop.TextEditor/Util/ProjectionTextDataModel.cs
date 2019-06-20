b1367380c8938c20926a67e0fa2c25285c8297f1
Kirill Osenkov <github@osenkov.com>
2019-06-19 20:20:07 -0700

    Make DataBuffer read-only for real this time.

--- main/src/addins/MonoDevelop.TextEditor/MonoDevelop.TextEditor/Util/ProjectionTextDataModel.cs
+++ main/src/addins/MonoDevelop.TextEditor/MonoDevelop.TextEditor/Util/ProjectionTextDataModel.cs
@@ -37,7 +37,7 @@ namespace Microsoft.VisualStudio.Text.Editor
 		public event EventHandler<TextDataModelContentTypeChangedEventArgs> ContentTypeChanged;
 
 		public ITextBuffer DocumentBuffer { get; }
-		public ITextBuffer DataBuffer { get; set; }
+		public ITextBuffer DataBuffer { get; }
 
 		public IContentType ContentType => DocumentBuffer.ContentType;
 
