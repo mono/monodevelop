8349df656987d192641cca2d92ab4db185877f41
therzok <marius.ungureanu@xamarin.com>
2019-06-21 21:26:10 +0300

    Update new baseline

--- main/tests/performance/MonoDevelop.Ide.PerfTests/TestSolutionLoad.cs
+++ main/tests/performance/MonoDevelop.Ide.PerfTests/TestSolutionLoad.cs
@@ -43,7 +43,7 @@ namespace MonoDevelop.Ide.PerfTests
 		}
 
 		[Test]
-		[Benchmark (Tolerance = 0.2)]
+		[Benchmark (Tolerance = 0.20)]
 		public void TestLoad ()
 		{
 			OpenApplicationAndWait ();
