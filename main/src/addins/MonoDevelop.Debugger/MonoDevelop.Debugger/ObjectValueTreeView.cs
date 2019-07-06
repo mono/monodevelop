mono/monodevelop

commit ef5ea322347547cb6d971b6097b41965312178e6

[IDE] Overall energy consumption and UI hang improvements (#7695)
* Misc perf improvements immediately after project load

* Use ImmutableDictionary<TargetFramework>, CWT won't work - the strings are collected

* Dump

* Use DefaultScheduler via Task.Run, Task.Factory is context dependent

* [AssemblyBrowser] Cleanups, mostly related to task usage

* [Debugger, VCS] Some Task.Factory fixes

Fixes VSTS #900427 - Review usage of Task.Factory.StartNew

* Use ordinal comparisons here

* [ASsemblyBrowser] Fix child nodes not appearing in the list

* [VCS] Fix improper usage of cancellation token source

* Remove unneeded string ordering

* [AsssemblyBrowser] Fix workspace leak and delay project loading.

It's not common to get to a project item from the IDE, so we can delay loading that while we get everything else setup

* [MSBuild] Switch this to Task.Run, so we don't accidentally parse on the UI thread

* Add doc comment

* [AssemblyBrowser] Massively improve time it takes to open and a couple of fixes

Reuse the typesystem created for decompilation. No need to create a new one just to load
the treeview.

Prefer using AddChildren everywhere rather than Add. This helps not sort the tree every time
we're adding an element.

Don't bother gathering all the types in an assembly. Most likely we will only need to gather
types from just one assembly, so defer that to the namespace node builder.

Fix multiple iteration of a type's members when trying to load a project metadata view.

And fix nested types to appear in the tree.

* Optimize this a bit more

* Minor refactors to avoid extra Funcs

* Reuse typesystem for loader, add new feature transform to prevent language breaking

main/src/addins/MonoDevelop.Debugger/MonoDevelop.Debugger/ObjectValueTreeView.cs
@@ -947,15 +947,15 @@ namespace MonoDevelop.Debugger
 			}
 
 			int numberOfChildren = store.IterNChildren (iter);
-			Task.Factory.StartNew<ObjectValue[]> (delegate (object arg) {
+			Task.Run (() => {
 				try {
-					return ((ObjectValue)arg).GetRangeOfChildren (numberOfChildren - 1, 20);
+					return value.GetRangeOfChildren (numberOfChildren - 1, 20);
 				} catch (Exception ex) {
 					// Note: this should only happen if someone breaks ObjectValue.GetAllChildren()
 					LoggingService.LogError ("Failed to get ObjectValue children.", ex);
 					return new ObjectValue[0];
 				}
-			}, value, cancellationTokenSource.Token).ContinueWith (t => {
+			}, cancellationTokenSource.Token).ContinueWith (t => {
 				TreeIter it;
 				if (disposed)
 					return;
@@ -975,7 +975,7 @@ namespace MonoDevelop.Debugger
 				if (compact)
 					RecalculateWidth ();
 				enumerableLoading.Remove (value);
-			}, cancellationTokenSource.Token, TaskContinuationOptions.NotOnCanceled, Xwt.Application.UITaskScheduler);
+			}, cancellationTokenSource.Token, TaskContinuationOptions.NotOnCanceled, Runtime.MainTaskScheduler).Ignore ();
 		}
 
 		void RefreshRow (TreeIter iter, ObjectValue val)

main/src/addins/MonoDevelop.AssemblyBrowser/MonoDevelop.AssemblyBrowser/AssemblyBrowserViewContent.cs
@@ -40,6 +40,7 @@ using System.Collections.Immutable;
 using MonoDevelop.Ide;
 using Microsoft.CodeAnalysis.CodeRefactorings;
 using MonoDevelop.Ide.Gui.Documents;
+using System.Threading;
 
 namespace MonoDevelop.AssemblyBrowser
 {
@@ -47,7 +48,8 @@ namespace MonoDevelop.AssemblyBrowser
 	{
 		readonly static string[] defaultAssemblies = new string[] { "mscorlib", "System", "System.Core", "System.Xml" };
 		AssemblyBrowserWidget widget;
-		
+		CancellationTokenSource cts = new CancellationTokenSource ();
+
 		protected override Control OnGetViewControl (DocumentViewContent view)
 		{
 			widget.SetToolbar (view.GetToolbar ());
@@ -67,11 +69,13 @@ namespace MonoDevelop.AssemblyBrowser
 			FillWidget ();
 		}
 
-		protected override async Task OnInitialize (ModelDescriptor modelDescriptor, Properties status)
+		protected override Task OnInitialize (ModelDescriptor modelDescriptor, Properties status)
 		{
 			if (modelDescriptor is FileDescriptor fileDescriptor) {
 				Load (fileDescriptor.FilePath);
 			}
+
+			return Task.CompletedTask;
 		}
 
 		protected override bool OnTryReuseDocument (ModelDescriptor modelDescriptor)
@@ -92,9 +96,9 @@ namespace MonoDevelop.AssemblyBrowser
 		{
 			var loader = widget.AddReferenceByFileName (filePath);
 			if (loader != null) {
-				loader.LoadingTask.ContinueWith (delegate {
-					widget.SelectAssembly (loader);
-				});
+				loader.LoadingTask
+					.ContinueWith (t => widget.SelectAssembly (t.Result), Runtime.MainTaskScheduler)
+					.Ignore ();
 			}
 		}
 
@@ -105,12 +109,14 @@ namespace MonoDevelop.AssemblyBrowser
 
 		protected override void OnDispose ()
 		{
-			if (currentWs != null) 
-				currentWs.WorkspaceLoaded -= Handle_WorkspaceLoaded;
+			if (cts != null) {
+				cts.Cancel ();
+				cts.Dispose ();
+				cts = null;
+			}
 
 			widget = null;
-			if (Disposed != null)
-				Disposed (this, EventArgs.Empty);
+			Disposed?.Invoke (this, EventArgs.Empty);
 			base.OnDispose ();
 		}
 
@@ -162,40 +168,29 @@ namespace MonoDevelop.AssemblyBrowser
 			//FindDerivedClassesHandler.FindDerivedClasses (type);
 		}
 
-		void Handle_WorkspaceLoaded (object sender, EventArgs e)
-		{
-			foreach (var project in Ide.IdeApp.ProjectOperations.CurrentSelectedSolution.GetAllProjects ()) {
-				var nav = Widget.TreeView.GetNodeAtObject (project);
-				if (nav != null)
-					Widget.TreeView.RefreshNode (nav);
-			}
-		}
-
-		Ide.TypeSystem.MonoDevelopWorkspace currentWs;
-		public async void FillWidget ()
+		public void FillWidget ()
 		{
 			if (Ide.IdeApp.ProjectOperations.CurrentSelectedSolution == null) {
 				foreach (var assembly in defaultAssemblies) {
-					Widget.AddReferenceByAssemblyName (assembly); 
+					Widget.AddReferenceByAssemblyName (assembly);
 				}
 			} else {
-				var alreadyAdded = new HashSet<string> ();
-				currentWs = IdeApp.TypeSystemService.GetWorkspace (IdeApp.ProjectOperations.CurrentSelectedSolution);
-				if (currentWs != null)
-					currentWs.WorkspaceLoaded += Handle_WorkspaceLoaded;
-				var allTasks = new List<Task> ();
-				foreach (var project in Ide.IdeApp.ProjectOperations.CurrentSelectedSolution.GetAllProjects ()) {
-					try {
-						Widget.AddProject (project, false);
-					} catch (Exception e) {
-						LoggingService.LogError ("Error while adding project " + project.Name + " to the tree.", e);
-					}
-				}
-				await Task.WhenAll (allTasks).ContinueWith (delegate {
-					Runtime.RunInMainThread (delegate {
+				var token = cts.Token;
+
+				var workspace = IdeApp.TypeSystemService.GetWorkspaceAsync (IdeApp.ProjectOperations.CurrentSelectedSolution)
+					.ContinueWith (t => {
+						if (token.IsCancellationRequested)
+							return;
+
+						foreach (var project in IdeApp.ProjectOperations.CurrentSelectedSolution.GetAllProjects ()) {
+							try {
+								Widget.AddProject (project, false);
+							} catch (Exception e) {
+								LoggingService.LogError ("Error while adding project " + project.Name + " to the tree.", e);
+							}
+						}
 						widget.StartSearch ();
-					});
-				});
+					}, token, TaskContinuationOptions.DenyChildAttach, Runtime.MainTaskScheduler);
 			}
 		}
 	}

main/src/addins/MonoDevelop.AssemblyBrowser/MonoDevelop.AssemblyBrowser/AssemblyBrowserWidget.cs
@@ -880,19 +880,19 @@ namespace MonoDevelop.AssemblyBrowser
 				inspectEditor.Options = assemblyBrowserEditorOptions;
 				this.inspectEditor.MimeType = "text/x-csharp";
 				builder.DecompileAsync (inspectEditor, nav, new DecompileFlags { PublicOnly = PublicApiOnly, MethodBodies = false })
-					.ContinueWith (l => SetReferencedSegments (l.Result), Runtime.MainTaskScheduler);
+					.ContinueWith (l => SetReferencedSegments (l.Result), Runtime.MainTaskScheduler).Ignore ();
 				break;
 			case 1:
 				inspectEditor.Options = assemblyBrowserEditorOptions;
 				this.inspectEditor.MimeType = "text/x-ilasm";
 				builder.DisassembleAsync (inspectEditor, nav)
-					.ContinueWith (l => SetReferencedSegments (l.Result), Runtime.MainTaskScheduler);
+					.ContinueWith (l => SetReferencedSegments (l.Result), Runtime.MainTaskScheduler).Ignore ();
 				break;
 			case 2:
 				inspectEditor.Options = assemblyBrowserEditorOptions;
 				this.inspectEditor.MimeType = "text/x-csharp";
 				builder.DecompileAsync (inspectEditor, nav, new DecompileFlags { PublicOnly = PublicApiOnly, MethodBodies = true })
-					.ContinueWith (l => SetReferencedSegments (l.Result), Runtime.MainTaskScheduler);
+					.ContinueWith (l => SetReferencedSegments (l.Result), Runtime.MainTaskScheduler).Ignore ();
 				break;
 			default:
 				inspectEditor.Options = assemblyBrowserEditorOptions;
@@ -914,24 +914,30 @@ namespace MonoDevelop.AssemblyBrowser
 		internal void Open (string url, AssemblyLoader currentAssembly = null, bool expandNode = true)
 		{
 			Task.WhenAll (this.definitions.Select (d => d.LoadingTask)).ContinueWith (d => {
-				Application.Invoke ((o, args) => {
-					suspendNavigation = false;
-					ITreeNavigator nav = SearchMember (url, expandNode);
-					if (definitions.Count == 0) // we've been disposed
-						return;
-					if (nav != null)
-						return;
-					try {
-						if (currentAssembly != null) {
-							OpenFromAssembly (url, currentAssembly);
-						} else {
-							OpenFromAssemblyNames (url);
-						}
-					} catch (Exception e) {
-						LoggingService.LogError ("Error while opening the assembly browser with id:" + url, e);
+				// At least one of them failed.
+				if (d.IsFaulted) {
+					LoggingService.LogError ("Failed to load assemblies", d.Exception);
+
+					// It's possible the assembly in which the type we're looking for exists
+					// so try probing for it regardless.
+				}
+
+				suspendNavigation = false;
+				ITreeNavigator nav = SearchMember (url, expandNode);
+				if (definitions.Count == 0) // we've been disposed
+					return;
+				if (nav != null)
+					return;
+				try {
+					if (currentAssembly != null) {
+						OpenFromAssembly (url, currentAssembly);
+					} else {
+						OpenFromAssemblyNames (url);
 					}
-				});
-			});
+				} catch (Exception e) {
+					LoggingService.LogError ("Error while opening the assembly browser with id:" + url, e);
+				}
+			}, Runtime.MainTaskScheduler).Ignore ();
 		}
 
 		void OpenFromAssembly (string url, AssemblyLoader currentAssembly, bool expandNode = true)
@@ -960,16 +966,14 @@ namespace MonoDevelop.AssemblyBrowser
 				result.LoadingTask.ContinueWith (t2 => {
 					if (definitions.Count == 0) // disposed
 						return;
-					Application.Invoke ((o, args) => {
-						var nav = SearchMember (url, expandNode);
-						if (nav == null) {
-							if (++i == references.Length)
-								LoggingService.LogError ("Assembly browser: Can't find: " + url + ".");
-							else
-								loadNext ();
-						}
-					});
-				}, TaskScheduler.Current);
+					var nav = SearchMember (url, expandNode);
+					if (nav == null) {
+						if (++i == references.Length)
+							LoggingService.LogError ("Assembly browser: Can't find: " + url + ".");
+						else
+							loadNext ();
+					}
+				}, Runtime.MainTaskScheduler).Ignore ();
 			};
 		}
 
@@ -997,51 +1001,44 @@ namespace MonoDevelop.AssemblyBrowser
 					LoggingService.LogError ("Assembly browser: Can't find: " + url + ".");
 				}
 				return;
-			};
-			Task.Factory.ContinueWhenAll (tasks.ToArray (), tarr => {
-				var exceptions = tarr.Where (t => t.IsFaulted).Select (t => t.Exception).ToArray ();
-				if (exceptions != null) {
-					var ex = new AggregateException (exceptions).Flatten ();
-					if (ex.InnerExceptions.Count > 0) {
-						foreach (var inner in ex.InnerExceptions) {
-							LoggingService.LogError ("Error while loading assembly in the browser.", inner);
-						}
-						throw ex;
+			}
+
+			Task.WhenAll (tasks.ToArray ())
+				.ContinueWith (t => {
+					if (t.IsFaulted) {
+						LoggingService.LogError ("Error while loading assemblies in the browser", t.Exception);
+						return;
 					}
-				}
-				if (definitions.Count == 0) // disposed
-					return;
-				Application.Invoke ((o, args) => {
+
+					if (definitions.Count == 0) // disposed
+						return;
+
 					var nav = SearchMember (url);
 					if (nav == null) {
 						LoggingService.LogError ("Assembly browser: Can't find: " + url + ".");
 					}
-				});
-			}, CancellationToken.None, TaskContinuationOptions.None, TaskScheduler.Current);
+				}, Runtime.MainTaskScheduler).Ignore ();
 		}
 		
-		internal void SelectAssembly (AssemblyLoader loader)
+		internal void SelectAssembly (PEFile cu)
 		{
-			PEFile cu = loader.Assembly;
-			Application.Invoke ((o, args) => {
-				ITreeNavigator nav = TreeView.GetRootNode ();
-				if (nav == null)
-					return;
+			ITreeNavigator nav = TreeView.GetRootNode ();
+			if (nav == null)
+				return;
 
-				if (expandedMember) {
-					expandedMember = false;
+			if (expandedMember) {
+				expandedMember = false;
+				return;
+			}
+
+			do {
+				if (nav.DataItem == cu || (nav.DataItem as AssemblyLoader)?.Assembly == cu) {
+					nav.ExpandToNode ();
+					nav.Selected = true;
+					nav.ScrollToNode ();
 					return;
 				}
-
-				do {
-					if (nav.DataItem == cu || (nav.DataItem as AssemblyLoader)?.Assembly == cu) {
-						nav.ExpandToNode ();
-						nav.Selected = true;
-						nav.ScrollToNode ();
-						return;
-					}
-				} while (nav.MoveNext ());
-			});
+			} while (nav.MoveNext ());
 		}
 		
 		void Dispose<T> (ITreeNavigator nav) where T:class, IDisposable
@@ -1095,6 +1092,8 @@ namespace MonoDevelop.AssemblyBrowser
 					def.Dispose ();
 				definitions = definitions.Clear ();
 			}
+
+			projects.Clear ();
 			
 			ActiveMember = null;
 			resultListStore = null;
@@ -1124,7 +1123,7 @@ namespace MonoDevelop.AssemblyBrowser
 
 
 		ImmutableList<AssemblyLoader> definitions = ImmutableList<AssemblyLoader>.Empty;
-		List<Project> projects = new List<Project> ();
+		HashSet<Project> projects = new HashSet<Project> ();
 		
 		internal AssemblyLoader AddReferenceByAssemblyName (PEFile reference, bool expand = false)
 		{
@@ -1158,38 +1157,36 @@ namespace MonoDevelop.AssemblyBrowser
 			var result = new AssemblyLoader (this, fileName);
 			definitions = definitions.Add (result);
 			result.LoadingTask = result.LoadingTask.ContinueWith (task => {
-				Application.Invoke ((o, args) => {
-					if (TreeView == null || definitions.Count == 0)
-						return;
-					var fullName = result.Assembly.FullName;
+				if (TreeView == null || definitions.Count == 0)
+					return task.Result;
+				var fullName = result.Assembly.FullName;
 
-					// filter duplicate assemblies, can happen on opening the same assembly at different locations.
-					foreach (var d in definitions) {
-						if (!d.IsLoaded || d == result)
-							continue;
-						if (d.Assembly.FullName == fullName) {
-							definitions = definitions.Remove (result);
-							LoggingService.LogInfo ("AssemblyBrowser: Loaded duplicate assembly : " + fullName); // Write a log info in case that happens, shouldn't happen often.
-							return;
-						}
+				// filter duplicate assemblies, can happen on opening the same assembly at different locations.
+				foreach (var d in definitions) {
+					if (!d.IsLoaded || d == result)
+						continue;
+					if (d.Assembly.FullName == fullName) {
+						definitions = definitions.Remove (result);
+						LoggingService.LogInfo ("AssemblyBrowser: Loaded duplicate assembly : " + fullName); // Write a log info in case that happens, shouldn't happen often.
+						return task.Result;
 					}
-						
-					try {
-						ITreeBuilder builder;
-						if (definitions.Count + projects.Count == 1) {
-							builder = TreeView.LoadTree (result);
-						} else {
-							builder = TreeView.AddChild (result, false);
-						}
-						if (TreeView.GetSelectedNode () == null)
-							builder.Selected = builder.Expanded = expand;
-					} catch (Exception e) {
-						LoggingService.LogError ("Error while adding assembly to the assembly list", e);
+				}
+
+				try {
+					ITreeBuilder builder;
+					if (definitions.Count + projects.Count == 1) {
+						builder = TreeView.LoadTree (result);
+					} else {
+						builder = TreeView.AddChild (result, expand);
 					}
-				});
+					if (TreeView.GetSelectedNode () == null)
+						builder.Selected = builder.Expanded = expand;
+				} catch (Exception e) {
+					LoggingService.LogError ("Error while adding assembly to the assembly list", e);
+				}
 				return task.Result;
-			}
-			);
+			}, Runtime.MainTaskScheduler);
+		
 			return result;
 		}
 		
@@ -1198,7 +1195,7 @@ namespace MonoDevelop.AssemblyBrowser
 			if (project == null)
 				throw new ArgumentNullException ("project");
 
-			if (projects.Contains (project)) {
+			if (!projects.Add (project)) {
 				// Select the project.
 				if (selectReference) {
 					ITreeNavigator navigator = TreeView.GetNodeAtObject (project);
@@ -1209,14 +1206,16 @@ namespace MonoDevelop.AssemblyBrowser
 
 				return;
 			}
-			projects.Add (project);
+
 			ITreeBuilder builder;
 			if (definitions.Count + projects.Count == 1) {
 				builder = TreeView.LoadTree (project);
 			} else {
-				builder = TreeView.AddChild (project);
+				builder = TreeView.AddChild (project, false);
 			}
-			builder.Selected = builder.Expanded = selectReference;
+
+			if (TreeView.GetSelectedNode () == null || selectReference)
+				builder.Selected = builder.Expanded = selectReference;
 		}
 
 		//MonoDevelop.Components.RoundedFrame popupWidgetFrame;
@@ -1258,6 +1257,7 @@ namespace MonoDevelop.AssemblyBrowser
 			foreach (var def in ensuredDefinitions) {
 				if (!definitions.Contains (def)) {
 					definitions = definitions.Add (def);
+
 					Application.Invoke ((o, args) => {
 						if (ensuredDefinitions.Count + projects.Count == 1) {
 							TreeView.LoadTree (def.LoadingTask.Result);

main/src/addins/MonoDevelop.AssemblyBrowser/MonoDevelop.AssemblyBrowser/AssemblyLoader.cs
@@ -35,6 +35,7 @@ using ICSharpCode.Decompiler.Metadata;
 using System.Reflection.Metadata;
 using System.Linq;
 using ICSharpCode.Decompiler.TypeSystem.Implementation;
+using ICSharpCode.Decompiler.CSharp.Transforms;
 
 namespace MonoDevelop.AssemblyBrowser
 {
@@ -63,18 +64,13 @@ namespace MonoDevelop.AssemblyBrowser
 		public PEFile Assembly => AssemblyTask.Result;
 		public Task<PEFile> AssemblyTask => assemblyDefinitionTaskSource.Task;
 
-		public MetadataReader ModuleDefinition {
-			get {
-				return assemblyLoaderTask.Result.Metadata;
-			}
-		}
-
 		CSharpDecompiler csharpDecompiler;
 
 		public CSharpDecompiler CSharpDecompiler {
 			get {
 				if (csharpDecompiler == null) {
-					csharpDecompiler = new CSharpDecompiler (DecompilerTypeSystem, new ICSharpCode.Decompiler.DecompilerSettings ());
+					csharpDecompiler = new CSharpDecompiler (DecompilerTypeSystem, new ICSharpCode.Decompiler.DecompilerSettings (LanguageVersion.Latest));
+					csharpDecompiler.AstTransforms.Add (new EscapeInvalidIdentifiers ());
 				}
 
 				return csharpDecompiler;
@@ -84,14 +80,11 @@ namespace MonoDevelop.AssemblyBrowser
 		DecompilerTypeSystem decompilerTypeSystem;
 		public DecompilerTypeSystem DecompilerTypeSystem { 
 			get {
-				LoadTypeSystem (Assembly);
+				if (decompilerTypeSystem == null) {
+					decompilerTypeSystem = new DecompilerTypeSystem (Assembly, new AssemblyResolver (Assembly, widget));
+				}
 				return decompilerTypeSystem;
 			}
-		}
-
-		void LoadTypeSystem (PEFile peFile)
-		{
-			decompilerTypeSystem = new DecompilerTypeSystem (peFile, new AssemblyResolver (Assembly, widget));
 		}
 
 		public Error Error { get; internal set; }
@@ -122,19 +115,7 @@ namespace MonoDevelop.AssemblyBrowser
 					assemblyDefinitionTaskSource.SetResult (null);
 					return null;
 				} finally { IsLoaded = true; }
-			});
-		}
-
-		ICompilation typeSystem;
-
-		public ICompilation GetMinimalTypeSystem ()
-		{
-			if (typeSystem != null)
-				return typeSystem;
-			var assembly = Assembly;
-			if (assembly == null)
-				return null;
-			return typeSystem = new SimpleCompilation (assembly.WithOptions (TypeSystemOptions.Default | TypeSystemOptions.Uncached | TypeSystemOptions.KeepModifiers), MinimalCorlib.Instance);
+			}, src.Token);
 		}
 
 		class MyUniversalAssemblyResolver : UniversalAssemblyResolver

main/src/addins/MonoDevelop.AssemblyBrowser/MonoDevelop.AssemblyBrowser/MetadataExtensions.cs
@@ -40,6 +40,11 @@ namespace MonoDevelop.AssemblyBrowser
 			entity.Accessibility == Accessibility.ProtectedOrInternal ||
 			entity.Accessibility == Accessibility.Public;
 
+		public static bool IsPublic (this Microsoft.CodeAnalysis.ISymbol entity) =>
+			entity.DeclaredAccessibility == Microsoft.CodeAnalysis.Accessibility.Protected ||
+			entity.DeclaredAccessibility == Microsoft.CodeAnalysis.Accessibility.ProtectedOrInternal ||
+			entity.DeclaredAccessibility == Microsoft.CodeAnalysis.Accessibility.Public;
+
 		public static string GetStockIcon (this Accessibility attributes)
 		{
 			switch (attributes) {

main/src/addins/MonoDevelop.AssemblyBrowser/MonoDevelop.AssemblyBrowser/TreeNodes/AssemblyBrowserTypeNodeBuilder.cs
@@ -34,6 +34,7 @@ using MonoDevelop.Ide.TypeSystem;
 using ICSharpCode.Decompiler.CSharp.OutputVisitor;
 using System.Threading.Tasks;
 using System.Collections.Generic;
+using System.Linq;
 
 namespace MonoDevelop.AssemblyBrowser
 {
@@ -79,5 +80,16 @@ namespace MonoDevelop.AssemblyBrowser
 			return treeBuilder.GetParentDataItem (typeof(AssemblyLoader), true) != null;
 		}
 
+		protected static void AddFilteredChildren<T> (ITreeBuilder builder, IReadOnlyCollection<T> collection, bool publicApiOnly) where T:IEntity
+		{
+			if (collection.Count == 0)
+				return;
+
+			var children = publicApiOnly
+				? collection.Where (x => x.IsPublic ())
+				: collection;
+
+			builder.AddChildren (collection);
+		}
 	}
 }

main/src/addins/MonoDevelop.AssemblyBrowser/MonoDevelop.AssemblyBrowser/TreeNodes/AssemblyReferenceNodeBuilder.cs
@@ -82,7 +82,7 @@ namespace MonoDevelop.AssemblyBrowser
 				if (e2 == null)
 					return -1;
 				
-				return e1.Name.CompareTo (e2.Name);
+				return string.Compare(e1.Name, e2.Name, StringComparison.Ordinal);
 			} catch (Exception e) {
 				LoggingService.LogError ("Exception in assembly browser sort function.", e);
 				return -1;

main/src/addins/MonoDevelop.AssemblyBrowser/MonoDevelop.AssemblyBrowser/TreeNodes/BaseTypeFolderNodeBuilder.cs
@@ -67,9 +67,7 @@ namespace MonoDevelop.AssemblyBrowser
 		
 		public override int CompareObjects (ITreeNavigator thisNode, ITreeNavigator otherNode)
 		{
-			var r1 = thisNode.DataItem as ITypeReference;
-			var r2 = thisNode.DataItem as ITypeReference;
-			return r1.ToString ().CompareTo (r2.ToString ());
+			return string.Compare (thisNode.NodeName, otherNode.NodeName, StringComparison.Ordinal);
 		}
 	}
 	

commit fe9ada69d7c7a9b2c87446c64eef304dec77e579

[Debugger] Fix regression introduced when optimizing handlers
https://github.com/mono/monodevelop/commit/ef5ea322347547cb6d971b6097b41965312178e6 introduced a regression where we would load the children on a background thread.

That seems to race with debugger suspending/resuming. Go back to running on the UI thread. Also, fix up usage of the token source.

main/src/addins/MonoDevelop.Debugger/MonoDevelop.Debugger/ObjectValueTreeView.cs
@@ -947,15 +947,20 @@ namespace MonoDevelop.Debugger
 			}
 
 			int numberOfChildren = store.IterNChildren (iter);
-			Task.Run (() => {
+
+			var token = cancellationTokenSource.Token;
+			Runtime.RunInMainThread (() => {
+				if (token.IsCancellationRequested)
+					return Array.Empty<ObjectValue> ();
+
 				try {
 					return value.GetRangeOfChildren (numberOfChildren - 1, 20);
 				} catch (Exception ex) {
 					// Note: this should only happen if someone breaks ObjectValue.GetAllChildren()
 					LoggingService.LogError ("Failed to get ObjectValue children.", ex);
-					return new ObjectValue[0];
+					return Array.Empty<ObjectValue> ();
 				}
-			}, cancellationTokenSource.Token).ContinueWith (t => {
+			}).ContinueWith (t => {
 				TreeIter it;
 				if (disposed)
 					return;
@@ -975,7 +980,7 @@ namespace MonoDevelop.Debugger
 				if (compact)
 					RecalculateWidth ();
 				enumerableLoading.Remove (value);
-			}, cancellationTokenSource.Token, TaskContinuationOptions.NotOnCanceled, Runtime.MainTaskScheduler).Ignore ();
+			}, token, TaskContinuationOptions.OnlyOnRanToCompletion, Runtime.MainTaskScheduler).Ignore ();
 		}
 
 		void RefreshRow (TreeIter iter, ObjectValue val)