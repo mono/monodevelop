c6af3e627a728356c3ae44d2c35806862feaf583
Rodrigo Moya <rodrigo.moya@xamarin.com>
2019-07-17 17:57:19 +0200

    [AspNetCore] Add support for browsers as execution targets

--- main/src/addins/MonoDevelop.AspNetCore/MonoDevelop.AspNetCore/AspNetCoreProjectExtension.cs
+++ main/src/addins/MonoDevelop.AspNetCore/MonoDevelop.AspNetCore/AspNetCoreProjectExtension.cs
@@ -24,8 +24,10 @@
 // OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
 // THE SOFTWARE.
 using System;
+using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using GLib;
 using MonoDevelop.Core;
 using MonoDevelop.Core.Execution;
 using MonoDevelop.DotNetCore;
@@ -98,6 +100,20 @@ namespace MonoDevelop.AspNetCore
 				return CheckCertificateThenExecute (monitor, context, configuration, runConfiguration);
 			}
 			return base.OnExecute (monitor, context, configuration, runConfiguration);
+		}
+
+		protected override IEnumerable<ExecutionTarget> OnGetExecutionTargets (ConfigurationSelector configuration)
+		{
+			var result = new List<ExecutionTarget> ();
+			foreach (var browser in IdeServices.DesktopService.GetApplications ("https://localhost")) {
+				if (browser.IsDefault) {
+					result.Insert (0, new AspNetCoreExecutionTarget (browser));
+				} else {
+					result.Add (new AspNetCoreExecutionTarget (browser));
+				}
+			}
+
+			return result.Count > 0 ? result : base.OnGetExecutionTargets (configuration);
 		}
 
 		async Task CheckCertificateThenExecute (ProgressMonitor monitor, ExecutionContext context, ConfigurationSelector configuration, SolutionItemRunConfiguration runConfiguration)
