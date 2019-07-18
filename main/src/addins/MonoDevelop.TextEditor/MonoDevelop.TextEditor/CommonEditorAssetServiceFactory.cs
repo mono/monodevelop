74396cb2ae4d876619339aa63f66f90fbb048e99
David Karlaš <david.karlas@microsoft.com>
2019-07-18 14:44:38 +0200

    Implemented CommonEditorAssetService

---
+++ main/src/addins/MonoDevelop.TextEditor/MonoDevelop.TextEditor/CommonEditorAssetServiceFactory.cs
@@ -0,0 +1,20 @@
+﻿namespace Microsoft.VisualStudio.TextMate.VSWindows
+{
+    using System;
+    using System.Collections.Generic;
+    using System.ComponentModel.Composition;
+    using Microsoft.VisualStudio.Editor;
+    using Microsoft.VisualStudio.Text;
+
+    /// <summary>
+    /// Defines a mechanism for publicly sharing common editor infrastructure such
+    /// as colorization, match highlighting, word completion, etc.
+    /// </summary>
+    [Export (typeof (ICommonEditorAssetServiceFactory))]
+    [Export (typeof (CommonEditorAssetServiceFactory))]
+    public sealed class CommonEditorAssetServiceFactory : ICommonEditorAssetServiceFactory
+    {
+        public ICommonEditorAssetService GetOrCreate(ITextBuffer textBuffer)
+            => new CommonEditorAssetService(this, textBuffer);
+    }
+}
