f5813d288b51742dcb9907f9c611e3cfa87fb60e
Cody Russell <cody@jhu.edu>
2019-08-12 10:18:42 -0500

    [IDE] [a11y] Rewrite new configuration dialog in Xwt
    This is to fix a11y issues with the combo entries, but it's a small
    enough dialog to just rewrite from Gtk to Xwt now.
    
    Fixes VSTS #753601

---
+++ main/src/core/MonoDevelop.Ide/MonoDevelop.Ide.Gui.Dialogs/NewConfigurationDialog.cs
@@ -0,0 +1,172 @@
+//
+// NewConfigurationDialog.cs
+//
+// Author:
+//       Cody Russell <coruss@microsoft.com>
+//
+// Copyright (c) 2019 Microsoft
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in
+// all copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+// THE SOFTWARE.
+
+using System;
+using System.Collections.Generic;
+using MonoDevelop.Projects;
+using MonoDevelop.Core;
+using Xwt;
+
+namespace MonoDevelop.Ide.Gui.Dialogs
+{
+	public class NewConfigurationDialog : Dialog
+	{
+		public string ConfigName {
+			get {
+				string plat = MultiConfigItemOptionsPanel.GetPlatformId (comboPlatform.TextEntry.Text.Trim ());
+				if (string.IsNullOrEmpty (plat))
+					return comboName.TextEntry.Text.Trim ();
+				else
+					return comboName.TextEntry.Text.Trim () + "|" + plat;
+			}
+			set {
+				int i = value.LastIndexOf ('|');
+				if (i == -1) {
+					comboName.TextEntry.Text = value;
+					comboPlatform.TextEntry.Text = string.Empty;
+				} else {
+					comboName.TextEntry.Text = value.Substring (0, i);
+					comboPlatform.TextEntry.Text = MultiConfigItemOptionsPanel.GetPlatformName (value.Substring (i + 1));
+				}
+			}
+		}
+
+		public bool CreateChildren {
+			get => createChildrenCheck.Active;
+		}
+
+		public NewConfigurationDialog (ItemConfigurationCollection<ItemConfiguration> configurations)
+			: this (null, configurations)
+		{
+		}
+
+		public NewConfigurationDialog (IConfigurationTarget item, ItemConfigurationCollection<ItemConfiguration> configurations)
+		{
+			Build ();
+
+			this.configurations = configurations;
+
+			SetupConfigs (item);
+		}
+
+		void Build ()
+		{
+			Padding = 0;
+			Resizable = false;
+			Title = GettextCatalog.GetString ("New Configuration");
+
+			var mainVBox = new VBox () { Spacing = 6, Margin = 6 };
+			var table = new Table { DefaultColumnSpacing = 6, DefaultRowSpacing = 6 };
+
+			var label = new Label { Text = GettextCatalog.GetString ("Name:") };
+			comboName = new ComboBoxEntry ();
+			comboName.Accessible.LabelWidget = label;
+			table.Add (label, 0, 0);
+			table.Add (comboName, 1, 0);
+
+			comboName.TextEntry.Changed += TextEntry_Changed;
+
+			label = new Label { Text = GettextCatalog.GetString ("Platform:") };
+			comboPlatform = new ComboBoxEntry ();
+			comboPlatform.Accessible.LabelWidget = label;
+			comboPlatform.WidthRequest = 250;
+			table.Add (label, 0, 1);
+			table.Add (comboPlatform, 1, 1);
+
+			createChildrenCheck = new CheckBox { Label = GettextCatalog.GetString ("Create configurations for all solution items") };
+
+			mainVBox.PackStart (table, true, true);
+			mainVBox.PackStart (createChildrenCheck, true);
+
+			var cancelButton = new DialogButton (Command.Cancel);
+			Buttons.Add (cancelButton);
+
+			okButton = new DialogButton (Command.Ok);
+			okButton.Clicked += OkButtonClicked;
+			Buttons.Add (okButton);
+
+			DefaultCommand = okButton.Command;
+
+			Content = mainVBox;
+		}
+
+		private void TextEntry_Changed (object sender, EventArgs e)
+		{
+			// This does nothing. For some reason without having this attached to
+			// comboName.TextEntry.Changed, when we hit OK then there is an empty
+			// string in comboName.TextEntry (even though the text is visible).
+			// This doesn't happen on comboPlatform.TextEntry though.
+		}
+
+		protected override void Dispose (bool disposing)
+		{
+			base.Dispose (disposing);
+
+			if (disposing) {
+				okButton.Clicked -= OkButtonClicked;
+				comboName.TextEntry.Changed -= TextEntry_Changed;
+			}
+		}
+
+		void SetupConfigs (IConfigurationTarget item)
+		{
+			var configs = new HashSet<string> ();
+			var platforms = new HashSet<string> ();
+
+			foreach (var conf in configurations) {
+				if (configs.Add (conf.Name))
+					comboName.Items.Add (conf.Name);
+				var plat = MultiConfigItemOptionsPanel.GetPlatformName (conf.Platform);
+				if (platforms.Add (plat))
+					comboPlatform.Items.Add (plat);
+			}
+
+			comboPlatform.TextEntry.Text = MultiConfigItemOptionsPanel.GetPlatformName ("");
+			if (!(item is Solution)) {
+				createChildrenCheck.Active = false;
+				createChildrenCheck.Visible = false;
+			}
+		}
+
+		void OkButtonClicked (object sender, EventArgs e)
+		{
+			var name = comboName.TextEntry.Text.Trim ();
+			if (name.Length == 0 || name.IndexOf ('|') != -1) {
+				MessageService.ShowWarning (this, GettextCatalog.GetString ("Please enter a valid configuration name."));
+			} else if (configurations[ConfigName] != null) {
+				MessageService.ShowWarning (this, GettextCatalog.GetString ("A configuration with the name '{0}' already exists.", ConfigName));
+			} else {
+				Respond (Command.Ok);
+			}
+		}
+
+		ItemConfigurationCollection<ItemConfiguration> configurations;
+		ComboBoxEntry comboName;
+		ComboBoxEntry comboPlatform;
+		CheckBox createChildrenCheck;
+		DialogButton okButton;
+	}
+}
