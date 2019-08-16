252a1555917291d5f2d10479139e854d5a5857d4
Kirill Osenkov <github@osenkov.com>
2019-08-16 11:12:54 -0700

    Fix Windows build.

--- main/src/core/MonoDevelop.Ide/MonoDevelop.Ide.Gui/ViewCommandHandlers.cs
+++ main/src/core/MonoDevelop.Ide/MonoDevelop.Ide.Gui/ViewCommandHandlers.cs
@@ -525,8 +525,10 @@ namespace MonoDevelop.Ide.Gui
 			info.Enabled = info.Visible = false;
 #else
 			info.Text = IsFoldMarkerMarginEnabled ? GettextCatalog.GetString ("Disable _Folding") : GettextCatalog.GetString ("Enable _Folding");
+#if !WINDOWS
 			info.Enabled = GetContent<IFoldable> () != null ||
 				GetContent<Microsoft.VisualStudio.Text.Editor.ITextView3> () != null;
+#endif
 			// As we need to support both the new and the legacy editor, we need to check if perhaps
 			// we are running in the new one. The legacy editor already implements <see cref="ITextView"/>
 			// so we can't simply look for that and we do not want to import anything related to Cocoa. 
