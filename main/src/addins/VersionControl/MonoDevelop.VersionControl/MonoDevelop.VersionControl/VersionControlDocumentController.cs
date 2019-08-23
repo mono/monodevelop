d58a3c5c0bc01073dcfb528f3bed685488866ed3
Mike Krüger <mikkrg@microsoft.com>
2019-08-23 09:04:10 +0200

    [VersionControl] Document controller now reacts on file status change.

--- main/src/addins/VersionControl/MonoDevelop.VersionControl/MonoDevelop.VersionControl/VersionControlDocumentController.cs
+++ main/src/addins/VersionControl/MonoDevelop.VersionControl/MonoDevelop.VersionControl/VersionControlDocumentController.cs
@@ -49,6 +49,7 @@ namespace MonoDevelop.VersionControl
 		DocumentView blameView;
 		DocumentView logView;
 		DocumentView mergeView;
+		private DocumentView mainView;
 
 		public override async Task<bool> SupportsController (DocumentController controller)
 		{
@@ -70,22 +71,84 @@ namespace MonoDevelop.VersionControl
 			repo = VersionControlService.GetRepository (project);
 			if (repo == null)
 				return false;
-			return repo.TryGetVersionInfo (fileController.FilePath, out var info) && info.IsVersioned;
+
+			return true;
 		}
 
 		protected internal override async Task<DocumentView> OnInitializeView ()
 		{
-			var mainView = await base.OnInitializeView ();
+			mainView = await base.OnInitializeView ();
 			var controller = (FileDocumentController)Controller;
 			var item = new VersionControlItem (repo, project, controller.FilePath, false, null);
 			vcInfo = new VersionControlDocumentInfo (this, Controller, item, item.Repository);
 			vcInfo.Document = Controller.Document;
-
-			diffView = await TryAttachView (mainView, vcInfo, DiffCommand.DiffViewHandlers, GettextCatalog.GetString ("Changes"), GettextCatalog.GetString ("Shows the differences in the code between the current code and the version in the repository"));
-			blameView = await TryAttachView (mainView, vcInfo, BlameCommand.BlameViewHandlers, GettextCatalog.GetString ("Authors"), GettextCatalog.GetString ("Shows the authors of the current file"));
-			logView = await TryAttachView (mainView, vcInfo, LogCommand.LogViewHandlers, GettextCatalog.GetString ("Log"), GettextCatalog.GetString ("Shows the source control log for the current file"));
-			mergeView = await TryAttachView (mainView, vcInfo, MergeCommand.MergeViewHandlers, GettextCatalog.GetString ("Merge"), GettextCatalog.GetString ("Shows the merge view for the current file"));
+			await UpdateSubviewsAsync ();
+			VersionControlService.FileStatusChanged += VersionControlService_FileStatusChanged;
 			return mainView;
+		}
+
+		public override void Dispose ()
+		{
+			VersionControlService.FileStatusChanged -= VersionControlService_FileStatusChanged;
+			base.Dispose ();
+		}
+
+		void VersionControlService_FileStatusChanged (object sender, FileUpdateEventArgs args)
+		{
+
+			foreach (var file in args) {
+				if (!vcInfo.Document.FilePath.IsNullOrEmpty && vcInfo.Document.FilePath.Equals (file.FilePath)) {
+					Runtime.RunInMainThread (async () => {
+						await UpdateSubviewsAsync ();
+					});
+				}
+			}
+		}
+
+		bool showSubviews;
+		async Task UpdateSubviewsAsync ()
+		{
+			var hasVersionInfo = repo.TryGetVersionInfo (this.vcInfo.Document.FilePath, out var info);
+			if (hasVersionInfo)
+				vcInfo.Item.VersionInfo = info;
+
+			if (!hasVersionInfo || !info.IsVersioned) {
+				if (!showSubviews)
+					return;
+				showSubviews = false;
+				if (diffView != null) {
+					mainView.AttachedViews.Remove (diffView);
+					diffView.Dispose ();
+					diffView = null;
+				}
+
+				if (blameView != null) {
+					mainView.AttachedViews.Remove (blameView);
+					blameView.Dispose ();
+					blameView = null;
+				}
+
+				if (logView != null) {
+					mainView.AttachedViews.Remove (logView);
+					logView.Dispose ();
+					logView = null;
+				}
+
+				if (mergeView != null) {
+					mainView.AttachedViews.Remove (mergeView);
+					mergeView.Dispose ();
+					mergeView = null;
+				}
+
+			} else {
+				if (showSubviews)
+					return;
+				showSubviews = true;
+				diffView = await TryAttachView (mainView, vcInfo, DiffCommand.DiffViewHandlers, GettextCatalog.GetString ("Changes"), GettextCatalog.GetString ("Shows the differences in the code between the current code and the version in the repository"));
+				blameView = await TryAttachView (mainView, vcInfo, BlameCommand.BlameViewHandlers, GettextCatalog.GetString ("Authors"), GettextCatalog.GetString ("Shows the authors of the current file"));
+				logView = await TryAttachView (mainView, vcInfo, LogCommand.LogViewHandlers, GettextCatalog.GetString ("Log"), GettextCatalog.GetString ("Shows the source control log for the current file"));
+				mergeView = await TryAttachView (mainView, vcInfo, MergeCommand.MergeViewHandlers, GettextCatalog.GetString ("Merge"), GettextCatalog.GetString ("Shows the merge view for the current file"));
+			}
 		}
 
 		async Task<DocumentView> TryAttachView (DocumentView mainView, VersionControlDocumentInfo info, string type, string title, string description)
