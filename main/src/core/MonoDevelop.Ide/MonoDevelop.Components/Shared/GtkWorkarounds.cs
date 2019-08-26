fce944aaaeb9f31f737c1cf9a94470f374b3b4e8
therzok <marius.ungureanu@xamarin.com>
2019-08-26 16:04:03 +0300

    [Mac] Fix Terminate selector signature and reuse more code

--- main/src/core/MonoDevelop.Ide/MonoDevelop.Components/Shared/GtkWorkarounds.cs
+++ main/src/core/MonoDevelop.Ide/MonoDevelop.Components/Shared/GtkWorkarounds.cs
@@ -34,29 +34,14 @@ namespace MonoDevelop.Components
 {
 	public static partial class GtkWorkarounds
 	{
-#if MAC
-		const string LIBOBJC = ObjCRuntime.Constants.ObjectiveCLibrary;
-
-		[DllImport (LIBOBJC, EntryPoint = "objc_msgSend")]
-		static extern IntPtr objc_msgSend_IntPtr (IntPtr klass, IntPtr selector);
-
-		[DllImport (LIBOBJC, EntryPoint = "objc_msgSend")]
-		static extern void objc_msgSend_NSInt64_NSInt32 (IntPtr klass, IntPtr selector, int arg);
-
-		static void MacTerminate ()
-		{
-			var app = objc_msgSend_IntPtr (
-				ObjCRuntime.Class.GetHandle ("NSApplication"),
-				ObjCRuntime.Selector.GetHandle ("sharedApplication"));
-
-			objc_msgSend_NSInt64_NSInt32 (app, ObjCRuntime.Selector.GetHandle ("terminate:"), 0);
-		}
-#endif
-
 		public static void Terminate ()
 		{
 #if MAC
-			MacTerminate ();
+			var app = Mac.Messaging.IntPtr_objc_msgSend (
+				ObjCRuntime.Class.GetHandle ("NSApplication"),
+				ObjCRuntime.Selector.GetHandle ("sharedApplication"));
+
+			Mac.Messaging.void_objc_msgSend_IntPtr (app, ObjCRuntime.Selector.GetHandle ("terminate:"), IntPtr.Zero);
 #endif
 		}
 	}
