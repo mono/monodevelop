cb10a4f206bd8cedd064a6679267f11de36083c8
Matt Ward <matt.ward@microsoft.com>
2019-08-27 10:59:56 +0100

    [NuGet] Update NuGet to version 5.3.0-rtm.6192
    Matches the version included with MSBuild in Mono 6.4.0.179
    
    Fixes VSTS #972633 Update NuGet to 5.3.0-rtm.6192 to match mono's
    msbuild

--- main/src/addins/MonoDevelop.PackageManagement/MonoDevelop.PackageManagement.Tests/MonoDevelop.PackageManagement.Tests.Helpers/FakePackageSearchMetadata.cs
+++ main/src/addins/MonoDevelop.PackageManagement/MonoDevelop.PackageManagement.Tests/MonoDevelop.PackageManagement.Tests.Helpers/FakePackageSearchMetadata.cs
@@ -29,6 +29,7 @@ using System.Collections.Generic;
 using System.Threading.Tasks;
 using NuGet.Packaging;
 using NuGet.Packaging.Core;
+using NuGet.Protocol;
 using NuGet.Protocol.Core.Types;
 
 namespace MonoDevelop.PackageManagement.Tests.Helpers
@@ -78,6 +79,11 @@ namespace MonoDevelop.PackageManagement.Tests.Helpers
 		public Uri PackageDetailsUrl { get; set; }
 
 		public Task<IEnumerable<VersionInfo>> GetVersionsAsync ()
+		{
+			throw new NotImplementedException ();
+		}
+
+		public Task<PackageDeprecationMetadata> GetDeprecationMetadataAsync ()
 		{
 			throw new NotImplementedException ();
 		}
