2592c46cd590ae23dc2fc87d817558bfd468ca60
Keting Yang <yang25k@mtholyoke.edu>
2019-08-29 09:59:58 -0400

    Sort list

--- main/src/addins/MonoDevelop.UnitTesting/MonoDevelop.UnitTesting.VsTest/VsTestTestClass.cs
+++ main/src/addins/MonoDevelop.UnitTesting/MonoDevelop.UnitTesting.VsTest/VsTestTestClass.cs
@@ -23,16 +23,15 @@
 // LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
 // OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
 // THE SOFTWARE.
-
+using System;
 using System.Collections.Generic;
+using System.Linq;
 using Microsoft.VisualStudio.TestPlatform.ObjectModel;
 using MonoDevelop.Core.Execution;
+using MonoDevelop.Ide;
+using MonoDevelop.Ide.TypeSystem;
 using MonoDevelop.Projects;
 using MonoDevelop.UnitTesting;
-using MonoDevelop.Ide;
-using System;
-using MonoDevelop.Ide.TypeSystem;
-using System.Linq;
 
 namespace MonoDevelop.UnitTesting.VsTest
 {
