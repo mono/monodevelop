dbbd300c90d29cb231db5a458743eabcf94f9365
Rodrigo Moya <rodrigo.moya@xamarin.com>
2019-09-04 14:10:33 +0200

    [DotNetCore] Add .NET Core runtime "component"

--- main/src/addins/MonoDevelop.DotNetCore/MonoDevelop.DotNetCore/DotNetCoreSystemInformation.cs
+++ main/src/addins/MonoDevelop.DotNetCore/MonoDevelop.DotNetCore/DotNetCoreSystemInformation.cs
@@ -35,7 +35,7 @@ namespace MonoDevelop.DotNetCore
 {
 	class DotNetCoreSystemInformation : ProductInformationProvider
 	{
-		public override string Title => GettextCatalog.GetString (".NET Core");
+		public override string Title => GettextCatalog.GetString (".NET Core SDK");
 
 		public override string Description => GetDescription ();
 
@@ -67,8 +67,6 @@ namespace MonoDevelop.DotNetCore
 		{
 			var description = new StringBuilder ();
 
-			description.AppendLine (GettextCatalog.GetString ("Runtime: {0}", GetDotNetRuntimeLocation ()));
-			AppendDotNetCoreRuntimeVersions (description);
 			description.AppendLine (GettextCatalog.GetString ("SDK: {0}", GetDotNetSdkLocation ()));
 			AppendDotNetCoreSdkVersions (description);
 			description.AppendLine (GettextCatalog.GetString ("MSBuild SDKs: {0}", GetMSBuildSdksLocation ()));
@@ -76,15 +74,7 @@ namespace MonoDevelop.DotNetCore
 			return description.ToString ();
 		}
 
-		static string GetDotNetRuntimeLocation ()
-		{
-			if (DotNetCoreRuntime.IsInstalled)
-				return DotNetCoreRuntime.FileName;
-
-			return GetNotInstalledString ();
-		}
-
-		static string GetNotInstalledString ()
+		internal static string GetNotInstalledString ()
 		{
 			return GettextCatalog.GetString ("Not installed");
 		}
@@ -108,15 +98,6 @@ namespace MonoDevelop.DotNetCore
 			return GetNotInstalledString ();
 		}
 
-		static void AppendDotNetCoreRuntimeVersions (StringBuilder description)
-		{
-			AppendVersions (
-				description,
-				DotNetCoreRuntime.Versions,
-				version => GettextCatalog.GetString ("Runtime Version: {0}", version),
-				() => GettextCatalog.GetString ("Runtime Versions:"));
-		}
-
 		static void AppendDotNetCoreSdkVersions (StringBuilder description)
 		{
 			AppendVersions (
@@ -126,7 +107,7 @@ namespace MonoDevelop.DotNetCore
 				() => GettextCatalog.GetString ("SDK Versions:"));
 		}
 
-		static void AppendVersions (
+		internal static void AppendVersions (
 			StringBuilder description,
 			DotNetCoreVersion[] versions,
 			Func<DotNetCoreVersion, string> getSingleVersionString,
