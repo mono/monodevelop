7bc09ad9d5ccb0483a9f1a8f53647eedf7aa8d43
Matt Ward <matt.ward@microsoft.com>
2019-09-11 16:47:15 +0100

    [DotNetCore] Do not show update count label on Frameworks folder
    The Frameworks folder for .NET Core 3.0 and .NET Standard 2.1
    projects would show an update count label if the project had package
    updates. Framework references cannot be updated in the IDE, and also
    are not typically defined in the project itself, so the update count
    should not be shown.
    
    Fixes VSTS #980887 - Frameworks node under dependencies has a message
    about updates next to it

---
+++ main/src/addins/MonoDevelop.DotNetCore/MonoDevelop.DotNetCore.Tests/MonoDevelop.DotNetCore.Tests/FakeUpdatedPackagesInWorkspace.cs
@@ -0,0 +1,76 @@
+//
+// FakeUpdatedPackagesInWorkspace.cs
+//
+// Author:
+//       Matt Ward <matt.ward@microsoft.com>
+//
+// Copyright (c) 2019 Microsoft Corporation
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in
+// all copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+// THE SOFTWARE.
+
+using System.Collections.Generic;
+using System.Linq;
+using MonoDevelop.PackageManagement;
+using NuGet.Packaging.Core;
+using NuGet.Protocol.Core.Types;
+
+namespace MonoDevelop.DotNetCore.Tests
+{
+	class FakeUpdatedPackagesInWorkspace : IUpdatedNuGetPackagesInWorkspace
+	{
+		public List<UpdatedNuGetPackagesInProject> ProjectsWithUpdatedPackages = new List<UpdatedNuGetPackagesInProject> ();
+
+		public void AddUpdatedPackages (params PackageIdentity [] packages)
+		{
+			var updatedPackages = new UpdatedNuGetPackagesInProject (null, packages);
+			ProjectsWithUpdatedPackages.Add (updatedPackages);
+		}
+
+		public void Clear ()
+		{
+			ProjectsWithUpdatedPackages.Clear ();
+		}
+
+		public void CheckForUpdates ()
+		{
+		}
+
+		public UpdatedNuGetPackagesInProject GetUpdatedPackages (IDotNetProject project)
+		{
+			return ProjectsWithUpdatedPackages.FirstOrDefault () ?? new UpdatedNuGetPackagesInProject (project);
+		}
+
+		public bool AnyUpdates ()
+		{
+			return ProjectsWithUpdatedPackages.Any ();
+		}
+
+		public void Clear (ISolution solution)
+		{
+		}
+
+		public void CheckForUpdates (ISolution solution)
+		{
+		}
+
+		public void CheckForUpdates (ISolution solution, ISourceRepositoryProvider sourceRepositoryProvider)
+		{
+		}
+	}
+}
