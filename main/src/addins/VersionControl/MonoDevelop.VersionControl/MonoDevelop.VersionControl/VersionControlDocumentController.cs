5cd3d07f8dc4c015c2caa9b1012733d70a17226f
Lluis Sanchez <llsan@microsoft.com>
2019-09-12 10:56:43 +0200

    Fix version control view initialization issue
    When a controller is being initialized, the Document property may not
    yet be set. This happens for example, when a designer view creates a
    editor view on its own to be added to a container together with the
    designer. In that scenario, the editor view is created and fully
    initialized before it is added to the document. The old implementation
    of VersionControlDocumentController relied on the Document property to
    be set to get the file name. Instead, now it gets the file name from
    the controller.
    
    There are other classes that use the
    VersionControlDocumentInfo.Document property, but they do it after the
    view is initialized and added to a document. To make sure that
    property returns a valid document, it now returns the document
    directly from the controller.
    
    Also removed the use of weak references for the attached views. There
    references should go away together with the extension object.

--- main/src/addins/VersionControl/MonoDevelop.VersionControl/MonoDevelop.VersionControl/VersionControlDocumentController.cs
+++ main/src/addins/VersionControl/MonoDevelop.VersionControl/MonoDevelop.VersionControl/VersionControlDocumentController.cs
@@ -44,10 +44,10 @@ namespace MonoDevelop.VersionControl
 		WorkspaceObject project;
 		Repository repo;
 
-		readonly WeakReference<DocumentView> diffViewRef = new WeakReference<DocumentView> (null);
-		readonly WeakReference<DocumentView> blameViewRef = new WeakReference<DocumentView> (null);
-		readonly WeakReference<DocumentView> logViewRef = new WeakReference<DocumentView> (null);
-		readonly WeakReference<DocumentView> mergeViewRef = new WeakReference<DocumentView> (null);
+		DocumentView diffView;
+		DocumentView blameView;
+		DocumentView logView;
+		DocumentView mergeView;
 
 		DocumentView mainView;
 		VersionControlDocumentInfo vcInfo;
@@ -81,9 +81,7 @@ namespace MonoDevelop.VersionControl
 			mainView = await base.OnInitializeView ();
 			var controller = (FileDocumentController)Controller;
 			var item = new VersionControlItem (repo, project, controller.FilePath, false, null);
-			vcInfo = new VersionControlDocumentInfo (this, Controller, item, item.Repository) {
-				Document = Controller.Document
-			};
+			vcInfo = new VersionControlDocumentInfo (this, Controller, item, item.Repository);
 			await UpdateSubviewsAsync ();
 			VersionControlService.FileStatusChanged += VersionControlService_FileStatusChanged;
 			return mainView;
@@ -97,12 +95,12 @@ namespace MonoDevelop.VersionControl
 
 		void VersionControlService_FileStatusChanged (object sender, FileUpdateEventArgs args)
 		{
-
+			var controller = (FileDocumentController)Controller;
 			foreach (var file in args) {
-				if (!vcInfo.Document.FilePath.IsNullOrEmpty && vcInfo.Document.FilePath.Equals (file.FilePath)) {
+				if (!controller.FilePath.IsNullOrEmpty && controller.FilePath.Equals (file.FilePath)) {
 					Runtime.RunInMainThread (async () => {
 						await UpdateSubviewsAsync ();
-					});
+					}).Ignore();
 				}
 			}
 		}
@@ -110,7 +108,8 @@ namespace MonoDevelop.VersionControl
 		bool showSubviews;
 		async Task UpdateSubviewsAsync ()
 		{
-			var hasVersionInfo = repo.TryGetVersionInfo (this.vcInfo.Document.FilePath, out var info);
+			var controller = (FileDocumentController)Controller;
+			var hasVersionInfo = repo.TryGetVersionInfo (controller.FilePath, out var info);
 			if (hasVersionInfo)
 				vcInfo.Item.VersionInfo = info;
 
@@ -118,37 +117,37 @@ namespace MonoDevelop.VersionControl
 				if (!showSubviews)
 					return;
 				showSubviews = false;
-				if (diffViewRef.TryGetTarget (out var diffView) && diffView != null) {
+				if (diffView != null) {
 					mainView.AttachedViews.Remove (diffView);
 					diffView.Dispose ();
-					diffViewRef.SetTarget (null);
+					diffView = null;
 				}
 
-				if (blameViewRef.TryGetTarget (out var blameView) && blameView != null) {
+				if (blameView != null) {
 					mainView.AttachedViews.Remove (blameView);
 					blameView.Dispose ();
-					blameViewRef.SetTarget (null);
+					blameView = null;
 				}
 
-				if (logViewRef.TryGetTarget (out var logView) && logView != null) {
+				if (logView != null) {
 					mainView.AttachedViews.Remove (logView);
 					logView.Dispose ();
-					logViewRef.SetTarget (null);
+					logView = null;
 				}
 
-				if (mergeViewRef.TryGetTarget (out var mergeView) && mergeView != null) {
+				if (mergeView != null) {
 					mainView.AttachedViews.Remove (mergeView);
 					mergeView.Dispose ();
-					mergeViewRef.SetTarget (null);
+					mergeView = null;
 				}
 			} else {
 				if (showSubviews)
 					return;
 				showSubviews = true;
-				diffViewRef.SetTarget (await TryAttachView (mainView, vcInfo, DiffCommand.DiffViewHandlers, GettextCatalog.GetString ("Changes"), GettextCatalog.GetString ("Shows the differences in the code between the current code and the version in the repository")));
-				blameViewRef.SetTarget (await TryAttachView (mainView, vcInfo, BlameCommand.BlameViewHandlers, GettextCatalog.GetString ("Authors"), GettextCatalog.GetString ("Shows the authors of the current file")));
-				logViewRef.SetTarget (await TryAttachView (mainView, vcInfo, LogCommand.LogViewHandlers, GettextCatalog.GetString ("Log"), GettextCatalog.GetString ("Shows the source control log for the current file")));
-				mergeViewRef.SetTarget (await TryAttachView (mainView, vcInfo, MergeCommand.MergeViewHandlers, GettextCatalog.GetString ("Merge"), GettextCatalog.GetString ("Shows the merge view for the current file")));
+				diffView = await TryAttachView (mainView, vcInfo, DiffCommand.DiffViewHandlers, GettextCatalog.GetString ("Changes"), GettextCatalog.GetString ("Shows the differences in the code between the current code and the version in the repository"));
+				blameView = await TryAttachView (mainView, vcInfo, BlameCommand.BlameViewHandlers, GettextCatalog.GetString ("Authors"), GettextCatalog.GetString ("Shows the authors of the current file"));
+				logView = await TryAttachView (mainView, vcInfo, LogCommand.LogViewHandlers, GettextCatalog.GetString ("Log"), GettextCatalog.GetString ("Shows the source control log for the current file"));
+				mergeView = await TryAttachView (mainView, vcInfo, MergeCommand.MergeViewHandlers, GettextCatalog.GetString ("Merge"), GettextCatalog.GetString ("Shows the merge view for the current file"));
 			}
 		}
 
@@ -172,7 +171,7 @@ namespace MonoDevelop.VersionControl
 
 		internal void ShowDiffView (Revision originalRevision = null, Revision diffRevision = null, int line = -1)
 		{
-			if (diffViewRef.TryGetTarget (out var diffView)) {
+			if (diffView != null) {
 				if (originalRevision != null && diffRevision != null && diffView.SourceController is DiffView content) {
 					content.ComparisonWidget.info.RunAfterUpdate (delegate {
 						content.ComparisonWidget.SetRevision (content.ComparisonWidget.DiffEditor, diffRevision);
@@ -189,13 +188,13 @@ namespace MonoDevelop.VersionControl
 
 		internal void ShowBlameView ()
 		{
-			if (blameViewRef.TryGetTarget(out var blameView))
+			if (blameView != null)
 				blameView.SetActive ();
 		}
 
 		internal void ShowLogView (Revision revision = null)
 		{
-			if (logViewRef.TryGetTarget (out var logView)) {
+			if (logView != null) {
 				if (revision != null && logView.SourceController is LogView content)
 					content.LogWidget.SelectedRevision = revision;
 
@@ -205,7 +204,7 @@ namespace MonoDevelop.VersionControl
 
 		internal void ShowMergeView ()
 		{
-			if (mergeViewRef.TryGetTarget (out var mergeView))
+			if (mergeView != null)
 				mergeView.SetActive ();
 		}
 	}
