9aab05e1a09d5ef3d29db8e6ba12fc08747af173
Mike Krüger <mikkrg@microsoft.com>
2019-09-19 14:57:23 +0200

    Fixes VSTS Bug 983399: [VersionControl] Credential dialog is on top of all applications
    https://devdiv.visualstudio.com/DevDiv/_workitems/edit/983399
    
    ProgressDialog didn't had the parent set. In VS4Mac the parent is a
    NSWindow so it's needed to set the parent in that case after the gtk
    window has been created.

--- main/src/core/MonoDevelop.Ide/MonoDevelop.Components/Window.cs
+++ main/src/core/MonoDevelop.Ide/MonoDevelop.Components/Window.cs
@@ -24,6 +24,10 @@
 // OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
 // THE SOFTWARE.
 using System;
+using MonoDevelop.Core;
+#if MAC
+using AppKit;
+#endif
 
 namespace MonoDevelop.Components
 {
@@ -207,5 +211,26 @@ namespace MonoDevelop.Components
 			throw new NotSupportedException ();
 		}
 	}
-}
 
+	public static class WindowExt
+	{
+		public static void SetParentToWindow (this Gtk.Window window, MonoDevelop.Components.Window parent)
+		{
+			try {
+#if MAC
+				var nsWindow = parent.GetNativeWidget<NSWindow> ();
+				if (nsWindow != null) {
+					var myNSWindow = MonoDevelop.Components.Mac.GtkMacInterop.GetNSWindow (window);
+					myNSWindow.ParentWindow = nsWindow;
+				} else {
+					window.TransientFor = parent;
+				}
+#else
+				window.TransientFor = parent;
+#endif
+			} catch (Exception e) {
+				LoggingService.LogInternalError ("Error while setting parent.", e);
+			}
+		}
+	}
+}
