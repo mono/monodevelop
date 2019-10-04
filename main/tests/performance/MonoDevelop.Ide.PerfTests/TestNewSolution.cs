8293ffcd1e61fc869cbbf111ef0457ea978aff1c
Matt Ward <matt.ward@microsoft.com>
2019-10-04 18:21:41 +0100

    [PerfTests] Add new Forms solution test and fix template selection
    Added a test that creates a Xamarin.Forms Blank App for iOS and Android.
    Fixed the template selection in the New Project dialog. The existing
    NewProjectController UITest code did not select the template categories.
    The console test was working since this was being selected by default.

--- main/tests/performance/MonoDevelop.Ide.PerfTests/TestNewSolution.cs
+++ main/tests/performance/MonoDevelop.Ide.PerfTests/TestNewSolution.cs
@@ -24,6 +24,9 @@
 // OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
 // THE SOFTWARE.
 
+using System;
+using System.Linq;
+using MonoDevelop.Components.AutoTest;
 using MonoDevelop.Core;
 using MonoDevelop.Core.Instrumentation;
 using MonoDevelop.PerformanceTesting;
@@ -44,7 +47,7 @@ namespace MonoDevelop.Ide.PerfTests
 		}
 
 		[Test]
-		[Benchmark (Tolerance = 0.50)]
+		[Benchmark (Tolerance = 2)]
 		public void TestNewConsoleProject ()
 		{
 			OpenApplicationAndWait ();
@@ -70,13 +73,69 @@ namespace MonoDevelop.Ide.PerfTests
 			Benchmark.SetTime (t.TotalSeconds);
 		}
 
-		void CreateNewSolutionAndWait (TemplateSelectionOptions templateOptions)
+		[Test]
+		[Benchmark (Tolerance = 5)]
+		public void TestNewXamarinFormsProject ()
+		{
+			OpenApplicationAndWait ();
+
+			var templateOptions = new TemplateSelectionOptions {
+				CategoryRoot = "Multiplatform",
+				Category = "App",
+				TemplateKindRoot = "Xamarin.Forms",
+				TemplateKind = "Blank Forms App"
+			};
+
+			CreateNewXamarinFormsSolutionAndWait (templateOptions);
+
+			Session.RunAndWaitForTimer (() => {
+				// Do nothing. Cannot call this with CreateNewSolutionAndWait since the timer is not available
+				// at that point but is available after it has been called. Using RunAndWaitForTimer here
+				// ensures we wait for the timer to change. Otherwise we would get the timer with no duration
+				// since the project is still being created.
+			}, "Ide.NewProjectDialog.ProjectCreated", 60000);
+
+			var t = Session.GetTimerDuration ("Ide.NewProjectDialog.ProjectCreated");
+
+			Benchmark.SetTime (t.TotalSeconds);
+		}
+
+		void CreateNewSolutionAndWait (TemplateSelectionOptions template, Action<NewProjectController> wizardPageAction = null)
 		{
 			var newProject = new NewProjectController ();
 			newProject.Open ();
 
-			newProject.Select (templateOptions);
+			AppQuery categoryUIQuery (AppQuery c) => templateCategoriesQuery (c)
+				.Contains (template.CategoryRoot)
+				.Children ()
+				.Text (template.Category);
+
+			Session.SelectElement (templateCategoriesWidgetQuery);
+			Session.SelectElement (categoryUIQuery);
+			bool result = Session.WaitForElement (c => categoryUIQuery (c).Selected ()).Any ();
+
+			Assert.IsTrue (result, "Project template category not selected {0}/{1}", template.CategoryRoot, template.Category);
+
+			AppQuery templateUIQuery (AppQuery c) => templatesQuery (c)
+				.Contains (template.TemplateKindRoot)
+				.Children ()
+				.Text (template.TemplateKind);
+
+			Session.SelectElement (templatesWidgetQuery);
+			Session.SelectElement (templateUIQuery);
+			result = Session.WaitForElement (c => templateUIQuery (c).Selected ()).Any ();
+
+			Assert.IsTrue (result, "Project template not selected {0}/{1}", template.TemplateKindRoot, template.TemplateKind);
+
+			// Does not work.
+			//result = newProject.SelectTemplateType (template.CategoryRoot, template.Category);
+
+			// Does not work.
+			//result = newProject.SelectTemplate (template.TemplateKindRoot, template.TemplateKind);
+
 			newProject.Next ();
+
+			wizardPageAction?.Invoke (newProject);
 
 			newProject.SetProjectName ("NewProjectTest", false);
 
@@ -94,5 +153,29 @@ namespace MonoDevelop.Ide.PerfTests
 
 			newProject.Create ();
 		}
+
+		void CreateNewXamarinFormsSolutionAndWait (TemplateSelectionOptions templateOptions)
+		{
+			CreateNewSolutionAndWait (templateOptions, FillInXamarinFormsTemplateWizardPage);
+		}
+
+		void FillInXamarinFormsTemplateWizardPage (NewProjectController newProject)
+		{
+			Session.EnterText (c => c.Marked ("appNameTextBox"), "formstest");
+			Session.EnterText (c => c.Marked ("organizationTextBox"), "com.test");
+
+			Session.ToggleElement (c => c.CheckButton ().Text ("Android"), true);
+			Session.WaitForElement (c => c.CheckButton ().Text ("iOS").Property ("Active", true));
+
+			Session.ToggleElement (c => c.CheckButton ().Text ("Use .NET Standard").Visibility (true), true);
+			Session.WaitForElement (c => c.CheckButton ().Text ("Use .NET Standard").Visibility (true).Property ("Active", true));
+
+			newProject.Next	();
+		}
+
+		readonly Func<AppQuery, AppQuery> templateCategoriesWidgetQuery = c => c.TreeView ().Marked ("templateCategoriesTreeView");
+		readonly Func<AppQuery, AppQuery> templatesWidgetQuery = c => c.TreeView ().Marked ("templatesTreeView");
+		readonly Func<AppQuery, AppQuery> templateCategoriesQuery = c => c.TreeView ().Marked ("templateCategoriesTreeView").Model ("templateCategoriesListStore__Name");
+		readonly Func<AppQuery, AppQuery> templatesQuery = c => c.TreeView ().Marked ("templatesTreeView").Model ("templateListStore__Name");
 	}
 }
