1b73fe181598f202dfdd5c113c904f261715024d
Matt Ward <matt.ward@microsoft.com>
2019-10-07 16:13:05 +0100

    [Core] Fix ReferencePath items being ignored by the type system
    The DotNetProject's GetReferencedAssemblies method was not returning
    all references used by a project. A project may define extra
    assemblies by adding ReferencePath items. These were being
    filtered out since only the output parameters defined by the targets
    ResolveAssemblyReferencesDesignTime and ResolveProjectReferencesDesignTime
    which missed some references.
    
    Fixes VSTS #991961 - Project model reports wrong references for
    WebTools projects

---
+++ main/tests/test-projects/ReferencePathTest/ReferencePathTest/MyClass.cs
@@ -0,0 +1,15 @@
+﻿
+using System;
+using System.ComponentModel.Composition;
+
+namespace ReferencePathTest
+{
+	public class MyClass
+	{
+		CreationPolicy policy;
+
+		public MyClass ()
+		{
+		}
+	}
+}
