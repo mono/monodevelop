928d08d0b72396e14d1326022a9959053e3368aa
nosami <jasonimison@gmail.com>
2019-10-27 20:12:50 +0000

    Add wizard first page

--- main/src/addins/MonoDevelop.AspNetCore/MonoDevelop.AspNetCore.Scaffolding/Scaffolder.cs
+++ main/src/addins/MonoDevelop.AspNetCore/MonoDevelop.AspNetCore.Scaffolding/Scaffolder.cs
@@ -24,6 +24,7 @@
 // OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
 // THE SOFTWARE.
 using System;
+using System.Collections.Generic;
 using System.ComponentModel;
 using System.Reflection;
 using System.Threading;
@@ -33,17 +34,220 @@ using MonoDevelop.Ide.Gui.Wizard;
 using Xwt;
 using Xwt.Drawing;
 
-namespace Scaffolder
+namespace MonoDevelop.AspNetCore.Scaffolding
 {
-    class ScaffolderTemplateSelect : WizardDialogPageBase
-    {
-        public ScaffolderTemplateSelect()
-        {
-        }
+	class ScaffolderArgs
+	{ 
+    
+    }
+
+	abstract class ScaffolderWizardPageBase : WizardDialogPageBase
+	{
+		string subSubTitle;
+		protected ScaffolderWizardPageBase (ScaffolderArgs args)
+		{
+			Args = args;
+		}
+
+		public string SubSubTitle {
+			get => subSubTitle;
+			protected set => subSubTitle = value;
+		}
+
+		public ScaffolderArgs Args { get; }
 
         protected override Control CreateControl()
         {
-            return new MonoDevelop.Components.XwtControl(new ListBox());
+            var icon = new Xwt.ImageView(StockIcons.Information);
+            var mainBox = new VBox { Spacing = 0 };
+			var label = new Label (subSubTitle);
+			label.Font = label.Font.WithSize (18);
+			mainBox.PackStart (label, margin: 30);
+			var separator = new HSeparator ();
+			mainBox.PackStart (separator);
+
+			mainBox.PackStart (GetMainControl (), margin: 20);
+			//mainBox.ExpandVertical = true;
+			return new XwtControl (mainBox);
+		}
+
+		protected abstract Widget GetMainControl ();
+	}
+
+	class ScaffolderField
+	{ 
+        string CommandLineName { get; }
+		Type Type { get; }
+        string DisplayName { get; }
+
+		public ScaffolderField(string commandLineName, string displayName, Type type)
+		{
+			CommandLineName = commandLineName;
+			DisplayName = displayName;
+			Type = type;
+		}
+    }
+
+	interface IScaffolder
+	{
+		string Name { get; }
+		string CommandLineName { get; }
+		IEnumerable<ScaffolderField> Fields { get; }
+    }
+
+	class EmptyMvcControllerScaffolder : IScaffolder
+	{
+		public string Name => "MVC Controller - Empty";
+		public string CommandLineName => "controller";
+
+		public IEnumerable<ScaffolderField> Fields =>
+			new [] { new ScaffolderField ("name", "Name", typeof (string)) };
+	}
+
+	class MvcControllerWithActionsScaffolder : IScaffolder
+	{
+		public string Name => "MVC Controller with read / write actions";
+		public string CommandLineName => "controller";
+
+		public IEnumerable<ScaffolderField> Fields =>
+			new [] { new ScaffolderField ("name", "Name", typeof (string)) };
+	}
+
+	class EmptyApiControllerScaffolder : IScaffolder
+	{
+		public string Name => "API Controller - Empty";
+		public string CommandLineName => "controller";
+
+		public IEnumerable<ScaffolderField> Fields =>
+			new [] { new ScaffolderField ("name", "Name", typeof (string)) };
+	}
+
+	class ApiControllerWithActionsScaffolder : IScaffolder
+	{
+		public string Name => "API Controller with read / write actions";
+		public string CommandLineName => "controller";
+
+		public IEnumerable<ScaffolderField> Fields =>
+			new [] { new ScaffolderField ("name", "Name", typeof (string)) };
+	}
+
+	class ApiControllerEntityFrameworkScaffolder : IScaffolder
+	{
+		public string Name => "API Controller with actions using Entity Framework";
+		public string CommandLineName => "controller";
+
+		public IEnumerable<ScaffolderField> Fields =>
+			new [] { new ScaffolderField ("name", "Name", typeof (string)) };
+	}
+
+	class RazorPageScaffolder : IScaffolder
+	{
+		public string Name => "Razor Page";
+		public string CommandLineName => "controller";
+
+		public IEnumerable<ScaffolderField> Fields =>
+			new [] { new ScaffolderField ("name", "Name", typeof (string)) };
+	}
+
+	class RazorPageEntityFrameworkScaffolder : IScaffolder
+	{
+		public string Name => "Razor Page using Entity Framework";
+		public string CommandLineName => "controller";
+
+		public IEnumerable<ScaffolderField> Fields =>
+			new [] { new ScaffolderField ("name", "Name", typeof (string)) };
+	}
+
+	class RazorPageEntityFrameworkCrudScaffolder : IScaffolder
+	{
+		public string Name => "Razor Page using Entity Framework (CRUD)";
+		public string CommandLineName => "controller";
+
+		public IEnumerable<ScaffolderField> Fields =>
+			new [] { new ScaffolderField ("name", "Name", typeof (string)) };
+	}
+
+	class IdentityScaffolder : IScaffolder
+	{
+		public string Name => "Identity";
+		public string CommandLineName => "controller";
+
+		public IEnumerable<ScaffolderField> Fields =>
+			new [] { new ScaffolderField ("name", "Name", typeof (string)) };
+	}
+
+	class LayoutScaffolder : IScaffolder
+	{
+		public string Name => "Layout";
+		public string CommandLineName => "controller";
+
+		public IEnumerable<ScaffolderField> Fields =>
+			new [] { new ScaffolderField ("name", "Name", typeof (string)) };
+	}
+
+	class ScaffolderTemplateSelect : ScaffolderWizardPageBase
+    {
+		public ScaffolderTemplateSelect () : base (new ScaffolderArgs ())
+		{
+			this.CanGoBack = true;
+			this.CanGoNext = true;
+			this.PageSubtitle = "Select Scaffolder";
+			this.PageIcon = StockIcons.Question;
+			this.SubSubTitle = "Select Scaffolder SUB";
+		}
+
+		private Lazy<IScaffolder[]> GetScaffolders()
+		{ 
+			var scaffolders = new IScaffolder [] {
+				new EmptyMvcControllerScaffolder(),
+				new MvcControllerWithActionsScaffolder(),
+				new EmptyApiControllerScaffolder(),
+				new ApiControllerWithActionsScaffolder(),
+				new ApiControllerEntityFrameworkScaffolder(),
+				new RazorPageScaffolder(),
+				new RazorPageEntityFrameworkScaffolder(),
+				new RazorPageEntityFrameworkCrudScaffolder(),
+				new IdentityScaffolder(),
+				new LayoutScaffolder()
+			};
+			return new Lazy<IScaffolder[]>(() => scaffolders);
+        }
+
+        protected override Widget GetMainControl()
+        {
+			var icon = new DataField<Image> ();
+			var name = new DataField<string> ();
+
+			var listStore = new ListStore (icon, name);
+
+			foreach (var scaffolder in GetScaffolders().Value) {
+				var row = listStore.AddRow ();
+				var png = Image.FromResource ("file-web-32.png");
+				listStore.SetValue (row, icon, png);
+				listStore.SetValue (row, name, scaffolder.Name);
+			}
+
+			var listBox = new ListBox ();
+			listBox.Views.Add (new ImageCellView (icon));
+			listBox.Views.Add (new TextCellView (name));
+
+			listBox.DataSource = listStore;
+			//listBox.Items.Add ("MVC Controller – Empty");
+			//listBox.Items.Add ("MVC Controller with read / write actions");
+			//listBox.Items.Add ("API Controller – Empty");
+			//listBox.Items.Add ("API Controller with read / write actions");
+			//listBox.Items.Add ("API Controller with actions using Entity Framework");
+			//listBox.Items.Add ("Razor Page");
+			//listBox.Items.Add ("Razor Page using Entity Framework");
+			//listBox.Items.Add ("Razor Page using Entity Framework (CRUD)");
+			//listBox.Items.Add ("Identity");
+			//listBox.Items.Add ("Layout ");
+			listBox.HeightRequest = 400;
+			listBox.WidthRequest = 300;
+			//listBox.Font = MonoDevelop.Ide.Gui.Styles.DefaultFont;
+			//listBox.ExpandVertical = true;
+			//mainBox. = new WidgetSpacing (20, 20, 20, 20);
+			return listBox;
         }
     }
 
@@ -51,88 +255,14 @@ namespace Scaffolder
     {
         public ScaffolderWizard(string title, IWizardDialogPage firstPage) : base(title, StockIcons.Information, null, firstPage)
         {
-            var rightSideImage = new Xwt.ImageView(StockIcons.ZoomIn);
+			this.DefaultPageSize = new Size (600, 500);
 
+			var rightSideImage = new Xwt.ImageView(Xwt.Drawing.Image.FromResource ("aspnet-wizard-page.png"));
             var rightSideWidget = new Xwt.FrameBox (rightSideImage);
 			rightSideWidget.BackgroundColor = MonoDevelop.Ide.Gui.Styles.Wizard.PageBackgroundColor;
-        }
-    }
-
-    class ScaffolderSelect : Xwt.Window
-
-    {
-        public ScaffolderSelect()
-        {
-            Title = "Add New Scaffolded Item";
-            Width = 600;
-            Height = 500;
-            Resizable = true;
-
-            var mainBox = new VBox { Spacing = 0 };
-
-            var icon = new Xwt.ImageView(StockIcons.Information);
-
-            var title = new Label
-            {
-                Markup = $"<b>Configure Scaffolding</b>",
-            };
-            title.Font = title.Font.WithSize(title.Font.Size + 2);
-
-            var assembly = this.GetType().Assembly;
-            var assemblyName = assembly.GetName();
-
-            var scopyright = (assembly.GetCustomAttribute(typeof(AssemblyCopyrightAttribute)) as AssemblyCopyrightAttribute)?.Copyright ?? "Copyright © Xamarin 2017";
-            var sversion = assemblyName.Version.ToString(assemblyName.Version.Revision > 0 ? 4 : 3);
-
-            var gitVersionInformationType = assembly.GetType(assemblyName.Name + ".GitVersionInformation");
-            var gitVersionBranch = "Branch";
-            var gitVersionSha = "Sha";
-            var sgitversion = string.Empty;
-            if (!string.IsNullOrEmpty(gitVersionBranch) && !string.IsNullOrEmpty(gitVersionSha))
-                sgitversion = $"({gitVersionBranch}/{gitVersionSha})";
-
-            Label gitVersion = null;
-            if (!string.IsNullOrEmpty(sgitversion))
-                gitVersion = new Label(sgitversion) { TextAlignment = Alignment.Center };
-
-            var version = new Label($"Version {sversion}") { TextAlignment = Alignment.Center };
-            var copyright = new Label(scopyright) { TextAlignment = Alignment.Center };
-
-            mainBox.PackStart(title, marginTop: 15);
-            mainBox.PackStart(version, marginTop: 10);
-            mainBox.PackStart(icon);
-            if (gitVersion != null)
-                mainBox.PackStart(gitVersion);
-            mainBox.PackStart(copyright, marginTop: 10);
-
-            var listBox = new ListBox();
-            listBox.Items.Add("MVC Controller – Empty");
-            listBox.Items.Add("MVC Controller with read / write actions");
-            listBox.Items.Add("API Controller – Empty");
-            listBox.Items.Add("API Controller with read / write actions");
-            listBox.Items.Add("API Controller with actions using Entity Framework");
-            listBox.Items.Add("Razor Page");
-            listBox.Items.Add("Razor Page using Entity Framework");
-            listBox.Items.Add("Razor Page using Entity Framework (CRUD) ");
-            listBox.Items.Add("Identity");
-            listBox.Items.Add("Layout ");
-            mainBox.PackStart (listBox, true);
-
-            Padding = new WidgetSpacing(20, 20, 20, 20);
-            var hbox = new HBox();
-            var cancelButton = new Button("Cancel");
-            var backButton = new Button("Back");
-            var nextButton = new Button("Next");
-
-            nextButton.IsDefault = true;
-
-            hbox.PackStart(cancelButton);
-            hbox.PackEnd(nextButton);
-            hbox.PackEnd(backButton);
-            
-            mainBox.PackEnd(hbox);
-
-            Content = mainBox;
-        }
+			////rightSideWidget.ExpandHorizontal = true;
+			//rightSideWidget.ExpandVertical = true;
+			this.RightSideWidget = new XwtControl (rightSideWidget);
+		}
     }
 }
