5758253b9456265335b4d5330bd46ba6b9f1c367
Matt Ward <matt.ward@microsoft.com>
2020-01-21 13:09:27 +0000

    [Core] Ensure multi-run config updated after project reload
    With a multi-run configuration defined for the solution, if a
    project is reloaded, the multi-run config would not have the
    latest project run configuration. This would result in changes to
    the project run configuration in project options, such as adding
    or removing startup arguments, not being used when the multi-run
    configuration was used to run the solution.
    
    On reloading the new project run configuration is resolved and
    updated in the multi-run configuration. Previously only the
    SolutionItem was updated.
    
    Fixes VSTS #653269 - Multi startup project run configuration -
    arguments not passed after reload

---
+++ main/tests/test-projects/test-multi-run-config-reload/ProjectA/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ProjectA
+{
+	class MainClass
+	{
+		public static void Main (string[] args)
+		{
+			Console.WriteLine ("Project A: args.Length={0}", args.Length);
+		}
+	}
+}
